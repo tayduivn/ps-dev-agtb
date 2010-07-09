223ede6a9af6e0670276c9d38151637ab9846a63
Jenny Gonsalves <jenny@sugarcrm.com>
2010-07-08 18:00:40 -0700

    Merging r57080 through r57226 from maint601.

--- sugarcrm/modules/DynamicFields/templates/Fields/Forms/datetimecombo.php
+++ sugarcrm/modules/DynamicFields/templates/Fields/Forms/datetimecombo.php
@@ -29,13 +29,23 @@ if(!defined('sugarEntry') || !sugarEntry) die('Not A Valid Entry Point');
 require_once('modules/DynamicFields/templates/Fields/TemplateDatetimecombo.php');
 
 function get_body(&$ss, $vardef){
-	$defaultTime = '12:00pm';
-	$hours = "12";
-	$minitues = "00";
-	$meridiem = "pm";
+	$defaultTime = '';
+	$hours = "";
+	$minitues = "";
+	$meridiem = "";
 	$td = new TemplateDatetimecombo();
 	$ss->assign('default_values', array_flip($td->dateStrings));
-	$ss->assign('default_hours_values', array_flip($td->hoursStrings));
+	
+    global $timedate;
+    $user_time_format = $timedate->get_user_time_format();
+    $show_meridiem = preg_match('/pm$/i', $user_time_format) ? true : false;
+    if($show_meridiem) {
+    	$ss->assign('default_hours_values', array_flip($td->hoursStrings));
+    } else {
+    	$ss->assign('default_hours_values', array_flip($td->hoursStrings24));
+    }
+
+    $ss->assign('show_meridiem', $show_meridiem);
 	$ss->assign('default_minutes_values', array_flip($td->minutesStrings));
 	$ss->assign('default_meridiem_values', array_flip($td->meridiemStrings));
 	if(isset($vardef['display_default']) && strstr($vardef['display_default'] , '&')){
@@ -45,6 +55,14 @@ function get_body(&$ss, $vardef){
 		$hours = substr($defaultTime, 0, 2); 
 		$minitues = substr($defaultTime, 3, 2);
 		$meridiem = substr($defaultTime, 5, 2);
+		if(!$show_meridiem) {
+		   preg_match('/(am|pm)$/i', $meridiem, $matches);
+		   if(strtolower($matches[0]) == 'am' && $hours == 12) {
+		   	  $hours = '00';
+		   } else if (strtolower($matches[0]) == 'pm' && $hours != 12) {
+		   	  $hours += 12;
+		   }
+		}
 		$ss->assign('default_date', $date);
 	}
 	$ss->assign('default_hours', $hours);
