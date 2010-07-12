6a9a8487aaafd44d000706673b7a10c406c05733
Jenny Gonsalves <jenny@sugarcrm.com>
2010-07-12 16:40:01 -0700

    Bug 10006: Adding support for IS NOT qualifier for Name fields.

--- sugarcrm/modules/Reports/templates/templates_modules_def_js.php
+++ sugarcrm/modules/Reports/templates/templates_modules_def_js.php
@@ -461,8 +461,10 @@ filter_defs['url'] = qualifiers;
 
 var qualifiers_name = new Array();
 var is_def = {name:'is',value:'<?php echo $mod_strings['LBL_IS']; ?>'};
+var is_not_def = {name:'is_not',value:'<?php echo $mod_strings['LBL_IS_NOT']; ?>'};
 var one_of_def = {name:'one_of',value:'<?php echo $mod_strings['LBL_ONE_OF']; ?>'};
 qualifiers_name = qualifiers_name.concat(qualifiers);
+qualifiers_name.unshift(is_not_def);
 qualifiers_name.unshift(is_def);
 filter_defs['name'] = qualifiers_name;
 filter_defs['fullname'] = qualifiers_name;
