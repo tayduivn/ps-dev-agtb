e8ee28391aa125876a60175e780fbb15e5954151
Stanislav Malyshev <smalyshev@gmail.com>
2010-07-22 16:46:10 -0700

    Bug #37478: proper fix using the security query API

--- sugarcrm/modules/KBDocuments/KBDocument.php
+++ sugarcrm/modules/KBDocuments/KBDocument.php
@@ -256,7 +256,7 @@ class KBDocument extends SugarBean {
     function create_new_list_query($order_by, $where,$filter=array(),$params=array(), $show_deleted = 0,$join_type='', $return_array = false,$parentbean){
         global $current_user;
         $ret_array=array();
-        $ret_array['select'] = "SELECT jt0.id assigned_user_id, jt0.user_name assigned_user_name, jt1.id kbdoc_approver_id, jt1.user_name kbdoc_approver_name, kvr.views_number views_number";
+        $ret_array['select'] = "SELECT DISTINCT jt0.id assigned_user_id, jt0.user_name assigned_user_name, jt1.id kbdoc_approver_id, jt1.user_name kbdoc_approver_name, kvr.views_number views_number";
         $ret_array['select'] .= ", kbdocuments.id, kbdocuments.kbdocument_name, kbdocuments.active_date, kbdocuments.exp_date, kbdocuments.status_id, kbdocuments.date_entered date_entered, kbdocuments.date_modified, kbdocuments.deleted, kbdocuments.is_external_article, kbdocuments.modified_user_id";
         //BEGIN SUGARCRM flav=ent ONLY
          $ret_array['select'] .= ", kbdocuments.created_by";
@@ -264,15 +264,13 @@ class KBDocument extends SugarBean {
 
         //BEGIN SUGARCRM flav=pro ONLY
         if (!is_admin($current_user) && !$this->disable_row_level_security){
-            $ret_array['select'] .= ", jt3.team_id ";
+            $ret_array['select'] .= ", kbdocuments.team_id ";
         }
         //END SUGARCRM flav=pro ONLY
-        $ret_array['from'] = " FROM kbdocuments left join kbdocuments_views_ratings kvr ON kbdocuments.id = kvr.kbdocument_id  LEFT JOIN  users jt0 ON jt0.id= kbdocuments.assigned_user_id AND jt0.deleted=0  LEFT JOIN  users jt1 ON jt1.id= kbdocuments.kbdoc_approver_id AND jt1.deleted=0";
+        $ret_array['from'] = " FROM kbdocuments left join kbdocuments_views_ratings kvr ON kbdocuments.id = kvr.kbdocument_id  LEFT JOIN  users jt0 ON jt0.id= kbdocuments.assigned_user_id AND jt0.deleted=0  LEFT JOIN  users jt1 ON jt1.id= kbdocuments.kbdoc_approver_id AND jt1.deleted=0 ";
         //BEGIN SUGARCRM flav=pro ONLY
         if (!is_admin($current_user) && !$this->disable_row_level_security){
-        	$ret_array['from'] .=  " INNER JOIN team_sets_teams ON team_sets_teams.team_set_id = kbdocuments.team_set_id";
-        	$ret_array['from'] .=  " INNER JOIN team_memberships jt3 ON team_sets_teams.team_id = jt3.team_id";
-        	$ret_array['from'] .=  " AND jt3.user_id = '$current_user->id' AND jt3.deleted=0 ";
+            $this->add_team_security_where_clause($ret_array['from']);
         }
         //END SUGARCRM flav=pro ONLY
         $ret_array['where'] = ' WHERE '. $where;
@@ -281,7 +279,7 @@ class KBDocument extends SugarBean {
         $temp_order = strtolower($temp_order);
         if($temp_order == 'asc'  ||  $temp_order == 'desc'){$order_by = '';}
 
-        $ret_array['order_by'] = " GROUP BY kbdocuments.id ".(!empty($order_by) ? ' ORDER BY '. $order_by : '  ORDER BY kbdocuments.date_entered');
+        $ret_array['order_by'] = (!empty($order_by) ? ' ORDER BY '. $order_by : '  ORDER BY kbdocuments.date_entered');
 
         $ret_array['from_min'] = $ret_array['from'];
         return $ret_array;
