110ee8c7d5644db2d152f2c3e6e9c5df36655fad
John Mertic <jmertic@sugarcrm.com>
2010-08-12 17:25:23 -0400

    Since the updated unzip() function isn't available at this point, just remove the two problematic directories instead

--- sugarcrm/modules/UpgradeWizard/silentUpgrade_step1.php
+++ sugarcrm/modules/UpgradeWizard/silentUpgrade_step1.php
@@ -607,14 +607,15 @@ $install_file = clean_path("{$cwd}/{$sugar_config['upload_dir']}upgrades/patch/"
 $_SESSION['unzip_dir'] = $unzip_dir;
 $_SESSION['install_file'] = $install_file;
 $_SESSION['zip_from_dir'] = $zip_from_dir;
-
+rmdir_recursive($unzip_dir.'/scripts');
+rmdir_recursive($unzip_dir.'/manifest.php');
 mkdir_recursive($unzip_dir);
 if(!is_dir($unzip_dir)) {
 	fwrite(STDERR,"\n{$unzip_dir} is not an available directory\nFAILURE\n");
 	exit(1);
 }
 
-unzip($argv[1], $unzip_dir, true);
+unzip($argv[1], $unzip_dir);
 // mimic standard UW by copy patch zip to appropriate dir
 copy($argv[1], $install_file);
 ////	END UPGRADE PREP
