1425a9c6a77186493f5ea19044ad907b476cd5b9
John Mertic <jmertic@sugarcrm.com>
2010-08-17 23:06:07 -0400

    Bug 38723 - Add status filter to My Meetings dashlet.

--- sugarcrm/modules/Meetings/Dashlets/MyMeetingsDashlet/MyMeetingsDashlet.data.php
+++ sugarcrm/modules/Meetings/Dashlets/MyMeetingsDashlet/MyMeetingsDashlet.data.php
@@ -32,8 +32,9 @@ if(!defined('sugarEntry') || !sugarEntry) die('Not A Valid Entry Point');
 global $current_user;
 
 $dashletData['MyMeetingsDashlet']['searchFields'] = array('name'             => array('default' => ''),
+                                                          'status'           => array('default' => array('Planned')),
                                                           'date_start'       => array('default' => ''),
-                                                          'date_entered'     => array('default' => ''),
+                                                          'date_entered'     => array('default' => ''),                                                         
                                                           //BEGIN SUGARCRM flav=pro ONLY
                                                           'team_id'          => array('default' => '', 'label'=>'LBL_TEAMS'),
                                                           //END SUGARCRM flav=pro ONLY
