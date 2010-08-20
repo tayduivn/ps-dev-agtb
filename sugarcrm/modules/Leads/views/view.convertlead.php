3507e1f4cfc6ca23e8efcaa62513f36b5d9e8323
Jenny Gonsalves <jenny@sugarcrm.com>
2010-08-20 12:53:18 -0700

    Merging with maint_6_0_1 revisions 57708:57838

--- sugarcrm/modules/Leads/views/view.convertlead.php
+++ sugarcrm/modules/Leads/views/view.convertlead.php
@@ -93,6 +93,10 @@ class ViewConvertLead extends SugarView
 	                {
 	                    $focus->name = $this->focus->account_name;
 	                } 
+	                else if ($module == "Opportunities" && $field == 'amount')
+	                {
+	                    $focus->amount = $this->focus->opportunity_amount;
+	                } 
 	                else if ($field == "id")
                     {
 						//If it is not a contact, don't copy the ID from the lead
@@ -112,7 +116,7 @@ class ViewConvertLead extends SugarView
             }
             
             //Copy over email data
-            $ev->setup($module, $focus, $this->medataDataFile, "modules/Leads/tpls/ConvertLead.tpl");
+            $ev->setup($module, $focus, $this->medataDataFile, "modules/Leads/tpls/ConvertLead.tpl", false);
             $ev->process();
             echo($ev->display(false));
             echo($this->getValidationJS($module, $focus, $vdef[$ev->view]));
