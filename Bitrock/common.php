9e9d5a34f7a36710227fb18d36a25069b91b14c1
kjing <kjing@sugarcrm.com>
2010-08-20 13:59:55 -0700

    update pineapple with 6.2.0beta

--- Bitrock/common.php
+++ Bitrock/common.php
@@ -2,7 +2,7 @@
 
 $LOCAL_APACHE_DOCROOT   = "/var/www/html";
 global $VER;
-if ($VER == '') $VER="6.1.0beta";
+if ($VER == '') $VER="6.2.0beta";
 $VER_SUFFIX     = "BitRock-$VER";
 $SUGAR_VERSION  = $VER;
 $PUBLISH_DIR    = "/home/public/builds/$VER_SUFFIX";
