44542388c80d66b146e5292acbf0fa4ff2cd15e8
Jenny Gonsalves <jenny@sugarcrm.com>
2010-09-02 17:29:31 -0700

    Bug 39417 - Adding more missing 60x to 61x indices.

--- build/scripts_for_patch/60x_to_61x_mssql.sql
+++ build/scripts_for_patch/60x_to_61x_mssql.sql
@@ -12,3 +12,14 @@ CREATE NONCLUSTERED INDEX idx_leads_primary on leads_audit (id);
 CREATE NONCLUSTERED INDEX idx_opportunities_primary on opportunities_audit (id);
 CREATE NONCLUSTERED INDEX idx_project_task_primary on project_task_audit (id);
 
+-- //BEGIN SUGARCRM flav=pro ONLY
+CREATE NONCLUSTERED INDEX idx_contracts_primary on contracts_audit (id);
+CREATE NONCLUSTERED INDEX idx_kbcontents_primary on kbcontents_audit (id);
+CREATE NONCLUSTERED INDEX idx_products_primary on products_audit (id);
+CREATE NONCLUSTERED INDEX idx_quotes_primary on quotes_audit (id);
+CREATE NONCLUSTERED INDEX pk_team_sets_teams on team_sets_teams (id);
+-- //END SUGARCRM flav=pro ONLY
+
+
+
+
