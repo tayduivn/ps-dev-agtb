6dd03a49a206627141d1948d8b115d012c3ecf82
lam <lam@192.168.0.6>
2010-09-03 20:33:58 -0500

    bug 39478

--- sugarcrm/modules/Reports/index.php
+++ sugarcrm/modules/Reports/index.php
@@ -147,9 +147,9 @@ else
 if ( empty($_REQUEST['search_form_only']) ) {
     $params = array();
     if(!empty($_REQUEST['favorite']))
-        $params[] = $mod_strings['LBL_FAVORITES_TITLE'];
+        $params[] = "<span class='pointer'>&raquo;</span>".$mod_strings['LBL_FAVORITES_TITLE'];
     else
-        $params[] = $mod_strings['LBL_MODULE_NAME'];
+        $params[] = "<span class='pointer'>&raquo;</span>".$app_strings['LBL_SEARCH'];
     echo getClassicModuleTitle("Reports", $params, true);
 }
 
