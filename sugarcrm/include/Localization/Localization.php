bbf9886f333a0310e7c0b8b695ec62a066dccf38
John Mertic <jmertic@sugarcrm.com>
2010-09-07 14:18:17 -0400

    Bug 39171 - Default the Charset selector in the Emails settings to the sugar_config default_email_charset setting when it is not set in the user's settings. Also make sure we are checking the correct user preference in Localization::getPrecedentPreference()

--- sugarcrm/include/Localization/Localization.php
+++ sugarcrm/include/Localization/Localization.php
@@ -57,7 +57,7 @@ class Localization {
 		'KOI8-R',       //Cyrillic Russian
 		'KOI8-U',       //Cyrillic Ukranian
 		'SJIS',         //MS Japanese
-		'UTF-8',        //'UTF-8
+		'UTF-8',        //UTF-8
 		);
 	var $localeNameFormat;
 	var $localeNameFormatDefault;
@@ -120,9 +120,9 @@ class Localization {
 		// Bug 39171 - If we are asking for default_email_charset, check in emailSettings['defaultOutboundCharset'] as well
 		if ( $prefName == 'default_email_charset' ) {
 		    if($user != null) {
-                $emailSettings = $user->getPreference('emailSettings');
+                $emailSettings = $user->getPreference('emailSettings', 'Emails');
             } elseif(!empty($current_user)) {
-                $emailSettings = $current_user->getPreference('emailSettings');
+                $emailSettings = $current_user->getPreference('emailSettings', 'Emails');
             }
             if ( isset($emailSettings['defaultOutboundCharset']) ) {
                 $userPref = $emailSettings['defaultOutboundCharset'];
