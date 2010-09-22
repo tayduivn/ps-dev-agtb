1c7cc7612a8ef4cdd4978e9850cbaf56c476302e
eddy <eddy@.cup1.sugarcrm.net>
2010-09-22 15:00:06 -0700

    bug 39580 --  Refactored code to only store Get and Post arrays from request in user preferences.

--- sugarcrm/modules/MySettings/StoreQuery.php
+++ sugarcrm/modules/MySettings/StoreQuery.php
@@ -98,23 +98,27 @@ class StoreQuery{
 
 	
 	function saveFromRequest($name){
-		if(isset($_REQUEST['query'])){
-			if(!empty($_REQUEST['clear_query']) && $_REQUEST['clear_query'] == 'true'){
+		//make new request array out of superglobal post and get arrays, this is done
+		//manually so as to avoid cookie array
+		$request =  array_merge($_POST,$_GET);	
+
+		if(isset($request['query'])){
+			if(!empty($request['clear_query']) && $request['clear_query'] == 'true'){
 				$this->clearQuery($name);
 				return;	
 			}
 			$saveType = $this->getSaveType($name);
 			
 			if($saveType == 'myitems'){
-				if(!empty($_REQUEST['current_user_only'])){
-					$this->query['current_user_only'] = $_REQUEST['current_user_only'];
+				if(!empty($request['current_user_only'])){
+					$this->query['current_user_only'] = $request['current_user_only'];
 					$this->query['query'] = true;
 				}
 				$this->saveQuery($name);
 				
 			}else if($saveType == 'all'){
                 $blockVariables = array('mass', 'uid', 'massupdate', 'delete', 'merge', 'selectCount', 'current_query_by_page');
-				$this->query = $_REQUEST;
+				$this->query = $request;
                 foreach($blockVariables as $block) {
                     unset($this->query[$block]);
                 }
