8ae1dd3f70e8f83239ae6aa8d395309566be844f
Jenny Gonsalves <jenny@sugarcrm.com>
2010-09-22 17:42:57 -0700

    Merging with maint_6_0_1 (svn merge -r 58149:58250)

--- sugarcrm/modules/UpgradeWizard/uw_utils.php
+++ sugarcrm/modules/UpgradeWizard/uw_utils.php
@@ -29,7 +29,7 @@ if(!defined('sugarEntry') || !sugarEntry) die('Not A Valid Entry Point');
  * by SugarCRM are Copyright (C) 2005 SugarCRM, Inc.; All Rights Reserved.
  */
 
-// $Id: uw_utils.php 56650 2010-05-24 18:53:17Z jenny $
+// $Id: uw_utils.php 58174 2010-09-14 18:18:39Z kjing $
 
 /**
  * Backs-up files that are targeted for patch/upgrade to a restore directory
@@ -3917,7 +3917,7 @@ function set_upgrade_progress($currStep,$currState,$currStepSub='',$currStepSubS
 			if($currStepSub != null && $currStepSubState !=null){
 				//check if new status to be set or update
 				//get the latest in array. since it has sub components prepare an array
-				if(is_array($upgrade_config[sizeof($upgrade_config)][$currStep])){
+				if(!empty($upgrade_config[sizeof($upgrade_config)][$currStep]) && is_array($upgrade_config[sizeof($upgrade_config)][$currStep])){
 					$latestStepSub = currSubStep($upgrade_config[sizeof($upgrade_config)][$currStep]);
 					if($latestStepSub == $currStepSub){
 						$upgrade_config[sizeof($upgrade_config)][$currStep][$latestStepSub]=$currStepSubState;
