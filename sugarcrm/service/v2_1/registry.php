b4369f5719b048cd75550d6c62a076ef7e5a0a2a
dwheeler@sugarcrm.com <dwheeler@dwheeler-laptop.(none)>
2010-09-22 17:47:42 -0700

    Merging with maint_6_0_1 (svn merge -r 58149:58250)

--- sugarcrm/service/v2_1/registry.php
+++ sugarcrm/service/v2_1/registry.php
@@ -94,7 +94,7 @@ class registry_v2_1 extends registry {
 			'link_value'=>array('name'=>'link_value', 'type'=>'tns:link_value'),
 			)
 		);
-		
+		//BEGIN SUGARCRM flav=pro ONLY
 		 $this->serviceClass->registerType(
 			'report_field_list',
 			'complexType',
@@ -107,7 +107,7 @@ class registry_v2_1 extends registry {
 			),
 			'tns:field_list2'
 		);
-		
+		//END SUGARCRM flav=pro ONLY
 		$this->serviceClass->registerType(
 			'field_list2',
 			'complexType',
@@ -118,7 +118,7 @@ class registry_v2_1 extends registry {
 			"field_list"=>array('name'=>'field_list', 'type'=>'tns:field_list'),
 			)
 		);
-		
+		//BEGIN SUGARCRM flav=pro ONLY
 		 $this->serviceClass->registerType(
 			'report_entry_list',
 			'complexType',
@@ -131,7 +131,7 @@ class registry_v2_1 extends registry {
 			),
 			'tns:entry_list2'
 		);
-		
+		//END SUGARCRM flav=pro ONLY
 		$this->serviceClass->registerType(
 			'entry_list2',
 			'complexType',
