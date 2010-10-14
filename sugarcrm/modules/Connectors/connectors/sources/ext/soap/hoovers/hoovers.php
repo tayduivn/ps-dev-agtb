d5b7712df4b39bbd5dfae0ff9f99472e03e9e48e
John Mertic <jmertic@sugarcrm.com>
2010-10-14 13:55:27 -0400

    Added quick test to see where this is failing at.

--- sugarcrm/modules/Connectors/connectors/sources/ext/soap/hoovers/hoovers.php
+++ sugarcrm/modules/Connectors/connectors/sources/ext/soap/hoovers/hoovers.php
@@ -55,7 +55,7 @@ if(!defined('sugarEntry') || !sugarEntry) die('Not A Valid Entry Point');
 	 		$this->_client = new nusoapclient($properties['hoovers_endpoint'], true);
             $this->_client->setHeaders("<API-KEY xmlns='http://webservice.hoovers.com'>{$clientKey}</API-KEY>");
             */
-	 		
+	 		display_stack_trace();
 	 		$this->_client = new SoapClient($properties['hoovers_wsdl'],
                 array('trace' => true, 'exceptions' => true, 'location' => $properties['hoovers_endpoint'])
             );
