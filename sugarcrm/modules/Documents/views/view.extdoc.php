50e6d79ddf4f64f8b0454a38940e422e23e4e5f7
lam <lam@192.168.0.6>
2010-10-14 00:13:19 -0500

    mockup document list view

--- sugarcrm/modules/Documents/views/view.extdoc.php
+++ sugarcrm/modules/Documents/views/view.extdoc.php
@@ -36,6 +36,67 @@ class DocumentsViewExtdoc extends ViewDetail
 	var $options = array('show_header' => false, 'show_title' => false, 'show_subpanels' => false, 'show_search' => true, 'show_footer' => false, 'show_javascript' => false, 'view_print' => false,);
 
  	public function display(){
- 		echo "hello world";	
+		echo "<script>
+		function fillSelect(filename) {
+			var oForm = document.forms[form_id];
+			oForm.elements[\"file_name\"].value = filename;
+			DCMenu.closeOverlay();
+		}
+		</script>";
+		echo "<style>
+		table.dcListView th,table.dcListView td {
+			text-align: left;
+			color: #666;
+			padding: 5px 10px;
+		}
+		
+		table.dcListView th {
+			background-color:#EBEBED;
+		}
+
+		table.dcListView td {
+			border-bottom: 1px solid #eee;
+		}
+		
+		</style>";
+		echo "
+		<table>
+			<tr>
+			<td>
+			<input type=\"text\" id=\"search\" name=\"search\">
+			</td>
+			<td>
+			<input type=\"submit\" name=\"submit\" value=\"Search Documents\">
+			</td>
+			</tr>
+		</table>
+		
+		<table width='500' class='dcListView' cellpadding='0' cellspacing='0'>
+		<tr>
+			<th>Type</th>
+			<th>Name</th>
+			<th>Last Modified</th>
+			<th>Owner</th>
+		</tr>
+		<tr>
+			<td class='type' width='20'><img src='themes/default/images/xls_image_inline.gif'></td>
+			<td class='name'><a href='javascript: fillSelect(\"sales.xls\")'>Sales Matrix</a></td>
+			<td class='lastModified'>12/12/10 05:30:00</td>
+			<td class='owner'>Majed Itani</td>
+		</tr>
+		<tr>
+			<td class='type' width='20'><img src='themes/default/images/pdf_image_inline.gif'></td>
+			<td class='name'><a href='javascript: fillSelect(\"whitepaper.pdf\")'>Product White Paper</a></td>
+			<td class='lastModified'>12/11/10 05:30:00</td>
+			<td class='owner'>Roger Smith</td>
+		</tr>
+		<tr>
+			<td class='type' width='20'><img src='themes/default/images/doc_image_inline.gif'></td>
+			<td class='name'><a href='javascript: fillSelect(\"letter.doc\")'>Company Letter Head</a></td>
+			<td class='lastModified'>02/11/10 05:30:00</td>
+			<td class='owner'>Roger Smith</td>
+		</tr>
+		</table>";
+
  	}
 }
