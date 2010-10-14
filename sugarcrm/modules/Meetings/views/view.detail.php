afe97846fcb97d800b56eda62b0bbd278b893569
Majed Itani <mitani@mitani.cup1.sugarcrm.net>
2010-10-13 18:07:57 -0700

    adds a listbytype list view for displaying a list of a specific meeting type

--- sugarcrm/modules/Meetings/views/view.detail.php
+++ sugarcrm/modules/Meetings/views/view.detail.php
@@ -10,7 +10,7 @@ class MeetingsViewDetail extends ViewDetail {
    }
 
    function display() {
-      if ($this->bean->type != 'Other') {
+      if ($this->bean->type != 'SugarCRM') {
          $login_info = EAPM::getLoginInfo(strtolower($this->bean->type));
          if ($login_info['name'] == $this->bean->creator) {
             $this->bean->displayed_url = $this->bean->host_url;
---
+++ sugarcrm/modules/Meetings/views/view.listbytype.php
@@ -0,0 +1,26 @@
+<?php
+
+require_once('include/MVC/View/views/view.list.php');
+require_once('modules/EAPM/EAPM.php');
+class MeetingsViewListbytype extends ViewList {
+    var $options = array('show_header' => false, 'show_title' => false, 'show_subpanels' => false, 'show_search' => true, 'show_footer' => false, 'show_javascript' => false, 'view_print' => false,);
+
+   function MeetingsViewListbytype() {
+  
+      parent::ViewList();
+   }
+
+   function listViewProcess(){
+   	 	
+   		$this->lv->show_action_dropdown = false;
+   		$this->lv->multiSelect = false;
+   		$type = 'lotus';
+   		$this->where = " meetings.type = '$type' ";
+
+		unset($this->searchForm->searchdefs['layout']['advanced_search']);
+	
+		parent::listViewProcess();
+	
+ 	}
+
+}
