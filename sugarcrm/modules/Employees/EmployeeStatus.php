31e6fa3ee4cedf4e1e2e19f18a2b9a8bc607f2ef
hshemer <hshemer@hshemers-MacBook-Pro.local>
2010-10-19 15:56:49 -0400

    Bug:36615 user status in Employee remains consistent after user tries to edit the page

--- sugarcrm/modules/Employees/EmployeeStatus.php
+++ sugarcrm/modules/Employees/EmployeeStatus.php
@@ -38,31 +38,32 @@ if(!defined('sugarEntry') || !sugarEntry) die('Not A Valid Entry Point');
  */
 
 function getEmployeeStatusOptions($focus, $name = 'employee_status', $value, $view = 'DetailView') {
+
+	
 	global $current_user, $app_list_strings;
-    if($view == 'EditView' || $view == 'MassUpdate') {
+    if(($view == 'EditView' || $view == 'MassUpdate') && is_admin($current_user)) {
 	   
-	   if (is_admin($current_user)) {
-            $employee_status  = "<select name='$name'";
-			if(!empty($sugar_config['default_user_name']) 
-			   && $sugar_config['default_user_name'] == $focus->user_name 
-			   && isset($sugar_config['lock_default_user_name']) 
-			   && $sugar_config['lock_default_user_name'])
+	   	$employee_status  = "<select name='$name'";
+		if(!empty($sugar_config['default_user_name']) 
+			&& $sugar_config['default_user_name'] == $focus->user_name 
+			&& isset($sugar_config['lock_default_user_name']) 
+			&& $sugar_config['lock_default_user_name'])
 		    {
 				$employee_status .= " disabled ";
 			}
 			$employee_status .= ">";
 			$employee_status .= get_select_options_with_id($app_list_strings['employee_status_dom'], $focus->employee_status);
 			$employee_status .= "</select>\n";
-	   } else { 
-	   	    $employee_status = ''; 
-	   }
-	   return $employee_status;
-	} //if($view == 'EditView')
-	
-    if ( isset($app_list_strings['employee_status_dom'][$focus->employee_status]) )
+			return $employee_status;
+	 }
+	   	
+	 if ( isset($app_list_strings['employee_status_dom'][$focus->employee_status]) )
+	 {
         return $app_list_strings['employee_status_dom'][$focus->employee_status];
-    
-	return $focus->employee_status;
+	 }
+	  
+	 return $focus->employee_status;
+		
 }
 
 function getMessengerTypeOptions($focus, $name = 'messenger_type', $value, $view = 'DetailView') {
