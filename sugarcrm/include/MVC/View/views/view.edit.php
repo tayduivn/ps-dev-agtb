208609363af877cbaff47792a6386f50e72884d6
Sadek Baroudi <sadek@sugarcrm.com>
2010-10-26 21:21:12 -0700

    Revert "Bug 39945 - Remove options for parent type dropdowns for modules that are hidden" - should have checked into pineapple, since it's a kitchen sink bug
    This reverts commit 7d0e3a645860ecc31fa93c5fe451688cb8f437ca.

--- sugarcrm/include/MVC/View/views/view.edit.php
+++ sugarcrm/include/MVC/View/views/view.edit.php
@@ -21,10 +21,8 @@ require_once('include/EditView/EditView2.php');
  	}
 
  	function preDisplay(){
-		
-		$this->removeDisabledTabsFromParentType();
-		
-		$metadataFile = $this->getMetaDataFile();
+ 		
+ 	    $metadataFile = $this->getMetaDataFile();
  		$this->ev = new EditView();
  		$this->ev->ss =& $this->ss;
  		$this->ev->setup($this->module, $this->bean, $metadataFile, 'include/EditView/EditView.tpl');
@@ -36,47 +34,6 @@ require_once('include/EditView/EditView2.php');
 		echo $this->ev->display($this->showTitle);
  	}
 
-	function removeDisabledTabsFromParentType(){
-        require_once("modules/MySettings/TabController.php");
-        $controller = new TabController();
-        $tabs = $controller->get_tabs_system();
-		$enabled = $tabs[0];
 
-		// In the next if blocks, we add back in modules that are categories of other modules, to avoid removing them unnecessarily
-		if(isset($enabled['Project'])){
-			$enabled['ProjectTask'] = 'ProjectTask';
-		}
-		
-		if(isset($enabled['Activities'])){
-			$enabled['Calls'] = 'Calls';
-			$enabled['Meetings'] = 'Meetings';
-			$enabled['Tasks'] = 'Tasks';
-			$enabled['Notes'] = 'Notes';
-			$enabled['Emails'] = 'Emails';
-		}
-		
-		if(isset($enabled['Campaigns'])){
-			$enabled['Prospects'] = 'Prospects';
-		}
-		
-		if(isset($enabled['Products'])){
-			$enabled['ProductTemplates'] = 'ProductTemplates';
-		}
-		
-		// Remove them from the parent_type_display options, which are used in Calls, Meetings, and Tasks
-		global $app_list_strings;
-		foreach($app_list_strings['parent_type_display'] as $key => $val){
-			if(!in_array($key, $enabled)){
-				unset($app_list_strings['parent_type_display'][$key]);
-			}
-		}
-
-		// Remove them from the record_type_display_notes options, which are used in Notes
-		foreach($app_list_strings['record_type_display_notes'] as $key => $val){
-			if(!in_array($key, $enabled)){
-				unset($app_list_strings['record_type_display_notes'][$key]);
-			}
-		}
-	}
  }
 ?>
