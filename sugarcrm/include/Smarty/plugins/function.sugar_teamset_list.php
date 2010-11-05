2ea93f5f021a5fb13718c1bd5d424523002025d5
Sadek Baroudi <sadek@sugarcrm.com>
2010-11-04 22:20:31 -0700

    LGPL 2.1 Compliance

--- sugarcrm/include/Smarty/plugins/function.sugar_teamset_list.php
+++ sugarcrm/include/Smarty/plugins/function.sugar_teamset_list.php
@@ -1,4 +1,27 @@
 <?php
+
+/*
+
+Modification information for LGPL compliance
+
+r56990 - 2010-06-16 13:05:36 -0700 (Wed, 16 Jun 2010) - kjing - snapshot "Mango" svn branch to a new one for GitHub sync
+
+r56989 - 2010-06-16 13:01:33 -0700 (Wed, 16 Jun 2010) - kjing - defunt "Mango" svn dev branch before github cutover
+
+r55980 - 2010-04-19 13:31:28 -0700 (Mon, 19 Apr 2010) - kjing - create Mango (6.1) based on windex
+
+r51719 - 2009-10-22 10:18:00 -0700 (Thu, 22 Oct 2009) - mitani - Converted to Build 3  tags and updated the build system 
+
+r51634 - 2009-10-19 13:32:22 -0700 (Mon, 19 Oct 2009) - mitani - Windex is the branch for Sugar Sales 1.0 development
+
+r50375 - 2009-08-24 18:07:43 -0700 (Mon, 24 Aug 2009) - dwong - branch kobe2 from tokyo r50372
+
+r48064 - 2009-06-04 11:20:27 -0700 (Thu, 04 Jun 2009) - rob - Bug 30503: Moved out the list code for the TeamSets into the sugarfield, added a special smarty function to call it, changed the SugarFeed dashlet to call that function
+
+
+*/
+
+
 //FILE SUGARCRM flav=pro ONLY
 if(!defined('sugarEntry') || !sugarEntry) die('Not A Valid Entry Point');
 /*********************************************************************************
--- sugarcrm/include/jsolait/lib/jsonrpc.js
+++ sugarcrm/include/jsolait/lib/jsonrpc.js
@@ -1,4 +1,65 @@
 /*
+
+Modification information for LGPL compliance
+
+r56990 - 2010-06-16 13:05:36 -0700 (Wed, 16 Jun 2010) - kjing - snapshot "Mango" svn branch to a new one for GitHub sync
+
+r56989 - 2010-06-16 13:01:33 -0700 (Wed, 16 Jun 2010) - kjing - defunt "Mango" svn dev branch before github cutover
+
+r55980 - 2010-04-19 13:31:28 -0700 (Mon, 19 Apr 2010) - kjing - create Mango (6.1) based on windex
+
+r51719 - 2009-10-22 10:18:00 -0700 (Thu, 22 Oct 2009) - mitani - Converted to Build 3  tags and updated the build system 
+
+r51634 - 2009-10-19 13:32:22 -0700 (Mon, 19 Oct 2009) - mitani - Windex is the branch for Sugar Sales 1.0 development
+
+r50375 - 2009-08-24 18:07:43 -0700 (Mon, 24 Aug 2009) - dwong - branch kobe2 from tokyo r50372
+
+r42807 - 2008-12-29 11:16:59 -0800 (Mon, 29 Dec 2008) - dwong - Branch from trunk/sugarcrm r42806 to branches/tokyo/sugarcrm
+
+r21575 - 2007-04-09 16:28:53 -0700 (Mon, 09 Apr 2007) - chris - Bug 12274 - SECURITY: CSRF (cross-site request forgery) fix.
+Fix back-ported to:
+Maint 451
+SecFix 451c
+Maint 450
+Portal Maint 450
+SecFix 421b
+Touched:
+	./index.php
+	./json.php
+	./json_server.php
+	./include/jsclass_async.js
+	./include/jsclass_base.js
+	./include/javascript/yui/connection.js
+	./include/jsolait/lib/jsonrpc.js
+	./include/jsolait/lib/jsonrpclite.js
+	./include/JSON.js
+	./include/JSON.php
+	./modules/Emails/Email.js
+
+r21544 - 2007-04-09 15:12:37 -0700 (Mon, 09 Apr 2007) - chris - Bug 12274 - SECURITY: CSRF (cross-site request forgery) fix.
+Touched:
+include/JSON.js
+include/javascript/
+-jsclass_async.js
+-jsclass_base.js
+-yui/connection.js
+include/jsolait/lib/jsonrpc.js
+
+r6158 - 2005-07-12 12:06:05 -0700 (Tue, 12 Jul 2005) - andrew - Need to pass data.
+
+r6129 - 2005-07-08 16:42:58 -0700 (Fri, 08 Jul 2005) - julian - Added in-depth error reporting for "Not well formed" error
+
+r5313 - 2005-05-13 10:01:43 -0700 (Fri, 13 May 2005) - ajay - 1010, this script now uses json.js for decoding.
+
+r4286 - 2005-04-18 04:04:05 -0700 (Mon, 18 Apr 2005) - robert - scheduler:
+ - finishing up
+
+r4085 - 2005-04-13 17:30:42 -0700 (Wed, 13 Apr 2005) - robert - adding meeting scheduler and accept/decline
+
+
+*/
+
+/*
   Copyright (c) 2003-2004 Jan-Klaas Kollhof
   
   This file is part of the JavaScript o lait library(jsolait).
