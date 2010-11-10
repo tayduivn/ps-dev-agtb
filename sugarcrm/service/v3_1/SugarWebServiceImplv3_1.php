4d7cb4a0bab8901bb6214b38acd4ab4802112af1
Andreas Sandberg <asandberg@sugarcrm.com>
2010-11-10 01:32:37 -0800

    Updating v3_1 set entry to track view and return modified fields

--- sugarcrm/service/v3_1/SugarWebServiceImplv3_1.php
+++ sugarcrm/service/v3_1/SugarWebServiceImplv3_1.php
@@ -178,7 +178,76 @@ class SugarWebServiceImplv3_1 extends SugarWebServiceImplv3 {
         return array('entry_list'=>$output_list, 'relationship_list' => $linkoutput_list);
     }
     
-    
+    /**
+     * Update or create a single SugarBean.
+     *
+     * @param String $session -- Session ID returned by a previous call to login.
+     * @param String $module_name -- The name of the module to return records from.  This name should be the name the module was developed under (changing a tab name is studio does not affect the name that should be passed into this method)..
+     * @param Array $name_value_list -- The keys of the array are the SugarBean attributes, the values of the array are the values the attributes should have.
+     * @param Bool $track_view -- Should the tracker be notified that the action was performed on the bean.
+     * @return Array    'id' -- the ID of the bean that was written to (-1 on error)
+     * @exception 'SoapFault' -- The SOAP error, if any
+     */
+    function set_entry($session,$module_name, $name_value_list, $track_view = FALSE){
+        global  $beanList, $beanFiles, $current_user;
+
+        $GLOBALS['log']->info('Begin: SugarWebServiceImpl->set_entry');
+        if (self::$helperObject->isLogLevelDebug()) {
+            $GLOBALS['log']->debug('SoapHelperWebServices->set_entry - input data is ' . var_export($name_value_list, true));
+        } // if
+        $error = new SoapError();
+        if (!self::$helperObject->checkSessionAndModuleAccess($session, 'invalid_session', $module_name, 'write', 'no_access', $error)) {
+            $GLOBALS['log']->info('End: SugarWebServiceImpl->set_entry');
+            return;
+        } // if
+        $class_name = $beanList[$module_name];
+        require_once($beanFiles[$class_name]);
+        $seed = new $class_name();
+        foreach($name_value_list as $name=>$value){
+            if(is_array($value) &&  $value['name'] == 'id'){
+                $seed->retrieve($value['value']);
+                break;
+            }else if($name === 'id' ){
+
+                $seed->retrieve($value);
+            }
+        }
+        
+        $return_fields = array();
+        foreach($name_value_list as $name=>$value){
+            if($module_name == 'Users' && !empty($seed->id) && ($seed->id != $current_user->id) && $name == 'user_hash'){
+                continue;
+            }
+            if(!is_array($value)){
+                $seed->$name = $value;
+                $return_fields[] = $name;
+            }else{
+                $seed->$value['name'] = $value['value'];
+                $return_fields[] = $value['name'];
+            }
+        }
+        if (!self::$helperObject->checkACLAccess($seed, 'Save', $error, 'no_access') || ($seed->deleted == 1  && !self::$helperObject->checkACLAccess($seed, 'Delete', $error, 'no_access'))) {
+            $GLOBALS['log']->info('End: SugarWebServiceImpl->set_entry');
+            return;
+        } // if
+
+        $seed->save(self::$helperObject->checkSaveOnNotify());
+        
+        $return_entry_list = self::$helperObject->get_return_value_for_fields($seed, $module_name,$return_fields );
+        
+        if($seed->deleted == 1){
+            $seed->mark_deleted($seed->id);
+        }
+
+        if($track_view){
+            self::$helperObject->trackView($seed, 'editview');
+        }
+
+        $GLOBALS['log']->info('End: SugarWebServiceImpl->set_entry');
+        return array('id'=>$seed->id, 'entry_list' => $return_entry_list);
+    } // fn
+
+
     /**
      * Log the user into the application
      *
