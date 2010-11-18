308b03a7b6b340e0f68418a1c9867d352e58d602
unknown <John Mertic@.(none)>
2010-11-18 12:14:16 -0500

    Bug 16338 - Changed a bunch of href='#' attributes to href='javascript:void(0)' instead.

--- sugarcrm/include/Dashlets/Dashlet.php
+++ sugarcrm/include/Dashlets/Dashlet.php
@@ -75,7 +75,7 @@ class Dashlet {
     	
     	
         if($this->isConfigurable) 
-            $additionalTitle = '<td nowrap width="1%" style="padding-right: 0px;"><div class="dashletToolSet"><a href="#" onclick="SUGAR.mySugar.configureDashlet(\'' 
+            $additionalTitle = '<td nowrap width="1%" style="padding-right: 0px;"><div class="dashletToolSet"><a href="javascript:void(0)" onclick="SUGAR.mySugar.configureDashlet(\'' 
                                . $this->id . '\'); return false;">'    
                                . SugarThemeRegistry::current()->getImage('dashlet-header-edit','title="' . translate('LBL_DASHLET_EDIT', 'Home') . '" alt="' . translate('LBL_DASHLET_EDIT', 'Home') . '"  border="0"  align="absmiddle"').'</a>' 
                                . '';
@@ -90,7 +90,7 @@ class Dashlet {
     	
     	$additionalTitle = '';
         if($this->isRefreshable)
-            $additionalTitle .= '<a href="#" onclick="SUGAR.mySugar.retrieveDashlet(\'' 
+            $additionalTitle .= '<a href="javascript:void(0)" onclick="SUGAR.mySugar.retrieveDashlet(\'' 
                                 . $this->id . '\'); return false;">'
                                 . SugarThemeRegistry::current()->getImage('dashlet-header-refresh','border="0" align="absmiddle" title="' . translate('LBL_DASHLET_REFRESH', 'Home') . '" alt="' . translate('LBL_DASHLET_REFRESH', 'Home') . '"')
                                 . '</a>';
@@ -102,7 +102,7 @@ class Dashlet {
     	if (!empty($sugar_config['lock_homepage']) && $sugar_config['lock_homepage'] == true)
 			return '</div></td></tr></table>';
     	$additionalTitle = '';
-        $additionalTitle .= '<a href="#" onclick="SUGAR.mySugar.deleteDashlet(\'' 
+        $additionalTitle .= '<a href="javascript:void(0)" onclick="SUGAR.mySugar.deleteDashlet(\'' 
                             . $this->id . '\'); return false;">'
                             . SugarThemeRegistry::current()->getImage('dashlet-header-close','border="0" align="absmiddle" title="' . translate('LBL_DASHLET_DELETE', 'Home') . '" alt="' . translate('LBL_DASHLET_DELETE', 'Home') . '"')
                             . '</a></div></td></tr></table>';
