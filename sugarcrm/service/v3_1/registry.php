f4310f049da57727c029bfe2b8a80753d16800d4
Andreas Sandberg <asandberg@sugarcrm.com>
2010-11-18 16:23:45 -0800

    Updating SOAP unit tests for v3_1 and fixing get_entry_list relationship return array

--- sugarcrm/service/v3_1/registry.php
+++ sugarcrm/service/v3_1/registry.php
@@ -172,5 +172,98 @@ class registry_v3_1 extends registry_v3 {
 					'link_fields'=>array('name'=>'link_fields', 'type'=>'tns:link_field_list'),
 				)
 		);
+		
+		//From v2_1, can't extend from this class because of versioning.
+		$this->serviceClass->registerType(
+			'link_list2',
+			'complexType',
+			'struct',
+			'all',
+			'',
+			array(
+			'link_list'=>array('name'=>'link_list', 'type'=>'tns:link_list'),
+			)
+		);
+		$this->serviceClass->registerType(
+		    'link_lists',
+			'complexType',
+		   	 'array',
+		   	 '',
+		  	  'SOAP-ENC:Array',
+			array(),
+		    array(
+		        array('ref'=>'SOAP-ENC:arrayType', 'wsdl:arrayType'=>'tns:link_list2[]')
+		    ),
+			'tns:link_list2'
+		);
+		
+		$this->serviceClass->registerType(
+		    'link_array_list',
+			'complexType',
+		   	 'array',
+		   	 '',
+		  	  'SOAP-ENC:Array',
+			array(),
+		    array(
+		        array('ref'=>'SOAP-ENC:arrayType', 'wsdl:arrayType'=>'tns:link_value2[]')
+		    ),
+			'tns:link_value2'
+		);
+		
+		$this->serviceClass->registerType(
+			'link_value2',
+			'complexType',
+			'struct',
+			'all',
+			'',
+			array(
+			'link_value'=>array('name'=>'link_value', 'type'=>'tns:link_value'),
+			)
+		);
+		
+		$this->serviceClass->registerType(
+			'report_field_list',
+			'complexType',
+			'array',
+			'',
+			'SOAP-ENC:Array',
+			array(),
+			array(
+			array('ref'=>'SOAP-ENC:arrayType','wsdl:arrayType'=>'tns:field_list2[]')
+			),
+			'tns:field_list2'
+		);
+		$this->serviceClass->registerType(
+			'field_list2',
+			'complexType',
+			'struct',
+			'all',
+			'',
+			array(
+			"field_list"=>array('name'=>'field_list', 'type'=>'tns:field_list'),
+			)
+		);
+		$this->serviceClass->registerType(
+			'report_entry_list',
+			'complexType',
+			'array',
+			'',
+			'SOAP-ENC:Array',
+			array(),
+			array(
+			array('ref'=>'SOAP-ENC:arrayType', 'wsdl:arrayType'=>'tns:entry_list2[]')
+			),
+			'tns:entry_list2'
+		);
+		$this->serviceClass->registerType(
+			'entry_list2',
+			'complexType',
+			'struct',
+			'all',
+			'',
+			array(
+			"entry_list"=>array('name'=>'entry_list', 'type'=>'tns:entry_list'),
+			)
+		);
 	}
 }
