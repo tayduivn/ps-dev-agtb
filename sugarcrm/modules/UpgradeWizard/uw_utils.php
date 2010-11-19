457761d95808bd3427c9f728f68a6b446dffde8a
Sadek Baroudi <sadek@sugarcrm.com>
2010-11-19 01:07:29 -0800

    Bug 40161 - Added condition for silent upgrade. Also, we were missing the new code in the 6.1.1 branch.

--- sugarcrm/modules/UpgradeWizard/uw_utils.php
+++ sugarcrm/modules/UpgradeWizard/uw_utils.php
@@ -5454,4 +5454,89 @@ function upgradeModulesForTeam() {
 		
 		logThis('End upgrade_connectors', $path);
 	}
+
+
+	function removeSilentUpgradeVarsCache(){
+	    global $silent_upgrade_vars_loaded;
+
+	    $cacheFileDir = "{$GLOBALS['sugar_config']['cache_dir']}/modules/UpgradeWizard";
+	    $cacheFile = "{$cacheFileDir}/silentUpgradeCache.php";
+
+	    if(file_exists($cacheFile)){
+	        unlink($cacheFile);
+	    }
+
+	    $silent_upgrade_vars_loaded = array(); // Set to empty to reset it
+
+	    return true;
+	}
+
+	function loadSilentUpgradeVars(){
+	    global $silent_upgrade_vars_loaded;
+
+	    if(empty($silent_upgrade_vars_loaded)){
+	        $cacheFile = "{$GLOBALS['sugar_config']['cache_dir']}/modules/UpgradeWizard/silentUpgradeCache.php";
+	        // We have no pre existing vars
+	        if(!file_exists($cacheFile)){
+	            // Set the vars array so it's loaded
+	            $silent_upgrade_vars_loaded = array('vars' => array());
+	        }
+	        else{
+	            require_once($cacheFile);
+	            $silent_upgrade_vars_loaded = $silent_upgrade_vars_cache;
+	        }
+	    }
+
+	    return true;
+	}
+
+	function writeSilentUpgradeVars(){
+	    global $silent_upgrade_vars_loaded;
+
+	    if(empty($silent_upgrade_vars_loaded)){
+	        return false; // You should have set some values before trying to write the silent upgrade vars
+	    }
+
+	    $cacheFileDir = "{$GLOBALS['sugar_config']['cache_dir']}/modules/UpgradeWizard";
+	    $cacheFile = "{$cacheFileDir}/silentUpgradeCache.php";
+
+	    require_once('include/dir_inc.php');
+	    if(!mkdir_recursive($cacheFileDir)){
+	        return false;
+	    }
+	    require_once('include/utils/file_utils.php');
+	    if(!write_array_to_file('silent_upgrade_vars_cache', $silent_upgrade_vars_loaded, $cacheFile, 'w')){
+	        return false;
+	    }
+
+	    return true;
+	}
+
+	function setSilentUpgradeVar($var, $value){
+	    if(!loadSilentUpgradeVars()){
+	        return false;
+	    }
+
+	    global $silent_upgrade_vars_loaded;
+
+	    $silent_upgrade_vars_loaded['vars'][$var] = $value;
+
+	    return true;
+	}
+
+	function getSilentUpgradeVar($var){
+	    if(!loadSilentUpgradeVars()){
+	        return false;
+	    }
+
+	    global $silent_upgrade_vars_loaded;
+
+	    if(!isset($silent_upgrade_vars_loaded['vars'][$var])){
+	        return null;
+	    }
+	    else{
+	        return $silent_upgrade_vars_loaded['vars'][$var];
+	    }
+	}
+
 ?>
