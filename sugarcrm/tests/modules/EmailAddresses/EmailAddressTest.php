e16280e7e74280aa70e391789302ce84f0b30883
John Mertic <jmertic@sugarcrm.com>
2010-11-23 12:23:24 -0500

    Bug 33769 - Allow calling EmailAddress::save() the same way as you would call SugarBean::save() and have it work, rather than the way you would need to call SugarEmailAddress::save() as it's defined. Fixes a problem with updating email address records over web services.

--- sugarcrm/tests/modules/EmailAddresses/EmailAddressTest.php
+++ sugarcrm/tests/modules/EmailAddresses/EmailAddressTest.php
@@ -21,7 +21,7 @@ class EmailAddressTest extends Sugar_PHPUnit_Framework_TestCase
         $GLOBALS['db']->query($query);
 	}
 	
-	public function testEmailAddress ()
+	public function testEmailAddress()
 	{
 		$id = '';
 		$module = '';
@@ -44,5 +44,18 @@ class EmailAddressTest extends Sugar_PHPUnit_Framework_TestCase
 		$this->assertEquals($this->emailaddress->addresses[0]['email_address'], $this->testEmailAddressString);
 		$this->assertEquals($this->emailaddress->addresses[0]['primary_address'], 1);
 	}
+	
+	public function testSaveEmailAddressUsingSugarbeanSave()
+	{
+	    $this->emailaddress->email_address = $this->testEmailAddressString;
+	    $this->emailaddress->opt_out = '1';
+	    $this->emailaddress->save();
+	    
+	    $this->assertNotEmpty($this->emailaddress->id);
+	    $this->assertEquals(
+	        $this->emailaddress->id,
+	        $GLOBALS['db']->getOne("SELECT id FROM email_addresses WHERE id = '{$this->emailaddress->id}' AND email_address = '{$this->testEmailAddressString}' and opt_out = '1'"),
+	        'Email Address record not added'
+	        );
+	}
 }
-?>
