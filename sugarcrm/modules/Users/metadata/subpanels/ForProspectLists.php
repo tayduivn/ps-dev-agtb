abca63b5f78f149b1c74aa1280a73c0a4547e7fe
roger <roger@sugar-crms-macbook-pro.local>
2010-11-22 22:04:18 -0500

    bug: 39959 - username link to go to Employees

--- sugarcrm/modules/Users/metadata/subpanels/ForProspectLists.php
+++ sugarcrm/modules/Users/metadata/subpanels/ForProspectLists.php
@@ -48,6 +48,7 @@ $subpanel_layout = array(
 		),
 		'name'=>array(
 			'vname' => 'LBL_LIST_NAME',
+		 	'target_module' => 'Employees',
 		 	'module' => 'Users',
 		    'widget_class' => 'SubPanelDetailViewLink',
 	 		'width' => '35%',
