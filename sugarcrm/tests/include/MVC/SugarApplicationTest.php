88a6705f6f5196eb6afe52f650735d0d59d6aee7
Stanislav Malyshev <smalyshev@gmail.com>
2010-11-30 09:27:31 +0100

    fix SugarApplication test so it doesn't break others

--- sugarcrm/tests/include/MVC/SugarApplicationTest.php
+++ sugarcrm/tests/include/MVC/SugarApplicationTest.php
@@ -4,7 +4,9 @@ require_once 'include/MVC/SugarApplication.php';
 class SugarApplicationTest extends Sugar_PHPUnit_Framework_TestCase
 {
     private $_app;
-    
+    // Run in isolation so that it doesn't mess up other tests
+    public $inIsolation = true;
+
     public function setUp()
     {
         $startTime = microtime();
@@ -15,39 +17,24 @@ class SugarApplicationTest extends Sugar_PHPUnit_Framework_TestCase
         if ( isset($_SESSION['authenticated_user_theme']) )
             unset($_SESSION['authenticated_user_theme']);
     }
-    
+
     private function _loadUser()
     {
         $GLOBALS['current_user'] = SugarTestUserUtilities::createAnonymousUser();
         $_SESSION[$GLOBALS['current_user']->user_name.'_PREFERENCES']['global']['gridline'] = 'on';
     }
-    
-    private function _removeUser() 
+
+    private function _removeUser()
     {
         SugarTestUserUtilities::removeAllCreatedAnonymousUsers();
         unset($GLOBALS['current_user']);
     }
-    
+
     public function tearDown()
     {
-        unset($GLOBALS['current_user']);
-        unset($GLOBALS['moduleList']);
-        unset($GLOBALS['request_string']);
-        unset($GLOBALS['adminOnlyList']);
-        unset($GLOBALS['modListHeader']);
-        unset($GLOBALS['modInvisList']);
-        unset($GLOBALS['app_strings']);
-        unset($GLOBALS['system_config']);
-        unset($GLOBALS['app_list_strings']);
-        unset($GLOBALS['mod_strings']);
-        unset($GLOBALS['theme']);
-        unset($GLOBALS['image_path']);
-        unset($GLOBALS['starttTime']);
-        unset($GLOBALS['sugar_version']);
-        unset($GLOBALS['sugar_flavor']);
         $GLOBALS['current_language'] = $GLOBALS['sugar_config']['default_language'];
     }
-    
+
     public function testSetupPrint()
     {
         $_GET['foo'] = 'bar';
@@ -75,58 +62,58 @@ class SugarApplicationTest extends Sugar_PHPUnit_Framework_TestCase
         $this->assertEquals('foo[1]=notMultidemensional&foo[2]=notMultidemensional&dog=cat&print=true', $GLOBALS['request_string']
         );
     }
-    
+
     public function testLoadDisplaySettingsDefault()
     {
         $this->_loadUser();
-        
+
         $this->_app->loadDisplaySettings();
-        
+
         $this->assertEquals($GLOBALS['theme'],
             $GLOBALS['sugar_config']['default_theme']);
-        
+
         $this->_removeUser();
     }
-    
+
     public function testLoadDisplaySettingsAuthUserTheme()
     {
         $this->_loadUser();
-        
+
         $_SESSION['authenticated_user_theme'] = 'Sugar';
-        
+
         $this->_app->loadDisplaySettings();
-        
+
         $this->assertEquals($GLOBALS['theme'],
             $_SESSION['authenticated_user_theme']);
-        
+
         $this->_removeUser();
     }
-    
+
     public function testLoadDisplaySettingsUserTheme()
     {
         $this->_loadUser();
-        
+
         $_REQUEST['usertheme'] = 'Sugar';
-        
+
         $this->_app->loadDisplaySettings();
-        
+
         $this->assertEquals($GLOBALS['theme'],
             $_REQUEST['usertheme']);
-        
+
         $this->_removeUser();
     }
-    
+
     public function testLoadGlobals()
     {
-        $this->_app->controller = 
+        $this->_app->controller =
             ControllerFactory::getController($this->_app->default_module);
         $this->_app->loadGlobals();
-        
+
         $this->assertEquals($GLOBALS['currentModule'],$this->_app->default_module);
         $this->assertEquals($_REQUEST['module'],$this->_app->default_module);
         $this->assertEquals($_REQUEST['action'],$this->_app->default_action);
     }
-    
+
     /**
      * @ticket 33283
      */
@@ -138,25 +125,25 @@ class SugarApplicationTest extends Sugar_PHPUnit_Framework_TestCase
             $old_sugar_version = $GLOBALS['sugar_version'];
         include 'sugar_version.php';
         $GLOBALS['sugar_version'] = $sugar_version;
-        
+
         // first test a valid value
         $GLOBALS['sugar_db_version'] = $sugar_db_version;
         $this->assertTrue($this->_app->checkDatabaseVersion(false));
-        
+
         $GLOBALS['sugar_db_version'] = '1.1.1';
         // then test to see if we pull against the cache the valid value
         $this->assertTrue($this->_app->checkDatabaseVersion(false));
-        
+
         // now retest to be sure we actually do the check again
         sugar_cache_put('checkDatabaseVersion_row_count', 0);
         $this->assertFalse($this->_app->checkDatabaseVersion(false));
-        
+
         if ( isset($old_sugar_db_version) )
             $GLOBALS['sugar_db_version'] = $old_sugar_db_version;
         if ( isset($old_sugar_version) )
             $GLOBALS['sugar_version'] = $old_sugar_version;
     }
-    
+
     public function testLoadLanguages()
     {
     	$this->_app->controller->module = 'Contacts';
