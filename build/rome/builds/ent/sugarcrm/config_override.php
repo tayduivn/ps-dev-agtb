163c5612cce4de16bd3449bc32c7142e9ed4dfd7
hshemer <hshemer@hshemers-MacBook-Pro.local>
2010-12-07 09:22:57 -0500

    BUG39097 Display Downloads Tab is checked by default

---
+++ build/rome/builds/ent/sugarcrm/config_override.php
@@ -0,0 +1,9 @@
+<?php
+/***CONFIGURATOR***/
+$sugar_config['log_memory_usage'] = true;
+$sugar_config['default_module_favicon'] = false;
+$sugar_config['stack_trace_errors'] = false;
+$sugar_config['developerMode'] = false;
+$sugar_config['lock_subpanels'] = true;
+$sugar_config['show_download_tab'] = true;
+/***CONFIGURATOR***/
