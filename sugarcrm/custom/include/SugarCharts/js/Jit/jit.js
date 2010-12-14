88159e270b60146b9ee05b234acae172837d2446
lam <lam@192.168.0.6>
2010-12-14 16:23:50 -0600

    adding headers and fixing IE javascript bug causing jscharts to not render

--- sugarcrm/custom/include/SugarCharts/js/Jit/jit.js
+++ sugarcrm/custom/include/SugarCharts/js/Jit/jit.js
@@ -1,3 +1,31 @@
+/**
+ * LICENSE: The contents of this file are subject to the SugarCRM Professional
+ * End User License Agreement ("License") which can be viewed at
+ * http://www.sugarcrm.com/EULA.  By installing or using this file, You have
+ * unconditionally agreed to the terms and conditions of the License, and You
+ * may not use this file except in compliance with the License.  Under the
+ * terms of the license, You shall not, among other things: 1) sublicense,
+ * resell, rent, lease, redistribute, assign or otherwise transfer Your
+ * rights to the Software, and 2) use the Software for timesharing or service
+ * bureau purposes such as hosting the Software for commercial gain and/or for
+ * the benefit of a third party.  Use of the Software may be subject to
+ * applicable fees and any use of the Software without first paying applicable
+ * fees is strictly prohibited.  You do not have the right to remove SugarCRM
+ * copyrights from the source code or user interface.
+ *
+ * All copies of the Covered Code must include on each user interface screen:
+ *  (i) the "Powered by SugarCRM" logo and
+ *  (ii) the SugarCRM copyright notice
+ * in the same form as they appear in the distribution.  See full license for
+ * requirements.
+ *
+ * Your Warranty, Limitations of liability and Indemnity are expressly stated
+ * in the License.  Please refer to the License for the specific language
+ * governing these rights and limitations under the License.  Portions created
+ * by SugarCRM are Copyright (C) 2006 SugarCRM, Inc.; All Rights Reserved.
+ */
+
+// $Id: jit.js 2010-12-01 23:11:36Z lhuynh $
 
 /*
   Copyright (c) 2010, Nicolas Garcia Belmonte
@@ -11381,7 +11409,7 @@ $jit.BarChart = new Class({
           				valueLabelDim = 50;
           				valueLabel = document.createElement('div');
           				valueLabel.innerHTML =  valuelabelArray[i];
-          				valueLabel.class = "rotatedLabel";
+//          				valueLabel.class = "rotatedLabel";
           				valueLabel.className = "rotatedLabel";
           				valueLabel.style.position = "absolute";
 						valueLabel.style.textAlign = "left";
@@ -11525,7 +11553,7 @@ $jit.BarChart = new Class({
 					labelDiv = document.createElement('div');
 					labelDiv.innerHTML = labelValue;
 					labelDiv.className = "rotatedLabel";
-					labelDiv.class = "rotatedLabel";
+//					labelDiv.class = "rotatedLabel";
 					labelDiv.style.top = (htmlOrigin - (labelDim/2)) + "px";
 					labelDiv.style.left = margin.left + "px";
 					labelDiv.style.width = labelDim + "px";
@@ -14883,8 +14911,9 @@ $jit.GaugeChart = new Class({
       var val = values[i];
 	  if(val.label != 'GaugePosition') {
 		  var valArray = $.splat(val.values);
-		  var linkArray = $.splat(val.links);
+		  var linkArray = (val.links == "undefined" || val.links == undefined) ? new Array() : $.splat(val.links);
 		  var valuelabelsArray = $.splat(val.valuelabels);
+
 		  ch.push({
 			'id': prefix + val.label,
 			'name': val.label,
