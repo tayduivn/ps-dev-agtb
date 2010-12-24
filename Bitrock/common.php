a794ed306462cb69543e5ee6ed380b2014f1a5c6
Oliver Yang <oyang@sugarcrm.com>
2010-12-23 17:47:51 -0800

    Fixed a typo on BUILD_TYPES

--- Bitrock/common.php
+++ Bitrock/common.php
@@ -6,7 +6,7 @@ if ($VER == '') $VER="6.2.0beta";
 $VER_SUFFIX     = "BitRock-$VER";
 $SUGAR_VERSION  = $VER;
 $PUBLISH_DIR    = "/home/public/builds/$VER_SUFFIX";
-$BUILD_TYPES    = array( "ce,ent" );
+$BUILD_TYPES    = array( "ce","ent");
 
 $BITROCK_BASE = "/home/build/bitrock-base";
 $FASTSTACK_SVN = "http://svn1.sjc.sugarcrm.pvt/faststack/branches/tokyo";
