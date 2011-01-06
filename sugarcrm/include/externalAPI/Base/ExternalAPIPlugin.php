d74a605bdbc17dea26e85b2da0211b8a432b9e79
roger <roger@sugar-crms-macbook-pro-3.local>
2011-01-06 12:55:21 -0500

    fixing merge issues

--- sugarcrm/include/externalAPI/Base/ExternalAPIPlugin.php
+++ sugarcrm/include/externalAPI/Base/ExternalAPIPlugin.php
@@ -2,6 +2,6 @@
 interface ExternalAPIPlugin {
     public function supports($method = '');
     public function loadEAPM($eapmBean);
-    public function checkLogin($eapmBean);
+    public function checkLogin($eapmBean = null);
     public function logOff();
 }
