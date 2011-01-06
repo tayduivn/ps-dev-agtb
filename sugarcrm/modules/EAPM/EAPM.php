c118b26b1e9391dd9ec337f76420080c5ad86191
roger <roger@sugar-crms-macbook-pro-3.local>
2011-01-05 21:45:46 -0500

    bug: 41553 - remove External accounts for assigned user when the user is deleted

--- sugarcrm/modules/EAPM/EAPM.php
+++ sugarcrm/modules/EAPM/EAPM.php
@@ -153,6 +153,16 @@ class EAPM extends Basic {
         $this->oauth_secret = "";
         $this->api_data = "";
 	}
+
+    /**
+     * Given a user remove their associated accounts. This is called when a user is deleted from the system.
+     * @param  $user_id
+     * @return void
+     */
+    public function delete_user_accounts($user_id){
+        $sql = "UPDATE {$this->table_name} SET deleted = 1, active = 0 WHERE assigned_user_id = '{$user_id}'";
+        $GLOBALS['db']->query($sql,true);
+    }
 }
 
 // External API integration, for the dropdown list of what external API's are available
