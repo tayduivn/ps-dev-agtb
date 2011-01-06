6fcf19820f474f5dd1840ca81dfcf9665ec83748
roger <roger@sugar-crms-macbook-pro-3.local>
2011-01-06 13:31:02 -0500

    fixing merge issues

--- sugarcrm/include/externalAPI/Base/WebMeeting.php
+++ sugarcrm/include/externalAPI/Base/WebMeeting.php
@@ -3,7 +3,7 @@
 interface WebMeeting {
 	public function scheduleMeeting($bean);
 	public function unscheduleMeeting($bean);
-	public function inviteAttendee($bean, $attendee, $sendInvites);
+	public function inviteAttendee($bean, $attendee, $sendInvites = false);
 	public function uninviteAttendee($bean, $attendee);
 	public function listMyMeetings();
 	public function getMeetingDetails($bean);
