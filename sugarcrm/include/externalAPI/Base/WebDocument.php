c5f3b083f2c314cd9ac4ba6ea59f94d5b3dfc47e
roger <roger@sugar-crms-macbook-pro-3.local>
2011-01-07 14:22:59 -0500

    merging back some interface default params from pineapple

--- sugarcrm/include/externalAPI/Base/WebDocument.php
+++ sugarcrm/include/externalAPI/Base/WebDocument.php
@@ -4,5 +4,5 @@ interface WebDocument {
     public function downloadDoc($documentId, $documentFormat);	
 	public function shareDoc($documentId, $emails);	
 	public function deleteDoc($documentId);
-    public function searchDoc($keywords, $flushDocCache);
+    public function searchDoc($keywords, $flushDocCache = false);
 }
