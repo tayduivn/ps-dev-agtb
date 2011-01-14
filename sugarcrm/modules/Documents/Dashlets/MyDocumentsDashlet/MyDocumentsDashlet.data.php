a1dfe2913986dd5efe9680a27c4dbce1cc8b91d9
roger <roger@sugar-crms-macbook-pro-3.local>
2011-01-13 19:52:59 -0500

    changed from SugarCRM type to Sugar

--- sugarcrm/modules/Documents/Dashlets/MyDocumentsDashlet/MyDocumentsDashlet.data.php
+++ sugarcrm/modules/Documents/Dashlets/MyDocumentsDashlet/MyDocumentsDashlet.data.php
@@ -32,7 +32,7 @@ global $current_user;
 $dashletData['MyDocumentsDashlet']['searchFields'] = array('date_entered'    => array('default' => ''),
                                                           'document_name'    => array('default' => ''),
                                                           'category_id'      => array('default' => ''),
- 														  'doc_type'  => array('default' => 'SugarCRM'),
+ 														  'doc_type'  => array('default' => 'Sugar'),
  														  'status_id'     => array('default' => ''),
  														  'active_date'      => array('default' => ''),
                                                           //BEGIN SUGARCRM flav=pro ONLY
