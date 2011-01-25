b03ff4d08c4dd5eb9999f4d073f44f237771dedd
Collin Lee <clee@Collin-Lee-MacBook-Pro.local>
2011-01-24 19:19:05 -0500

    Fixed test.  Needed to declare a different class name.

--- sugarcrm/tests/include/SugarCharts/CustomSugarChartFactoryTest.php
+++ sugarcrm/tests/include/SugarCharts/CustomSugarChartFactoryTest.php
@@ -3,90 +3,86 @@ require_once('include/SugarCharts/SugarChartFactory.php');
 
 class CustomSugarChartFactoryTest extends Sugar_PHPUnit_Framework_TestCase {
 
-var $hasCustomJit = false;
-var $hasCustomFlash = false;
+public static function setUpBeforeClass()
+{
+    $GLOBALS['current_user'] = SugarTestUserUtilities::createAnonymousUser();
+}
 
-	public static function setUpBeforeClass()
-	{
-        $GLOBALS['current_user'] = SugarTestUserUtilities::createAnonymousUser();
-	}
-
-	public static function tearDownAfterClass()
-	{
-	    SugarTestUserUtilities::removeAllCreatedAnonymousUsers();
-        unset($GLOBALS['current_user']);
-	}
+public static function tearDownAfterClass()
+{
+    SugarTestUserUtilities::removeAllCreatedAnonymousUsers();
+    unset($GLOBALS['current_user']);
+}
 
 
 public function setUp()
 {
-    $this->markTestIncomplete('Disabled until Jit problem is fixed');
-    return;
-	if(file_exists('custom/include/SugarCharts/Jit/Jit.php'))
-	{
-		$this->hasCustomJit = true;
-		copy('custom/include/SugarCharts/Jit/Jit.php', 'custom/include/SugarCharts/Jit/Jit.php.bak');
-		unlink('custom/include/SugarCharts/Jit/Jit.php');
+
+mkdir_recursive('custom/include/SugarCharts/CustomSugarChartFactory');
+	
+$the_string = <<<EOQ
+<?php
+
+require_once("include/SugarCharts/JsChart.php");
+
+class CustomSugarChartFactory extends JsChart {
+	
+	function __construct() {
+		parent::__construct();
+	}
+	
+	function getChartResources() {
+		return '
+		<link type="text/css" href="'.getJSPath('include/SugarCharts/Jit/css/base.css').'" rel="stylesheet" />
+		<!--[if IE]><script language="javascript" type="text/javascript" src="'.getJSPath('include/SugarCharts/Jit/js/Jit/Extras/excanvas.js').'"></script><![endif]-->
+		<script language="javascript" type="text/javascript" src="'.getJSPath('include/SugarCharts/Jit/js/Jit/jit.js').'"></script>
+		<script language="javascript" type="text/javascript" src="'.getJSPath('include/SugarCharts/Jit/js/sugarCharts.js').'"></script>
+		';
+	}
+	
+	function getMySugarChartResources() {
+		return '
+		<script language="javascript" type="text/javascript" src="'.getJSPath('include/SugarCharts/Jit/js/mySugarCharts.js').'"></script>
+		';
+	}
+	
+
+	function display(\$name, \$xmlFile, \$width='320', \$height='480', \$resize=false) {
+	
+		parent::display(\$name, \$xmlFile, \$width, \$height, \$resize);
+
+		return \$this->ss->fetch('include/SugarCharts/Jit/tpls/chart.tpl');	
+	}
+	
+
+	function getDashletScript(\$id,\$xmlFile="") {
+		
+		parent::getDashletScript(\$id,\$xmlFile);
+		return \$this->ss->fetch('include/SugarCharts/Jit/tpls/DashletGenericChartScript.tpl');
 	}
 
-	if(file_exists('custom/include/SugarCharts/SugarFlash/SugarFlash.php'))
-	{
-		$this->hasCustomFlash = true;
-		copy('custom/include/SugarCharts/SugarFlash/SugarFlash.php', 'custom/include/SugarCharts/SugarFlash/SugarFlash.php.bak');
-		unlink('custom/include/SugarCharts/SugarFlash/SugarFlash.php');
-	}
+}
+
+?>
+EOQ;
+
+$fp = sugar_fopen('custom/include/SugarCharts/CustomSugarChartFactory/CustomSugarChartFactory.php', "w");
+fwrite($fp, $the_string );
+fclose($fp );
 
 }
 
 public function tearDown()
 {
-	if($this->hasCustomJit && file_exists('custom/include/SugarCharts/Jit/Jit.php.bak'))
-	{
-	   copy('custom/include/SugarCharts/Jit/Jit.php.bak', 'custom/include/SugarCharts/Jit/Jit.php');
-	} else if(file_exists('custom/include/SugarCharts/Jit/Jit.php')) {
-	   unlink('custom/include/SugarCharts/Jit/Jit.php');
-	}
-
-	if($this->hasCustomFlash && file_exists('custom/include/SugarCharts/SugarFlash/SugarFlash.php.bak'))
-	{
-	   copy('custom/include/SugarCharts/SugarFlash/SugarFlash.php.bak', 'custom/include/SugarCharts/SugarFlash/SugarFlash.php');
-	} else if(file_exists('custom/include/SugarCharts/SugarFlash/SugarFlash.php')) {
-	   unlink('custom/include/SugarCharts/SugarFlash/SugarFlash.php');
-	}
-
-	if(file_exists('custom/include/SugarCharts/Jit/Jit.php.bak'))
-	{
-	   unlink('custom/include/SugarCharts/Jit/Jit.php.bak');
-	}
-
-	if(file_exists('custom/include/SugarCharts/SugarFlash/SugarFlash.php.bak')) {
-	   unlink('custom/include/SugarCharts/SugarFlash/SugarFlash.php.bak');
-	}
+	rmdir_recursive('custom/include/SugarCharts/CustomSugarChartFactory');
 }
 
 
-public function testCustomFactories()
+public function testCustomFactory()
 {
-	if(!file_exists('custom/include/SugarCharts/Jit'))
-	{
-		mkdir_recursive('custom/include/SugarCharts/Jit');
-	}
-
-	if(!file_exists('custom/include/SugarCharts/SugarFlash'))
-	{
-		mkdir_recursive('custom/include/SugarCharts/SugarFlash');
-	}
-
-	copy('include/SugarCharts/Jit/Jit.php', 'custom/include/SugarCharts/Jit/Jit.php');
-	copy('include/SugarCharts/SugarFlash/SugarFlash.php', 'custom/include/SugarCharts/SugarFlash/SugarFlash.php');
-
-	$sugarChart = SugarChartFactory::getInstance('Jit');
+	$sugarChart = SugarChartFactory::getInstance('CustomSugarChartFactory');
 	$name = get_class($sugarChart);
-	$this->assertEquals('Jit', $name, 'Assert engine is Jit');
-
-	$sugarChart = SugarChartFactory::getInstance('SugarFlash');
-	$name = get_class($sugarChart);
-	$this->assertEquals('SugarFlash', $name, 'Assert engine is SugarFlash');
+	$this->assertEquals('CustomSugarChartFactory', $name, 'Assert engine is CustomSugarChartFactory');
 }
 
 }
