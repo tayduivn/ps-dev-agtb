baf1f33c78c2471d482e71539aee2f4f76f60a1f
roger <roger@sugar-crms-macbook-pro-3.local>
2011-01-27 15:54:22 -0500

    Bug: 41586 - set assigned_user_name as readonly

--- sugarcrm/modules/EAPM/metadata/editviewdefs.php
+++ sugarcrm/modules/EAPM/metadata/editviewdefs.php
@@ -58,7 +58,10 @@ $viewdefs[$module_name]['EditView'] = array(
     ),
     array (
         array('name' => 'url', 'displayParams' => array('required' => true) ),
-        'assigned_user_name',
+        array('name' => 'assigned_user_name',
+              'type' => 'readonly',
+              'customCode'=>'{$fields.assigned_user_name.value}<input name="assigned_user_id" type="hidden" value="{$fields.assigned_user_id.value}" autocomplete="off">',
+            ),
     ),
     array (
         'description',
