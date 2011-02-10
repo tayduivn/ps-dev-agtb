c5512538543bd0aa1d290f712f6b41343de8c8ec
roger <roger@sugar-crms-macbook-pro-3.local>
2011-02-10 09:41:02 -0500

    Bug: 42195 - added copyright symbol fo eapm

--- sugarcrm/modules/Connectors/connectors/sources/ext/eapm/facebook/config.php
+++ sugarcrm/modules/Connectors/connectors/sources/ext/eapm/facebook/config.php
@@ -3,7 +3,7 @@
 if(!defined('sugarEntry') || !sugarEntry) die('Not A Valid Entry Point');
 
 $config = array (
-  'name' => 'Facebook',
+  'name' => 'Facebook&#169;',
   'eapm' => array(
     'enabled' => true,
     'only' => true,
