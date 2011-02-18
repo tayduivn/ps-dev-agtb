ba8a3a4e8f9f1bf92540796463fc520105c90e88
David Wheeler <dwheeler@sugarcrm.com>
2011-02-18 14:45:47 -0800

    Bug 40740: Added icons for dependent and calculated fields in layout editors.

--- sugarcrm/modules/ModuleBuilder/views/view.layoutview.php
+++ sugarcrm/modules/ModuleBuilder/views/view.layoutview.php
@@ -208,6 +208,7 @@ class ViewLayoutView extends ViewEdit
         $smarty->assign ( 'disable_layout', $disableLayout) ;
         $smarty->assign ( 'required_fields', $requiredFields) ;
         $smarty->assign ( 'layout', $parser->getLayout () ) ;
+        $smarty->assign ( 'field_defs', $parser->getFieldDefs () ) ;
         $smarty->assign ( 'view_module', $this->editModule ) ;
         $smarty->assign ( 'view', $this->editLayout ) ;
         $smarty->assign ( 'maxColumns', $parser->getMaxColumns() ) ;
