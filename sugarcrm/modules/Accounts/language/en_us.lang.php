331bd050f37bba1af06637ed30c244c90ec86f26
roger <roger@sugar-crms-macbook-pro-3.local>
2011-03-03 09:19:33 -0500

    Bug: 42595 - Document subpanel label for accounts was tagged incorrectly

--- sugarcrm/modules/Accounts/language/en_us.lang.php
+++ sugarcrm/modules/Accounts/language/en_us.lang.php
@@ -38,9 +38,9 @@ $mod_strings = array (
 	'LBL_CONTRACTS_SUBPANEL_TITLE'=>'Contracts',
 	'LBL_PRODUCTS_SUBPANEL_TITLE' => 'Products',
 	'LBL_QUOTES_SUBPANEL_TITLE' => 'Quotes',
-    'LBL_DOCUMENTS_SUBPANEL_TITLE' => 'Documents',
 	'LNK_ACCOUNT_REPORTS' => 'View Account Reports',
 	//END SUGARCRM flav=pro ONLY
+    'LBL_DOCUMENTS_SUBPANEL_TITLE' => 'Documents',
 	// Dashlet Categories
 	'LBL_CHARTS'    => 'Charts',
 	'LBL_DEFAULT' => 'Views',
