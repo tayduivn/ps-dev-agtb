2946c1b1164159be0bba1bae49cb17e3133698ec
Collin Lee <clee@Collin-Lee-MacBook-Pro.local>
2011-03-04 15:28:35 -0500

    Bug: 42617
    The skipped query  "ALTER TABLE email_addr_bean_rel ALTER COLUMN bean_module varchar(100) NULL" is most likely caused by the fact that the alter statement on the bean_module column is linked to indexes in the table.  The alter statement is executed via the .sql file.  The new repair code can now correctly amend this problem so I have taken it out of the .sql file.  Tested the upgrade without the manual SQL statements in the .sql file and table columns are altered correctly.

--- build/scripts_for_patch/61x_to_62x_mysql.sql
+++ build/scripts_for_patch/61x_to_62x_mysql.sql
@@ -1,7 +1,3 @@
-ALTER TABLE `email_addr_bean_rel` MODIFY COLUMN `bean_module` varchar(100) NULL;
-
-ALTER TABLE `emails_beans` MODIFY COLUMN `bean_module` varchar(100) NULL;
-
 ALTER TABLE meetings ALTER COLUMN status SET DEFAULT 'Planned';
 ALTER TABLE calls ALTER COLUMN status SET DEFAULT 'Planned';
 ALTER TABLE tasks ALTER COLUMN status SET DEFAULT 'Not Started';
