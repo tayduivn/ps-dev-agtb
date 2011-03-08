72640b042c3f7cd7a83651e039bc9bd4b1f4170f
Collin Lee <clee@Collin-Lee-MacBook-Pro.local>
2011-03-08 10:46:04 -0500

    Bug: 42691
    The date_modified and date_entered fields have range search enabled by default, but the corresponding SearchFields.php file did not reflect this for the Accounts module.  Fixed the SearchFields.php file and vardefs.php files to default provide OOTB date_entered and date_modified field search support.

--- sugarcrm/modules/Leads/metadata/SearchFields.php
+++ sugarcrm/modules/Leads/metadata/SearchFields.php
@@ -63,6 +63,14 @@ $searchFields['Leads'] =
 			'operator'=>'not in',
 			'closed_values' => array('Dead', 'Recycled', 'Converted'),
 			'type'=>'bool',
-		),	
+		),
+		//Range Search Support 
+	    'range_date_entered' => array ('query_type' => 'default', 'enable_range_search' => true, 'is_date_field' => true),
+	    'start_range_date_entered' => array ('query_type' => 'default', 'enable_range_search' => true, 'is_date_field' => true),
+	    'end_range_date_entered' => array ('query_type' => 'default', 'enable_range_search' => true, 'is_date_field' => true),
+	    'range_date_modified' => array ('query_type' => 'default', 'enable_range_search' => true, 'is_date_field' => true),
+	    'start_range_date_modified' => array ('query_type' => 'default', 'enable_range_search' => true, 'is_date_field' => true),
+        'end_range_date_modified' => array ('query_type' => 'default', 'enable_range_search' => true, 'is_date_field' => true),	
+	    //Range Search Support 				
     );
 ?>
