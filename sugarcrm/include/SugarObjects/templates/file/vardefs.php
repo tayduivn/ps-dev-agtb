604a5d5902e07a217d487d4f416d7a5d33eba533
Collin Lee <clee@Collin-Lee-MacBook-Pro.local>
2011-03-14 09:52:34 -0400

    Bug: 42710
    Add support for searching on custom File type modules.

--- sugarcrm/include/SugarObjects/templates/file/vardefs.php
+++ sugarcrm/include/SugarObjects/templates/file/vardefs.php
@@ -37,7 +37,8 @@ if(!defined('sugarEntry') || !sugarEntry) die('Not A Valid Entry Point');
     'type' => 'name',
 	'dbType' => 'varchar',
     'len' => '255',
-    'required'=>true
+    'required'=>true,
+    'unified_search' => true,
   ),
 
 'name'=>
