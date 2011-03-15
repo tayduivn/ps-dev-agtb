b10061bbc3ca9fbafb60188d9af612b2b47d0dae
lam <lam@192.168.0.6>
2011-03-15 15:46:54 -0500

    bug 42865 - made labels on barcharts less prominent

--- sugarcrm/include/SugarCharts/SugarChartFactory.php
+++ sugarcrm/include/SugarCharts/SugarChartFactory.php
@@ -63,6 +63,7 @@ class SugarChartFactory
         } else if(file_exists($file)) {
           require_once($file);
         } else {
+        	$GLOBALS['log']->debug("using default engine include/SugarCharts/".$defaultEngine."/".$defaultEngine.$module.".php");
            require_once("include/SugarCharts/".$defaultEngine."/".$defaultEngine.$module.".php");
         }
 
