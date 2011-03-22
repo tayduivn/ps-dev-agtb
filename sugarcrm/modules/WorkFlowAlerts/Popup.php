186d69b377bd7a39c3f0a0ef3646723026dbe471
Stanislav Malyshev <smalyshev@gmail.com>
2011-03-22 16:59:19 -0700

    fix 43017 - sanitize 'html' parameter before using it as filename

--- sugarcrm/modules/WorkFlowAlerts/Popup.php
+++ sugarcrm/modules/WorkFlowAlerts/Popup.php
@@ -55,8 +55,8 @@ if(!empty($_REQUEST['parent_id']) && $_REQUEST['parent_id']!="") {
     $workflow_object = $seed_object->get_workflow_object();
 
 } else {
-	sugar_die("You shouldn't be here");	
-}	
+	sugar_die("You shouldn't be here");
+}
 
 
 
@@ -69,7 +69,8 @@ if (!isset($_REQUEST['html'])) {
 	$GLOBALS['log']->debug("using file modules/WorkFlowAlerts/Popup_picker.html");
 }
 else {
-	$GLOBALS['log']->debug("_REQUEST['html'] is ".$_REQUEST['html']);
+    $_REQUEST['html'] = preg_replace("/[^a-zA-Z0-9_]/", "", $_REQUEST['html']);
+    $GLOBALS['log']->debug("_REQUEST['html'] is ".$_REQUEST['html']);
 	$form =new XTemplate ('modules/WorkFlowAlerts/'.$_REQUEST['html'].'.html');
 	$GLOBALS['log']->debug("using file modules/WorkFlowAlerts/".$_REQUEST['html'].'.html');
 }
@@ -77,7 +78,7 @@ else {
 $form->assign("MOD", $mod_strings);
 $form->assign("APP", $app_strings);
 
-$focus = new WorkFlowAlert();  
+$focus = new WorkFlowAlert();
 //Add When Expressions Object is availabe
 //$exp_object = new Expressions();
 
@@ -92,15 +93,15 @@ if(isset($_REQUEST['record']) && isset($_REQUEST['record'])) {
         $the_javascript .= "}\n";
         $the_javascript .= "</script>\n";
 
-	$form->assign("ID", $focus->id); 
-    $form->assign("PARENT_ID", $_REQUEST['parent_id']);    
+	$form->assign("ID", $focus->id);
+    $form->assign("PARENT_ID", $_REQUEST['parent_id']);
     $form->assign("ADDRESS_TYPE", get_select_options_with_id($app_list_strings['wflow_address_type_dom'],$focus->address_type));
 	$form->assign("USER_TYPE", get_select_options_with_id($app_list_strings['wflow_user_type_dom'],$focus->user_type));
-        
+
 	$form->assign("REL_MODULE1", get_select_options_with_id($focus->get_rel_module_array($workflow_object->base_module),$focus->rel_module1));
 	$form->assign("ARRAY_TYPE", get_select_options_with_id($app_list_strings['wflow_array_type_dom'],$focus->array_type));
-	$form->assign("RELATE_TYPE", get_select_options_with_id($app_list_strings['wflow_relate_type_dom'],$focus->relate_type));	
-	$form->assign("FIELD_VALUE", get_select_options_with_id($focus->get_field_value_array($workflow_object->base_module, "User"),$focus->field_value));	
+	$form->assign("RELATE_TYPE", get_select_options_with_id($app_list_strings['wflow_relate_type_dom'],$focus->relate_type));
+	$form->assign("FIELD_VALUE", get_select_options_with_id($focus->get_field_value_array($workflow_object->base_module, "User"),$focus->field_value));
 
 if($focus->user_type=="Related"){
 
@@ -109,24 +110,24 @@ if($focus->user_type=="Related"){
 		$form->assign("REL_FIELD_VALUE", $focus->field_value);
 		$form->assign("REL_EMAIL_VALUE", $focus->rel_email_value);
 	} else {
-	}	
+	}
 
 	$form->assign("REL_MODULE2", $focus->rel_module2);
-	
-	
+
+
 //end if else Current or Related
 }
 
 //close window and refresh parent if needed
 
 if(!empty($_REQUEST['special_action']) && $_REQUEST['special_action'] == "refresh"){
-	
+
 	$special_javascript = "window.opener.document.DetailView.action.value = 'DetailView'; \n";
 	$special_javascript .= "window.opener.document.DetailView.submit(); \n";
 	$special_javascript .= "window.close();";
 	$form->assign("SPECIAL_JAVASCRIPT", $special_javascript);
-	
-}	
+
+}
 
 $form->assign("MODULE_NAME", $currentModule);
 //$form->assign("FORM", $_REQUEST['form']);
