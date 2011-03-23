2c4af1dfa65234bd6e11dee90eb2eacda7f40d43
roger <roger@sugar-crms-macbook-pro-3.local>
2011-03-22 20:57:23 -0400

    Bug: 43003 - fixed merge issue whcih added id to subpanel buttons

--- sugarcrm/include/generic/SugarWidgets/SugarWidgetSubPanelTopButton.php
+++ sugarcrm/include/generic/SugarWidgets/SugarWidgetSubPanelTopButton.php
@@ -1,4 +1,4 @@
-<?php
+    <?php
 if(!defined('sugarEntry') || !sugarEntry) die('Not A Valid Entry Point');
 /**
  * SugarWidgetSubPanelTopButton
@@ -268,10 +268,10 @@ class SugarWidgetSubPanelTopButton extends SugarWidget
         if ( isset($_REQUEST['layout_def_key']) && $_REQUEST['layout_def_key'] == 'UserEAPM' ) {
             // Subpanels generally don't go on the editview, so we have to handle this special
             $megaLink = $this->_get_form($defines, $additionalFormFields,true);
-            $button = "<input title='$this->title' accesskey='$this->access_key' class='button' type='submit' name='" . $this->getWidgetId() . "_create_button' value='  $this->form_value  ' onclick='javascript:document.location=\"index.php?".$megaLink."\"; return false;'/>";
+            $button = "<input title='$this->title' accesskey='$this->access_key' class='button' type='submit' name='$inputID' id='$inputID' value='  $this->form_value  ' onclick='javascript:document.location=\"index.php?".$megaLink."\"; return false;'/>";
         } else {
             $button = $this->_get_form($defines, $additionalFormFields);
-            $button .= "<input title='$this->title' accesskey='$this->access_key' class='button' type='submit' name='" . $this->getWidgetId() . "_create_button' value='  $this->form_value  ' />\n</form>";
+            $button .= "<input title='$this->title' accesskey='$this->access_key' class='button' type='submit' name='$inputID' id='$inputID' value='  $this->form_value  ' />\n</form>";
         }
         return $button;
 	}
