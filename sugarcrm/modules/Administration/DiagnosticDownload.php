e7b2ab97d56549b8b6e47d105b552624be5acc75
Majed Itani <mitani@mitani.local>
2011-04-09 00:01:20 -0700

    Bug #43330

--- sugarcrm/modules/Administration/DiagnosticDownload.php
+++ sugarcrm/modules/Administration/DiagnosticDownload.php
@@ -28,23 +28,31 @@ if(!defined('sugarEntry') || !sugarEntry) die('Not A Valid Entry Point');
  * $Id: DiagnosticDownload.php 45203 2009-03-17 17:46:55Z maubert $
  ********************************************************************************/
 
-if(!isset($_REQUEST['file']))
+global $current_user;
+
+
+if (!is_admin($current_user)) sugar_die("Unauthorized access to administration.");
+
+http://localhost:8888/Mango/build/rome/builds/ent/sugarcrm/cache/diagnostic/d9b809a4-c2e3-f93e-eb95-4d9ffe76db13/diagnostic20110408-233534.zip
+if(!isset($_REQUEST['guid']) || !isset($_REQUEST['time']))
 {
 	die('Did not receive a filename to download');
 }
-
-$filesize = filesize(getcwd()."/{$GLOBALS['sugar_config']['cache_dir']}diagnostic/".$_REQUEST['file'].".zip");
+$time = str_replace(array('.', '/', '\\'), '', $_REQUEST['time']);
+$guid = str_replace(array('.', '/', '\\'), '', $_REQUEST['guid']);
+$path = getcwd()."/{$GLOBALS['sugar_config']['cache_dir']}diagnostic/{$guid}/diagnostic{$time}.zip";
+$filesize = filesize($path);
 
 header('Content-type: application/zip');
 header("Pragma: public");
 header("Expires: 0");
 header("Cache-Control: must-revalidate, post-check=0, pre-check=0");
 header("Cache-Control: private",false);
-header("Content-Disposition: attachment; filename=".$_REQUEST['file'].".zip");
+header("Content-Disposition: attachment; filename=$guid.zip");
 header("Content-Transfer-Encoding: binary");
 header("Content-Length: $filesize");
 
-readfile(getcwd().'/'.$GLOBALS['sugar_config']['cache_dir'].'diagnostic/'.$_REQUEST['file'].'.zip');
+readfile($path);
 
 
 ?>
