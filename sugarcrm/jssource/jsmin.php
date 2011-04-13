44239325f1a6ad84357a19b6d8983f9b2f0a2733
Peter Deng <pdeng@redsticker.cup1.sugarcrm.net>
2011-04-13 15:45:34 -0700

    Bnelson's improved regex detection

--- sugarcrm/jssource/jsmin.php
+++ sugarcrm/jssource/jsmin.php
@@ -1,8 +1,20 @@
 <?php
 
 
+class JSMin {
+    /**
+     * Calls the SugarMin minify function.
+     *
+     * @param string $js Javascript to be minified
+     * @return string Minified javascript
+     */
+    public static function minify($js, $filename = '') {
+        return SugarMin::minify($js);
+    }
+}
+
 /**
- * SugarMin is a Javascript minifier with two levels of compression. The default compression
+ * SugarMin is a Javascript minifier with two levels of compression. The default compression 
  * is set to light. Light compression will preserve some line breaks that may interfere with
  * operation of the script. Deep compression will remove all the line breaks, but before using
  * deep compression make sure the script has passed JSLint.
@@ -10,15 +22,17 @@
 class SugarMin {
     protected $noSpaceChars = array('\\', "$", '_');
     protected $postNewLineSafeChars = array('\\', '$', '_', '{', '[', '(', '+', '-');
+    protected $regexOpeners = array('(', '=', '[', ':');
     protected $preNewLineSafeChars = array('\\', '$', '_', '}', ']', ')', '+', '-', '"', "'");
     protected $regexChars = array('(', ',',  '=', ':', '[', '!', '&', '|', '?', '{', '}', ';');
     protected $compression;
     protected $inLiteral = false;
+    protected $inRegex = false;
     protected $inMlComment = false;
-    protected $lastchar = null;
-
+    protected $lastChar = null;
+    protected $lastRegexOpener = null;
     private function __construct() {}
-
+    
     /**
      * Entry point function to minify javascript.
      *
@@ -36,16 +50,16 @@ class SugarMin {
             throw $e;
         }
     }
-
+	
     /**
-	 * jsParser will take javascript source code and
+	 * jsParser will take javascript source code and 
 	 *
-	 * @param string $js
-	 * @param string $currentOptions
+	 * @param string $js 
+	 * @param string $currentOptions 
 	 * @return void
 	 */
     protected function jsParser($js, $compression = 'light') {
-
+	    
         // We first perform a few operations to simplify our
         // minification process. We convert all carriage returns
         // into line breaks and delete runs of spacees and line breaks.
@@ -53,12 +67,12 @@ class SugarMin {
         $js = str_replace("\r\n", "\n", $js);
         $js = str_replace("\r", "\n", $js);
         $js = preg_replace("/\n+/","\n", $js);
-
+        
         // Split our string up into an array and iterate over each line
         // to do processing.
         $input = explode("\n", $js);
         $primedInput = '';
-
+        
         // In the first pass we will strip out multiline comments and single line comments
         // To allow for easier parsing / processing in the second pass.
         for ($index = 0; $index < count($input); $index++) {
@@ -68,12 +82,12 @@ class SugarMin {
             $line = $this->getEscapedLine($line);
 
             $line = trim($line, " \t");
-
+            
             // If the line is empty, ignore it.
             if (strlen($line) == 0) {
                 continue;
             }
-
+            
             $primedInput[] = $line;
         }
 
@@ -81,20 +95,21 @@ class SugarMin {
         // advanced parsing / stripping of spaces and literals.
         $input = $primedInput;
         $output = '';
-
+        
         for ($index = 0; $index < count($input); $index++) {
             $line = $input[$index];
             $newLine = '';
             $len = strlen($line);
 
             $nextLine = ($index < count($input) -1 ) ? $input[$index + 1] : '';
-
+            
             $lastChar = $line[$len - 1];
             $nextChar = ($nextLine) ? $nextLine[0] : null;
-
+            
             // Iterate through the string one character at a time.
             for ($i = 0; $i < $len; $i++) {
                 switch($line[$i]) {
+                    /*
                     // We will need to check to see if the / is the start of a regular expression.
                     // There is an issue if you have a pattern that follows a string, the parser will
                     // not recognize it as a reguler expression. Example: return / someregex/;
@@ -103,16 +118,16 @@ class SugarMin {
                         if (in_array($newLine[strlen($newLine) - 1], $this->regexChars)) {
                             $nesting = 0;
                             $newLine .= $line[$i];
-
-                            for ($j = $i + 1; $j < $len; $j++) {
+                            
+                            for ($j = $i + 1; $j < $len; $j++) { 
                                 if ($line[$j] == '[') {
                                     $nesting++;
                                 } else if ($line[$j] == ']') {
                                     $nesting--;
                                 }
-
+                                
                                 $newLine .= $line[$j];
-
+                                
                                 if ($line[$j] == '/' && $nesting == 0 && $newLine[strlen($newLine) - 1] != "\\") {
                                     break;
                                 }
@@ -122,25 +137,27 @@ class SugarMin {
                             $newLine .= $line[$i];
                         }
                         break;
+                     
+                     */
                     // String literals shall be transcribed as is.
                     case '"':
                     case "'":
                         $literal = $delimiter = $line[$i];
-
+                        
                         for ($j = $i + 1; $j < strlen($line); $j++) {
                             $literal .= $line[$j];
-
+                            
                             if ($line[$j] == "\\") {
                                 $literal .= $line[$j + 1];
                                 $j++;
                                 continue;
                             }
-
+                            
                             if ($line[$j] == $delimiter) {
                                 break;
                             }
                         }
-
+                        
                         $i = $j;
                         $newLine .= $literal;
                         break;
@@ -159,14 +176,14 @@ class SugarMin {
                         break;
                 }
             }
-
+            
             if ((ctype_alnum($lastChar) || in_array($lastChar, $this->preNewLineSafeChars)) && ((in_array($nextChar, $this->postNewLineSafeChars) || ctype_alnum($nextChar)))) {
                 $newLine .= "\n";
             }
-
+            
             $output .= $newLine;
         }
-
+        
         if ($compression == 'deep') {
             return trim(str_replace("\n", "", $output));
         } else {
@@ -174,28 +191,38 @@ class SugarMin {
         }
 	}
 
-    protected function getEscapedLine($line)
-    {
+    protected function getEscapedLine($line) {
+        $this->lastChar = null;
         $len = strlen($line);
         $res = '';
-
-        for($i = 0; $i < $len; $i ++)
-        {
+        
+        for($i = 0; $i < $len; $i ++) {
             $char = $line[$i];
-            if ($this->inMlComment)
-            {
-                $this->checkMlComment($char);
+
+            // We need to check to see if it's valid to have a regular expression at this point.
+            // So we store away the last valid character prior to a regex.
+            if (in_array($char, $this->regexOpeners)) {
+                $this->lastRegexOpener = $char;
+            } else if (!in_array($char, array(' ',"\t", '/'))) {
+                $this->lastRegexOpener = null;
             }
-            else
-            {
-                if ($this->checkLiteral($char))
+
+            if ($this->inMlComment) {
+                $this->checkMlComment($char);
+            } else {
+                $regex = $this->checkRegex($line, $i);
+                if ($regex !== false)
+                {
+                    $res = $res . $regex;
+                    $i = $i + strlen($regex) - 1;
+                }
+                elseif ($this->checkLiteral($char))
                 {
                     $res = $res . $char;
                 }
                 elseif ($this->checkComment($char))
                 {
                     // Return with the result with the / from // removed
-                    $this->lastchar = null;
                     return substr($res, 0, -1);
                 }
                 elseif($this->checkMlComment($char))
@@ -208,20 +235,56 @@ class SugarMin {
                     $res = $res . $char;
                 }
             }
-            $this->lastchar = $char;
+            $this->lastChar = $char;
         }
-        $this->lastchar = null;
         return $res;
+    }
+
+    protected function checkRegex($line, $i) {
+        if ($this->lastRegexOpener !== null) {
+            $char = $line[$i];
+
+            if ($char === '/') {
+                $len = strlen($line);
+                if ($len > $i + 1) {
+                    $char2 = $line[$i + 1];
+                    if ($char2 !== '*' && $char2 !== '/') {
+                        $ret = "/";
+                        $escape = 0;
+                        for ($j = $i + 1; $j < $len; $j ++) {
+                            $char = $line[$j];
+                            $ret = $ret . $char;
+                            switch($char) {
+                                case '/':
+                                    if ($escape % 2 == 0)
+                                    {
+                                        return $ret;
+                                    }
+                                    break;
+                                case '\\':
+                                    $escape++;
+                                    break;
+                                default:
+                                    $escape = 0;
+                                    break;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        return false;
     }
 
 
     protected function checkMlComment($char)
     {
-        if ($this->inMlComment && (($this->lastchar === '*') && ($char === '/')))
+        if ($this->inMlComment && (($this->lastChar === '*') && ($char === '/')))
         {
             $this->inMlComment = false;
         }
-        elseif (($this->lastchar === '/') && ($char === '*'))
+        elseif (($this->lastChar === '/') && ($char === '*'))
         {
             $this->inMlComment = true;
         }
@@ -230,7 +293,7 @@ class SugarMin {
 
     protected function checkComment($char)
     {
-        if (($char === '/') &&  ($this->lastchar === '/'))
+        if (($char === '/') &&  ($this->lastChar === '/'))
         {
             // Reset the last char as we should be escaping out of checking now.
             return true;
@@ -238,6 +301,7 @@ class SugarMin {
 
         return false;
     }
+    
     /**
      * Check to see if the character at a certain position is inside a string literal.
      * Returns true if the character at $position is inside a string literal and false
@@ -257,7 +321,10 @@ class SugarMin {
                 // Flip the literal bit
                 $this->inLiteral = (!$this->inLiteral);
             }
-            $escapes = 0;
+            else
+            {
+                $escapes = 0;
+            }
         }
         elseif ($char == '"' || $char == "'") {
             $this->inLiteral = true;
