d1efadca3fd6a76edba4ede685526b9d2ded3ec1
Jimmy Chen <jchen@sugarcrm.com>
2011-04-13 16:33:09 -0700

    Bug 43307 adding parentheses in where caluse to avoid ambiguity

--- sugarcrm/modules/Reports/Report.php
+++ sugarcrm/modules/Reports/Report.php
@@ -897,7 +897,8 @@ print "<BR>";
     }
     
     function filtersIterate($filters, &$where_clause) {
-        //$where_arr = array();
+        $where_clause .= '(';
+    	//$where_arr = array();
         $operator = $filters['operator'];
         $isSubCondition = 0;
         if(count($filters) < 2) { // We only have an operator and an empty Filter Box.
@@ -922,6 +923,7 @@ print "<BR>";
                 $where_clause .= " $operator ";
             
         }
+        $where_clause .= ')';
     }
     
     function create_where()
