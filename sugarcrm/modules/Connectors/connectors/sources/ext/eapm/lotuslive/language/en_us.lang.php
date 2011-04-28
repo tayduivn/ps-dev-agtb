bd7cdc7a73d8708ae7f62e92334add85abb0832a
Rob Aagaard <rob@sugarcrm.com>
2011-04-28 15:10:33 -0600

    Bug 43679: Add information on how to obtain a Lotus Live oAuth key.

--- sugarcrm/modules/Connectors/connectors/sources/ext/eapm/lotuslive/language/en_us.lang.php
+++ sugarcrm/modules/Connectors/connectors/sources/ext/eapm/lotuslive/language/en_us.lang.php
@@ -22,7 +22,19 @@ if(!defined('sugarEntry') || !sugarEntry) die('Not A Valid Entry Point');
  ********************************************************************************/
 
 $connector_strings = array (
-    'LBL_LICENSING_INFO' => '<table border="0" cellspacing="1"><tr><td valign="top" width="35%" class="dataLabel">The OAuth Consumer Key and Secret are strings you obtain from LotusLive&#169;.</td></tr></table>',
+    'LBL_LICENSING_INFO' => '<table border="0" cellspacing="1"><tr><td valign="top" width="35%" class="dataLabel">Obtain a Key and Secret from LotusLive&copy; by registering your Sugar instance as a new application.<br>
+&nbsp;<br>
+Steps to register your instance:<br>
+&nbsp;<br>
+<ol>
+<li>Log in to your LotusLive account (you must be a LotusLive administrator): <a href="https://www.lotuslive.com/" target="_new">https://www.lotuslive.com/</a></li>
+<li>Click Administration in the navigator bar at the top.</li>
+<li>Click Manage Company Apps.</li>
+<li>Click Register App.</li>
+<li>Enter a name and description for the application when prompted. The name and description of your application can be changed at any time by clicking the menu next to the application and selecting Edit properties.</li>
+<li>During registration, your application will be assigned an OAuth key and secret pair. Registered applications are displayed in Manage Custom Apps. To view the assigned key and secret, click the menu for the appropriate application and select Get Credentials.</li>
+</ol>
+</td></tr></table>',
     'oauth_consumer_key' => 'OAuth Consumer Key',
     'oauth_consumer_secret' => 'OAuth Consumer Secret',
 );
