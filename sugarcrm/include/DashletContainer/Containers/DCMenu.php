03bb02a97027c1f4df9c0e24881427bfbaccd586
Jelle Vink <jvink@sugarcrm.com>
2011-05-02 20:14:14 +0200

    example how to replace getImageURL by getImage to leverage the usage of the css sprites

--- sugarcrm/include/DashletContainer/Containers/DCMenu.php
+++ sugarcrm/include/DashletContainer/Containers/DCMenu.php
@@ -24,9 +24,6 @@ class DCMenu extends DashletContainer
 {	
 	protected function getMenuItem($module)
 	{
-	    $imageURL = SugarThemeRegistry::current()->getImageURL("icon_{$module}_bar_32.png");
-	    if (empty($imageURL))
-	    	$imageURL = SugarThemeRegistry::current()->getImageURL("icon_generic_bar_32.png");
 	    $module_mod_strings = return_module_language($GLOBALS['current_language'], $module);
 	    if ( isset($module_mod_strings['LNK_NEW_RECORD']) )
 	        $createRecordTitle = $module_mod_strings['LNK_NEW_RECORD'];
@@ -36,8 +33,10 @@ class DCMenu extends DashletContainer
 	        $createRecordTitle = $module_mod_strings['LNK_NEW_CASE'];
         else
 	        $createRecordTitle = $GLOBALS['app_strings']['LBL_CREATE_BUTTON_LABEL'].' '.$module_mod_strings['LBL_MODULE_NAME'];
+
+		$image = SugarThemeRegistry::current()->getImage("icon_{$module}_bar_32","class='icon' alt='{$createRecordTitle}' title='{$createRecordTitle}'", null, null, '.png');
 	    return <<<EOQ
-		<li><a href="javascript: if ( DCMenu.menu ) DCMenu.menu('$module','$createRecordTitle');"><img class='icon' src='{$imageURL}' alt='{$createRecordTitle}' title='{$createRecordTitle}'></a></li>	
+		<li><a href="javascript: if ( DCMenu.menu ) DCMenu.menu('$module','$createRecordTitle');">{$image}</a></li>	
 EOQ;
 
 	}
@@ -77,31 +76,33 @@ EOQ;
             }
 
 		if($unreadNotifications > 0) {
-			$iconImageUrl = SugarThemeRegistry::current()->getImageURL("dcMenuSugarCube_alert.png");
+			$cubeIcon = "dcMenuSugarCube_alert";
 			$code = '<div id="notifCount" class="notifCount">'.$unreadNotifications.'</div>';
 			$class = "";
 		} else {
-			$iconImageUrl = SugarThemeRegistry::current()->getImageURL("dcMenuSugarCube.png");
+			$cubeIcon = "dcMenuSugarCube";
 			$code = '';
 			$class = ' class="none"';
 		}
-		
+	
+		$image = SugarThemeRegistry::current()->getImage($cubeIcon,"class='dc_notif_icon' border='0'", null, null, '.png');
 		$notificationsHTML = <<<EOQ
 		
 			<div id="dcmenuSugarCube" $class>
-			  <a href="javascript: DCMenu.notificationsList();" class="notice"><img class='dc_notif_icon' src='$iconImageUrl' border="0"></a>
+			  <a href="javascript: DCMenu.notificationsList();" class="notice">{$image}</a>
 			  $code
 			</div>	
 			
 EOQ;
 		} else {
-			$iconImageUrl = SugarThemeRegistry::current()->getImageURL("dcMenuSugarCube.png");
+			$cubeIcon = "dcMenuSugarCube";
 				$code = '';
 				$class = ' class="none"';
+			$image = SugarThemeRegistry::current()->getImage($cubeIcon,"class='dc_notif_icon' border='0'", null, null, '.png');
 				$notificationsHTML = <<<EOQ
 			
 			<div id="dcmenuSugarCube" $class>
-			  <img class='dc_notif_icon' src='$iconImageUrl' border="0">
+			  $image
 			  $code
 			</div>	
 EOQ;
