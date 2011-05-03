ebe1be65fbcd73bd2e86c7a788b7cd25097338ed
Stanislav Malyshev <smalyshev@gmail.com>
2011-05-03 11:56:52 -0700

    fix user save on install

--- sugarcrm/modules/Users/User.php
+++ sugarcrm/modules/Users/User.php
@@ -961,7 +961,7 @@ EOQ;
 		if (is_admin($current_user)) {
 			$remaining_admins = $this->db->getOne("SELECT COUNT(*) as c from users where is_admin = 1 AND deleted=0");
 
-			if (($remaining_admins <= 1) && ($this->is_admin != "on") && ($this->id == $current_user->id)) {
+			if (($remaining_admins <= 1) && $this->id == $current_user->id && !is_admin($this)) {
 				$GLOBALS['log']->debug("Number of remaining administrator accounts: {$remaining_admins}");
 				$this->error_string .= $mod_strings['ERR_LAST_ADMIN_1'].$this->user_name.$mod_strings['ERR_LAST_ADMIN_2'];
 				$verified = FALSE;
