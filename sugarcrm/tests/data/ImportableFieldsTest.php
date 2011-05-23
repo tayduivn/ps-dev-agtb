4382e6149834d448ded44be8240996bb1b067e5e
John Mertic <jmertic@sugarcrm.com>
2011-05-23 11:22:53 -0400

    Add license headers for all of the unit test files.

--- sugarcrm/tests/data/ImportableFieldsTest.php
+++ sugarcrm/tests/data/ImportableFieldsTest.php
@@ -1,5 +1,6 @@
 <?php
-/********************************************************************************
+
+/*********************************************************************************
  *The contents of this file are subject to the SugarCRM Professional End User License Agreement
  *("License") which can be viewed at http://www.sugarcrm.com/EULA.
  *By installing or using this file, You have unconditionally agreed to the terms and conditions of the License, and You may
