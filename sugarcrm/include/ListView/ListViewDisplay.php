77fa69ced41fba2317e4e9bd6039eac36526ab1b
Jenny Gonsalves <jenny@sugarcrm.com>
2011-05-25 16:50:46 -0700

    Bug 44157 - Reverting commits f4f031639b9a4f0d32305d47748637e2561d4dbb and bae43f98ca98e3ed2b0fa043bffd48b01030a5dd as they cause this bug.

--- sugarcrm/include/ListView/ListViewDisplay.php
+++ sugarcrm/include/ListView/ListViewDisplay.php
@@ -509,8 +509,8 @@ EOHTML;
 	protected function buildTargetList()
 	{
         global $app_strings;
-        $current_query_by_page = base64_encode(serialize(array_merge($_POST, $_GET)));
-
+        $current_query_by_page = base64_encode(serialize($_REQUEST));
+        
 		$js = <<<EOF
             if(sugarListView.get_checks_count() < 1) {
                 alert('{$app_strings['LBL_LISTVIEW_NO_SELECTED']}');
