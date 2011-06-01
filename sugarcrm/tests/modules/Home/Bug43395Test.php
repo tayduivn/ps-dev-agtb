10b6f93bcd30876c84e520f881f3740930705785
Collin Lee <clee@Collin-Lee-MacBook-Pro.local>
2011-06-01 18:22:42 -0400

    Changed test to use predefined admin user.

--- sugarcrm/tests/modules/Home/Bug43395Test.php
+++ sugarcrm/tests/modules/Home/Bug43395Test.php
@@ -9,8 +9,9 @@ class Bug43395Test extends Sugar_PHPUnit_Framework_OutputTestCase
     	global $app_strings, $app_list_strings;
         $app_strings = return_application_language($GLOBALS['current_language']);
         $app_list_strings = return_app_list_strings_language($GLOBALS['current_language']);    	
-    	
-        $GLOBALS['current_user'] = SugarTestUserUtilities::createAnonymousUser();
+    	$user = new User();
+    	$user->retrieve('1');
+        $GLOBALS['current_user'] = $user;
         self::$contact = SugarTestContactUtilities::createContact();
         self::$contact->first_name = 'Bug43395';
         self::$contact->last_name = 'Test';
@@ -22,13 +23,12 @@ class Bug43395Test extends Sugar_PHPUnit_Framework_OutputTestCase
     {
         unset($_REQUEST['data']);
         unset($_REQUEST['query']);
-	    SugarTestUserUtilities::removeAllCreatedAnonymousUsers();
-        unset($GLOBALS['current_user']);
         SugarTestContactUtilities::removeAllCreatedContacts();
     }
     
     public function testFormatResults()
     {	
+    	$_REQUEST = array();
     	$_REQUEST['data'] = '{"form":"search_form","method":"query","modules":["Contacts"],"group":"or","field_list":["name","id"],"populate_list":["contact_c_basic","contact_id_c_basic"],"required_list":["parent_id"],"conditions":[{"name":"name","op":"like_custom","end":"%","value":""}],"order":"name","limit":"30","no_match_text":"No Match"}';
         $_REQUEST['query'] = self::$contact->first_name;
         require_once 'modules/Home/quicksearchQuery.php';
@@ -45,18 +45,18 @@ class Bug43395Test extends Sugar_PHPUnit_Framework_OutputTestCase
  		self::$quickSearch = new quicksearchQuery();
 		$result = self::$quickSearch->query($data);
 		$resultBean = $json->decodeReal($result);
-	    $this->assertEquals($resultBean['fields'][0]['name'], self::$contact->first_name . ' ' . self::$contact->last_name);
+	    $this->assertEquals($resultBean['fields'][0]['name'], self::$contact->first_name . ' ' . self::$contact->last_name, 'Assert that the quicksearch returns a contact name without salutation');
     }
     
     public function testPersonLocaleNameFormattting()
     {
     	self::$contact->createLocaleFormattedName = true;
     	self::$contact->_create_proper_name_field();
-    	$this->assertEquals(self::$contact->name, 'Mr. Bug43395 Test');
+    	$this->assertEquals(self::$contact->name, 'Mr. Bug43395 Test', 'Assert that _create_proper_name_field with createLocaleFormattedName set to true returns salutation');
 
     	self::$contact->createLocaleFormattedName = false;
     	self::$contact->_create_proper_name_field();
-    	$this->assertEquals(self::$contact->name, 'Bug43395 Test');
+    	$this->assertEquals(self::$contact->name, 'Bug43395 Test', 'Assert that _create_proper_name_field with createLocaleFormattedName set to false does not return salutation');
     }
     
 }
