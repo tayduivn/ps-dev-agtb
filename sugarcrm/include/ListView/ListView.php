tayduivn/ps-dev-agtb

commit dd228783d91c57b59556d168e3c11b212333cee6

Revert "empty Mango folder - syncing"
This reverts commit 4032fdd4314dd46a783078995f4ec07ce7df55d8.

sugarcrm/include/generic/SugarWidgets/SugarWidgetField.php
@@ -1,0 +1,228 @@
+<?php
+if(!defined('sugarEntry') || !sugarEntry)
+	die('Not A Valid Entry Point');
+/**
+ * SugarWidgetField
+ *
+ * LICENSE: The contents of this file are subject to the SugarCRM Professional
+ * End User License Agreement ("License") which can be viewed at
+ * http://www.sugarcrm.com/EULA.  By installing or using this file, You have
+ * unconditionally agreed to the terms and conditions of the License, and You
+ * may not use this file except in compliance with the License.  Under the
+ * terms of the license, You shall not, among other things: 1) sublicense,
+ * resell, rent, lease, redistribute, assign or otherwise transfer Your
+ * rights to the Software, and 2) use the Software for timesharing or service
+ * bureau purposes such as hosting the Software for commercial gain and/or for
+ * the benefit of a third party.  Use of the Software may be subject to
+ * applicable fees and any use of the Software without first paying applicable
+ * fees is strictly prohibited.  You do not have the right to remove SugarCRM
+ * copyrights from the source code or user interface.
+ *
+ * All copies of the Covered Code must include on each user interface screen:
+ *  (i) the "Powered by SugarCRM" logo and
+ *  (ii) the SugarCRM copyright notice
+ * in the same form as they appear in the distribution.  See full license for
+ * requirements.
+ *
+ * Your Warranty, Limitations of liability and Indemnity are expressly stated
+ * in the License.  Please refer to the License for the specific language
+ * governing these rights and limitations under the License.  Portions created
+ * by SugarCRM are Copyright (C) 2005 SugarCRM, Inc.; All Rights Reserved.
+ */
+
+// $Id: SugarWidgetField.php 57035 2010-06-19 01:58:47Z kjing $
+
+require_once ('include/generic/SugarWidgets/SugarWidget.php');
+
+class SugarWidgetField extends SugarWidget {
+
+	function SugarWidgetField(&$layout_manager) {
+        parent::SugarWidget($layout_manager);
+    }
+
+	function display($layout_def) {
+		//print $layout_def['start_link_wrapper']."===";
+		$context = $this->layout_manager->getAttribute('context'); //_ppd($context);
+		$func_name = 'display'.$context;
+
+		if (!empty ($context) && method_exists($this, $func_name)) {
+			return $this-> $func_name ($layout_def);
+		} else {
+			return 'display not found:'.$func_name;
+		}
+	}
+
+	function _get_column_alias($layout_def) {
+		$alias_arr = array ();
+
+		if (!empty ($layout_def['name']) && $layout_def['name'] == 'count') {
+			return 'count';
+		}
+
+		if (!empty ($layout_def['table_alias'])) {
+			array_push($alias_arr, $layout_def['table_alias']);
+		}
+
+		if (!empty ($layout_def['name'])) {
+			array_push($alias_arr, $layout_def['name']);
+		}
+
+		return $this->getTruncatedColumnAlias(implode("_", $alias_arr));
+	}
+
+	function & displayDetailLabel(& $layout_def) {
+
+		return '';
+	}
+
+	function & displayDetail($layout_def) {
+
+		return '';
+	}
+	function displayHeaderCellPlain($layout_def) {
+		$module_name = $this->layout_manager->getAttribute('module_name');
+		$header_cell_text = '';
+		$key = '';
+
+		if (!empty ($layout_def['label'])) {
+			$header_cell_text = $layout_def['label'];
+		}
+		elseif (!empty ($layout_def['vname'])) {
+			$key = $layout_def['vname'];
+
+			if (empty ($key)) {
+				$header_cell_text = $layout_def['name'];
+			} else {
+				$header_cell_text = translate($key, $module_name);
+			}
+		}
+		return $header_cell_text;
+	}
+
+	function displayHeaderCell($layout_def) {
+		$module_name = $this->layout_manager->getAttribute('module_name');
+
+		$this->local_current_module = $_REQUEST['module'];
+		$this->is_dynamic = true;
+		// don't show sort links if name isn't defined
+		if (empty ($layout_def['name'])) {
+			return $layout_def['label'];
+		}
+		if (isset ($layout_def['sortable']) && !$layout_def['sortable']) {
+			return $this->displayHeaderCellPlain($layout_def);
+		}
+
+		$header_cell_text = '';
+		$key = '';
+
+		if (!empty ($layout_def['vname'])) {
+			$key = $layout_def['vname'];
+		}
+
+		if (empty ($key)) {
+			$header_cell_text = $layout_def['name'];
+		} else {
+			$header_cell_text = translate($key, $module_name);
+		}
+
+		$subpanel_module = $layout_def['subpanel_module'];
+		$html_var = $subpanel_module . "_CELL";
+		if (empty ($this->base_URL)) {
+			$this->base_URL = ListView :: getBaseURL($html_var);
+			$split_url = explode('&to_pdf=true&action=SubPanelViewer&subpanel=', $this->base_URL);
+			$this->base_URL = $split_url[0];
+			$this->base_URL .= '&inline=true&to_pdf=true&action=SubPanelViewer&subpanel=';
+		}
+		$sort_by_name = $layout_def['name'];
+		if (isset ($layout_def['sort_by'])) {
+			$sort_by_name = $layout_def['sort_by'];
+		}
+
+		$sort_by = ListView :: getSessionVariableName($html_var, "ORDER_BY").'='.$sort_by_name;
+
+		$start = (empty ($layout_def['start_link_wrapper'])) ? '' : $layout_def['start_link_wrapper'];
+		$end = (empty ($layout_def['end_link_wrapper'])) ? '' : $layout_def['end_link_wrapper'];
+
+		$header_cell = "<a class=\"listViewThLinkS1\" href=\"".$start.$this->base_URL.$subpanel_module.'&'.$sort_by.$end."\">";
+		$header_cell .= $header_cell_text;
+		$header_cell .= "</a>";
+
+		$imgArrow = '';
+
+		if (isset ($layout_def['sort'])) {
+			$imgArrow = $layout_def['sort'];
+		}
+		$arrow_start = ListView :: getArrowUpDownStart($imgArrow);
+
+		$header_cell .= " ".$arrow_start;
+
+		return $header_cell;
+
+	}
+
+	function displayList($layout_def) {
+		return $this->displayListPlain($layout_def);
+	}
+
+	function displayListPlain($layout_def) {
+		$value= $this->_get_list_value($layout_def);
+		if (isset($layout_def['widget_type']) && $layout_def['widget_type'] =='checkbox') {
+			if ($value != '' &&  ($value == 'on' || intval($value) == 1 || $value == 'yes'))
+			{
+				return "<input name='checkbox_display' class='checkbox' type='checkbox' disabled='true' checked>";
+			}
+			return "<input name='checkbox_display' class='checkbox' type='checkbox' disabled='true'>";
+		}
+		return $value;
+	}
+
+	function _get_list_value(& $layout_def) {
+		$key = '';
+		$value = '';
+
+		if (isset ($layout_def['varname'])) {
+			$key = strtoupper($layout_def['varname']);
+		} else {
+			$key = $this->_get_column_alias($layout_def);
+			$key = strtoupper($key);
+		}
+
+		if (isset ($layout_def['fields'][$key])) {
+			return $layout_def['fields'][$key];
+		}
+		return $value;
+
+	}
+
+	function & displayEditLabel($layout_def) {
+		return '';
+	}
+
+	function & displayEdit($layout_def) {
+		return '';
+	}
+
+	function & displaySearchLabel($layout_def) {
+		return '';
+	}
+
+	function & displaySearch($layout_def) {
+		return '';
+	}
+
+	function displayInput($layout_def) {
+		return ' -- Not Implemented --';
+	}
+
+    function getVardef($layout_def) {
+        $myName = $layout_def['column_key'];
+        $vardef = $this->layout_manager->defs['reporter']->all_fields[$myName];
+
+        if ( !isset($vardef) ) {
+            // No vardef, return an empty array
+            return array();
+        } else {
+            return $vardef;
+        }
+    }
+}

sugarcrm/modules/Forecasts/ListView.php
@@ -1,0 +1,158 @@
+<?php
+if(!defined('sugarEntry') || !sugarEntry) die('Not A Valid Entry Point');
+/*********************************************************************************
+ *The contents of this file are subject to the SugarCRM Professional End User License Agreement 
+ *("License") which can be viewed at http://www.sugarcrm.com/EULA.  
+ *By installing or using this file, You have unconditionally agreed to the terms and conditions of the License, and You may 
+ *not use this file except in compliance with the License. Under the terms of the license, You 
+ *shall not, among other things: 1) sublicense, resell, rent, lease, redistribute, assign or 
+ *otherwise transfer Your rights to the Software, and 2) use the Software for timesharing or 
+ *service bureau purposes such as hosting the Software for commercial gain and/or for the benefit 
+ *of a third party.  Use of the Software may be subject to applicable fees and any use of the 
+ *Software without first paying applicable fees is strictly prohibited.  You do not have the 
+ *right to remove SugarCRM copyrights from the source code or user interface. 
+ * All copies of the Covered Code must include on each user interface screen:
+ * (i) the "Powered by SugarCRM" logo and 
+ * (ii) the SugarCRM copyright notice 
+ * in the same form as they appear in the distribution.  See full license for requirements.
+ *Your Warranty, Limitations of liability and Indemnity are expressly stated in the License.  Please refer 
+ *to the License for the specific language governing these rights and limitations under the License.
+ *Portions created by SugarCRM are Copyright (C) 2004 SugarCRM, Inc.; All Rights Reserved.  
+ ********************************************************************************/
+/*********************************************************************************
+ * $Id: ListView.php 55055 2010-03-03 19:00:58Z roger $
+ * Description:  TODO: To be written.
+ * Portions created by SugarCRM are Copyright (C) SugarCRM, Inc.
+ * All Rights Reserved.
+ * Contributor(s): ______________________________________..
+ ********************************************************************************/
+
+
+
+
+
+
+
+require_once('modules/Forecasts/Common.php');
+
+
+global $app_strings;
+global $app_list_strings;
+global $current_language;
+global $current_user;
+global $urlPrefix;
+global $currentModule;
+
+if (isset($_REQUEST['query'])) {
+	if (isset($_REQUEST['clear_query'])) {
+		$current_user->setPreference("forecast_timeperiod_id",'');
+	 	$current_user->setPreference("forecast_type",'');		
+	} else {		
+		if (isset($_REQUEST['timeperiod_id'])) {
+			$current_user->setPreference("forecast_timeperiod_id",$_REQUEST['timeperiod_id']);
+		}		
+		if (isset($_REQUEST['forecast_type'])){
+		 	$current_user->setPreference("forecast_type",$_REQUEST['forecast_type']);
+		}
+	}		
+}
+
+$ws_timeperiod_id="";
+$current_module_strings = return_module_language($current_language, 'Forecasts');
+//get user_id, if null set to logged in user.
+if (isset($_REQUEST['user_id'])) $ws_current_user_id = $_REQUEST['user_id'];
+if (empty($ws_current_user_id)) {
+	$ws_current_user_id = $current_user->id;
+}
+$comm = new Common();
+$comm->set_current_user($ws_current_user_id);
+$comm->setup();
+
+//get forecast type if null, if user is manager set to rollup else direct.
+if (isset($_REQUEST['forecast_type'])) $ws_forecast_type = $_REQUEST['forecast_type'];	
+if (empty($ws_forecast_type)) {
+	$ws_forecast_type = $current_user->getPreference("forecast_type");
+}
+if (empty($ws_forecast_type)) {
+	if ($comm->is_user_manager()) {
+		$ws_forecast_type = "Rollup";
+	} else {
+		$ws_forecast_type = "Direct";
+	}
+}
+//get timperiod_id, if null do nothing.
+if (isset($_REQUEST['timeperiod_id'])) $ws_timeperiod_id = $_REQUEST['timeperiod_id'];
+if (empty($ws_timeperiod_id)) {
+	$ws_timeperiod_id = $current_user->getPreference("forecast_timeperiod_id");
+}
+
+//get logged in users currency preference.
+$Currency = new Currency();
+$Currency->retrieve($current_user->getPreference('currency'));
+if( $Currency->deleted != 1)
+   $CurrencySymbol= $Currency->symbol;
+else
+   $CurrencySymbol= $currency->getDefaultCurrencySymbol();
+
+$seedForecast = new Forecast();
+
+echo getClassicModuleTitle($mod_strings['LBL_MODULE_NAME'], array($mod_strings['LBL_FORECAST_HISTORY_TITLE']), false);
+
+//START:processing the search form.
+$fcst_timeperiods =  $comm->get_my_forecasted_timeperiods($ws_current_user_id);
+
+echo get_form_header($mod_strings['LBL_DV_FORECAST_PERIOD'], "", false);
+$search_form=new XTemplate ('modules/Forecasts/SearchForm.html');
+$search_form->assign("MOD", $current_module_strings);
+$search_form->assign("APP", $app_strings);
+
+if ($comm->is_user_manager()) {
+	if ($ws_forecast_type == "Rollup")
+		$forecast_select = "<input name='forecast_type' class='radio' type='radio' value='Direct' > ".$current_module_strings['LBL_DV_MY_FORECASTS']."&nbsp;&nbsp;&nbsp;<input name='forecast_type' class='radio' type='radio' value='Rollup' checked> ".$current_module_strings['LBL_DV_MY_TEAM'];
+	else
+		$forecast_select = "<input name='forecast_type' class='radio' type='radio' value='Direct' checked > ".$current_module_strings['LBL_DV_MY_FORECASTS']."&nbsp;&nbsp;&nbsp;<input name='forecast_type' class='radio' type='radio' value='Rollup' > ".$current_module_strings['LBL_DV_MY_TEAM'];		
+} else {
+	$forecast_select = "<input type='hidden' name='forecast_type' value='Direct'/>";
+}
+$search_form->assign("FORECAST_TYPE", $forecast_select);
+
+//time period selection list
+$time_select_list="";
+if (count($fcst_timeperiods) == 0) {
+	$time_select_list="<OPTION VALUE='' SELECTED>".$app_strings['LBL_NONE']."</OPTION> ";
+} else {
+	$time_select_list="<OPTION VALUE=''>".$app_strings['LBL_NONE']."</OPTION> ";
+	foreach ($fcst_timeperiods as $key=>$value) {
+		if ($key == $ws_timeperiod_id)
+			$time_select_list .= "<OPTION VALUE='$key' SELECTED>$value </OPTION> ";
+		else
+			$time_select_list .= "<OPTION VALUE='$key'>$value </OPTION>";
+	}
+}
+$search_form->assign("CURRENT_TIMEPERIODS",$time_select_list);
+$search_form->assign("CURRENT_USER_ID", $current_user->id);
+$search_form->assign("SUBMODULE", "");
+
+$search_form->assign("JAVASCRIPT", get_clear_form_js());
+
+$search_form->parse("main");
+$search_form->out("main");
+//END: processing the search form.
+
+$where = " forecasts.user_id='$ws_current_user_id'";
+if (!empty($ws_forecast_type)) $where .= " AND forecast_type ='".$ws_forecast_type."'";  
+if (!empty($ws_timeperiod_id)) $where .= " AND timeperiod_id ='".$ws_timeperiod_id."'";  	
+
+//START: processing the list view.
+$ListView = new ListView();
+$ListView->initNewXTemplate('modules/Forecasts/ListView.html',$current_module_strings);
+$ListView->setHeaderTitle($current_module_strings['LBL_LIST_FORM_TITLE']);
+$ListView->setQuery($where, "", "", "FORECAST_LIST");
+$ListView->xTemplateAssign("CURRENCY_SYMBOL",$CurrencySymbol);
+$ListView->show_mass_update=false;
+$ListView->show_export_button=false;
+$ListView->show_delete_button=false;
+$ListView->show_select_menu=false;
+$ListView->processListView($seedForecast, "main", "FORECAST_LIST");
+//END: processing the list view.
+?>

sugarcrm/modules/Groups/ListView.php
@@ -1,0 +1,41 @@
+<?php
+if(!defined('sugarEntry') || !sugarEntry) die('Not A Valid Entry Point');
+/*********************************************************************************
+ * The contents of this file are subject to the SugarCRM Professional End User
+ * License Agreement ("License") which can be viewed at
+ * http://www.sugarcrm.com/EULA.  By installing or using this file, You have
+ * unconditionally agreed to the terms and conditions of the License, and You may
+ * not use this file except in compliance with the License. Under the terms of the
+ * license, You shall not, among other things: 1) sublicense, resell, rent, lease,
+ * redistribute, assign or otherwise transfer Your rights to the Software, and 2)
+ * use the Software for timesharing or service bureau purposes such as hosting the
+ * Software for commercial gain and/or for the benefit of a third party.  Use of
+ * the Software may be subject to applicable fees and any use of the Software
+ * without first paying applicable fees is strictly prohibited.  You do not have
+ * the right to remove SugarCRM copyrights from the source code or user interface.
+ * All copies of the Covered Code must include on each user interface screen:
+ * (i) the "Powered by SugarCRM" logo and (ii) the SugarCRM copyright notice
+ * in the same form as they appear in the distribution.  See full license for
+ * requirements.  Your Warranty, Limitations of liability and Indemnity are
+ * expressly stated in the License.  Please refer to the License for the specific
+ * language governing these rights and limitations under the License.
+ * Portions created by SugarCRM are Copyright (C) 2004 SugarCRM, Inc.;
+ * All Rights Reserved.
+ ********************************************************************************/
+
+
+global $mod_strings;
+global $current_language;
+
+$focus = new Group();
+$where = ' users.users.is_group = 1 ';
+
+$current_module_strings = return_module_language($current_language, 'Users');
+
+$ListView = new ListView();
+$ListView->initNewXTemplate('modules/Groups/ListView.html',$current_module_strings);
+$ListView->setHeaderTitle($mod_strings['LBL_LIST_TITLE']);
+$ListView->setQuery($where, "", "last_name, first_name", "USER");
+$ListView->show_mass_update=false;
+$ListView->processListView($focus, "main", "USER");
+?>

sugarcrm/modules/InboundEmail/ListView.php
@@ -1,0 +1,62 @@
+<?php
+if(!defined('sugarEntry') || !sugarEntry) die('Not A Valid Entry Point');
+/*********************************************************************************
+ * The contents of this file are subject to the SugarCRM Professional End User
+ * License Agreement ("License") which can be viewed at
+ * http://www.sugarcrm.com/EULA.  By installing or using this file, You have
+ * unconditionally agreed to the terms and conditions of the License, and You may
+ * not use this file except in compliance with the License. Under the terms of the
+ * license, You shall not, among other things: 1) sublicense, resell, rent, lease,
+ * redistribute, assign or otherwise transfer Your rights to the Software, and 2)
+ * use the Software for timesharing or service bureau purposes such as hosting the
+ * Software for commercial gain and/or for the benefit of a third party.  Use of
+ * the Software may be subject to applicable fees and any use of the Software
+ * without first paying applicable fees is strictly prohibited.  You do not have
+ * the right to remove SugarCRM copyrights from the source code or user interface.
+ * All copies of the Covered Code must include on each user interface screen:
+ * (i) the "Powered by SugarCRM" logo and (ii) the SugarCRM copyright notice
+ * in the same form as they appear in the distribution.  See full license for
+ * requirements.  Your Warranty, Limitations of liability and Indemnity are
+ * expressly stated in the License.  Please refer to the License for the specific
+ * language governing these rights and limitations under the License.
+ * Portions created by SugarCRM are Copyright (C) 2004 SugarCRM, Inc.;
+ * All Rights Reserved.
+ ********************************************************************************/
+global $theme;
+global $mod_strings;
+global $app_list_strings;
+global $current_user;
+
+$focus = new InboundEmail();
+$focus->checkImap();
+
+///////////////////////////////////////////////////////////////////////////////
+////	I-E SYSTEM SETTINGS
+////	handle saving settings
+if(isset($_REQUEST['save']) && $_REQUEST['save'] == 'true') {
+	$focus->saveInboundEmailSystemSettings('Case', $_REQUEST['inbound_email_case_macro']);
+}
+////	END I-E SYSTEM SETTINGS
+///////////////////////////////////////////////////////////////////////////////
+
+if(is_admin($current_user) && $_REQUEST['module'] != 'DynamicLayout' && !empty($_SESSION['editinplace'])){	
+	$ListView->setHeaderText("<a href='index.php?action=index&module=DynamicLayout&from_action=ListView&from_module=".$_REQUEST['module'] ."'>".SugarThemeRegistry::current()->getImage/*ALTFIXED*/("EditLayout","border='0' align='bottom'",null,null,'.gif',$mod_strings['LBL_EDIT_LAYOUT'])."</a>" );
+}
+
+$where = '';
+$limit = '0';
+$orderBy = 'date_entered';
+$varName = $focus->object_name;
+$allowByOverride = true;
+
+$listView = new ListView();
+$listView->initNewXTemplate('modules/InboundEmail/ListView.html', $mod_strings);
+$listView->setHeaderTitle($mod_strings['LBL_MODULE_TITLE']);
+
+echo $focus->getSystemSettingsForm();
+$listView->show_export_button = false;
+$listView->ignorePopulateOnly = TRUE; //Always show all records, ignore save_query performance setting.
+$listView->setQuery($where, $limit, $orderBy, 'InboundEmail', $allowByOverride);
+$listView->xTemplateAssign("EDIT_INLINE_IMG", SugarThemeRegistry::current()->getImage/*ALTFIXED*/('edit_inline','align="absmiddle" border="0"', null,null,'.gif',$app_strings['LNK_EDIT'])); 
+$listView->processListView($focus, "main", "InboundEmail");
+

sugarcrm/modules/KBDocuments/ListView.php
@@ -1,0 +1,180 @@
+<?php
+if(!defined('sugarEntry') || !sugarEntry) die('Not A Valid Entry Point');
+/*********************************************************************************
+ *The contents of this file are subject to the SugarCRM Professional End User License Agreement 
+ *("License") which can be viewed at http://www.sugarcrm.com/EULA.  
+ *By installing or using this file, You have unconditionally agreed to the terms and conditions of the License, and You may 
+ *not use this file except in compliance with the License. Under the terms of the license, You 
+ *shall not, among other things: 1) sublicense, resell, rent, lease, redistribute, assign or 
+ *otherwise transfer Your rights to the Software, and 2) use the Software for timesharing or 
+ *service bureau purposes such as hosting the Software for commercial gain and/or for the benefit 
+ *of a third party.  Use of the Software may be subject to applicable fees and any use of the 
+ *Software without first paying applicable fees is strictly prohibited.  You do not have the 
+ *right to remove SugarCRM copyrights from the source code or user interface. 
+ * All copies of the Covered Code must include on each user interface screen:
+ * (i) the "Powered by SugarCRM" logo and 
+ * (ii) the SugarCRM copyright notice 
+ * in the same form as they appear in the distribution.  See full license for requirements.
+ *Your Warranty, Limitations of liability and Indemnity are expressly stated in the License.  Please refer 
+ *to the License for the specific language governing these rights and limitations under the License.
+ *Portions created by SugarCRM are Copyright (C) 2004 SugarCRM, Inc.; All Rights Reserved.  
+ ********************************************************************************/
+/*********************************************************************************
+ * $Id: ListView.php 19107 2007-01-06 00:58:36 +0000 (Sat, 06 Jan 2007) wayne $
+ * Description:  TODO: To be written.
+ * Portions created by SugarCRM are Copyright (C) SugarCRM, Inc.
+ * All Rights Reserved.
+ * Contributor(s): ______________________________________..
+ ********************************************************************************/
+
+
+require_once('modules/KBDocuments/KBDcoument.php');
+
+
+require_once('include/ListView/ListViewSmarty.php');
+
+require_once('custom/modules/Cases/metadata/listviewdefs.php');	
+
+
+require_once('include/SearchForm/SearchForm.php');
+
+global $app_strings;
+global $app_list_strings;
+global $current_language;
+$current_module_strings = return_module_language($current_language, 'Cases');
+
+global $urlPrefix;
+global $currentModule;
+
+global $theme;
+global $current_user;
+// focus_list is the means of passing data to a ListView.
+global $focus_list;
+
+// clear the display columns back to default when clear query is called
+if(!empty($_REQUEST['clear_query']) && $_REQUEST['clear_query'] == 'true')  
+    $current_user->setPreference('ListViewDisplayColumns', array(), 0, $currentModule);
+
+$savedDisplayColumns = $current_user->getPreference('ListViewDisplayColumns', $currentModule); // get user defined display columns
+
+$json = getJSONobj();
+
+$seedCase = new KBDcoument(); // seed bean
+$searchForm = new SearchForm('Cases', $seedCase); // new searchform instance
+
+// setup listview smarty
+$lv = new ListViewSmarty();
+
+if(!empty($_REQUEST['saved_search_select']) && $_REQUEST['saved_search_select']!='_none') {
+    if(empty($_REQUEST['fts_search']) && empty($_REQUEST['fts_search_ADV']) && (empty($_REQUEST['clear_query']) || $_REQUEST['clear_query']!='true')) {
+        $saved_search = loadBean('SavedSearch');
+        $saved_search->retrieveSavedSearch($_REQUEST['saved_search_select']);
+        $saved_search->populateRequest();
+    }
+    elseif(!empty($_REQUEST['fts_search']) || !empty($_REQUEST['fts_search_ADV'])) { // click the search button, after retrieving from saved_search
+        $_SESSION['LastSavedView'][$_REQUEST['module']] = '';
+        unset($_REQUEST['saved_search_select']);
+        unset($_REQUEST['saved_search_select_name']);
+    }
+}
+
+$displayColumns = array();
+// check $_REQUEST if new display columns from post
+if(!empty($_REQUEST['displayColumns'])) {
+    foreach(explode('|', $_REQUEST['displayColumns']) as $num => $col) {
+        if(!empty($listViewDefs['Cases'][$col])) 
+            $displayColumns[$col] = $listViewDefs['Cases'][$col];
+    }    
+}
+elseif(!empty($savedDisplayColumns)) { // use user defined display columns from preferences 
+    $displayColumns = $savedDisplayColumns;
+}
+else { // use columns defined in listviewdefs for default display columns
+    foreach($listViewDefs['Cases'] as $col => $params) {
+        if(!empty($params['default']) && $params['default'])
+            $displayColumns[$col] = $params;
+    }
+} 
+$params = array('massupdate' => true); // setup ListViewSmarty params
+if(!empty($_REQUEST['orderBy'])) { // order by coming from $_REQUEST
+    $params['orderBy'] = $_REQUEST['orderBy'];
+    $params['overrideOrder'] = true;
+    if(!empty($_REQUEST['sortOrder'])) $params['sortOrder'] = $_REQUEST['sortOrder'];
+}
+
+$lv->displayColumns = $displayColumns;
+
+if(!empty($_REQUEST['search_form_only']) && $_REQUEST['search_form_only']) { // handle ajax requests for search forms only
+    switch($_REQUEST['search_form_view']) {
+        case 'basic_search':
+            $searchForm->setup();
+            $searchForm->displayBasic(false);
+            break;
+        case 'advanced_search':
+            $searchForm->setup();
+            $searchForm->displayAdvanced(false);
+            break;
+        case 'saved_views':
+            echo $searchForm->displaySavedViews($listViewDefs, $lv, false);
+            break;
+    }
+    return;
+}
+
+// use the stored query if there is one
+if (!isset($where)) $where = "";
+require_once('modules/MySettings/StoreQuery.php');
+$storeQuery = new StoreQuery();
+if(!isset($_REQUEST['query'])){
+    $storeQuery->loadQuery($currentModule);
+    $storeQuery->populateRequest();
+}else{
+    $storeQuery->saveFromGet($currentModule);   
+}
+if(isset($_REQUEST['query']))
+{
+    // we have a query
+    // first save columns 
+    $current_user->setPreference('ListViewDisplayColumns', $displayColumns, 0, $currentModule); 
+    if(!empty($_SERVER['HTTP_REFERER']) && preg_match('/action=EditView/', $_SERVER['HTTP_REFERER'])) { // from EditView cancel
+        $searchForm->populateFromArray($storeQuery->query);
+    }
+    else {
+        $searchForm->populateFromRequest();
+    }
+    $where_clauses = $searchForm->generateSearchWhere(true, "Cases"); // builds the where clause from search field inputs
+
+    if (count($where_clauses) > 0 )$where = implode(' and ', $where_clauses);
+    $GLOBALS['log']->info("Here is the where clause for the list view: $where");
+}
+
+// start display
+// which tab of search form to display
+if(!isset($_REQUEST['search_form']) || $_REQUEST['search_form'] != 'false') {
+    $searchForm->setup();
+    if(isset($_REQUEST['searchFormTab']) && $_REQUEST['searchFormTab'] == 'advanced_search') {
+        $searchForm->displayAdvanced();
+    }
+    elseif(isset($_REQUEST['searchFormTab']) && $_REQUEST['searchFormTab'] == 'saved_views'){
+        $searchForm->displaySavedViews($listViewDefs, $lv);
+    }
+    else {
+        $searchForm->displayBasic();
+    }
+}
+
+$lv->setup($seedCase, 'include/ListView/ListViewGeneric.tpl', $where, $params);
+
+$savedSearchName = empty($_REQUEST['saved_search_select_name']) ? '' : (' - ' . $_REQUEST['saved_search_select_name']);
+echo get_form_header($current_module_strings['LBL_LIST_FORM_TITLE'] . $savedSearchName, '', false);
+echo $lv->display();
+
+$savedSearch = new SavedSearch();
+$json = getJSONobj();
+// fills in saved views select box on shortcut menu
+$savedSearchSelects = $json->encode(array($GLOBALS['app_strings']['LBL_SAVED_SEARCH_SHORTCUT'] . '<br>' . $savedSearch->getSelect('KBDocuments')));
+$str = "<script>
+YAHOO.util.Event.addListener(window, 'load', SUGAR.util.fillShortcuts, $savedSearchSelects);
+</script>";
+echo $str;
+?>

commit 00280515b8ace178aadee9c535e5178ca619ff79

Fixed some php errors/notices introduced by dd228783d91c57b59556d168e3c11b212333cee6

sugarcrm/include/ListView/ListView.php
@@ -1659,7 +1659,7 @@ function getUserVariable($localVarName, $varName) {
         }
         $imgFileParts = pathinfo(SugarThemeRegistry::current()->getImageURL("arrow{$upDown}.gif"));
 
-        list($width,$height) = ListView::getArrowUpDownImageSize();
+        list($width,$height) = ListView::getArrowUpDownImageSize($upDown);
         return " width='$width' height='$height' align='absmiddle' alt=".translate('LBL_SORT').">";
     }
 

sugarcrm/include/generic/SugarWidgets/SugarWidgetField.php
@@ -97,7 +97,8 @@ class SugarWidgetField extends SugarWidget {
 		$this->local_current_module = $_REQUEST['module'];
 		$this->is_dynamic = true;
 		// don't show sort links if name isn't defined
-		if (empty ($layout_def['name']) || (isset ($layout_def['sortable']) && !$layout_def['sortable']) ) {
+		if ((empty ($layout_def['name']) || (isset ($layout_def['sortable']) && !$layout_def['sortable']))
+        && !empty ($layout_def['label'])) {
 			return $layout_def['label'];
 		}
 		if (isset ($layout_def['sortable']) && !$layout_def['sortable']) {