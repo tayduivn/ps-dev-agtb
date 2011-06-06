77a9d40612d4e7c1826d656b03ff63b2e6dcae24
David Wheeler <dwheeler@sugarcrm.com>
2011-06-06 14:45:11 -0700

    Bug 44333: Fixed issue where beans that have just been saved cannot have one2many bean based relationships updated in the save php stack.

--- sugarcrm/data/SugarBean.php
+++ sugarcrm/data/SugarBean.php
@@ -1375,7 +1375,7 @@ class SugarBean
                 $saved_beans[$this->module_name] = array();
         if (empty($saved_beans[$this->module_name][$this->id]))
         {
-            $saved_beans[$this->module_name][$this->id] = true;
+            $saved_beans[$this->module_name][$this->id] = 'in_progress';
         }
         //BEGIN SUGARCRM flav=pro ONLY
         // if the module has a team_id field and no team_id is specified, set team_id as the current_user's default team
@@ -1694,6 +1694,7 @@ class SugarBean
 
         //Now that the record has been saved, we don't want to insert again on further saves
         $this->new_with_id = false;
+        $saved_beans[$this->module_name][$this->id] = 'done';
         return $this->id;
     }
 
