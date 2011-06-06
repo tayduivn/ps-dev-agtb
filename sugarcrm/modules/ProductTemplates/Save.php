decc728a48e43e3485e475656946896ccc3162ef
David Wheeler <dwheeler@sugarcrm.com>
2011-06-05 19:37:03 -0700

    Fixed various AjaxUI bugs around redirects and subpanels

--- sugarcrm/modules/ProductTemplates/Save.php
+++ sugarcrm/modules/ProductTemplates/Save.php
@@ -57,5 +57,6 @@ if(isset($_REQUEST['return_id']) && $_REQUEST['return_id'] != "") $return_id = $
 
 $GLOBALS['log']->debug("Saved record with id of ".$return_id);
 
-header("Location: index.php?action=$return_action&module=$return_module&record=$return_id");
+handleRedirect($return_id, $return_module);
+//header("Location: index.php?action=$return_action&module=$return_module&record=$return_id");
 ?>
