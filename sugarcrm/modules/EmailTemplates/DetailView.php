e2ffa5e05512341be2319dfc4a321fbe6320fbe3
Artem Vysotsky <artem.vysotsky@gmail.com>
2011-06-07 17:25:27 +0300

    Bug 36797: "Email Templates dont obey roles"

--- sugarcrm/modules/EmailTemplates/DetailView.php
+++ sugarcrm/modules/EmailTemplates/DetailView.php
@@ -110,11 +110,11 @@ $xtpl->assign("DATE_ENTERED", $focus->date_entered);
 $xtpl->assign("ASSIGNED_USER_NAME", $focus->assigned_user_name);
 
 
-if(ACLController::checkAccess('EmailTemplates', 'edit', true)) {
+if($focus->ACLAccess('EditView')) {
 	$xtpl->parse("main.edit");
 	//$xtpl->out("EDIT");
-		
-} 
+
+}
 //BEGIN SUGARCRM flav=pro ONLY
 
 require_once('modules/Teams/TeamSetManager.php');
