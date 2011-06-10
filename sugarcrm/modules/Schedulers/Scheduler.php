3b5868aaf82190678783140546784626c1550399
John Howells <jhowells@sugarcrm.com>
2011-06-10 14:02:25 -0700

    bug 42456: removed text referring to running cron through the web

--- sugarcrm/modules/Schedulers/Scheduler.php
+++ sugarcrm/modules/Schedulers/Scheduler.php
@@ -806,13 +806,13 @@ class Scheduler extends SugarBean {
         if(is_windows()) {
 			if(isset($_SERVER['Path']) && !empty($_SERVER['Path'])) { // IIS IUSR_xxx may not have access to Path or it is not set
 				if(!strpos($_SERVER['Path'], 'php')) {
-					$error = '<em>'.$mod_strings['LBL_NO_PHP_CLI'].'</em>';
+//					$error = '<em>'.$mod_strings['LBL_NO_PHP_CLI'].'</em>';
 				}
 			}
 		} else {
 			if(isset($_SERVER['Path']) && !empty($_SERVER['Path'])) { // some Linux servers do not make this available
 				if(!strpos($_SERVER['PATH'], 'php')) {
-					$error = '<em>'.$mod_strings['LBL_NO_PHP_CLI'].'</em>';
+//					$error = '<em>'.$mod_strings['LBL_NO_PHP_CLI'].'</em>';
 				}
 			}
 		}
