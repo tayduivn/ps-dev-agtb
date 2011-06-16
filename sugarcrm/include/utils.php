f73536f3fdb54e118d7c7bc48b111a055aa67a7c
Collin Lee <clee@Collin-Lee-MacBook-Pro.local>
2011-06-16 18:59:19 -0400

    Added int tags back around _pp function.

--- sugarcrm/include/utils.php
+++ sugarcrm/include/utils.php
@@ -2790,6 +2790,7 @@ function _ppf($bean, $die=false) {
  */
 function _pp($mixed)
 {
+	//BEGIN SUGARCRM flav=int ONLY
 	echo "\n<pre>\n";
 	print_r($mixed);
 
@@ -2799,6 +2800,7 @@ function _pp($mixed)
 		echo "\n\n _pp caller, file: " . $stack[0]['file']. ' line#: ' .$stack[0]['line'];
 	}
 	echo "\n</pre>\n";
+	//END SUGARCRM flav=int ONLY
 }
 
 /**
