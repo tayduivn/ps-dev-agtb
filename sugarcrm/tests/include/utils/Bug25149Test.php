e1faf5b9908d33d7026ab2421260215319661826
Frank Steegmans <fsteegmans@Orval.local>
2011-06-16 12:02:31 -0700

    stack59 prebuild fixes: Fixing MySQL specific constructs as MS SQL chokes on them.(cherry picked from commit d5b18fabca8d7f887b9db9ec931916896dbd0be0)

---
+++ sugarcrm/tests/include/utils/Bug25149Test.php
@@ -0,0 +1,27 @@
+<?php
+require_once 'modules/Products/Product.php';
+
+/**
+ * @group 25149
+ */
+class Bug25149Test extends Sugar_PHPUnit_Framework_TestCase
+{
+    public function testExportAllProductFields()
+    {
+        $product = new Product();
+
+        $order_by = array();
+        $where = '';
+        $query = $product->create_export_query($order_by, $where);
+
+        $db = DBManagerFactory::getInstance();
+        $result = $db->limitQuery($query, 1, 1, true, '');
+        $export_fields = $db->getFieldsArray($result, true);
+
+        $query = 'SELECT * FROM '.$product->table_name;
+        $result = $db->limitQuery($query, 1, 1, true, '');
+        $table_fields = $db->getFieldsArray($result, true);
+
+        $this->assertGreaterThanOrEqual(count($table_fields), count($export_fields));
+    }
+}
