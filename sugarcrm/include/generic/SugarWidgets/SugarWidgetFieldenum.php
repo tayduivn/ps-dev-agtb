e35c195e3cdaab7e561758160884b722e2444614
Stanislav Malyshev <smalyshev@gmail.com>
2011-07-01 15:13:25 -0700

    fix filename

--- sugarcrm/include/generic/SugarWidgets/SugarWidgetFieldenum.php
+++ sugarcrm/include/generic/SugarWidgets/SugarWidgetFieldenum.php
@@ -19,7 +19,7 @@ if(!defined('sugarEntry') || !sugarEntry) die('Not A Valid Entry Point');
  *to the License for the specific language governing these rights and limitations under the License.
  *Portions created by SugarCRM are Copyright (C) 2004 SugarCRM, Inc.; All Rights Reserved.
  ********************************************************************************/
-require_once('include/generic/SugarWidgets/SugarWidgetFieldVarchar.php');
+require_once('include/generic/SugarWidgets/SugarWidgetFieldvarchar.php');
 
 class SugarWidgetFieldEnum extends SugarWidgetFieldVarchar
 {
