933e1cb228fd3394170abd50b85c0b48f400f754
Stanislav Malyshev <smalyshev@gmail.com>
2011-07-05 21:28:53 -0700

    fix 43395 test for CE

--- sugarcrm/include/SugarObjects/templates/person/Person.php
+++ sugarcrm/include/SugarObjects/templates/person/Person.php
@@ -31,17 +31,17 @@ require_once('include/SugarObjects/templates/basic/Basic.php');
 
 
 class Person extends Basic
-{	
+{
     var $picture;
-    
+
     //Variable to control whether or not to invoke the getLocalFormatttedName method with title and salutation
     var $createLocaleFormattedName = true;
-    
+
 	function Person(){
 		parent::Basic();
 		$this->emailAddress = new SugarEmailAddress();
 	}
-	
+
 	// need to override to have a name field created for this class
 	function retrieve($id = -1, $encode=true) {
 		$ret_val = parent::retrieve($id, $encode);
@@ -49,35 +49,38 @@ class Person extends Basic
 		$this->emailAddress->handleLegacyRetrieve($this);
 		return $ret_val;
 	}
-	
+
 	/**
 	 * Generate the name field from the first_name and last_name fields.
 	 */
-	function _create_proper_name_field() 
+	function _create_proper_name_field()
 	{
 		global $locale, $app_list_strings;
 		//BEGIN SUGARCRM flav=pro ONLY
 		if(isset($GLOBALS['current_user']->id) && $this->bean_implements('ACL') && !ACLField::hasAccess('first_name', $this->module_dir, $GLOBALS['current_user']->id, $this->isOwner($GLOBALS['current_user']->id))){
 			$full_name = $this->last_name;
-		} else if($this->createLocaleFormattedName) {
-		//END SUGARCRM flav=pro ONLY
-		    // Bug 38648 - If the given saluation doesn't exist in the dropdown, don't display it as part of the full name
-		    $salutation = '';
-		    if(isset($this->field_defs['salutation']['options']) 
-		            && isset($app_list_strings[$this->field_defs['salutation']['options']])
-		            && isset($app_list_strings[$this->field_defs['salutation']['options']][$this->salutation]) ) {
-		        $salutation = $app_list_strings[$this->field_defs['salutation']['options']][$this->salutation];
-		    }
-			$full_name = $locale->getLocaleFormattedName($this->first_name, $this->last_name, $salutation, $this->title);
-		//BEGIN SUGARCRM flav=pro ONLY
-		} else {
-			$full_name = $locale->getLocaleFormattedName($this->first_name, $this->last_name);
-		}
-		//END SUGARCRM flav=pro ONLY
-		$this->name = $full_name;
-		$this->full_name = $full_name; //used by campaigns
-	}
-	
+        } else {
+        //END SUGARCRM flav=pro ONLY
+            if ($this->createLocaleFormattedName) {
+                // Bug 38648 - If the given saluation doesn't exist in the dropdown, don't display it as part of the full name
+                $salutation = '';
+                if (isset($this->field_defs['salutation']['options']) &&
+                     isset($app_list_strings[$this->field_defs['salutation']['options']]) &&
+                     isset($app_list_strings[$this->field_defs['salutation']['options']][$this->salutation])) {
+                        $salutation = $app_list_strings[$this->field_defs['salutation']['options']][$this->salutation];
+                }
+                $full_name = $locale->getLocaleFormattedName($this->first_name, $this->last_name, $salutation, $this->title);
+            } else {
+                $full_name = $locale->getLocaleFormattedName($this->first_name, $this->last_name);
+            }
+
+         //BEGIN SUGARCRM flav=pro ONLY
+         }
+         //END SUGARCRM flav=pro ONLY
+        $this->name = $full_name;
+        $this->full_name = $full_name; //used by campaigns
+    }
+
 	function save($check_notify=false) {
 		$this->add_address_streets('primary_address_street');
 		$this->add_address_streets('alt_address_street');
@@ -99,14 +102,14 @@ class Person extends Basic
         }
 		return $this->id;
 	}
-	
+
 	function get_summary_text() {
 		$this->_create_proper_name_field();
         return $this->name;
 	}
-	
+
 	function get_list_view_data() {
-		
+
 		global $system_config;
 		global $current_user;
 		$this->_create_proper_name_field();
@@ -117,7 +120,7 @@ class Person extends Basic
 		$temp_array['EMAIL1_LINK'] = $current_user->getEmailLink('email1', $this, '', '', 'ListView');
 		return $temp_array;
 	}
-    
+
     /**
      * @see SugarBean::populateRelatedBean()
  	 */
@@ -126,7 +129,7 @@ class Person extends Basic
         )
     {
         parent::populateRelatedBean($newbean);
-        
+
         if ( $newbean instanceOf Company ) {
             $newbean->phone_fax = $this->phone_fax;
             $newbean->phone_office = $this->phone_work;
