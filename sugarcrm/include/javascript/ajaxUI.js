e0b43e31e38b371715555f984c8197fbea57271a
David Wheeler <dwheeler@sugarcrm.com>
2011-07-12 15:51:08 -0700

    Bug 45159: Fixed bug where studio would no longer load after previous related commit.

--- sugarcrm/include/javascript/ajaxUI.js
+++ sugarcrm/include/javascript/ajaxUI.js
@@ -173,13 +173,18 @@ SUGAR.ajaxUI = {
         var url = YAHOO.util.History.getBookmarkedState('ajaxUILoc');
         var aRegex = /action=([^&]*)/.exec(window.location);
         var action = aRegex ? aRegex[1] : false;
-        var go = url != null || action == "ajaxui";
-        url = url ? url : 'index.php?module=Home&action=index';
-        YAHOO.util.History.register('ajaxUILoc', url, SUGAR.ajaxUI.go);
-        YAHOO.util.History.initialize("ajaxUI-history-field", "ajaxUI-history-iframe");
-        SUGAR.ajaxUI.hist_loaded = true;
-        if (go)
-            SUGAR.ajaxUI.go(url);
+        var mRegex = /module=([^&]*)/.exec(window.location);
+        var module = mRegex ? mRegex[1] : false;
+        if (module != "ModuleBuilder")
+        {
+            var go = url != null || action == "ajaxui";
+            url = url ? url : 'index.php?module=Home&action=index';
+            YAHOO.util.History.register('ajaxUILoc', url, SUGAR.ajaxUI.go);
+            YAHOO.util.History.initialize("ajaxUI-history-field", "ajaxUI-history-iframe");
+            SUGAR.ajaxUI.hist_loaded = true;
+            if (go)
+                SUGAR.ajaxUI.go(url);
+        }
         SUGAR_callsInProgress--;
     },
     print: function()
