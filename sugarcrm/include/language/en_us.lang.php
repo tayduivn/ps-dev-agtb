e7b430c77df805e74558bd31bd3e0732310928e4
sugared <eddy@sugarcrm.com>
2011-07-14 02:47:52 -0700

    BUG -44391 updated labels and ordering for export.  Also added support for ordering in custom modules

--- sugarcrm/include/language/en_us.lang.php
+++ sugarcrm/include/language/en_us.lang.php
@@ -3110,6 +3110,9 @@ $app_strings = array (
     'LBL_EXPORT_ASSIGNED_USER_NAME' => 'Assigned User',
     'LBL_EXPORT_REPORTS_TO_ID' => 'Reports To',
     'LBL_EXPORT_FULL_NAME' => 'Full Name',
+    'LBL_EXPORT_TEAM_ID' => 'Team ID',
+    'LBL_EXPORT_TEAM_NAME' => 'Teams',
+    'LBL_EXPORT_TEAM_SET_ID' => 'Team Set ID',
 
 
     );
