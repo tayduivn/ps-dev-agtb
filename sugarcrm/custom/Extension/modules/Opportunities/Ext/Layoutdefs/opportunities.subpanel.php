ecd4bfcb86ad36fc346a6f1be65973b0fdc3e4df
Aamir Mansoor <amansoor@sugarcrm.com>
2011-07-18 17:28:07 -0700

    Merged with correct branch

---
+++ sugarcrm/custom/Extension/modules/Opportunities/Ext/Layoutdefs/opportunities.subpanel.php
@@ -0,0 +1,14 @@
+<?php
+$layout_defs['Opportunities']['subpanel_setup']['contact_history'] =  array(
+        			'order' => 20,
+		        	'sort_order' => 'desc',
+        			'sort_by' => 'date_entered',
+        			'title_key' => 'LBL_CONTACT_HISTORY_SUBPANEL_TITLE',
+					'module' => 'Emails',
+	                'subpanel_name' => 'ForContactHistory',
+	                'get_subpanel_data' => 'function:get_unlinked_email_query_via_link',
+	    		    'function_parameters' => array('import_function_file' => 'modules/SNIP/utils.php', 'link' => 'contacts'),
+	                'generate_select'=>true,
+				    'get_distinct_data' => true,
+                    'top_buttons' => array(),
+);
