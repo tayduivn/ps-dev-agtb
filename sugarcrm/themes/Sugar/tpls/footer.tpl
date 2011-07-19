tayduivn/ps-dev-agtb

commit 173fcea03e7f5c12a809ff9559b0e5df0d50f74c

second round of checkins for quickedits.  Fixed various quirks

sugarcrm/themes/Sugar/tpls/footer.tpl
@@ -59,9 +59,51 @@ if (SUGAR.ajaxUI && !SUGAR.ajaxUI.hist_loaded)
     YAHOO.util.History.initialize("ajaxUI-history-field", "ajaxUI-history-iframe");
     {/if}{literal}
 }
+
+//qe_init function sets listeners to click event on elements of 'quickEdit' class
+DCMenu.qe_refresh = false;
+DCMenu.qe_handle;
+function qe_init(){
+    //do not process if YUI is undefined
+    if(typeof(YUI)=='undefined'){
+        return;
+    }
+
+
+    //remove all existing listeners.  This will prevent adding multiple listeners per element and firing multiple events per click
+    if(typeof(DCMenu.qe_handle) !='undefined'){
+        DCMenu.qe_handle.detach();
+    }
+
+    //set listeners on click event, and define function to call
+    YUI().use('node', function(Y) {
+        var qe = Y.all('.quickEdit');
+        var refreshDashletID;
+        var refreshListID;
+
+        //store event listener handle for future use, and define function to call on click event
+        DCMenu.qe_handle = qe.on('click', function(e) {
+            //function will flash message, and retrieve data from element to pass on to DC.miniEditView function
+            ajaxStatus.flashStatus(SUGAR.language.get('app_strings', 'LBL_LOADING'),800);
+            e.preventDefault();
+            if(typeof(e.currentTarget.getAttribute('data-dashlet-id'))!='undefined'){
+                refreshDashletID = e.currentTarget.getAttribute('data-dashlet-id');
+            }
+            if(typeof(e.currentTarget.getAttribute('data-list'))!='undefined'){
+                refreshListID = e.currentTarget.getAttribute('data-list');
+            }
+            DCMenu.miniEditView(e.currentTarget.getAttribute('data-module'), e.currentTarget.getAttribute('data-record'),refreshListID,refreshDashletID);
+        });
+
+    });
+}
+
+    qe_init();
+
 {/literal}
 
 </script>
 
 </body>
 </html>
+

sugarcrm/include/DashletContainer/Containers/DCMenu.js
@@ -255,13 +255,18 @@ var DCMenu = YUI({combine: true, timeout: 10000, base:"include/javascript/yui3/b
 	DCMenu.miniDetailView = function(module, id){
 		quickRequest('spot', 'index.php?to_pdf=1&module=' + module + '&action=quick&record=' + id , miniDetailViewResults);
 	}
+    //this form is used by quickEdits to provide a modal edit form
 	DCMenu.miniEditView = function(module, id, refreshListID, refreshDashletID){
+        //use pased in values to determine if this is being fired from a dashlet or list
+        //populate the qe_refresh variable with the correct refresh string to execute on DCMenu.save
         if(typeof(refreshListID) !='undefined' && refreshListID !=''){
-            qe_refresh = 'SUGAR.ajaxUI.loadContent("index.php?module='+module+'&action=index&ignore='+new Date().getTime()+'");';
-            //qe_refresh = 'window.location.reload(false)';
+            //this is a list, so add a time stamp to url so ajaxUI detects a change and refreshes the screen
+            DCMenu.qe_refresh = 'SUGAR.ajaxUI.loadContent("index.php?module='+module+'&action=index&ignore='+new Date().getTime()+'");';
+
         }
         if(typeof(refreshDashletID) !='undefined' && refreshDashletID !=''){
-            qe_refresh = 'SUGAR.mySugar.retrieveDashlet("'+refreshDashletID+'");';
+            //this is a dashlet, use the passed in id to refresh the dashlet
+            DCMenu.qe_refresh = 'SUGAR.mySugar.retrieveDashlet("'+refreshDashletID+'");';
         }
 		quickRequest('spot', 'index.php?to_pdf=1&module=' + module + '&action=Quickedit&record=' + id , miniDetailViewResults);
 	}
@@ -301,7 +306,10 @@ var DCMenu = YUI({combine: true, timeout: 10000, base:"include/javascript/yui3/b
 		});
 		lastLoadedMenu=undefined;
 		DCMenu.closeOverlay();
-        setTimeout('qe_init();', 3000); //!!!! this approach is a hack, we need to figure out how to call this method after the ajax page loads
+        //if DCMenu.qe_refresh is set to a string, then eval it as it is a js reload command (either reloads dashlet or list view)
+        if(typeof(DCMenu.qe_refresh) =='string'){
+            eval(DCMenu.qe_refresh);
+        }
 		return false;
 	}
 

sugarcrm/include/Dashlets/DashletGenericDisplay.tpl
@@ -181,7 +181,3 @@
 	{/foreach}
 </table>
 
-<script>{literal}    if(typeof(qe_init) != 'undefined'){
-        qe_init(); //!! THIS IS A HACK!  need to find way to trigger off qe_init on last dashlet initial load
-                    // qe_init is defined in footer.tpl
-    }{/literal}</script>

sugarcrm/include/MySugar/javascript/MySugar.js
@@ -616,6 +616,12 @@ SUGAR.mySugar = function() {
 				//BEGIN SUGARCRM flav=pro || flav=sales ONLY
 				SUGAR.mySugar.attachToggleToolsetEvent(id);
 				//END SUGARCRM flav=pro || flav=sales ONLY
+
+                //we need to reinit the quickEdit Listeners whenever a dashlet is refreshed
+                if(typeof(qe_init) =='function'){
+                    //reinitialize the Quick Edit events
+                    qe_init();
+                }
 			}
 			
 			SUGAR.mySugar.currentDashlet = document.getElementById('dashlet_entire_' + id);

sugarcrm/include/generic/SugarWidgets/SugarWidgetSubPanelEditButton.php
@@ -56,7 +56,7 @@ class SugarWidgetSubPanelEditButton extends SugarWidgetField
 		$onclick .= "retValz = SUGAR.subpanelUtils.sendAndRetrieve('" . $formname
 			. "', 'subpanel_" . $layout_def['subpanel_id'] . "', '" . addslashes($app_strings['LBL_LOADING'])
 			. "', '" . $layout_def['subpanel_id'] . "');";
-		$onclick .= "document.forms['{$formname}'].record.value='';retValz";
+		$onclick .= "document.forms['{$formname}'].record.value='';retValz;return false;";
 
 		$href = "#";
 		/*if($layout_def['EditView']){

sugarcrm/modules/Home/index.php
@@ -728,5 +728,6 @@ $mySugarResources = $sugarChart->getMySugarChartResources();
 $sugar_smarty->assign('chartResources', $resources);
 $sugar_smarty->assign('mySugarChartResources', $mySugarResources);
 echo $sugar_smarty->fetch('include/MySugar/tpls/MySugar.tpl');
-
+//init the quickEdit listeners after the dashlets have loaded on home page the first time
+echo"<script>if(typeof(qe_init) != 'undefined'){qe_init();}</script>";
 ?>

commit b89321b29da6a920b3b6ab5038ac8e2f15bf2fbf

Fixed JS error on login page introduced by 173fcea03e7f5c12a809ff9559b0e5df0d50f74c

sugarcrm/themes/Sugar/tpls/footer.tpl
@@ -58,7 +58,7 @@ if(SUGAR.util.isTouchScreen()) {
 function qe_init(){
 
     //do not process if YUI is undefined
-    if(typeof(YUI)=='undefined'){
+    if(typeof(YUI)=='undefined' || typeof(DCMenu) == 'undefined'){
         return;
     }
 