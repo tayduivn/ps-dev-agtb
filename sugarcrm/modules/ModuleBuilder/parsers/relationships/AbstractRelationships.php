65f536bb427ff28d18f81e24ade953dd86ae6092
David Wheeler <dwheeler@sugarcrm.com>
2011-07-26 16:46:35 -0700

    Bug 45542: activities subpanels extensions were not being written correctly. Only the history subpanel was shown.

--- sugarcrm/modules/ModuleBuilder/parsers/relationships/AbstractRelationships.php
+++ sugarcrm/modules/ModuleBuilder/parsers/relationships/AbstractRelationships.php
@@ -420,17 +420,21 @@ class AbstractRelationships
         foreach ( $subpanelDefinitions as $moduleName => $definitions )
         {
             $filename = "$basepath/layoutdefs/{$relationshipName}_{$moduleName}.php" ;
-            $out =  "<?php\n// created: " . date('Y-m-d H:i:s') . "\n";
+            $subpanelVarname = 'layout_defs["' . $moduleName . '"]["subpanel_setup"]';
+            $out = "";
             foreach ( $definitions as $definition )
             {
                 $GLOBALS [ 'log' ]->debug ( get_class ( $this ) . "->saveSubpanelDefinitions(): saving the following to {$filename}" . print_r ( $definition, true ) ) ;
-            	if (empty($definition ['get_subpanel_data']) || $definition ['subpanel_name'] == 'history' ||  $definition ['subpanel_name'] == 'activities') {
-               		$definition ['get_subpanel_data'] = $definition ['subpanel_name'];
-               	}
-               	$out .= '$layout_defs["' . $moduleName . '"]["subpanel_setup"]["' . $definition ['get_subpanel_data'] . '"] = ' 
-               	      . var_export_helper($definition) . ";\n";
+                if (empty($definition ['get_subpanel_data']) || $definition ['subpanel_name'] == 'history' || $definition ['subpanel_name'] == 'activities') {
+                    $definition ['get_subpanel_data'] = $definition ['subpanel_name'];
+                }
+                $out .= override_value_to_string($subpanelVarname, strtolower ( $definition [ 'get_subpanel_data' ] ), $definition) . "\n";
             }
-            file_put_contents($filename, $out);
+            if (!empty($out)) {
+                $out = "<?php\n // created: " . date('Y-m-d H:i:s') . "\n" . $out;
+                sugar_file_put_contents($filename, $out);
+            }
+
             $installDefs [ $moduleName ] = array ( 'from' => "{$installDefPrefix}/relationships/layoutdefs/{$relationshipName}_{$moduleName}.php" , 'to_module' => $moduleName ) ;
         }
         return $installDefs ;
