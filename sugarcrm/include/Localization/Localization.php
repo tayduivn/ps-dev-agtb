83a95018a9b1723d8c7e8a303fc756e364af4780
poovaraghavan <poovaraghavan@angleritech.com>
2011-08-11 22:44:25 +0530

    Bug has been re-fixed

--- sugarcrm/include/Localization/Localization.php
+++ sugarcrm/include/Localization/Localization.php
@@ -156,15 +156,15 @@ class Localization {
 			return array();
 		}
 
-        // load default from config.php
-		$this->currencies['-99'] = array(
-			'name'		=> $sugar_config['default_currency_name'],
-			'symbol'	=> $sugar_config['default_currency_symbol'],
-			'conversion_rate' => 1
-			);
-
-		$load = sugar_cache_retrieve('currency_list');
+        $load = sugar_cache_retrieve('currency_list');
         if ( !is_array($load) ) {
+			// load default from config.php
+			$this->currencies['-99'] = array(
+				'name'		=> $sugar_config['default_currency_name'],
+				'symbol'	=> $sugar_config['default_currency_symbol'],
+				'conversion_rate' => 1
+				);
+
             $q = "SELECT id, name, symbol, conversion_rate FROM currencies WHERE status = 'Active' and deleted = 0";
             $r = $db->query($q);
 
