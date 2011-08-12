cc8ec73e3f4857e59a635ac6e46964ed82b16e21
Andreas Sandberg <asandberg@sugarcrm.com>
2011-08-11 17:19:16 -0700

    Bug#45963 - Import wizard showing modules that do not make sense

---
+++ sugarcrm/tests/modules/Import/Bug45963Test.php
@@ -0,0 +1,20 @@
+<?php
+
+require_once('modules/Import/Importer.php');
+
+class Bug39756Test extends Sugar_PHPUnit_Framework_TestCase
+{
+
+    /**
+     * @group bug45963
+     */
+    public function testGetImportableModules()
+    {
+        $modules = Importer::getImportableModules();
+
+        $this->assertEmpty($modules['Groups']);
+        $this->assertNotEmpty($modules['Contacts']);
+        $this->assertNotEmpty($modules['Accounts']);
+    }
+}
+
