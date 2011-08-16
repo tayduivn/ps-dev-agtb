05eb4433ced41b3b9619127d4704c9973e159117
Andreas Sandberg <asandberg@sugarcrm.com>
2011-08-15 22:11:16 -0700

    Bug#45816 - Add index to the campaign_log table

--- build/scripts_for_patch/61x_to_63x_mysql.sql
+++ build/scripts_for_patch/61x_to_63x_mysql.sql
@@ -9,3 +9,6 @@ DROP INDEX idx_folders_rel_folder_id on folders_rel;
 ALTER TABLE folders_rel ADD INDEX idx_fr_id_deleted_poly (folder_id, deleted, polymorphic_id);
 
 ALTER TABLE bugs ADD INDEX idx_bugs_assigned_user (assigned_user_id);
+
+DROP INDEX idx_target_id on campaign_log;
+ALTER TABLE campaign_log ADD INDEX idx_target_deleted (target_id, deleted);
--- build/scripts_for_patch/61x_to_63x_oracle.sql
+++ build/scripts_for_patch/61x_to_63x_oracle.sql
@@ -6,3 +6,6 @@ ALTER TABLE tasks MODIFY status DEFAULT 'Not Planned';
 DROP INDEX idx_folders_rel_folder_id;
 create index idx_fr_id_deleted_poly on folders_rel (folder_id, deleted, polymorphic_id);
 create index idx_bugs_assigned_user on bugs (assigned_user_id);
+
+DROP INDEX idx_target_id;
+create index idx_target_deleted on campaign_log (target_id, deleted);
