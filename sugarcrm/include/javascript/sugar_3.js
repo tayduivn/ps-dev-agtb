112e7acf3d9b04cfb7e16ddeb658579fb2fd0252
Collin Lee <clee@Collin-Lee.local>
2011-08-23 12:52:45 -0400

    Bug: 46201
    Fix issue with the backgroundColor css attribute not fading back to original color for IE.

--- sugarcrm/include/javascript/sugar_3.js
+++ sugarcrm/include/javascript/sugar_3.js
@@ -641,7 +641,7 @@ function add_error_style(formname, input, txt, flash) {
 	            }
 	        }
 		}
-		window.setTimeout("inputsWithErrors[" + (inputsWithErrors.length - 1) + "].style.backgroundColor = null;", 2000);
+		window.setTimeout("inputsWithErrors[" + (inputsWithErrors.length - 1) + "].style.backgroundColor = '';", 2000);
     }
 
   } catch ( e ) {
