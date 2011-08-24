2dd8f348994a63a4f68ca0c2a1493f115a682b32
Andreas Sandberg <asandberg@sugarcrm.com>
2011-08-23 17:56:41 -0700

    Bug#46288 - Some scripts need to excute after upgrade duirng the repair

--- build/scripts_for_patch/62x_to_63x_mssql.sql
+++ build/scripts_for_patch/62x_to_63x_mssql.sql
@@ -3,6 +3,6 @@ CREATE NONCLUSTERED INDEX idx_fr_id_deleted_poly on folders_rel (folder_id, dele
 
 CREATE NONCLUSTERED INDEX idx_bugs_assigned_user on bugs (assigned_user_id);
 
-CREATE NONCLUSTERED INDEX idx_target_deleted on campaign_log (target_id, deleted);
+CREATE NONCLUSTERED INDEX idx_target_id_deleted on campaign_log (target_id, deleted);
 
 
--- build/scripts_for_patch/62x_to_63x_oracle.sql
+++ build/scripts_for_patch/62x_to_63x_oracle.sql
@@ -5,4 +5,4 @@ create index idx_fr_id_deleted_poly on folders_rel (folder_id, deleted, polymorp
 
 create index idx_bugs_assigned_user on bugs (assigned_user_id);
 
-create index idx_target_deleted on campaign_log (target_id, deleted);
+create index idx_target_id_deleted on campaign_log (target_id, deleted);
