19b920e60e221e335d2b4605d1c75562c3972285
Stanislav Malyshev <smalyshev@gmail.com>
2011-09-07 17:54:27 -0700

    fix underfined variable

--- sugarcrm/modules/Emails/Save.php
+++ sugarcrm/modules/Emails/Save.php
@@ -1,18 +1,31 @@
 <?php
 if(!defined('sugarEntry') || !sugarEntry) die('Not A Valid Entry Point');
 /*********************************************************************************
- * The contents of this file are subject to
- * *******************************************************************************/
-/*********************************************************************************
- * $Id: Save.php 45763 2009-04-01 19:16:18Z majed $
- * Description:
- * Portions created by SugarCRM are Copyright (C) SugarCRM, Inc. All Rights
- * Reserved. Contributor(s): ______________________________________..
- *********************************************************************************/
-
-
-
-
+ * The contents of this file are subject to the SugarCRM Master Subscription
+ * Agreement ("License") which can be viewed at
+ * http://www.sugarcrm.com/crm/en/msa/master_subscription_agreement_11_April_2011.pdf
+ * By installing or using this file, You have unconditionally agreed to the
+ * terms and conditions of the License, and You may not use this file except in
+ * compliance with the License.  Under the terms of the license, You shall not,
+ * among other things: 1) sublicense, resell, rent, lease, redistribute, assign
+ * or otherwise transfer Your rights to the Software, and 2) use the Software
+ * for timesharing or service bureau purposes such as hosting the Software for
+ * commercial gain and/or for the benefit of a third party.  Use of the Software
+ * may be subject to applicable fees and any use of the Software without first
+ * paying applicable fees is strictly prohibited.  You do not have the right to
+ * remove SugarCRM copyrights from the source code or user interface.
+ *
+ * All copies of the Covered Code must include on each user interface screen:
+ *  (i) the "Powered by SugarCRM" logo and
+ *  (ii) the SugarCRM copyright notice
+ * in the same form as they appear in the distribution.  See full license for
+ * requirements.
+ *
+ * Your Warranty, Limitations of liability and Indemnity are expressly stated
+ * in the License.  Please refer to the License for the specific language
+ * governing these rights and limitations under the License.  Portions created
+ * by SugarCRM are Copyright (C) 2004-2011 SugarCRM, Inc.; All Rights Reserved.
+ ********************************************************************************/
 
 ///////////////////////////////////////////////////////////////////////////////
 ////	EMAIL SEND/SAVE SETUP
@@ -176,6 +189,11 @@ $focus->db->query($query);
 // delete al the relationship of this email with all the beans
 //$query = "update emails_beans set deleted = 1, bean_id = '', bean_module = '' WHERE email_id = '{$focus->id}'";
 //$focus->db->query($query);
+if(!empty($_REQUEST['to_addrs_ids'])) {
+    $exContactIds = explode(';', $_REQUEST['to_addrs_ids']);
+} else {
+    $exContactIds = array();
+}
 
 if(isset($_REQUEST['object_type']) && !empty($_REQUEST['object_type']) && isset($_REQUEST['object_id']) && !empty($_REQUEST['object_id'])) {
     //run linking code only if the object_id has not been linked as part of the contacts above and it is an OOB relationship
@@ -212,9 +230,8 @@ $focus->save(false);
 $focus->load_relationship('users');
 $focus->users->add($current_user->id);
 
-if(!empty($_REQUEST['to_addrs_ids'])) {
+if(!empty($exContactIds)) {
 	$focus->load_relationship('contacts');
-	$exContactIds = explode(';', $_REQUEST['to_addrs_ids']);
 	foreach($exContactIds as $contactId) {
 		$contactId = trim($contactId);
 		$focus->contacts->add($contactId);
