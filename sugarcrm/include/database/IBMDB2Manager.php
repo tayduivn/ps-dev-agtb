23ce8ff3d26ce667b37e3b672d40701283e7f675
Stanislav Malyshev <smalyshev@gmail.com>
2011-09-15 14:59:18 -0700

    type_class the same as parent, no need to override

--- sugarcrm/include/database/IBMDB2Manager.php
+++ sugarcrm/include/database/IBMDB2Manager.php
@@ -100,29 +100,6 @@ class IBMDB2Manager  extends DBManager
 
 	);
 
-	/**~
-	 * @var array
-	 */
-	// TODO: Note sure if the $type_class is required, just keeping it for consistency with the MySQL and Oracle implementations
-	protected $type_class = array(
-			'int'      => 'int',
-			'double'   => 'float',
-			'float'    => 'float',
-			'uint'     => 'int',
-			'ulong'    => 'int',
-			'long'     => 'int',
-			'short'    => 'int',
-			'date'     => 'date',
-			'datetime' => 'date',
-			'datetimecombo' => 'date',
-			'time'     => 'time',
-			'bool'     => 'bool',
-			'tinyint'  => 'int',
-			'currency' => 'float',
-			'decimal'  => 'float',
-			'decimal2' => 'float',
-	);
-
 	/**+
 	 * @var array
 	 */
