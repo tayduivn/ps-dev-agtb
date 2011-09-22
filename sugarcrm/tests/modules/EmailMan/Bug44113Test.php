14a3f375db3b6a7b02407c7c45cb1226b38c444c
Collin Lee <clee@Collin-Lee.local>
2011-09-22 17:32:38 -0400

    Added setUp/tearDown support so that original email_xss string is preserved after test completes.

--- sugarcrm/tests/modules/EmailMan/Bug44113Test.php
+++ sugarcrm/tests/modules/EmailMan/Bug44113Test.php
@@ -34,13 +34,33 @@ class Bug44113Test extends Sugar_PHPUnit_Framework_TestCase
 	private $cfg;   // configurator
 	private $emailMan;
     private $email_xss; // the security settings to be saved in config_ovverride
-    
+    private $original_email_xss = null;
+
 	public function setUp()
 	{
         $GLOBALS['current_user'] = SugarTestUserUtilities::createAnonymousUser();
         $GLOBALS['current_user']->is_admin = '1';
 
-          // email_xss settings to be saved using config_override
+
+        global $sugar_config;
+        if(isset($sugar_config['email_xss']))
+        {
+              $this->_original_email_xss = $sugar_config['email_xss'];
+        }
+
+        /*
+        if(file_exists('config_override.php') && isset($this->original_email_xss))
+        {
+              require("config_override.php");
+              $this->cfg = new Configurator();
+              if(isset($this->cfg->config['email_xss']))
+              {
+                  $this->cfg->config['email_xss'] = $this->original_email_xss;
+                  $this->cfg->handleOverride();
+              }
+        }
+        */
+        // email_xss settings to be saved using config_override
         $this->email_xss = array(
             //'applet' => 'applet',
             'form' => 'form',
@@ -52,6 +72,14 @@ class Bug44113Test extends Sugar_PHPUnit_Framework_TestCase
 	
 	public function tearDown()
 	{
+
+        if(isset($this->original_email_xss))
+        {
+           $this->cfg = new Configurator();
+           $this->cfg->config['email_xss'] = $this->original_email_xss;
+           $this->cfg->handleOverride();
+        }
+        
 		unset($this->cfg);
         unset($this->emailMan);
         unset($this->email_xss);
@@ -75,8 +103,6 @@ class Bug44113Test extends Sugar_PHPUnit_Framework_TestCase
       }
 
       $new_security_settings = base64_encode(serialize($this->email_xss));
-
-
 
       // make sure that settings from config.php are untouched
       require("config.php");
@@ -102,8 +128,8 @@ class Bug44113Test extends Sugar_PHPUnit_Framework_TestCase
      */
 	public function testSavingToConfigOverride()
 	{
-        $this->cfg = new Configurator();
-        global $sugar_config;
+       $this->cfg = new Configurator();
+       global $sugar_config;
 
        $new_security_settings = base64_encode(serialize($this->email_xss));
 
@@ -111,10 +137,7 @@ class Bug44113Test extends Sugar_PHPUnit_Framework_TestCase
        $this->cfg->handleOverride();
 
        // just test to make sure that configuration is saved
-       $this->assertEquals($sugar_config['email_xss'], $new_security_settings,
-                         "testing configurator");
-
-
+       $this->assertEquals($sugar_config['email_xss'], $new_security_settings, "testing configurator");
     }
 
 }
