7c76398aafa965458af1dc59448618b4784a0f40
Kanstantsin Shpikat <k.shpikat@intetics.com>
2011-09-23 18:17:36 +0300

    Bug 46400: Accounts->History->View Summary needs close button

--- sugarcrm/modules/Activities/Popup_picker.php
+++ sugarcrm/modules/Activities/Popup_picker.php
@@ -350,6 +350,7 @@ class Popup_Picker
         echo getClassicModuleTitle($focus->module_dir, array(translate('LBL_MODULE_NAME', $focus->module_dir),$focus->name), false);
         echo "</td><td align='right' class='moduleTitle'>";
         echo "<A href='javascript:print();' class='utilsLink'><img src='".SugarThemeRegistry::current()->getImageURL("print.gif")."' width='13' height='13' alt='".$app_strings['LNK_PRINT']."' border='0' align='absmiddle'></a>&nbsp;<A href='javascript:print();' class='utilsLink'>".$app_strings['LNK_PRINT']."</A>\n";
+        echo "<A href='javascript:window.close();' id='hiddenCloseLinkForSoda'></A>\n";
         echo "</td></tr></table>";
 
         $oddRow = true;
