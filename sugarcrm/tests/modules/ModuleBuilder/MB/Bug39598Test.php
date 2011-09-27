49eb2f0d2d33025c2adb8291fc4c595207582769
Stanislav Malyshev <smalyshev@gmail.com>
2011-09-27 16:38:47 -0700

    fix unit tests

--- sugarcrm/tests/modules/ModuleBuilder/MB/Bug39598Test.php
+++ sugarcrm/tests/modules/ModuleBuilder/MB/Bug39598Test.php
@@ -30,9 +30,9 @@ class Bug39598Test extends Sugar_PHPUnit_Framework_TestCase
 
     public function setUp()
     {
-        $this->tmpDir = sys_get_temp_dir() . DIRECTORY_SEPARATOR . 'bug32821';
+        $this->tmpDir = sys_get_temp_dir() . DIRECTORY_SEPARATOR . 'bug39598';
         mkdir($this->tmpDir);
-        $this->tmpFile = tempnam($this->tmpDir, 'bug32821');
+        $this->tmpFile = tempnam($this->tmpDir, 'bug39598');
         //rename file to 'relationships.php'
         rename($this->tmpFile, $this->tmpDir . DIRECTORY_SEPARATOR . 'relationships.php');
         $this->tmpFile = $this->tmpDir . DIRECTORY_SEPARATOR . 'relationships.php';
@@ -51,6 +51,6 @@ class Bug39598Test extends Sugar_PHPUnit_Framework_TestCase
         $mbModule->renameMetaData($this->tmpDir, 'test_oldname');
         $replacedContents = file_get_contents($this->tmpFile);
         $this->assertEquals('[\'test_newname\'] => ', $replacedContents, 'Module name replaced correctly in relationships metadata');
-        
+
     }
 }
