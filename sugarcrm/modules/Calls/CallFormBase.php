02b1b526c254d11389be067e39bea3297ab325fb
Andreas Sandberg <asandberg@sugarcrm.com>
2011-10-20 15:25:11 -0700

    Bug#47867 - The free/busy status doesn' show in scheduling list bar

--- sugarcrm/modules/Calls/CallFormBase.php
+++ sugarcrm/modules/Calls/CallFormBase.php
@@ -251,7 +251,9 @@ function handleSave($prefix,$redirect=true,$useRequired=false) {
 	    $_POST['user_invitees'] = str_replace(',,', ',', $_POST['user_invitees']);
   	}
 
-    if(isset($_POST['isSaveFromDetailView']) && $_POST['isSaveFromDetailView'] == 'true'){
+    if( (isset($_POST['isSaveFromDetailView']) && $_POST['isSaveFromDetailView'] == 'true') ||
+        (isset($_POST['is_ajax_call']) && !empty($_POST['is_ajax_call']))
+    ){
         $focus->save(true);
         $return_id = $focus->id;
     }else{
