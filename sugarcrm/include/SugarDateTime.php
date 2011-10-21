bb2e5263c89a2f22cd9a7df158eab418558973b1
Stanislav Malyshev <smalyshev@gmail.com>
2011-10-21 15:30:42 -0700

    fix bug 47955: handle dates > 2038 properly

--- sugarcrm/include/SugarDateTime.php
+++ sugarcrm/include/SugarDateTime.php
@@ -108,7 +108,7 @@ class SugarDateTime extends DateTime
 		if(!$d) {
 			return false;
 		}
-		$sd = new self("@".$d->getTimestamp());
+		$sd = new self($d->format(DateTime::ISO8601));
 		$sd->setTimezone($d->getTimezone());
 		return $sd;
 	}
