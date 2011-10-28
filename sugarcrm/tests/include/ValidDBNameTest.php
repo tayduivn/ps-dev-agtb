920103e28702e39dbb1644da291e35cb2664997c
David Wheeler <dwheeler@sugarcrm.com>
2011-10-28 10:03:09 -0700

    Revert "'Duplicate' function issue in MB + unit test modification for getValidDBName() function"
    This reverts commit f212cb01c0e8cea871652e4332b4266df1729550.

--- sugarcrm/tests/include/ValidDBNameTest.php
+++ sugarcrm/tests/include/ValidDBNameTest.php
@@ -33,14 +33,6 @@ class ValidDBNameTest extends Sugar_PHPUnit_Framework_TestCase
             getValidDBName('idx_test_123_id')
         );
     }
-    
-    public function testLongNameEffected()
-    {
-        $this->assertNotEquals(
-            getValidDBName('eeeee_eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee_opportunities'),
-            getValidDBName('eeeee_eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee1_opportunities')
-        );
-    }
 
     public function testmaxLengthParam()
     {
