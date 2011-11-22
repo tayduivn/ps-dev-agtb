aa7476896a26ca3df8a4acdc46f696489f466d91
Gabriel Rael <grael@sugarcrm.com>
2011-11-22 14:19:42 -0500

    fix missing paren

--- sugarcrm/modules/Users/metadata/quickcreatedefs.php
+++ sugarcrm/modules/Users/metadata/quickcreatedefs.php
@@ -26,23 +26,23 @@
  * by SugarCRM are Copyright (C) 2004-2006 SugarCRM, Inc.; All Rights Reserved.
  ********************************************************************************/
 $viewdefs['Users']['QuickCreate'] = array(
-    'templateMeta' => array('maxColumns' => '2', 
+    'templateMeta' => array('maxColumns' => '2',
                             'widths' => array(
-                                array('label' => '10', 'field' => '30'), 
+                                array('label' => '10', 'field' => '30'),
                                 array('label' => '10', 'field' => '30')
                             ),
                             'form' => array(
                                 'headerTpl'=>'modules/Users/tpls/EditViewHeader.tpl',
                                 'footerTpl'=>'modules/Users/tpls/EditViewFooter.tpl',
                             ),
-                      ),
+    ),
     'panels' => array (
         'LBL_USER_INFORMATION' => array (
             array(
                 array(
                     'name'=>'user_name',
                     'displayParams' => array('required'=>true),
-                    ),
+                ),
                 'first_name'
             ),
             array(
@@ -55,37 +55,42 @@ $viewdefs['Users']['QuickCreate'] = array(
                     'name'=>'last_name',
                     'displayParams' => array('required'=>true),
                 ),
-            array(
                 array(
-                    'name'=>'email1',
-                    'displayParams' => array('required'=>true),
-                ),
-                array(
-                    'name'=>'UserType',
-                    'customCode'=>'{if $IS_ADMIN}{$USER_TYPE_DROPDOWN}{else}{$USER_TYPE_READONLY}{/if}',
+                    array(
+                        'name'=>'email1',
+                        'displayParams' => array('required'=>true),
+                    ),
+                    array(
+                        'name'=>'UserType',
+                        'customCode'=>'{if $IS_ADMIN}{$USER_TYPE_DROPDOWN}{else}{$USER_TYPE_READONLY}{/if}',
+                    ),
                 ),
             ),
-        ),
-        'LBL_EMPLOYEE_INFORMATION' => array(
-            array(array(
-                      'name'=>'employee_status',
-                      'customCode'=>'{if $IS_ADMIN}@@FIELD@@{else}{$EMPLOYEE_STATUS_READONLY}{/if}',
-                  ),
-                  'show_on_employees'),
-            array(array(
-                      'name'=>'title',
-                      'customCode'=>'{if $IS_ADMIN}@@FIELD@@{else}{$TITLE_READONLY}{/if}',
-                  ),
-                  'phone_work'),
-            array(array(
-                      'name'=>'department',
-                      'customCode'=>'{if $IS_ADMIN}@@FIELD@@{else}{$DEPT_READONLY}{/if}',
-                  ),
-            ),
-            array(array(
-                      'name'=>'reports_to_name',
-                      'customCode'=>'{if $IS_ADMIN}@@FIELD@@{else}{$REPORTS_TO_READONLY}{/if}',
-                  ),
+            'LBL_EMPLOYEE_INFORMATION' => array(
+                array(
+                    array(
+                        'name'=>'employee_status',
+                        'customCode'=>'{if $IS_ADMIN}@@FIELD@@{else}{$EMPLOYEE_STATUS_READONLY}{/if}',
+                    ),
+                    'show_on_employees'),
+                array(
+                    array(
+                        'name'=>'title',
+                        'customCode'=>'{if $IS_ADMIN}@@FIELD@@{else}{$TITLE_READONLY}{/if}',
+                    ),
+                    'phone_work'),
+                array(
+                    array(
+                        'name'=>'department',
+                        'customCode'=>'{if $IS_ADMIN}@@FIELD@@{else}{$DEPT_READONLY}{/if}',
+                    ),
+                ),
+                array(
+                    array(
+                        'name'=>'reports_to_name',
+                        'customCode'=>'{if $IS_ADMIN}@@FIELD@@{else}{$REPORTS_TO_READONLY}{/if}',
+                    ),
+                ),
             ),
         ),
     ),
