bab1cdc3df3cdd8ce5f74be2eec0dfc68ed284fb
John Mertic <jmertic@gmail.com>
2011-12-16 16:01:17 -0500

    Commented out change that broke tests.
    That said, we should really look at changing this, since it does break the possibility of this type of customization being done.

--- sugarcrm/include/EditView/SubpanelQuickCreate.php
+++ sugarcrm/include/EditView/SubpanelQuickCreate.php
@@ -66,8 +66,10 @@ class SubpanelQuickCreate{
 		// Bug 49219 - Check empty before set defaults, or the settings from viewdefs above will be overridden.
 		if (empty($this->ev->defs['templateMeta']['form']['headerTpl'])) $this->ev->defs['templateMeta']['form']['headerTpl'] = 'include/EditView/header.tpl';
 		if (empty($this->ev->defs['templateMeta']['form']['footerTpl'])) $this->ev->defs['templateMeta']['form']['footerTpl'] = 'include/EditView/footer.tpl';
-		if (empty($this->ev->defs['templateMeta']['form']['buttons'])) $this->ev->defs['templateMeta']['form']['buttons'] = array('SUBPANELSAVE', 'SUBPANELCANCEL', 'SUBPANELFULLFORM');	
-
+		// Comment below, breaks many out of the box viewdefs
+		/*if (empty($this->ev->defs['templateMeta']['form']['buttons'])) $this->ev->defs['templateMeta']['form']['buttons'] = array('SUBPANELSAVE', 'SUBPANELCANCEL', 'SUBPANELFULLFORM');*/
+		$this->ev->defs['templateMeta']['form']['buttons'] = array('SUBPANELSAVE', 'SUBPANELCANCEL', 'SUBPANELFULLFORM');
+		
 	    //Load the parent view class if it exists.  Check for custom file first
         loadParentView('edit');
 
