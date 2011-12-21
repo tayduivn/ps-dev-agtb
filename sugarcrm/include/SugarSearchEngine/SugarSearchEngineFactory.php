4224f844c60854bfb428bdb04fe849a67cc04ef8
Andreas Sandberg <asandberg@sugarcrm.com>
2011-12-20 20:32:34 -0800

    Separated out factory code

---
+++ sugarcrm/include/SugarSearchEngine/SugarSearchEngineFactory.php
@@ -0,0 +1,119 @@
+<?php
+if(!defined('sugarEntry') || !sugarEntry) die('Not A Valid Entry Point');
+/*********************************************************************************
+ *The contents of this file are subject to the SugarCRM Professional End User License Agreement
+ *("License") which can be viewed at http://www.sugarcrm.com/EULA.
+ *By installing or using this file, You have unconditionally agreed to the terms and conditions of the License, and You may
+ *not use this file except in compliance with the License. Under the terms of the license, You
+ *shall not, among other things: 1) sublicense, resell, rent, lease, redistribute, assign or
+ *otherwise transfer Your rights to the Software, and 2) use the Software for timesharing or
+ *service bureau purposes such as hosting the Software for commercial gain and/or for the benefit
+ *of a third party.  Use of the Software may be subject to applicable fees and any use of the
+ *Software without first paying applicable fees is strictly prohibited.  You do not have the
+ *right to remove SugarCRM copyrights from the source code or user interface.
+ * All copies of the Covered Code must include on each user interface screen:
+ * (i) the "Powered by SugarCRM" logo and
+ * (ii) the SugarCRM copyright notice
+ * in the same form as they appear in the distribution.  See full license for requirements.
+ *Your Warranty, Limitations of liability and Indemnity are expressly stated in the License.  Please refer
+ *to the License for the specific language governing these rights and limitations under the License.
+ *Portions created by SugarCRM are Copyright (C) 2006 SugarCRM, Inc.; All Rights
+ *Reserved.
+ ********************************************************************************/
+
+
+class SugarSearchEngineFactory
+{
+    public static $_instance;
+
+    /**
+     *
+     */
+    private function __construct()
+    {
+
+
+    }
+
+    /**
+     * getInstance()
+     *
+     * Connect to the backend engine and store for later use
+     *
+     * @static
+     * @return void
+     */
+    public static function getInstance($name = '')
+    {
+       if (!isset(self::$_instance))
+       {
+           self::$_instance = self::setupEngine($name);
+       }
+
+       return self::$_instance;
+    }
+
+    /**
+     * @static
+     * @param string $name
+     * @return bool
+     */
+    protected static function setupEngine($name = '')
+    {
+        if( empty($name) )
+        {
+            //if the name is empty then let's try to see if we have one configured in the config
+            if(!empty($GLOBALS['sugar_config']['full_text_engine']))
+            {
+                $keys = array_keys($GLOBALS['sugar_config']['full_text_engine']);
+                $name = $keys[0];
+            }
+            else
+            {
+
+            }
+        }
+
+        $defaultLocation = "include/SugarSearchEngine/{$name}/SugarSearchEngine{$name}.php";
+        $engineInstance = self::loadSearchEngineFromLocation("custom" . DIRECTORY_SEPARATOR . $defaultLocation);
+
+        if($engineInstance === FALSE)
+        {
+            $engineInstance = self::loadSearchEngineFromLocation($defaultLocation);
+        }
+
+        $GLOBALS['log']->fatal("Found Sugar Search Engine: {$engineInstance}");
+
+        return $engineInstance;
+    }
+
+    /**
+     * @static
+     * @param $filePath
+     * @return bool
+     */
+    protected static function loadSearchEngineFromLocation($filePath)
+    {
+        $filePath = realpath($filePath);
+        if( is_file($filePath) )
+        {
+            require_once($filePath);
+            $engineClass = basename($filePath, ".php");
+            $engineInstance = new $engineClass();
+
+            if ($engineInstance instanceof SugarSearchEngineInterface )
+            {
+                return $engineInstance;
+            }
+            else
+            {
+                return FALSE;
+            }
+        }
+        else
+        {
+            return FALSE;
+        }
+    }
+}
+
