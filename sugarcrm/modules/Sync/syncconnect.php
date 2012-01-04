1461f15b9d28619a437220a189d2cfa0e6a9b9ef
Stanislav Malyshev <smalyshev@gmail.com>
2012-01-03 23:54:49 -0800

    fix Sync - use config instead of user_hash to login

--- sugarcrm/modules/Sync/syncconnect.php
+++ sugarcrm/modules/Sync/syncconnect.php
@@ -29,20 +29,20 @@ if($sync_module_index == -1){
 		echo  get_form_header(translate('LBL_SYNC_SETUP','Sync') ,'',false);
 		display_progress_bar('sync_setup', $current_step , $module_steps);
 		echo '<br>';
-		
-	
+
+
 		echo '<div id="records_progress_div" style="display:inline">';
 		display_flow_bar('file_update', .5);
-	
-			
-		
+
+
+
 		echo '</div></td><td><div id = "current_substatus">&nbsp;<div><br><input type="button" name="stop_sync_btn" id="stop_sync_btn" class="button" value="Stop Sync" onclick="window.close();"></td></tr></table>';
 		echo '<table width="100%"><tr><td colspan="2" id="current_msg"  valign="top"><div id="show_conflict_div">&nbsp;<div></td></tr></table>';
 		echo '<div id="current_status">&nbsp;</div>';
 }
 if($sync_module_index == -1)add_to_msg("Logging Into Server...");
-	$result = $soapclient->call('login',array('user_auth'=>array('user_name'=>$current_user->user_name,'password'=>$current_user->user_hash, 'version'=>'1.5'), 'application_name'=>'MobileClient'));
-		
+	$result = $soapclient->call('login',array('user_auth'=>sync_get_user_auth_data(), 'application_name'=>'MobileClient'));
+
 	if(!has_error($result)){
 		$session = $result['id'];
 		$_SESSION['sync_session'] = $session;
@@ -50,28 +50,28 @@ if($sync_module_index == -1)add_to_msg("Logging Into Server...");
                 global $sugar_config;
                 global $sugar_flavor;
                 global $sugar_version;
-                
+
                 $soapclient->setHeaders('sugar_version='.$sugar_version);
                 $result = $soapclient->call('get_system_status', array('session'=>$session, 'unique_key'=>$sugar_config['unique_key']));
-				
+
 				if(!has_error($result) && $result['id'] == 0){
                     $result = $soapclient->call('get_sugar_flavor', array());
-                   
+
                     if(!has_error($result) && $result == $sugar_flavor){
 				        if($sync_module_index == -1){
 					       add_to_msg('Updating Files -<b>Please Wait</b>- <br><br>');
 					       $current_step++;
 					       update_progress_bar('sync_setup', $current_step , $module_steps);
-				
+
 					       if(isset($_REQUEST['new_sync']) ){
-				
+
 					       // run the file sync
 	    			        require_once( "include/utils/disc_client_utils.php" );
 	    			        $from_sync_client = true;
-	   				
+
                             //before we do a file sync we need to install any necessary upgrades
                             $upgrade_applied = get_required_upgrades($soapclient, $session);
-                    
+
 	   				        $results = disc_client_get_zip( $soapclient, $session, true, 0, $upgrade_applied);
 	   				        $_REQUEST['do_action']='execute';
 	   				        $_REQUEST['repair_silent'] = true;
@@ -116,7 +116,7 @@ if($sync_module_index == -1)add_to_msg("Logging Into Server...");
                                 xmlhttp.send(null);
                                 </script>";
                             //rebuild
-                           
+
                             echo "<div id='rrresult'></div>
                                 <script>
                                 var xmlhttp=false;
@@ -156,28 +156,28 @@ if($sync_module_index == -1)add_to_msg("Logging Into Server...");
                                 xmlhttp.open('GET', 'index.php?module=Administration&action=RebuildRelationship&to_pdf=true', true);
                                 xmlhttp.send(null);
                                 </script>";
-                               
-                                
+
+
                                 require_once('ModuleInstall/ModuleInstaller.php');
                                 $mi = new ModuleInstaller();
                                 $mi->rebuild_all(true);
 								$current_user->is_admin = 0;
-                                
+
                                 $db = &DBManagerFactory::getInstance();
                                 $query = "DELETE FROM versions WHERE name='Rebuild Extensions'";
                                 $db->query($query);
-                        
+
                                 // insert a new database row to show the rebuild extensions is done
                                 $id = create_guid();
                                 $gmdate = TimeDate::getInstance()->nowDb();
                                 $date_entered = db_convert("'$gmdate'", 'datetime');
                                 $query = 'INSERT INTO versions (id, deleted, date_entered, date_modified, modified_user_id, created_by, name, file_version, db_version) '
-                                    . "VALUES ('$id', '0', $date_entered, $date_entered, '1', '1', 'Rebuild Extensions', '4.0.0', '4.0.0')"; 
+                                    . "VALUES ('$id', '0', $date_entered, $date_entered, '1', '1', 'Rebuild Extensions', '4.0.0', '4.0.0')";
                                 $db->query($query);
-                        
+
 	   				            $current_step++;
 					            update_progress_bar('sync_setup', $current_step , $module_steps);
-	   				            	
+
 					            add_to_msg('Done updating files<br>');
 	   				            add_to_msg('Updating User Information<br>');
 	   				            $current_step++;
@@ -195,9 +195,9 @@ if($sync_module_index == -1)add_to_msg("Logging Into Server...");
                         add_to_msg('Server and Client must both be running the same flavor of Sugar.');
                     }
                 }else{
-                    add_to_msg('Your Offline Client instance has been disabled by your administrator');    
+                    add_to_msg('Your Offline Client instance has been disabled by your administrator');
                 }
-		
+
 	}else{
 		add_to_msg('Failed to Login<br>');
 	}
