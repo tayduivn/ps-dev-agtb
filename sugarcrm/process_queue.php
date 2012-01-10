6aad0c169d044018c4fb4d5ba5ed7482df19065b
Jenny Gonsalves <jenny@sugarcrm.com>
2012-01-10 11:56:38 -0800

    Revert "Merge pull request #1791 from Michael-Gusev/bug46898"
    This reverts commit 41e7d006d64d42242673c19e2ead661ae5618e9b, reversing
    changes made to 820f6e41eff7b63d9b1284df9f06bbe17c28de39.

--- sugarcrm/process_queue.php
+++ sugarcrm/process_queue.php
@@ -56,8 +56,7 @@ require_once('modules/ReportMaker/process_scheduled.php');
 
 global $report_modules,$modListHeader,$current_user;
 
-foreach ($reports_to_email as $schedule_info)
-{
+foreach($reports_to_email as $schedule_id => $schedule_info) {
 	$GLOBALS['log']->debug('-----> in Reports foreach() loop');
 	
 	$user = new User();
