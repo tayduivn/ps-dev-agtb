tayduivn/ps-dev-agtb

commit a8f0609fed1bf09f0d1d439632a34f1c87a21912

fix cache cleaning - need to clean app_* too

sugarcrm/modules/Administration/QuickRepairAndRebuild.php
@@ -346,6 +346,17 @@ class RepairAndClear
                     LanguageManager::clearLanguageCache($module_name);
             }
         }
+        // Clear app* cache values too
+        if(!empty($GLOBALS['sugar_config']['languages'])) {
+            $languages = $GLOBALS['sugar_config']['languages'];
+        } else {
+            $languages = array($GLOBALS['current_language'] => $GLOBALS['current_language']);
+        }
+        foreach(array_keys($languages) as $lang) {
+        	sugar_cache_clear('app_strings.'.$language);
+        	sugar_cache_clear('app_list_strings.'.$language);
+        }
+
 	}
 
 	/**

commit 5bdac2e8bca532c69972f34e2729f5e7b13e9f28

fix undefined variable notices caused by commit a8f0609fed1bf09f0d1d439632a34f1c87a21912

sugarcrm/modules/Administration/QuickRepairAndRebuild.php
@@ -352,7 +352,7 @@ class RepairAndClear
         } else {
             $languages = array($GLOBALS['current_language'] => $GLOBALS['current_language']);
         }
-        foreach(array_keys($languages) as $lang) {
+        foreach(array_keys($languages) as $language) {
         	sugar_cache_clear('app_strings.'.$language);
         	sugar_cache_clear('app_list_strings.'.$language);
         }