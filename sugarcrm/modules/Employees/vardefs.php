85e63a71919eb7684558e31383e9f6d312b44460
Gabriel Rael <grael@sugarcrm.com>
2012-01-17 12:12:48 -0500

    Bug 49716 - fix non-required fields for Employees module
    Set required to false in vardefs for non-required fields in Employees module

--- sugarcrm/modules/Employees/vardefs.php
+++ sugarcrm/modules/Employees/vardefs.php
@@ -35,4 +35,10 @@ $dictionary['Employee']['fields']['email_link_type']['massupdate']=false;
 $dictionary['Employee']['fields']['email1']['required']=false;
 $dictionary['Employee']['fields']['email_addresses']['required']=false;
 $dictionary['Employee']['fields']['email_addresses_primary']['required']=false;
+// bugs 47553 & 49716
+$dictionary['Employee']['fields']['date_entered']['required']=false;
+$dictionary['Employee']['fields']['date_modified']['required']=false;
+$dictionary['Employee']['fields']['status']['required']=false;
+$dictionary['Employee']['fields']['system_generated_password']['required']=false;
+$dictionary['Employee']['fields']['team_name']['required']=false;
 ?>
