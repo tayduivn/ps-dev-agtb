249be7a31469f02847e12966d07aeebe432620ed
asuharev <a.suharev@intetics.com>
2012-01-17 09:49:25 +0300

    Bug #47699 : Assignment notification display Module Name instead of Module Label

--- sugarcrm/data/SugarBean.php
+++ sugarcrm/data/SugarBean.php
@@ -1833,7 +1833,7 @@ class SugarBean
         if(in_array('set_notification_body', get_class_methods($this))) {
             $xtpl = $this->set_notification_body($xtpl, $this);
         } else {
-            $xtpl->assign("OBJECT", $this->object_name);
+            $xtpl->assign("OBJECT", translate('LBL_MODULE_NAME'));
             $template_name = "Default";
         }
         if(!empty($_SESSION["special_notification"]) && $_SESSION["special_notification"]) {
