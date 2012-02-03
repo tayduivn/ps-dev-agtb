22be73a483987ceb72eab7176c66911fd7a7bd73
Andreas Sandberg <asandberg@sugarcrm.com>
2012-02-03 13:52:20 -0800

    Adding metadata helper

--- sugarcrm/include/MVC/View/views/view.fts.php
+++ sugarcrm/include/MVC/View/views/view.fts.php
@@ -23,7 +23,7 @@ if(!defined('sugarEntry') || !sugarEntry) die('Not A Valid Entry Point');
 
 require_once('include/MVC/View/SugarView.php');
 require_once('include/SugarSearchEngine/SugarSearchEngineFactory.php');
-
+require_once('include/SugarSearchEngine/SugarSearchEngineMetadataHelper.php');
 
 class ViewFts extends SugarView
 {
