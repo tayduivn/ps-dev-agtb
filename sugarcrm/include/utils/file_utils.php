6be202f90242504981ce20c7894ca0ce53ee964c
Jimmy Chen <jchen@sugarcrm.com>
2012-02-06 14:51:53 -0800

    ig hook_array is already there, do not override it

--- sugarcrm/include/utils/file_utils.php
+++ sugarcrm/include/utils/file_utils.php
@@ -428,8 +428,12 @@ function createFTSLogicHook($filePath = 'application/Ext/LogicHooks/logichooks.e
     $fp = sugar_fopen($customFileLoc, 'wb');
     $contents = <<<CIA
 <?php
-\$hook_array = array();
-\$hook_array['after_save'] = array();
+if (!isset(\$hook_array) || !is_array(\$hook_array)) {
+    \$hook_array = array();
+}
+if (!isset(\$hook_array['after_save']) || !is_array(\$hook_array['after_save'])) {
+    \$hook_array['after_save'] = array();
+}
 \$hook_array['after_save'][] = array(1, 'fts', 'include/SugarSearchEngine/SugarSearchEngineQueueManager.php', 'SugarSearchEngineQueueManager', 'populateIndexQueue');
 CIA;
 
