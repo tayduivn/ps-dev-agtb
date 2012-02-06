dd7f38d27ae90d77a663341976419ab76024abe2
Michael Gusev <misha@guseff.com>
2012-02-06 13:42:01 +0300

    Fixes for unit test Bug49964Test

--- sugarcrm/tests/modules/ModuleBuilder/Bug49964Test.php
+++ sugarcrm/tests/modules/ModuleBuilder/Bug49964Test.php
@@ -38,64 +38,118 @@ require_once 'modules/ModuleBuilder/parsers/views/DeployedMetaDataImplementation
 class Bug49964Test extends Sugar_PHPUnit_Framework_TestCase
 {
     private $_metadata = array(
-        'set_complete' => array('width' => '1',
+        'set_complete' => array(
+            'width' => '1',
             'label'    => 'LBL_LIST_CLOSE',
             'default'  => true,
             'sortable' => false,
-            'related_fields' => array('status')),
-            'join_meeting' => array('width'    => '1',
+            'related_fields' => array(
+                'status'
+            )
+        ),
+        'join_meeting' => array(
+            'width'    => '1',
             'label'    => 'LBL_LIST_JOIN_MEETING',
             'default'  => true,
             'sortable' => false,
             'noHeader' => true,
-            'related_fields' => array('host_url')),
-            'name' => array('width'   => '40',
+            'related_fields' => array(
+                'host_url'
+            )
+        ),
+        'name' => array(
+            'width'   => '40',
             'label'   => 'LBL_SUBJECT',
             'link'    => true,
-            'default' => true),
-            'parent_name' => array('width' => '29',
+            'default' => true
+        ),
+        'parent_name' => array(
+            'width' => '29',
             'label' => 'LBL_LIST_RELATED_TO',
             'sortable' => false,
             'dynamic_module' => 'PARENT_TYPE',
             'link' => true,
             'id' => 'PARENT_ID',
             'ACLTag' => 'PARENT',
-            'related_fields' => array('parent_id', 'parent_type'),
-            'default' => true),
-            'duration' => array('width'    => '15',
+            'related_fields' => array(
+                'parent_id',
+                'parent_type'
+            ),
+            'default' => true
+        ),
+        'duration' => array(
+            'width'    => '15',
             'label'    => 'LBL_DURATION',
             'sortable' => false,
-            'related_fields' => array('duration_hours', 'duration_minutes')),
-            'date_start' => array('width'   => '15',
+            'related_fields' => array(
+                'duration_hours',
+                'duration_minutes'
+            )
+        ),
+        'date_start' => array(
+            'width'   => '15',
             'label'   => 'LBL_DATE',
             'default' => true,
-            'related_fields' => array('time_start')),
-            'set_accept_links'=> array('width'    => '10',
+            'related_fields' => array(
+                'time_start'
+            )
+        ),
+        'set_accept_links'=> array(
+            'width'    => '10',
             'label'    => 'LBL_ACCEPT_THIS',
             'sortable' => false,
             'default' => true,
-            'related_fields' => array('status')),
-            'status' => array('width'   => '8',
-            'label'   => 'LBL_STATUS'),
-            'type' => array('width'   => '8',
-            'label'   => 'LBL_TYPE'),
-            'date_entered' => array('width'   => '15',
-            'label'   => 'LBL_DATE_ENTERED'),
-            'date_modified' => array('width'   => '15',
-            'label'   => 'LBL_DATE_MODIFIED'),
-            'created_by' => array('width'   => '8',
-            'label'   => 'LBL_CREATED'),
-            'assigned_user_name' => array('width'   => '8',
-            'label'   => 'LBL_LIST_ASSIGNED_USER'),
-            'team_name' => array('width'   => '15',
-            'label'   => 'LBL_LIST_TEAM')
-            );
+            'related_fields' => array(
+                'status'
+            )
+        ),
+        'status' => array(
+            'width'   => '8',
+            'label'   => 'LBL_STATUS'
+        ),
+        'type' => array(
+            'width'   => '8',
+            'label'   => 'LBL_TYPE'
+        ),
+        'date_entered' => array(
+            'width'   => '15',
+            'label'   => 'LBL_DATE_ENTERED'
+        ),
+        'date_modified' => array(
+            'width'   => '15',
+            'label'   => 'LBL_DATE_MODIFIED'
+        ),
+        'created_by' => array(
+            'width'   => '8',
+            'label'   => 'LBL_CREATED'
+        ),
+        'assigned_user_name' => array(
+            'width'   => '8',
+            'label'   => 'LBL_LIST_ASSIGNED_USER'
+        ),
+        'team_name' => array(
+            'width'   => '15',
+            'label'   => 'LBL_LIST_TEAM'
+        )
+    );
+
     public function setUp()
     {
+        global $beanList, $beanFiles;
+        require('include/modules.php');
+
         $_REQUEST = array();
         $_REQUEST['view'] = 'dashlet';
         $_REQUEST['view_module'] = 'Meetings';
         $GLOBALS['app_list_strings'] = return_app_list_strings_language($GLOBALS['current_language']);
+    }
+
+    public function tearDown()
+    {
+        $_REQUEST = array();
+        unset($GLOBALS['app_list_strings']);
+        unset($GLOBALS['beanList']);
+        unset($GLOBALS['beanFiles']);
     }
 
     /**
