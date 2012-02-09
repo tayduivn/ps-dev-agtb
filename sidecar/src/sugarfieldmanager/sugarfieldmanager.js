10ba0bea22f9bb56973e00fbc6b11a6edf295038
David Tam <david.cheung.tam@gmail.com>
2012-02-09 10:53:27 -0800

    reformat and switch view checks

--- sidecar/src/sugarfieldmanager/sugarfieldmanager.js
+++ sidecar/src/sugarfieldmanager/sugarfieldmanager.js
@@ -9,141 +9,143 @@
     //var privateVars;
     app.augment('sugarFieldManager',
         (function () {
-        var instance;
+            var instance;
 
-        function init(args) {
-            instance = new SugarFieldManager();
-            _.bind(instance.handleResponse, instance);
-            return instance;
-        };
+            function init(args) {
+                instance = new SugarFieldManager();
+                _.bind(instance.handleResponse, instance);
+                return instance;
+            }
 
-        function SugarFieldManager() {
-            return {
-                //TODO move this to global cache
-                fieldsObj:{},
-                fieldsHash:'',
-                fieldTypeMap: {
-                                    varchar : "text",
-                                    name : "text",
-                                    text : "textarea"
-                                },
+            ;
+
+            function SugarFieldManager() {
+                return {
+                    //TODO move this to global cache
+                    fieldsObj:{},
+                    fieldsHash:'',
+                    fieldTypeMap:{
+                        varchar:"text",
+                        name:"text",
+                        text:"textarea"
+                    },
 
 
-                /**
-                 * Retrieves sugarFields and stores them internally
-                 *
-                 * @return bool
-                 */
-                syncFields:function () {
-                    // call api field sync with current field hash
-                    //TODO put real api call
-                    var that = this;
-                    result = SUGAR.App.sugarFieldsSync(that, this.handleResponse);
-                    return result;
-                },
+                    /**
+                     * Retrieves sugarFields and stores them internally
+                     *
+                     * @return bool
+                     */
+                    syncFields:function () {
+                        // call api field sync with current field hash
+                        //TODO put real api call
+                        var that = this;
+                        result = SUGAR.App.sugarFieldsSync(that, this.handleResponse);
+                        return result;
+                    },
 
-                /**
-                 * Callback handles API response for sugarfields
-                 *
-                 * @param  obj that sugarfield manager object
-                 * @param  obj response response from sugarFields
-                 * @return bool
-                 */
-                handleResponse:function (that, response) {
-                    // if we got something set fields and list
-                    if (response.fieldsHash != that.fieldsHash) {
-                        that.fieldsObj = response.fieldsData;
-                        that.fieldsHash = response.fieldsHash;
-                        return true;
-                    } else {
-                        return false;
-                    }
-                },
-
-                /**
-                 * Gets sugarFields from cache
-                 *
-                 * @param  array array of field objects that follow {fname:"xyz", view:"editView"}
-                 * @return obj of sugar fields stored by fieldname.viewtype
-                 */
-                getFields:function (fields) {
-
-                    // init results
-                    var fresult = {};
-                    var result = {};
-                    var name = "";
-                    var view = "";
-
-                    // loop over fields and set them in the result
-                    for (field in fields) {
-
-                        name=fields[field]['name'];
-
-                        if (fields[field]['view']) {
-                            view=fields[field]['view'];
-                        }
-
-                        if (!(result[name])) {
-                            result[name] = {}; // pre allocate the field in results
-                        }
-
-                        fresult = this.getField(name, view);
-                        if(view != "") {
-                            result[name][fields[field]['view']]=fresult;
+                    /**
+                     * Callback handles API response for sugarfields
+                     *
+                     * @param  obj that sugarfield manager object
+                     * @param  obj response response from sugarFields
+                     * @return bool
+                     */
+                    handleResponse:function (that, response) {
+                        // if we got something set fields and list
+                        if (response.fieldsHash != that.fieldsHash) {
+                            that.fieldsObj = response.fieldsData;
+                            that.fieldsHash = response.fieldsHash;
+                            return true;
                         } else {
-                            result[name]=fresult;
+                            return false;
+                        }
+                    },
+
+                    /**
+                     * Gets sugarFields from cache
+                     *
+                     * @param  array array of field objects that follow {fname:"xyz", view:"editView"}
+                     * @return obj of sugar fields stored by fieldname.viewtype
+                     */
+                    getFields:function (fields) {
+
+                        // init results
+                        var fresult = {};
+                        var result = {};
+                        var name = "";
+                        var view = "";
+
+                        // loop over fields and set them in the result
+                        for (field in fields) {
+
+                            name = fields[field]['name'];
+
+                            if (fields[field]['view']) {
+                                view = fields[field]['view'];
+                            }
+
+                            if (!(result[name])) {
+                                result[name] = {}; // pre allocate the field in results
+                            }
+
+                            fresult = this.getField(name, view);
+                            if (!view) {
+                                result[name][fields[field]['view']] = fresult;
+                            } else {
+                                result[name] = fresult;
+                            }
+
                         }
 
-                    }
-
-                    //return results
-                    return result;
-                },
+                        //return results
+                        return result;
+                    },
 
 
+                    /**
+                     * Gets sugarField from cache
+                     *
+                     * @param  object that follows {fname:"xyz", view:"editView"}
+                     * @return obj of sugar fields stored by fieldname.viewtype
+                     */
+                    getField:function (name, view) {
+                        // init results
+                        var result = {};
 
-                /**
-                 * Gets sugarField from cache
-                 *
-                 * @param  object that follows {fname:"xyz", view:"editView"}
-                 * @return obj of sugar fields stored by fieldname.viewtype
-                 */
-                getField:function (name, view) {
-                    // init results
-                    var result = {};
-
-                    name = this.fieldTypeMap[name] || name;
+                        name = this.fieldTypeMap[name] || name;
 
 
                         // assign fields to results if set
-                        if (view !="" && this.fieldsObj[name] && this.fieldsObj[name][view]) {
+                        if (!view && this.fieldsObj[name] && this.fieldsObj[name][view]) {
                             result = this.fieldsObj[name][view];
-                        // fall back to default if field for this view doesnt exist
+                            // fall back to default if field for this view doesnt exist
                         } else if (this.fieldsObj[name] && this.fieldsObj[name]['default']) {
                             result = this.fieldsObj[name]['default'];
                         } else {
                             result = {error:"No such field in field cache."};
                         }
 
-                    //return result
-                    return result;
-                },
+                        //return result
+                        return result;
+                    },
 
-                /**
-                 * Resets sugarFieldManager internal state to blank
-                 *
-                 * @return bool
-                 */
-                reset:function () {
-                    //reset all my internal variables
-                    this.fieldsObj = {};
-                    this.fieldsHash = '';
+                    /**
+                     * Resets sugarFieldManager internal state to blank
+                     *
+                     * @return bool
+                     */
+                    reset:function () {
+                        //reset all my internal variables
+                        this.fieldsObj = {};
+                        this.fieldsHash = '';
 
-                    return true;
-                }
+                        return true;
+                    }
+                };
             };
-        };
 
-        return instance || init();;
-    }()))
+            return instance || init();
+            ;
+        }()))
 }(SUGAR.App));
