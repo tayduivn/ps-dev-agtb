925c46f78bc9b403a689639605079617379200be
trampus <trampus@gmail.com>
2012-02-13 13:07:42 -0800

    init check in

--- sugarcrm/rest/index.php
+++ sugarcrm/rest/index.php
@@ -1,22 +1,7 @@
 <?php
 
+    include_once("include/rest/RestController.php");
 
-    $uri = $_SERVER["REQUEST_URI"];
-    $method = $_SERVER["REQUEST_METHOD"];
-    $encoding = $_SERVER["HTTP_ACCEPT_ENCODING"];
-
-    if ($method == "GET") {
-        print "<p>(*){$uri}</p>\n<p>";
-        print_r($_GET);
-        print "</p>\n";
-        $tmp = explode("/", $_GET["url"]);
-        var_dump($tmp);
-    } else {
-
-    }
-
-
-
-
-
+    $controller = new RestController();
+    $controller->execute();
 
