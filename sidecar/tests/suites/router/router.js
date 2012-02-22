aee5138b5a136e3ab0e6241390daf14058480409
Peter D <ixce@projectixce.net>
2012-02-21 18:43:57 -0800

    Fixed test not passing

--- sidecar/tests/suites/router/router.js
+++ sidecar/tests/suites/router/router.js
@@ -4,14 +4,14 @@ describe("Router", function() {
             loadView: function(args) {}
         };
 
-    SUGAR.App.router.start();
-
     it("should call the controller to load the default view", function() {
         var mock = sinon.mock(controller);
         mock.expects("loadView").once();
 
         // Initialize the router
         SUGAR.App.router.init({controller: controller});
+        SUGAR.App.router.start();
         expect(mock.verify()).toBeTruthy();
+
     });
 });
