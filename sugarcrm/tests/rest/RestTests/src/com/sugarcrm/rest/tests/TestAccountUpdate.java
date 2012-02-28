46bf27a616602ecbf48c732ff9ca7c4f46d10a55
trampus <trampus@gmail.com>
2012-02-28 14:08:23 -0800

    junit tests working just need to get ant files finished.

--- sugarcrm/tests/rest/RestTests/src/com/sugarcrm/rest/tests/TestAccountUpdate.java
+++ sugarcrm/tests/rest/RestTests/src/com/sugarcrm/rest/tests/TestAccountUpdate.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.*;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.Date;
 import java.util.HashMap;
 
 import org.apache.http.HttpEntity;
@@ -18,12 +19,14 @@ import org.apache.http.protocol.HttpContext;
 import org.junit.Test;
 
 import com.google.gson.Gson;
+import com.sugarcrm.rest.tests.TestAccountCreate.AccountId;
 import com.sugarcrm.rest.tests.TestAccountObjects.UserId;
 
 public class TestAccountUpdate {
 
 	@Test
 	public void test() {
+		TestData testData = new TestData();
 		String buffer = null;
 		int status = -1;
 		String uri = "";
@@ -34,12 +37,18 @@ public class TestAccountUpdate {
 		DefaultHttpClient client = null;
 		StringEntity entity;
 		HashMap<String, Object> data = new HashMap<String, Object>();
-		data.put("username", "admin");
-		data.put("password", "admin");
 		data.put("type", "text");
-		String accID = "4a0a649c-4088-01ce-bba2-4f44070b2214";
+		String tmp;
+		String account_name = "New Rest Account Name";
+		String account_newName = account_name;
 		
 		try {
+			Date date = new Date();
+			String time = String.valueOf(date.getTime());
+			account_name = String.format("%s-%s", account_name, time);
+			data.put("username", testData.getValue("sugaruser"));
+			data.put("password", testData.getValue("sugarpass"));
+			
 			context = new BasicHttpContext();
 			client = new DefaultHttpClient();
 			Gson json = new Gson();
@@ -48,7 +57,7 @@ public class TestAccountUpdate {
 			
 			System.out.printf("Sending login info:\n");
 			entity = new StringEntity(jsonString, "application/json", "UTF-8");
-			uri = String.format("%s/login", TestData.BaseURL);
+			uri = String.format("%s/login", testData.getValue("sugarinst"));
 			post = new HttpPost(uri);
 			post.setEntity(entity);
 			response = client.execute(post, context);
@@ -56,9 +65,8 @@ public class TestAccountUpdate {
 			
 			status = response.getStatusLine().getStatusCode();
 			if (status != 200) {
-				String tmp = String.format("Error: Status Code is '%d', was expecting: '200'!", status);
+				tmp = String.format("Error: Status Code is '%d', was expecting: '200'!", status);
 				fail(tmp);
-				System.out.printf("Login failed!\n");
 			}
 			
 			buffer = TestUtils.bufferToString(responseData);
@@ -69,22 +77,88 @@ public class TestAccountUpdate {
 			
 			// try to use Accounts object/module //
 			System.out.printf("(*)Getting Accounts Object...\n");
-			uri = String.format("%s/Accounts/%s", TestData.BaseURL, accID);
+			uri = String.format("%s/Accounts", testData.getValue("sugarinst"));
+
+			System.out.printf("(*)URI: %s\n", uri);
+			HashMap<String, String> postData = new HashMap<String, String>();
+			
+			postData.put("name", account_name);
+			postData.put("description", "Testing\n1\n2\n3...\nDONE>...");
+			postData.put("phone_office", "7078314197771");
+			postData.put("email1", "foo@bar.com");
+			String postJson = json.toJson(postData);
+			
+			// create new account //
+			post = new HttpPost(uri);
+			entity = new StringEntity(postJson, "application/json", "UTF-8");
+			post.setEntity(entity);
+			post.addHeader("OAuth-Token", id.token.toString());
+			post.addHeader("User-Agent", "evilkook");
+			
+			response = client.execute(post);
+			status = response.getStatusLine().getStatusCode();
+			if (status != 200) {
+				tmp = String.format("Error: Status Code is '%d', was expecting: '200'!", status);
+				fail(tmp);
+			}
+			
+			responseData = response.getEntity();
+			buffer = TestUtils.bufferToString(responseData);
+			System.out.printf("RESPONSE: '%s'\n\n", buffer);
+			AccountId accID = json.fromJson(buffer, AccountId.class);
+			System.out.printf("New Account ID: '%s'\n", accID.id);
+			// finished account create //
+			
+			// try to use Accounts object/module //
+			date = new Date();
+			time = String.valueOf(date.getTime());
+			account_newName = String.format("%s-%s", account_name, time);
+			
+			System.out.printf("(*)Getting Accounts Object...\n");
+			uri = String.format("%s/Accounts/%s", testData.getValue("sugarinst"), accID.id);
 
 			System.out.printf("(*)URI: %s\n\n", uri);
 			HashMap<String, String> putData = new HashMap<String, String>();
-			putData.put("name", "Updated Name --- 666");
+			putData.put("name", account_newName);
 			String putJson = json.toJson(putData);
 			HttpPut put = new HttpPut(uri);
 			entity = new StringEntity(putJson, "application/json", "UTF-8");
 			put.setEntity(entity);
-			put.addHeader("OAuth Token", id.token.toString());
+			put.addHeader("OAuth-Token", id.token.toString());
 			put.addHeader("User-Agent", "evilkook");
 			
 			response = client.execute(put);
+			status = response.getStatusLine().getStatusCode();
+			if (status != 200) {
+				tmp = String.format("Error: Status Code is '%d', was expecting: '200'!", status);
+				fail(tmp);
+			}
+			
 			responseData = response.getEntity();
 			buffer = TestUtils.bufferToString(responseData);
 			System.out.printf("RESPONSE: '%s'\n\n", buffer);
+			// finished with update //
+			
+			// read account back //
+			uri = String.format("%s/Accounts/%s", testData.getValue("sugarinst"), accID.id);
+			get = new HttpGet(uri);
+			System.out.printf("URI: '%s'\n", uri);
+			get.addHeader("OAuth-Token", id.token.toString());
+			get.addHeader("User-Agent", "evilkook");
+			response = client.execute(get);
+			status = response.getStatusLine().getStatusCode();
+			if (status != 200) {
+				tmp = String.format("Error: Status Code is '%d', was expecting: '200'!", status);
+				fail(tmp);
+			}
+			
+			responseData = response.getEntity();
+			buffer = TestUtils.bufferToString(responseData);
+			System.out.printf("RESPONSE: '%s'\n\n", buffer);
+			HashMap<String, Object> accountInfo = json.fromJson(buffer, HashMap.class);
+			assertEquals(account_newName, accountInfo.get("name"));
+			//finished reading account //
+			
 			System.out.printf("(*)Finished getting Accounts Object...\n");
 		} catch (Exception exp) {
 			exp.printStackTrace();
@@ -96,4 +170,7 @@ public class TestAccountUpdate {
 		private String token;
 	}
 	
+	public class AccountId {
+		private String id;
+	}
 }
