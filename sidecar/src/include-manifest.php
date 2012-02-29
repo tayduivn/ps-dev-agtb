41b6e1b04d8e38633557f0842e83bc8e6e4a28b3
Peter D <ixce@projectixce.net>
2012-02-29 13:09:13 -0800

    Added dependencies and added inclusion of library files

--- sidecar/src/include-manifest.php
+++ sidecar/src/include-manifest.php
@@ -1,9 +1,31 @@
 <?php
 
+$libraryFiles = array(
+    'lib/jquery/jquery.min.js',
+    'lib/backbone/underscore.min.js',
+    'lib/backbone/backbone.min.js',
+    'lib/stash/stash.min.js',
+);
+
 $includeFiles = array(
+    //
+    'lib/handlebars/handlebars-1.0.0.beta.6.js',
+
     'src/app.js',
     'src/cache/cache.js',
     'src/template/template.js',
+    'src/sugarfieldmanager/sugarfieldmanager.js',
+    'lib/sugarapi/sugarapi.js',
+    'src/events/events.js',
+    'src/context/context.js',
     'src/controller/controller.js',
-    'src/controller/router.js'
+    'src/controller/router.js',
+    'src/metadata/metadata-manager.js',
+    'src/logger.js',
+    'src/config.js',
+    'src/data/bean.js',
+    'src/data/bean-collection.js',
+    'src/data/data-manager.js',
+    'src/data/validation.js',
+    'src/layout/layout.js',
 );
