d2f9eb55fea22bce0e11d236285a9537d6d9c127
Akshay Joshi <ajoshi@sugarcrm.com>
2012-02-29 18:37:30 -0800

    Add some token detection borrowed from UglifyJS.

--- sugarcrm/jssource/jsmin.php
+++ sugarcrm/jssource/jsmin.php
@@ -24,8 +24,30 @@ class SugarMin {
     protected $preNewLineSafeChars = array('\\', '$', '_', '}', ']', ')', '+', '-', '"', "'");
     protected $regexChars = array('(', ',',  '=', ':', '[', '!', '&', '|', '?', '{', '}', ';');
     protected $compression;
+    protected $whitespaceChars = array(" ", "\n", "\r", "\t", "\f");
 
-    private function __construct() {}
+    /**
+     * jsParser will take javascript source code and minify it.
+     *
+     * Note: There is a lot of redundant code since both passes
+     * operate similarly but with slight differences. It will probably
+     * be a good idea to refactor the code at a later point when it is stable.
+     *
+     * JSParser will perform 3 passes on the code. Pass 1 takes care of single
+     * line and mult-line comments. Pass 2 performs some sanitation on each of the lines
+     * and pass 3 works on stripping out unnecessary spaces.
+     *
+     * @param string $js
+     * @param string $currentOptions
+     * @return void
+     */
+    private function __construct($text, $compression) {
+        $this->text = $text;
+        $this->compression = $compression;
+        $this->pos = 0;
+        $this->line = 0;
+        $this->col = 0;
+    }
 
     /**
      * Entry point function to minify javascript.
@@ -36,8 +58,8 @@ class SugarMin {
      */
     static public function minify($js, $compression = 'light') {
         try {
-            $me = new SugarMin();
-            $output = $me->jsParser($js, $compression);
+            $me = new SugarMin($js, $compression);
+            $output = $me->jsParser();
             return $output;
         } catch (Exception $e) {
             // Exception handling is left up to the implementer.
@@ -45,136 +67,181 @@ class SugarMin {
         }
     }
 
-    /**
-	 * jsParser will take javascript source code and minify it.
-     *
-     * Note: There is a lot of redundant code since both passes
-     * operate similarly but with slight differences. It will probably
-     * be a good idea to refactor the code at a later point when it is stable.
-	 *
-     * JSParser will perform 3 passes on the code. Pass 1 takes care of single
-     * line and mult-line comments. Pass 2 performs some sanitation on each of the lines
-     * and pass 3 works on stripping out unnecessary spaces.
-     *
-	 * @param string $js
-	 * @param string $currentOptions
-	 * @return void
-	 */
-    protected function jsParser($js, $compression = 'light') {
-
+    private function replace_newlines() {
         // We first perform a few operations to simplify our
         // minification process. We convert all carriage returns
         // into line breaks.
-        $js = str_replace("\r\n", "\n", $js);
-        $js = str_replace("\r", "\n", $js);
-        $js = preg_replace("/\n+/","\n", $js);
+        $this->text = str_replace("\r\n", "\n", $this->text);
+        $this->text = str_replace("\r", "\n", $this->text);
+
+        // Then, we find all extra new line characters and remove those.
+        $this->text = preg_replace("/\n+/","\n", $this->text);
+    }
+
+    function find($str, $signal_eof = FALSE) {
+        $pos = strpos($this->text, $str);
+        if($signal_eof && $pos === FALSE) {
+            throw new Exception;
+        }
+        return $pos;
+    }
+
+    private function tryEOF($message, $function) {
+        try {
+            return $function();
+        } catch (Exception $e) {
+            throw new Exception($message);
+        }
+    }
+
+    private function read_line_comment() {
+        $this->nextChar();
+        // The following line is a hack until PHP 5.4.
+        $ref = $this;
+        return $this->tryEOF('Unterminated single-line comment.', function() use ($ref) {
+            $i = $ref->find("\n", TRUE);
+            if($i === FALSE) {
+                // Comment goes until the end of file.
+                $ret = substr($ref->text, $ref->pos);
+                $ref->pos = strlen($ref->text);
+            } else {
+                $ret = substr($ref->text, $ref->pos, $i-$ref->pos);
+            }
+            return $ret;
+        });
+    }
+
+    private function read_multiline_comment() {
+        $this->nextChar();
+        // The following line is a hack until PHP 5.4.
+        $ref = $this;
+        return $this->tryEOF('Unterminated multi-line comment.', function () use ($ref) {
+            $i = $ref->find("*/", TRUE);
+            $text = substr($ref->text, $ref->pos, $i-$ref->pos);
+            $ref->nextChar(); $ref->nextChar();
+            $ref->line += count(explode("\n", $text)) - 1;
+            return $text;
+        });
+    }
+
+    private function handle_slash(&$str) {
+        $this->nextChar();
+        switch($this->peek()) {
+            case "/":
+                // Single line comment.
+                $comment = '//'.$this->read_line_comment();
+                echo "Got comment: ".$comment."\n---\n";
+                break;
+            case "*":
+                // Multi-line comment.
+                $comment = '/*'.$this->read_multiline_comment().'*/';
+                echo "Got multiline comment: ".$comment."\n---\n";
+                break;
+            default:
+                // Looks like a regex, smells like a regex. Also could be a division operator.
+                // TODO: need to catch the division operator.
+
+                // The following line is a hack until PHP 5.4.
+                $ref = $this;
+                $str .= $this->tryEOF('Unterminated regular expression.', function ($regex = "") use ($ref) {
+                    $prev_backslash = false;
+                    $ch = null;
+                    $in_class = false;
+                    
+                    while($ch = $ref->nextChar(TRUE)) {
+                        if ($prev_backslash) {
+                            $regex .= "\\" + $ch;
+                            $prev_backslash = FALSE;
+                        } elseif ($ch == "[") {
+                            $in_class = TRUE;
+                            $regex .= $ch;
+                        } elseif ($in_class && $ch == "]") {
+                            $in_class = FALSE;
+                            $regex .= $ch;
+                        } elseif ($ch == '/' && !$in_class) {
+                            break;
+                        } elseif ($ch == '\\') {
+                            $prev_backslash = TRUE;
+                        } else {
+                            $regex .= $ch;
+                        }
+                    }
+
+                    // TODO: check if we need the read_name() method.
+                    return $regex;
+                });
+                break;
+        }
+    }
+
+    function peek() {
+        return $this->charAt($this->pos);
+    }
+
+    function nextChar($signal_eof = FALSE) {
+        $ch = $this->charAt($this->pos++);
+        if($signal_eof && !$ch) {
+            throw new Exception;
+        }
+        if($ch == "\n") {
+            $this->line++;
+            $this->col = 0;
+        } else {
+            $this->col++;
+        }
+        return $ch;
+    }
+
+    function isEOF() {
+        return $this->pos >= strlen($this->text);
+    }
+
+    function charAt($pos) {
+        return substr($this->text, $pos, 1);
+    }
+
+    protected function jsParser() {
+        $this->replace_newlines();
+        
 
         $stripped_js = '';
-        $prevChar = null;
-        $lastChar = null;
 
         // Pass 1, strip out single line and multi-line comments.
-        for ($i = 0; $i < strlen($js); $i++) {
-            $char = $js[$i];
-
-            if (strlen($stripped_js) > 0) {
-                $lastChar = $stripped_js[strlen($stripped_js) - 1];
-            }
-
-            if ($lastChar != " " && $lastChar != "\n" && $lastChar != null) {
-                $prevChar = $lastChar;
-            }
-
+        while(!$this->isEOF()) {
+            $char = $this->peek();
             switch ($char) {
                 case "\\": // If escape character
-                    $stripped_js .= $char.$js[$i + 1];
-                    $i++;
+                    $next = $this->nextChar();
+                    $stripped_js .= $char.$next;
                     break;
                 case '"': // If string literal
                 case "'":
                     $literal = $delimiter = $char;
 
-                    for ($j = $i + 1; $j < strlen($js); $j++) {
-                        $literal .= $js[$j];
-
-                        if ($js[$j] == "\\") {
-                            $literal .= $js[$j + 1];
-                            $j++;
+                    while(!$this->isEOF()) {
+                        $char = $this->nextChar();
+                        $literal .= $char;
+                        if($char == "\\") {
+                            $literal .= $this->nextChar();
+                            // Don't match the delimiter if it's escaped.
                             continue;
                         }
 
-                        if ($js[$j] == $delimiter) {
+                        if ($char == $delimiter) {
+                            // Found the closing delimiter.
                             break;
                         }
                     }
 
-                    $i = $j;
                     $stripped_js .= $literal;
                     break;
                 case "/": // If comment or regex
-                    if ($js[$i + 1] == "/") {
-                        $comment = $char;
-                        for ($j = $i + 1; $j < strlen($js); $j++) {
-                            if ($js[$j] == "\\") {
-                                $j++;
-                                continue;
-                            }
-
-                            if (!isset($js[$j + 1]) || $js[$j + 1] == "\n") {
-                                break;
-                            }
-
-                            $comment .= $js[$j];
-                        }
-                        $i = $j;
-                        break;
-                    } else if ($js[$i + 1] == "*") {
-                        $mlcomment = $char;
-                        for ($j = $i + 1; $j < strlen($js); $j++) {
-                            if ($js[$j] == "\\") {
-                                $j++;
-                                continue;
-                            }
-
-                            if ($js[$j] == "*" && $js[$j + 1] == "/") {
-                                break;
-                            }
-
-                            $mlcomment .= $js[$j];
-                        }
-
-                        $i = $j + 1;
-                        break;
-                    } else if (in_array($prevChar, $this->regexChars) || $prevChar == null) {
-                        $nesting = 0;
-                        $stripped_js .= $js[$i];
-
-                        for ($j = $i + 1; $j < strlen($js); $j++) {
-                            if ($js[$j] == "\\") {
-                                $stripped_js .= $js[$j].$js[$j + 1];
-                                $j++;
-                                continue;
-                            }
-
-                            if ($js[$j] == '[') {
-                                $nesting++;
-                            } else if ($js[$j] == ']') {
-                                $nesting--;
-                            }
-
-                            $stripped_js .= $js[$j];
-
-                            if ($js[$j] == '/' && $nesting == 0 && $prevChar != "\\") {
-                                break;
-                            }
-                        }
-                        $i = $j;
-                        break;
-                    }
+                    $this->handle_slash($stripped_js);
                 default:
                     $stripped_js .= $char;
                     break;
             }
+            $this->nextChar();
         }
 
         // Split our string up into an array and iterate over each line
