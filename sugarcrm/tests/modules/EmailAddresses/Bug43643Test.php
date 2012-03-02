ed79d261235662ec062ff2c4b70cb9bbca728ddf
Collin Lee <clee@sugarcrm.com>
2012-03-02 11:23:00 -0500

    Removed the RestApiTest from the modules/Teams directory.  This is not a useful test.
    Fixed Bug43643Test.php

--- sugarcrm/tests/modules/EmailAddresses/Bug43643Test.php
+++ sugarcrm/tests/modules/EmailAddresses/Bug43643Test.php
@@ -28,37 +28,50 @@
  */
 class Bug43643Test extends Sugar_PHPUnit_Framework_TestCase
 {
+    public function setUp()
+    {
+        global $current_user, $beanList, $beanFiles;
+        $current_user = SugarTestUserUtilities::createAnonymousUser();
+        require('include/modules.php');
+    }
+
+    public function tearDown()
+    {
+        SugarTestUserUtilities::removeAllCreatedAnonymousUsers();
+    }
+
+
     /**
      * Test EmailAddress::testReplyToAddress() method.
      */
     public function testReplyToAddress()
     {
-        $user = SugarTestUserUtilities::createAnonymousUser();
-        $user->load_relationship('email_addresses');
+        global $current_user;
+        $current_user->load_relationship('email_addresses');
 
         $non_reply_to_value = 'non-reply-to@example.com';
-        $non_reply_to_address = $this->addEmailAddress($user, $non_reply_to_value);
+        $non_reply_to_address = $this->addEmailAddress($current_user, $non_reply_to_value);
 
         require_once 'modules/EmailAddresses/EmailAddress.php';
         $email_address = new EmailAddress();
 
         // ensure that the only email address value is returned
-        $non_reply_to_result = $email_address->getReplyToAddress($user);
+        $non_reply_to_result = $email_address->getReplyToAddress($current_user);
         $this->assertEquals($non_reply_to_value, $non_reply_to_result);
 
         // ensure that empty string is returned when there is no reply-to
         // address exists
-        $reply_to_only_result1 = $email_address->getReplyToAddress($user, true);
+        $reply_to_only_result1 = $email_address->getReplyToAddress($current_user, true);
         $this->assertEquals('', $reply_to_only_result1);
 
         // create reply-to address
         $reply_to_value = 'some-address-2@example.com';
-        $reply_to_address = $this->addEmailAddress($user, $reply_to_value, array(
+        $reply_to_address = $this->addEmailAddress($current_user, $reply_to_value, array(
             'reply_to_address' => true,
         ));
 
         // ensure that reply-to address is returned
-        $reply_to_only_result2 = $email_address->getReplyToAddress($user, true);
+        $reply_to_only_result2 = $email_address->getReplyToAddress($current_user, true);
         $this->assertEquals($reply_to_value, $reply_to_only_result2);
 
         // clean everything up
@@ -88,6 +101,7 @@ class Bug43643Test extends Sugar_PHPUnit_Framework_TestCase
 
         // create relation between user and email address
         $email_addresses->add(array($email_address), $additional_values);
+        $GLOBALS['db']->commit();
         return $email_address;
     }
 }
