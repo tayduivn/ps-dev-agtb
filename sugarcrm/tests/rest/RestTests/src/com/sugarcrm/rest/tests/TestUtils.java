3d077949bf171f2655281fcfa4b2107e8e5469c8
trampus <trampus@gmail.com>
2012-03-02 16:55:37 -0800

    updated

--- sugarcrm/tests/rest/RestTests/src/com/sugarcrm/rest/tests/TestUtils.java
+++ sugarcrm/tests/rest/RestTests/src/com/sugarcrm/rest/tests/TestUtils.java
@@ -1,13 +1,17 @@
 package com.sugarcrm.rest.tests;
 
 import java.io.BufferedReader;
-import java.io.FileWriter;
 import java.io.InputStreamReader;
-
 import org.apache.http.HttpEntity;
 
 public class TestUtils {
 
+	/**
+	 * This function takes an {@link HttpEntity} and converts its payload into a {@link String}.
+	 * 
+	 * @param entity
+	 * @return
+	 */
 	public static String bufferToString(HttpEntity entity) {
 		String buffer = "";
 		String tmp = "";
@@ -17,8 +21,7 @@ public class TestUtils {
 		try {
 			in = new InputStreamReader(entity.getContent());
 			reader = new BufferedReader(in);
-			
-			
+
 			while ((tmp = reader.readLine()) != null) {
 				buffer = buffer + tmp;
 			}
@@ -29,6 +32,4 @@ public class TestUtils {
 		
 		return buffer;
 	}
-		
-	
 }
