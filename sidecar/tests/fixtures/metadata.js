94213fb74b772ffddb156c5b664d7192124cdd63
David Tam <david.cheung.tam@gmail.com>
2012-03-05 10:22:23 -0800

    added cases metadata and updated routing to default index to cases list view

--- sidecar/tests/fixtures/metadata.js
+++ sidecar/tests/fixtures/metadata.js
@@ -8,6 +8,213 @@
 
 fixtures = typeof(fixtures) == "object" ? fixtures : {};
 fixtures.metadata = {
+    "Cases":{
+        "primary_bean":"Case",
+        "beans":{
+            "Case":{
+                "vardefs":{
+                    "table":"cases",
+                    "fields":{
+                        "id":{
+                            "name":"id",
+                            "type":"id"
+                        },
+                        "case_number":{
+                            "name":"case_number",
+                            "type":"varchar"
+                        },
+                        "name":{
+                            "name":"name",
+                            "type":"varchar"
+                        },
+                        "description":{
+                            "name":"description",
+                            "type":"text"
+                        },
+                        "type":{
+                            "name":"type",
+                            "type":"varchar"
+                        },
+                        "status":{
+                            "name":"status",
+                            "type":"varchar"
+                        },
+                        "date_entered":{
+                            "name":"date_entered",
+                            "type":"varchar"
+                        },
+                        "created_by":{
+                            "name":"created_by",
+                            "type":"varchar"
+                        },
+                        "date_modified":{
+                            "name":"date_modified",
+                            "type":"varchar"
+                        },
+                        "modified_user_id":{
+                            "name":"modified_user_id",
+                            "type":"varchar"
+                        }
+                    }
+                },
+                "relationships":{
+
+                }
+            }
+        },
+        "views":{
+            "editView":{
+                "buttons":[
+                    {
+                        name:"save_button",
+                        type:"button_save",
+                        label:"Save",
+                        value:"save",
+                        primary:true
+                    },
+                    {
+                        name:"cancel_button",
+                        type:"button",
+                        label:"Cancel",
+                        value:"cancel",
+                        route:{
+                            action:"detail",
+                            module: "Cases"
+                        },
+                        events : {
+                            click : "SUGAR.App.myExtension.callback",
+                            drag: "",
+                            foo : 'function(e){console.log(this)}'
+                        },
+                        primary:false
+                    }
+                ],
+                "panels":[
+                    {
+                        "label":"Details",
+                        "fields":[
+                            {name:"case_number", label:"Case Number", "class":"foo"},
+                            {name:"name", label:"Name"},
+                            {name:"status", label:"Status"},
+                            {name:"description", label:"Description"}
+
+                        ]
+                    }
+                ]
+
+            },
+            "detailView":{
+                "buttons":[
+                    {
+                        name:"edit_button",
+                        type:"button",
+                        label:"Edit",
+                        value:"edit",
+                        route:{
+                            action:"edit"
+                        },
+                        primary:true
+                    }
+                ],
+                "panels":[
+                    {
+                        "label":"Details",
+                        "fields":[
+                            {name:"case_number", label:"Case Number", "class":"foo"},
+                            {name:"name", label:"Name"},
+                            {name:"status", label:"Status"},
+                            {name:"description", label:"Description"}
+
+                        ]
+                    }
+                ]
+            },
+            "quickCreate":{
+
+            },
+            //This is stored in a listviewdefs variable on the server, but its inconsistent with the rest of the app
+            "listView":{
+                "panels":[
+                    {
+                        "label":"LBL_PANEL_1",
+                        "fields":[
+                            {name:"case_number", label:"Case Number", "class":"foo"},
+                            {name:"name", label:"Name"},
+                            {name:"status", label:"Status"},
+                            {type:"sugarField_actionsLink", label:"Actions"}
+                        ]
+                    }
+                ]
+            },
+            //Subpanel layout defs
+            "subpanelView":{
+
+            }
+        },
+        //Layouts map an action to a lyout that defines a set of views and how to display them
+        //Different clients will get different layouts for the same actions
+        "layouts":{
+            "edit":{
+                //Default layout is a single view
+                "type":"simple",
+                "components":[
+                    {view:"editView"}
+                ]
+            },
+            "detail":{
+                "type":"rows",
+                "components":[
+                    {view:"detailView"},
+                    {view:"subpanelView"}
+                ]
+            },
+            "list":{
+                //Default layout is a single view
+                "type":"simple",
+                "components":[
+                    {view:"listView"}
+                ]
+            },
+            //Example of a sublayout. Two columns on the top and one view below that
+            "sublayout":{
+                "type":"rows",
+                "components":[
+                    {"layout":{
+                        "type":"columns",
+                        "components":[
+                            {view:"editView"},
+                            {view:"detailView"}
+                        ]
+                    }},
+                    {"view":"subpanelView"}
+                ]
+            },
+            //Layout with context switch. Edit view with related detail view
+            "complexlayout":{
+                "type":"columns",
+                "components":[
+                    {"view":"editView"},
+                    {
+                        "view":"detailView",
+                        //Name of link to pull the new context from, In this case a single account
+                        "context":"accounts"
+                    }
+                ]
+            },
+            //Layout that references another layout
+            "detailplus":{
+                "type":"fluid",
+                "components":[
+                    {view:"subpanelView",
+                        size:2},
+                    {layout:"edit",
+                        size:6},
+                    {layout:"detail",
+                        size:3}
+                ]
+            }
+        }
+    },
     "Contacts":{
         "primary_bean":"Contact",
         "beans":{
