4c5fc6cb9378faff70865b3f023b75b7796e4996
trampus <trampus@gmail.com>
2012-03-13 15:43:11 -0700

    added phpdoc

--- sugarcrm/include/rest/internalObjects/Result.php
+++ sugarcrm/include/rest/internalObjects/Result.php
@@ -1,5 +1,14 @@
 <?php
 
+/**
+ * This class is going to be used for all classes for passing back results so that any classing using the
+ * sub classes can have a stanrdard error.  This will allow for a soap api to be created on top of this code, and
+ * let the soap class & rest class handle all error codes as they see fit.
+ *
+ * Note:
+ *  Currently this class is not in use.
+ *
+ */
 class Result {
 
     private $data = null;
