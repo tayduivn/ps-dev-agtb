2327aff74e8c7f8306b76199d454bbcd5cb9762f
Akshay Joshi <ajoshi@sugarcrm.com>
2012-03-14 14:26:53 -0700

    Bug 51270 - convert 'Elastic Search' to 'elasticsearch'.

--- sugarcrm/install/language/en_us.lang.php
+++ sugarcrm/install/language/en_us.lang.php
@@ -574,7 +574,7 @@ $mod_strings = array(
     'LBL_FTS_HOST'     => 'Host',
     'LBL_FTS_PORT'     => 'Port',
     'LBL_FTS_TYPE'     => 'Search Engine Type',
-    'LBL_FTS_HELP'      => 'To enable full-text searching, select the search engine type and enter the Host and Port where the search engine is hosted. Sugar includes built-in support for the Elastic Search engine.',
+    'LBL_FTS_HELP'      => 'To enable full-text searching, select the search engine type and enter the Host and Port where the search engine is hosted. Sugar includes built-in support for the elasticsearch engine.',
     'LBL_FTS_ERROR'    => 'Unable to connect to Full Text Search server, please verify your settings.',
     // END SUGARCRM flav=pro ONLY
 
