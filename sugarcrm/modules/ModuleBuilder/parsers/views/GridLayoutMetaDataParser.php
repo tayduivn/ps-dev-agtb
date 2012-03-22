473bdb64e947c7ad5fd64c24f97eb54b08077755
Zlata Damarad <zlata-domorad@yandex.ru>
2012-03-22 12:26:44 +0300

    bug 43787: Cases quick create layout allows user to input case number

--- sugarcrm/modules/ModuleBuilder/parsers/views/GridLayoutMetaDataParser.php
+++ sugarcrm/modules/ModuleBuilder/parsers/views/GridLayoutMetaDataParser.php
@@ -761,6 +761,11 @@ class GridLayoutMetaDataParser extends AbstractMetaDataParser implements MetaDat
                     'customCode' => true , 'customLabel' => true , 'tabindex' => true , 'hideLabel' => true) ) ;
         if (!empty($def['vname']) && empty($def['label']))
             $ret['label'] = $def['vname'];
+        // bug 43787: Cases quick create layout allows user to input case number 
+        if (!empty($def['readonly']))
+        {
+            $ret['type'] = 'readonly';
+        }
 		return $ret;
 	}
 	
