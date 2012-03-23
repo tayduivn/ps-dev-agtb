d6626afcda2f9bc418cce81be5b3b810c84f8c40
Wes Moran <westonnh@gmail.com>
2012-03-23 15:07:49 -0400

    Updated mobile pages
    Added zepto, swipes for menus and actions, fixed basic links

--- sugarcrm/styleguide/js/mobile.js
+++ sugarcrm/styleguide/js/mobile.js
@@ -1,32 +1,62 @@
-// Make links keep in app 
-(function(a,b,c){if(c in b&&b[c]){var d,e=a.location,f=/^(a|html)$/i;a.addEventListener("click",function(a){d=a.target;while(!f.test(d.nodeName))d=d.parentNode;"href"in d&&(chref=d.href).replace(e.href,"").indexOf("#")&&(!/^[a-z\+\.\-]+:/i.test(chref)||chref.indexOf(e.protocol+"/"+e.host)===0)&&(a.preventDefault(),e.href=d.href)},!1)}})(document,window.navigator,"standalone")
+(function($) {
+  (function(a,b,c){if(c in b&&b[c]){var d,e=a.location,f=/^(a|html)$/i;a.addEventListener("click",function(a){d=a.target;while(!f.test(d.nodeName))d=d.parentNode;"href"in d&&(chref=d.href).replace(e.href,"").indexOf("#")&&(!/^[a-z\+\.\-]+:/i.test(chref)||chref.indexOf(e.protocol+"/"+e.host)===0)&&(a.preventDefault(),e.href=d.href)},!1)}})(document,window.navigator,"standalone")
+  
+    // swipe for top nav
+    $('.navbar').bind('touchmove', function (e) {e.preventDefault();} );
 
-!function ($) {
-  $(function(){
-    
-    if ( $(window).width() < 768) {		
-      // trigger the module menu
-      $('.cube').click(function () {
-        $('html').find('body').toggleClass('onL');
-        return false;
-      })
+		$('#logo').swipeRight(function () {
+      $('html').find('body').addClass('onL');
+		})
+		
+		$('#create').swipeLeft(function () {
+      $('html').find('body').addClass('onR');
+		})  		
+		
+		$('#moduleList').swipeLeft(function () {
+      $('html').find('body').removeClass('onL');
+		})
+		
+		$('#createList').swipeRight(function () {
+      $('html').find('body').removeClass('onR');
+		})  		
+		$('.navbar').swipeDown(function () {
+      $('body').find('#searchForm').toggleClass('hide');
+		})
+		
+    // trigger the module menu - this could be a tap function but zepto will not honor return false
+    $('.cube').click(function () {
+      $('html').find('body').toggleClass('onL');
+      return false;
+    })
 
-      // trigger the module menu
-      $('.launch').click(function () {
-        $('html').find('body').toggleClass('onR');
-        return false;
-      })
-    }
+    // trigger the module menu - this could be a tap function but zepto will not honor return false
+    $('.launch').click(function () {
+      $('html').find('body').toggleClass('onR');
+      return false;
+    })
+
 
     // toggle stars (needs tap logic for mobile)
     $('article').find('[class^=icon-star]').on('click', function () {
       $(this).toggleClass('icon-star icon-star-empty');
     })
-    $('article [id^=listing-action]').find('.grip').on('click', function () {
-      $(this).parent().find('span').toggleClass('hide on');
+    
+
+		$('article').swipeLeft(function () {
+		  $(this).find('.grip').addClass('on');
+      $(this).find('[id^=listing-action] span').removeClass('hide').addClass('on');
+		})
+		
+		$('article').swipeRight(function () {
+      $(this).find('.grip').removeClass('on');
+      $(this).find('[id^=listing-action] span').addClass('hide').removeClass('on');
+		})
+		
+    $('article .grip').on('click', function () {
+      $(this).next('.actions').toggleClass('hide');
       $(this).toggleClass('on');
     })
-    
+
     // search toggle
     $('.navbar').find('#search').on('click', function () {
       $('body').find('#searchForm').toggleClass('hide');
@@ -47,5 +77,5 @@
       $(this).parent().remove();
       $('container-fluid').html('<div class="top alert alert-danger alert-block">Opportunity has been removed.</div>');
     })
-  })
-}(window.jQuery)
+    
+})(window.Zepto);
