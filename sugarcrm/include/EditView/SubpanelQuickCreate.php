0379b903386f11d6be6d3056ad9b174cb5e3d067
Muddsar Jamil <mjamil@sugarcrm.com>
2012-03-28 14:56:59 -0700

    Revert "Merge pull request #2527 from RobertGonzalez/bug_51105"
    This reverts commit fab6cfa04087b875c9a8d25cc6d82d74839967df, reversing
    changes made to 3ee1a89e4c7962612a78a9d0a5a2833a8bf13bd5.

--- sugarcrm/include/EditView/SubpanelQuickCreate.php
+++ sugarcrm/include/EditView/SubpanelQuickCreate.php
@@ -26,30 +26,35 @@ require_once('include/EditView/EditView2.php');
  * @api
  */
 class SubpanelQuickCreate{
-	public $defaultProcess = true;
-
-    /**
-     * The view type to use
-     *
-     * @var string
-     */
-    public $viewType = 'QuickCreate';
+	var $defaultProcess = true;
 
     public function SubpanelQuickCreate($module, $view='QuickCreate', $proccessOverride = false)
     {
-        $this->viewType = $view;
         //treat quickedit and quickcreate views as the same
-        if($this->viewType == 'QuickEdit')
-        {
-            $this->viewType = 'QuickCreate';
-        }
+        if($view == 'QuickEdit') {$view = 'QuickCreate';}
 
-        // Get the viewdefs source file, called here to ensure proper viewType setting
-        $source = $this->getModuleViewDefsSourceFile($module, $this->viewType);
+		// locate the best viewdefs to use: 1. custom/module/quickcreatedefs.php 2. module/quickcreatedefs.php 3. custom/module/editviewdefs.php 4. module/editviewdefs.php
+		$base = 'modules/' . $module . '/metadata/';
+		$source = 'custom/' . $base . strtolower($view) . 'defs.php';
+		if (!file_exists( $source))
+		{
+			$source = $base . strtolower($view) . 'defs.php';
+			if (!file_exists($source))
+			{
+				//if our view does not exist default to EditView
+				$view = 'EditView';
+				$source = 'custom/' . $base . 'editviewdefs.php';
+				if (!file_exists($source))
+				{
+					$source = $base . 'editviewdefs.php';
+				}
+			}
+		}
 
         $this->ev = $this->getEditView();
-		$this->ev->view = $this->viewType;
+		$this->ev->view = $view;
 		$this->ev->ss = new Sugar_Smarty();
+		//$_REQUEST['return_action'] = 'SubPanelViewer';
 
         $class = $GLOBALS['beanList'][$module];
         $bean = new $class();
@@ -60,15 +65,8 @@ class SubpanelQuickCreate{
 		unset($bean);
 
 
-        // Bug 49219 - Check empty before set defaults, or the settings from viewdefs above will be overridden.
-        // Bug 51105 - For Subpanel quick creates with a relationship that is different from the return module
-        // we NEED to use the EditView/header.tpl and NOT the module EditViewHeader.tpl to enforce proper
-        // relate_to value passing
-		if (
-            // Bug 49219
-            !isset($this->ev->defs['templateMeta']['form']['headerTpl'])
-            // Bug 51105
-            || (!empty($_REQUEST['return_module']) && !empty($_REQUEST['return_relationship']) && $_REQUEST['return_module'] != $_REQUEST['return_relationship']))
+		// Bug 49219 - Check empty before set defaults, or the settings from viewdefs above will be overridden.
+		if (!isset($this->ev->defs['templateMeta']['form']['headerTpl']))
         {
             $this->ev->defs['templateMeta']['form']['headerTpl'] = 'include/EditView/header.tpl';
         }
@@ -142,37 +140,5 @@ class SubpanelQuickCreate{
     protected function getEditView()
     {
         return new EditView();
-    }
-
-    /**
-     * Finds and returns the best viewdefs to use:
-     *  1. custom/module/quickcreatedefs.php
-     *  2. module/quickcreatedefs.php
-     *  3. custom/module/editviewdefs.php
-     *  4. module/editviewdefs.php
-     *
-     * @param $module
-     * @param $view
-     * @return string The path to the viewdefs file to use
-     */
-    public function getModuleViewDefsSourceFile($module, $view) {
-        $base = 'modules/' . $module . '/metadata/';
-		$source = 'custom/' . $base . strtolower($view) . 'defs.php';
-		if (!file_exists( $source))
-		{
-			$source = $base . strtolower($view) . 'defs.php';
-			if (!file_exists($source))
-			{
-				//if our view does not exist default to EditView
-				$this->viewType = 'EditView';
-				$source = 'custom/' . $base . 'editviewdefs.php';
-				if (!file_exists($source))
-				{
-					$source = $base . 'editviewdefs.php';
-				}
-			}
-		}
-
-        return $source;
     }
 }
