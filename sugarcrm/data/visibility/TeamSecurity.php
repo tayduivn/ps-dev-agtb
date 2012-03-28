3a940f1df03537c19db2197e61d950ba92ede5e5
Stanislav Malyshev <smalyshev@gmail.com>
2012-03-28 16:04:40 -0700

    test fix

--- sugarcrm/data/visibility/TeamSecurity.php
+++ sugarcrm/data/visibility/TeamSecurity.php
@@ -34,6 +34,11 @@ class TeamSecurity extends SugarVisibility
             // We need to confirm that the user is a member of the team of the item.
 
             global $current_user;
+            if(empty($current_user)) {
+                $current_user_id = '';
+            } else {
+                $current_user_id = $current_user->id;
+            }
             // The user either has to be an admin, or be assigned to the team that owns the data
             $team_table_alias = 'team_memberships';
             $table_alias = $this->getOption('table_alias');
@@ -43,10 +48,10 @@ class TeamSecurity extends SugarVisibility
                 $table_alias = $this->bean->table_name;
             }
 
-            if ( !$current_user->isAdminForModule($this->module_dir) && $this->module_dir != 'WorkFlow') {
+            if ((empty($current_user) || !$current_user->isAdminForModule($this->module_dir)) && $this->module_dir != 'WorkFlow') {
                 $query .= "INNER JOIN (select tst.team_set_id from team_sets_teams tst";
                 $query .= " INNER JOIN team_memberships {$team_table_alias} ON tst.team_id = {$team_table_alias}.team_id
-                                    AND {$team_table_alias}.user_id = '$current_user->id'
+                                    AND {$team_table_alias}.user_id = '$current_user_id'
                                     AND {$team_table_alias}.deleted=0 group by tst.team_set_id) {$table_alias}_tf on {$table_alias}_tf.team_set_id  = {$table_alias}.team_set_id ";
             }
             if($this->getOption('join_teams')) {
