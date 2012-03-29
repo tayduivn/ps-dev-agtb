828edaafbc3ffe318058fbbf528f5bc358d07e21
Akshay Joshi <ajoshi@sugarcrm.com>
2012-03-29 15:21:29 -0700

    Fix for #51707: Some admin modules not shown with new UI.

--- sugarcrm/include/ListView/ListView.php
+++ sugarcrm/include/ListView/ListView.php
@@ -1491,8 +1491,6 @@ $close_inline_img = SugarThemeRegistry::current()->getImage('close_inline', 'bor
         global $app_strings, $sugar_version, $sugar_config;
         global $currentModule;
 
-
-
         $this->xTemplate->assign('BG_HILITE', $hilite_bg);
         $this->xTemplate->assign('CHECKALL', SugarThemeRegistry::current()->getImage('blank', '', 1, 1, ".gif", ''));
     //$this->xTemplate->assign("BG_CLICK", $click_bg);
@@ -1588,6 +1586,17 @@ $close_inline_img = SugarThemeRegistry::current()->getImage('close_inline', 'bor
                     if(trim($results['string']) == '') $results['string'] = $app_strings['LBL_NONE'];
                     $fields[$results['fieldToAddTo']] = $fields[$results['fieldToAddTo']].'</a>';
                 }
+
+                if($aItem->ACLAccess('Delete')) {
+                    $delete = '<a class="listViewTdToolsS1" onclick="return confirm(\''.$this->local_mod_strings['NTC_DELETE_CONFIRMATION'].'\')" href="'.'index.php?action=Delete&module='.$aItem->module_dir.'&record='.$fields['ID'].'&return_module='.$aItem->module_dir.'&return_action=index&return_id=">'.$this->local_app_strings['LBL_DELETE_INLINE'].'</a>';
+                    require_once('include/Smarty/plugins/function.sugar_action_menu.php');
+                    $fields['DELETE_BUTTON'] = smarty_function_sugar_action_menu(array(
+                        'id' => $aItem->module_dir.'_'.$fields['ID'].'_create_button',
+                        'buttons' => array($delete),
+                    ), $this);
+
+                }
+
                 $this->xTemplate->assign($html_varName, $fields);
                 $aItem->setupCustomFields($aItem->module_dir);
                 $aItem->custom_fields->populateAllXTPL($this->xTemplate, 'detail', $html_varName, $fields);
