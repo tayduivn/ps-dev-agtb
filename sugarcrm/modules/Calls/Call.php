3ad1472646d2c952bf8c2f531a562b2ed07d6891
Collin Lee <clee@sugarcrm.com>
2012-04-03 14:30:32 -0400

    Bug: 51078
    Revert code changes done for Bug 49385 so that we can sort again by Contacts in Calls and Meetings listview.  The changes for 49385 removed that capability because it collapsed the Calls and Meetings.

--- sugarcrm/modules/Calls/Call.php
+++ sugarcrm/modules/Calls/Call.php
@@ -27,10 +27,7 @@ if(!defined('sugarEntry') || !sugarEntry) die('Not A Valid Entry Point');
  * Contributor(s): ______________________________________..
  ********************************************************************************/
 
-// Call is used to store customer information.
-require_once('modules/Activities/Activity.php');
-class Call extends Activity
-{
+class Call extends SugarBean {
 	var $field_name_map;
 	// Stored fields
 	var $id;
@@ -449,16 +446,8 @@ class Call extends Activity
 		}
 
         $call_fields['CONTACT_ID'] = $this->contact_id;
-        //If we have a contact id and there are more than one contacts found for this meeting then let's create a hover link
-        if($this->alter_many_to_many_query && !empty($this->contact_id) && isset($this->secondary_select_count) && $this->secondary_select_count > 1)
-        {
-           $call_fields['CONTACT_NAME'] = $this->createManyToManyDetailHoverLink($this->contact_name, $this->contact_id);
-        } else {
-           $call_fields['CONTACT_NAME'] = $this->contact_name;
-        }
-
+        $call_fields['CONTACT_NAME'] = $this->contact_name;
 		$call_fields['PARENT_NAME'] = $this->parent_name;
-
         $call_fields['REMINDER_CHECKED'] = $this->reminder_time==-1 ? false : true;
 
 		return $call_fields;
