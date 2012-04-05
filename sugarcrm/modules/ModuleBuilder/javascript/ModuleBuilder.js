2874ac941cdb49379666e47351a32179adc8b653
Collin Lee <clee@sugarcrm.com>
2012-04-04 22:13:36 -0400

    Bug: 51982
    Revert fix from 50085.

--- sugarcrm/modules/ModuleBuilder/javascript/ModuleBuilder.js
+++ sugarcrm/modules/ModuleBuilder/javascript/ModuleBuilder.js
@@ -1002,7 +1002,7 @@ if (typeof(ModuleBuilder) == 'undefined') {
 		paramsToUrl : function (params) {
 			url = "";
 			for (i in params) {
-				url += escape(i) + "=" + escape(params[i]) + "&";
+                url += i + "=" + params[i] + "&";
 			}
 			return url;
 		},
