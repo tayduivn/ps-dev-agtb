4f33ea980b2a3cd805c68b0bbff23a7c70a0b6b0
Ryan Bacon <rbacon@sugarcrm.com>
2012-04-06 11:37:06 -0700

    updating to the new metadata location

--- sugarcrm/modules/Bugs/metadata/portal/views/edit.php
+++ sugarcrm/modules/Bugs/metadata/portal/views/edit.php
@@ -1,79 +1,87 @@
 <?php
-$viewdefs['Bugs']['portal']['view']['edit'] =
-    array(
-        'templateMeta' =>
-        array(
-            'maxColumns' => '2',
-            'widths' =>
-            array(
-                array(
-                    'label' => '10',
-                    'field' => '30',
-                ),
-                array(
-                    'label' => '10',
-                    'field' => '30',
-                ),
-            ),
-            'formId' => 'BugEditView',
-            'formName' => 'BugEditView',
-            'hiddenInputs' =>
-            array(
-                'module' => 'Bugs',
-                'returnmodule' => 'Bugs',
-                'returnaction' => 'DetailView',
-                'action' => 'Save',
-            ),
-            'hiddenFields' =>
-            array(
-                array(
-                    'name' => 'portal_viewable',
-                    'operator' => '=',
-                    'value' => '1',
-                ),
-            ),
-            'useTabs' => false,
+$viewdefs ['Bugs']['EditView'] =
+  array (
+    'templateMeta' => 
+    array (
+      'maxColumns' => '2',
+      'widths' => 
+      array (
+        array (
+          'label' => '10',
+          'field' => '30',
         ),
-        'panels' =>
-        array(
-            array(
-                'label' => 'default',
-                'fields' =>
-                array(
-                    'priority',
-                    'status',
-                    'source',
-                    'product_category',
-                    array(
-                        'name' => 'resolution',
-                        'displayParams' =>
-                        array(
-                            'colspan' => 2,
-                        ),
-                    ),
-                    array(
-                        'name' => 'name',
-                        'displayParams' =>
-                        array(
-                            'colspan' => 2,
-                        ),
-                    ),
-                    array(
-                        'name' => 'description',
-                        'displayParams' =>
-                        array(
-                            'colspan' => 2,
-                        ),
-                    ),
-                    array(
-                        'name' => 'work_log',
-                        'displayParams' =>
-                        array(
-                            'colspan' => 2,
-                        ),
-                    ),
-                ),
-            ),
+        array (
+          'label' => '10',
+          'field' => '30',
         ),
-    );
+      ),
+      'formId' => 'BugEditView',
+      'formName' => 'BugEditView',
+      'hiddenInputs' => 
+      array (
+        'module' => 'Bugs',
+        'returnmodule' => 'Bugs',
+        'returnaction' => 'DetailView',
+        'action' => 'Save',
+      ),
+      'hiddenFields' => 
+      array (
+        array (
+          'name' => 'portal_viewable',
+          'operator' => '=',
+          'value' => '1',
+        ),
+      ),
+      'useTabs' => false,
+    ),
+    'panels' => 
+    array (
+      array (
+        'label' => 'default',
+        'fields' => 
+        array (
+          array (
+            'name' => 'priority',
+          ),
+          array (
+            'name' => 'status',
+          ),
+          array (
+            'name' => 'source',
+          ),
+          array (
+            'name' => 'product_category',
+          ),
+          array (
+            'name' => 'resolution',
+            'displayParams' => 
+            array (
+              'colspan' => 2,
+            ),
+          ),
+          array (
+            'name' => 'name',
+            'displayParams' => 
+            array (
+              'colspan' => 2,
+            ),
+          ),
+          array (
+            'name' => 'description',
+            'displayParams' => 
+            array (
+              'colspan' => 2,
+            ),
+          ),
+          array (
+            'name' => 'work_log',
+            'displayParams' => 
+            array (
+              'colspan' => 2,
+            ),
+          ),
+        ),
+      ),
+    ),
+);
 ?>
