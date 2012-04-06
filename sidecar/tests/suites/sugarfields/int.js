874a3f249fcb83d59fb53e2cc67bd6651a1ece58
Julien Girault <jgirault@sugarcrm.com>
2012-04-06 16:52:24 -0700

    Improve tests for int, and fix a bug in the js code

--- sidecar/tests/suites/sugarfields/int.js
+++ sidecar/tests/suites/sugarfields/int.js
@@ -2,12 +2,21 @@ describe("sugarfields", function() {
 
     describe("integer", function() {
         it("should format the value", function() {
-            var fieldType = {sugarField:{type:'integer'}};
-            var field = SUGAR.App.metadata.get(fieldType);
-            field.controller = SugarTest.loadSugarField('int/int');
 
-            expect(field.controller.format("122.65678")).toEqual("123");
-            expect(field.controller.unformat("123.256")).toEqual("123");
+            app = SUGAR.App;
+            var field = app.sugarFieldManager.get({
+                def: {
+                    type: "integer",
+                    number_group_seperator: ","
+                },
+                view: "detail",
+                label: "",
+                model: { "Contacts": { fields: { }}}
+            });
+            var controller = SugarTest.loadSugarField('int/int');
+            field = _.extend(field, controller);
+            expect(field.format("123456.502")).toEqual("123,457");
+            expect(field.unformat("123456.498")).toEqual("123456");
         });
 
     });
--- sugarcrm/clients/base/fields/int/int.js
+++ sugarcrm/clients/base/fields/int/int.js
@@ -1,10 +1,10 @@
 ({
     unformat:function(value){
-        value = SUGAR.App.utils.formatNumber(value, 1, 0, "", ".");
+        value = SUGAR.App.utils.formatNumber(value, 0, 0, "", ".");
         return value
         },
     format:function(value){
-        value = SUGAR.App.utils.formatNumber(value, 1, 0, this.number_group_seperator, ".");
+        value = SUGAR.App.utils.formatNumber(value, 0, 0, this.number_group_seperator, ".");
         return value
     }
 })
