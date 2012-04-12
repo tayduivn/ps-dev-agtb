d91bad15843327432d2abd9baf39e228628e1867
David Tam <david.cheung.tam@gmail.com>
2012-04-11 17:48:05 -0700

    cleaning up

--- sidecar/src/view/views/header-view.js
+++ sidecar/src/view/views/header-view.js
@@ -7,8 +7,6 @@
      * @extends View.View
      */
     app.view.views.HeaderView = app.view.View.extend({
-        events: {
-        },
       render: function(){
             this.moduleList = this.moduleList || _.toArray(app.metadata.getModuleList());
            app.view.View.prototype.render.call(this);
