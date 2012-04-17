b649682107238eace84aade3c2d6af3d616700f9
Ryan Bacon <rbacon@sugarcrm.com>
2012-04-17 16:55:44 -0700

    now its sidecar baby

--- sugarcrm/tests/modules/ModuleBuilder/parsers/SidecarGridLayoutMetaDataParserTest.php
+++ sugarcrm/tests/modules/ModuleBuilder/parsers/SidecarGridLayoutMetaDataParserTest.php
@@ -48,11 +48,6 @@ class SidecarGridLayoutMetaDataParserTest extends Sugar_PHPUnit_Framework_TestCa
         SugarTestUserUtilities::removeAllCreatedAnonymousUsers();
     }
 
-    public function testdoWeHasAParser()
-    {
-        $this->assertInstanceOf('PortalGridLayoutMetaDataParser',$this->_parser);
-    }
-
     protected function getMockRequestArray() {
         return array(
             'PORTAL' => '1',
@@ -111,121 +106,6 @@ class SidecarGridLayoutMetaDataParserTest extends Sugar_PHPUnit_Framework_TestCa
             'view_module' => 'Leads',
         );
     }
-
-//
-//        // We use a derived class to aid in stubbing out test properties and functions
-//        $parser = new SearchViewMetaDataParserTestDerivative("basic_search");
-//
-//        // Creating a mock object for the DeployedMetaDataImplementation
-//
-//        $impl = $this->getMock('DeployedMetaDataImplementation',
-//                               array('getOriginalViewdefs'),
-//                               array(),
-//                               'DeployedMetaDataImplementation_Mock',
-//                               FALSE);
-//
-//        // Making the getOriginalViewdefs function return the test viewdefs and verify that it is being called once
-//        $impl->expects($this->once())
-//                ->method('getOriginalViewdefs')
-//                ->will($this->returnValue($orgViewDefs));
-//
-//        // Replace the protected implementation with our Mock object
-//        $parser->setImpl($impl);
-//
-//        // Execute the method under test
-//        $result = $parser->getOriginalViewDefs();
-//
-//        // Verify that the returned result matches our expectations
-//        $this->assertEquals($result, $expectedResult);
-//
-//        //echo "Done";
-//    }
-
-//    public function testdoWeHasAParser()
-//    {
-//        $parser = new PortalGridLayoutMetaDataParser(MB_PORTALEDITVIEW, 'Leads');
-//        $this->assertInstanceOf('PortalGridLayoutMetaDataParser',$parser);
-//    }
-
-//    public function testWTFDoesThisDo()
-//    {
-//        $postString = <<<EOL
-//PORTAL	1
-//action	saveLayout
-//module	ModuleBuilder
-//panel-1-label	0
-//panel-1-name	Default
-//panels_as_tabs
-//slot-1-0-label	Salutation
-//slot-1-0-name	salutation
-//slot-1-1-name	(empty)
-//slot-1-10-label	Email Opt Out:
-//slot-1-10-name	email_opt_out
-//slot-1-11-name	(filler)
-//slot-1-12-label	Title:
-//slot-1-12-name	title
-//slot-1-13-label	Department:
-//slot-1-13-name	department
-//slot-1-14-label	Account Name:
-//slot-1-14-name	account_name
-//slot-1-15-name	(empty)
-//slot-1-16-label	Primary Address Street
-//slot-1-16-name	primary_address_street
-//slot-1-17-name	(empty)
-//slot-1-18-label	Primary Address City
-//slot-1-18-name	primary_address_city
-//slot-1-19-label	Primary Address State
-//slot-1-19-name	primary_address_state
-//slot-1-2-label	First Name:
-//slot-1-2-name	first_name
-//slot-1-20-label	Primary Address Postalcode
-//slot-1-20-name	primary_address_postalcode
-//slot-1-21-label	Primary Address Country
-//slot-1-21-name	primary_address_country
-//slot-1-22-label	LBL_DATE_ENTERED
-//slot-1-22-name	date_entered
-//slot-1-23-label	LBL_DATE_MODIFIED
-//slot-1-23-name	date_modified
-//slot-1-3-label	Last Name:
-//slot-1-3-name	last_name
-//slot-1-4-label	Office Phone:
-//slot-1-4-name	phone_work
-//slot-1-5-label	Mobile:
-//slot-1-5-name	phone_mobile
-//slot-1-6-label	Home Phone:
-//slot-1-6-name	phone_home
-//slot-1-7-label	Do Not Call:
-//slot-1-7-name	do_not_call
-//slot-1-8-label	Email Address:
-//slot-1-8-name	email1
-//slot-1-9-label	Other Email:
-//slot-1-9-name	email2
-//sync_detail_and_edit
-//to_pdf	1
-//view	EditView
-//view_module	Leads
-//EOL;
-//
-////        $_POST = $_REQUEST = $this->createRequestFromString($postString);
-////        $oldParser = new ParserPortalLayoutView();
-////        $oldParser->init('Leads', 'EditView');
-//
-//print_r($this->_parser->getAvailableFields());
-//    echo '-----------------',PHP_EOL;
-//print_r($this->_parser->getFieldDefs());
-//    echo '-----------------',PHP_EOL;
-//print_r($this->_parser->getLayout());
-//    echo '-----------------',PHP_EOL;
-//print_r($this->_parser->getCalculatedFields());
-//    echo '-----------------',PHP_EOL;
-//print_r($this->_parser->getMaxColumns());
-////        $this->assertEquals($oldParser->getAvailableFields(), $this->_parser->getAvailableFields());
-////        $this->assertEquals($oldParser->getFieldDefs(), $this->_parser->getFieldDefs());
-////        $this->assertEquals($oldParser->getLayout(), $this->_parser->getLayout());
-////        $this->assertEquals($oldParser->getCalculatedFields(), $this->_parser->getCalculatedFields());
-////        $this->assertEquals($oldParser->getMaxColumns(), $this->_parser->getMaxColumns());
-//    }
-
 
     /*
     * data provider for testing converting to and from canonical form
