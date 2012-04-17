e8d070df257aaa246edd93165e45f0f12d068044
Rob Levin <rlevin@sugarcrm.com>
2012-04-16 20:14:44 -0700

    fixes invalid test for roundTime et al

--- sidecar/tests/suites/fields/datetimecombo.js
+++ sidecar/tests/suites/fields/datetimecombo.js
@@ -7,14 +7,14 @@ describe("sugarfields", function() {
             var field = SugarFieldTest.createField("datetimecombo", "detail");
             field = _.extend(field, controller);
 
-            var unformatedValue = new Date(2012, 03, 09, 09, 50, 58);
+            var unformatedValue = new Date(2012, 3, 9, 9, 50, 58);
             var expectedValue =
             {
                 dateTime: unformatedValue,
                 date: '2012-04-09',
-                time: '10:50:58',
+                time: '10:00:58',
                 hours: '10',
-                minutes: '50',
+                minutes: '00',
                 seconds: '58',
                 amPm: 'am'
             };
