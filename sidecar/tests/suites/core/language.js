2f857f56f51259ecfa2f216241de0789bae2d83c
Rob Levin <rlevin@sugarcrm.com>
2012-04-19 14:58:26 -0700

    resolved conflict and updated two tests

--- sidecar/tests/suites/core/language.js
+++ sidecar/tests/suites/core/language.js
@@ -52,12 +52,38 @@ describe("Sugar App Language Manager", function() {
         var setData = fixtures.language.Accounts,
             string;
         var appStrings = fixtures.metadata.appStrings;
-
         lang.setAppStrings(appStrings);
 
         string = lang.get("THIS_LABEL_DOES_NOT_EXIST");
 
         expect(string).toEqual("THIS_LABEL_DOES_NOT_EXIST");
+    });
+    it("should retreive using translate given module and label name", function() {
+        var setData = fixtures.language.Accounts,
+            string;
+
+        lang.setLabel("Accounts", setData);
+        string = lang.translate("LBL_ANNUAL_REVENUE", "Accounts");
+
+        expect(string).toEqual("Annual Revenue");
+    });
+
+    it("should translate falling back to app string if label name but no module provided", function() {
+        var setData = fixtures.language.Accounts,
+            string;
+
+        lang.setLabel("Accounts", setData);
+        string = lang.translate("DEFAULT");
+        expect(string).toEqual(fixtures.metadata.appStrings['DEFAULT']);
+    });
+
+    it("should translate falling back to empty string if not found", function() {
+        var setData = fixtures.language.Accounts,
+            string;
+
+        lang.setLabel("Accounts", setData);
+        string = lang.translate("BOGUS");
+        expect(string).toEqual("BOGUS");
     });
 
     it("should save app list strings to the language cache and app cache", function() {
@@ -76,4 +102,18 @@ describe("Sugar App Language Manager", function() {
         expect(lang.appStrings).toEqual(appStrings);
         expect(appCache.get("language:appStrings")).toEqual(fixtures.metadata.appStrings);
     });
+
+    it("should get app strings from app cache", function() {
+        var appStrings = fixtures.metadata.appStrings;
+
+        lang.setAppStrings(appStrings);
+        expect(lang.getAppStrings('DEFAULT')).toEqual(fixtures.metadata.appStrings['DEFAULT']);
+    });
+
+    it("should return false if can't find app strings from key", function() {
+        var appStrings = fixtures.metadata.appStrings;
+        lang.setAppStrings(appStrings);
+        expect(lang.getAppStrings('BOGUS')).toEqual(false);
+    });
 });
+
