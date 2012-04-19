4aa923d42923da324f545e1e42ed71cea1539064
Rob Levin <rlevin@sugarcrm.com>
2012-04-19 14:04:12 -0700

    possibly a typo but this needed to be in the it("should…form otherwise it gets run before our spec-helper!

--- sidecar/tests/suites/view/views/header-view.js
+++ sidecar/tests/suites/view/views/header-view.js
@@ -1,37 +1,33 @@
 describe("headerView", function() {
-    describe("should set current module", function() {
-            var options = {
-                context: {get: function() {
-                    return 'cases'
-                }},
-                id: "1",
-                template: function() {
-                    return 'asdf'
-                }
-            };
-            var view = new SUGAR.App.view.views.HeaderView(options);
-            view.render();
-            expect(view.currentModule).toEqual('cases');
-        }
-    ),
-        describe("should set the current module list", function() {
-                var result = fixtures.metadata.moduleList;
-                delete result._hash;
-                var options = {
-                    context: {get: function() {
-                        return 'cases'
-                    }},
-                    id: "1",
-                    template: function() {
-                        return 'asdf'
-                    }
-                };
-
-
-                var view = new SUGAR.App.view.views.HeaderView(options);
-                view.render();
-
-                expect(view.moduleList).toEqual(_.toArray(result));
+    it("should set current module", function() {
+        var options = {
+            context: {get: function() {
+                return 'cases'
+            }},
+            id: "1",
+            template: function() {
+                return 'asdf'
             }
-        )
+        };
+        var view = new SUGAR.App.view.views.HeaderView(options);
+        view.render();
+        expect(view.currentModule).toEqual('cases');
+    });
+    it("should set the current module list", function() {
+        var result = fixtures.metadata.moduleList;
+        delete result._hash;
+        var options = {
+            context: {get: function() {
+                return 'cases'
+            }},
+            id: "1",
+            template: function() {
+                return 'asdf'
+            }
+        };
+
+        var view = new SUGAR.App.view.views.HeaderView(options);
+        view.render();
+        expect(view.moduleList).toEqual(_.toArray(result));
+    });
 });
