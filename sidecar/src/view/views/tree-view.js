4c22d081cf578daea1c4b7e1c12ff10d576e1d48
Jon Whitcraft <jwhitcraft@sugarcrm.com>
2012-04-25 07:53:18 -0400

    Tree View - Load Users from Sugar to Populate tree dynamicly
    - This also has a simple fix for the sugarapi.js to make it work with the new relateAPI on the sugar side.

--- sidecar/src/view/views/tree-view.js
+++ sidecar/src/view/views/tree-view.js
@@ -8,28 +8,115 @@
      */
     app.view.views.TreeView = app.view.View.extend({
 
+
+        rendered : false,
+
+        primary_user : '',
+
+        reportees : '',
+
+        /**
+         * Start the rendering of the JS Tree
+         */
         render : function (){
+
+            // only let this render once.  since if there is more than one view on a layout it renders twice
+            if(this.rendered) return;
+
             app.view.View.prototype.render.call(this);
-            $("#demo1")
+
+            this.primary_user = SUGAR.App.data.createBean('Users', { id: 'seed_jim_id'});
+            this.primary_user.on('change', this.postUserFetch, this);
+            this.primary_user.fetch({ fields: ['full_name', 'id', 'first_name', 'last_name'] });
+
+            this.rendered = true;
+        },
+
+        /**
+         * This is the success call for when it fetches the primary user from Sugar
+         *
+         * @param {Data.Bean} model
+         * @param response
+         */
+        postUserFetch : function(model, response) {
+            // load up the report to users
+            this.reportees = app.data.createRelatedCollection(this.primary_user, "reportees");
+            this.reportees.on('app:collection:fetch', this.postReporteesFetch, this);
+            this.reportees.fetch({relate: true});
+        },
+
+        /**
+         * Fetch handler event if the reportees collection changes
+         *
+         * @param {Data.BeanCollection} collection
+         * @param response
+         */
+        postReporteesFetch : function(collection, response)
+        {
+            // success here, lets format the tree
+            this.renderTree();
+        },
+
+        /**
+         * Loop though all the Reportess and make a JSON array for the jsTree to consume.
+         *
+         * @param {String} parent_id
+         * @return {Array}
+         */
+        findChildren : function(parent_id)
+        {
+            var children = [];
+
+            _.each(this.reportees.models, function(reportee) {
+                if(reportee.get('reports_to_id') == parent_id) {
+                    var child = {
+                        "data" : reportee.get('full_name')
+                    };
+
+                    // check for children
+                    var _reportee_children = this.findChildren(reportee.get('id'));
+
+                    if(_reportee_children.length > 0) {
+                        child.children = _reportee_children;
+                    }
+
+                    children.push(child);
+                }
+            }, this);
+
+            return children;
+        },
+
+        /**
+         * Render the JS Tree
+         *
+         * @private
+         */
+        renderTree : function()
+        {
+            var tree_data = { "data" : [
+                {
+                    "data" : this.primary_user.get('full_name'),
+                    "children" : this.findChildren(this.primary_user.get('id')),
+                    "state" : "open"
+                }
+            ] };
+
+            $("#jsTree")
             // call `.jstree` with the options object
             .jstree({
                 // the `plugins` array allows you to configure the active plugins on this instance
-                "plugins" : ["themes","html_data","ui","crrm"],
+                "plugins" : ["themes","json_data","ui","crrm"],
                 "themes" : {
                             "theme" : "classic",
                             "dots" : true,
                             "icons" : false
                         },
                 // each plugin you have included can have its own config object
-                "core" : { "initially_open" : [ "phtml_1" ] }
+                "json_data" : tree_data
                 // it makes sense to configure a plugin only if overriding the defaults
             });
-        },
-
-        getFields: function() {
-            return ["name"];
         }
-
     });
 
 })(SUGAR.App);
