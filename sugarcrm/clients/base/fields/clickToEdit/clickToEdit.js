ad8662e9dcebcc6bf6fe5ac75b080a7cc17cd3c1
Travis Hubbard <thubbard@sugarcrm.com>
2012-04-25 16:04:43 -0400

    sets model via .editable function not onDomChange

--- sugarcrm/clients/base/fields/clickToEdit/clickToEdit.js
+++ sugarcrm/clients/base/fields/clickToEdit/clickToEdit.js
@@ -1,19 +1,15 @@
 ({
     render:function() {
+
         this.app.view.Field.prototype.render.call(this);
         var self = this;
         var ctefield = this.$el.find('.' + this.cteclass);
+
         ctefield.editable(function(value, settings) {
+                self.model.set( self.name , value );
                 return value;
             }
         );
         return this;
-    },
-
-    bindDomChange: function(model, fieldName) {
-        var thing = this.$el.find('.' + this.cteclass);
-        thing.on('change', function(ev) {
-            model.set(fieldName, ev.target.value);
-        });
     }
 })
