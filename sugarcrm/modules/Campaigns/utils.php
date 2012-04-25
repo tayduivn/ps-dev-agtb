97c4721849de6b17e7bda188784b9ca04ee58965
Marko Knezevic <mknezevic@sugarcrm.com>
2012-04-25 02:06:03 +0200

    bug52155 fix formating...

--- sugarcrm/modules/Campaigns/utils.php
+++ sugarcrm/modules/Campaigns/utils.php
@@ -896,31 +896,31 @@ function write_mail_merge_log_entry($campaign_id,$pl_row) {
 		$focus->db->query($delete_query);
 
 		$query="SELECT prospect_lists.id prospect_list_id, plp.related_id related_id, plp.related_type related_type from prospect_lists ";
-        $query.=" INNER JOIN prospect_lists_prospects plp ON plp.prospect_list_id = prospect_lists.id";
-        $query.=" INNER JOIN prospect_list_campaigns plc ON plc.prospect_list_id = prospect_lists.id";
+		$query.=" INNER JOIN prospect_lists_prospects plp ON plp.prospect_list_id = prospect_lists.id";
+		$query.=" INNER JOIN prospect_list_campaigns plc ON plc.prospect_list_id = prospect_lists.id";
 		$query.=" WHERE plc.campaign_id='".$GLOBALS['db']->quote($focus->id)."'"; 
 		$query.=" AND prospect_lists.deleted=0";
 		$query.=" AND plc.deleted=0";
-        $query.=" AND plp.deleted=0";
+		$query.=" AND plp.deleted=0";
 		$query.=" AND prospect_lists.list_type!='test' AND prospect_lists.list_type not like 'exempt%'";
 		$result=$focus->db->query($query);
 		while (($row=$focus->db->fetchByAssoc($result))!=null ) {
 			$prospect_list_id=$row['prospect_list_id'];
-            $related_id=$row['related_id'];
-            $related_type=$row['related_type'];
-            $guid = create_guid();
-            $current_date = $focus->db->now();
+			$related_id=$row['related_id'];
+			$related_type=$row['related_type'];
+			$guid = create_guid();
+			$current_date = $focus->db->now();
 
 			$insert_query= "INSERT INTO campaign_log (id,activity_date, campaign_id, target_tracker_key,list_id, target_id, target_type, activity_type";
 			$insert_query.=')';
-            $insert_query.=" VALUES ('{$guid}', $current_date, '{$focus->id}', '{$guid}', '{$prospect_list_id}', '{$related_id}', '{$related_type}', 'targeted' )";
+			$insert_query.=" VALUES ('{$guid}', $current_date, '{$focus->id}', '{$guid}', '{$prospect_list_id}', '{$related_id}', '{$related_type}', 'targeted' )";
 
-            $focus->db->query($insert_query);
+			$focus->db->query($insert_query);
 		}
-        global $mod_strings;
-        //return success message
-        return $mod_strings['LBL_DEFAULT_LIST_ENTRIES_WERE_PROCESSED'];
-    }
+		global $mod_strings;
+		//return success message
+		return $mod_strings['LBL_DEFAULT_LIST_ENTRIES_WERE_PROCESSED'];
+	}
 
     function create_campaign_log_entry($campaign_id, $focus, $rel_name, $rel_bean, $target_id = ''){
         global $timedate;
