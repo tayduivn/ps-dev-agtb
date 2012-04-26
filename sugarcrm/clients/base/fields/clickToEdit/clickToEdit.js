06a3571169ba8f21f0a901f405edb6ba54231070
Gabriel Rael <grael@sugarcrm.com>
2012-04-26 11:00:18 -0400

    work towards adding pencil icon on hover for clickToEdit

--- sugarcrm/clients/base/fields/clickToEdit/clickToEdit.js
+++ sugarcrm/clients/base/fields/clickToEdit/clickToEdit.js
@@ -1,16 +1,27 @@
 ({
-    render:function() {
+    events: {
+        'mouseenter': 'showActions',
+        'mouseleave': 'hideActions'
+    },
 
+    ctefield: {},
+    cteicon: {},
+
+    render:function() {
         this.app.view.Field.prototype.render.call(this);
         var self = this;
-        var ctefield = this.$el.find('.' + this.cteclass);
-
-        ctefield.editable(function(value, settings) {
+        this.ctefield = this.$el.find('.cte' + this.cteclass);
+        this.ctefield.editable(function(value, settings) {
                 self.model.set(self.name,value);
                 self.model.save(self.name,value);
                 return value;
             }
         );
+
+        this.cteicon = this.ctefield.parent().find('.cteimage');
+        _.each(this.events, function(act, event){
+            this.ctefield.on(event, this.act);
+        }, this);
         return this;
     },
     
@@ -20,5 +31,14 @@
      * @param model
      * @param fieldName
      */
-    bindDomChange: function(model, fieldName) {}
+    bindDomChange: function(model, fieldName) {},
+
+    showActions: function (e) {
+        this.cteicon.show();
+    },
+
+    hideActions: function(e) {
+        this.cteicon.hide();
+    },
+
 })
