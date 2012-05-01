91f4fefbdfd83baf70cef6558bc39451e40e7bb8
Lam Huynh <lam@sugarcrm.com>
2012-05-01 00:31:18 -0500

    forecast chart mockups

--- sugarcrm/include/SugarCharts/Jit/js/sugarCharts.js
+++ sugarcrm/include/SugarCharts/Jit/js/sugarCharts.js
@@ -28,7 +28,7 @@
 // $Id: customSugarCharts.js 2010-12-01 23:11:36Z lhuynh $
 
 function loadSugarChart (chartId,jsonFilename,css,chartConfig) {
-
+    this.chartObject = "";
                 //Bug#45831
                 if(document.getElementById(chartId) == null) {
                     return false;
@@ -48,184 +48,162 @@ function loadSugarChart (chartId,jsonFilename,css,chartConfig) {
 				})();
 
 			var delay = 500;
+            var that = this;
+
 			switch(chartConfig["chartType"]) {
 			case "barChart":
-				var handleFailure = function(o){
-				alert('fail');
-					if(o.responseText !== undefined){
-						alert('failed');
-					}
-				}	
-				var handleSuccess = function(o){
+                var request = jQuery.ajax({
+                    url: jsonFilename + "?r=" + new Date().getTime(),
+                    dataType:"text",
+                    async: false,
+                    success: function(data) {
+                        if(data !== undefined && data != "No Data"){
+                            var json = eval('('+data+')');
 
-					if(o.responseText !== undefined && o.responseText != "No Data"){	
-					var json = eval('('+o.responseText+')');
+                            var properties = $jit.util.splat(json.properties)[0];
+                            var marginBottom = (chartConfig["orientation"] == 'vertical' && json.values.length > 8) ? 20*4 : 20;
+                            //init BarChart
+                            var barChart = new $jit.BarChart({
+                                //id of the visualization container
+                                injectInto: chartId,
+                                //whether to add animations
+                                animate: false,
+                                nodeCount: json.values.length,
+                                renderBackground: chartConfig['imageExportType'] == "jpg" ? true: false,
+                                backgroundColor: 'rgb(255,255,255)',
+                                colorStop1: 'rgba(255,255,255,.8)',
+                                colorStop2: 'rgba(255,255,255,0)',
+                                shadow: {
+                                    enable: false,
+                                    size: 2
+                                },
+                                //horizontal or vertical barcharts
+                                orientation: chartConfig["orientation"],
+                                hoveredColor: false,
+                                Title: {
+                                    text: properties['title'],
+                                    size: 16,
+                                    color: '#444444',
+                                    offset: 20
+                                },
+                                Subtitle: {
+                                    text: properties['subtitle'],
+                                    size: 11,
+                                    color: css["color"],
+                                    offset: 20
+                                },
+                                Ticks: {
+                                    enable: true,
+                                    color: css["gridLineColor"]
+                                },
+                                //bars separation
+                                barsOffset: (chartConfig["orientation"] == "vertical") ? 30 : 20,
+                                //visualization offset
+                                Margin: {
+                                    top:20,
+                                    left: 30,
+                                    right: 20,
+                                    bottom: marginBottom
+                                },
+                                ScrollNote: {
+                                    text: (chartConfig["scroll"] && SUGAR.util.isTouchScreen()) ? "Use two fingers to scroll" : "",
+                                    size: 12
+                                },
+                                Events: {
+                                    enable: true,
+                                    onClick: function(node) {
+                                        if(!node || SUGAR.util.isTouchScreen()) return;
+                                        if(node.link == undefined || node.link == '') return;
+                                        window.location.href=node.link;
+                                    }
+                                },
+                                //labels offset position
+                                labelOffset: 5,
+                                //bars style
+                                type: useGradients? chartConfig["barType"]+':gradient' : chartConfig["barType"],
+                                //whether to show the aggregation of the values
+                                showAggregates: (chartConfig["showAggregates"] != undefined) ? chartConfig["showAggregates"] : true,
+                                showNodeLabels: (chartConfig["showNodeLabels"] != undefined) ? chartConfig["showNodeLabels"] : true,
+                                //whether to show the labels for the bars
+                                showLabels:true,
+                                //labels style
+                                Label: {
+                                    type: labelType, //Native or HTML
+                                    size: 12,
+                                    family: css["font-family"],
+                                    color: css["color"],
+                                    colorAlt: "#ffffff"
+                                },
+                                //add tooltips
+                                Tips: {
+                                    enable: true,
+                                    onShow: function(tip, elem) {
 
-				var properties = $jit.util.splat(json.properties)[0];	
-				var marginBottom = (chartConfig["orientation"] == 'vertical' && json.values.length > 8) ? 20*4 : 20;
-				//init BarChart
-				var barChart = new $jit.BarChart({
-				  //id of the visualization container
-				  injectInto: chartId,
-				  //whether to add animations
-				  animate: false,
-				  nodeCount: json.values.length,
-				  renderBackground: chartConfig['imageExportType'] == "jpg" ? true: false,
-				  backgroundColor: 'rgb(255,255,255)',
-				  colorStop1: 'rgba(255,255,255,.8)',
-				  colorStop2: 'rgba(255,255,255,0)',
-				  shadow: {
-				     enable: false,
-				     size: 2	
-				  },
-				  //horizontal or vertical barcharts
-				  orientation: chartConfig["orientation"],
-				  hoveredColor: false,
-				  Title: {
-					text: properties['title'],
-					size: 16,
-					color: '#444444',
-					offset: 20
-				  },
-				  Subtitle: {
-					text: properties['subtitle'],
-					size: 11,
-					color: css["color"],
-					offset: 20
-				  },
-				  Ticks: {
-					enable: true,
-					color: css["gridLineColor"]
-				  },
-				  //bars separation
-				  barsOffset: (chartConfig["orientation"] == "vertical") ? 30 : 20,
-				  //visualization offset
-				  Margin: {
-					top:20,
-					left: 30,
-					right: 20,
-					bottom: marginBottom
-				  },
-				  ScrollNote: {
-				  	text: (chartConfig["scroll"] && SUGAR.util.isTouchScreen()) ? "Use two fingers to scroll" : "",
-				  	size: 12
-				  },
-				  Events: {
-					enable: true,
-					onClick: function(node) {  
-					if(!node || SUGAR.util.isTouchScreen()) return;
-					if(node.link == undefined || node.link == '') return;
-					window.location.href=node.link;
-					}
-				  },
-				  //labels offset position
-				  labelOffset: 5,
-				  //bars style
-				  type: useGradients? chartConfig["barType"]+':gradient' : chartConfig["barType"],
-				  //whether to show the aggregation of the values
-				  showAggregates: (chartConfig["showAggregates"] != undefined) ? chartConfig["showAggregates"] : true,
-				  showNodeLabels: (chartConfig["showNodeLabels"] != undefined) ? chartConfig["showNodeLabels"] : true,
-				  //whether to show the labels for the bars
-				  showLabels:true,
-				  //labels style
-				  Label: {
-					type: labelType, //Native or HTML
-					size: 12,
-					family: css["font-family"],
-					color: css["color"],
-					colorAlt: "#ffffff"
-				  },
-				  //add tooltips
-				  Tips: {
-					enable: true,
-					onShow: function(tip, elem) {
-						
-						if(elem.type == 'marker') {
-							  tip.innerHTML = '<b>' + elem.name + '</b>: ' + elem.valuelabel ;
-						} else {
-							  if(elem.link != 'undefined' && elem.link != '') {
-								drillDown = (SUGAR.util.isTouchScreen()) ? "<br><a href='"+ elem.link +"'>Click to drilldown</a>" : "<br>Click to drilldown";
-							  } else {
-								drillDown = "";
-							  }
-		
-							  if(elem.valuelabel != 'undefined' && elem.valuelabel != undefined && elem.valuelabel != '') {
-								value = "elem.valuelabel";
-							  } else {
-								value = "elem.value";
-							  }
-							  eval("tip.innerHTML = 'Sales Stage: <b>' + elem."+chartConfig["tip"]+" + '</b><br> Amount: <b>' + "+value+" + '</b> <br> Percentage: <b>' + elem.percentage + '</b>' + drillDown");
-						}
-					}
-				  }
-				});
-				//load JSON data.
-				barChart.loadJSON(json);
-				
+                                        if(elem.type == 'marker') {
+                                            tip.innerHTML = '<b>' + elem.name + '</b>: ' + elem.valuelabel ;
+                                        } else {
+                                            if(elem.link != 'undefined' && elem.link != '') {
+                                                drillDown = (SUGAR.util.isTouchScreen()) ? "<br><a href='"+ elem.link +"'>Click to drilldown</a>" : "<br>Click to drilldown";
+                                            } else {
+                                                drillDown = "";
+                                            }
 
-				//end
-				
-				/*
-				var list = $jit.id('id-list'),
-					button = $jit.id('update'),
-					orn = $jit.id('switch-orientation');
-				//update json on click 'Update Data'
-				$jit.util.addEvent(button, 'click', function() {
-				  var util = $jit.util;
-				  if(util.hasClass(button, 'gray')) return;
-				  util.removeClass(button, 'white');
-				  util.addClass(button, 'gray');
-				  barChart.updateJSON(json2);
-				});
-				*/
-				//dynamically add legend to list
-				var list = $jit.id('legend'+chartId);
-				var legend = barChart.getLegend(),
-					cols = (typeof SUGAR == 'undefined' || typeof SUGAR.mySugar == 'undefined') ? 8 : 4,
-					rows = Math.ceil(legend["name"].length/cols),
-					table = "<table cellpadding='0' cellspacing='0' align='left'>";
-				var j = 0;
-				for(i=0;i<rows;i++) {
-					table += "<tr>"; 
-					for(td=0;td<cols;td++) {
-						
-						table += '<td width=\'16\' valign=\'top\'>';
-						if(legend["name"][j] != undefined) {
-							table += '<div class=\'query-color\' style=\'background-color:'
-							  + legend["color"][j] +'\'>&nbsp;</div>';
-						}
-						  
-						table += '</td>';
-						table += '<td class=\'label\' valign=\'top\'>';
-						if(legend["name"][j] != undefined) {
-							table += legend["name"][j];
-						}
-						  
-						table += '</td>';
-						j++;
-						}
-					table += "</tr>"; 
-				}
-				
-					table += "</table>";
-				list.innerHTML = table;
-				
-				
-				//save canvas to image for pdf consumption
-				$jit.util.saveImageTest(chartId,jsonFilename,chartConfig["imageExportType"]);
-		    	
-                trackWindowResize(barChart, chartId, json);
-					}
-				}
-				
-				var callback =
-				{
-				  success:handleSuccess,
-				  failure:handleFailure,
-				  argument: { foo:'foo', bar:''}
-				};
-				
-				var request = YAHOO.util.Connect.asyncRequest('GET', jsonFilename + "?r=" + new Date().getTime(), callback);
+                                            if(elem.valuelabel != 'undefined' && elem.valuelabel != undefined && elem.valuelabel != '') {
+                                                value = "elem.valuelabel";
+                                            } else {
+                                                value = "elem.value";
+                                            }
+                                            eval("tip.innerHTML = 'Sales Stage: <b>' + elem."+chartConfig["tip"]+" + '</b><br> Amount: <b>' + "+value+" + '</b> <br> Percentage: <b>' + elem.percentage + '</b>' + drillDown");
+                                        }
+                                    }
+                                }
+                            });
+                            //load JSON data.
+                            barChart.loadJSON(json);
+
+                            //dynamically add legend to list
+                            var list = $jit.id('legend'+chartId);
+                            var legend = barChart.getLegend(),
+                                cols = (typeof SUGAR == 'undefined' || typeof SUGAR.mySugar == 'undefined') ? 8 : 4,
+                                rows = Math.ceil(legend["name"].length/cols),
+                                table = "<table cellpadding='0' cellspacing='0' align='left'>";
+                            var j = 0;
+                            for(i=0;i<rows;i++) {
+                                table += "<tr>";
+                                for(td=0;td<cols;td++) {
+
+                                    table += '<td width=\'16\' valign=\'top\'>';
+                                    if(legend["name"][j] != undefined) {
+                                        table += '<div class=\'query-color\' style=\'background-color:'
+                                            + legend["color"][j] +'\'>&nbsp;</div>';
+                                    }
+
+                                    table += '</td>';
+                                    table += '<td class=\'label\' valign=\'top\'>';
+                                    if(legend["name"][j] != undefined) {
+                                        table += legend["name"][j];
+                                    }
+
+                                    table += '</td>';
+                                    j++;
+                                }
+                                table += "</tr>";
+                            }
+
+                            table += "</table>";
+                            list.innerHTML = table;
+
+
+                            //save canvas to image for pdf consumption
+                            $jit.util.saveImageTest(chartId,jsonFilename,chartConfig["imageExportType"]);
+
+                            trackWindowResize(barChart, chartId, json);
+                            that.chartObject = barChart;
+
+                    }
+                }
+                });
+
 				break;
 				
 			case "lineChart":
@@ -887,3 +865,30 @@ function loadSugarChart (chartId,jsonFilename,css,chartConfig) {
                 });
 			}
 		}
+
+function updateChart(jsonFilename,chart) {
+
+
+    jQuery.ajax({
+        url: jsonFilename + "?r=" + new Date().getTime(),
+        dataType:"text",
+        async: false,
+        success: function(data) {
+            if(data !== undefined && data != "No Data"){
+                var json = eval('('+data+')');
+                chart.updateJSON(json);
+            }
+        }
+    });
+
+
+}
+
+function swapChart(chartId,jsonFilename,css,chartConfig){
+    $("#"+chartId).empty();
+    $("#legend"+chartId).empty();
+    $("#tiptip_holder").empty();
+    var chart = new loadSugarChart(chartId,jsonFilename,css,chartConfig);
+    return chart;
+
+}
--- sugarcrm/jssource/JSGroupings.php
+++ sugarcrm/jssource/JSGroupings.php
@@ -63,7 +63,7 @@
 			'include/javascript/jquery/jquery-ui-min.js'          => 'include/javascript/sugar_grp1_jquery.js',
 			'include/javascript/jquery/jquery.json-2.3.js'        => 'include/javascript/sugar_grp1_jquery.js',
 			//bootstrap
-            //'include/javascript/jquery/bootstrap/bootstrap.min.js'              => 'include/javascript/sugar_grp1_jquery.js',			
+            'include/javascript/jquery/bootstrap/bootstrap.min.js'              => 'include/javascript/sugar_grp1_jquery.js',			
             //jquery for moddule menus
             'include/javascript/jquery/jquery.hoverIntent.js'              => 'include/javascript/sugar_grp1_jquery.js',
             'include/javascript/jquery/jquery.hoverscroll.js'              => 'include/javascript/sugar_grp1_jquery.js',
@@ -73,7 +73,9 @@
             'include/javascript/jquery/jquery.sugarMenu.js'              => 'include/javascript/sugar_grp1_jquery.js',
             'include/javascript/jquery/jquery.highLight.js'              => 'include/javascript/sugar_grp1_jquery.js',
             'include/javascript/jquery/jquery.showLoading.js'              => 'include/javascript/sugar_grp1_jquery.js',
-            
+            'include/javascript/jquery/jquery.dataTables.min.js'              => 'include/javascript/sugar_grp1_jquery.js',
+            'include/javascript/jquery/jquery.jeditable.js'              => 'include/javascript/sugar_grp1_jquery.js',
+
             
 			),
            $sugar_field_grp = array(
