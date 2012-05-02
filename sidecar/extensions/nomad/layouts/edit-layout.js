db468647529247594457efcd1f57faa078fccb52
Greg Khanlarov <gkhanlarov@sugarcrm.com>
2012-05-02 14:36:38 -0700

    Stubbed out detail and edit view

--- sidecar/extensions/nomad/layouts/edit-layout.js
+++ sidecar/extensions/nomad/layouts/edit-layout.js
@@ -1,3 +1,21 @@
 (function(app) {
 
+    var _meta = {
+        type: "edit",
+        components: [
+            { view: "edit" }
+        ]
+    };
+
+    app.view.layouts.EditLayout = app.view.Layout.extend({
+
+        initialize: function(options) {
+
+            // We don't have base metadata for layouts in metadata payload
+            // Set it here for now
+            this.options.meta = _meta;
+            app.view.Layout.prototype.initialize.call(this, options);
+        }
+    });
+
 })(SUGAR.App);
---
+++ sidecar/extensions/nomad/views/left-menu.js
@@ -0,0 +1,7 @@
+(function(app) {
+
+    app.view.views.LeftMenuView = app.view.View.extend({
+
+    });
+
+})(SUGAR.App);
