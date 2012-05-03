eacfe881003930f68b52deccce24d6b1d5835cba
Rob Levin <rlevin@sugarcrm.com>
2012-05-03 13:02:04 -0700

    Fixes 28302663 and 28969205 singularize and acl protect create tasks dropdowns

--- sidecar/src/view/views/header-view.js
+++ sidecar/src/view/views/header-view.js
@@ -23,6 +23,7 @@
         render: function() {
             if (!app.api.isAuthenticated()) return;
             this.setModuleInfo();
+            this.setCreateTasksList();
             app.view.View.prototype.render.call(this);
         },
         hide: function() {
@@ -31,19 +32,34 @@
         show: function() {
             this.$el.show();
         },
+        setCreateTasksList: function() {
+            var self = this;
+            self.createListLabels = [];
+            try {
+                if(app.acl.hasAccess('edit', app.data.createBean('Bugs',{assigned_user_id: 'assignedUserId'}))) {
+                    self.createListLabels.push(app.lang.get('LBL_CREATE_BUG', 'Emails'));
+                }
+                if(app.acl.hasAccess('edit', app.data.createBean('Cases', {assigned_user_id: 'assignedUserId'}))) {
+                    self.createListLabels.push(app.lang.get('LBL_CREATE_CASE', 'Emails'));
+                }
+                if(app.acl.hasAccess('edit', app.data.createBean('Leads', {assigned_user_id: 'assignedUserId'}))) {
+                    self.createListLabels.push(app.lang.get('LBL_CREATE_LEAD', 'Emails'));
+                }
+                // At time of writing KBDocuments module is not supported so this will throw. I put it here so that bugs, cases,
+                // and leads still render properly. When available, Create KBDocument will be pushed into second to last indice. 
+                if(app.acl.hasAccess('edit', app.data.createBean('KBDocuments', {assigned_user_id: 'assignedUserId'}))) {
+                    self.createListLabels.splice([self.createListLabels.length - 1], 0, app.lang.getAppStrings('LBL_CREATE_KB_DOCUMENT'));
+                    
+                }
+            } catch(e) {
+                return;
+            }
+        },
         setModuleInfo: function() {
             var self = this;
             this.createListLabels = [];
             this.currentModule = this.context.get('module');
             this.moduleList = _.toArray(app.metadata.getModuleList());
-
-            // TODO - would be nice if this were more dynamic 
-            // Unfortunately, can't use: SUGAR.App.lang.getAppListStrings("moduleListSingular")
-            // because it does not return KBDocuments, and Bugs => Bug Tracker is unhelpful, etc.
-            this.createListLabels.push(SUGAR.App.lang.get('LBL_CREATE_BUG', 'Emails'));
-            this.createListLabels.push(SUGAR.App.lang.get('LBL_CREATE_CASE', 'Emails'));
-            this.createListLabels.push('Create KBDocument'); // TODO: When server provides this use instead
-            this.createListLabels.push(SUGAR.App.lang.get('LBL_CREATE_LEAD', 'Emails'));
         }
 
     });
