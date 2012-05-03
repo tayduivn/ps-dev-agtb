0ffe8f94f8fe9842c2eb141c87c0921b315c8c88
Greg Khanlarov <gkhanlarov@sugarcrm.com>
2012-05-03 14:18:41 -0700

    Refactored header view for mobile

---
+++ sidecar/extensions/nomad/views/header-view.js
@@ -0,0 +1,36 @@
+(function(app) {
+
+    app.view.views.HeaderView = app.view.View.extend({
+
+        initialize: function(options) {
+            app.view.View.prototype.initialize.call(this, options);
+
+            var self = this;
+            app.events.on("app:sync:complete", function() {
+                self.render();
+            });
+        },
+
+        render: function() {
+            if (!app.api.isAuthenticated()) {
+                this.$el.addClass("hide");
+            }
+            else {
+                this.$el.removeClass("hide");
+                this._renderLeftList();
+                this._renderRightList();
+                app.view.View.prototype.render.call(this);
+            }
+        },
+
+        _renderLeftList: function() {
+            // TODO: Implement
+        },
+
+        _renderRightList: function() {
+            // TODO: Implement
+        }
+
+    });
+
+})(SUGAR.App);
