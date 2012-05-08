16bf36d00dfba20045873eba9a94dc8526c035b6
Ruslan Golovach <r.golovach@intetics.com>
2012-05-08 16:02:19 +0300

    context menu

--- sidecar/extensions/nomad/views/list-view.js
+++ sidecar/extensions/nomad/views/list-view.js
@@ -2,12 +2,17 @@
 
     app.view.views.ListView = app.view.View.extend({
         events:{
-            'click  .show-more-button':'showMoreRecords'
+            'click  .show-more-button':'showMoreRecords',
+            'click  article .grip':'onClickGrip',
+            'swipeLeft article':'swipeLeftArticle',
+            'swipeRight article':'swipeRightArticle'
         },
         initialize: function(options) {
             // Mobile shows only the first two fields
             options.meta.panels[0].fields.length = 2;
             app.view.View.prototype.initialize.call(this, options);
+
+            this.activeArticle = null;
         },
 
         addOne: function(model) {
@@ -34,9 +39,26 @@
             }
         },
         showMoreRecords:function() {
-
            this.collection.paginate({page:this.collection.page,add:true});
+        },
+        onClickGrip:function (e) {
+            var grip = $(e.target);
+            var isActive = grip.hasClass('on');
+            grip.closest('article').trigger(isActive ? 'swipeRight' : 'swipeLeft');
+        },
+        swipeLeftArticle:function(e){
+            if (this.activeArticle) {
+                this.activeArticle.trigger('swipeRight');
+            }
 
+            this.activeArticle = $(e.target);
+            this.activeArticle.find('.grip').addClass('on');
+            this.activeArticle.find('[id^=listing-action] .actions').removeClass('hide').addClass('on');
+
+        },
+        swipeRightArticle:function(e){
+            this.activeArticle.find('.grip').removeClass('on');
+            this.activeArticle.find('[id^=listing-action] .actions').addClass('hide').removeClass('on');
         }
 
     });
