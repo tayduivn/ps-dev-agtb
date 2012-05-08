52da9bd27dcab60e538bb3e684df8d6c5bdae7b8
Jon Whitcraft <jwhitcraft@sugarcrm.com>
2012-05-08 10:02:46 -0400

    Charting API Update to handle the group by methods
    - Added DocBlocks

--- sugarcrm/include/SugarCharts/ReportBuilder.php
+++ sugarcrm/include/SugarCharts/ReportBuilder.php
@@ -3,6 +3,11 @@
 class ReportBuilder
 {
 
+    /**
+     * The Default Structure For A Report JSON
+     *
+     * @var array
+     */
     protected $defaultReport = array(
         'display_columns' => array(),
         'module' => '',
@@ -26,24 +31,51 @@ class ReportBuilder
 
     /**
      * An array of Created Bean
+     *
+     * @var array
      */
     protected $beans = array();
 
+    /**
+     * Mapping of Modules to Table Keys
+     *
+     * @var array
+     */
     protected $table_keys = array();
 
+    /**
+     * What's the default module
+     *
+     * @var string
+     */
     protected $self_module;
 
+    /**
+     * Class Constructor
+     *
+     * @param string $module    The Default Starting Module
+     */
     public function __construct($module)
     {
         $this->self_module = $module;
         $this->defaultReport['module'] = $module;
-        $this->addTable($this->self_module, 'self');
-
+        $this->addModule($this->self_module, 'self');
     }
 
-    public function addTable($module, $key = null)
+    /**
+     * Add module to the report
+     *
+     * @param string $module        The module we are adding
+     * @param null|string $key      The key for the module we are adding
+     * @return ReportBuilder
+     */
+    public function addModule($module, $key = null)
     {
         $bean = $this->getBean($module);
+
+        if (empty($key)) {
+            // we need to generate keys
+        }
 
         $this->table_keys[$module] = $key;
 
@@ -51,11 +83,20 @@ class ReportBuilder
             'value' => $bean->module_dir,
             'module' => $bean->module_dir,
             'label' => $bean->module_dir,
+            'parent' => '',
+            'children' => array(),
         );
 
         return $this;
     }
 
+    /**
+     * Add A Field To Group By
+     *
+     * @param string $field         Which field do we want to group by
+     * @param null|string $module   Which module the field belongs to
+     * @return ReportBuilder
+     */
     public function addGroupBy($field, $module = null)
     {
         if (empty($module)) {
@@ -79,6 +120,13 @@ class ReportBuilder
         return $this;
     }
 
+    /**
+     * Add a Column to the Summary Output
+     *
+     * @param string $field         Which field to add
+     * @param string $module        Which module does the field belong to
+     * @return ReportBuilder
+     */
     public function addSummaryColumn($field, $module)
     {
         if (!($module instanceof SugarBean)) {
@@ -104,19 +152,29 @@ class ReportBuilder
         return $this;
     }
 
+    /**
+     * Add A Count Column to the Summary
+     *
+     * @return ReportBuilder
+     */
     public function addSummaryCount()
     {
         $this->defaultReport['summary_columns'][] = array(
-                'name' => 'count',
-                'label' => 'Count',
-                'table_key' => 'self',
-                'group_function' => "count",
-                'field_type' => ''
-            );
+            'name' => 'count',
+            'label' => 'Count',
+            'table_key' => 'self',
+            'group_function' => "count",
+            'field_type' => ''
+        );
 
         return $this;
     }
 
+    /**
+     * Return the Data as a JSON String
+     *
+     * @return string
+     */
     public function toJson()
     {
         return json_encode($this->defaultReport);
