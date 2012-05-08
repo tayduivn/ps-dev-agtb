5ae06e200d92c3054ebfdf8f73b6f2f2c14444f0
Lam Huynh <lam@sugarcrm.com>
2012-05-08 01:34:52 -0500

    adding charts to mockups

---
+++ sugarcrm/styleguide/sugar7/forecast-json-month.js
@@ -0,0 +1,246 @@
+{
+	'properties': [
+
+	{
+
+		'gauge_target_list':'Array'
+,
+		'title':''
+,
+		'subtitle':''
+,
+		'type':'horizontal group by chart'
+,
+		'legend':'on'
+,
+		'labels':'value'
+,
+		'print':'on'
+,
+		'goal_marker_type':'group'
+,
+		'goal_marker_color':'#3FB300'
+,
+        'label_name':'Sales Stage'
+,
+        'value_name':'Amount'
+	}
+
+	],
+
+	'label': [
+
+		'Qualified'
+,
+		'Proposed'
+,
+		'Quotes'
+,
+        'Closed/Won'
+
+	],
+
+	'color': [
+
+		'#8c2b2b'
+,
+		'#468c2b'
+,
+		'#2b5d8c'
+,
+		'#cd5200'
+,
+		'#e6bf00'
+,
+		'#7f3acd'
+,
+		'#00a9b8'
+,
+		'#572323'
+,
+		'#004d00'
+,
+		'#000087'
+,
+		'#e48d30'
+,
+		'#9fba09'
+,
+		'#560066'
+,
+		'#009f92'
+,
+		'#b36262'
+,
+		'#38795c'
+,
+		'#3D3D99'
+,
+		'#99623d'
+,
+		'#998a3d'
+,
+		'#994e78'
+,
+		'#3d6899'
+,
+		'#CC0000'
+,
+		'#00CC00'
+,
+		'#0000CC'
+,
+		'#cc5200'
+,
+		'#ccaa00'
+,
+		'#6600cc'
+,
+		'#005fcc'
+
+	],
+
+	'values': [
+
+	{
+
+		'label': 'Jan',
+
+		'gvalue': '400',
+
+		'gvaluelabel': '400K',
+
+		'values': [
+			100
+,
+			200
+,
+			50
+,
+            50
+
+
+		],
+
+		'valuelabels': [
+			'100K'
+,
+			'200K'
+,
+			'50K'
+,
+            '50K'
+
+
+		],
+
+		'links': [
+			''
+,
+			''
+,
+			''
+
+
+		],
+		'goalmarkervalue' : 410,
+		'goalmarkervaluelabel' : '410K',
+		'goalmarkerlabel' : 'Quota'
+
+	}
+,
+	{
+
+		'label': 'Feb',
+
+		'gvalue': '590',
+
+		'gvaluelabel': '590K',
+
+		'values': [
+			120
+,
+			190
+,
+			240
+,
+            40
+
+
+		],
+
+		'valuelabels': [
+			'120K'
+,
+			'190K'
+,
+			'240K'
+,
+            '40K'
+
+		],
+
+		'links': [
+			''
+,
+			''
+,
+			''
+
+
+		],
+		'goalmarkervalue' : 410,
+		'goalmarkervaluelabel' : '410k',
+		'goalmarkerlabel' : 'Quota'
+
+	}
+,
+	{
+
+		'label': 'Mar',
+
+		'gvalue': '410',
+
+		'gvaluelabel': '410K',
+
+		'values': [
+			10
+,
+			100
+,
+			200
+,
+            100
+
+		],
+
+		'valuelabels': [
+			'10K'
+,
+			'100K'
+,
+			'200K'
+,
+            '100K'
+
+		],
+
+		'links': [
+			''
+,
+			''
+,
+			''
+
+
+		],
+		'goalmarkervalue' : 410,
+		'goalmarkervaluelabel' : '410K',
+		'goalmarkerlabel' : 'Quota'
+
+	}
+
+
+	]
+
+}
