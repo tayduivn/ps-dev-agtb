1f31b42a49dfb4d4170054feb2dfb50581758a93
Gabriel Rael <grael@sugarcrm.com>
2012-05-09 08:24:17 -0400

    Begin to clean up clickToEdit

--- sugarcrm/clients/base/fields/clickToEdit/clickToEdit.js
+++ sugarcrm/clients/base/fields/clickToEdit/clickToEdit.js
@@ -5,20 +5,20 @@
     },
 
     ctePath: "",
-    ctefield: {},
-    cteicon: {},
-    undoicon:{},
+    cteField: {},
+    cteIcon: {},
+    undoIcon:{},
+    undoValue: "",
 
     initialize: function(options) {
         this.app.view.Field.prototype.initialize.call(this, options);
-        this.ctePath = this.app.config.serverUrl + "/../../clients/base/fields/clickToEdit";
-    },
-
-    render:function() {
-        this.app.view.Field.prototype.render.call(this);
 
         var self = this;
 
+        this.ctePath = this.app.config.serverUrl + "/../../clients/base/fields/clickToEdit";
+
+        // temporarily add chosen clickToEdit type, here
+        // TODO:  make this happen more elegantly, and add more type
         $.editable.addInputType("chosen", {
             element: function(settings, original) {
                 var selEl = $('<select class="cteSelect">');
@@ -43,56 +43,35 @@
                 $("input", this).val($("select", this).filter(".cteSelect").val());
             }
         });
+    },
 
+    render:function() {
+        this.app.view.Field.prototype.render.call(this);
 
-        this.ctefield = this.$el.find('.cte' + this.cteclass);
-        this.cteicon = this.ctefield.parent().find('.cteimage');
-        this.undoicon = this.ctefield.parent().find('.cteundo');
+        var self = this;
 
-        this.undoicon.on('click', null, self, this.doUndo);
-        this.ctefield.editable(self.doChange,
+        this.cteField = this.$el.find('.cte' + this.cteclass);
+        this.cteIcon = this.cteField.parent().find('.cteimage');
+        this.undoIcon = this.cteField.parent().find('.cteundo');
+
+        this.undoIcon.on('click', null, self, this.doUndo);
+        this.cteField.editable(self.doChange,
             {
                 type: self.ctetype || "text",
                 select: true,
                 onedit: self.doEdit,
                 onreset: function(){console.log("onreset"); console.log(this);},
-                onsubmit: function(){console.log("onsubmit"); console.log(this);},
+                onsubmit: self.editFinished,
                 onblur: "submit",
-                callback: self.callback,
                 context: self
             }
         );
 
         // bind the events specific to this field
         _.each(this.events, function(act, event){
-            this.ctefield.on(event, this.act);
+            this.cteField.on(event, this.act);
         }, this);
         return this;
-    },
-
-    doChange: function(value, settings) {
-        console.log("ajax");
-        settings.context.model.set(settings.context.name, value);
-        settings.context.model.save(settings.context.name, value);
-        return value;
-    },
-
-    doEdit: function(settings, original) {
-        console.log("doEdit");
-        settings.context.cteicon.hide();
-        settings.context.undoValue = original.innerText;
-    },
-
-    doUndo: function(e) {
-        console.log("doUndo");
-        $(this).hide();
-        e.data.model.set(e.data.name, e.data.undoValue);
-        e.data.model.save(e.data.name, e.data.undoValue);
-    },
-
-    callback: function(value, settings) {
-        console.log("callback");
-        settings.context.undoicon.show();
     },
 
     /***
@@ -103,12 +82,67 @@
      */
     bindDomChange: function(model, fieldName) {},
 
+    /**
+     * The function editable expects to be the AJAX call for the edit.  We simply return the value since all Ajax/REST
+     * calls happen in backbone
+     * @param value
+     * @param settings
+     */
+    doChange: function(value, settings) {
+        return value;
+    },
+
+    /**
+     * This is called when the clickToEdit is initiated.  Saves the undo value for the field.
+     * @param settings
+     * @param original
+     */
+    doEdit: function(settings, original) {
+        settings.context.cteIcon.hide();
+        if (settings.context.undoValue != original.innerText) {
+            // This just saves the undovalue in memory as part of the current context.
+            //TODO:  make the undo value persistent
+            settings.context.undoValue = original.innerText;
+        }
+    },
+
+    /**
+     * Event handler for the event that triggers an undo
+     * @param e the event
+     */
+    doUndo: function(e) {
+        $(this).hide();
+        e.data.model.set(e.data.name, e.data.undoValue);
+        e.data.model.save(e.data.name, e.data.undoValue);
+    },
+
+    /**
+     * This is called when the clickToEdit value is submitted
+     * @param value
+     * @param settings
+     */
+    editFinished: function(value, settings) {
+        if (value != settings.context.undoValue) {
+            settings.context.model.set(settings.context.name, value);
+            settings.context.model.save(settings.context.name, value);
+            settings.context.undoIcon.show();
+        }
+        return value;
+    },
+
+    /**
+     * Event handler to show the clickToEdit pencil icon
+     * @param e an event
+     */
     showActions: function (e) {
-        this.cteicon.show();
+        this.cteIcon.show();
     },
 
+    /**
+     * Event handler to hide the clickToEdit pencil icon
+     * @param e an event
+     */
     hideActions: function(e) {
-        this.cteicon.hide();
-    },
-
+        this.cteIcon.hide();
+    }
 })
