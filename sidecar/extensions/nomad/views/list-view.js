2290b1a03a0da758a6785a8bca86d12b06f57fd0
Ruslan Golovach <r.golovach@intetics.com>
2012-05-10 23:26:55 +0300

    show more top button + search + some fixes (article id fix and onRemoveItem fix)

--- sidecar/extensions/nomad/views/list-view.js
+++ sidecar/extensions/nomad/views/list-view.js
@@ -2,11 +2,13 @@
 
     app.view.views.ListView = app.view.View.extend({
         events:{
-            'click  .show-more-button':'showMoreRecords',
+            'click  .show-more-top-btn':'showMoreTopRecords',
+            'click  .show-more-bottom-btn':'showMoreBottomRecords',
             'click  article .grip':'onClickGrip',
             'swipeLeft article':'onSwipeLeftItem',
             'swipeRight article':'onSwipeRightItem',
-            'click .remove-item-btn':'onRemoveItem'
+            'click .remove-item-btn':'onRemoveItem',
+            'keydown .search-query':'onKyeDown'
         },
         initialize: function(options) {
             // Mobile shows only the first two fields
@@ -14,22 +16,40 @@
             app.view.View.prototype.initialize.call(this, options);
 
             this.activeArticle = null;
-        },
 
-        addOne: function(model) {
+            this.topItemsModels = [];
+            this.timerId = null;
+            this.ITEM_TYPE_DELAY = 400;
+        },
+        onKyeDown:function(){
+            if(this.timerId)
+            {
+                window.clearTimeout(this.timerId);
+            }
+
+            this.timerId = window.setTimeout(_.bind(this.search,this),this.ITEM_TYPE_DELAY);
+        },
+        search:function(){
+          this.collection.fetch();
+        },
+        addOne: function(model,collection,options) {
             app.logger.debug('ADD ONE!');
             var fieldId = app.view.getFieldId();
             var item = Handlebars.helpers.listItem(model, this, this.meta.panels[0].fields);
-            this.$('.items').append(item.toString());
+
+            if (options.addTop) {
+                this.$('.items').children().first().before(item.toString());
+            } else {
+                this.$('.items').append(item.toString());
+            }
 
             for(var i = fieldId + 1; i <= app.view.getFieldId(); ++i) {
                 this._renderField(this.fields[i]);
             }
         },
-
         removeOne: function(model) {
             app.logger.debug('REMOVE ONE!');
-            this.$("#" + model.module + model.id).remove();
+            this.$("#" + this.module + model.id).remove();
         },
 
         bindDataChange: function() {
@@ -39,8 +59,38 @@
                 this.collection.on('remove', this.removeOne, this);
             }
         },
-        showMoreRecords:function() {
-           this.collection.paginate({page:this.collection.page,add:true});
+        showMoreTopRecords:function () {
+            var count = Math.min(app.config.maxQueryResult,this.topItemsModels.length);
+
+            for (var i = 0; i < count; i++) {
+                this.collection.add(this.topItemsModels.pop(),{addTop:true,at:0});
+            }
+            this.showMoreItems();
+        },
+        showMoreBottomRecords:function () {
+            this.collection.paginate({add:true, success:_.bind(function () {
+                this.showMoreItems(true);
+            }, this)});
+        },
+        showMoreItems:function (isBottomShift) {
+            var model = null;
+            while (app.config.maxQueryResult < this.collection.length) {
+
+                if(!isBottomShift){
+                    model = this.collection.pop({silent:true});
+                }else{
+                    model = this.collection.shift({silent:true});
+                    this.topItemsModels.push(model);
+                }
+                this.removeOne(model);
+            }
+
+            if (this.topItemsModels.length) {
+                this.$('.show-more-top-btn').show();
+            }else{
+                this.$('.show-more-top-btn').hide();
+            }
+
         },
         onClickGrip:function (e) {
             var grip = $(e.target);
@@ -63,9 +113,8 @@
         },
         onRemoveItem:function(e){
             //need confirm!!!!
-            var cid = $(e.target).closest('article').attr('model');
+            var cid = $(e.target).closest('article').attr('id').replace(this.module,'');
             var model = this.collection.get(cid);
-            this.collection.remove(model);
             model.destroy();
         },
         onEditItem:function(){
