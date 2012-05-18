8616202fa7a7c543c97c61810f3731a2387dc570
Rob Levin <rlevin@sugarcrm.com>
2012-05-18 09:10:56 -0700

    Added options that Ray from QA requested and generally improved script.

--- sidecar/tests/ci_runner.sh
+++ sidecar/tests/ci_runner.sh
@@ -1,13 +1,17 @@
 #!/usr/bin/env bash
-# Description: Used to create JUnit XML compatible CI reports 
-# from running our Jasmine based test suite.
+# Description: Used to create JUnit XML compatible CI reports from running
+# the sidecar Jasmine based test suite.
 #
-# E.g: ci_runner.sh -q -o ../../path/to/my_output_dir
+# Example:
+# sidecar/tests/ci_runner.sh 
+# -o FOO \
+# -m ~/programming/sugar/Mango \
+# -p ~/bin/phantomjs-1.5.0/bin/phantomjs \
+# -q
+# In the above example the output files would be placed in ./FOO, using the
+# phantomjs provided. The script would subshell into the -m directory and
+# no output would be produced. This is probably the most secure was to run.
 
-# ----------- YOU NEED TO DEFINE THESE! ------------- #
-PHANTOMJS="${HOME}/bin/phantomjs-1.5.0/bin/phantomjs"
-TESTDIR="${HOME}/programming/sugar/Mango/sidecar/tests"
-# --------------------------------------------------- #
 RUNNER="global.html"
 JASMINE_2_JUNITXML_RUNNER="phantomjs_jasminexml_runner.js"
 QUIET=-1
@@ -16,16 +20,20 @@ function main() {
     prepare_script "$@"
     execute_jasmine_runner
 }
-
 function prepare_script() {
     parse_args "$@"
-    get_full_path_to_output_dir ${OUTPUT_DIR}
+    setup_paths
     # we can get undefined results if we don't clean dir from previous runs
     check_if_output_dir_exists
 }
-
+function setup_paths() {
+    # Gets full path to our "required" directories
+    ABS_OUTPUT_DIR=$(get_full_path_to_dir $OUTPUT_DIR)
+    MANGO_DIR=$(get_full_path_to_dir $MANGO_DIR)
+    ABS_TEST_DIR="${MANGO_DIR}/sidecar/tests"
+}
 function check_if_output_dir_exists() {
-    if [ -d $ABS_OUTPUT_DIR ]; then
+    if [ -d ${ABS_OUTPUT_DIR} ]; then
         if [ ${QUIET} -eq 1 ]; then
             rm -rf $ABS_OUTPUT_DIR
         else
@@ -40,12 +48,11 @@ function check_if_output_dir_exists() {
         fi
     fi
 }
-
 function execute_jasmine_runner() {
     # Need to be in test directory
-    pushd ${TESTDIR} > /dev/null 2>&1
+    pushd ${ABS_TEST_DIR} > /dev/null 2>&1
     if [ ${QUIET} -eq 1 ]; then
-        ${PHANTOMJS} ${TESTDIR}/runners/${JASMINE_2_JUNITXML_RUNNER} ${TESTDIR}/runners/${RUNNER} ${ABS_OUTPUT_DIR} > /dev/null 2>&1
+        ${PHANTOMJS} ${ABS_TEST_DIR}/runners/${JASMINE_2_JUNITXML_RUNNER} ${ABS_TEST_DIR}/runners/${RUNNER} ${ABS_OUTPUT_DIR} > /dev/null 2>&1
     else
         echo "About to build JUnit XML Reports for Sidecar...this may take a minute"
         ${PHANTOMJS} runners/${JASMINE_2_JUNITXML_RUNNER} runners/${RUNNER} ${ABS_OUTPUT_DIR}
@@ -66,34 +73,39 @@ function execute_jasmine_runner() {
     fi
     popd > /dev/null 2>&1
 }
-
 function usage() {
     echo "
-Usage: $(basename $0) -o <output_dir> [-q quiet] 
-       -o specifies the output directory 
-       -q run in quiet mode 
+Usage: $(basename $0) -o <output_dir> -m <mango_dir> [-q quiet] [-p phantomjs] 
+    -o specifies the output directory where JUnit XML files will be written (required)
+    -m specifies the Mango directory - directory where the repo was checked out to (required)
+    -p specifies location of phantomjs command (optional). If not provided assumes 'phantomjs' command on PATH (not aliased!)
+    -q run in quiet mode (optional). User should note that we remove the output directory provided in -o if exists. Be careful!
 "
     exit 1
 }
-
 function parse_args() {
     if [ $# -eq 0 ] ; then
         usage
     fi
-    while getopts "qo:" opt; do
+    while getopts "qo:p:m:" opt; do
         case $opt in
             q) QUIET=1	;;
             o) OUTPUT_DIR="$OPTARG" ;;
+            m) MANGO_DIR="$OPTARG" ;;
+            p) PHANTOMJS="$OPTARG" ;;
         esac
     done
     shift $(($OPTIND - 1))
 
-    if [ -z ${OUTPUT_DIR} ]; then
+    if [[ -z ${OUTPUT_DIR} || -z ${MANGO_DIR} ]]; then
         usage
     fi
-}
 
-function get_full_path_to_output_dir() {
+    if [ -z ${PHANTOMJS} ]; then
+        PHANTOMJS=`which phantomjs` # assume it's on their PATH
+    fi
+}
+function get_full_path_to_dir() {
     local FILE=$1
     # remove any trailing slash
     FILE=${FILE%/}
@@ -106,13 +118,13 @@ function get_full_path_to_output_dir() {
         cd "$DC"
     fi
     local fileap=$(pwd -P)
-    ABS_OUTPUT_DIR=$fileap/$file_basename
+    local fullpath=$fileap/$file_basename
 
     cd "-" &>/dev/null
-    return 0
+
+    echo ${fullpath} # Bash's way of returning strings :(
 }
 
 main "$@"
-
 exit 0
 
