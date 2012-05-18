2b05850c36bda1d006a4857c4e9f2b3e0b1b0f3a
Julien Girault <jgirault@sugarcrm.com>
2012-05-18 16:25:04 -0700

    Make adding a note metadata driven. Updates the subdetail view

--- sidecar/src/view/views/subdetail-view.js
+++ sidecar/src/view/views/subdetail-view.js
@@ -12,28 +12,6 @@
         },
         initialize: function(options) {
             app.view.View.prototype.initialize.call(this, options);
-
-            app.events.register(
-                /**
-                 * Fired when the user clicks on a item in the activity stream
-                 * Helps pushing the model to the subdetail view.
-                 *
-                 * <pre><code>
-                 * obj.on("app:view:activity:subdetail", callback);
-                 * </pre></code>
-                 * @event
-                 */
-                "app:view:activity:subdetail",
-                this
-            );
-
-            var self = this;
-            app.events.on("app:view:activity:subdetail", function(model) {
-                if (model) {
-                    self.model.set(model);
-                }
-            });
-
             this.fallbackFieldTemplate = "detail";
         },
         render: function() {
@@ -41,7 +19,6 @@
             //avoid to have an empty detail view
         },
         bindDataChange: function() {
-            var self = this;
             if (this.model) {
                 this.model.on("change", function() {
                         app.view.View.prototype.render.call(this);
@@ -49,7 +26,6 @@
                 );
             }
         },
-
         // Delegate events
         closeSubdetail: function() {
             this.model.clear();
