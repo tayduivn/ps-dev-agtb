eb7988e572874a7159a149596541436c98f4ba8f
Mike Popov <m.popov@intetics.com>
2012-05-18 19:12:59 +0300

    Add field messages template. Override sidecar handleValidationError function.

---
+++ sidecar/extensions/nomad/views/field.js
@@ -0,0 +1,35 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: mpopov
+ * Date: 5/18/12
+ * Time: 5:21 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+(function(app) {
+
+    app.view.Field = app.view.Field.extend({
+        /**
+         * Handles how validation errors are appended to the fields dom element
+         * @param {Object} errors hash of validation errors
+         */
+        handleValidationError: function(errors) {
+            var template,
+                errMessages = [];
+
+            this.$('.control-group').addClass("error");
+
+            //create data array for template
+            _.each(errors, function(errorContext, errorName) {
+                errMessages.push(app.error.getErrorString(errorName, errorContext));
+            });
+
+            //get template and output result
+            template = app.template.get('field.messages');
+            this.$('.controls').append(template(errMessages));
+
+            app.alert.show('field_validation_error', {level:'general', messages:'Validation error!'});
+        }
+    });
+
+})(SUGAR.App)
