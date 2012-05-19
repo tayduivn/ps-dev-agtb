bb3248fe2850f019221e186daab209d4a53bdf45
f1vlad <kulchitski@gmail.com>
2012-05-18 17:19:48 -0700

    Move all events to `onpress`

--- sugarcrm/styleguide/js/mobile.js
+++ sugarcrm/styleguide/js/mobile.js
@@ -1,6 +1,32 @@
 (function($) {
-	// swipe for top nav
-	$('#logo').bind('touchmove', function (e) {
+        var navbar_bottom_handler = function(e){
+		e.preventDefault();
+		if($(this).parent().hasClass('teaser')) {
+			$(this).parent().removeClass('teaser');
+		} else {
+			$(this).parent().toggleClass('exposed');
+		}
+		return false;
+	    },
+	    cube_handler = function (e) {
+		e.preventDefault();
+		if($('body').hasClass('onL')){
+		    $('#logo').trigger('swipeLeft');
+		}else{
+		    $('#logo').trigger('swipeRight');
+		}
+		return false;
+	    },
+	    launch_handler = function (e) {
+		e.preventDefault();
+		if($('body').hasClass('onR')===true){
+		    $('#create').trigger('swipeRight');
+		} else {
+		    $('#create').trigger('swipeLeft');
+		}
+		return false;
+            };
+	$('#logo,.navbar-bottom,.thrhld,#create,#createList,#search').bind('touchmove', function (e) {
 		e.preventDefault();} 
 	);
 	$('#logo, h1.nomad').swipeRight(function () {
@@ -14,9 +40,6 @@
 	      $('html').find('body').toggleClass('onL');
 	      return false;
 	});
-	$('#create').bind('touchmove', function (e) {
-		e.preventDefault();}
-	);
 	$('#create, h1.nomad').swipeLeft(function () {
 		closeBottomMenu();
 		$('html').find('body').addClass('onR');
@@ -27,27 +50,13 @@
 	$('#moduleList').swipeLeft(function () {
 		$('html').find('body').removeClass('onL');
 	});
-	$('#createList').bind('touchmove', function (e) {
-		e.preventDefault();}
-	);
 	$('#createList').swipeRight(function () {
 		$('html').find('body').removeClass('onR');
-	});
-	$('#search').bind('touchmove', function (e) {
-		e.preventDefault();
 	});
 	$('#search').swipeDown(function () {
 		$('body').find('#searchForm').toggleClass('hide');
 	});
-
-	$('.thrhld').on('click',function () {
-		if($(this).parent().hasClass('teaser')) {
-			$(this).parent().removeClass('teaser');
-		} else {
-			$(this).parent().toggleClass('exposed');
-			//.css('height',window.innerWidth);
-		}
-	});
+	$('.navbar-bottom').onpress('.thrhld',navbar_bottom_handler);
 	$('.navbar-bottom .thrhld').swipeDown(function(){
 	        $(this).parent().removeClass('exposed teaser');
 	});
@@ -56,24 +65,10 @@
 	});
 
     // trigger the module menu - this could be a tap function but zepto will not honor return false
-    $('.cube').live('click', function () {
-	if($('body').hasClass('onL')){
-	    $('#logo').trigger('swipeLeft');
-	}else{
-	    $('#logo').trigger('swipeRight');
-	}
-	return false;
-    });
+    $('body').onpress('.cube', cube_handler);
 
     // trigger the module menu - this could be a tap function but zepto will not honor return false
-    $('.launch').live('click', function () {
-	if($('body').hasClass('onR')===true){
-	    $('#create').trigger('swipeRight');
-	} else {
-	    $('#create').trigger('swipeLeft');
-	}
-	return false;
-    });
+    $('body').onpress('.launch', launch_handler);
 
     $('article').live('swipeLeft',function () {
 	var anchor=$(this);
@@ -103,43 +98,45 @@
 	    return false;
     });
     // fake phone for prototype
-    $('#record-action .icon-phone, .btn-group .btn.call, .controls .btn.call').on('click', function () {
-	
-	    $('body').append('<div class="over"><h4>Place a call</h4><p><a href="tel:605-334-2345" class="btn btn-large">Home (605)-334-2345</a></p><p><a class="btn btn-large">Mobile (605)-334-2345</a></p><p><a class="btn btn-large">Office (605)-334-2345</a></p><p><a href="javascript:return false;" class="btn btn-inverse btn-large" id="cancel">Cancel</a></p></div>');
-	    return false;
+    $('body').onpress('#record-action .icon-phone, .btn-group .btn.call, .controls .btn.call', function () {
+	$('body').append('<div class="over"><h4>Place a call</h4><p><a href="tel:605-334-2345" class="btn btn-large">Home (605)-334-2345</a></p><p><a class="btn btn-large">Mobile (605)-334-2345</a></p><p><a class="btn btn-large">Office (605)-334-2345</a></p><p><a href="javascript:return false;" class="btn btn-inverse btn-large" id="cancel">Cancel</a></p></div>');
+	return false;
     });
-    $('.over #cancel').live('click tap', function () {
-		$(this).closest('.over').remove();
-		return false;
+    $('body').onpress('.over #cancel', function () {
+	$(this).closest('.over').remove();
+	return false;
     });
-    $('a[title=Remove]').live('click', function () {
+    $('body').onpress('a[title=Remove]', function () {
 		$(this).closest('article').addClass('deleted').anim({ translateX: window.innerWidth + 'px', opacity: '0'}, .5, 'ease-out');
-		setTimeout(function () {
-		    $('.deleted').remove();
-		}, 250);
-		return false;
+	setTimeout(function () {
+	    $('.deleted').remove();
+	}, 250);
+	return false;
     });
-    $('#tour').on('click', function () {
-      $(this).remove();
+    $('body').onpress('#tour', function (e) {
+	e.preventDefault();
+	$(this).remove();
     });
-    $('#nomad:not(.flow) #back .back, #nomad:not(.flow) .back.btn').on('click', function(){
-		if(history.length<=2) {
-			window.location="./";
-		}else{
-		    window.history.back(-1);		
-		}
+    $('body').onpress('#nomad:not(.flow) #back .back, #nomad:not(.flow) .back.btn', function(){
+	if(history.length<=2) {
+		window.location="./";
+	}else{
+	    window.history.back(-1);		
+	}
     });
-    $('.listing > article:last-child a.show_more_posts').live('click', function(e){
+    $('body').onpress('.listing > article:last-child a.show_more_posts', function(e){
+	e.preventDefault();
         $(this).closest('article').remove();
 	inject_posts('append',$('.listing'),5);
 	return false;
     });
-    $('.listing > article:nth-child(3) a.show_more_posts').live('click', function(e){
+    $('body').onpress('.listing > article:nth-child(3) a.show_more_posts', function(e){
 	$(this).closest('article').remove();
 	inject_posts('prepend',$('.listing'),5);
 	return false;
     });
-    $('.listing > article.nav').live('click', function(e){
+    $('body').onpress('.listing > article.nav', function(e){
+	e.preventDefault();
         $(this).find('a').first().css('border','1px solid red').trigger('click');
     });
 
@@ -194,14 +191,15 @@
         }, 3000);
     }
 
-    $(".icon-star-empty, .icon-star").live('click tap',function(){
+    $("body").onpress('.icon-star-empty, .icon-star',function(e){
+	e.preventDefault();
 	var rn=Math.floor(Math.random()*100);
 	$('body').append('<div id="demo-general" class="tmp-' + rn + ' alert alert-general" style="display:block;"><strong>Loading...</strong></div>');
 	setTimeout(function(ia){
             $('.alert.tmp-'+rn).anim({ translateY: window.innerHeight + 'px', opacity: '0'}, 3, 'ease-out', function (){ $('.alert.tmp-'+rn).remove() });
         }, 3000);
     });
-      $("a[title=Remove]").live('click tap',function(){
+      $("body").onpress('a[title=Remove]',function(){
   	var rn=Math.floor(Math.random()*100);
   	$('body').append('<div id="demo-general" class="tmp-' + rn + ' alert alert-success" style="display:block;"><strong>Success!</strong> You removed an item!</div>');
   	setTimeout(function(ia){
@@ -210,7 +208,7 @@
       });
 
 
-    $('.icon-star, .icon-star-empty').live('click tap',function(){
+    $('body').onpress('.icon-star, .icon-star-empty',function(){
         $(this).toggleClass('icon-star-empty').toggleClass('icon-star');
     });
     
@@ -220,8 +218,6 @@
 	}
     });
 
-
-
     var fd_touch_event = "tap",
         fd_detail_view = $('.layout.detail'),
 	fd_listing_view = $('.layout.listing'),
@@ -229,7 +225,7 @@
 	fd_navbar = $('.navbar'),
 	fd_back_btn = $('#back');
 
-    $('#nomad.flow #login_button').on('click',function(){
+    $('body').onpress('#nomad.flow #login_button',function(){
 	fd_listing_view.show().css({opacity:'0',left:'0',right:'0'});
 	fd_listing_view.anim({opacity:'1'},.75,'ease-in',function(){
 	    fd_navbar.show();
@@ -267,15 +263,6 @@
 	$('#nomad.flow #back .back').trigger(fd_touch_event);
 	return false;
     })
-    
-//alert('trigger #back click when these are clicked: h1.nomad, h3.nomad --- because they`re overlapping back button');
-
-//   .anim({ translateX: window.innerWidth + 'px', opacity: '0'}, .5, 'ease-out');
-
-
-
-
-
 
 })(window.Zepto);
 
