0e053e5c4a81e129291508a58d2ccc1b00cc892f
Deepali Szczesny <deepali@sugarcrm.com>
2012-05-21 16:28:26 -0400

    Fixing unit test failure on Meetings

--- sugarcrm/tests/modules/Meetings/MeetingTest.php
+++ sugarcrm/tests/modules/Meetings/MeetingTest.php
@@ -70,11 +70,11 @@ class MeetingTest extends Sugar_PHPUnit_Framework_TestCase
 		$meeting->name = 'Test Meeting Recurring';
 		
 		$meeting->recurring_source = 'Outlook';
-		// disable edit for recurring meetings synced from outlook 	
-		$this->assertFalse($meeting->ACLAccess('edit'));
+        // can edit
+		$this->assertTrue($meeting->ACLAccess('edit'));
 		
 		$meeting->recurring_source = '';
-		// enable to rest
+		// can edit
 		$this->assertTrue($meeting->ACLAccess('edit'));
 	}
 	
