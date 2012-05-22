b3a27450a834dff08d422cce0819e5120c92613e
Mike Popov <m.popov@intetics.com>
2012-05-23 02:44:57 +0300

    Fix detail view.

--- sidecar/extensions/nomad/views/detail-view.js
+++ sidecar/extensions/nomad/views/detail-view.js
@@ -13,30 +13,34 @@
          * Overrides default views method to pass custom data object as the context.
          * @protected
          */
-        _render: function() {
+         _render: function () {
             //find 'name' field, otherwise get the first field as header field
             var headerField, headerPanelInd, headerFieldInd;
-            _.each(this.meta.panels, function (panel, panelIndex) {
-                _.each(panel.fields, function (field, fieldIndex) {
-                    if (field.name == "name") {
-                        headerField = field;
-                        headerPanelInd = panelIndex;
-                        headerFieldInd = fieldIndex;
-                    }
-                });
-            });
+            headerField = this.filterFields(function (field, fieldIndex, panel, panelIndex) {
+                if (field.name == "name") {
+                    headerField = field;
+                    headerPanelInd = panelIndex;
+                    headerFieldInd = fieldIndex;
+                    return true;
+                }
+            })[0];
             if (!headerField) {
                 headerField = this.meta.panels[0].fields[0];
                 headerPanelInd = 0;
                 headerFieldInd = 0;
             }
 
-            // get four other fields to output
+            //get four other fields to output
             var fields = [], i = 0;
             while (fields.length < 4) {
                 if (!(headerFieldInd == i && headerPanelInd == 0)) fields.push(this.meta.panels[0].fields[i]);
                 i++;
             }
+
+            //get link type fields
+            var linkFields = this.filterFields(function (field, fieldIndex, panel, panelIndex) {
+                if (field.type == "link") return true;
+            });
 
             //create custom data object
             var self = this,
@@ -44,13 +48,30 @@
                     viewObj: self,
                     customData: {
                         headerField: headerField,
-                        fields: fields
+                        fields: fields,
+                        links: linkFields
                     }
                 };
 
             //pass custom data object as the context
             this._renderWithContext(dataObj);
+        },
+
+        /**
+         * Iterate over all the fields and get needed ones, depending on filterFunc result
+         * @param filterFunc
+         * @return {*}
+         */
+        filterFields: function (filterFunc) {
+            var filteredFields = [];
+            _.each(this.meta.panels, function (panel, panelIndex) {
+                _.each(panel.fields, function (field, fieldIndex) {
+                    if (filterFunc(field, fieldIndex, panel, panelIndex)) filteredFields.push(field);
+                });
+            });
+            return filteredFields;
         }
+
     });
 
 })(SUGAR.App);
