e1e935e65b6c92b3be6679fd43f75e190f08b14f
Mike Popov <m.popov@intetics.com>
2012-05-23 19:01:14 +0300

    Fix detail view. Edit-view 'phone' button.

--- sidecar/extensions/nomad/templates/fields/phone/phone.js
+++ sidecar/extensions/nomad/templates/fields/phone/phone.js
@@ -1,16 +1,19 @@
 (function(app) {
 
-    app.view.fields.IntField = app.view.Field.extend({
-
-        // TODO: Override base implementation
-        // Check out Mango/sugarcrm/clients/base/fields/int/int.js
+    app.view.fields.PhoneField = app.view.Field.extend({
+        events: {
+            "click .call": function () {
+                //var phoneNumber = this.value;
+            }
+        },
 
         unformat: function(value) {
+            return value;
         },
 
         format: function(value) {
+            return value;
         }
-
     });
 
 })(SUGAR.App);
