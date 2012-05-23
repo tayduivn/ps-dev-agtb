e9dde3615341edc508af69ff26d3551cabc73fac
Mike Popov <m.popov@intetics.com>
2012-05-23 14:18:07 +0300

    Fix detail view. Fields templates & controllers.

---
+++ sidecar/extensions/nomad/templates/fields/phone/phone.js
@@ -0,0 +1,16 @@
+(function(app) {
+
+    app.view.fields.IntField = app.view.Field.extend({
+
+        // TODO: Override base implementation
+        // Check out Mango/sugarcrm/clients/base/fields/int/int.js
+
+        unformat: function(value) {
+        },
+
+        format: function(value) {
+        }
+
+    });
+
+})(SUGAR.App);
