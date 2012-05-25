5dde884958ed00fd1997c3f865ad1913f7c17e20
Mike Popov <m.popov@intetics.com>
2012-05-25 18:13:36 +0300

    Fix detail & edit view.

--- sidecar/extensions/nomad/views/edit-view.js
+++ sidecar/extensions/nomad/views/edit-view.js
@@ -2,33 +2,37 @@
 
     app.view.views.EditView = app.view.View.extend({
         events: {
-            "click #saveRecord": function () {
-                var model = this.context.get("model"),
-                    module = model.module;
+            "click #saveRecord": "saveRecord",
+            "click #backRecord": "cancel"
+        },
+        initialize: function (options) {
+            app.view.View.prototype.initialize.call(this, options);
+            this.backupModel();
+        },
+        saveRecord: function () {
+            var model = this.context.get("model"),
+                module = model.module;
 
-                model.save(null, {
-                    success: function (model, resp) {
-                        console.log("---saved successfully");
-
-                        //navigate to record details
-                        if (this.name == 'edit') {
-                            app.router.goBack();
-                        } else {
-                            app.router.navigate(module + '/' + resp.id, {trigger: true});
-                        }
-                    },
-                    error: function () {
-                        console.log("---save error");
-                    }
-                });
-            },
-            "click #backRecord": function () {
-                app.router.goBack();
-            }/*,
-            "focusin input": function (e) {
-                debugger;
-                //$(e.srcElement).parent(".control-group").removeClass('error');
-            }*/
+            model.save(null, {
+                success: function (model, resp) {
+                    app.alert.show('save_success', {level:'success', messages:'Saved successfully.', autoClose:'true'});
+                    app.router.goBack();
+                },
+                error: function (model, resp, options) {
+                    app.alert.show('save_error', {level:'error', messages:'Save error!', autoClose:'true'});
+                }
+            });
+        },
+        cancel: function (e) {
+            this.restoreModel();
+            app.router.goBack();
+        },
+        backupModel: function () {
+            var serializedModel = JSON.stringify(this.model.attributes);
+            this._modelBackup = JSON.parse(serializedModel);
+        },
+        restoreModel: function () {
+            this.model.set(this._modelBackup);
         }
     });
 
