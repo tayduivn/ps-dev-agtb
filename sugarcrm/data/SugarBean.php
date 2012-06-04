befa6b26d40db7ccc79a031e968118bba4b587de
Joe Wang <cwang@sugarcrm.com>
2012-06-04 16:44:23 -0700

    Bug_53479: Advanced Search shows Warning message and no records after adding a custom decimal field to Accounts module

--- sugarcrm/data/SugarBean.php
+++ sugarcrm/data/SugarBean.php
@@ -3295,8 +3295,12 @@ function save_relationship_changes($is_update, $exclude=array())
                 $ret_array['select'] .= ", $this->table_name.$field $alias";
                 $selectedFields["$this->table_name.$field"] = true;
             } else if(  (!isset($data['source']) || $data['source'] == 'custom_fields') && (!empty($alias) || !empty($filter) )) {
-                $ret_array['select'] .= ", $this->table_name"."_cstm".".$field $alias";
-                $selectedFields["$this->table_name.$field"] = true;
+                $select_array = array_map('trim', explode(',', $ret_array['select']));
+                // do not add if the custom column already exists in the select
+                if (!in_array($this->table_name."_cstm.".$field, $select_array)) {
+                    $ret_array['select'] .= ", $this->table_name"."_cstm".".$field $alias";
+                    $selectedFields["$this->table_name.$field"] = true;
+                }
             }
 
 
