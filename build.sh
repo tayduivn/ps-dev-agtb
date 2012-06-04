b975bad8fdb5751e2aabce794bcbc48b031c4785
Steven Parsley <sparsley@sugarcrm.com>
2012-06-04 11:51:27 -0400

    bug: 53060 Removing file that was not supposed to be checked in

--- build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-cp /Applications/MAMP/htdocs/Mango/ent/sugarcrm/config.php /tmp/config.php
-~/bin/sugarbuildfresh
-cp /tmp/config.php /Applications/MAMP/htdocs/Mango/ent/sugarcrm/config.php
