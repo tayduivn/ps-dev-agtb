e5c3195d9807a02e1f6e75a645528fce3af32420
Jenny Gonsalves <jenny@sugarcrm.com>
2012-06-06 11:43:53 -0700

    Bug 53622 - Making the dynamic params appear consistently.

--- sugarcrm/modules/Users/UserViewHelper.php
+++ sugarcrm/modules/Users/UserViewHelper.php
@@ -456,7 +456,7 @@ class UserViewHelper {
         }
         $this->ss->assign("CALENDAR_PUBLISH_URL", $publish_url);
         $this->ss->assign("CALENDAR_SEARCH_URL", $sugar_config['site_url'].'/vcal_server.php/type=vfb&email=%NAME%@%SERVER%');
-        $this->ss->assign("CALENDAR_ICAL_URL", $sugar_config['site_url'].'/ical_server.php?type=ics&key=<mysecretkey>&user_name=&lt;myusername&gt;');
+        $this->ss->assign("CALENDAR_ICAL_URL", $sugar_config['site_url'].'/ical_server.php?type=ics&key=%MYSECRETKEY%&user_name=%MYUSERNAME%');
         //END SUGARCRM flav!=sales ONLY
         //BEGIN SUGARCRM flav!=dce ONLY
         //BEGIN SUGARCRM flav=ent ONLY
