ada19559f33bb5af6f585c935377332f893d1799
Collin Lee <clee@sugarcrm.com>
2012-06-13 01:44:40 -0400

    Update committed view to use some live data.

--- sugarcrm/clients/base/views/forecastsCommitted/forecastsCommitted.js
+++ sugarcrm/clients/base/views/forecastsCommitted/forecastsCommitted.js
@@ -7,20 +7,39 @@
 ({
     viewSelector: '.forecastsCommitted',
 
-    bindDataChange: function() {
-        var self = this,
-            model = this.context.model.forecasts.committed;
+    initialize : function(options) {
+        app.view.View.prototype.initialize.call(this, options);
+        //Set defaults
+        this.fullName = app.user.get('full_name');
+        this.bestCase = 0;
+        this.likelyCase = 0;
+    },
 
+    bindDataChange: function() {
+        var self = this, model = this.context.model.forecasts.committed;
         model.on('change', function() {
-            self.buildForecastsCommitted(this);
+            self.buildForecastsCommitted(model);
         });
     },
 
     buildForecastsCommitted: function(model) {
         var self = this;
-        _.each(model.attributes, function(data, key) {
+        var count = 0;
+        var entries = model.get('committed');
+        _.each(entries, function(data, key)
+        {
+            //If this is the first entry, then set the likelyCase & bestCase variables in handlebar template
+            if(++count == 1)
+            {
+              self.likelyCase = data['likely_case'];
+              self.bestCase = data['best_case'];
+            } else {
 
+            }
         });
+
+        //Call render again
+        this.render();
     },
 
     events: {
