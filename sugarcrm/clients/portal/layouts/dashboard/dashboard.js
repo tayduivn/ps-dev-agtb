f9df85cb2582278542c82b36f54efe533381598f
Julien Girault <jgirault@sugarcrm.com>
2012-06-18 15:32:50 -0700

    Initialize a new array before pushing components into

--- sugarcrm/clients/portal/layouts/dashboard/dashboard.js
+++ sugarcrm/clients/portal/layouts/dashboard/dashboard.js
@@ -3,6 +3,7 @@
         // Figure out the modules that are available to the user.
         this.moduleList = app.metadata.getModuleList({visible: true});
 
+        options.meta.components = [];
         // Add components metadata as specified in the module list
         _.each(this.moduleList, function(module) {
             options.meta.components.push({layout: "list", context: {limit: 5, module: module}});
