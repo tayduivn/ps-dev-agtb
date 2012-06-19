04ffd2efacf5008e6cf9560fb11c15167dd27f33
Jon Whitcraft <jwhitcraft@sugarcrm.com>
2012-06-19 09:56:29 -0400

    Fix up the code to use the report builder the proper way.

--- sugarcrm/modules/Forecasts/api/ForecastsWorksheetApi.php
+++ sugarcrm/modules/Forecasts/api/ForecastsWorksheetApi.php
@@ -60,14 +60,12 @@ class ForecastsWorksheetApi extends ModuleApi {
         $app_list_strings = return_app_list_strings_language('en');
         $app_strings = return_application_language('en');
         $mod_strings = return_module_language('en', 'Opportunities');
-        $saved_report = new SavedReport();
         $report_defs = array();
-        $report_defs['ForecastSeedReport1'] = array('Opportunities', 'ForecastSeedReport1', '{"display_columns":[{"name":"forecast","label":"Include in Forecast","table_key":"self"},{"name":"name","label":"Opportunity Name","table_key":"self"},{"name":"date_closed","label":"Expected Close Date","table_key":"self"},{"name":"sales_stage","label":"Sales Stage","table_key":"self"},{"name":"probability","label":"Probability (%)","table_key":"self"},{"name":"amount","label":"Opportunity Amount","table_key":"self"},{"name":"best_case_worksheet","label":"Best Case (adjusted)","table_key":"self"},{"name":"likely_case_worksheet","label":"Likely Case (adjusted)","table_key":"self"}],"module":"Opportunities","group_defs":[{"name":"date_closed","label":"Month: Expected Close Date","column_function":"month","qualifier":"month","table_key":"self","type":"date"},{"name":"sales_stage","label":"Sales Stage","table_key":"self","type":"enum"}],"summary_columns":[{"name":"date_closed","label":"Month: Expected Close Date","column_function":"month","qualifier":"month","table_key":"self"},{"name":"sales_stage","label":"Sales Stage","table_key":"self"},{"name":"likely_case_worksheet","label":"SUM: Likely Case (adjusted)","field_type":"currency","group_function":"sum","table_key":"self"}],"report_name":"Test","chart_type":"vBarF","do_round":1,"chart_description":"","numerical_chart_column":"self:likely_case_worksheet:sum","numerical_chart_column_type":"currency","assigned_user_id":"1","report_type":"summary","full_table_list":{"self":{"value":"Opportunities","module":"Opportunities","label":"Opportunities"},"Opportunities:assigned_user_link":{"name":"Opportunities  >  Assigned to User","parent":"self","link_def":{"name":"assigned_user_link","relationship_name":"opportunities_assigned_user","bean_is_lhs":false,"link_type":"one","label":"Assigned to User","module":"Users","table_key":"Opportunities:assigned_user_link"},"dependents":["Filter.1_table_filter_row_2"],"module":"Users","label":"Assigned to User"}},"filters_def":{"Filter_1":{"operator":"AND","0":{"name":"timeperiod_id","table_key":"self","qualifier_name":"is","runtime":1,"input_name0":["aba5fc2a-4693-b984-956d-4fd9f9b32dc6"]},"1":{"name":"id","table_key":"Opportunities:assigned_user_link","qualifier_name":"is","runtime":1,"input_name0":["Current User"]}}}}', 'detailed_summary', 'vBarF');
-        $report = new Report($report_defs['ForecastSeedReport1'][2]);
+        $report_defs['ForecastSeedReport1'] = array('Opportunities', 'ForecastSeedReport1', '{"display_columns":[{"name":"forecast","label":"Include in Forecast","table_key":"self"},{"name":"name","label":"Opportunity Name","table_key":"self"},{"name":"date_closed","label":"Expected Close Date","table_key":"self"},{"name":"sales_stage","label":"Sales Stage","table_key":"self"},{"name":"probability","label":"Probability (%)","table_key":"self"},{"name":"amount","label":"Opportunity Amount","table_key":"self"},{"name":"best_case_worksheet","label":"Best Case (adjusted)","table_key":"self"},{"name":"likely_case_worksheet","label":"Likely Case (adjusted)","table_key":"self"}],"module":"Opportunities","group_defs":[{"name":"date_closed","label":"Month: Expected Close Date","column_function":"month","qualifier":"month","table_key":"self","type":"date"},{"name":"sales_stage","label":"Sales Stage","table_key":"self","type":"enum"}],"summary_columns":[{"name":"date_closed","label":"Month: Expected Close Date","column_function":"month","qualifier":"month","table_key":"self"},{"name":"sales_stage","label":"Sales Stage","table_key":"self"},{"name":"likely_case_worksheet","label":"SUM: Likely Case (adjusted)","field_type":"currency","group_function":"sum","table_key":"self"}],"report_name":"Test","chart_type":"vBarF","do_round":1,"chart_description":"","numerical_chart_column":"self:likely_case_worksheet:sum","numerical_chart_column_type":"currency","assigned_user_id":"1","report_type":"summary","full_table_list":{"self":{"value":"Opportunities","module":"Opportunities","label":"Opportunities"}},"filters_def":{}}', 'detailed_summary', 'vBarF');
 
         $testFilters = array(
             'timeperiod_id' => isset($args['timeperiod_id']) ? $args['timeperiod_id'] : array('$is' => TimePeriod::getCurrentId()),
-            'assigned_user_link' => array('id' => 'seed_chris_id'),
+            //'assigned_user_link' => array('id' => 'seed_chris_id'),
             //'id' => isset($args['user_id']) ? $args['user_id'] : $current_user->id
         );
 
@@ -76,11 +74,23 @@ class ForecastsWorksheetApi extends ModuleApi {
         require_once("include/SugarParsers/Converter/Report.php");
         require_once("include/SugarCharts/ReportBuilder.php");
 
+        // create the a report builder instance
+        $rb = new ReportBuilder("Opportunities");
+        // load the default report into the report builder
+        $rb->setDefaultReport($report_defs['ForecastSeedReport1'][2]);
+
+        // parse any filters from above
         $filter = new SugarParsers_Filter(new Opportunity());
         $filter->parse($testFilters);
-        $converter = new SugarParsers_Converter_Report(new ReportBuilder("Opportunities"));
+        $converter = new SugarParsers_Converter_Report($rb);
         $reportFilters = $filter->convert($converter);
-        $report->report_def['filters_def'] = $reportFilters;
+        // add the filter to the report builder
+        $rb->addFilter($reportFilters);
+
+        // create the json for the reporting engine to use
+        $chart_contents = $rb->toJson();
+
+        $report = new Report($chart_contents);
 
         $report->clear_group_by();
         $report->create_order_by();
