01a4d6e1d241370f7bff0a62a49dba8d330600a4
Collin Lee <clee@sugarcrm.com>
2012-06-19 14:42:06 -0400

    Added worksheetManager endpoint

---
+++ sugarcrm/modules/Forecasts/api/ForecastsWorksheetManagerApi.php
@@ -0,0 +1,148 @@
+<?php
+if(!defined('sugarEntry') || !sugarEntry) die('Not A Valid Entry Point');
+/********************************************************************************
+ *The contents of this file are subject to the SugarCRM Professional End User License Agreement
+ *("License") which can be viewed at http://www.sugarcrm.com/EULA.
+ *By installing or using this file, You have unconditionally agreed to the terms and conditions of the License, and You may
+ *not use this file except in compliance with the License. Under the terms of the license, You
+ *shall not, among other things: 1) sublicense, resell, rent, lease, redistribute, assign or
+ *otherwise transfer Your rights to the Software, and 2) use the Software for timesharing or
+ *service bureau purposes such as hosting the Software for commercial gain and/or for the benefit
+ *of a third party.  Use of the Software may be subject to applicable fees and any use of the
+ *Software without first paying applicable fees is strictly prohibited.  You do not have the
+ *right to remove SugarCRM copyrights from the source code or user interface.
+ * All copies of the Covered Code must include on each user interface screen:
+ * (i) the "Powered by SugarCRM" logo and
+ * (ii) the SugarCRM copyright notice
+ * in the same form as they appear in the distribution.  See full license for requirements.
+ *Your Warranty, Limitations of liability and Indemnity are expressly stated in the License.  Please refer
+ *to the License for the specific language governing these rights and limitations under the License.
+ *Portions created by SugarCRM are Copyright (C) 2004 SugarCRM, Inc.; All Rights Reserved.
+ ********************************************************************************/
+
+require_once('include/api/ModuleApi.php');
+
+class ForecastsWorksheetManagerApi extends ModuleApi {
+
+    public function __construct()
+    {
+
+    }
+
+    public function registerApiRest()
+    {
+        $parentApi = parent::registerApiRest();
+        //Extend with test method
+        $parentApi= array (
+            'worksheetManager' => array(
+                'reqType' => 'GET',
+                'path' => array('Forecasts','worksheetManager'),
+                'pathVars' => array('',''),
+                'method' => 'worksheetManager',
+                'shortHelp' => 'Worksheet for manager view',
+                'longHelp' => 'include/api/html/modules/Forecasts/ForecastWorksheetManagerApi.html#ping',
+            ),
+        );
+        return $parentApi;
+    }
+
+    /**
+     * This method returns the result for a sales rep view/manager's opportunities view
+     *
+     * @param $api
+     * @param $args
+     * @return array
+     */
+    public function worksheetManager($api, $args)
+    {
+        require_once('modules/Reports/Report.php');
+        global $current_user, $mod_strings, $app_list_strings, $app_strings;
+
+
+        if(isset($args['user_id']))
+        {
+            $user = new User();
+            $user->retrieve($args['user_id']);
+        } else {
+            $user = $current_user;
+        }
+
+
+        if(!User::isManager($user->id))
+        {
+           //Error
+           return array();
+        }
+
+        $app_list_strings = return_app_list_strings_language('en');
+        $app_strings = return_application_language('en');
+        $mod_strings = return_module_language('en', 'Opportunities');
+        $report_defs = array();
+        $report_defs['ForecastSeedReport1'] = array('Opportunities', 'ForecastSeedReport1', '{"display_columns":[{"name":"forecast","label":"Include in Forecast","table_key":"self"},{"name":"name","label":"Opportunity Name","table_key":"self"},{"name":"date_closed","label":"Expected Close Date","table_key":"self"},{"name":"sales_stage","label":"Sales Stage","table_key":"self"},{"name":"probability","label":"Probability (%)","table_key":"self"},{"name":"amount","label":"Opportunity Amount","table_key":"self"},{"name":"best_case_worksheet","label":"Best Case (adjusted)","table_key":"self"},{"name":"likely_case_worksheet","label":"Likely Case (adjusted)","table_key":"self"}],"module":"Opportunities","group_defs":[{"name":"date_closed","label":"Month: Expected Close Date","column_function":"month","qualifier":"month","table_key":"self","type":"date"},{"name":"sales_stage","label":"Sales Stage","table_key":"self","type":"enum"}],"summary_columns":[{"name":"date_closed","label":"Month: Expected Close Date","column_function":"month","qualifier":"month","table_key":"self"},{"name":"sales_stage","label":"Sales Stage","table_key":"self"},{"name":"likely_case_worksheet","label":"SUM: Likely Case (adjusted)","field_type":"currency","group_function":"sum","table_key":"self"}],"report_name":"Test","chart_type":"vBarF","do_round":1,"chart_description":"","numerical_chart_column":"self:likely_case_worksheet:sum","numerical_chart_column_type":"currency","assigned_user_id":"1","report_type":"summary","full_table_list":{"self":{"value":"Opportunities","module":"Opportunities","label":"Opportunities"}},"filters_def":{}}', 'detailed_summary', 'vBarF');
+
+        $testFilters = array(
+            'timeperiod_id' => isset($args['timeperiod_id']) ? $args['timeperiod_id'] : array('$is' => TimePeriod::getCurrentId()),
+            //'assigned_user_link' => array('id' => 'seed_chris_id'),
+            //'id' => isset($args['user_id']) ? $args['user_id'] : $current_user->id
+        );
+
+
+        require_once('include/SugarParsers/Filter.php');
+        require_once("include/SugarParsers/Converter/Report.php");
+        require_once("include/SugarCharts/ReportBuilder.php");
+
+        // create the a report builder instance
+        $rb = new ReportBuilder("Opportunities");
+        // load the default report into the report builder
+        $rb->setDefaultReport($report_defs['ForecastSeedReport1'][2]);
+
+        // parse any filters from above
+        $filter = new SugarParsers_Filter(new Opportunity());
+        $filter->parse($testFilters);
+        $converter = new SugarParsers_Converter_Report($rb);
+        $reportFilters = $filter->convert($converter);
+        //_pp($reportFilters);
+        //die();
+        // add the filter to the report builder
+        $rb->addFilter($reportFilters);
+
+        // create the json for the reporting engine to use
+        $chart_contents = $rb->toJson();
+
+        $report = new Report($chart_contents);
+
+        $report->clear_group_by();
+        $report->create_order_by();
+        $report->create_select();
+        $report->create_where();
+        $report->create_group_by(false);
+        $report->create_from();
+        $report->create_query();
+        $limit = false;
+        if ($report->report_type == 'tabular' && $report->enable_paging) {
+            $report->total_count = $report->execute_count_query();
+            $limit = true;
+        }
+        $result = $GLOBALS['db']->query($report->query);
+
+        $opps = array();
+        while(($row=$GLOBALS['db']->fetchByAssoc($result))!=null)
+        {
+            $row['id'] = $row['primaryid'];
+            $row['forecast'] = $row['opportunities_forecast'];
+            $row['name'] = $row['opportunities_name'];
+            $row['amount'] = $row['opportunities_amount'];
+            $row['date_closed'] = $row['opportunities_date_closed'];
+            $row['probability'] = $row['opportunities_probability'];
+            $row['sales_stage'] = $row['opportunities_sales_stage'];
+            $row['best_case_worksheet'] = $row['OPPORTUNITIES_BEST_CAS81CC16'];
+            $row['likely_case_worksheet'] = $row['OPPORTUNITIES_LIKELY_C7E6E04'];
+
+            //Should we unset the data we don't need here so as to limit data sent back?
+
+            $opps[] = $row;
+        }
+        return $opps;
+    }
+
+}
