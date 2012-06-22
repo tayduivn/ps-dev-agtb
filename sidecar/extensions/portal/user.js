6872db0769c169fae2db80e299ce43aa107dc10b
Rob Levin <rlevin@sugarcrm.com>
2012-06-22 14:32:14 -0700

    remove comments

--- sidecar/extensions/portal/user.js
+++ sidecar/extensions/portal/user.js
@@ -11,19 +11,6 @@
      * @return {Boolean} true if user is of type: support_portal, otherwise false (and user is a "normal user").
      */
     app.user.isSupportPortalUser = function() {
-/*
-current_user: {timezone:null, datepref:m/d/Y, timepref:H:i, type:support_portal,…}
-account_ids: [b8b6d47a-b7b3-0290-c073-4fe4b90bc2c9]
-0: "b8b6d47a-b7b3-0290-c073-4fe4b90bc2c9"
-datepref: "m/d/Y"
-full_name: "Ruben Pick"
-id: "10a846d9-8b14-9f30-16d0-4fe4b952968d"
-timepref: "H:i"
-timezone: null
-type: "support_portal"
-user_id: "4ee508e9-4578-d13b-da2c-4fe4be78e573"
-user_name: "support_portal"
-*/
         return this.get('type') === 'support_portal';
     };
 
