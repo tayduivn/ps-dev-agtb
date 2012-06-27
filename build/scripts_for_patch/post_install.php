a26ad36199e926588ea47389374f21f92bf70db4
Andrija Vucinic <avucinic@sugarcrm.com>
2012-06-27 16:18:08 +0200

    Bug 53935 + post_install.php 	- add sendEmailReminders job if it doesn't exist after the upgrade

--- build/scripts_for_patch/post_install.php
+++ build/scripts_for_patch/post_install.php
@@ -459,7 +459,7 @@ function post_install() {
     }
 
     if($origVersion < '651') {
-        // add cleanup job if not there
+        // add cleanJobQueue job if not there
         $job = new Scheduler();
         $job->retrieve_by_string_fields(array("job" => 'function::cleanJobQueue'));
         if(empty($job->id)) {
@@ -475,6 +475,23 @@ function post_install() {
             $job->catch_up           = '0';
             $job->save();
         }
+        
+		// add sendEmailReminders job if not there
+		$job = new Scheduler();
+		$job->retrieve_by_string_fields(array("job" => 'function::sendEmailReminders'));
+		if(empty($job->id)) {
+			// not found - create
+			$job->name               = translate('LBL_OOTB_SEND_EMAIL_REMINDERS', 'Schedulers'); 
+			$job->job                = 'function::sendEmailReminders';
+			$job->date_time_start    = "2012-01-01 00:00:01";
+			$job->date_time_end      = "2030-12-31 23:59:59";
+			$job->job_interval       = '*::*::*::*::*';
+			$job->status             = 'Active';
+			$job->created_by         = '1';
+			$job->modified_user_id   = '1';
+			$job->catch_up           = '0';
+			$job->save();
+		}
     }
 }
 
