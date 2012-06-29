d0e3ae5fdc78a44990b2b9f2fef2fd1aa1f47815
Collin Lee <clee@sugarcrm.com>
2012-06-29 03:38:27 -0700

    Formatted code for better readability.

--- sugarcrm/clients/base/fields/toggle/toggle.js
+++ sugarcrm/clients/base/fields/toggle/toggle.js
@@ -2,7 +2,7 @@
     events : { 'click .iphone-toggle-buttons span' : 'toggle' },
 
     toggle : function(event) {
-       var include = this.model.get('forecast') === true ? false : true;
+       var include = (this.model.get('forecast') === true) ? false : true;
        this.model.set('forecast', include);
        this.model.set('id', this.model.get('id'));
        this.model.url = 'rest/v10/modules/Opportunities';
