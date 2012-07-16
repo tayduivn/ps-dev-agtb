f3bcb24341a764123039395e708aad61112e741a
John Barlow <jbarlow@sugarcrm.com>
2012-07-16 10:49:37 -0400

    Story 32622513: Adding auto calc back in on the sales rep worksheet.

--- sugarcrm/clients/forecasts/views/forecastsWorksheet/forecastsWorksheet.js
+++ sugarcrm/clients/forecasts/views/forecastsWorksheet/forecastsWorksheet.js
@@ -155,6 +155,10 @@
                 this.updateWorksheetBySelectedTimePeriod({id: defaultValues.timeperiod_id});
                 this.updateWorksheetBySelectedCategory({id: defaultValues.category});
             }, this);
+            this.context.forecasts.worksheet.on("change", function() {
+            	this.calculateTotals();
+            	this.totalView.render();
+            }, this);
         }
     },
 
