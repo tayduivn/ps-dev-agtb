db99f92e079c2976e4bbf478cb5c1a2f41b84009
Collin Lee <clee@sugarcrm.com>
2012-07-17 14:45:50 -0400

    Fixed erroneous require_once path for SugarFieldEnum.php.

--- sugarcrm/include/SugarFields/Fields/Timeperiod/SugarFieldTimeperiod.php
+++ sugarcrm/include/SugarFields/Fields/Timeperiod/SugarFieldTimeperiod.php
@@ -2,7 +2,7 @@
 /*********************************************************************************
  * The contents of this file are subject to
  * *******************************************************************************/
-require_once('include/SugarFields/Fields/Base/SugarFieldEnum.php');
+require_once('include/SugarFields/Fields/Enum/SugarFieldEnum.php');
 
 class SugarFieldTimeperiod extends SugarFieldEnum {
 
