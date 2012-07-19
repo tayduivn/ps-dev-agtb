5cc117776ac50a528d95dce5d0ed49c746fa5b3a
Collin Lee <clee@sugarcrm.com>
2012-07-19 12:29:21 -0400

    Updated jasmine test.  We are expecting the sidecar update to be approved so we just test the def setting instead of viewName.

--- sugarcrm/tests/clients/forecasts/suites/fields/toggle.js
+++ sugarcrm/tests/clients/forecasts/suites/fields/toggle.js
@@ -1,33 +1,36 @@
 describe("toggle field", function() {
 
-    beforeEach(function() {
-        var fieldDef = {
-            "name": "forecast",
-            "type": "toggle"
-        };
-        var controller = SugarFieldTest.loadSugarField('toggle/toggle');
-        this.field = SugarFieldTest.createField("forecast", "toggle");
-        var model = new Backbone.Model({forecast: true});
-        this.field = _.extend(this.field, controller);
-        this.field.def = fieldDef;
-        this.field.model = model;
-    });
-
     describe("test detail view", function() {
         it("should show detail view", function() {
-           var optionsDef = {viewName : 'detail'};
-           this.field.options.def = optionsDef;
-           var output = this.field._render();
-           expect(output.options.viewName).toEqual(optionsDef.viewName);
+            var fieldDef = {
+                "name": "forecast",
+                "type": "toggle",
+                "view": "detail"
+            };
+            var controller = SugarFieldTest.loadSugarField('toggle/toggle');
+            this.field = SugarFieldTest.createField("forecast", "toggle", "detail", fieldDef);
+            var model = new Backbone.Model({forecast: true});
+            this.field = _.extend(this.field, controller);
+            this.field.def = fieldDef;
+            this.field.model = model;
+            expect(this.field.def.view).toEqual(fieldDef.view);
         });
     });
 
     describe("test default (edit) view", function() {
         it("should show default (edit) view", function() {
-           var optionsDef = {viewName : 'default'};
-           this.field.options.def = optionsDef;
-           var output = this.field._render();
-           expect(output.options.viewName).toEqual(optionsDef.viewName);
+            var fieldDef = {
+                "name": "forecast",
+                "type": "toggle",
+                "view": "default"
+            };
+            var controller = SugarFieldTest.loadSugarField('toggle/toggle');
+            this.field = SugarFieldTest.createField("forecast", "toggle", "default", fieldDef);
+            var model = new Backbone.Model({forecast: true});
+            this.field = _.extend(this.field, controller);
+            this.field.def = fieldDef;
+            this.field.model = model;
+            expect(this.field.def.view).toEqual(fieldDef.view);
         });
     });
 });
