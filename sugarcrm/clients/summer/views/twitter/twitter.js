194dbd04aa79a755c410c694d7cf2a56ba8c4c63
Khanh Dao <kdao@sugarcrm.com>
2012-07-25 11:58:47 -0700

    fix twittwr widget

--- sugarcrm/clients/summer/views/twitter/twitter.js
+++ sugarcrm/clients/summer/views/twitter/twitter.js
@@ -4,45 +4,24 @@
         app.view.View.prototype.initialize.call(this,options);
     },
 
-    injectTwitter: function( ) {
-        console.log('inject twitter');
+    getTweets: function () {
         var self = this;
+        var twitter = 'sugarcrm';
+        $.ajax({
+            url: "http://twitter.com/statuses/user_timeline/" + twitter + ".json?count=5&callback=?",
+            dataType: "jsonp",
+            success: function (data) {
+                self.tweets = data;
+                app.view.View.prototype._renderHtml.call(self);
+            },
+            context: this
+        });
 
-        var script = "";
-        script += "<script>";
-        script += "new TWTR.Widget({";
-        script +=    "version: 2,";
-        script += "type: 'profile',";
-        script += "rpp: 4,";
-        script +=  "interval: 30000,";
-        script +=    "width: 250,";
-        script +=    "height: 300,";
-        script +=     "theme: {";
-        script +=     "shell: {";
-        script +=     "background: '#333333',";
-        script +=     "color: '#ffffff'";
-        script +=   "},";
-        script += "tweets: {";
-        script +=    "background: '#000000',";
-        script +=   "color: '#ffffff',";
-        script +=     "links: '#4aed05'";
-        script +=   "}";
-        script += "},";
-        script += "features: {";
-        script +=    "scrollbar: false,";
-        script +=    "loop: false,";
-        script +=     "live: false,";
-        script +=    "behavior: 'all'";
-        script +=    "}";
-        script += "}).render().setUser('sugarcrm').start();";
-        script += "</script>";
-        this.$(".twitter-profile").append(script);
-        
     },
 
     bindDataChange: function () {
         var self = this;
-        this.model.on('change', function () { self.injectTwitter(); }, this);
+        this.model.on('change', self.getTweets, this);
     }
 
 })
