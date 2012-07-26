958b0689ca70217c4e2d34ddc73d33d75d9698c3
Maruchi Kim <mkim@sugarcrm.com>
2012-07-26 13:05:31 -0700

    google plus widget and templating

--- sugarcrm/clients/summer/views/gplus/gplus.js
+++ sugarcrm/clients/summer/views/gplus/gplus.js
@@ -7,6 +7,7 @@
         var self = this;
         var api_key = "AIzaSyCIXeFNDztbmOPSX9jA1eRAzGzDmCmM9Ig";  // kdao api_key
         //var googlePlus_id = "100975726761624703436";   // sugarcrm id
+        
         var googlePlus_id = this.model.get('google_plus');
         $.ajax ({
             url : "https://www.googleapis.com/plus/v1/people/" + googlePlus_id + "?key=" + api_key,
@@ -27,11 +28,62 @@
             dataType: "json",
             success: function ( data ) {
                 self.gPlusPosts = [];
+                var date = "";
                 for (var i=0; i < data.items.length; i++ ) {
+                    date ="";
+                    date+=data.items[i].published.substring(5,7);
+                    if (date == '01'){
+                        date = 'January';
+                    }
+                    if (date == '02'){
+                        date = 'February';
+                    }
+                    if (date == '03'){
+                        date = 'March';
+                    }
+                    if (date == '04'){
+                        date = 'April';
+                    }
+                    if (date == '05'){
+                        date = 'May';
+                    }
+                    if (date == '06'){
+                        date = 'June';
+                    }
+                    if (date == '07'){
+                        date = 'July';
+                    }
+                    if (date == '08'){
+                        date = 'August';
+                    }
+                    if (date == '09'){
+                        date = 'September';
+                    }
+                    if (date == '10'){
+                        date = 'October';
+                    }
+                    if (date == '11'){
+                        date = 'November';
+                    }
+                    if (date == '12'){
+                        date = 'December';
+                    }
+
+                    date += " ";
+                    date += data.items[i].published.substring(8,10);
+                    date += ", ";
+                    date += data.items[i].published.substring(0,4);
+                    date += " - ";
+
+
+
                     self.gPlusPosts.push ( {
+                        image: self.gPlusImageUrl,
                         content: data.items[i].title,
                         author: data.items[i].object.displayName,
-                        attachments: data.items[i].object.attachments
+                        attachments: data.items[i].object.attachments,
+                        published: date,
+                        url: data.items[i].url
                     });
                 }
                 self.render();
@@ -46,6 +98,7 @@
     bindDataChange: function () {
         var self = this;
         this.model.on( "change", self.getData, this );
-    }
+    },
+
 
 })
