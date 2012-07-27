609f8be7e59f3b14582a53252f409005a6386489
Khanh Dao <kdao@sugarcrm.com>
2012-07-26 17:01:50 -0700

    put some modified lib modules for image search & linkedin

--- sugarcrm/modules/Accounts/metadata/summer/layouts/sidebar.php
+++ sugarcrm/modules/Accounts/metadata/summer/layouts/sidebar.php
@@ -1,12 +1,19 @@
 <?php
 require_once('clients/summer/SideBarLayout.php');
 $layout = new SideBarLayout();
+//$layout->push('main', array('view'=>'countrychart'));
 //$layout->push('main', array('view'=>'crunchbase'));
-$layout->push('main', array('view'=>'gplus'));
-$layout->push('main', array('view'=>'maps'));
-$layout->push('main', array('view'=>'news'));
-$layout->push('main', array('view'=>'twitter'));
-$layout->push('main', array('view'=>'imagesearch'));
-
+//$layout->push('main', array('view'=>'currencyconverter'));
+//$layout->push('main', array('view'=>'exchangerates'));
+//$layout->push('main', array('view'=>'facebook'));
+//$layout->push('main', array('view'=>'gplus'));
+//$layout->push('main', array('view'=>'header'));
+//$layout->push('main', array('view'=>'imagesearch'));
+$layout->push('main', array('view'=>'linkedin'));
+//$layout->push('main', array('view'=>'maps'));
+//$layout->push('main', array('view'=>'metrics'));
+//$layout->push('main', array('view'=>'news'));
+//$layout->push('main', array('view'=>'trends'));
+//$layout->push('main', array('view'=>'twitter'));
 
 $viewdefs['Accounts']['summer']['layout']['sidebar'] = $layout->getLayout();
