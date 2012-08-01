738a63acebb3ad797faebf721ccc1d26db4cbda9
Neil Harding <nharding@sugarcrm.com>
2012-08-01 14:36:25 -0400

    An initial edit layout - will likely need to change.

--- sugarcrm/clients/base/layouts/edit/edit.php
+++ sugarcrm/clients/base/layouts/edit/edit.php
@@ -1,8 +1,32 @@
 <?php
-/**
- * Created by JetBrains PhpStorm.
- * User: dwheeler
- * Date: 4/2/12
- * Time: 4:51 PM
- * To change this template use File | Settings | File Templates.
- */
+$viewdefs['base']['layout']['edit'] = array(
+    'type' => 'simple',
+    'components' =>
+    array(
+        0 => array(
+            'view' => 'subnav',
+        ),
+        1 => array(
+            'layout' =>
+            array(
+                'type' => 'fluid',
+                'components' =>
+                array(
+                    0 => array(
+                        'layout' =>
+                        array(
+                            'type' => 'simple',
+                            'span' => 11,
+                            'components' =>
+                            array(
+                                0 => array(
+                                    'view' => 'edit',
+                                ),
+                            ),
+                        ),
+                    ),
+                ),
+            ),
+        ),
+    ),
+);
