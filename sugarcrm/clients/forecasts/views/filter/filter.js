679ec36537af3627d83647ab8cda9de3bbea014a
Gabriel Rael <grael@sugarcrm.com>
2012-08-06 14:34:32 -0400

    Adds selectedCategory sets to forecasts model, but still not rendering chosen field properly on selections.

--- sugarcrm/clients/forecasts/views/filter/filter.js
+++ sugarcrm/clients/forecasts/views/filter/filter.js
@@ -21,9 +21,11 @@
         if (field.name == 'category') {
             if (app.config.showBuckets) {
                 field.def.options = 'commit_stage_dom';
+                field.value = "70";
                 field.def.value = "70";
             } else {
                 field.def.options = 'forecasts_filters_category';
+                field.value = "Committed"; //this should work to set the value of the select field, but it is getting reset somewhere in sidecar processing
                 field.def.value = "Committed";
             }
             field.def.multi = app.config.showBuckets;
@@ -45,9 +47,26 @@
          * @param input the (de)selection
          * @private
          */
+        // TODO: this may need to go in bindDomChange instead of a new function...
         field._updateSelections = function(event, input) {
-            // TODO:  this
+            var selectElement = this.$el.find("select");
+
+            if(this.def.multi) { // if it's a multiselect we need to add or drop the correct values from the filter model
+                // TODO:  This needs to be implemented across all views affected by filters, based on the decisions made for
+                //  how buckets get defined.
+                if (_.has(input, "selected")) {
+                    var id = input.selected;
+                } else if(_.has(input, "deselected")) {
+                }
+            } else {  // not multi, just set the selected filter
+                var id = input.selected;
+//                this.view.context.forecasts.filters.set('category', id);
+                // TODO:  This needs to be fixed throughout to be {id: label} or something similar to be in line with buckets
+                this.view.context.forecasts.set('selectedCategory', {id: id, label:app.lang.getAppListStrings(this.def.options)[id]});
+                selectElement.val(id);
+            }
         };
+
         field.events = _.extend({"change select": "_updateSelections"}, field.events);
         return field;
     }
