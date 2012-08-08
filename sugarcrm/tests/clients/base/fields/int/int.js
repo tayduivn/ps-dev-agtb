81f608572974e17cd405b248a534466466b46c21
Rob Levin <rlevin@sugarcrm.com>
2012-08-08 15:22:49 -0700

    Cleaning up app.view.* after specs run.

--- sugarcrm/tests/clients/base/fields/int/int.js
+++ sugarcrm/tests/clients/base/fields/int/int.js
@@ -1,8 +1,21 @@
 describe("sugarfields", function() {
 
+    var app, field;
+
+    beforeEach(function() {
+        app = SugarTest.app;
+        field = SugarTest.createField("base","int", "int", "detail", { number_group_seperator: "," });
+    });
+
+    afterEach(function() {
+        app.cache.cutAll();
+        app.view.reset();
+        delete Handlebars.templates;
+        field = null;
+    });
+
     describe("integer", function() {
         it("should format the value", function() {
-            var field = SugarTest.createField("base","int", "int", "detail", { number_group_seperator: "," });
             expect(field.format("123456.502")).toEqual("123,457");
             expect(field.unformat("123456.498")).toEqual("123456");
         });
