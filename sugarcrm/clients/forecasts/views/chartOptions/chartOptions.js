1c1e81c363bf401b37bd3b30fd2f47e23ad5c0b5
Jon Whitcraft <jwhitcraft@sugarcrm.com>
2012-08-14 11:12:52 -0400

    Bug 55108 - Hide Group By Select box when selected User is a manager
    Hide the group by selected user box when the user is a manager.

--- sugarcrm/clients/forecasts/views/chartOptions/chartOptions.js
+++ sugarcrm/clients/forecasts/views/chartOptions/chartOptions.js
@@ -7,6 +7,51 @@
 ({
 
     /**
+     * Current Selected User
+     */
+    selectedUser: null,
+
+    /**
+     * Fields created in this class
+     */
+    fields : [],
+
+    /**
+     * Initialize because we need to set the selectedUser variable
+     * @param options
+     */
+    initialize:function (options) {
+        app.view.View.prototype.initialize.call(this, options);
+        this.selectedUser = {id: app.user.get('id'), isManager:app.user.get('isManager'), showOpps : false};
+    },
+
+    /**
+     * Watch for the selectedUser Change
+     */
+    bindDataChange: function() {
+
+        var self = this;
+
+        if(this.context && this.context.forecasts) {
+            this.context.forecasts.on("change:selectedUser", function(context, user) {
+                self.selectedUser = {id: user.id, "isManager":user.isManager, showOpps: user.showOpps};
+                this.toggleGroupByFieldVisibility();
+            }, this);
+        }
+    },
+
+    /**
+     * Method to toggle the field visibility of the group by field
+     */
+    toggleGroupByFieldVisibility: function() {
+        if(!_.isUndefined(this.fields['group_by']) && this.selectedUser.isManager && this.selectedUser.showOpps === false) {
+            this.fields['group_by'].$el.hide();
+        } else {
+            this.fields['group_by'].$el.show();
+        }
+    },
+
+    /**
      * Overriding _renderField because we need to set up the events to set the proper value depending on which field is
      * being changed.
      * binary for forecasts and adjusts the category filter accordingly
@@ -16,6 +61,12 @@
     _renderField: function(field) {
         field = this._setUpCategoryField(field);
         app.view.View.prototype._renderField.call(this, field);
+
+        // save the fields for later use
+        this.fields[field.name] = field;
+
+        // toggle the visibility of the group by field for the initial render
+        this.toggleGroupByFieldVisibility();
     },
 
     /**
