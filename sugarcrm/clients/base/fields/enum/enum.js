0efc7f1cdca6b8d4d3fed03beb660e659595be21
Rob Levin <rlevin@sugarcrm.com>
2012-08-21 17:49:51 -0700

    Changed multi format returned by server from ^foo^,^bar^ to ['foo','bar']. Fixed multienum detail view.

--- sugarcrm/clients/base/fields/enum/enum.js
+++ sugarcrm/clients/base/fields/enum/enum.js
@@ -5,6 +5,18 @@
         this.$(this.fieldTag).chosen();
         this.$(".chzn-container").addClass("tleft");
         return this;
+    },
+    unformat:function(value) {
+        return value;
+    },
+    format:function(value) {
+        var newval = '';
+        if(this.def.isMultiSelect && this.view.name === 'detail') {
+            _.each(value, function(el) {
+                newval += el + ', ';
+            });
+            newval = newval.slice(0, newval.length - 2); // strips extra ', '
+        }
+        return newval;
     }
-
 })
