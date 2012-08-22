00194e35b7dd4fd6c5163a3ee64c5720625eaf36
Majed Itani <mitani@gmail.com>
2012-08-22 12:19:56 -0700

    adds the sidebar for the accounts list

--- sugarcrm/modules/Accounts/metadata/summer/layouts/list-sidebar.php
+++ sugarcrm/modules/Accounts/metadata/summer/layouts/list-sidebar.php
@@ -3,4 +3,4 @@ require_once('clients/summer/SideBarLayout.php');
 $layout = new SideBarLayout();
 $layout->push('main', array('view'=>'countrychart', 'context'=>array('source'=>'SalesByCountry')));
 
-$viewdefs['Accounts']['summer']['layout']['listsidebar'] = $layout->getLayout();
+$viewdefs['Accounts']['summer']['layout']['list-sidebar'] = $layout->getLayout();
