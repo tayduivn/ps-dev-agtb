a06df2f5f7acf4de9f2a8ea04143233bed97e964
Peter D <ixce@projectixce.net>
2012-08-22 20:50:05 -0700

    Added labels for create

--- sugarcrm/modules/Opportunities/language/en_us.lang.php
+++ sugarcrm/modules/Opportunities/language/en_us.lang.php
@@ -103,6 +103,7 @@ $mod_strings = array (
 //BEGIN SUGARCRM flav=pro ONLY
   'LNK_OPPORTUNITY_REPORTS' => 'View Opportunity Reports',
 //END SUGARCRM flav=pro ONLY
+    'LNK_CREATE' => 'Create Deal',
   'LNK_OPPORTUNITY_LIST' => 'View Opportunities',
   'ERR_DELETE_RECORD' => 'A record number must be specified to delete the opportunity.',
   'LBL_TOP_OPPORTUNITIES' => 'My Top Open Opportunities',
