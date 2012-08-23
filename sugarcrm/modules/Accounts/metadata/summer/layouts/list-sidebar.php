11a7ce67801df6f36a4b8bb6825f2345e06b1d27
Hai Qi <hqi@sugarcrm.com>
2012-08-22 21:24:00 -0700

    Added activity stream to accounts list view

--- sugarcrm/modules/Accounts/metadata/summer/layouts/list-sidebar.php
+++ sugarcrm/modules/Accounts/metadata/summer/layouts/list-sidebar.php
@@ -2,5 +2,5 @@
 require_once('clients/summer/SideBarLayout.php');
 $layout = new SideBarLayout();
 $layout->push('main', array('view'=>'countrychart', 'context'=>array('source'=>'SalesByCountry')));
-
+$layout->push('main',array('view'=>'activitystream'));
 $viewdefs['Accounts']['summer']['layout']['list-sidebar'] = $layout->getLayout();
