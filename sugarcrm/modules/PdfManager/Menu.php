68ec6fa1e45533a32bd6aca7ed83825f0dda3773
cmourizard <cedric.mourizard@synolia.com>
2012-08-28 09:47:53 +0200

    Bug #55656 Code review - Change ACLController::checkAccess to SugarACL::checkAccess

--- sugarcrm/modules/PdfManager/Menu.php
+++ sugarcrm/modules/PdfManager/Menu.php
@@ -29,7 +29,7 @@
 //FILE SUGARCRM flav=pro ONLY
 global $mod_strings, $sugar_config, $app_strings;
 
-if(ACLController::checkAccess('PdfManager', 'edit', true))$module_menu[] =Array("index.php?module=PdfManager&action=EditView&return_module=PdfManager&return_action=DetailView", $mod_strings['LNK_NEW_RECORD'],"CreatePdfManager");
-if(ACLController::checkAccess('PdfManager', 'list', true))$module_menu[] =Array("index.php?module=PdfManager&action=index&return_module=PdfManager&return_action=index", $mod_strings['LNK_LIST'],"PdfManager");
+if(SugarACL::checkAccess('PdfManager', 'edit', true))$module_menu[] =Array("index.php?module=PdfManager&action=EditView&return_module=PdfManager&return_action=DetailView", $mod_strings['LNK_NEW_RECORD'],"CreatePdfManager");
+if(SugarACL::checkAccess('PdfManager', 'list', true))$module_menu[] =Array("index.php?module=PdfManager&action=index&return_module=PdfManager&return_action=index", $mod_strings['LNK_LIST'],"PdfManager");
 
 $module_menu[] =Array("index.php?return_module=PdfManager&return_action=index&module=Configurator&action=SugarpdfSettings", $mod_strings['LNK_REPORT_CONFIG'], "PdfManager");
