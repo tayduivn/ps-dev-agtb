c0b40c07d103e9a3d6d4445f65a14bd3e35caf49
justinpark <jpark@sugarcrm.com>
2012-08-29 13:40:30 -0700

    seperates popup containers from modal layout widget
    Resolved Conflicts:
    
    	sugarcrm/clients/base/layouts/edit/edit.php
    	sugarcrm/clients/base/layouts/popup-detail/popup-detail.php
    	sugarcrm/clients/base/layouts/popup-edit/popup-edit.php
    	sugarcrm/clients/base/layouts/popup-list/popup-list.js
    	sugarcrm/clients/base/layouts/popup-list/popup-list.php
    	sugarcrm/clients/base/views/modal-header/modal-header.hbt
    	sugarcrm/clients/base/views/modal-header/modal-header.js
    	sugarcrm/clients/base/views/popup-edit/popup-edit.hbt
    
    Conflicts:
    
    	sugarcrm/clients/base/layouts/edit/edit.php

--- sugarcrm/clients/base/layouts/edit/edit.php
+++ sugarcrm/clients/base/layouts/edit/edit.php
@@ -1,4 +1,5 @@
 <?php
+<<<<<<< HEAD
 /**
  * Created by JetBrains PhpStorm.
  * User: dwheeler
@@ -6,3 +7,36 @@
  * Time: 4:51 PM
  * To change this template use File | Settings | File Templates.
  */
+=======
+$viewdefs['base']['layout']['edit'] = array(
+    'type' => 'simple',
+    'components' =>
+    array(
+        0 => array(
+            'view' => 'subnav',
+        ),
+        1 => array(
+            'layout' =>
+            array(
+                'type' => 'fluid',
+                'components' =>
+                array(
+                    0 => array(
+                        'layout' =>
+                        array(
+                            'type' => 'simple',
+                            'span' => 11,
+                            'components' =>
+                            array(
+                                0 => array(
+                                    'view' => 'edit',
+                                ),
+                            ),
+                        ),
+                    ),
+                ),
+            ),
+        ),
+    ),
+);
+>>>>>>> 3bf7542... seperates popup containers from modal layout widget
