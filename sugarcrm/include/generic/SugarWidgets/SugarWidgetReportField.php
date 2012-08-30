0f85b1b1c583d2cd98303e55a116ed19c064cb77
Muddsar Jamil <mjamil@sugarcrm.com>
2012-08-30 14:52:31 -0700

    adding comment to trigger upgrade package

--- sugarcrm/include/generic/SugarWidgets/SugarWidgetReportField.php
+++ sugarcrm/include/generic/SugarWidgets/SugarWidgetReportField.php
@@ -248,6 +248,7 @@ class SugarWidgetReportField extends SugarWidgetField
 		return 'primaryid';
 	}
 
+     // this comment is being added to trigger the upgrade package
         if ( ! empty($layout_def['group_function']) && $layout_def['group_function']=='count')
         {
                 return 'count';
