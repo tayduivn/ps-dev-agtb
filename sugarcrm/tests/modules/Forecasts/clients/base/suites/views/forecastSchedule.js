238576006441ddf8a360db6ad305466953f4bdd4
Travis Hubbard <thubbard@sugarcrm.com>
2012-09-04 10:07:31 -0400

    jasmine tests moved

--- sugarcrm/tests/modules/Forecasts/clients/base/suites/views/forecastSchedule.js
+++ sugarcrm/tests/modules/Forecasts/clients/base/suites/views/forecastSchedule.js
@@ -1,8 +1,7 @@
-//FILE SUGARCRM flav=pro ONLY
 /*********************************************************************************
- * The contents of this file are subject to the SugarCRM Enterprise End User
- * License Agreement ("License") which can be viewed at
- * http://www.sugarcrm.com/crm/products/sugar-enterprise-eula.html
+ * The contents of this file are subject to the SugarCRM Master Subscription
+ * Agreement ("License") which can be viewed at
+ * http://www.sugarcrm.com/crm/master-subscription-agreement
  * By installing or using this file, You have unconditionally agreed to the
  * terms and conditions of the License, and You may not use this file except in
  * compliance with the License.  Under the terms of the license, You shall not,
@@ -23,8 +22,9 @@
  * Your Warranty, Limitations of liability and Indemnity are expressly stated
  * in the License.  Please refer to the License for the specific language
  * governing these rights and limitations under the License.  Portions created
- * by SugarCRM are Copyright (C) 2004-2006 SugarCRM, Inc.; All Rights Reserved.
+ * by SugarCRM are Copyright (C) 2004-2012 SugarCRM, Inc.; All Rights Reserved.
  ********************************************************************************/
+
 
 describe("The expected opportunities view tests", function(){
 
