bd073e14b899952ce2b33e0d6efd94dbb0dec0b2
Peter D <ixce@projectixce.net>
2012-09-04 11:15:46 -0700

    Deleted old files

--- sugarcrm/modules/Accounts/metadata/summer/layouts/new-sidebar.php
+++
@@ -1,5 +0,0 @@
-<?php
-require_once('clients/summer/SideBarLayout.php');
-$layout = new SideBarLayout();
-$layout->push("main", array('view' => 'createhelp'));
-$viewdefs['Accounts']['summer']['layout']['new-sidebar'] = $layout->getLayout();
