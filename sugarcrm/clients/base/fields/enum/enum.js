3e63d181dd473dc6b3587f9196222fb791c7af16
Rob Levin <rlevin@sugarcrm.com>
2012-09-07 12:03:56 -0700

    Fixed issue that keys not labels were getting displayed; also, list view now works.

--- sugarcrm/clients/base/fields/enum/enum.js
+++ sugarcrm/clients/base/fields/enum/enum.js
@@ -10,13 +10,22 @@
         return value;
     },
     format:function(value) {
-        var newval = '';
-        if(this.def.isMultiSelect && this.view.name === 'detail') {
-            _.each(value, function(element) {
-                newval += element + ', ';
+        var newval = '', optionsObject, optionLabels;
+
+        if(this.def.isMultiSelect && this.view.name !== 'edit') {
+
+            // Gets the dropdown options e.g. {foo:foolbl, bar:barlbl ...}
+            optionsObject = app.lang.getAppListStrings(this.def.options);
+
+            // value are selected option keys .. grab corresponding labels
+            _.each(value, function(p) {
+                if(_.has(optionsObject, p)) {
+                    newval += optionsObject[p]+', ';
+                }
             });
             newval = newval.slice(0, newval.length - 2); // strips extra ', '
         } else {
+            // Normal dropdown, just get selected
             newval = this.model.get(this.name);
         }
         return newval;
