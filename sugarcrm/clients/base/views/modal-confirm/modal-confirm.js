dfd48fecd69392390448e740e4eef9a366e7500d
justinpark <jpark@sugarcrm.com>
2012-09-07 16:59:06 -0700

    Removed _.pick for simplicity case clean out setting context title and message Fixed hbt template for modal-header button

--- sugarcrm/clients/base/views/modal-confirm/modal-confirm.js
+++ sugarcrm/clients/base/views/modal-confirm/modal-confirm.js
@@ -3,10 +3,14 @@
         'click [name=close_button]' : 'close',
         'click [name=ok_button]' : 'ok'
     },
+    initialize: function(options) {
+        this.message = options.message;
+        app.view.View.prototype.initialize.call(this, options);
+    },
     close: function(evt) {
-        this.context.parent.trigger("modal:close");
+        this.layout.context.trigger("modal:close");
     },
     ok: function(evt) {
-        this.context.parent.trigger("modal:callback");
+        this.layout.context.trigger("modal:callback");
     }
 })
--- sugarcrm/clients/base/views/modal-header/modal-header.js
+++ sugarcrm/clients/base/views/modal-header/modal-header.js
@@ -5,10 +5,10 @@
     close: function() {
         this.layout.hide();
     },
-    _renderHtml: function() {
-        var self = this;
-        this.title = this.context.get("title") || '&nbsp;';
-        this.buttons = this.layout.context.get("buttons");
-        return app.view.View.prototype._renderHtml.call(this);
+    setTitle: function(title) {
+        this.title = title;
+    },
+    setButton: function(buttons) {
+        this.buttons = buttons;
     }
 })
