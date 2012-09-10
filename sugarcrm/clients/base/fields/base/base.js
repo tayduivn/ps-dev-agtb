d721bb694b2d661c4e32439210389ddd764a255a
justinpark <jpark@sugarcrm.com>
2012-09-10 12:03:38 -0700

    omitted eval stuff and using delegateEvents

--- sugarcrm/clients/base/fields/base/base.js
+++ sugarcrm/clients/base/fields/base/base.js
@@ -1,6 +1,18 @@
 ({
+    initialize: function(options) {
+        app.view.Field.prototype.initialize.call(this, options);
+        if(this.def.events) {
+            var self = this,
+                _customEvents = {};
+
+            _.each(this.def.events, function(function_code, evtType) {
+                _customEvents[evtType + " a[href]"] =  function_code;
+            });
+            self.delegateEvents(_customEvents);
+        }
+    },
     render: function() {
-        var action = "view";
+        var action = "view"
         if (this.def.link && this.def.route) {
             action = this.def.route.action;
         }
@@ -8,26 +20,5 @@
             this.def.link = false;
         };
         app.view.Field.prototype.render.call(this);
-    },
-    _renderHtml: function() {
-        var result = app.view.Field.prototype._renderHtml.call(this);
-        if(this.def.events) {
-            var self = this;
-            _.each(this.def.events, function(function_code, evtType) {
-                try{
-                    eval('var _function = ' + function_code);
-                } catch(e) {
-                    app.logger.error("Failed to eval custom onclick event function.\n" + e);
-                    // TODO: Trigger app:error event
-                }
-                if(_.isFunction(_function)) {
-                    self.$('a[href]').off(evtType).on(evtType, function(evt){
-                        _function.call(self, evt);
-                    });
-                }
-            });
-        }
-
-        return result;
     }
 })
--- sugarcrm/clients/base/fields/button/button.js
+++ sugarcrm/clients/base/fields/button/button.js
@@ -1,29 +1,20 @@
 ({
+    initialize: function(options) {
+        app.view.Field.prototype.initialize.call(this, options);
+        if(this.def.events) {
+            var self = this,
+                _customEvents = {};
+
+            _.each(this.def.events, function(function_code, evtType) {
+                _customEvents[evtType + " .btn"] =  function_code;
+            });
+            self.delegateEvents(_customEvents);
+        }
+    },
     _render:function(){
         // buttons use the value property in metadata to denote their action for acls
         if (app.acl.hasAccessToModel(this.def.value, this.model, this)) {
             app.view.Field.prototype._render.call(this);
         }
-    },
-    _renderHtml: function() {
-        var result = app.view.Field.prototype._renderHtml.call(this);
-        if(this.def.events) {
-            var self = this;
-            _.each(this.def.events, function(function_code, evtType) {
-                try{
-                    eval('var _function = ' + function_code);
-                } catch(e) {
-                    app.logger.error("Failed to eval custom onclick event function.\n" + e);
-                    // TODO: Trigger app:error event
-                }
-                if(_.isFunction(_function)) {
-                    self.$('.btn').off(evtType).on(evtType, function(evt){
-                        _function.call(self, evt);
-                    });
-                }
-            });
-        }
-
-        return result;
     }
 })
