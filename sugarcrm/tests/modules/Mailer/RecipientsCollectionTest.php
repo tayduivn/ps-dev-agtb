d409342a4b3ae77080b2e42ba64d0d8ca567ebec
Greg Levine <glevine@sugarcrm.com>
2012-09-10 14:25:41 -0400

    Added unit tests for RecipientsCollection.

--- sugarcrm/tests/modules/Mailer/RecipientsCollectionTest.php
+++ sugarcrm/tests/modules/Mailer/RecipientsCollectionTest.php
@@ -23,65 +23,173 @@ require_once('modules/Mailer/RecipientsCollection.php');
 
 class RecipientsCollectionTest extends Sugar_PHPUnit_Framework_TestCase
 {
-	/*public function testAddRecipients_useAddTo_PassInAnEmailIdentity_NoInvalidRecipientsReturned() {
-		$recipientsCollection = new RecipientsCollection();
-		$recipient = new EmailIdentity('foo@bar.com', 'Foo Bar');
+    /**
+     * This test essentially tests clearAll, clearTo, clearCc and clearBcc.
+     *
+     * @group mailer
+     */
+    public function testClearAll_ResultIsSuccessful() {
+        $recipientsCollection = new RecipientsCollection();
 
-		$expected = 0;
-		$actual = $recipientsCollection->addRecipients($recipient);
-		self::assertEquals($expected, count($actual), "No invalid recipients should have been returned");
-	}
+        $to = array(
+            new EmailIdentity("foo@bar.com", "Foo Bar"),
+            new EmailIdentity("qux@baz.net"),
+        );
+        $invalidTos = $recipientsCollection->addRecipients($to);
 
-	public function testAddRecipients_useAddTo_PassInAString_ReturnsTheRecipient() {
-		$recipientsCollection = new RecipientsCollection();
-		$recipient = 'foo@bar.com';
+        $bcc = array(
+            new EmailIdentity("abc@123.com"),
+            new EmailIdentity("tester@test.org"),
+        );
+        $invalidBccs = $recipientsCollection->addRecipients($bcc, RecipientsCollection::FunctionAddBcc);
 
-		$expected = 1;
-		$actual = $recipientsCollection->addRecipients($recipient);
-		self::assertEquals($expected, count($actual), "No invalid recipients should have been returned");
-		self::assertEquals($recipient, $actual[0]);
-	}
+        // make sure the recipients have been added
+        $expected = 4;
+        $allRecipients = $recipientsCollection->getAll();
+        $actual = count($allRecipients['to']) + count($allRecipients['cc']) + count($allRecipients['bcc']);
+        self::assertEquals($expected, $actual, "{$expected} recipients should have been added");
 
-	public function testAddRecipients_useAddCc_PassInAnArrayOfEmailIdentityObjects_NoInvalidRecipientsReturned() {
-		$recipientsCollection = new RecipientsCollection();
-		$recipients = array(
-			new EmailIdentity('foo@bar.com', 'Foo Bar'),
-			new EmailIdentity('qux@baz.net'),
-		);
+        // now clear all recipients
+        $recipientsCollection->clearAll();
+        $expected = 0;
+        $allRecipients = $recipientsCollection->getAll();
+        $actual = count($allRecipients['to']) + count($allRecipients['cc']) + count($allRecipients['bcc']);
+        self::assertEquals($expected, $actual, "{$expected} recipients should remain");
+    }
 
-		$expected = 0;
-		$actual = $recipientsCollection->addRecipients($recipients, RecipientsCollection::FunctionAddCc);
-		self::assertEquals($expected, count($actual), "No invalid recipients should have been returned");
+    /**
+     * This test essentially tests addRecipients and addTo.
+     *
+     * @group mailer
+     */
+    public function testAddRecipients_UseAddTo_PassInAnEmailIdentity_NoInvalidRecipientsReturned() {
+        $recipientsCollection = new RecipientsCollection();
+        $recipient = new EmailIdentity("foo@bar.com", "Foo Bar");
 
-		$cc = $recipientsCollection->getCc();
-		self::assertEquals($recipients[1]->getEmail(), $cc[1]->getEmail());
-	}
+        $expected = 0;
+        $actual = $recipientsCollection->addRecipients($recipient);
+        self::assertEquals($expected, count($actual), "{$expected} invalid recipients should have been returned");
+    }
 
-	public function testClearAll() {
-		$recipientsCollection = new RecipientsCollection();
-		$to = array(
-			new EmailIdentity('foo@bar.com', 'Foo Bar'),
-			new EmailIdentity('qux@baz.net'),
-		);
-		$bcc = array(
-			new EmailIdentity('abc@123.com'),
-			new EmailIdentity('tester@test.org'),
-		);
+    /**
+     * @group mailer
+     */
+    public function testAddRecipients_UseAddTo_PassInAString_ReturnsTheInvalidRecipient() {
+        $recipientsCollection = new RecipientsCollection();
+        $recipient = "foo@bar.com";
 
-		$invalidTos = $recipientsCollection->addRecipients($to);
-		$invalidBccs = $recipientsCollection->addRecipients($bcc, RecipientsCollection::FunctionAddBcc);
+        $expected = 1;
+        $actual = $recipientsCollection->addRecipients($recipient);
+        self::assertEquals($expected, count($actual), "{$expected} invalid recipient should have been returned");
+        self::assertEquals($recipient, $actual[0], "The identity of the invalid recipient should have been returned");
+    }
 
-		// make sure the recipients have been added
-		$expected = 4;
-		$allRecipients = $recipientsCollection->getAll();
-		$actual = count($allRecipients['to']) + count($allRecipients['cc']) + count($allRecipients['bcc']);
-		self::assertEquals($expected, $actual, "{$expected} recipients should have been added");
+    /**
+     * This test essentially tests addRecipients and addCc.
+     *
+     * @group mailer
+     */
+    public function testAddRecipients_UseAddCc_PassInAnArrayOfEmailIdentityObjects_NoInvalidRecipientsReturned() {
+        $recipientsCollection = new RecipientsCollection();
+        $recipients = array(
+            new EmailIdentity("foo@bar.com", "Foo Bar"),
+            new EmailIdentity("qux@baz.net"),
+        );
 
-		// now clear all recipients
-		$recipientsCollection->clearAll();
-		$expected = 0;
-		$allRecipients = $recipientsCollection->getAll();
-		$actual = count($allRecipients['to']) + count($allRecipients['cc']) + count($allRecipients['bcc']);
-		self::assertEquals($expected, $actual, "No recipients should remain");
-	}*/
+        $expected = 0;
+        $actual = $recipientsCollection->addRecipients($recipients, RecipientsCollection::FunctionAddCc);
+        self::assertEquals($expected, count($actual), "{$expected} invalid recipients should have been returned");
+
+        $expected = 2;
+        $actual = $recipientsCollection->getCc();
+        self::assertEquals($expected, count($actual), "{$expected} recipients should have been added to the CC list");
+
+        $expected = $recipients[1]->getEmail();
+        self::assertEquals($expected, $actual[$expected]->getEmail());
+    }
+
+    /**
+     * This test essentially tests addRecipients and addBcc.
+     *
+     * @group mailer
+     */
+    public function testAddRecipients_UseAddBcc_PassInAnArrayOfEmailIdentityObjectsWithOneInvalidRecipient_OnlyTheInvalidRecipientIsReturned() {
+        $recipientsCollection = new RecipientsCollection();
+        $recipients = array(
+            new EmailIdentity("foo@bar.com", "Foo Bar"),
+            new EmailIdentity("qux@baz.net"),
+            "abc@123.com",
+        );
+
+        $expected = 1;
+        $actual = $recipientsCollection->addRecipients($recipients, RecipientsCollection::FunctionAddBcc);
+        self::assertEquals($expected, count($actual), "{$expected} invalid recipient should have been returned");
+        self::assertEquals($recipients[2], $actual[0], "The identity of the invalid recipient should have been returned");
+
+        $expected = 2;
+        $actual = $recipientsCollection->getBcc();
+        self::assertEquals($expected, count($actual), "{$expected} recipients should have been added to the BCC list");
+
+        $expected = $recipients[1]->getEmail();
+        self::assertEquals($expected, $actual[$expected]->getEmail());
+    }
+
+    /**
+     * This test essentially tests getAll, getTo, getCc and getBcc.
+     *
+     * @group mailer
+     */
+    public function testGetAll_HasRecipients_ReturnsNonEmptyArrays() {
+        $recipientsCollection = new RecipientsCollection();
+
+        $to = array(
+            new EmailIdentity("foo@bar.com", "Foo Bar"),
+            new EmailIdentity("qux@baz.net"),
+        );
+        $invalidTos = $recipientsCollection->addRecipients($to);
+
+        $cc = array(
+            new EmailIdentity("abc@123.com"),
+        );
+        $invalidCcs = $recipientsCollection->addRecipients($cc, RecipientsCollection::FunctionAddCc);
+
+        $bcc = array(
+            new EmailIdentity("tester@test.org"),
+        );
+        $invalidBccs = $recipientsCollection->addRecipients($bcc, RecipientsCollection::FunctionAddBcc);
+
+        $expected = 4;
+        $allRecipients = $recipientsCollection->getAll();
+        $actual = count($allRecipients['to']) + count($allRecipients['cc']) + count($allRecipients['bcc']);
+        self::assertEquals($expected, $actual, "{$expected} recipients should have been added");
+
+        $expected = $to[1]->getEmail();
+        $actual = $allRecipients['to'][$expected]->getEmail();
+        self::assertEquals($expected, $actual, "{$expected} should have been found in the TO list");
+
+        $expected = $cc[0]->getEmail();
+        $actual = $allRecipients['cc'][$expected]->getEmail();
+        self::assertEquals($expected, $actual, "{$expected} should have been found in the CC list");
+
+        $expected = $bcc[0]->getEmail();
+        $actual = $allRecipients['bcc'][$expected]->getEmail();
+        self::assertEquals($expected, $actual, "{$expected} should have been found in the BCC list");
+    }
+
+    /**
+     * This test essentially tests getAll, getTo, getCc and getBcc.
+     *
+     * @group mailer
+     */
+    public function testGetAll_HasNoRecipients_ReturnsEmptyArrays() {
+        $recipientsCollection = new RecipientsCollection();
+
+        $expected = 0;
+        $allRecipients = $recipientsCollection->getAll();
+        $actual = count($allRecipients['to']) + count($allRecipients['cc']) + count($allRecipients['bcc']);
+        self::assertEquals($expected, $actual, "{$expected} recipients should have been found");
+        self::assertEmpty($allRecipients['to'], "{$expected} recipients should have been found in the TO list");
+        self::assertEmpty($allRecipients['cc'], "{$expected} recipients should have been found in the CC list");
+        self::assertEmpty($allRecipients['bcc'], "{$expected} recipients should have been found in the BCC list");
+    }
 }
