e48ca482003d5f0c236e8cae5376e5cf9eded796
Akshay Joshi <me@akshayjoshi.com>
2012-09-10 13:55:42 -0700

    Add the initial treemap module.

--- sugarcrm/modules/Opportunities/clients/summer/layouts/list-sidebar/list-sidebar.php
+++ sugarcrm/modules/Opportunities/clients/summer/layouts/list-sidebar/list-sidebar.php
@@ -2,9 +2,11 @@
 
 require_once('clients/summer/SideBarLayout.php');
 $layout = new SideBarLayout();
-//$layout->push('top', array('view'=>'subnav'));
+//$layout->push('top', array('view'=>'subnav'));`
+
+$layout->push('main',array('view'=>'treemap'));
 $layout->push('main',array('view'=>'untouched'));
-//$layout->push('main',array('view'=>'leaderboard'));
+$layout->push('main',array('view'=>'leaderboard'));
 $layout->push('main',array('view'=>'activitystream'));
 
 $viewdefs['Opportunities']['summer']['layout']['list-sidebar'] = $layout->getLayout();
