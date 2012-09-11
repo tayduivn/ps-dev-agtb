d48097f008d83226b80faccd6db170db4a66e57b
Akshay Joshi <me@akshayjoshi.com>
2012-09-10 17:08:46 -0700

    Improve the custom report for the opportunities treemap.

--- sugarcrm/modules/Reports/custom/OpportunityByUserStage.php
+++ sugarcrm/modules/Reports/custom/OpportunityByUserStage.php
@@ -1,11 +1,36 @@
 <?php
-$result = $GLOBALS['db']->query('SELECT assigned_user_id, name, id, amount_usdollar, sales_stage FROM opportunities');
-while($row = $GLOBALS['db']->fetchByAssoc($result)){
-    if(!isset($data[$row['assigned_user_id']])) {
-        $data[$row['assigned_user_id']] = array();
+$opportunity_result = $GLOBALS['db']->query('SELECT assigned_user_id, name, id, amount_usdollar as value, sales_stage FROM opportunities');
+
+$user_result = $GLOBALS['db']->query('SELECT assigned_user_id as id, users.first_name, users.last_name FROM opportunities JOIN users ON users.id = opportunities.assigned_user_id GROUP BY assigned_user_id');
+
+// TODO: Un-hardcode this by doing a query like the following:
+// SELECT sales_stage FROM opportunities GROUP BY sales_stage
+$stages = array('Prospecting', 'Closed Lost', 'Closed Won', 'Qualification');
+$temp = array();
+$data = array('name' => "Opportunities", 'children' => array());
+
+while($row = $GLOBALS['db']->fetchByAssoc($user_result)) {
+    $temp[$row['id']] = array('name' => $row['first_name'].' '.$row['last_name'],
+        'children' => array()
+    );
+    foreach($stages as $stage) {
+        $temp[$row['id']]['children'][$stage] = array(
+            'name' => $stage,
+            'children' => array()
+        );
     }
-    if(!isset($data[$row['assigned_user_id']][$row['sales_stage']])) {
-        $data[$row['assigned_user_id']][$row['sales_stage']] = array();
+}
+
+while($row = $GLOBALS['db']->fetchByAssoc($opportunity_result)){
+    $row['value'] = (int)$row['value'];
+    $temp[$row['assigned_user_id']]['children'][$row['sales_stage']]['children'][] = $row;
+}
+
+foreach($temp as $user) {
+    $new_array = array();
+    foreach($user['children'] as $sales_stage) {
+        $new_array[] = $sales_stage;
     }
-    $data[$row['assigned_user_id']][$row['sales_stage']] = $row;
+    $user['children'] = $new_array;
+    $data['children'][] = $user;
 }
