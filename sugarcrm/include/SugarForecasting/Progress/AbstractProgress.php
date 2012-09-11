8c741d2b283097ad6c4e01ce6ef826cc267b5097
Jon Whitcraft <jwhitcraft@sugarcrm.com>
2012-09-11 09:47:03 -0700

    SFA-92 - Move Progress API To Classes
    - Move the Progress API into their own classes
    - Adjust the end point to look for custom classes
    - Move the Arg handling code out into it's own abstract class instead of coping the code multiple times.

---
+++ sugarcrm/include/SugarForecasting/Progress/AbstractProgress.php
@@ -0,0 +1,49 @@
+<?php
+/********************************************************************************
+ *The contents of this file are subject to the SugarCRM Professional End User License Agreement
+ *("License") which can be viewed at http://www.sugarcrm.com/EULA.
+ *By installing or using this file, You have unconditionally agreed to the terms and conditions of the License, and You may
+ *not use this file except in compliance with the License. Under the terms of the license, You
+ *shall not, among other things: 1) sublicense, resell, rent, lease, redistribute, assign or
+ *otherwise transfer Your rights to the Software, and 2) use the Software for timesharing or
+ *service bureau purposes such as hosting the Software for commercial gain and/or for the benefit
+ *of a third party.  Use of the Software may be subject to applicable fees and any use of the
+ *Software without first paying applicable fees is strictly prohibited.  You do not have the
+ *right to remove SugarCRM copyrights from the source code or user interface.
+ * All copies of the Covered Code must include on each user interface screen:
+ * (i) the "Powered by SugarCRM" logo and
+ * (ii) the SugarCRM copyright notice
+ * in the same form as they appear in the distribution.  See full license for requirements.
+ *Your Warranty, Limitations of liability and Indemnity are expressly stated in the License.  Please refer
+ *to the License for the specific language governing these rights and limitations under the License.
+ *Portions created by SugarCRM are Copyright (C) 2004 SugarCRM, Inc.; All Rights Reserved.
+ ********************************************************************************/
+
+require_once('include/SugarForecasting/Progress/ProgressInterface.php');
+require_once('include/SugarForecasting/AbstractForecastArgs.php');
+abstract class SugarForecasting_Progress_AbstractProgress extends SugarForecasting_AbstractForecastArgs implements SugarForecasting_Progress_ProgressInterface
+{
+    /**
+     * Class Constructor
+     * @param array $args       Service Arguments
+     */
+    public function __construct($args)
+    {
+        parent::__construct($args);
+
+        $this->loadConfigArgs();
+    }
+
+    /**
+     * Get Settings from the Config Table.
+     */
+    public function loadConfigArgs() {
+        /* @var $admin Administration */
+        $admin = BeanFactory::getBean('Administration');
+        $admin->retrieveSettings();
+
+        // decode and json decode the settings from teh administration to set the sales stages for closed won and closed lost
+        $this->setArg('sales_stage_won', json_decode(html_entity_decode($admin->settings["base_sales_stage_won"])));
+        $this->setArg('sales_stage_lost', json_decode(html_entity_decode($admin->settings["base_sales_stage_lost"])));
+    }
+}
