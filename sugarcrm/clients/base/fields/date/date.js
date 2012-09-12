525881d26bd950b6dcab93b527fc12bd0e36eded
Rob Levin <rlevin@sugarcrm.com>
2012-09-12 12:00:53 -0700

    I saw issue duplicate to bug 56249 where date is one day behind on detail. Using date parts to Date constructor for said code path as that's only reliable way.

--- sugarcrm/clients/base/fields/date/date.js
+++ sugarcrm/clients/base/fields/date/date.js
@@ -34,7 +34,8 @@
         // If there is a default 'string' value like "yesterday", format it as a date
         if(!value && this.def.display_default) {
             value = app.date.parseDisplayDefault(this.def.display_default);
-            jsDate = new Date(value);
+            parts = value.match(/(\d+)/g);
+            jsDate = new Date(parts[0], parts[1]-1, parts[2]); //months are 0-based
             this.model.set(this.name, app.date.format(jsDate, this.serverDateFormat));
         } else {
             // Bug 56249 .. Date constructor doesn't reliably handle yyyy-mm-dd
