a50b1b02dce0a707ea6bbf0ab7972c3942a3e184
Akshay Joshi <me@akshayjoshi.com>
2012-09-13 15:29:28 -0700

    Fix sed to use commas as delimiters to not mess up on path names.

--- sugarcrm/summer/splash/setupDevEnv.sh
+++ sugarcrm/summer/splash/setupDevEnv.sh
@@ -21,18 +21,18 @@ if [[ "$platform" == 'osx' ]]; then
 	port="$(osascript -e 'Tell application "System Events" to display dialog "Enter the port:" default answer "8888"' -e 'text returned of result' 2>/dev/null)"
 	path="$(osascript -e 'Tell application "System Events" to display dialog "Enter the path from the webroot without initial/trailing slashes:" default answer "sugar66"' -e 'text returned of result' 2>/dev/null)"
 
-	sed -i "" "s/\<web_root\>/$path/g" .htaccess
-	sed -i "" "s/\<host\>/$name/g" .htaccess
-	sed -i "" "s/\<port\>/$port/g" .htaccess
-	sed -i "" "s/\<web_root\>/$path/g" boxoffice/.htaccess
-	sed -i "" "s/\<host\>/$name/g" boxoffice/.htaccess
-	sed -i "" "s/\<port\>/$port/g" boxoffice/.htaccess
-	sed -i "" "s/<web_root>/$path/g" configs/config.php
-	sed -i "" "s/<host>/$name/g" configs/config.php
-	sed -i "" "s/<port>/$port/g" configs/config.php
-	sed -i "" "s/<host>/$name/g" config.php
-	sed -i "" "s/<port>/$port/g" config.php
-	sed -i "" "s/<web_root>/$path/g" config.php
+	sed -i "" "s,<web_root>,$path,g" .htaccess
+	sed -i "" "s,<host>,$name,g" .htaccess
+	sed -i "" "s,<port>,$port,g" .htaccess
+	sed -i "" "s,<web_root>,$path,g" boxoffice/.htaccess
+	sed -i "" "s,<host>,$name,g" boxoffice/.htaccess
+	sed -i "" "s,<port>,$port,g" boxoffice/.htaccess
+	sed -i "" "s,<web_root>,$path,g" configs/config.php
+	sed -i "" "s,<host>,$name,g" configs/config.php
+	sed -i "" "s,<port>,$port,g" configs/config.php
+	sed -i "" "s,<host>,$name,g" config.php
+	sed -i "" "s,<port>,$port,g" config.php
+	sed -i "" "s,<web_root>,$path,g" config.php
 
 	echo "All done. Go forth and install the sweet, sweet goodness."
 else
