acca78da4a4a18d597d8a6d945354ad0f5a3fdc0
David Tam <david.cheung.tam@gmail.com>
2012-09-13 17:03:17 -0700

    Bug 56429 incomplete urls do not work in url fields

---
+++ sugarcrm/tests/clients/base/fields/url/url.js
@@ -0,0 +1,27 @@
+describe("sugarfields", function() {
+
+    var app, field;
+
+    beforeEach(function() {
+        app = SugarTest.app;
+        field = SugarTest.createField("base","url", "url", "edit");
+    });
+
+    afterEach(function() {
+        app.cache.cutAll();
+        app.view.reset();
+        delete Handlebars.templates;
+        field = null;
+    });
+
+    describe("url", function() {
+        it("should add http if missing on format and leave https and http alone", function() {
+            var completeURL = "http://www.google.com";
+            var completeHttpsURL = "http://www.google.com";
+            var incompURL = "www.google.com";
+            expect(field.format(completeURL)).toEqual(completeURL);
+            expect(field.format(completeHttpsURL)).toEqual(completeHttpsURL);
+            expect(field.format(incompURL)).toEqual(completeURL);
+        });
+    });
+});
