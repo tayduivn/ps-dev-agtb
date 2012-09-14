16ac9837d958fb31ef3cd1bb197613840282862d
Rob Aagaard <rob@sugarcrm.com>
2012-09-14 10:03:34 -0600

    Needed to fix a lot of vardefs so we could run through every module when we were building the module definitions.

--- sugarcrm/include/MetaDataManager/MetaDataManager.php
+++ sugarcrm/include/MetaDataManager/MetaDataManager.php
@@ -673,9 +673,7 @@ class MetaDataManager {
             $moduleList = array_keys($wireless_module_registry);
         } else {
             // Loading a standard module list
-            require_once("modules/MySettings/TabController.php");
-            $controller = new TabController();
-            $moduleList = array_keys($controller->get_user_tabs($this->user));
+            $moduleList = array_keys($GLOBALS['app_list_strings']['moduleList']);
         }
 
         $oldModuleList = $moduleList;
