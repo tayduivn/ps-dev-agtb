332282c7d9b66236fb771922c0a29945405bf9e6
Matthew Marum <mmarum@sugarcrm.com>
2012-09-15 16:43:16 -0400

    Bug56143: Portal updates to use API error messages when available

--- sugarcrm/portal2/error.js
+++ sugarcrm/portal2/error.js
@@ -14,7 +14,7 @@
      */
     app.error.handleNeedLoginError = function(error) {
         backToLogin(true);
-        app.alert.show("needs_login_error", {level: "error", messages: "LBL_PORTAL_INVALID_CREDS", title:"LBL_PORTAL_INVALID_CREDS_TITLE", autoClose: true});
+        app.alert.show("needs_login_error", {level: "error", messages: error.message ? error.message : "LBL_PORTAL_INVALID_CREDS", title:"LBL_PORTAL_INVALID_CREDS_TITLE", autoClose: true});
     };
 
     /**
@@ -22,7 +22,7 @@
      */
     app.error.handleInvalidGrantError = function(error) {
         backToLogin(true);
-        app.alert.show("invalid_grant_error", {level: "error", messages: "LBL_PORTAL_INVALID_GRANT", title:"LBL_PORTAL_INVALID_GRANT_TITLE", autoClose: true});
+        app.alert.show("invalid_grant_error", {level: "error", messages: error.message ? error.message : "LBL_PORTAL_INVALID_GRANT", title:"LBL_PORTAL_INVALID_GRANT_TITLE", autoClose: true});
     };
 
     /**
@@ -30,14 +30,14 @@
      */
     app.error.handleInvalidClientError = function(error) {
         backToLogin(true);
-        app.alert.show("invalid_client_error", {level: "error", messages: "LBL_PORTAL_AUTH_FAILED", title:"LBL_PORTAL_AUTH_FAILED_TITLE", autoClose: true});
+        app.alert.show("invalid_client_error", {level: "error", messages: error.message ? error.message : "LBL_PORTAL_AUTH_FAILED", title:"LBL_PORTAL_AUTH_FAILED_TITLE", autoClose: true});
     };
     /**
      * Invalid request handler. 
      */
     app.error.handleInvalidRequestError = function(error) {
         backToLogin(true);
-        app.alert.show("invalid_request_error", {level: "error", messages: "LBL_PORTAL_INVALID_REQUEST", title:"LBL_PORTAL_INVALID_REQUEST_TITLE", autoClose: true});
+        app.alert.show("invalid_request_error", {level: "error", messages: error.message ? error.message : "LBL_PORTAL_INVALID_REQUEST", title:"LBL_PORTAL_INVALID_REQUEST_TITLE", autoClose: true});
     };
 
     /**
@@ -45,7 +45,7 @@
      */
     app.error.handleTimeoutError = function(error) {
         backToLogin(true);
-        app.alert.show("timeout_error", {level: "error", messages: "LBL_PORTAL_REQUEST_TIMEOUT", title:"LBL_PORTAL_REQUEST_TIMEOUT_TITLE", autoClose: true});
+        app.alert.show("timeout_error", {level: "error", messages: error.message ? error.message : "LBL_PORTAL_REQUEST_TIMEOUT", title:"LBL_PORTAL_REQUEST_TIMEOUT_TITLE", autoClose: true});
     };
 
     /**
@@ -53,14 +53,14 @@
      */
     app.error.handleUnauthorizedError = function(error) {
         backToLogin(true);
-        app.alert.show("unauthorized_request_error", {level: "error", messages: "LBL_PORTAL_UNAUTHORIZED", title:"LBL_PORTAL_UNAUTHORIZED_TITLE", autoClose: true});
+        app.alert.show("unauthorized_request_error", {level: "error", messages: error.message ? error.message : "LBL_PORTAL_UNAUTHORIZED", title:"LBL_PORTAL_UNAUTHORIZED_TITLE", autoClose: true});
     };
 
     /**
      * 403 Forbidden error handler. 
      */
     app.error.handleForbiddenError = function(error) {
-        app.alert.show("forbidden_request_error", {level: "error", messages: "LBL_PORTAL_RESOURCE_UNAVAILABLE", title:"LBL_PORTAL_RESOURCE_UNAVAILABLE_TITLE", autoClose: true});
+        app.alert.show("forbidden_request_error", {level: "error", messages: error.message ? error.message : "LBL_PORTAL_RESOURCE_UNAVAILABLE", title:"LBL_PORTAL_RESOURCE_UNAVAILABLE_TITLE", autoClose: true});
     };
     
     /**
@@ -80,7 +80,7 @@
      */
     app.error.handleMethodNotAllowedError = function(error) {
         backToLogin(true);
-        app.alert.show("not_allowed_error", {level: "error", messages: "LBL_PORTAL_METHOD_NOT_ALLOWED", title:"LBL_PORTAL_METHOD_NOT_ALLOWED_TITLE", autoClose: true});
+        app.alert.show("not_allowed_error", {level: "error", messages: error.message ? error.message : "LBL_PORTAL_METHOD_NOT_ALLOWED", title:"LBL_PORTAL_METHOD_NOT_ALLOWED_TITLE", autoClose: true});
     };
 
     /**
@@ -90,7 +90,7 @@
         backToLogin(true);
         // TODO: For finer grained control we could sniff the {error: <code>} in the response text (JSON) for one of:
         // missing_parameter, invalid_parameter, request_failure
-        app.alert.show("precondtion_failure_error", {level: "error", messages: "LBL_PORTAL_PRECONDITION_MISSING", title:"LBL_PORTAL_PRECONDITION_MISSING_TITLE", autoClose: true});
+        app.alert.show("precondtion_failure_error", {level: "error", messages: error.message ? error.message : "LBL_PORTAL_PRECONDITION_MISSING", title:"LBL_PORTAL_PRECONDITION_MISSING_TITLE", autoClose: true});
     };
        
     /**
