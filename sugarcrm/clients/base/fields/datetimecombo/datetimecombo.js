264b2223a900565b7f36998d2fa015c2ea61833c
Rob Levin <rlevin@sugarcrm.com>
2012-09-14 18:05:53 -0700

    Added am/pm blank option to be consistent. Using underscore funcitons. Fixed bugs.

--- sugarcrm/clients/base/fields/datetimecombo/datetimecombo.js
+++ sugarcrm/clients/base/fields/datetimecombo/datetimecombo.js
@@ -1,15 +1,21 @@
 ({
     // datetimecombo
-    _render:function(value) {
-        var self = this, userTimePrefs;
+    initialize: function(options) {
+        var userTimePrefs;
+        this.lastHourSelected   = null;
+        this.lastMinuteSelected = null;
+        this.lastAmPmSelected   = null;
+
         // Determines if we're using 12 or 24 hour clock conventions
         userTimePrefs = app.user.get('timepref');
-        // 'h' and 'g' specify types of 12 hour time formats (g without leading zeros)
-        this.showAmPm = (userTimePrefs.match(/h/)!==null || userTimePrefs.match(/g/)!==null) ? true : false;
+        // h specifies 12 hour format (TODO: refactor date.js to support g/G options and add here)
+        this.showAmPm = userTimePrefs.match(/h/)!==null ? true : false; // TODO: date.js doesn't yet support g/G options 
         this.timeOptions.hours = this.getHours();
-
+        this.app.view.Field.prototype.initialize.call(this, options);
+    },
+    _render:function(value) {
+        var self = this;
         app.view.Field.prototype._render.call(self);
-
         $(function() {
             if(self.view.name === 'edit') {
                 $(".datepicker").datepicker({
@@ -20,14 +26,11 @@
             }
         });
     },
-    lastHourSelected: null,
-    lastMinuteSelected: null,
-    showAmPm: false,
     unformat:function(value) {
         var jsDate, 
             myUser = app.user;
         jsDate = app.date.parse(value, myUser.get('datepref')+' '+ myUser.get('timepref'));
-        if(jsDate && typeof jsDate.toISOString === 'function') {
+        if(jsDate && _.isFunction(jsDate.toISOString)) {
             return jsDate.toISOString();
         } else {
             app.logger.error("Issue converting date to iso string; no toISOString available for date created for value: "+value);
@@ -36,10 +39,9 @@
     },
 
     format:function(value) {
-        var jsDate, output, usersDateFormatPreference, usersTimeFormatPreference, myUser = app.user, d, parts;
+        var jsDate, output, usersDateFormatPreference, usersTimeFormatPreference, myUser = app.user, d, parts, before24Hours;
         usersDateFormatPreference = myUser.get('datepref');
         usersTimeFormatPreference = myUser.get('timepref');
-
 
         // If there is a default 'string' value like "yesterday", format it as a date
         if(!value && this.def.display_default) {
@@ -52,6 +54,9 @@
             // In case ISO 8601 get it back to js native date which date.format understands
             jsDate = new Date(value);
         }
+
+        // Save the 24 hour based hours in case we're using ampm to determine if am or pm later 
+        before24Hours = jsDate.getHours();
         value  = app.date.format(jsDate, usersDateFormatPreference)+' '+app.date.format(jsDate, usersTimeFormatPreference);
         jsDate = app.date.parse(value);
         jsDate = app.date.roundTime(jsDate);
@@ -63,38 +68,37 @@
             hours: app.date.format(jsDate, (this.showAmPm ? 'h' : 'H')),
             minutes: app.date.format(jsDate, 'i'),
             seconds: app.date.format(jsDate, 's'),
-            amPm: this.showAmPm ? (app.date.format(jsDate, 'H') < 12 ? 'am' : 'pm') : ''
+            amPm: this.showAmPm ? (before24Hours < 12 ? 'am' : 'pm') : ''
         };
         this.lastHourSelected   = value.hours;
         this.lastMinuteSelected = value.minutes;
+        this.lastAmPmSelected   = value.amPm;
 
         //0am must be shown as 12am if we're on a 12 hour based time format
-        if(typeof value.amPm != "undefined" && value.amPm == 'am' && value.hours == 0) {
+        if(!_.isUndefined(value.amPm) && value.amPm === 'am' && value.hours == 0) {
             value.hours = '12';
             d = new Date();
             d.setHours(12); 
-            d.setMinutes(0); 
+            d.setMinutes(value.minutes); 
             value.time = app.date.format(d, usersTimeFormatPreference);
-            this.model.set(this.name, this.buildUnformatted(value.date, '00', value.minutes, value.amPm), {silent: true});
-            this.lastHourSelected   = '00';
+            this.model.set(this.name, this.buildUnformatted(value.date, '00', value.minutes), {silent: true});
+            this.lastHourSelected  = '00';
         }
-        
         return value;
     },
     getHours: function() {
         if(this.showAmPm) {
             return  [
                 {key: "", value: ""},
-                {key: "0", value: "00"},
-                {key: "1", value: "01"},
-                {key: "2", value: "02"},
-                {key: "3", value: "03"},
-                {key: "4", value: "04"},
-                {key: "5", value: "05"},
-                {key: "6", value: "06"},
-                {key: "7", value: "07"},
-                {key: "8", value: "08"},
-                {key: "9", value: "09"},
+                {key: "01", value: "01"},
+                {key: "02", value: "02"},
+                {key: "03", value: "03"},
+                {key: "04", value: "04"},
+                {key: "05", value: "05"},
+                {key: "06", value: "06"},
+                {key: "07", value: "07"},
+                {key: "08", value: "08"},
+                {key: "09", value: "09"},
                 {key: "10", value: "10"},
                 {key: "11", value: "11"},
                 {key: "12", value: "12"}
@@ -138,7 +142,8 @@
             {key: "30", value: "30"},
             {key: "45", value: "45"}
         ],
-            amPm: [
+        amPm: [
+            {key: "", value: ""},
             {key: "am", value: "am"},
             {key: "pm", value: "pm"}
         ]
@@ -156,71 +161,67 @@
         date.on('change', function(ev) {
             var timeObj;
             // If user selects date but no hour/minutes selected yet, prepopulate with 12:00am
-            hr = self.patched12AmHour(amPm, hour);
+            hr = self.patchHour(amPm.val(), hour.val());
             timeObj = self.setIfNoTime(hr, minute.val());
-            model.set(fieldName, self.buildUnformatted(date.val(), timeObj.hour, timeObj.minutes, timeObj.amPm));
+            model.set(fieldName, self.buildUnformatted(date.val(), timeObj.hour, timeObj.minutes));
         });
         hour.on('change', function(ev) {
             // If no date (display default set to none), user may select hrs, minutes, etc., so defaults to today's date 
             date.val(self.setIfNoDate(date.val()));
-            hr = self.patched12AmHour(amPm, hour);
             // if user attempts to select first "blank" option, set back to previous hour selected
-            hr = hr ? hr : self.lastHourSelected; 
+            hr = hour.val() ? hour.val() : self.lastHourSelected; 
+            hr = self.patchHour(amPm.val(), hr);
             self.lastHourSelected = hr;
-
             // if selecting hour and no minutes yet put a default value to make it meaningful
             min  = minute.val() ? minute.val() : '00'; 
-
             // Force trigger since internally 00/12 are both represented as 00 so won't always trigger
-            model.set(fieldName, self.buildUnformatted(date.val(), hr, min, amPm.val()), {silent: true});
+            model.set(fieldName, self.buildUnformatted(date.val(), hr, min), {silent: true});
             model.trigger('change:'+fieldName);
         });
         minute.on('change', function(ev) {
             date.val(self.setIfNoDate(date.val()));
             // if selecting minutes and no hours yet put a default value to make it meaningful
-            hr = hour.val() ? hour.val() : '00';
-            hr = self.patched12AmHour(amPm, hour);
+            hr = self.patchHour(amPm.val(), hour.val());
             min  = minute.val() ? minute.val() : self.lastMinuteSelected;
             self.lastMinuteSelected = min;
-            model.set(fieldName, self.buildUnformatted(date.val(), hr, min, amPm.val()), {silent: true});
+            model.set(fieldName, self.buildUnformatted(date.val(), hr, min), {silent: true});
             model.trigger('change:'+fieldName);
         });
         amPm.on('change', function(ev) {
-            var isWrongAmPm = false;
-
+            var ampm, timeObj;
             date.val(self.setIfNoDate(date.val()));
-            //am only be allowed 0-12; pm 12-23
-            if(parseInt(hour.val(), 10) >= 13 && amPm.val()==='am') {
-                amPm.val('pm');
-                isWrongAmPm = true;
+
+            // If valid am or pm select fine, but also force blank selection to "bounce back" to previously selected
+            if(!amPm.val()) {
+                amPm.find("option").attr("selected",false);
+                ampm = self.lastAmPmSelected;
+                amPm.find("option[value='"+ampm+"']").attr("selected",true);
+            } else {
+                ampm = amPm.val();
             }
-            else if(parseInt(hour.val(), 10) < 12 && amPm.val()==='pm') {
-                amPm.val('am');
-                isWrongAmPm = true;
-            }
-            if(isWrongAmPm) {
-                app.alert.show('wrong_ampm', {
-                    level: 'warning',
-                    title: app.lang.get('LBL_PORTAL_WRONG_AMPM'),
-                    messages: app.lang.get('LBL_PORTAL_WRONG_AMPM_MSG'),
-                    autoClose: true});
-            }
-            model.set(fieldName, self.buildUnformatted(date.val(), hour.val(), minute.val(), amPm.val()));
+            self.lastAmPmSelected = ampm; 
+            hr = self.patchHour(amPm.val(), hour.val());
+            timeObj = self.setIfNoTime(hr, minute.val(), ampm);
+            model.set(fieldName, self.buildUnformatted(date.val(), timeObj.hour, timeObj.minutes));
         });
     },
-    buildUnformatted: function(d, h, m, amPm) {
-        return this.unformat(d + ' ' + h + ':' + m + ':00' +':'+ amPm);
+    buildUnformatted: function(d, h, m) {
+        return this.unformat(d + ' ' + h + ':' + m + ':00');
     },
     
     // Checks if h and/or m are falsy, if so, sets to '00'
-    setIfNoTime: function(h, m) {
+    setIfNoTime: function(h, m, ampm) {
         var o = {};
         // Essentially, if we have no time parts, we're going to default to 12:00am
         if(!h && !m) {
-            o.amPm = 'am';
+            o.amPm = ampm ? ampm : 'am';
         }
-        o.hour = h ? h : '00'; // will downstream turn to 12am but internally needs to be 00 ;=)
+        o.hour = h ? h : '00'; // will downstream turn to 12am but internally needs to be 00
         o.minutes = m ? m : '00';
+        
+        //Convert 12am to 00 and also 00pm to 12
+        o.hour = o.hour==='12' && o.amPm==='am' ? '00' : o.hour;
+        o.hour = o.hour==='00' && o.amPm==='pm' ? '12' : o.hour;
         return o;
     },
     // Checks if dateToCheck is falsy .. if so returns today date formatted by user's prefs
@@ -234,14 +235,19 @@
     },
 
     // If we have 00am we patch the displayed value to 12 am but we still want internally to represent as 00am
-    // if on 12 hour time format .. if not this function will just return hour val anyway.
-    patched12AmHour: function (amPm, hour) {
-        // Patch 12am to 00am as we need it this way internally though we present 12am (if on 12 hr time format)
-        if(amPm.val() && amPm.val() === 'am' && hour.val() === '12') {
-            return '00';
-        } else {
-            return hour.val();
+    // if on 12 hour time format .. if not this function will just return hour val anyway. 
+    patchHour: function (ampm, hour) {
+        var hr = hour ? parseInt(hour, 10) : 0;
+        if(this.showAmPm) {
+            // Patch 12am to 00am as we need it this way internally though we present 12am (if on 12 hr time format)
+            if(ampm && ampm === 'am' && hr === 12) {
+                return '00';
+            } else if(hr < 12 && ampm === 'pm') {
+                // add 12 e.g. 4pm becomes 16 - again for internal iso representation
+                return hr+12+'';
+            }
         }
+        return hour;
     }
     
 })
