f9f1bbc95636dd9ac153ff6ee66f280c59f3352f
Rob Levin <rlevin@sugarcrm.com>
2012-09-17 10:28:28 -0700

    Now that datetimecombo production code added initialize method, timepref needs to be set on user before component helper creates field. Reorganized tests accordingly.

--- sugarcrm/tests/clients/base/fields/datetimecombo/datetimecombo.js
+++ sugarcrm/tests/clients/base/fields/datetimecombo/datetimecombo.js
@@ -1,31 +1,37 @@
 describe("datetimecombo field", function() {
     var app, field;
+    var app, field, myUser;
 
     beforeEach(function() {
         app = SugarTest.app;
-        field = SugarTest.createField("base","datetimecombo", "datetimecombo", "detail");
+        myUser = SUGAR.App.user;
+        myUser.set('datepref','m/d/Y');
     });
 
     afterEach(function() {
         app.cache.cutAll();
         app.view.reset();
         delete Handlebars.templates;
+        myUser = null;
         field = null;
     });
 
-    describe("datetimecombo", function() {
+    describe("datetimecombo test with 'H:i' time format", function() {
+        beforeEach(function() {
+            myUser.set('timepref','H:i'); 
+            field = SugarTest.createField("base","datetimecombo", "datetimecombo", "detail");
+        });
+
         it("should format the date time combo", function() {
-            var myUser = SUGAR.App.user, expectedValue, jsDate, unformatedValue;
-            myUser.set('datepref','m/d/Y');
-            myUser.set('timepref','H:i');
+            var expectedValue, jsDate, unformatedValue, month, day, year;
             jsDate = new Date('2012-04-09T09:50:58Z');
             unformatedValue = jsDate.toISOString();
             expect(field.format(unformatedValue).date).toEqual('04/09/2012');
-            var month = jsDate.getMonth() + 1 + '';
+            month = jsDate.getMonth() + 1 + '';
             month = month.length === 1 ? '0' + month : month;
-            var day   = jsDate.getDate() + '';
+            day   = jsDate.getDate() + '';
             day   = day.length === 1 ? '0' + day : day;
-            var year  = jsDate.getFullYear() + '';
+            year  = jsDate.getFullYear() + '';
 
             // we round to nearest 15 minutes so if user's locale produces something like
             // 2:50, than that will be rounded up to 3:00; our test has :50 minutes so we add 1
@@ -38,16 +44,6 @@ describe("datetimecombo field", function() {
             expect(field.format(unformatedValue).time).toEqual(hours + ':00');
             expect(field.format(unformatedValue).seconds).toEqual('00');
         });
-        it("should convert 00am to 12am if on 12 hour time format", function() {
-            var myUser = SUGAR.App.user, jsDate, unformatedValue;
-            myUser.set('datepref','m/d/Y');
-            myUser.set('timepref','h:i');
-            // the field sets this based on h or H in timepref, but don't want to trigger _render ;=)
-            field.showAmPm = true;
-            jsDate = new Date("September 12, 1970 00:00:00")
-            unformatedValue = jsDate.toISOString();
-            expect(field.format(unformatedValue).hours).toEqual('12');
-        });
         it("should NOT convert 00am to 12am if on 24 hour time format", function() {
             var myUser = SUGAR.App.user, jsDate, unformatedValue;
             myUser.set('datepref','m/d/Y');
@@ -58,6 +54,23 @@ describe("datetimecombo field", function() {
             unformatedValue = jsDate.toISOString();
             expect(field.format(unformatedValue).hours).not.toEqual('12');
             expect(field.format(unformatedValue).hours).toEqual('00');
+        });
+    });
+
+    describe("datetimecombo test with 'h:i' time format", function() {
+        beforeEach(function() {
+            myUser.set('timepref','h:i'); 
+            field = SugarTest.createField("base","datetimecombo", "datetimecombo", "detail");
+        });
+        it("should convert 00am to 12am if on 12 hour time format", function() {
+            var myUser = SUGAR.App.user, jsDate, unformatedValue;
+            myUser.set('datepref','m/d/Y');
+            myUser.set('timepref','h:i');
+            // the field sets this based on h or H in timepref, but don't want to trigger _render ;=)
+            field.showAmPm = true;
+            jsDate = new Date("September 12, 1970 00:00:00")
+            unformatedValue = jsDate.toISOString();
+            expect(field.format(unformatedValue).hours).toEqual('12');
         });
     });
 });
