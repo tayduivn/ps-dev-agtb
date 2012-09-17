39c226f26c29107a2fc59a0cabb9f697b32c5669
Akshay Joshi <me@akshayjoshi.com>
2012-09-17 15:04:02 -0700

    Add creation of contact on click. Fixes ABE-44.

--- sugarcrm/clients/summer/views/recommended_contacts/recommended_contacts.js
+++ sugarcrm/clients/summer/views/recommended_contacts/recommended_contacts.js
@@ -13,6 +13,18 @@
                 $("#"+self.guid).html("");
                 _.each(o.contacts, function(contact) {
                     var el = $("<li />").html(contact.first_name+" "+contact.last_name+" <strong>&lt;"+contact.email+"&gt;</strong>");
+                    el.on("click", function() {
+                        var m = App.data.createBean("Contacts");
+                        m.set("first_name", contact.first_name);
+                        m.set("last_name", contact.last_name);
+                        m.set("email1", contact.email);
+                        m.save({}, {
+                            success: function(model, response) {
+                                App.navigate(App.context.getContext(), model);
+                            }
+                        });
+
+                    });
                     el.appendTo("#"+self.guid);
                 });
             }
