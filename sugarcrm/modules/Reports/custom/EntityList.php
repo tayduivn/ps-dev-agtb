b64a73fb2f637e3c38e1a9c7d696d0da86cc8e2c
Akshay Joshi <me@akshayjoshi.com>
2012-09-17 14:15:59 -0700

    Add team level security to tagging. Fixes ABE-38.

--- sugarcrm/modules/Reports/custom/EntityList.php
+++ sugarcrm/modules/Reports/custom/EntityList.php
@@ -1,18 +1,49 @@
 <?php
 
-$queries = array(
-    'Users' => 'SELECT id, CONCAT(first_name, " ", last_name) name FROM users',
-    'Contacts' => 'SELECT id, CONCAT(first_name, " ", last_name) name FROM contacts',
-    'Opportunities' => 'SELECT id, name FROM opportunities',
-    'Accounts' => 'SELECT id, name FROM accounts'
-);
-
-foreach($queries as $module => $sql) {
-    $result = $GLOBALS['db']->query($sql);
-
-    while($row = $GLOBALS['db']->fetchByAssoc($result)){
-        if(is_null($row['id']) || is_null($row['name'])) continue;
-        $row['module'] = $module;
-        $data[] = $row;
+// Users
+$seed = BeanFactory::getBean("Users");
+$result = $seed->get_list();
+foreach($result['list'] as $bean) {
+    if(!empty($bean->first_name)) {
+        $data[] = array(
+            'module' => $bean->module_name,
+            'name' => $bean->name,
+            'id' => $bean->id,
+        );
     }
+}
+
+// Contacts
+$seed = BeanFactory::getBean("Contacts");
+$result = $seed->get_list();
+foreach($result['list'] as $bean) {
+    if(!empty($bean->first_name)) {
+        $data[] = array(
+            'module' => $bean->module_name,
+            'name' => $bean->name,
+            'id' => $bean->id,
+        );
+    }
+}
+
+// Opportunities
+$seed = BeanFactory::getBean("Opportunities");
+$result = $seed->get_list();
+foreach($result['list'] as $bean) {
+    $data[] = array(
+        'module' => $bean->module_name,
+        'name' => $bean->name,
+        'id' => $bean->id,
+    );
+}
+
+// Accounts
+$seed = BeanFactory::getBean("Accounts");
+$result = $seed->get_list();
+foreach($result['list'] as $bean) {
+    $data[] = array(
+        'module' => $bean->module_name,
+        'name' => $bean->name,
+        'id' => $bean->id,
+    );
 }
