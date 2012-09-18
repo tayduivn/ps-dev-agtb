51b6dcc873c0bf85e8380b3c966fdb1b2998439a
Zlata Damarad <z.damarad@gmail.com>
2012-09-18 19:47:16 +0300

    [#SFA-121] - Change forecasting endpoints to return date values in IS0 8601

--- sugarcrm/include/SugarForecasting/Committed.php
+++ sugarcrm/include/SugarForecasting/Committed.php
@@ -66,7 +66,10 @@ class SugarForecasting_Committed extends SugarForecasting_AbstractForecast imple
         $results = $db->query($query);
 
         $forecasts = array();
+        $timedate = TimeDate::getInstance();
         while (($row = $db->fetchByAssoc($results))) {
+            $iso_date = $timedate->asIso($timedate->fromDb($row['date_entered']), $current_user);
+            $row['date_entered'] = $iso_date;
             $forecasts[] = $row;
         }
 
