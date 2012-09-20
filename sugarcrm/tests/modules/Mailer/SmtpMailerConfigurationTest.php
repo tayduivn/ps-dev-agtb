e1b1d6b3dfb0db1ae7b4aafc24c1a487c6455d93
Greg Levine <glevine@sugarcrm.com>
2012-09-19 23:38:37 -0400

    Changed MailerConfigurationTest to SmtpMailerConfigurationTest so that tests could be added for the Smtp case. Since SmtpMailerConfiguration extends MailerConfiguration, the same tests can be run on the derived methods.

--- sugarcrm/tests/modules/Mailer/SmtpMailerConfigurationTest.php
+++ sugarcrm/tests/modules/Mailer/SmtpMailerConfigurationTest.php
@@ -19,15 +19,15 @@
  *Portions created by SugarCRM are Copyright (C) 2004 SugarCRM, Inc.; All Rights Reserved.
  ********************************************************************************/
 
-require_once "modules/Mailer/MailerConfiguration.php";
+require_once "modules/Mailer/SmtpMailerConfiguration.php";
 
-class MailerConfigurationTest extends Sugar_PHPUnit_Framework_TestCase
+class SmtpMailerConfigurationTest extends Sugar_PHPUnit_Framework_TestCase
 {
     /**
      * @group mailer
      */
     public function testLoadDefaultConfigs_CharsetIsReset_WordwrapIsInitialized() {
-        $mailerConfig = new MailerConfiguration();
+        $mailerConfig = new SmtpMailerConfiguration();
 
         // change the default charset in order to show that loadDefaultConfigs will reset it
         $mailerConfig->setCharset("asdf"); // some asinine value that wouldn't actually be used
@@ -35,7 +35,7 @@ class MailerConfigurationTest extends Sugar_PHPUnit_Framework_TestCase
         // test that the charset has been changed from its default
         $expected = "asdf";
         $actual   = $mailerConfig->getCharset();
-        self::assertEquals($expected, $actual, "The charset should have been reset to {$expected}");
+        self::assertEquals($expected, $actual, "The charset should have been set to {$expected}");
 
         $mailerConfig->loadDefaultConfigs();
 
@@ -48,5 +48,51 @@ class MailerConfigurationTest extends Sugar_PHPUnit_Framework_TestCase
         $expected = 996;
         $actual   = $mailerConfig->getWordwrap();
         self::assertEquals($expected, $actual, "The wordwrap should have been initialized to {$expected}");
+    }
+
+    /**
+     * @group mailer
+     */
+    public function testSetEncoding_PassInAValidEncoding_EncodingIsSet() {
+        $mailerConfig = new SmtpMailerConfiguration();
+        $expected     = Encoding::EightBit;
+
+        $mailerConfig->setEncoding($expected);
+        $actual = $mailerConfig->getEncoding();
+        self::assertEquals($expected, $actual, "The encoding should have been set to {$expected}");
+    }
+
+    /**
+     * @group mailer
+     */
+    public function testSetEncoding_PassInAnInvalidEncoding_ThrowsException() {
+        $mailerConfig = new SmtpMailerConfiguration();
+        $encoding     = "asdf"; // some asinine value that wouldn't actually be used
+
+        self::setExpectedException("MailerException");
+        $mailerConfig->setEncoding($encoding);
+    }
+
+    /**
+     * @group mailer
+     */
+    public function testSetCommunicationProtocol_PassInAValidProtocol_CommunicationProtocolIsSet() {
+        $mailerConfig = new SmtpMailerConfiguration();
+        $expected     = SmtpMailerConfiguration::CommunicationProtocolSsl;
+
+        $mailerConfig->setCommunicationProtocol($expected);
+        $actual = $mailerConfig->getCommunicationProtocol();
+        self::assertEquals($expected, $actual, "The communication protocol should have been set to {$expected}");
+    }
+
+    /**
+     * @group mailer
+     */
+    public function testSetCommunicationProtocol_PassInAnInvalidProtocol_ThrowsException() {
+        $mailerConfig          = new SmtpMailerConfiguration();
+        $communicationProtocol = "asdf"; // some asinine value that wouldn't actually be used
+
+        self::setExpectedException("MailerException");
+        $mailerConfig->setCommunicationProtocol($communicationProtocol);
     }
 }
