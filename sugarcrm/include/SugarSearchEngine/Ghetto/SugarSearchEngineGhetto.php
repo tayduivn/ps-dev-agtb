eafb240308a41522c7060c30f9a0cf20002c23ae
Jim Bartek <jbartek@sugarcrm.com>
2012-09-20 22:49:51 -0700

    yo

--- sugarcrm/include/SugarSearchEngine/Ghetto/SugarSearchEngineGhetto.php
+++ sugarcrm/include/SugarSearchEngine/Ghetto/SugarSearchEngineGhetto.php
@@ -361,7 +361,7 @@ class SugarSearchEngineGhetto extends SugarSearchEngineAbstractBase
     }
     
     public function getServerStatus() {
-        return array('valid' => true, 'status' => "We be searchin' yo' dataz, dawg.");
+        return array('valid' => true, 'status' => "Yo, We be searchin' yo' dataz, dawg.");
     }
     
     public function bulkInsert(array $docs) {}
