568dffac9033c9cfbdb4d5ed4bd6a68869eb5f2d
Jon Whitcraft <jwhitcraft@sugarcrm.com>
2012-09-21 14:19:54 -0400

    SFA-149 - Fix the config settings to have them be returned correctly
    Fix the two classes that currently use forecast API settings to not have them use the the code that got moved up into the method.

--- sugarcrm/modules/Opportunities/SaveOverload.php
+++ sugarcrm/modules/Opportunities/SaveOverload.php
@@ -28,11 +28,12 @@ function perform_save(&$focus){
     //Determine the default commit_stage based on the probability
     if (empty($focus->commit_stage) && $focus->probability !== '')
     {
+        /* @var $admin Administration */
         $admin = BeanFactory::getBean('Administration');
-        $admin->retrieveSettings();
+        $settings = $admin->getConfigForModule('Forecasts');
 
         //Retrieve Forecasts_category_ranges and json decode as an associative array
-        $category_ranges = json_decode(html_entity_decode($admin->settings['Forecasts_category_ranges']), true);
+        $category_ranges = isset($settings['category_ranges']) ? $settings['category_ranges'] : array();
 
         foreach($category_ranges as $key=>$entry)
         {
