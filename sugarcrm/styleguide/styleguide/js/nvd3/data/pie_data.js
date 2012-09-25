6f8cdc01c0d7c64bd9acb22b4de57d32fe2c5d96
Wes Moran <westonnh@gmail.com>
2012-09-25 14:30:20 -0400

    Stats deo adjustment

--- sugarcrm/styleguide/styleguide/js/nvd3/data/pie_data.js
+++ sugarcrm/styleguide/styleguide/js/nvd3/data/pie_data.js
@@ -4,10 +4,6 @@ var pie_data = {
   },
   'data': [
     {
-      key: "Undefined"
-      , value: 100
-    },
-    {
       key: "Cold Call"
       , value: 100
     },
@@ -26,26 +22,6 @@ var pie_data = {
     {
       key: "Partner"
       , value: 100
-    },
-    {
-      key: "Public Relations"
-      , value: 145
-    },
-    {
-      key: "Direct Mail"
-      , value: 175
-    },
-    {
-      key: "Conference"
-      , value: 175
-    },
-    {
-      key: "Trade Show"
-      , value: 225
-    },
-    {
-      key: "Web Site"
-      , value: 60
     },
     {
       key: "Word of mouth"
@@ -76,36 +52,20 @@ var pie_data = {
   },
   'data': [
     {
-      key: "Undefined"
+      key: "Calls"
       , value: 100
     },
     {
-      key: "Cold Call"
-      , value: 100
-    },
-    {
-      key: "Existing Customer"
+      key: "Emails"
       , value: 75
     },
     {
-      key: "Self Generated"
+      key: "Texts"
+      , value: 20
+    },    
+    {
+      key: "Social"
       , value: 150
-    },
-    {
-      key: "Employee"
-      , value: 160
-    },
-    {
-      key: "Partner"
-      , value: 100
-    },
-    {
-      key: "Public Relations"
-      , value: 145
-    },
-    {
-      key: "Other"
-      , value: 25
     }
   ]
 };
