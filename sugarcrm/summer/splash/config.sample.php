d66c52acff0bee4dc8901c172401288953e2b99f
Akshay Joshi <me@akshayjoshi.com>
2012-09-26 14:25:47 -0700

    Allow for empty web_root paths.

--- sugarcrm/summer/splash/config.sample.php
+++ sugarcrm/summer/splash/config.sample.php
@@ -1,7 +1,7 @@
 <?php
 $config = array(
-    "top_url" => "http://<host>:<port>/<web_root>/",
-    "box_url" => "http://<host>:<port>/<web_root>/summer/splash/boxoffice/",
+    "top_url" => "http://<host>:<port><web_root>/",
+    "box_url" => "http://<host>:<port><web_root>/summer/splash/boxoffice/",
     "google_key" => "AIzaSyCEKeg7I_f7QpD1IIEFhlLPXJfLSEo7If8",
     "google_client_id" => "558627148191-2or9f4vjfvsnst6poapelfocgl0oa6mf.apps.googleusercontent.com",
     "google_client_secret" => "BiX5VGH723zlCFmB15bx7rVS",
--- sugarcrm/summer/splash/setupDevEnv.sh
+++ sugarcrm/summer/splash/setupDevEnv.sh
@@ -21,6 +21,10 @@ if [[ "$platform" == 'osx' ]]; then
 	port="$(osascript -e 'Tell application "System Events" to display dialog "Enter the port:" default answer "8888"' -e 'text returned of result' 2>/dev/null)"
 	path="$(osascript -e 'Tell application "System Events" to display dialog "Enter the path from the webroot without initial/trailing slashes:" default answer "sugar66"' -e 'text returned of result' 2>/dev/null)"
 
+    if [[ $path ]]; then
+        path="/""$path"
+    fi;
+
 	sed -i "" "s,<web_root>,$path,g" .htaccess
 	sed -i "" "s,<host>,$name,g" .htaccess
 	sed -i "" "s,<port>,$port,g" .htaccess
@@ -36,25 +40,30 @@ if [[ "$platform" == 'osx' ]]; then
 
 	echo "All done. Go forth and install the sweet, sweet goodness."
 elif [[ $# -eq 3 ]]; then
-        name=$1
-        port=$2
-        path=$3
-        cp .htaccess.sample .htaccess
-        cp boxoffice/.htaccess.sample boxoffice/.htaccess
-        cp config.sample.php config.php
-        cp configs/config.sample.php configs/config.php
-        sed -i  "s,<web_root>,$path,g" .htaccess
-        sed -i  "s,<host>,$name,g" .htaccess
-        sed -i  "s,<port>,$port,g" .htaccess
-        sed -i  "s,<web_root>,$path,g" boxoffice/.htaccess
-        sed -i  "s,<host>,$name,g" boxoffice/.htaccess
-        sed -i  "s,<port>,$port,g" boxoffice/.htaccess
-        sed -i  "s,<web_root>,$path,g" configs/config.php
-        sed -i  "s,<host>,$name,g" configs/config.php
-        sed -i  "s,<port>,$port,g" configs/config.php
-        sed -i  "s,<host>,$name,g" config.php
-        sed -i  "s,<port>,$port,g" config.php
-        sed -i  "s,<web_root>,$path,g" config.php
+    name=$1
+    port=$2
+    path=$3
+
+    if [[ $path ]]; then
+        path="/""$path"
+    fi;
+
+    cp .htaccess.sample .htaccess
+    cp boxoffice/.htaccess.sample boxoffice/.htaccess
+    cp config.sample.php config.php
+    cp configs/config.sample.php configs/config.php
+    sed -i  "s,<web_root>,$path,g" .htaccess
+    sed -i  "s,<host>,$name,g" .htaccess
+    sed -i  "s,<port>,$port,g" .htaccess
+    sed -i  "s,<web_root>,$path,g" boxoffice/.htaccess
+    sed -i  "s,<host>,$name,g" boxoffice/.htaccess
+    sed -i  "s,<port>,$port,g" boxoffice/.htaccess
+    sed -i  "s,<web_root>,$path,g" configs/config.php
+    sed -i  "s,<host>,$name,g" configs/config.php
+    sed -i  "s,<port>,$port,g" configs/config.php
+    sed -i  "s,<host>,$name,g" config.php
+    sed -i  "s,<port>,$port,g" config.php
+    sed -i  "s,<web_root>,$path,g" config.php
 	echo "All done. Go forth and install the sweet, sweet goodness."
 else
 	echo "Platform not supported."
