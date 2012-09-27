bf957118a993a41aaf65b3f79a54330faa78243b
Henry Rogers <henry@hhrogersii.com>
2012-09-27 12:41:24 -0400

    Remove old guard files

--- buildconfig.rb
+++
@@ -1,6 +0,0 @@
-# Build configuration options for the guard monitor.
-$buildOptions = {
-	:flav        => "ent",
-	:ver         => "6.6.0",
-	:build_dir   => "/Users/hrogers/Sites/builds/toffee",
-}
