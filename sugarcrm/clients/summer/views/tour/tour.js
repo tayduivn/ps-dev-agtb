a7ead74302a9563c25bfbd2c9d7a16d4a1fe67a9
Nima Dehnashi <ndehnashi@sugarcrm.com>
2012-09-26 18:46:22 -0700

    Refactored implementation of tour.js - popovers only init when hitting the "next" button - routing works from home -> accounts

--- sugarcrm/clients/summer/views/tour/tour.js
+++ sugarcrm/clients/summer/views/tour/tour.js
@@ -3,79 +3,53 @@
     },
     initialize: function(options) {
         var self = this;
-        //console.log(options);
         app.view.View.prototype.initialize.call(this, options);
-        //console.log("tour::initialize");
     },
     startTour: function(module, viewType, fullTour) {
-        //console.log("tour::start");
         this.tourMode = true;
         this.currentModule = module;
         this.viewType = viewType;
         this.fullTour = fullTour;
 
-        //console.log("startTour::fullTour", fullTour);
-        //console.log("startTour::this.fullTour", this.fullTour);
         // Route to the home page if the user clicked "Full Tour"
         if( fullTour ) {
             //TODO: When we have a "Home" module for summer, change this
-            module = "ActivityStream";
-            viewType = "dashboard";
+            this.currentModule = "ActivityStream";
+            this.viewType = "dashboard";
             app.router.navigate("#", {trigger: true});
         }
 
-        this.populatePopovers(module, viewType);
+        this.initPopover(this.currentModule, this.viewType);
     },
-    endTour: function(context, obj) {
-        context.filter("[data-tour='" + obj.id + "']").popover("hide");
+    endTour: function() {
         this.tourMode = false;
         app.alert.show("tour_end", {level: "info", title:"End of Tour",
             messages: "Thank you for taking the Summer tour! You can re-take the tour anytime by clicking the 'Tour' button in the footer.", autoClose: true});
         return;
     },
-    nextItem: function(context, index, obj, currentArray, data) {
-        /*
-        * This function should handle the following:
-        * - hiding the current popover element
-        * - showing the next popover element
-        * - if a next popover element is not defined, you are either:
-        *   a) at the end of the tour
-        *       - in this case, route to the home page or where
-        *         the user was when they started the tour
-        *   b) at the end of the tour for a module's view
-        *       - in this case, either:
-        *           a) route to the module's next view
-        *           b) route to the next module
-        * */
-
+    nextItem: function(index, obj, currentArray, data) {
         //console.log("next");
         var self = this;
-
-        // hide the current popover
-        // show the next popover
-        // bind next/prev functions
 
         if( obj === _.last(currentArray) ) {
             // Conditions to end the tour
             if( (this.fullTour && this.currentModule === "Opportunities" && this.viewType === "record") ||
                 !(this.fullTour) && (this.viewType === "record" || this.viewType === "dashboard" ) ) {
-                this.endTour(context, obj);
+                $("[data-tour='" + obj.id + "']").popover("hide");
+                this.endTour();
                 return;
             }
-            // figure out routing
-            // call populatePopovers()
 
             // If you're here, fulltour == true and viewtype == anything except record-opps OR
             // fulltour == false, and viewtype == list
             switch(this.currentModule, this.viewType) {
                 case "ActivityStream", "dashboard":
                     app.router.navigate("#Accounts", {trigger: true});
-                    //console.log("tourmode", this.tourMode);
-                    this.populatePopovers("Accounts", "list");
+                    this.initPopover("Accounts", "list");
                     break;
                 case "Accounts", "list":
                     // app.router.navigate("#Accounts/create", {trigger: true});
-                    // this.populatePopovers
+                    // this.initPopover
                     break;
                 case "Accounts", "newrecord":
                     break;
@@ -88,56 +62,41 @@
                 case "Opportunities", "list":
                     break;
              }
-            this.currentModule = app.controller.layout.options.module;
-            this.viewType = app.controller.layout.options.name;
-            this.populatePopovers(this.currentModule, this.viewType);
         }
         else {
             var nextIndex = index + 1,
-                nextObj = currentArray[nextIndex];
+                nextObj = currentArray[nextIndex],
+                $nextEl = $("[data-tour='" + nextObj.id + "']"),
+                templateEl = '<div class="popover '+ nextObj.id + '"><div class="arrow"></div><div class="popover-inner"><h3 class="popover-title"></h3><div class="popover-content"><p></p></div><div class="modal-footer" style="position: relative;"><a class="btn tour-end">End Tour</a><a class="btn btn-primary tour-prev">Prev</a><a class="btn btn-primary tour-next">Next</a></div></div></div>';
 
-            context.filter("[data-tour='" + obj.id + "']").popover("hide");
-            context.filter("[data-tour='" + nextObj.id + "']").popover("show");
+            // hide the current popover
+            $("[data-tour='" + obj.id + "']").popover("hide");
 
-            this.fixPopoverPosition(nextObj.placement, nextObj.id);
+            // show the next popover
+            if( $nextEl.length > 0 ) {
+                $nextEl.popover({title: nextObj.title, content: nextObj.content, placement: nextObj.placement,
+                                 trigger: "manual", template: templateEl}).popover("show");
 
-            // bind event listeners
-            $(".tour-next").on("click", function() {
-                self.nextItem(context, nextIndex, nextObj, currentArray, data);
-            });
-            $(".tour-prev").on("click", function() {
-                self.prevItem(context, nextIndex, nextObj, currentArray, data);
-            });
-            $(".tour-end").on("click", function() {
-                self.endTour(context, nextObj);
-            });
+                this.fixPopoverPosition(nextObj.placement, nextObj.id);
+                this.bindClickEvents(self, $nextEl, nextIndex, nextObj, currentArray, data);
+            }
         }
     },
-    prevItem: function(context, index, obj, currentArray, data) {
+    prevItem: function(index, obj, currentArray, data) {
         var self = this;
-
         //console.log("prev");
 
-        context.filter("[data-tour='" + obj.id + "']").popover("hide");
         var prevIndex = index - 1,
-            prevObj = currentArray[prevIndex];
+            prevObj = currentArray[prevIndex],
+            $prevEl = $("[data-tour='" + prevObj.id + "']");
 
-        context.filter("[data-tour='" + prevObj.id + "']").popover("show");
+        $("[data-tour='" + obj.id + "']").popover("hide");
+        $prevEl.popover("show");
 
-        // bind event listeners
-        $(".tour-next").on("click", function() {
-            self.nextItem(context, prevIndex, prevObj, currentArray, data);
-        });
-        $(".tour-prev").on("click", function() {
-            self.prevItem(context, prevIndex, prevObj, currentArray, data);
-        });
-        $(".tour-end").on("click", function() {
-            self.endTour(context, prevObj);
-        });
+        this.bindClickEvents(self, $prevEl, prevIndex, prevObj, currentArray, data);
     },
-    populatePopovers: function(module, viewType) {
+    initPopover: function(module, viewType) {
         var self = this,
-            $tourEl = $("[data-tour]"),
             tourData = {
                 "ActivityStream":{
                     "dashboard": [
@@ -147,7 +106,9 @@
                         {"id":"tour-navbar-search", "title":"Global Search", "content":"Text for global search", "placement":"bottom"},
                         {"id":"tour-navbar-user", "title":"User Menu", "content":"Text for navbar user menu", "placement":"bottom"},
                         {"id":"tour-navbar-quickcreate", "title":"Quick Create Menu", "content":"Text for navbar quick create", "placement":"bottom"},
-                        {"id":"tour-stream-filter", "title":"Activity Stream Filters", "content":"Text for activity stream filters", "placement":"right"},
+                        {"id":"tour-ActivityStream-stream-filter", "title":"Activity Stream Filters", "content":"Text for activity stream filters", "placement":"right"},
+                        {"id":"tour-stream-timeline", "title":"Activity Stream Timeline", "content":"Text for activity stream timeline", "placement":"bottom"},
+                        {"id":"tour-stream-calendar", "title":"Activity Stream Calendar", "content":"Text for activity stream calendar", "placement":"bottom"},
                         {"id":"tour-stream-post", "title":"Activity Stream Post", "content":"Text for activity stream post", "placement":"right"},
                         {"id":"tour-agenda", "title":"Agenda Widget", "content":"Text for agenda", "placement":"left"},
                         //{"id":"tour-recommended-contacts", "title":"Recommended Contacts", "content":"Text for recommended contacts", "placement":"left"},
@@ -158,11 +119,12 @@
                 },
                 "Accounts":{
                     "list": [
+                        {"id":"tour-navbar-Accounts", "title":"Accounts", "content":"Text for accounts navbar link", "placement":"bottom"},
                         {"id":"tour-Accounts-list-view", "title":"List of Companies", "content":"Text for accounts list view", "placement":"right"},
                         {"id":"tour-Accounts-list-create", "title":"Create a Company", "content":"Text for accounts list create button", "placement":"bottom"},
                         {"id":"tour-Accounts-list-search", "title":"Filter/Search Companies", "content":"Text for accounts list search/filter", "placement":"bottom"},
                         {"id":"tour-countrychart", "title":"Sales Country Chart", "content":"Text for accounts country chart", "placement":"left"},
-                        {"id":"tour-Accounts-stream", "title":"Accounts", "content":"Text for accounts activity stream", "placement":"left"}
+                        {"id":"tour-Accounts-stream-filter", "title":"Accounts Activity Stream", "content":"Text for accounts activity stream", "placement":"left"}
                     ],
                     "newrecord": [
                         {"id":"tour-accounts-record-businesscard", "content":"Text for accounts record business card", "placement":"top"},
@@ -176,65 +138,28 @@
                         {"id":"tour-accounts-record-todos", "content":"Text for accounts record related tasks", "placement":"top"},
                         {"id":"tour-accounts-record-map", "content":"Text for company map", "placement":"top"}
                     ]
-                },
-                "Contacts": {
-                    "list": [
-                        {"id":"tour-accounts-list-view", "content":"Text for accounts list view", "placement":"right"},
-                        {"id":"tour-accounts-countrychart", "content":"Text for accounts country chart", "placement":"top"},
-                        {"id":"tour-accounts-stream", "content":"Text for accounts activity stream", "placement":"top"},
-                        {"id":"tour-accounts-list-search", "content":"Text for accounts list search/filter", "placement":"top"},
-                        {"id":"tour-accounts-list-create", "content":"Text for accounts list create button", "placement":"top"}
-                    ],
-                    "record": [
-                        {"id":"tour-accounts-record-businesscard", "content":"Text for accounts record business card", "placement":"top"},
-                        {"id":"tour-accounts-record-stream", "content":"Text for accounts record activity stream", "placement":"top"},
-                        {"id":"tour-accounts-record-todos", "content":"Text for accounts record related tasks", "placement":"top"},
-                        {"id":"tour-accounts-record-map", "content":"Text for company map", "placement":"top"}
-                    ]
-                },
-                "Opportunities":{
-                    "list": [
-                        {"id":"tour-accounts-list-view", "content":"Text for accounts list view", "placement":"right"},
-                        {"id":"tour-accounts-countrychart", "content":"Text for accounts country chart", "placement":"top"},
-                        {"id":"tour-accounts-stream", "content":"Text for accounts activity stream", "placement":"top"},
-                        {"id":"tour-accounts-list-search", "content":"Text for accounts list search/filter", "placement":"top"},
-                        {"id":"tour-accounts-list-create", "content":"Text for accounts list create button", "placement":"top"}
-                    ],
-                    "record": [
-                        {"id":"tour-accounts-record-businesscard", "content":"Text for accounts record business card", "placement":"top"},
-                        {"id":"tour-accounts-record-stream", "content":"Text for accounts record activity stream", "placement":"top"},
-                        {"id":"tour-accounts-record-todos", "content":"Text for accounts record related tasks", "placement":"top"},
-                        {"id":"tour-accounts-record-map", "content":"Text for company map", "placement":"top"}
-                    ]
                 }
-            };
+            },
+            list = tourData[module][viewType],
+            firstObj = _.first(list),
+            $currentEl = $("[data-tour='" + firstObj.id + "']");
 
-        _.each(tourData[module][viewType], function(value, key, list) {
-            //console.log("value: ", value);
-            //console.log("key: ", key);
-            //console.log("list: ", list);
+        if( $currentEl.length > 0 ) {
+            var templateEl = '<div class="popover '+ firstObj.id + '"><div class="arrow"></div><div class="popover-inner"><h3 class="popover-title"></h3><div class="popover-content"><p></p></div><div class="modal-footer" style="position: relative;"><a class="btn tour-end">End Tour</a><a class="btn btn-primary tour-next">Next</a></div></div></div>';
 
-            var nextPrevEl = '<div class="popover '+ value.id + '"><div class="arrow"></div><div class="popover-inner"><h3 class="popover-title"></h3><div class="popover-content"><p></p></div><div class="modal-footer" style="position: relative;"><a class="btn tour-end">End Tour</a><a class="btn btn-primary tour-prev">Prev</a><a class="btn btn-primary tour-next">Next</a></div></div></div>',
-                currentEl = $tourEl.filter("[data-tour='" + value.id + "']");
+            $currentEl.popover({title: firstObj.title, content: firstObj.content, placement: firstObj.placement,
+                               trigger: "manual", template: templateEl}).popover("show");
 
-            // If this is the first popover item, show it, bind button listeners
-            if( value === _.first(list) ) {
-                var nextEl = '<div class="popover '+ value.id + '"><div class="arrow"></div><div class="popover-inner"><h3 class="popover-title"></h3><div class="popover-content"><p></p></div><div class="modal-footer" style="position: relative;"><a class="btn tour-end">End Tour</a><a class="btn btn-primary tour-next">Next</a></div></div></div>';
-                currentEl.popover({title: value.title, content: value.content, placement: value.placement, trigger: "manual", template: nextEl});
-                $tourEl.filter("[data-tour='" + value.id + "']").popover("show");
-                self.fixPopoverPosition(value.placement, value.id);
-
-                $(".tour-next").on("click", function() {
-                    self.nextItem($tourEl, key, value, list, tourData);
-                });
-                $(".tour-end").on("click", function() {
-                    self.endTour($tourEl, value);
-                });
-            }
-            else {
-                currentEl.popover({title: value.title, content: value.content, placement: value.placement, trigger: "manual", template: nextPrevEl});
-            }
-        });
+            this.fixPopoverPosition(firstObj.placement, firstObj.id);
+            this.bindClickEvents(self, $currentEl, 0, firstObj, list, tourData);
+            // return
+        }
+        else
+        {
+            console.log("oops!");
+            // try next object in list
+            // if it works, return
+        }
     },
     fixPopoverPosition: function(currentPlacement, className) {
         var windowEl = $(window),
@@ -283,5 +208,22 @@
         // Currently the z-index for popover is 1010, which hides it under
         // the navbar and the footer. Boost it to 1030 to account for that.
         $("." + className).css("z-index", 1030);
+    },
+    bindClickEvents: function(context, $el, index, obj, list, data) {
+
+        $(".tour-next").on("click", function() {
+            context.nextItem(index, obj, list, data);
+        });
+        $(".tour-end").on("click", function() {
+            $el.popover("hide");
+            context.endTour();
+        });
+
+        if( $(".tour-prev").length > 0 )
+        {
+            $(".tour-prev").on("click", function() {
+                context.prevItem(index, obj, list, data);
+            });
+        }
     }
 })
