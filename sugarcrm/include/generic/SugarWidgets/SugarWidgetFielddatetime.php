00c2f2a3e41ed0c45c9158848529af2b11abc824
Michael Gusev <misha@guseff.com>
2012-09-28 20:22:44 +0300

    Fixed for unit test.

--- sugarcrm/include/generic/SugarWidgets/SugarWidgetFielddatetime.php
+++ sugarcrm/include/generic/SugarWidgets/SugarWidgetFielddatetime.php
@@ -565,6 +565,32 @@ class SugarWidgetFieldDateTime extends SugarWidgetReportField
 	            array("'-'", $this->reporter->db->convert($column, "quarter")));
 	}
 
+    /**
+     * For oracle we have to return order by string like group by string instead of return field alias
+     *
+     * @param array $layout_def definition of field
+     * @return string order by string for field
+     */
+    public function queryOrderByQuarter($layout_def)
+    {
+        $column = $this->_get_column_select($layout_def);
+        $orderBy = $this->reporter->db->convert(
+            $this->reporter->db->convert($column, "date_format", array('%Y')),
+            'CONCAT',
+            array("'-'", $this->reporter->db->convert($column, "quarter"))
+        );
+
+
+        if (empty($layout_def['sort_dir']) || $layout_def['sort_dir'] == 'a')
+        {
+            return $orderBy." ASC";
+        }
+        else
+        {
+            return $orderBy." DESC";
+        }
+    }
+
     function displayInput(&$layout_def) {
     	global $timedate, $current_language, $app_strings;
         $home_mod_strings = return_module_language($current_language, 'Home');
--- sugarcrm/tests/modules/Forecasts/Bug54941Test.php
+++ sugarcrm/tests/modules/Forecasts/Bug54941Test.php
@@ -30,20 +30,88 @@
 
 require_once('modules/Forecasts/Common.php');
 
-class Bug54941 extends Sugar_PHPUnit_Framework_TestCase
+class Bug54941Test extends Sugar_PHPUnit_Framework_TestCase
 {
-	protected $db;
+    /**
+     * @var DBManager
+     */
+    protected $db;
+
+    /**
+     * @var string
+     */
+    protected $guid1;
+
+    /**
+     * @var string
+     */
+    protected $guid2;
 
     public function setUp()
     {
         $this->db = DBManagerFactory::getInstance();
 
 		// forecast schedule item insert
-		$query = "INSERT INTO `forecast_schedule` (`id`, `timeperiod_id`, `user_id`, `cascade_hierarchy`, `forecast_start_date`, `status`, `created_by`, `date_entered`, `date_modified`, `deleted`) VALUES ('2557277b-3164-fbea-ea6b-503606745568', '73c179c8-9de1-c859-772b-503603116e6d', '1', 0, '2012-06-01', 'Active', '1', '2012-08-23 10:30:58', '2012-08-23 10:30:58', 0);";
+        $query = "
+            INSERT INTO
+                forecast_schedule
+                (
+                    id,
+                    timeperiod_id,
+                    user_id,
+                    cascade_hierarchy,
+                    forecast_start_date,
+                    status,
+                    created_by,
+                    date_entered,
+                    date_modified,
+                    deleted
+                )
+                VALUES
+                (
+                    " . $this->db->quoted($this->guid1) . ",
+                    " . $this->db->quoted($this->guid2) . ",
+                    '1',
+                    0,
+                    " . $this->db->convert($this->db->quoted('2012-06-01'), 'date') . ",
+                    'Active',
+                    '1',
+                    " . $this->db->convert($this->db->quoted('2012-08-23 10:30:58'), 'datetime') . ",
+                    " . $this->db->convert($this->db->quoted('2012-08-23 10:30:58'), 'datetime') . ",
+                    0
+                )
+        ";
 		$this->db->query($query);
 
 		// time period item insert
-		$query = "INSERT INTO `timeperiods` (`id`, `name`, `parent_id`, `start_date`, `end_date`, `created_by`, `date_entered`, `date_modified`, `deleted`, `is_fiscal_year`) VALUES ('73c179c8-9de1-c859-772b-503603116e6d', 'FY-2012-August', '', '2012-08-01', '2012-08-31', '1', '2012-08-23 10:16:34', '2012-08-23 10:16:34', 0, 0);";
+        $query = "
+            INSERT INTO
+                timeperiods
+                (
+                    id,
+                    name,
+                    parent_id,
+                    start_date,
+                    end_date,
+                    created_by,
+                    date_entered,
+                    date_modified,
+                    deleted,
+                    is_fiscal_year
+                )
+                VALUES
+                (
+                    " . $this->db->quoted($this->guid2) . ",
+                    'FY-2012-August',
+                    '',
+                    " . $this->db->convert($this->db->quoted('2012-08-01'), 'date') . ",
+                    " . $this->db->convert($this->db->quoted('2012-08-31'), 'date') . ",
+                    '1',
+                    " . $this->db->convert($this->db->quoted('2012-08-23 10:16:34'), 'datetime') . ",
+                    " . $this->db->convert($this->db->quoted('2012-08-23 10:16:34'), 'datetime') . ",
+                    0,
+                    0)
+        ";
 		$this->db->query($query);
     }
 
@@ -51,11 +119,11 @@ class Bug54941 extends Sugar_PHPUnit_Framework_TestCase
     public function tearDown()
     {
         // forecast schedule item removal
-		$query = "DETETE FROM `forecast_schedule` WHERE id='2557277b-3164-fbea-ea6b-503606745568'";
+        $query = "DELETE FROM forecast_schedule WHERE id=" . $this->db->quoted($this->guid1);
 		$this->db->query($query);
 
 		// time period item removal
-		$query = "DETETE FROM `timeperiods` WHERE id='73c179c8-9de1-c859-772b-503603116e6d';";
+        $query = "DELETE FROM timeperiods WHERE id=" . $this->db->quoted($this->guid2);
 		$this->db->query($query);
     }
 
@@ -77,22 +145,25 @@ class Bug54941 extends Sugar_PHPUnit_Framework_TestCase
     public function testTimePeriodsIntervals($nowDate, $resultsFoundExpected)
 	{
 		$common = new Common();
-		$nowDate = $this->db->quoted($nowDate);
 
 		$query = "SELECT a.timeperiod_id, b.name, b.start_date, b.end_date, a.user_id, a.cascade_hierarchy"
 		            . " FROM forecast_schedule a, timeperiods b"
 		            . " WHERE a.timeperiod_id = b.id"
-		            . " AND b.id = '73c179c8-9de1-c859-772b-503603116e6d' "
-		            . " AND a.forecast_start_date <= $nowDate "
-		            . " AND b.end_date >= $nowDate "
+                    . " AND b.id = " . $this->db->quoted($this->guid2)
+                    . " AND a.forecast_start_date <= " . $this->db->convert($this->db->quoted($nowDate), 'date')
+                    . " AND b.end_date >= " . $this->db->convert($this->db->quoted($nowDate), 'date')
 		            . " AND a.deleted = 0"
 		            . " AND b.deleted = 0"
 		            . " AND a.status = 'Active'"
 		            . " ORDER BY b.start_date, b.end_date";
 
 		$result = $common->db->query($query);
-		$resultsFoundActual = $common->db->getRowCount($result);
+        $actual = 0;
+        while ($this->db->fetchByAssoc($result))
+        {
+            $actual ++;
+        }
 
-		$this->assertEquals($resultsFoundExpected, $resultsFoundActual);
+		$this->assertEquals($resultsFoundExpected, $actual);
     }
 }
