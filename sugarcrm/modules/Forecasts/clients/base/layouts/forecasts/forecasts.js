5eb24d89c8a38edaa0d5abd58114b15b093f7568
Jon Whitcraft <jwhitcraft@sugarcrm.com>
2012-10-02 14:52:05 -0400

    Bug NA - Theme Updates
    Update the theme to contain a sub-layout for the header module to combine all the items into so it it will only re-render.

--- sugarcrm/modules/Forecasts/clients/base/layouts/forecasts/forecasts.js
+++ sugarcrm/modules/Forecasts/clients/base/layouts/forecasts/forecasts.js
@@ -300,7 +300,7 @@
             }
 
             //add the components to the div
-            if (comp.name && this.$el.find(divName)[0]) {
+            if (compName && this.$el.find(divName)[0]) {
                 this.$el.find(divName).append(comp.$el);
             } else {
                 this.$el.append(comp.$el);
