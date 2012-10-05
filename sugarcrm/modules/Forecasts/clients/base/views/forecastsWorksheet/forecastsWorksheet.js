tayduivn/ps-dev-agtb

commit 9955fbde707ea895f1e793020d2ae01c54790fa4

Bug 56710: Added more dirty checks to worksheets.

sugarcrm/modules/Forecasts/clients/base/views/forecastsWorksheet/forecastsWorksheet.js
@@ -186,6 +186,15 @@
             		this.context.forecasts.set({reloadWorksheetFlag: false});
             	}
             }, this);
+            this.context.forecasts.on("change:checkDirtyWorksheetFlag", function(){
+            	if(this.context.forecasts.get('checkDirtyWorksheetFlag') && !this.showMe()){
+            		var model = this.context.forecasts.worksheet;
+            		model.url = this.createURL();
+            		this.safeFetch(false);
+            		this.context.forecasts.set({checkDirtyWorksheetFlag: false});
+            	}
+
+            }, this);
 
             this.context.forecasts.config.on('change:show_worksheet_likely', function(context, value) {
                 // only trigger if this component is rendered
@@ -261,8 +270,9 @@
     /**
      * This function checks to see if the worksheet is dirty, and gives the user the option
      * of saving their work before the sheet is fetched.
+     * @param fetch {boolean} Tells the function to go ahead and fetch if true, or runs dirty checks (saving) w/o fetching if false 
      */
-    safeFetch: function(){
+    safeFetch: function(fetch = true){
     	var collection = this._collection; 
     	var self = this;
     	if(collection.isDirty){
@@ -279,7 +289,9 @@
     			collection.isDirty = false;
 				$.when(!collection.isDirty).then(function(){
 	    			self.context.forecasts.set({reloadCommitButton: true});
-	    			collection.fetch();
+	    			if(fetch){
+	    				collection.fetch();
+	    			}
     		});
 			
 		}
@@ -287,12 +299,16 @@
     			//ignore, fetch still
     			collection.isDirty = false;
     			self.context.forecasts.set({reloadCommitButton: true});
-    			collection.fetch();
+    			if(fetch){
+    				collection.fetch();
+    			}
     		}
     	}
     	else{
     		//no changes, fetch like normal.
-    		collection.fetch();	
+    		if(fetch){
+				collection.fetch();
+			}	
     	}    	
     },
 
@@ -319,6 +335,7 @@
         }
         $("#view-sales-rep").show();
         $("#view-manager").hide();
+        this.context.forecasts.set({checkDirtyWorksheetFlag: true});
 		this.context.forecasts.set({currentWorksheet: "worksheet"});
         this.isEditableWorksheet = this.isMyWorksheet();
         this._setForecastColumn(this.meta.panels[0].fields);

sugarcrm/modules/Forecasts/clients/base/views/forecastsWorksheetManager/forecastsWorksheetManager.js
@@ -163,6 +163,15 @@
             	}
 
             }, this);
+            this.context.forecasts.on("change:checkDirtyWorksheetFlag", function(){
+            	if(this.context.forecasts.get('checkDirtyWorksheetFlag') && !this.showMe()){
+            		var model = this.context.forecasts.worksheetmanager;
+            		model.url = this.createURL();
+            		this.safeFetch(false);
+            		this.context.forecasts.set({checkDirtyWorksheetFlag: false});
+            	}
+
+            }, this);
 
             this.context.forecasts.config.on('change:show_worksheet_likely', function(context, value) {
                 // only trigger if this component is rendered
@@ -239,8 +248,9 @@
     /**
      * This function checks to see if the worksheet is dirty, and gives the user the option
      * of saving their work before the sheet is fetched.
+     * @param fetch {boolean} Tells the function to go ahead and fetch if true, or runs dirty checks (saving) w/o fetching if false 
      */
-    safeFetch: function(){
+    safeFetch: function(fetch = true){
     	var collection = this._collection;
     	var self = this;
     	if(collection.isDirty){
@@ -257,7 +267,9 @@
     			collection.isDirty = false;
 				$.when(!collection.isDirty).then(function(){
 	    			self.context.forecasts.set({reloadCommitButton: true});
-	    			collection.fetch();
+	    			if(fetch){
+	    				collection.fetch();
+	    			}
     		});
 
 		}
@@ -265,12 +277,18 @@
     			//ignore, fetch still
     			collection.isDirty = false;
     			self.context.forecasts.set({reloadCommitButton: true});
-    			collection.fetch();
+    			if(fetch){
+    				collection.fetch();
+    			}
+    			
     		}
     	}
     	else{
     		//no changes, fetch like normal.
-    		collection.fetch();
+    		if(fetch){
+    			collection.fetch();
+    		}
+    		
     	}
     },
 
@@ -301,6 +319,7 @@
         }
         $("#view-sales-rep").hide();
         $("#view-manager").show();
+        this.context.forecasts.set({checkDirtyWorksheetFlag: true});
         this.context.forecasts.set({currentWorksheet: "worksheetmanager"});
         app.view.View.prototype._render.call(this);
 

sugarcrm/modules/Forecasts/clients/base/layouts/forecasts/forecasts.js
@@ -46,6 +46,22 @@
                 selectedGroupBy : defaultSelections.group_by,
                 selectedDataSet: defaultSelections.dataset,
                 selectedUser : defaultSelections.selectedUser,
+                
+                /**
+                 * boolean to see fire the dirty check for switching between manager and rep worksheets of the same user
+                 */
+                checkDirtyWorksheetFlag: false,
+                
+                /**
+                 * boolean to reload the active worksheet
+                 */
+                reloadWorksheetFlag: false,
+                
+                /**
+                 * The active worksheet
+                 */
+                currentWorksheet: "",
+                
 
                 /**
                  * used across Forecasts to contain sales rep worksheet totals

commit 867e4fa25e473e18cfaa19f265046ea44e416a11

Fix javascript errors caused by commit 9955fbde707ea895f1e793020d2ae01c54790fa4.  The default variable syntax is not compliant across all browsers.

sugarcrm/modules/Forecasts/clients/base/views/forecastsWorksheet/forecastsWorksheet.js
@@ -182,7 +182,7 @@
             	if(this.context.forecasts.get('reloadWorksheetFlag') && this.showMe()){
             		var model = this.context.forecasts.worksheet;
             		model.url = this.createURL();
-            		this.safeFetch();
+            		this.safeFetch(true);
             		this.context.forecasts.set({reloadWorksheetFlag: false});
             	}
             }, this);
@@ -272,7 +272,7 @@
      * of saving their work before the sheet is fetched.
      * @param fetch {boolean} Tells the function to go ahead and fetch if true, or runs dirty checks (saving) w/o fetching if false 
      */
-    safeFetch: function(fetch = true){
+    safeFetch: function(fetch){
     	var collection = this._collection; 
     	var self = this;
     	if(collection.isDirty){
@@ -672,7 +672,7 @@
         	return false;
         }
         this._collection.url = this.createURL();
-        this.safeFetch();
+        this.safeFetch(true);
     },
 
     /**
@@ -775,7 +775,7 @@
         	return false;
         }
         this._collection.url = this.createURL();
-        this.safeFetch();
+        this.safeFetch(true);
     },
 
     /**

sugarcrm/modules/Forecasts/clients/base/views/forecastsWorksheetManager/forecastsWorksheetManager.js
@@ -123,7 +123,7 @@
         }
         this._collection = this.context.forecasts.worksheetmanager;
         this._collection.url = this.createURL();
-        this.safeFetch();
+        this.safeFetch(true);
     },
 
     bindDataChange: function() {
@@ -250,7 +250,7 @@
      * of saving their work before the sheet is fetched.
      * @param fetch {boolean} Tells the function to go ahead and fetch if true, or runs dirty checks (saving) w/o fetching if false 
      */
-    safeFetch: function(fetch = true){
+    safeFetch: function(fetch){
     	var collection = this._collection;
     	var self = this;
     	if(collection.isDirty){
@@ -547,7 +547,7 @@
             return false;
         }
         model.url = this.createURL();
-        this.safeFetch();
+        this.safeFetch(true);
     },
 
     /**
@@ -562,7 +562,7 @@
         	return false;
         }
         model.url = this.createURL();
-        this.safeFetch();
+        this.safeFetch(true);
     },
 
     createURL:function() {