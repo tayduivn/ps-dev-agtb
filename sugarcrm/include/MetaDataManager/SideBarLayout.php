d31f301f4cfe0f34ef2276d8b329c2c398a040a0
Peter D <ixce@projectixce.net>
2012-10-05 14:30:44 -0700

    Forgot to add layout files, removed sugaraccess test

---
+++ sugarcrm/include/MetaDataManager/SideBarLayout.php
@@ -0,0 +1,43 @@
+<?php
+
+class SideBarLayout {
+
+    protected $mainPane;
+    protected $sidePane;
+    protected $previewPane;
+    protected $containers = array();
+    protected $layout;
+    protected $baseLayout;
+
+    public function __construct() {
+        $this->layout = new GenericLayout("sidebar", "default");
+        $this->baseLayout = new GenericLayout("base");
+
+        $this->mainPane = $this->containers['main'] = new GenericLayout("main-pane");
+        $this->sidePane = $this->containers['side'] = new GenericLayout("side-pane");
+        $this->previewPane = $this->containers['preview'] = new GenericLayout("preview-pane");
+
+        $this->mainPane->set("span", 8);
+        $this->sidePane->set("span", 4);
+    }
+
+    public function push($section, $component) {
+        if (isset($this->containers[$section])) {
+            $this->containers[$section]->push($component);
+        }
+    }
+
+    public function setSectionSpan($section, $span) {
+        $this->containers[$section]->set("span", $span);
+    }
+
+    public function getLayout() {
+        $this->push("preview", array("view" => "preview"));
+        $this->layout->push(array("layout" => $this->mainPane->getLayout()));
+        $this->layout->push(array("layout" => $this->sidePane->getLayout()));
+        $this->layout->push(array("layout" => $this->previewPane->getLayout()));
+
+        $this->baseLayout->push($this->layout->getLayout(true));
+        return $this->baseLayout->getLayout();
+    }
+}
