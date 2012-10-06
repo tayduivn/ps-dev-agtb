771441d4cf59afa229d3f83cf803ff320b114f3b
Collin Lee <clee@sugarcrm.com>
2012-10-05 23:36:53 -0400

    Fixes two javascript errors:
    1) A function call was made on a handlebar instance that was not properly setup
    2) An extra comma was placed after a closure block that will trigger errors in browsers like IE
    
    Added a Jasmine test for forecastsCommitted to test that the buildForecastsCommitted method was called and the log entries were created.

--- sugarcrm/tests/modules/Forecasts/clients/base/suites/views/forecastsCommitted.js
+++ sugarcrm/tests/modules/Forecasts/clients/base/suites/views/forecastsCommitted.js
@@ -18,68 +18,42 @@
  *to the License for the specific language governing these rights and limitations under the License.
  *Portions created by SugarCRM are Copyright (C) 2004 SugarCRM, Inc.; All Rights Reserved.
  ********************************************************************************/
-/*
 describe("The forecastCommitted view", function(){
 
-    var app, view, committedSaveFunction;
+    var app, view, formatAmountLocaleStub, createHistoryLogStub;
 
     beforeEach(function() {
         app = SugarTest.app;
         view = SugarTest.loadFile("../modules/Forecasts/clients/base/views/forecastsCommitted", "forecastsCommitted", "js", function(d) { return eval(d); });
+        SugarTest.loadFile("../sidecar/src/utils", "currency", "js", function(d) { return eval(d); });
+        SugarTest.loadFile("../modules/Forecasts/clients/base/lib", "ForecastsUtils", "js", function(d) { return eval(d); });
+
+        var model1 = new Backbone.Model({date_entered:"2012-12-05T11:14:25-04:00", best_case : 100, likely_case : 90, base_rate : 1 });
+        var model2 = new Backbone.Model({date_entered:"2012-10-05T11:14:25-04:00", best_case : 110, likely_case : 100, base_rate : 1 });
+        var model3 = new Backbone.Model({date_entered:"2012-11-05T11:14:25-04:00", best_case : 120, likely_case : 110, base_rate : 1 });
+        var collection = new Backbone.Collection([model1, model2, model3]);
+        view._collection = collection;
+
+        formatAmountLocaleStub = sinon.stub(app.currency, "formatAmountLocale", function(value) {
+            return value;
+        });
+
+        createHistoryLogStub = sinon.stub(app.forecasts.utils, "createHistoryLog", function(model, previousModel) {
+            return "createHistoryLog";
+        });
+
+        view.render = function() {};
     });
 
-    describe("test commitForecast function", function() {
+    afterEach(function() {
+        formatAmountLocaleStub.restore();
+        createHistoryLogStub.restore();
+    });
 
-        beforeEach(function() {
-
-            view._collection = new Backbone.Collection();
-
-
-            //Set a spy on the set function
-            committedSaveFunction = sinon.spy(view._collection, "set");
-
-            view.context = {
-                forecasts : {
-                    get : function(key) {
-                        if(key == "commitButtonEnabled") {
-                            return true;
-                        } else if(key == "selectedUser") {
-                            return {
-                                isManager : false,
-                                showOpps : true
-                            }
-                        }
-                    },
-
-                    set : function(key, value) {
-
-                    }
-                }
-            };
-
-            view.totals = {
-                best_adjusted: 100,
-                likely_adjusted: 100,
-                worst_adjusted: 100,
-                amount: 100,
-                included_opp_count: 1
-            }
-
-            view.timePeriodId = 'abc';
-            view.forecastType = 'Direct';
+    describe("test buildForecastsCommitted function", function() {
+        it("should create two historyLog entries", function() {
+            view.buildForecastsCommitted();
+            expect(view.historyLog.length == 2).toBeTruthy();
         });
-
-        afterEach(function() {
-            committedSaveFunction.restore();
-        })
-
-
-        it("test to see that forecast uses base currency_id (-99) and base_rate (1)", function() {
-            view.commitForecast();
-            expect(committedSaveFunction).toHaveBeenCalled();
-        });
-
-
     });
 });
-*/
