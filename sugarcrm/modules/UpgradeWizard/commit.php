7a55ef655a924ceac9fc0e684c78abd8e0d80d74
Robert Gonzalez <rgonzalez@sugarcrm.com>
2012-10-09 20:59:46 -0700

    Bug 56837: Customization of mobile layout is lost after upgrade Fix missing upgrade version in version checking in Upgrade Wizard Add additional logging messages in commit script

--- sugarcrm/modules/UpgradeWizard/commit.php
+++ sugarcrm/modules/UpgradeWizard/commit.php
@@ -297,11 +297,23 @@ eoq;
         // 6.6 metadata enhancements for portal and wireless, should only be 
         // handled for upgrades FROM pre-6.6 to a version POST 6.6 and MUST be
         // handled AFTER inclusion of the upgrade package files
+        // We will need the manifest for this, so if it isn't set, get it
+        $upgrade_version = null;
+        if (isset($manifest['version'])) {
+            $upgrade_version = $manifest['version'];
+        } else {
+            // Get the manifest
+            require $unzip_dir . '/manifest.php';
+            if (isset($manifest['version'])) {
+                $upgrade_version = $manifest['version'];
+            }
+        }
+    
         if (!didThisStepRunBefore('commit','upgradePortalMobileMetadata')) {
-            if (version_compare($sugar_version, '6.6.0') == -1 && version_compare($manifest['version'], '6.6.0', '>=')) {
+            if (version_compare($sugar_version, '6.6.0') == -1 && ($upgrade_version === null || version_compare($upgrade_version, '6.6.0', '>='))) {
                 if (file_exists('modules/UpgradeWizard/SidecarUpdate/SidecarMetaDataUpgrader.php')) {
                     set_upgrade_progress('commit','in_progress','upgradePortalMobileMetadata','in_progress');
-                    logThis('Preparing to upgrade metadata to 6.6.0 compatibility...');
+                    logThis('Preparing to upgrade mobile and portal metadata to 6.6.0 compatibility...');
                     require_once 'modules/UpgradeWizard/SidecarUpdate/SidecarMetaDataUpgrader.php';
                     
                     // Get the sidecar metadata upgrader
@@ -310,7 +322,8 @@ eoq;
                     // Run the upgrader
                     $smdUpgrader->upgrade();
                     
-                    // Log failures if any
+                    // Log status and failures if any
+                    logThis('Mobile and portal view metadata file upgrade is complete.');
                     $failures = $smdUpgrader->getFailures();
                     if (!empty($failures)) {
                         logThis(count($failures) . ' metadata files failed to upgrade:');
