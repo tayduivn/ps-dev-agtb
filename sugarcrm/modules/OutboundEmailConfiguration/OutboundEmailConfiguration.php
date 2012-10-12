9433082cb7fd650af23a39bdfe2233e708fc9e0d
Greg Levine <glevine@sugarcrm.com>
2012-10-12 14:47:29 -0400

    jira: MAR-247 Moved code from MailerConfiguration into OutboundEmailConfiguration.

--- sugarcrm/modules/OutboundEmailConfiguration/OutboundEmailConfiguration.php
+++ sugarcrm/modules/OutboundEmailConfiguration/OutboundEmailConfiguration.php
@@ -28,7 +28,14 @@ require_once "SmtpMailerConfiguration.php"; // also imports MailerConfiguration.
 
 // external imports
 require_once "modules/Users/User.php";
+require_once "modules/Mailer/MailerException.php";    // requires MailerException in order to throw exceptions of that
+                                                      // type
+require_once "modules/Mailer/Encoding.php";           // needs the valid encodings defined in Encoding
+require_once "include/Localization/Localization.php"; // required for using the global $locale, which is a Localization
 
+/**
+ * Represents the base configurations and contains the logic for setting the configurations for a Mailer.
+ */
 class OutboundEmailConfiguration
 {
     public $mode;
@@ -45,7 +52,165 @@ class OutboundEmailConfiguration
     public $personal;
     public $mailerConfigData; // MailerConfiguration or SmtpMailerConfiguration
 
+    // protected members
+    protected $hostname; // the hostname to use in Message-ID and Received headers and as default HELO string
+                         // not the server hostname
+    protected $locale;   // the Localization object necessary for performing character set translations
+    protected $charset;  // the character set of the message
+    protected $encoding; // the encoding of the message, which must be one of the valid encodings from Encoding
+    protected $wordwrap; // number of characters per line before the message body wrap
+
+    /**
+     * @access public
+     */
     public function __construct(User $user) {
         $this->user_id = $user->id;
+
+        $this->loadDefaultConfigs();
+    }
+
+    /**
+     * Sets the mailer configuration to its default settings for this sending strategy.
+     *
+     * @access public
+     */
+    public function loadDefaultConfigs() {
+        $this->setHostname();
+        $this->setLocale();
+        $this->setCharset();
+        $this->setEncoding();
+        $this->setWordwrap();
+    }
+
+    /**
+     * Sets or overwrites the hostname configuration.
+     *
+     * @access public
+     * @param string $hostname required
+     * @throws MailerException
+     */
+    public function setHostname($hostname = "") {
+        if (!is_string($hostname)) {
+            throw new MailerException(
+                "Invalid Configuration: hostname must be a string",
+                MailerException::InvalidConfiguration
+            );
+        }
+
+        $this->hostname = $hostname;
+    }
+
+    /**
+     * Returns the hostname configuration.
+     *
+     * @access public
+     * @return string
+     */
+    public function getHostname() {
+        return $this->hostname;
+    }
+
+    /**
+     * Sets or overwrites the locale configuration.
+     *
+     * @access public
+     * @param Localization|null $locale Null is an acceptable value for the purposes of defaulting $this->locale to
+     *                                  null, but the setter should only be used publicly with a valid Localization
+     *                                  object.
+     */
+    public function setLocale(Localization $locale = null) {
+        $this->locale = $locale;
+    }
+
+    /**
+     * @access public
+     * @return Localization|null Null if initialized but never set.
+     */
+    public function getLocale() {
+        return $this->locale;
+    }
+
+    /**
+     * Sets or overwrites the charset configuration.
+     *
+     * @access public
+     * @param string $charset required
+     * @throws MailerException
+     */
+    public function setCharset($charset = "utf-8") {
+        if (!is_string($charset)) {
+            throw new MailerException(
+                "Invalid Configuration: charset must be a string",
+                MailerException::InvalidConfiguration
+            );
+        }
+
+        $this->charset = $charset;
+    }
+
+    /**
+     * Returns the charset configuration.
+     *
+     * @access public
+     * @return string
+     */
+    public function getCharset() {
+        return $this->charset;
+    }
+
+    /**
+     * Sets or overwrites the encoding configuration. Default to quoted-printable for plain/text.
+     *
+     * @access public
+     * @param string $encoding required
+     * @throws MailerException
+     */
+    public function setEncoding($encoding = Encoding::QuotedPrintable) {
+        if (!Encoding::isValid($encoding)) {
+            throw new MailerException(
+                "Invalid Configuration: encoding is invalid",
+                MailerException::InvalidConfiguration
+            );
+        }
+
+        $this->encoding = $encoding;
+    }
+
+    /**
+     * Returns the encoding configuration.
+     *
+     * @access public
+     * @return string
+     */
+    public function getEncoding() {
+        return $this->encoding;
+    }
+
+    /**
+     * Sets or overwrites the wordwrap configuration, which is the number of characters before a line will be wrapped.
+     *
+     * @access public
+     * @param int $chars required
+     * @throws MailerException
+     */
+    public function setWordwrap($chars = 996) {
+        if (!is_int($chars)) {
+            throw new MailerException(
+                "Invalid Configuration: wordwrap must be an integer",
+                MailerException::InvalidConfiguration
+            );
+        }
+
+        $this->wordwrap = $chars;
+    }
+
+    /**
+     * Returns the wordwrap configuration.
+     *
+     * @access public
+     * @return string
+     */
+    public function getWordwrap() {
+        return $this->wordwrap;
     }
 }
