35d7b2c19162b74b2993732edb7f7cbef48391e4
Joel Spriggs <jspriggs@sugarcrm.com>
2012-10-17 09:50:18 -0400

    SFA-198 : added check for timeperiod id when forecast is grabbing default id, if id comes back false then it defaults to empty strings to allow the module to perform

--- sugarcrm/modules/Forecasts/views/view.sidecar.php
+++ sugarcrm/modules/Forecasts/views/view.sidecar.php
@@ -78,8 +78,8 @@ class ForecastsViewSidecar extends SidecarView
         $forecasts_timeframes_dom = TimePeriod::get_not_fiscal_timeperiods_dom();
         // TODO:  These should probably get moved in with the config/admin settings, or by themselves since this file will probably going away.
         $id = TimePeriod::getCurrentId();
-        $defaultSelections["timeperiod_id"]["id"] = $id;
-        $defaultSelections["timeperiod_id"]["label"] = $forecasts_timeframes_dom[$id];
+        $defaultSelections["timeperiod_id"]["id"] = $id ? $id : '';
+        $defaultSelections["timeperiod_id"]["label"] = $id ? $forecasts_timeframes_dom[$id] : '';
 
         // INVESTIGATE:  these need to be more dynamic and deal with potential customizations based on how filters are built in admin and/or studio
         $admin = BeanFactory::getBean("Administration");
