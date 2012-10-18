638baad550bdc7b6bbc80b73593d5e25f559cc20
Majed Itani <mitani@sugarcrm.com>
2012-10-17 21:17:17 -0700

    Opportunities: Add news widget support.

--- sugarcrm/modules/Opportunities/clients/summer/layouts/sidebar/sidebar.php
+++ sugarcrm/modules/Opportunities/clients/summer/layouts/sidebar/sidebar.php
@@ -1,8 +1,10 @@
 <?php
 
 $layout = MetaDataManager::getLayout('GenericLayout');
-$layout->push(array('view' => 'recommended_experts'));
-$layout->push(array('view'=>'influencers'));
 $layout->push(array('view'=>'interactions'));
+$layout->push(array('view'=>'news'));
+$layout->push(array('view'=>'influencers'));
+$layout->push(array('view' => 'recommended_experts'));
 $layout->push(array('view'=>'todo-list'));
+
 $viewdefs['Opportunities']['summer']['layout']['sidebar'] = $layout->getLayout();
