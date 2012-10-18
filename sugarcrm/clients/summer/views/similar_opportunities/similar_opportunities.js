4229d05acb8d81f640df3d59e50345cc6a6b8545
Majed Itani <mitani@sugarcrm.com>
2012-10-17 21:18:27 -0700

    Opportunities: Add the similar opportunities widget.

---
+++ sugarcrm/clients/summer/views/similar_opportunities/similar_opportunities.js
@@ -0,0 +1,24 @@
+({
+    initialize: function(options) {
+        app.view.View.prototype.initialize.call(this,options);
+        this.collections = {};
+    },
+    loadData: function() {
+        var self = this,
+            url = app.api.buildURL(this.module, "similar", {"id": app.controller.context.get("model").id});
+
+        app.api.call("read", url, null, { success: function(data) {
+            _.each(data, function(key, value) {
+                data[value]["picture_url"] = data[value]["picture"] ? app.api.buildFileURL({
+                    module: "Users",
+                    id: data[value]["assigned_user_id"],
+                    field: "picture"
+                }) : "../clients/summer/views/imagesearch/anonymous.jpg";
+        	data[value]['amount'] = app.currency.formatAmountLocale(data[value]['amount'], data[value]['currency_id']);  
+	});
+
+            self.collections = data;
+            self.render();
+        }});
+    }
+})
--- sugarcrm/modules/Opportunities/clients/summer/layouts/sidebar/sidebar.php
+++ sugarcrm/modules/Opportunities/clients/summer/layouts/sidebar/sidebar.php
@@ -4,6 +4,7 @@ $layout = MetaDataManager::getLayout('GenericLayout');
 $layout->push(array('view'=>'interactions'));
 $layout->push(array('view'=>'news'));
 $layout->push(array('view'=>'influencers'));
+$layout->push(array('view'=>'similar_opportunities'));
 $layout->push(array('view' => 'recommended_experts'));
 $layout->push(array('view'=>'todo-list'));
 
