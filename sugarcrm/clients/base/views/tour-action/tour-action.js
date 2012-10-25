f072b3db081c7d438011013d1c565e8c6704837d
Nima Dehnashi <ndehnashi@sugarcrm.com>
2012-10-25 15:04:54 -0700

    Updates to summer tour, page overview feature added, app:view:change will not end the tour until the user refreshes or clicks "end tour"

--- sugarcrm/clients/base/views/tour-action/tour-action.js
+++ sugarcrm/clients/base/views/tour-action/tour-action.js
@@ -14,6 +14,8 @@
         app.view.View.prototype._renderHtml.call(this);
     },
     showTourModal: function() {
+        // TODO: When the <footer> tags have been replaced/start respecting z-indexes
+        // call this.layout.trigger(eventName, ...) to show a modal layout instead
         $('.system-tour').modal('show');
     }
 })
