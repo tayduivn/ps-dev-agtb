1c88548bad1c73ca6bfd47b80edba0bc315c0991
Gabriel Rael <grael@sugarcrm.com>
2012-10-26 09:12:40 -0400

    Remove leftover remnants of the test view used while developing range sliders

--- sugarcrm/modules/Forecasts/clients/base/layouts/forecasts/forecasts.php
+++ sugarcrm/modules/Forecasts/clients/base/layouts/forecasts/forecasts.php
@@ -3,9 +3,6 @@ $viewdefs['Forecasts']['base']['layout']['forecasts'] = array(
     'type' => 'forecasts',
     'components' => array(
         array(
-            'view' => 'testView',
-        ),
-        array(
             'view' => 'forecastsChart',
         ),
         array(
