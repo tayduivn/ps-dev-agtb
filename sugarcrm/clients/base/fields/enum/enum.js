bc746dd61cb0b93e95d934dcacb39a98161c0aa1
Matthew Marum <mmarum@sugarcrm.com>
2012-10-26 14:58:57 -0400

    Bug57814: Fix details view for dropdowns and multi-selects.

--- sugarcrm/clients/base/fields/enum/enum.js
+++ sugarcrm/clients/base/fields/enum/enum.js
@@ -1,12 +1,13 @@
 ({
     fieldTag: "select",
     _render: function() {
+
         var optionsKeys = _.keys(app.lang.getAppListStrings(this.def.options));
         //After rendering the dropdown, the selected value should be the value set in the model,
         //or the default value. The default value fallbacks to the first option if no other is selected.
-        //The chosen plugin displays it correclty, but the value is not set to the select and the model.
+        //The chosen plugin displays it correctly, but the value is not set to the select and the model.
         //Below the workaround to save this option to the model manually.
-        if (_.isUndefined(this.value)) {
+        if (_.isUndefined(this.model.get(this.name))) {
             var defaultValue = _.first(optionsKeys);
             if (defaultValue) {
                 this.$(this.fieldTag).val(defaultValue);
@@ -18,7 +19,7 @@
         var emptyIdx = _.indexOf(optionsKeys, "");
         if (emptyIdx !== -1) {
             chosenOptions.allow_single_deselect = true;
-            // if the blank option isnt at the top of the list we have to add it manually
+            // if the blank option isn't at the top of the list we have to add it manually
             if (emptyIdx > 1) {
                 this.hasBlank = true;
             }
@@ -36,7 +37,6 @@
         var newval = '', optionsObject, optionLabels;
 
         if(this.def.isMultiSelect && this.view.name !== 'edit') {
-
             // Gets the dropdown options e.g. {foo:foolbl, bar:barlbl ...}
             optionsObject = app.lang.getAppListStrings(this.def.options);
 
