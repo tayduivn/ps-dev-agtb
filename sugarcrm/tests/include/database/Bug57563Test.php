e640d1071d656146f37231278cf8e9bad4d75716
Joe Wang <cwang@sugarcrm.com>
2012-10-26 11:09:21 -0700

    Bug_57563: modified unit test

--- sugarcrm/tests/include/database/Bug57563Test.php
+++ sugarcrm/tests/include/database/Bug57563Test.php
@@ -43,31 +43,71 @@ class Bug57563Test extends Sugar_PHPUnit_Framework_TestCase
     {
     }
 
-    public function test_order_by_amount()
+    public function test_union_query_for_limitQuery()
     {
+        $query_object = new CustomQuery();
+
+        $m_closed = $query_object->db->convert('opportunities.date_closed', 'month');
+        $today = $query_object->db->convert('', 'today');
+        $m_date[0] = $query_object->db->convert($today, 'month');
+        for($i=1; $i<6; $i++)
+        {
+            $m_date[$i] = $query_object->db->convert($query_object->db->convert($today, 'add_date', array($i, 'month')), 'month');
+        }
+        $m_date5 = $query_object->db->convert($today, 'add_date', array(5, "month"));
+
         $query = "(
             SELECT
              'New Business' \"Opportunity Type\",
+            sum( case when $m_closed = $m_date[0] then opportunities.amount_usdollar else 0 end ) \"{sc}0{sc}0\",
+            sum( case when $m_closed = $m_date[1] then opportunities.amount_usdollar else 0 end ) \"{sc}0{sc}1\",
+            sum( case when $m_closed = $m_date[2] then opportunities.amount_usdollar else 0 end ) \"{sc}0{sc}2\",
+            sum( case when $m_closed = $m_date[3] then opportunities.amount_usdollar else 0 end ) \"{sc}0{sc}3\",
+            sum( case when $m_closed = $m_date[4] then opportunities.amount_usdollar else 0 end ) \"{sc}0{sc}4\",
+            sum( case when $m_closed = $m_date[5] then opportunities.amount_usdollar else 0 end ) \"{sc}0{sc}5\",
             SUM(opportunities.amount_usdollar) AS \"Total Revenue\"
             FROM opportunities
              LEFT JOIN accounts_opportunities
             ON opportunities.id=accounts_opportunities.opportunity_id
             LEFT JOIN accounts
             ON accounts_opportunities.account_id=accounts.id
-            WHERE opportunities.opportunity_type = 'New Business'
+            WHERE opportunities.date_closed <= $m_date5 AND  opportunities.date_closed >= $today AND opportunities.opportunity_type = 'New Business'
             ) UNION (
             SELECT
              'Existing Business' \"Opportunity Type\",
+            sum( case when $m_closed = $m_date[0] then opportunities.amount_usdollar else 0 end ) \"{sc}0{sc}0\",
+            sum( case when $m_closed = $m_date[1] then opportunities.amount_usdollar else 0 end ) \"{sc}0{sc}1\",
+            sum( case when $m_closed = $m_date[2] then opportunities.amount_usdollar else 0 end ) \"{sc}0{sc}2\",
+            sum( case when $m_closed = $m_date[3] then opportunities.amount_usdollar else 0 end ) \"{sc}0{sc}3\",
+            sum( case when $m_closed = $m_date[4] then opportunities.amount_usdollar else 0 end ) \"{sc}0{sc}4\",
+            sum( case when $m_closed = $m_date[5] then opportunities.amount_usdollar else 0 end ) \"{sc}0{sc}5\",
             SUM(opportunities.amount_usdollar) AS \"Total Revenue\"
             FROM opportunities
              LEFT JOIN accounts_opportunities
             ON opportunities.id=accounts_opportunities.opportunity_id
             LEFT JOIN accounts
             ON accounts_opportunities.account_id=accounts.id
-            WHERE opportunities.opportunity_type = 'Existing Business'
+            WHERE opportunities.date_closed <= $m_date5 AND  opportunities.date_closed >= $today AND opportunities.opportunity_type = 'Existing Business'
+            ) UNION (
+            SELECT
+             'Total Revenue' \"Opportunity Type\",
+            sum( case when $m_closed = $m_date[0] then opportunities.amount_usdollar else 0 end ) \"{sc}0{sc}0\",
+            sum( case when $m_closed = $m_date[1] then opportunities.amount_usdollar else 0 end ) \"{sc}0{sc}1\",
+            sum( case when $m_closed = $m_date[2] then opportunities.amount_usdollar else 0 end ) \"{sc}0{sc}2\",
+            sum( case when $m_closed = $m_date[3] then opportunities.amount_usdollar else 0 end ) \"{sc}0{sc}3\",
+            sum( case when $m_closed = $m_date[4] then opportunities.amount_usdollar else 0 end ) \"{sc}0{sc}4\",
+            sum( case when $m_closed = $m_date[5] then opportunities.amount_usdollar else 0 end ) \"{sc}0{sc}5\",
+            SUM(opportunities.amount_usdollar) AS \"Total Revenue\"
+            FROM opportunities
+             LEFT JOIN accounts_opportunities
+            ON opportunities.id=accounts_opportunities.opportunity_id
+            LEFT JOIN accounts
+            ON accounts_opportunities.account_id=accounts.id
+            WHERE opportunities.date_closed <= $m_date5 AND  opportunities.date_closed >= $today
             )";
 
         $this->_db->limitQuery($query, 0, 1);
+
         $this->assertEmpty($this->_db->lastError(), "lastError should return empty");
     }
 }
