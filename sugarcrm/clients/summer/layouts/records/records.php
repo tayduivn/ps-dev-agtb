9a34dee4cc16f821a51c1278bfd2cb393eeb654d
Nima Dehnashi <ndehnashi@sugarcrm.com>
2012-11-01 18:17:20 -0700

    Preview pane is now a layout with embedded preview views

--- sugarcrm/clients/summer/layouts/records/records.php
+++ sugarcrm/clients/summer/layouts/records/records.php
@@ -3,4 +3,5 @@
 $layout = MetaDataManager::getLayout('SideBarLayout');
 $layout->push('main', array('layout'=>'list-cluster'));
 $layout->push('side', array('layout'=>'list-sidebar'));
+$layout->push('preview', array('layout'=>'preview'));
 $viewdefs['summer']['layout']['records'] = $layout->getLayout();
