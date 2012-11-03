07df3daa07cf7d8c79ac013235f0d0cd293a4665
Jimmy Chen <jchen@sugarcrm.com>
2012-11-02 17:31:54 -0700

    Bug 58084 avoid calling the same js function twice

--- sugarcrm/modules/Calls/metadata/editviewdefs.php
+++ sugarcrm/modules/Calls/metadata/editviewdefs.php
@@ -89,7 +89,7 @@ array (
       'javascript' => '{sugar_getscript file="cache/include/javascript/sugar_grp_jsolait.js"}
 <script type="text/javascript">{$JSON_CONFIG_JAVASCRIPT}</script>
 <script>toggle_portal_flag();function toggle_portal_flag()  {ldelim} {$TOGGLE_JS} {rdelim}
-function formSubmitCheck(){ldelim}var duration=true;if(typeof(isValidDuration)!="undefined"){ldelim}duration=isValidDuration();{rdelim}if(check_form(\'EditView\') && isValidDuration() && CAL.checkRecurrenceForm()){ldelim}SUGAR.ajaxUI.submitForm("EditView");{rdelim}{rdelim}</script>',
+function formSubmitCheck(){ldelim}var duration=true;if(typeof(isValidDuration)!="undefined"){ldelim}duration=isValidDuration();{rdelim}if(check_form(\'EditView\') && duration && CAL.checkRecurrenceForm()){ldelim}SUGAR.ajaxUI.submitForm("EditView");{rdelim}{rdelim}</script>',
 
        'useTabs' => false,
     ),
