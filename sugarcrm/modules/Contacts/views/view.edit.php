96c6e6bb5bd9f45a1f0faff7ff460a6bd66ba4ed
Matthew Marum <mmarum@sugarcrm.com>
2012-11-02 22:18:45 -0400

    Bug57761: cleanup

--- sugarcrm/modules/Contacts/views/view.edit.php
+++ sugarcrm/modules/Contacts/views/view.edit.php
@@ -70,12 +70,12 @@ class ContactsViewEdit extends ViewEdit
                }
            }
 		   echo getVersionedScript('modules/Contacts/Contact.js');
-		   echo '<script language="javascript">';
+		   echo '<script type="text/javascript">';
            echo 'YAHOO.util.Event.onDOMReady(function() {';
-           echo 'addToValidateComparison(\'EditView\', \'portal_password\', \'varchar\', false, SUGAR.language.get(\'app_strings\', \'ERR_SQS_NO_MATCH_FIELD\') + SUGAR.language.get(\'Contacts\', \'LBL_PORTAL_PASSWORD\'), \'portal_password1\');';
-           echo 'addToValidateVerified(\'EditView\', \'portal_name_verified\', \'bool\', false, SUGAR.language.get(\'app_strings\', \'ERR_EXISTING_PORTAL_USERNAME\'));';
-           echo 'YAHOO.util.Event.on(\'portal_name\', \'blur\', validatePortalName);';
-           echo 'YAHOO.util.Event.on(\'portal_name\', \'keydown\', handleKeyDown);';
+           echo '   addToValidateComparison(\'EditView\', \'portal_password\', \'varchar\', false, SUGAR.language.get(\'app_strings\', \'ERR_SQS_NO_MATCH_FIELD\') + SUGAR.language.get(\'Contacts\', \'LBL_PORTAL_PASSWORD\'), \'portal_password1\');';
+           echo '   addToValidateVerified(\'EditView\', \'portal_name_verified\', \'bool\', false, SUGAR.language.get(\'app_strings\', \'ERR_EXISTING_PORTAL_USERNAME\'));';
+           echo '   YAHOO.util.Event.on(\'portal_name\', \'blur\', validatePortalName);';
+           echo '   YAHOO.util.Event.on(\'portal_name\', \'keydown\', handleKeyDown);';
            echo '});';
 		   echo '</script>';
 		}
