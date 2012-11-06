573e23278bf1d0b905f92d1b2c65ee9ff86113f7
Jim Bartek <jbartek@sugarcrm.com>
2012-11-06 11:33:06 -0500

    Bug57392 - CaseApiHelper was always resetting assigned user id.  It should only set assigned user id for non-portal users if its empty.

--- sugarcrm/modules/Cases/CasesApiHelper.php
+++ sugarcrm/modules/Cases/CasesApiHelper.php
@@ -60,7 +60,9 @@ class CasesApiHelper extends SugarBeanApiHelper
         // not a support portal user
         else
         {
-            $bean->assigned_user_id = $_SESSION['authenticated_user_id'];
+            if(empty($bean->assigned_user_id)) {
+                $bean->assigned_user_id = $_SESSION['authenticated_user_id'];
+            }
         }
         return $data;
     }
