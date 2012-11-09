e4853d338ae5f4f985d663bc6d3bd40f54674600
Julien Girault <julien.emac@gmail.com>
2012-11-09 19:20:29 +0100

    Customize file and image input button

--- sugarcrm/clients/base/fields/file/file.js
+++ sugarcrm/clients/base/fields/file/file.js
@@ -36,6 +36,7 @@
             this.attachments.push(fileObj);
         }
         app.view.Field.prototype._render.call(this);
+        this.resizeInput();
         return this;
     },
     startDownload: function(e) {
@@ -61,5 +62,34 @@
             //Keep empty because you cannot set a value of a type `file` input
             app.view.Field.prototype.bindDataChange.call(this);
         }
+    },
+    resizeInput: function () {
+          // Get label width so we can make button fluid, 12px default left/right padding
+          var lbl_width = (this.$('input[type=file]').parent().find('span strong').width() || 14) + 24;
+
+           this.$('input[type=file]').parent().find('span').css('width',lbl_width)
+          .closest('.upload-field-custom').css('width',lbl_width);
+    },
+    onUploadChange:function (e) {
+        var input = e.currentTarget,
+            $input = this.$(input);
+        if (input.value) {
+            var this_container = $input.parent('.file-upload').parent('.upload-field-custom'),
+                value_explode = input.value.split('\\'),
+                value = value_explode[value_explode.length - 1];
+
+            if (this_container.next('.file-upload-status').length > 0) {
+                this_container.next('.file-upload-status').remove();
+            }
+            //this_container.append('<span class="file-upload-status">'+value+'</span>');
+            $('<span class="file-upload-status">' + value + '</span>').insertAfter(this_container);
+        }
+
+    },
+    onUploadFocus:function () {
+        this.$(e.currentTarget).parent().addClass('focus');
+    },
+    onUploadBlur:function () {
+        this.$(e.currentTarget).parent().addClass('focus');
     }
 })
--- sugarcrm/clients/base/fields/image/image.js
+++ sugarcrm/clients/base/fields/image/image.js
@@ -4,7 +4,10 @@
         "mouseenter a" : "showButton",
         "mouseleave a" : "hideButton",
         "mouseleave img" : "hideButton",
-        "click .delete" : "delete"
+        "click .delete" : "delete",
+        "focus input[type=file]": "onUploadFocus",
+        "blur input[type=file]": "onUploadBlur",
+        "change input[type=file]": "onUploadChange"
     },
     fileUrl : "",
     _render: function() {
@@ -12,10 +15,21 @@
         this.model.fileField = this.name;
         this.fileURL = (this.value) ? this.buildUrl() + "&" + this.value : "";
         app.view.Field.prototype._render.call(this);
+        this.resizeInput();
         return this;
     },
-    bindDataChange:function() {
-        //Keep empty because you cannot set a value of a type `file` input
+    bindDataChange: function() {
+        if (this.view.name != "edit" && this.view.fallbackFieldTemplate != "edit") {
+            //Keep empty because you cannot set a value of a type `file` input
+            app.view.Field.prototype.bindDataChange.call(this);
+        }
+    },
+    resizeInput: function () {
+          // Get label width so we can make button fluid, 12px default left/right padding
+          var lbl_width = (this.$('input[type=file]').parent().find('span strong').width() || 14) + 24;
+
+           this.$('input[type=file]').parent().find('span').css('width',lbl_width)
+          .closest('.upload-field-custom').css('width',lbl_width);
     },
     buildUrl: function(options) {
         return app.api.buildFileURL({
@@ -42,5 +56,27 @@
                     app.error.handleHttpError(data, {});
                 }}
         );
+    },
+    onUploadChange:function (e) {
+        var input = e.currentTarget,
+            $input = this.$(input);
+        if (input.value) {
+            var this_container = $input.parent('.file-upload').parent('.upload-field-custom'),
+                value_explode = input.value.split('\\'),
+                value = value_explode[value_explode.length - 1];
+
+            if (this_container.next('.file-upload-status').length > 0) {
+                this_container.next('.file-upload-status').remove();
+            }
+            //this_container.append('<span class="file-upload-status">'+value+'</span>');
+            $('<span class="file-upload-status">' + value + '</span>').insertAfter(this_container);
+        }
+
+    },
+    onUploadFocus:function () {
+        this.$(e.currentTarget).parent().addClass('focus');
+    },
+    onUploadBlur:function () {
+        this.$(e.currentTarget).parent().addClass('focus');
     }
 })
