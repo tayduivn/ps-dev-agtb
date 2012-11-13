1be20e8a53202e509341ff788d5b75525ded98d4
Matthew Marum <mmarum@sugarcrm.com>
2012-11-13 14:05:38 -0500

    Bug58408:  Hide Date Created and Date Modified from Portal edit layout

--- sugarcrm/include/SugarObjects/templates/basic/vardefs.php
+++ sugarcrm/include/SugarObjects/templates/basic/vardefs.php
@@ -79,6 +79,9 @@ $vardefs = array(
 	    'group'=>'modified_by_name',
 	    'comment' => 'Date record last modified',
 	    'enable_range_search' => true,
+          'studio' => array(
+              'portaleditview' => false, // Bug58408 - hide from Portal edit layout
+          ),
 	    'options' => 'date_range_search_dom',
 	  ),
 		'modified_user_id' =>
