60048ed1e63dc4e4a3e462a7c6f84941aae6d5b0
ajmallen <jallen@sugarcrm.com>
2012-11-13 07:53:00 -0800

    Prepared Statements interim checkin. MysqliPreparedStatement framework

---
+++ sugarcrm/include/database/MysqliPreparedStatement.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: jallen
+ * Date: 11/12/12
+ * Time: 2:29 PM
+ * To change this template use File | Settings | File Templates.
+ */
+class MysqliPreparedStatement
+{
+
+}
