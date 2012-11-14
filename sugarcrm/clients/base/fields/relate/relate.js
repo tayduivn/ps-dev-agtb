54f0a7f4016ea7b50430fc6f69a1fcc4db67a15a
justinpark <jpark@sugarcrm.com>
2012-11-13 16:48:16 -0800

    Relate api for editview

--- sugarcrm/clients/base/fields/relate/relate.js
+++ sugarcrm/clients/base/fields/relate/relate.js
@@ -2,6 +2,8 @@
     events: {
         'keyup .chzn-search input': 'throttleSearch'
     },
+    allow_single_deselect: true,
+    minChars: 1,
     /**
      * Initializes field and binds all function calls to this
      * @param {Object} options
@@ -17,53 +19,90 @@
     _render: function() {
         var self = this;
         var result = app.view.Field.prototype._render.call(this);
-        this.$(".relateEdit").chosen({
-            no_results_text: "Searching for " // TODO Add labels support
-        }).change(function(event) {
-            var selected = $(event.target).find(':selected');
-            self.model.set(self.def.id_name, self.unformat(selected.attr('id')));
-            self.model.set(self.def.name, self.unformat(selected.attr('value')));
-        });
+        if(this.action == 'edit') {
+            this.$(".chzn-select").not(".chzn-done").chosen({
+                allow_single_deselect: self.allow_single_deselect,
+                no_results_text: app.lang.get("LBL_SEARCH_FOR")
+            }).change(function(evt){
+                    var selected = $(evt.currentTarget).find(':selected'),
+                        value = selected.text(),
+                        id = selected.val(),
+                        searchmore = selected.data("searchmore"),
+                        empty = selected.data("empty");
+                    if(searchmore || empty) {
+                        $(evt.currentTarget).val('');
+                        $(this).trigger("liszt:updated");
+                        //TODO: inline Modal for selection
+                    } else {
+                        self.setValue({id: id, value: value});
+                    }
+                }).on("liszt:updated", function() {
+                    self.setValue({id: '', value: ''});
+                });
+        }
         return result;
+    },
+    setValue: function(model) {
+        this.model.set(this.def.id_name, model.id, {silent: true});
+        this.model.set(this.def.name, model.value, {silent: true});
     },
     /**
      * Throttles search ajax
      * @param {Object} e event object
      * @param {Integer} interval interval to throttle
      */
-    throttleSearch: function(e, interval) {
-        if (interval === 0 && e.target.value != "") {
-            this.search(e);
-            return;
-        } else {
-            interval = 500;
-            clearTimeout(this.throttling);
-            delete this.throttling;
-        }
-
-        this.throttling = setTimeout(this.throttleSearch, interval, e, 0);
+    throttleSearch: function(evt) {
+        var term = evt.currentTarget.value;
+        if(this._previousTerm != term) {
+            this.search(term);
+        };
     },
+    onSearchSuccess: function() {},
     /**
      * Searches for related field
      * @param event
      */
-    search: function(event) {
-        var self = this;
-        var collection = app.data.createBeanCollection(this.def.module);
-        collection.fetch({
-            params: {q:event.target.value},  // TODO update this to filtering API
-            success: function(data) {
-                if (data.models.length > 0) {
-                    self.selectOptions = data.models;
-                    var options = self.optionsTemplateC(self);
-                    self.$('select').html(options);
-                    self.$('select').trigger("liszt:updated");
-                } else {
-                    //TODO trigger error we found nothing
+    search: _.debounce(function(term) {
+        var self = this,
+            searchModule = this.def.module,
+            chosen_select = this.$(".chzn-select").not(":disabled"),
+            params = {
+                limit: 3
+            };
+        this._previousTerm = term;
+        if(!_.isUndefined(term) && term) {
+            params.q = term;
+        }
+        var _success = function(data) {
+            chosen_select.children().not(":first").remove();
+            self.selectOptions = data.models;
+            var options = self.optionsTemplateC(self);
+            chosen_select.append(options);
+            chosen_select.trigger("liszt:updated");
+            self.$(".chzn-search input").val(term);
+        };
+
+        if(term.length >= this.minChars) {
+            chosen_select.children().not(":first").not(":selected").remove();
+            var adv_search = app.lang.get("LBL_LOADING"),
+                opt_adv_search = new Option(adv_search, '');
+            $(opt_adv_search).html(adv_search).appendTo(chosen_select);
+
+            chosen_select.trigger("liszt:updated");
+            self.$(".chzn-search input").val(term);
+
+            self.search_collection = app.data.createBeanCollection(searchModule);
+            self.search_collection.fetch({
+                params: params,
+                success: _success,
+                error: function() {
+                    app.logger.error("Unable to fetch the bean collection.");
+                    chosen_select.children().not(":first").remove();
+                    var adv_search = app.lang.get("LBL_SEARCH_UNAVAILABLE"),
+                        opt_adv_search = new Option(adv_search, '');
+                    $(opt_adv_search).html(adv_search).appendTo(chosen_select);
                 }
-            }
-
-        });
-    }
-
+            });
+        }
+    }, app.config.requiredElapsed || 500)
 })
