47d40c5f8712279cad5a7eee63921c3dff5a4c33
Joo Lee <jlee@sugarcrm.com>
2012-11-15 16:31:27 -0500

    Added other action links to the headerpane.
    Jira: SIDECAR-112

--- sugarcrm/clients/base/views/create/create.js
+++ sugarcrm/clients/base/views/create/create.js
@@ -1,22 +1,34 @@
 ({
     extendsFrom: 'RecordView',
 
+    events: {
+        'click [name=save_button]': 'save',
+        'click [name=cancel_button]': 'cancel',
+        'click [name=save_create_button]': 'saveAndCreate',
+        'click [name=save_view_button]': 'saveAndView',
+        'click [name=restore_button]': 'restoreModel'
+    },
+
+    // button states
+    STATE: {
+        CREATE: 'create',
+        SAVE: 'save',
+        EDIT: 'edit',
+        DUPLICATE: 'duplicate'
+    },
+
     /**
      * Initialize the view and prepare the model with default button metadata
      * for the current layout.
      */
     initialize: function(options) {
-        _.extend(this.events, {
-            'click [name=save_button]': 'save',
-            'click [name=cancel_button]': 'cancel'
-        });
-
         app.view.views.RecordView.prototype.initialize.call(this, options);
     },
 
     _renderHtml: function() {
         app.view.views.RecordView.prototype._renderHtml.call(this);
         this.setTitle(app.lang.get('LBL_CREATE_BUTTON_LABEL', this.module) + ' ' + this.moduleSingular);
+        this.setButtonAsCreate();
     },
 
     save: function() {
@@ -39,17 +51,109 @@
         app.view.views.RecordView.prototype.bindDataChange.call(this);
 
         this.model.on("change", function() {
-            var $saveButton = this.$('[name=save_button]');
             if (this.model.isValid(undefined, true)) {
-                $saveButton
-                    .removeClass("disabled")
-                    .addClass('btn-primary');
+                this.setButtonAsSave();
             } else {
-                $saveButton
-                    .addClass("disabled")
-                    .removeClass('btn-primary');
+                this.setButtonAsCreate();
             }
         }, this);
+    },
+
+    /**
+     * Handle click on save and create another link
+     */
+    saveAndCreate: function() {
+//        this.context.trigger('quickcreate:saveAndCreate');
+    },
+
+    /**
+     * Handle click on save and view link
+     */
+    saveAndView: function() {
+//        this.context.trigger('quickcreate:saveAndView');
+    },
+
+    /**
+     * Handle click on restore to original link
+     */
+    restoreModel: function() {
+//        this.context.trigger('quickcreate:restore');
+    },
+
+    /**
+     * Change button to Create
+     */
+    setButtonAsCreate: function() {
+        this.setButtonStates(this.STATE.CREATE);
+    },
+
+    /**
+     * Change button to Ignore Duplicate and Save
+     */
+    setButtonAsIgnoreDuplicate: function() {
+        this.setButtonStates(this.STATE.DUPLICATE);
+    },
+
+    /**
+     * Change button to Edit
+     */
+    setButtonAsEdit: function() {
+        this.setButtonStates(this.STATE.EDIT);
+    },
+
+    /**
+     * Change button to Save
+     */
+    setButtonAsSave: function() {
+        this.setButtonStates(this.STATE.SAVE);
+    },
+
+    /**
+     * Change the behavior of buttons depending on the state that they should be in
+     * @param state
+     */
+    setButtonStates: function(state) {
+        var $buttons = {
+            save:        this.$("[name=save_button]"),
+            saveAndNew:  this.$("[name=save_create_button]"),
+            saveAndView: this.$("[name=save_view_button]"),
+            cancel:      this.$("[name=cancel]"),
+            undo:        this.$("[name=restore_button]")
+        };
+
+        switch (state) {
+            case this.STATE.CREATE:
+                $buttons.save
+                    .text(app.lang.get('LBL_SAVE_BUTTON_LABEL', this.module))
+                    .addClass("disabled")
+                    .removeClass('btn-primary');
+                $buttons.saveAndNew.toggleClass('hide', true);
+                $buttons.saveAndView.toggleClass('hide', true);
+                $buttons.undo.toggleClass('hide', true);
+                break;
+            case this.STATE.SAVE:
+                $buttons.save
+                    .text(app.lang.get('LBL_SAVE_BUTTON_LABEL', this.module))
+                    .removeClass("disabled")
+                    .addClass('btn-primary');
+                $buttons.saveAndNew.toggleClass('hide', false);
+                $buttons.saveAndView.toggleClass('hide', false);
+                $buttons.undo.toggleClass('hide', true);
+                break;
+            case this.STATE.EDIT:
+                break;
+            case this.STATE.DUPLICATE:
+                break;
+            default:
+                $buttons.save
+                    .text(app.lang.get('LBL_SAVE_BUTTON_LABEL', this.module))
+                    .addClass("disabled")
+                    .removeClass('btn-primary');
+                $buttons.saveAndNew.toggleClass('hide', true);
+                $buttons.saveAndView.toggleClass('hide', true);
+                $buttons.undo.toggleClass('hide', true);
+                break;
+        }
     }
 
 })
