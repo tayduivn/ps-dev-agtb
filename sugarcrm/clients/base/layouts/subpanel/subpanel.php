5af20dbc40028cff97bdab7acf121df4e8369d2c
Peter D <ixce@projectixce.net>
2012-11-16 15:22:03 -0800

    Minor fixes to summer metadata

--- sugarcrm/clients/base/layouts/subpanel/subpanel.php
+++ sugarcrm/clients/base/layouts/subpanel/subpanel.php
@@ -1,8 +1,8 @@
 <?php
 
-$layout = MetaDataManager::getLayout('GenericLayout', array('type' => 'subpanel'));
+$layout = MetaDataManager::getLayout('GenericLayout', array('type' => 'subpanel', 'default' => 'list'));
 $layout->push(array('view' => 'activitystream', 'label' => 'Activity Stream', 'hidden' => 'true'));
 $layout->push(array('view' => 'calendar', 'label' => 'Calendar', 'hidden' => 'true'));
 $layout->push(array('view' => 'timeline', 'label' => 'Timeline', 'hidden' => 'true'));
-$layout->push(array('view' => 'list', 'label' => 'List'));
+$layout->push(array('view' => 'list', 'label' => 'List', 'hidden' => 'true'));
 $viewdefs['base']['layout']['subpanel'] = $layout->getLayout();
