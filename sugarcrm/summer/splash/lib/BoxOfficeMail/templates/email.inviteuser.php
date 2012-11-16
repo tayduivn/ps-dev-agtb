f56dd3fab862e740511a5bb44efbea3b5fcfcccd
Nima Dehnashi <ndehnashi@sugarcrm.com>
2012-11-15 17:41:07 -0800

    Coding like it's 1999: email templates for Summer

--- sugarcrm/summer/splash/lib/BoxOfficeMail/templates/email.inviteuser.php
+++ sugarcrm/summer/splash/lib/BoxOfficeMail/templates/email.inviteuser.php
@@ -1,45 +1,74 @@
 <?php
 
-$subject = 'You\'ve been invited to Sugar by SugarCRM';
-
+$subject = $user['first_name'] . ' ' . $user['last_name'] . ' has invited you to try Sugar by SugarCRM';
 
 $message = <<<EOQ
 
-
-<table cellspacing="0" cellpadding="0" border="0" width="600" style="background-color: #333; font-family: helvetica, arial, sans serif; padding: 40px 50px 50px 50px">
+<center>
+<table background="{$url}/lib/BoxOfficeMail/templates/img/cubes.png" cellspacing="0" cellpadding="0" border="0" width="800" style="background-color: #F5F5F5; font-family: helvetica, arial, sans serif; padding: 40px 50px 10px 50px;">
 <tr>
- <td>
-   <h1 style="color: #fff; font-size: 18px; padding: 10px 0; border-bottom: 4px solid #2288B5;">{$user['first_name']} {$user['last_name']} has invited to try out the SugarCRM.</h1>
+  <td>
+    <img src="{$url}/lib/BoxOfficeMail/templates/img/SugarCRM_logo.png" />
+  </td>
+</tr>
+<tr>
+ <td colspan="3">
+   <h1 style="color: #333; font-size: 18px; padding: 10px 0; border-bottom: 4px solid #E61718;">{$user['first_name']} {$user['last_name']} has invited you to try Sugar by SugarCRM.</h1>
  </td>
 </tr>
 <tr>
- <td style="padding: 10px 0; font-size: 15px; color: #fff; font-size: 15px;">
-     <p>SugarCRM is the modern productivity application that helps you manage your customers and deals.
-     In short, it makes getting your job done <strong style="color: #ff9900;">easier</strong>.</p>
-
-     <p>
-       SugarCRM offers:
-     </p>
-     <ul style="list-style: none; padding: 0;">
-       <li style="font-weight: bold; padding: 3px 0; margin: 0;">Customer Management</li>
-       <li style="font-weight: bold; padding: 3px 0; margin: 0;">Snappy and Intuitive User Interface</li>
-       <li style="font-weight: bold; padding: 3px 0; margin: 0;">Seamless Collaboration</li>
-     </ul>
-
-     Come check it out @ <a style="font-weight: bold; color: #2288B5; text-decoration: none;" onmouseover="this.style.color='#ff9900'" onmouseout="this.style.color='#2288B5'" href="{$url}">{$url}</a>.
-     <br>
-     Please register and login with this email address and choose instance <strong style="color: #ff9900;">{$instance['name']}</strong>.
-
-   </div>
+ <td style="padding: 10px 0; font-size: 15px; color: #333; font-size: 15px; width: 50%; vertical-align: top">
+     <p style="margin-top: 0">SugarCRM is the modern productivity application that helps you manage your customers and deals.
+     In short, it makes getting your job done <strong style="color: #333;">easier</strong>.</p>
+    <center>
+        <a style="color: #333; text-decoration: none; cursor:pointer;" href="{$url}">
+            <span style="text-decoration: underline; font-weight: bold">Click here to get started</span>
+            <img style="padding-top: 10px;" src="{$url}/lib/BoxOfficeMail/templates/img/getstarted.png"/>
+        </a>
+    </center>
  </td>
+  <td style="padding-top: 10px; font-size: 15px; color: #333; font-size: 15px; padding-left: 20px; width: 50%; vertical-align: top">
+     <span>SugarCRM offers:</span>
+     <ul style="list-style: none; padding: 0; margin-top: 0">
+       <li style="font-weight: bold; padding: 3px 0; margin: 0;">
+        <img style="position: relative; bottom: 10px; vertical-align:middle" src="{$url}/lib/BoxOfficeMail/templates/img/customer.png"/>
+        <span style="margin-left: 5px;">Customer Management</span>
+      </li>
+       <li style="font-weight: bold; padding: 3px 0; margin: 0;">
+        <img style="position: relative; bottom: 10px; vertical-align:middle" src="{$url}/lib/BoxOfficeMail/templates/img/ui.png"/>
+        <span style="margin-left: 5px; ">Snappy and Intuitive User Interface</span>
+      </li>
+       <li style="font-weight: bold; padding: 3px 0; margin: 0;">
+        <img style="position: relative; bottom: 10px; vertical-align:middle" src="{$url}/lib/BoxOfficeMail/templates/img/collaboration.png"/>
+        <span style="margin-left: 5px; ">Seamless Collaboration</span>
+      </li>
+     </ul>
+ </td>
+ </tr>
+ <tr>
+  <td style="font-size: 15px; color: #333;" colspan="2">
+    <p>Please register/login with your email address, and select <strong>{$instance['name']}</strong> to begin using Sugar today!</p>
+    <img style="margin-right: 7px; padding-bottom: 30px" src="{$url}/lib/BoxOfficeMail/templates/img/dashboard.png">
+    <img style="margin-right: 7px; padding-bottom: 30px" src="{$url}/lib/BoxOfficeMail/templates/img/forecasts.png">
+    <img style="padding-bottom: 30px" src="{$url}/lib/BoxOfficeMail/templates/img/contacts.png">
+  </td>
+ </tr>
 </table>
+<table cellspacing="0" cellpadding="0" border="0" width="800" style="background-color: #333; font-family: helvetica, arial, sans serif; padding: 30px 50px 30px 50px; margin: 0 auto;">
+  <tr>
+    <td style="font-size: 11px; color: #FFF; text-align: center" colspan="2">
+      <span>&copy; 2012 &middot; SugarCRM &middot; 10050 North Wolfe Rd, Cupertino, CA</span>
+    </td>
+  </tr>
+</table>
+</center>
 
 EOQ;
 
 
 
 $txt = <<<EOQ
-{$user['first_name']} {$user['last_name']} has invited to try out the Sugar by SugarCRM.
+{$user['first_name']} {$user['last_name']} has invited you to try Sugar by SugarCRM.
 
 SugarCRM is the modern productivity application that helps you manage your customers and deals.
 
@@ -47,7 +76,7 @@ In short, it makes getting your job done easier.
 
 Come check it out at {$url}.
 
-Please register and login with this email address and choose instance {$instance['name']}.
+Please register and login with this email address, and choose instance {$instance['name']}.
 
 
 
