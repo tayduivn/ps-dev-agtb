5093a8f413864c1ce81c9fbf21839ba949dd7a55
John Barlow <jbarlow@sugarcrm.com>
2012-11-19 12:17:40 -0500

    another commit, because Thubs is ruthless!

--- sugarcrm/modules/Forecasts/clients/base/views/forecastsConfigWizardButtons/forecastsConfigWizardButtons.js
+++ sugarcrm/modules/Forecasts/clients/base/views/forecastsConfigWizardButtons/forecastsConfigWizardButtons.js
@@ -79,9 +79,11 @@
             $(evt.target).addClass('disabled');
             var self = this;
             
-            this.model.set({is_setup:true,
-                            show_forecasts_commit_warnings: true
-                            });
+            this.model.set({
+                is_setup:true,
+                show_forecasts_commit_warnings: true
+            });
+            
             // push this model back to the main config model
             this.context.forecasts.config.set(this.model.toJSON());
             this.context.forecasts.config.save({}, {
