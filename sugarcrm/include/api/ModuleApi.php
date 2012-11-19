0a42d3dfd483eeeff110f79b98bfdc450362aa20
Deepali Szczesny <deepali@sugarcrm.com>
2012-11-19 11:46:28 -0500

    Revert "Merge pull request #7856 from dszczesny/6_6_1"
    This reverts commit 002e1e838b4ed7c0a85121e46d95d3f6fd972ed6, reversing
    changes made to 3b4d690794f34c906b737a2d68d84a5927ff90f2.

--- sugarcrm/include/api/ModuleApi.php
+++ sugarcrm/include/api/ModuleApi.php
@@ -57,22 +57,6 @@ class ModuleApi extends SugarApi {
                 'shortHelp' => 'This method deletes a record of the specified type',
                 'longHelp' => 'include/api/help/module_delete_help.html',
             ),
-            'favorite' => array(
-                'reqType' => 'PUT',
-                'path' => array('<module>','?', 'favorite'),
-                'pathVars' => array('module','record', 'favorite'),
-                'method' => 'setFavorite',
-                'shortHelp' => 'This method sets a record of the specified type as a favorite',
-                'longHelp' => 'include/api/help/module_favorite_help.html',
-            ),
-            'unfavorite' => array(
-                'reqType' => 'PUT',
-                'path' => array('<module>','?', 'unfavorite'),
-                'pathVars' => array('module','record', 'unfavorite'),
-                'method' => 'unsetFavorite',
-                'shortHelp' => 'This method unsets a record of the specified type as a favorite',
-                'longHelp' => 'include/api/help/module_favorite_help.html',
-            ),
         );
     }
 
@@ -104,21 +88,6 @@ class ModuleApi extends SugarApi {
 
         $data = $this->formatBean($api, $args, $bean);
 
-        return $data;
-    }
-
-    public function setFavorite($api, $args) {
-        $this->requireArgs($args, array('module', 'record'));
-        $this->toggleFavorites($args['module'], $args['record'], true);
-        $bean = $this->loadBean($api, $args, 'view');
-        $data = $this->formatBean($api, $args, $bean);
-        return $data;
-    }
-    public function unsetFavorite($api, $args) {
-        $this->requireArgs($args, array('module', 'record'));
-        $this->toggleFavorites($args['module'], $args['record'], false);
-        $bean = $this->loadBean($api, $args, 'view');
-        $data = $this->formatBean($api, $args, $bean);
         return $data;
     }
 
