21d27ae950bb1120cad154a1a62bc56d6e7276f1
Steven Parsley <sparsley@sugarcrm.com>
2012-11-20 14:02:47 -0500

    Add logic to hide the convert button when the lead has been converted.  The logic is commented out while development is being done for easier testing

--- sugarcrm/modules/Leads/clients/base/views/record/record.js
+++ sugarcrm/modules/Leads/clients/base/views/record/record.js
@@ -10,6 +10,20 @@
        'click .lead-convert': 'showConvert'
     },
 
+    initialize: function(options) {
+        _.bindAll(this);
+
+        app.view.View.prototype.initialize.call(this, options);
+    },
+
+    render: function() {
+        app.view.View.prototype.render.call(this);
+        // Set the save button to show if the model has been edited.
+        this.model.on("change", function() {
+            //this.setLeadButtonStates();
+        }, this);
+    },
+
     showConvert: function() {
         var layout = app.view.createLayout({
             context: this.context,
@@ -20,5 +34,19 @@
 
         $('.headerpane').parent().before(layout.$el);
         layout.render();
+    },
+
+    /**
+     * Change the behavior of buttons depending on the state that they should be in
+     */
+    setLeadButtonStates: function() {
+        debugger;
+        var convertButton = this.$('.lead-convert'),
+            convertedState = this.model.get('converted') == '1' ? true : false;
+
+        if(convertedState) {
+            convertButton.toggleClass('hide', true);
+        }
     }
+
 })
