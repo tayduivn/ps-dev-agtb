07651aba408f0b22d43c04b877b09339b0b34e94
f1vlad <kulchitski@gmail.com>
2012-11-20 23:34:40 -0500

    Add updated upload widget

--- sugarcrm/styleguide/styleguide/js/application.js
+++ sugarcrm/styleguide/styleguide/js/application.js
@@ -194,8 +194,75 @@
     // timeout the alerts
     setTimeout( function (){$('.timeten').fadeOut().remove();}, 3000);
 
-  });
 
+    /* Custom file upload overrides and avatar widget */
+    var uobj = [],
+      onUploadChange = function (e) {
+        var status = $(this);
+        if ( this.value ) {
+          var this_container = $(this).parent('.file-upload').parent('.upload-field-custom'),
+            value_explode = this.value.split('\\'),
+            value = value_explode[value_explode.length-1];
+
+          if($(this).closest('.upload-field-custom').hasClass('avatar')===true) { /* hide status for avatars */
+            var opts = "hide";
+          }
+
+          if(this_container.next('.file-upload-status').length > 0){
+            this_container.next('.file-upload-status').remove();
+          }
+          //this_container.append('<span class="file-upload-status">'+value+'</span>');
+          $('<span class="file-upload-status '+opts+' ">'+value+'</span>').insertAfter(this_container);
+        }
+      }, 
+      onUploadFocus = function () {
+        $(this).parent().addClass('focus');
+      },
+      onUploadBlur = function () {
+        $(this).parent().addClass('focus');
+      };
+    
+    $('.upload-field-custom input[type=file]').each(function() {     
+      // Bind events
+      $(this)
+        .bind('focus',onUploadFocus)
+        .bind('blur',onUploadBlur)
+        .bind('change',onUploadChange);
+
+      // Get label width so we can make button fluid, 12px default left/right padding
+      var lbl_width = $(this).parent().find('span strong').width() + 24;
+      console.log(lbl_width);
+      $(this)
+        .parent().find('span').css('width',lbl_width)
+        .closest('.upload-field-custom').css('width',lbl_width);
+
+      // Set current state 
+      onUploadChange.call(this);      
+
+      // Minimizes the text input part in IE
+      $(this).css('width','0');
+    });
+
+
+
+
+
+    $('#photoimg').live('change', function() {
+      $("#preview1").html('');
+      $("#preview1").html('<span class="loading">Loading...</span>');
+      $("#imageform").ajaxForm({
+        target: '#preview1'
+      }).submit();
+    });
+
+
+    $('.preview.avatar').click(function(e){
+        $(this).closest('.span10').find('label.file-upload span strong').trigger('click');
+    })
+
+
+
+  });
 }(window.jQuery);
 
 function throwMessage(data,status,temp) {
