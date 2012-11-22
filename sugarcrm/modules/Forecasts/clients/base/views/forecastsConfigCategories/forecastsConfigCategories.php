52ff361c19c8b94cdb0f4218f759fc2ef5ee0e48
Gabriel Rael <grael@sugarcrm.com>
2012-11-21 22:31:17 -0500

    remove no longer needed code

--- sugarcrm/modules/Forecasts/clients/base/views/forecastsConfigCategories/forecastsConfigCategories.php
+++ sugarcrm/modules/Forecasts/clients/base/views/forecastsConfigCategories/forecastsConfigCategories.php
@@ -73,16 +73,6 @@ $viewdefs['Forecasts']['base']['view']['forecastsConfigCategories'] = array(
                             'enabled' => true,
                         ),
 //END SUGARCRM flav=int ONLY
-//                        array(
-//                            'name' => 'exclude',
-//                            'type' => 'range',
-//                            'view' => 'edit',
-//                            'sliderType' => 'connected',
-//                            'minRange' => 0,
-//                            'maxRange' => 100,
-//                            'default' => true,
-//                            'enabled' => true,
-//                        ),
                     ),
                 ),
                 array(
