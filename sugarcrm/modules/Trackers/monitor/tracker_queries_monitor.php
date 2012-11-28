13301932ead6132ba6307e29b72e87d8674f0824
Collin Lee <clee@sugarcrm.com>
2012-11-28 16:34:04 -0500

    Fix unit test failure by fixing the tracker queries monitor so that we always store to the cached_data array, but only write when we flush the output.

--- sugarcrm/modules/Trackers/monitor/tracker_queries_monitor.php
+++ sugarcrm/modules/Trackers/monitor/tracker_queries_monitor.php
@@ -50,77 +50,75 @@ class tracker_queries_monitor extends Monitor implements Trackable {
      * 
      */
     public function save($flush=true) {
-    
-    	if(!$flush) {
-    		$this->cached_data[] = $this->toArray();
-    	} else {
-	    	if(empty($GLOBALS['tracker_' . $this->table_name]) && !empty($this->cached_data)) {
- 				require_once('modules/Trackers/TrackerUtility.php');
-	    		$write_entries = array();
-	    		
-				foreach($this->cached_data as $entry) {
-					$query = str_replace(array("\r", "\n", "\r\n", "\t"), ' ', $entry['text']);
-			        $query = preg_replace("/\s{2,}/", ' ', $query);
-			        $query = TrackerUtility::getGenericSQL($query);
-			        $entry['text'] = $query;
-			        
-			        $md5 = md5($query);	
 
-			        if(!isset($write_entries[$md5])) {
-			           
-			           $entry['query_hash'] = $md5;
-			           $result = $GLOBALS['db']->query("SELECT * FROM tracker_queries WHERE query_hash = '{$md5}'");
-			           
-					   if ($row = $GLOBALS['db']->fetchByAssoc($result)) {	  
-				            $entry['query_id'] = $row['query_id'];
-				            $entry['run_count'] = $row['run_count'] + 1;
-				            $entry['sec_total'] = $row['sec_total'] + $entry['sec_total'];
-				            $entry['sec_avg'] =  ($entry['sec_total'] / $entry['run_count']);
-				        } else {
-				            $entry['query_id'] = create_guid();
-				            $entry['run_count'] = 1;
-				            $entry['sec_total'] = $entry['sec_total'];
-				            $entry['sec_avg'] = $entry['sec_total'];
-				        }			           
-			            $write_entries[$md5] = $entry;
-			        } else {
-			        	$write_entries[$md5]['run_count'] = $write_entries[$md5]['run_count']++;
-			        	$write_entries[$md5]['sec_total'] = $write_entries[$md5]['sec_total'] + $entry['sec_total'];
-			        	$write_entries[$md5]['sec_avg'] = ($write_entries[$md5]['sec_total'] / $write_entries[$md5]['run_count']);
-			        } //if-else				   		
-				} //foreach
-				
-				
-				$trackerManager = TrackerManager::getInstance();
-	    		
-	            if($monitor2 = $trackerManager->getMonitor('tracker_tracker_queries')){ 
-		            $trackerManager->pause();
-			    	//Loop through the stored cached data entries
-			    	foreach($write_entries as $write_e) {
-			    		//Set the values from the cached data entries
-			    		foreach($write_e as $name=>$value) {
-			    			$this->$name = $value;    		
-			    		} //foreach
-			    			
-		                //Write to the tracker_tracker_monitor monitor              		
-			    		$monitor2->setValue('monitor_id', $this->monitor_id);        
-	       				$monitor2->setValue('date_modified', $this->date_modified);	
-	                    $monitor2->setValue('query_id', $this->query_id);
-		               	$monitor2->save($flush); // <--- save to tracker_tracker_monitor
-	                   	
-	                   	foreach($this->stores as $s) {
-			    			$store = $this->getStore($s);
-			    			//Flush to the store
-			    			$store->flush($this);
-	                    }
-	                    //Clear the monitor
-			    		$this->clear();
-			    	} //foreach
-	                $trackerManager->unPause();	
-				}
-	    	} //if
-            unset($this->cached_data);
-    	} //if-else
+        $this->cached_data[] = $this->toArray();
+
+    	if($flush && empty($GLOBALS['tracker_' . $this->table_name]) && !empty($this->cached_data)) {
+            require_once('modules/Trackers/TrackerUtility.php');
+            $write_entries = array();
+
+            foreach($this->cached_data as $entry) {
+                $query = str_replace(array("\r", "\n", "\r\n", "\t"), ' ', $entry['text']);
+                $query = preg_replace("/\s{2,}/", ' ', $query);
+                $query = TrackerUtility::getGenericSQL($query);
+                $entry['text'] = $query;
+
+                $md5 = md5($query);
+
+                if(!isset($write_entries[$md5])) {
+
+                   $entry['query_hash'] = $md5;
+                   $result = $GLOBALS['db']->query("SELECT * FROM tracker_queries WHERE query_hash = '{$md5}'");
+
+                   if ($row = $GLOBALS['db']->fetchByAssoc($result)) {
+                        $entry['query_id'] = $row['query_id'];
+                        $entry['run_count'] = $row['run_count'] + 1;
+                        $entry['sec_total'] = $row['sec_total'] + $entry['sec_total'];
+                        $entry['sec_avg'] =  ($entry['sec_total'] / $entry['run_count']);
+                    } else {
+                        $entry['query_id'] = create_guid();
+                        $entry['run_count'] = 1;
+                        $entry['sec_total'] = $entry['sec_total'];
+                        $entry['sec_avg'] = $entry['sec_total'];
+                    }
+                    $write_entries[$md5] = $entry;
+                } else {
+                    $write_entries[$md5]['run_count'] = $write_entries[$md5]['run_count']++;
+                    $write_entries[$md5]['sec_total'] = $write_entries[$md5]['sec_total'] + $entry['sec_total'];
+                    $write_entries[$md5]['sec_avg'] = ($write_entries[$md5]['sec_total'] / $write_entries[$md5]['run_count']);
+                } //if-else
+            } //foreach
+
+
+            $trackerManager = TrackerManager::getInstance();
+
+            if($monitor2 = $trackerManager->getMonitor('tracker_tracker_queries')){
+                $trackerManager->pause();
+                //Loop through the stored cached data entries
+                foreach($write_entries as $write_e) {
+                    //Set the values from the cached data entries
+                    foreach($write_e as $name=>$value) {
+                        $this->$name = $value;
+                    } //foreach
+
+                    //Write to the tracker_tracker_monitor monitor
+                    $monitor2->setValue('monitor_id', $this->monitor_id);
+                    $monitor2->setValue('date_modified', $this->date_modified);
+                    $monitor2->setValue('query_id', $this->query_id);
+                    $monitor2->save($flush); // <--- save to tracker_tracker_monitor
+
+                    foreach($this->stores as $s) {
+                        $store = $this->getStore($s);
+                        //Flush to the store
+                        $store->flush($this);
+                    }
+                    //Clear the monitor
+                    $this->clear();
+                } //foreach
+                $trackerManager->unPause();
+            }
+        unset($this->cached_data);
+    	} //if
    } //save
 }
 ?>
