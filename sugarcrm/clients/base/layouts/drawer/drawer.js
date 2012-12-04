84668ee827f6e4b4919ee71477fec9d3608169c3
Joo Lee <jlee@sugarcrm.com>
2012-12-04 12:48:33 -0500

    Refactored the drawer layout and made it to extend from modal layout.
    Jira: SIDECAR-134

--- sugarcrm/clients/base/layouts/drawer/drawer.js
+++ sugarcrm/clients/base/layouts/drawer/drawer.js
@@ -1,75 +1,35 @@
-/**
- * Create a drawer view (optionally modal)
- * Incomplete
- */
 ({
-    initialize: function(options) {
-        var self = this,
-            showEvent = options.meta.showEvent;
+    extendsFrom: 'ModalLayout',
 
-        options.meta = _.clone(options.meta);
-        this.metaComponents = options.meta.components;
-        options.meta.components = [];
-        if (options.meta.before){
-            _.each(options.meta.before, function(callback, event){
-                self.before(event, callback);
-            });
-        }
-        app.view.Layout.prototype.initialize.call(this, options);
+    expandTabHtml: '<div class="drawer-tab"><a href="#" title="Collapse list pane" class="btn edit-expand">'
+        + '<i class="icon-chevron-down"></i></a></div>',
+    $expandTab: null,
+
+    backdropHtml: "<div class='drawer-squeezed drawer-backdrop'></div>",
+    $backdrop: null,
+
+    baseComponents: [], //override modal's baseComponent
+
+    initialize: function(options) {
+        app.view.layouts.ModalLayout.prototype.initialize.call(this, options, true);
         this.$el.addClass("drawer");
-        if (showEvent)
-        {
-            if(_.isArray(showEvent)) {
-                //Bind the multiple event handler names
-                _.each(showEvent, function(evt, index) {
-                    self._bindShowEvent(evt);
-                });
-            } else {
-                self._bindShowEvent(showEvent);
-            }
-        }
     },
 
-    _bindShowEvent : function(event, delegate){
-        var self = this;
-        if (_.isObject(event))
-        {
-            delegate = event.delegate;
-            event = event.event;
-        }
-        if (delegate){
-            self.layout.events = self.layout.events || {};
-            self.layout.events[event] = function(params, callback){self.show(params, callback)};
-            self.layout.delegateEvents();
-        } else {
-            self.layout.on(event, function(params, callback){self.show(params, callback);}, self);
-        }
+    _placeComponent: function(component) {
+        app.view.Layout.prototype._placeComponent.call(this, component);
     },
 
     /**
-     *
-     * @param params
-     * @param callback
+     * Add components to the drawer layout and initialize
+     * @param components
      * @private
      */
-    _buildComponentsBeforeShow : function(params, callback) {
-        var params = params || {},
-            message = params.message || '',
-            components = (params.components || this.metaComponents || []),
-            title = (params.title || this.meta.title) + '';
-
+    _initializeComponents: function(components) {
         //stops for empty component elements
         if(components.length == 0) {
-            app.logger.error("Unable to display modal dialog: no components or message");
+            app.logger.error("Unable to display drawer layout: no components exist");
             return false;
         }
-
-        //Push down the existing content
-        this.existingContent = this.existingContent || $("#content>div>div:first");
-        if (!this.visible) {
-            this.existingContent.addClass("drawer-squeezed");
-        }
-
 
         //if previous modal-body exists, remove it.
         if(!_.isUndefined(this._initComponentSize)) {
@@ -83,76 +43,127 @@
             this._initComponentSize = this._components.length;
         }
 
-        this.backdrop = this.backdrop || $("<div class='drawer-squeezed drawer-backdrop'></div>").insertAfter(this.existingContent);
-        this.expandTab = this.expandTab || $('<div class="drawer-tab"><a href="#" title="Collapse list pane" class="btn edit-expand">'
-            + '<i class="icon-chevron-down"></i></a></div>').insertBefore(this.existingContent.children().first());
-
-        this.backdrop.css("display","block");
-        this.expandTab.css("display","block");
-
-        this.expandTab.off();
-        var self = this;
-        this.expandTab.on('click', function () {
-            $(this).find('i').toggleClass('icon-chevron-up').toggleClass('icon-chevron-down');
-            self.$el.toggleClass('expand');
-            self.backdrop.toggleClass('collapse');
-            self.existingContent.toggleClass('collapse');
-          return false;
-        });
-
         this._addComponentsFromDef(components);
-
-        this.context.off("drawer:callback");
-        this.context.on("drawer:callback", function(model) {
-            callback(model);
-            this.hide();
-        },this);
-        this.context.off("drawer:hide");
-        this.context.on("drawer:hide", this.hide, this);
-
-
     },
 
+    /**
+     * Show/open the drawer
+     * @param params
+     * @param callback
+     * @return {Boolean}
+     */
     show: function(params, callback) {
         if (!this.triggerBefore("show")) return false;
-        var self = this;
-        if (params.before){
-            _.each(params.before, function(callback, event){
-                self.offBefore(event);
-                self.before(event, callback);
-            });
+
+        this._setupEventDelegation(params, callback);
+
+        var components = (params.components || this.metaComponents || []);
+        if (this._initializeComponents(components) === false) {
+            return false;
         }
 
-        if (this._buildComponentsBeforeShow(params, callback) === false)
-            return false;
+        this._showDrawer();
+
         this.loadData();
         this.render();
 
-        //Clean out previous span css class
         this.$el.show();
-
-        this.visible = true;
         this.trigger("show");
+
         return true;
     },
-    hide: function(event) {
+
+    /**
+     * Hide/close the drawer
+     * @return {Boolean}
+     */
+    hide: function() {
         if (!this.triggerBefore("hide")) return false;
+
         this.$el.hide();
-        this.$el.removeClass('expand');
-        this.backdrop.removeClass('collapse');
-        this.existingContent.removeClass('collapse');
-        this.visible = false;
-        this.backdrop.css("display", "none");
-        this.expandTab.css("display", "none");
-        this.existingContent.removeClass("drawer-squeezed");
+        this._hideDrawer();
         this.trigger("hide");
+
         return true;
     },
 
     _dispose : function(){
-        delete this.existingContent;
-        delete this.backdrop;
-        delete this.expandTab;
-        app.view.layouts.ModalLayout.prototype._dispose.call(this);
+        delete this.$backdrop;
+        delete this.$expandTab;
+    },
+
+    _setupEventDelegation: function(params, callback) {
+        if (params.before){
+            _.each(params.before, function(callback, event){
+                this.offBefore(event);
+                this.before(event, callback);
+            }, this);
+        }
+
+        this.context.off("drawer:callback").on("drawer:callback", function(model) {
+            callback(model);
+            this.hide();
+        }, this);
+
+        this.context.off("drawer:hide").on("drawer:hide", this.hide, this);
+    },
+
+    /**
+     * Open the drawer
+     * @private
+     */
+    _showDrawer: function() {
+        var $existingContent = this.$el.next();
+        $existingContent.toggleClass("drawer-squeezed", true);
+
+        if (!this.$backdrop) {
+            this.$backdrop = $(this.backdropHtml);
+            this.$el.parent().append(this.$backdrop);
+        }
+
+        if (!this.$expandTab) {
+            this.$expandTab = $(this.expandTabHtml);
+            $existingContent.prepend(this.$expandTab);
+
+            // handle drawer tab behavior
+            this.$expandTab.on('click', _.bind(function(event) {
+                this._toggleDrawer(event.currentTarget);
+                return false;
+            }, this));
+        }
+
+        this.$backdrop.show();
+        this.$expandTab.show();
+    },
+
+    /**
+     * Close the drawer
+     * @private
+     */
+    _hideDrawer: function() {
+        this.$el.removeClass('expand');
+        this.$el.next().removeClass('collapse drawer-squeezed');
+        $('i', this.$expandTab)
+            .addClass('icon-chevron-down')
+            .removeClass('icon-chevron-up');
+
+        this.$expandTab.hide();
+
+        this.$backdrop.removeClass('collapse');
+        this.$backdrop.hide();
+    },
+
+    /**
+     * Toggle the drawer
+     * @param target
+     * @private
+     */
+    _toggleDrawer: function(target) {
+        $('i', target)
+            .toggleClass('icon-chevron-up')
+            .toggleClass('icon-chevron-down');
+        this.$el.toggleClass('expand');
+        this.$backdrop.toggleClass('collapse');
+        this.$el.next().toggleClass('collapse');
     }
 })
