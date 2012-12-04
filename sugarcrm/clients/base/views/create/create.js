8fa65b06f0da000ce2af1a802ed3debaaf9259d7
Joo Lee <jlee@sugarcrm.com>
2012-12-04 15:38:41 -0500

    Changed create view to interact with the drawer once save is complete.
    Jira: SIDECAR-134

--- sugarcrm/clients/base/views/create/create.js
+++ sugarcrm/clients/base/views/create/create.js
@@ -70,8 +70,7 @@
      */
     saveAndClose: function() {
         this.initiateSave(_.bind(function() {
-            //TODO: close pushdown modal instead
-            app.navigate(this.context, this.model, 'record');
+            this.cancel();
             this.alerts.showSuccess();
         }, this));
     },
@@ -118,9 +117,8 @@
     saveAndView: function() {
         this.context.lastSaveAction = this.SAVEACTIONS.SAVE_AND_VIEW;
         this.initiateSave(_.bind(function() {
-            //TODO: close pushdown modal
-            this.alerts.showSuccess();
             app.navigate(this.context, this.model);
+            this.alerts.showSuccess();
         }, this));
     },
 
@@ -512,14 +510,14 @@
         showDuplicateFound: function(numOfDupes, clickHandler) {
             var alert;
 
-            app.alert.show('record-saved', {
+            app.alert.show('record-duplicate', {
                 level: 'warning',
                 title: numOfDupes + ' Duplicate Records.',
                 messages: 'You can <a class="alert-action-save">ignore duplicates and save</a> or select to edit one of the duplicates.',
                 autoClose: true
             });
 
-            alert = app.alert.get('record-saved');
+            alert = app.alert.get('record-duplicate');
             alert.$('.alert-action-save').one('click', function() {
                 clickHandler();
                 alert.close();
