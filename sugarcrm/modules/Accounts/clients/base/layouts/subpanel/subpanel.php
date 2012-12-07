ffa771b5de7d89757f5209c672bec2c7dbcbd378
Peter Deng <ixce@projectixce.net>
2012-12-06 17:41:53 -0800

    Small fixes additions to subpanel

--- sugarcrm/modules/Accounts/clients/base/layouts/subpanel/subpanel.php
+++ sugarcrm/modules/Accounts/clients/base/layouts/subpanel/subpanel.php
@@ -1,6 +1,8 @@
 <?php
 
 $layout = MetaDataManager::getLayout("FilterPanelLayout");
-$layout->push(array("name" => "Related Contacts", "context" => array("link" => "contacts"), "toggles" => array("list")));
-$layout->push(array("name" => "Related Opportunities", "context" => array("link" => "opportunities"), "toggles" => array("list")));
+$layout->push(array("name" => "Member Organizations", "context" => array("link" => "members"), "toggles" => array("list")));
+$layout->push(array("name" => "Contacts", "context" => array("link" => "contacts"), "toggles" => array("list")));
+$layout->push(array("name" => "Opportunities", "context" => array("link" => "opportunities"), "toggles" => array("list")));
+$layout->push(array("name" => "Quotes", "context" => array("link" => "quotes"), "toggles" => array("list")));
 $viewdefs['Accounts']['base']['layout']['subpanel'] = $layout->getLayout();
