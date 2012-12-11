58e7828dd48a01994e2f7fa9f6402fffec567192
Jon Whitcraft <jwhitcraft@sugarcrm.com>
2012-12-10 20:34:32 -0500

    SFA-355 - Convert Int Field
    - set the isEditable in the initialize method
    - added the min and max Value support if they are defined in the defs
    - renamed canEdit() to isEditable()

--- sugarcrm/modules/Forecasts/clients/base/fields/editableInt/EditableInt.js
+++ sugarcrm/modules/Forecasts/clients/base/fields/editableInt/EditableInt.js
@@ -12,12 +12,27 @@
 
     errorCode : '',
 
+    _canEdit : true,
+
+    initialize : function(options) {
+
+        app.view.fields.IntField.prototype.initialize.call(this, options);
+
+        if(!_.isUndefined(this.context.forecasts) && !_.isUndefined(this.context.forecasts.config)) {
+            this._canEdit = !_.contains(
+                // join the two variable together from the config
+                this.context.forecasts.config.get("sales_stage_won").concat(
+                    this.context.forecasts.config.get("sales_stage_lost")
+                ), this.model.get('sales_stage'));
+        }
+    },
+
     /**
      *
      * @param evt
      */
     onMouseEnter : function(evt) {
-        if(!this.canEdit()) return;
+        if(!this.isEditable()) return;
         this.$el.find('i').addClass('icon-pencil icon-small');
     },
 
@@ -26,7 +41,7 @@
      * @param evt
      */
     onMouseLeave : function(evt) {
-        if(!this.canEdit()) return;
+        if(!this.isEditable()) return;
         this.$el.find('i').removeClass('icon-pencil icon-small');
     },
 
@@ -36,7 +51,7 @@
      */
     onClick : function(evt) {
         evt.preventDefault();
-        if(!this.canEdit()) return;
+        if(!this.isEditable()) return;
 
         this.options.viewName = 'edit';
         this.render();
@@ -73,7 +88,7 @@
     onBlur : function(evt) {
         // submit if unfocused
         evt.preventDefault();
-        if(!this.canEdit()) return;
+        if(!this.isEditable()) return;
         var value = this.$el.find('input').val();
         if(this.isValid(value)) {
             this.model.set(this.name, value);
@@ -97,6 +112,17 @@
             this.errorCode = 'LBL_CLICKTOEDIT_INVALID';
             return false;
         }
+
+        // we have digits, lets make sure it's int a valid range is one is specified
+        if(!_.isUndefined(this.def.minValue) && !_.isUndefined(this.def.maxValue)) {
+            // we have a min and max value
+            if(value < this.def.minValue || value > this.def.maxValue) {
+                this.errorCode = 'LBL_CLICKTOEDIT_INVALID';
+                return false;
+            }
+        }
+
+        // the value passed all validation, return true
         return true;
     },
 
@@ -105,7 +131,7 @@
      *
      * @return {boolean}
      */
-    canEdit : function() {
-        return true;
+    isEditable : function() {
+        return this._canEdit;
     }
 })
