fc7475eb677d6d2d42b8523adbf236d06fa6e593
Julien Girault <julien.emac@gmail.com>
2012-12-12 17:12:52 +0100

    Fix issues in return_app_list_strings_language

--- sugarcrm/include/utils.php
+++ sugarcrm/include/utils.php
@@ -983,36 +983,37 @@ function return_app_list_strings_language($language)
 	}
 	$langs[] = $language;
 
-	$app_list_strings_array = array();
+    $app_list_strings_array = array();
 
-	foreach ( $langs as $lang ) {
-	    foreach(SugarAutoLoader::existing(
-	        "include/language/$lang.lang.php",
-	        "include/language/$lang.lang.override.php",
-	        "include/language/$lang.lang.php.override"
-	    ) as $file) {
-            include $file;
-            $GLOBALS['log']->info("Found language file: $file");
-	    }
+    //Merge language files together
+   	foreach ( $langs as $key => $lang ) {
 
-        $app_list_strings_array[] = $app_list_strings;
+        $app_list_strings_state = $app_list_strings;
+   	    foreach(SugarAutoLoader::existing(
+   	        "include/language/$lang.lang.php",
+   	        "include/language/$lang.lang.override.php",
+   	        "include/language/$lang.lang.php.override"
+   	    ) as $file) {
+               include $file;
+               $GLOBALS['log']->info("Found language file: $file");
+   	    }
+
+       $app_list_strings_array[$lang] = $app_list_strings;
+       //Return to previous state unless we are on first iteration and do an intersect merge
+       if ($key > 0) {
+           $app_list_strings = $app_list_strings_state;
+           //In case a custom file doesn't exist for the expected language, we want the custom lists from the default language
+           //(if there are key additions/deletions) but we want strings in our expected language (as much as possible).
+           $app_list_strings = sugarArrayIntersectMerge($app_list_strings, $app_list_strings_array[$lang]);
+       }
+       foreach(SugarAutoLoader::existing(
+           "custom/application/Ext/Language/$lang.lang.ext.php",
+           "custom/include/language/$lang.lang.php"
+       ) as $file) {
+           $app_list_strings = _mergeCustomAppListStrings($file , $app_list_strings);
+           $GLOBALS['log']->info("Found extended language file: $file");
+       }
     }
-
-    $app_list_strings = array();
-    foreach ( $app_list_strings_array as $app_list_strings_item ) {
-        $app_list_strings = sugarLangArrayMerge($app_list_strings, $app_list_strings_item);
-    }
-
-
-
-    foreach(SugarAutoLoader::existing(
-        "custom/application/Ext/Language/$lang.lang.ext.php",
-        "custom/include/language/$lang.lang.php"
-    ) as $file) {
-        $app_list_strings = _mergeCustomAppListStrings($file , $app_list_strings);
-        $GLOBALS['log']->info("Found extended language file: $file");
-    }
-
 
     if(!isset($app_list_strings)) {
 		$GLOBALS['log']->fatal("Unable to load the application language file for the selected language ($language) or the default language ($default_language) or the en_us language");
@@ -3946,6 +3947,31 @@ function setPhpIniSettings() {
 	if(!empty($backtrack_limit)) {
 		ini_set('pcre.backtrack_limit', '-1');
 	}
+}
+
+
+/**
+ * Replace values from $gimp with values from $dom, but only the keys that are in $gimp are replaced.
+ *
+ * @param array $gimp the array whose values will be overloaded
+ * @param array $dom the array whose values will pwn the gimp's
+ * @return array beaten $gimp
+ */
+function sugarArrayIntersectMerge($gimp, $dom)
+{
+    if (is_array($gimp) && is_array($dom)) {
+        foreach ($gimp as $domKey => $domVal) {
+            if (isset($dom[$domKey])) {
+                if (is_array($dom[$domKey])) {
+                    $gimp[$domKey] = array_merge($gimp[$domKey], array_intersect_key($dom[$domKey], $gimp[$domKey]));
+                } else {
+                    $gimp[$domKey] = $dom[$domKey];
+                }
+            }
+        }
+    }
+
+    return $gimp;
 }
 
 /**
