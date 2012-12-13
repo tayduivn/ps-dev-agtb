tayduivn/ps-dev-agtb

commit 7205cf29eb37c2a05482d9815fbb26955561f301

Bug #57162: Upgrader needs to handle 3-dots releases and double digit values
Added version_compare. Now we can use how many dots as we want. Should be tested in silent upgrade before merge.

Conflicts:
	build/scripts_for_patch/pre_install.php
	sugarcrm/modules/UpgradeWizard/end.php
	sugarcrm/modules/UpgradeWizard/silentUpgrade_step1.php
	sugarcrm/modules/UpgradeWizard/silentUpgrade_step2.php

sugarcrm/modules/UpgradeWizard/end.php
@@ -211,7 +211,8 @@ logThis("Complete: Update custom module built using module builder to add favori
 //END SUGARCRM flav=pro ONLY
 
 if(isset($_SESSION['current_db_version']) && isset($_SESSION['target_db_version'])){
-	if($_SESSION['current_db_version'] != $_SESSION['target_db_version']){
+    if (version_compare($_SESSION['current_db_version'], $_SESSION['target_db_version'], '!='))
+    {
 		//BEGIN SUGARCRM flav=pro ONLY
 		logThis("Adding Saved Report Chart Types", $path);
 		if(file_exists("$unzip_dir/scripts/upgrade_homepage.php")) {
@@ -219,16 +220,12 @@ if(isset($_SESSION['current_db_version']) && isset($_SESSION['target_db_version'
 		    add_report_chart_types();
 		}
 		//END SUGARCRM flav=pro ONLY
-		logThis("Upgrading multienum data", $path);
-        if(file_exists("$unzip_dir/scripts/upgrade_multienum_data.php")) {
-            require_once("$unzip_dir/scripts/upgrade_multienum_data.php");
-            upgrade_multienum_data();
-        }
 	 }
 
 
 	 //keeping separate. making easily visible and readable
-	 if($_SESSION['current_db_version'] == $_SESSION['target_db_version']){
+     if (version_compare($_SESSION['current_db_version'], $_SESSION['target_db_version'], '='))
+     {
 	    $_REQUEST['upgradeWizard'] = true;
 	    ob_start();
 			include('modules/ACL/install_actions.php');
@@ -292,11 +289,14 @@ logThis('Begin upgrade_connectors', $path);
 upgrade_connectors();
 logThis('End upgrade_connectors', $path);
 
-
-//Upgrade system displayed tabs and subpanels
-if(function_exists('upgradeDisplayedTabsAndSubpanels'))
+if (version_compare($_SESSION['current_db_version'], '6.5.0', '<'))
 {
-	upgradeDisplayedTabsAndSubpanels($_SESSION['current_db_version']);
+    // Bug 53650 - Workflow Type Templates not saving Type upon upgrade to 6.5.0, usable as Email Templates
+    $db->query("UPDATE email_templates SET type = 'workflow' WHERE
+        coalesce(" . $db->convert("base_module", "length") . ",0) > 0
+        AND
+        coalesce(" . $db->convert("type", "length") . ",0) = 0
+    ");
 }
 
 //Unlink files that have been removed
@@ -310,6 +310,12 @@ if(function_exists('rebuildSprites') && function_exists('imagecreatetruecolor'))
     rebuildSprites(true);
 }
 
+//Run repairUpgradeHistoryTable
+if (version_compare($_SESSION['current_db_version'], '6.5.0', '<') && function_exists('repairUpgradeHistoryTable'))
+{
+    repairUpgradeHistoryTable();
+}
+
 require_once('modules/Administration/upgrade_custom_relationships.php');
 upgrade_custom_relationships();
 
@@ -320,7 +326,7 @@ updateRenamedModulesLabels();
 
 //BEGIN SUGARCRM flav=PRO ONLY
 //setup forecast defualt settings
-if($_SESSION['current_db_version'] < '670')
+if(version_compare($_SESSION['current_db_version'], '6.7.0', '<'))
 {
     require_once('modules/Forecasts/ForecastsDefaults.php');
     ForecastsDefaults::setupForecastSettings(true,$_SESSION['current_db_version'],$_SESSION['target_db_version']);

sugarcrm/modules/UpgradeWizard/silentUpgrade_step3.php
@@ -258,8 +258,9 @@ $admin->saveSetting('system','adminwizard',1);
 
 include("$unzip_dir/manifest.php");
 $ce_to_pro_ent = isset($manifest['name']) && preg_match('/^SugarCE.*?(Pro|Ent|Corp|Ult)$/', $manifest['name']);
-$origVersion = getSilentUpgradeVar('origVersion');
-if(!$origVersion){
+$sugar_version = getSilentUpgradeVar('origVersion');
+if (!$sugar_version)
+{
     global $silent_upgrade_vars_loaded;
     logThis("Error retrieving silent upgrade var for origVersion: cache dir is {$GLOBALS['sugar_config']['cache_dir']} -- full cache for \$silent_upgrade_vars_loaded is ".var_export($silent_upgrade_vars_loaded, true), $path);
 }
@@ -318,16 +319,9 @@ logThis('Begin upgrade_connectors', $path);
 upgrade_connectors();
 logThis('End upgrade_connectors', $path);
 
-//Upgrade system displayed tabs and subpanels
-if(function_exists('upgradeDisplayedTabsAndSubpanels'))
-{
-	upgradeDisplayedTabsAndSubpanels($origVersion);
-}
-
 //Unlink files that have been removed
-if(function_exists('unlinkUpgradeFiles'))
-{
-	unlinkUpgradeFiles($origVersion);
+if(function_exists('unlinkUpgradeFiles')) {
+    unlinkUpgradeFiles($sugar_version);
 }
 
 if(function_exists('rebuildSprites') && function_exists('imagecreatetruecolor'))
@@ -340,11 +334,10 @@ updateRenamedModulesLabels();
 
 //BEGIN SUGARCRM flav=PRO ONLY
 //setup forecast defualt settings
-if($origVersion < '670')
-{
+if (version_compare($sugar_version, '6.7.0', '<')) {
     require_once(clean_path($unzip_dir.'/scripts/upgrade_utils.php'));
     require_once($unzip_dir.'/'.$zip_from_dir.'/modules/Forecasts/ForecastsDefaults.php');
-    ForecastsDefaults::setupForecastSettings(true,$origVersion,getUpgradeVersion());
+    ForecastsDefaults::setupForecastSettings(true, $sugar_version, getUpgradeVersion());
     ForecastsDefaults::upgradeColumns();
 }
 //END SUGARCRM flav=PRO ONLY
@@ -357,7 +350,7 @@ logThis('Checking for mobile/portal metadata upgrade...');
 // handled for upgrades FROM pre-6.6 to a version POST 6.6 and MUST be
 // handled AFTER inclusion of the upgrade package files
 if (!didThisStepRunBefore('commit','upgradePortalMobileMetadata')) {
-    if ($origVersion < '660') {
+    if (version_compare($sugar_version, '6.6.0', '<')) {
         if (file_exists('modules/UpgradeWizard/SidecarUpdate/SidecarMetaDataUpgrader.php')) {
             set_upgrade_progress('commit','in_progress','upgradePortalMobileMetadata','in_progress');
             logThis('Sidecar Upgrade: Preparing to upgrade metadata to 6.6.0 compatibility through the silent upgrader ...');

sugarcrm/modules/UpgradeWizard/uw_utils.php
@@ -32,6 +32,46 @@ if(!defined('sugarEntry') || !sugarEntry) die('Not A Valid Entry Point');
 // $Id: uw_utils.php 58174 2010-09-14 18:18:39Z kjing $
 
 /**
+ * Implodes some parts of version with specified delimiter, beta & rc parts are removed all time
+ *
+ * @example ('6.5.6') returns 656
+ * @example ('6.5.6beta2') returns 656
+ * @example ('6.5.6rc3') returns 656
+ * @example ('6.6.0.1') returns 6601
+ * @example ('6.5.6', 3, 'x') returns 65x
+ * @example ('6', 3, '', '.') returns 6.0.0
+ *
+ * @param string $version like 6, 6.2, 6.5.0beta1, 6.6.0rc1, 6.5.7 (separated by dot)
+ * @param int $size number of the first parts of version which are requested
+ * @param string $lastSymbol replace last part of version by some string
+ * @param string $delimiter delimiter for result
+ * @return string
+ */
+function implodeVersion($version, $size = 0, $lastSymbol = '', $delimiter = '')
+{
+    preg_match('/^\d+(\.\d+)*/', $version, $parsedVersion);
+    if (empty($parsedVersion)) {
+        return '';
+    }
+
+    $parsedVersion = $parsedVersion[0];
+    $parsedVersion = explode('.', $parsedVersion);
+
+    if ($size == 0) {
+        $size = count($parsedVersion);
+    }
+
+    $parsedVersion = array_pad($parsedVersion, $size, 0);
+    $parsedVersion = array_slice($parsedVersion, 0, $size);
+    if ($lastSymbol !== '') {
+        array_pop($parsedVersion);
+        array_push($parsedVersion, $lastSymbol);
+    }
+
+    return implode($delimiter, $parsedVersion);
+}
+
+/**
  * Helper function for upgrade - get path from upload:// name
  * @param string $path
  * return string
@@ -118,7 +158,7 @@ function commitCopyNewFiles($unzip_dir, $zip_from_dir, $path='') {
 	logThis('Starting file copy process...', $path);
 	global $sugar_version;
 	$backwardModules='';
-    if(substr($sugar_version,0,1) >= 5){
+
     	$modules = getAllModules();
 			$backwardModules = array();
 			foreach($modules as $mod){
@@ -131,7 +171,6 @@ function commitCopyNewFiles($unzip_dir, $zip_from_dir, $path='') {
 			    	}
 			   }
 			}
-       }
 
 	$newFiles = findAllFiles(clean_path($unzip_dir . '/' . $zip_from_dir), array());
 	$zipPath = clean_path($unzip_dir . '/' . $zip_from_dir);
@@ -1396,9 +1435,10 @@ function preLicenseCheck() {
 	global $mod_strings;
 	global $sugar_version;
 
-	if(!isset($sugar_version) || empty($sugar_version)) {
-		require_once('./sugar_version.php');
-	}
+    if (empty($sugar_version))
+    {
+        require('sugar_version.php');
+    }
 
 if(!isset($_SESSION['unzip_dir']) || empty($_SESSION['unzip_dir'])) {
 		logThis('unzipping files in upgrade archive...');
@@ -1540,9 +1580,10 @@ function preflightCheck() {
 	global $mod_strings;
 	global $sugar_version;
 
-	if(!isset($sugar_version) || empty($sugar_version)) {
-		require_once('./sugar_version.php');
-	}
+    if (empty($sugar_version))
+    {
+        require('sugar_version.php');
+    }
 
 	unset($_SESSION['rebuild_relationships']);
 	unset($_SESSION['rebuild_extensions']);
@@ -2138,8 +2179,6 @@ function resetUwSession() {
 		unset($_SESSION['alterCustomTableQueries']);
 	if(isset($_SESSION['skip_zip_upload']))
 		unset($_SESSION['skip_zip_upload']);
-	if(isset($_SESSION['sugar_version_file']))
-		unset($_SESSION['sugar_version_file']);
 	if(isset($_SESSION['install_file']))
 		unset($_SESSION['install_file']);
 	if(isset($_SESSION['unzip_dir']))
@@ -4425,41 +4464,6 @@ function upgradeSugarCache($file)
 	}
 }
 
-
-/**
- * upgradeDisplayedTabsAndSubpanels
- *
- * @param $version String value of current system version (pre upgrade)
- */
-function upgradeDisplayedTabsAndSubpanels($version)
-{
-	if($version < '620')
-	{
-		logThis('start upgrading system displayed tabs and subpanels');
-	    require_once('modules/MySettings/TabController.php');
-	    $tc = new TabController();
-
-	    //grab the existing system tabs
-	    $tabs = $tc->get_tabs_system();
-
-	    //add Calls, Meetings, Tasks, Notes, Prospects (Targets) and ProspectLists (Target Lists)
-	    //to displayed tabs unless explicitly set to hidden
-	    $modules_to_add = array('Calls', 'Meetings', 'Tasks', 'Notes', 'Prospects', 'ProspectLists');
-	    $added_tabs = array();
-
-	    foreach($modules_to_add as $module)
-	    {
-		       $tabs[0][$module] = $module;
-		       $added_tabs[] = $module;
-	    }
-
-	    logThis('calling set_system_tabs on TabController to add tabs: ' . var_export($added_tabs, true));
-	    $tc->set_system_tabs($tabs[0]);
-	    logThis('finish upgrading system displayed tabs and subpanels');
-	}
-}
-
-
 /**
  * unlinkUpgradeFiles
  * This is a helper function to clean up
@@ -4494,26 +4498,26 @@ function unlinkUpgradeFiles($version)
        {
        		if(preg_match('/UpgradeRemoval(\d+)x\.php/', $script, $matches))
        		{
-       	   	   $checkVersion = $matches[1] + 1; //Increment by one to check everything equal or below the target version
        	   	   $upgradeClass = 'UpgradeRemoval' . $matches[1] . 'x';
        	   	   require_once($_SESSION['unzip_dir'].'/scripts/files_to_remove/' . $upgradeClass . '.php');
+               if (class_exists($upgradeClass) == false)
+               {
+                   continue;
+               }
 
-       	   	   //Check to make sure we should load and run this UpgradeRemoval instance
-       	   	   if($checkVersion <= $version && class_exists($upgradeClass))
-       	   	   {
-       	   	   	  $upgradeInstance = new $upgradeClass();
-       	   	   	  if($upgradeInstance instanceof UpgradeRemoval)
-       	   	   	  {
-       	   	   	  	  logThis('Running UpgradeRemoval instance ' . $upgradeClass);
-       	   	   	  	  logThis('Files will be backed up to custom/backup');
-	       	   	   	  $files = $upgradeInstance->getFilesToRemove($version);
-	       	   	   	  foreach($files as $file)
-	       	   	   	  {
-	       	   	   	  	 logThis($file);
-	       	   	   	  }
-	       	   	   	  $upgradeInstance->processFilesToRemove($files);
-       	   	   	  }
-       	   	   }
+                //Check to make sure we should load and run this UpgradeRemoval instance
+                $upgradeInstance = new $upgradeClass();
+                if ($upgradeInstance instanceof UpgradeRemoval && version_compare($upgradeInstance->version, $version, '<='))
+                {
+                    logThis('Running UpgradeRemoval instance ' . $upgradeClass);
+                    logThis('Files will be backed up to custom/backup');
+                    $files = $upgradeInstance->getFilesToRemove($version);
+                    foreach($files as $file)
+                    {
+                       logThis($file);
+                    }
+                    $upgradeInstance->processFilesToRemove($files);
+                }
        	    }
        }
     }

build/scripts_for_patch/files_to_remove/UpgradeRemoval62x.php
@@ -30,8 +30,11 @@ require_once('modules/UpgradeWizard/UpgradeRemoval.php');
 
 class UpgradeRemoval62x extends UpgradeRemoval
 {
-	
-	
+    /**
+     * @var string minimal version for removal
+     */
+    public $version = '6.2.2';
+
 /**
  * getFilesToRemove
  * Return an array of files/directories to remove for 62x upgrades
@@ -48,7 +51,7 @@ $files = array();
 // 4) Upgraded TinyMCE from 2.x to 3.x version
 // We will additionally clean up the legacy include/utils/external_cache direcotry
 
-if($version < '622')
+if (version_compare($version, $this->version, '<'))
 {
 	$files[] = 'include/utils/external_cache';
 	$files[] = 'include/jsolait';
@@ -85,7 +88,7 @@ if($version < '622')
 	$files[] = 'modules/Activities/OpenListView.php';
 }
 
-if($version < '624')
+if (version_compare($version, '6.2.4', '<'))
 {
         $files[] = 'modules/Emails/EditView.html';
         $files[] = 'json.php';
@@ -96,5 +99,3 @@ return $files;
 	
 		
 }
-
-?>

build/scripts_for_patch/files_to_remove/UpgradeRemoval64x.php
@@ -29,8 +29,11 @@ require_once('modules/UpgradeWizard/UpgradeRemoval.php');
 
 class UpgradeRemoval64x extends UpgradeRemoval
 {
-	
-	
+    /**
+     * @var string minimal version for removal
+     */
+    public $version = '6.4.0';
+
 /**
  * getFilesToRemove
  * Return an array of files/directories to remove for 64x upgrades
@@ -43,7 +46,7 @@ $files = array();
 // In 6.4.0 we did the following
 // 1) Removed from include/javascript/yui3/assets dpSyntaxHighlighter.css and dpSyntaxHighlighter.js files
 
-if($version < '640')
+if (version_compare($version, $this->version, '<'))
 {
 	$files[] = 'include/javascript/yui3/assets/dpSyntaxHighligther.css';
 	$files[] = 'include/javascript/yui3/assets/dpSyntaxHighligther.js';
@@ -83,5 +86,3 @@ return $files;
 	
 		
 }
-
-?>

build/scripts_for_patch/files_to_remove/UpgradeRemoval65x.php
@@ -30,6 +30,11 @@ require_once('modules/UpgradeWizard/UpgradeRemoval.php');
 class UpgradeRemoval65x extends UpgradeRemoval
 {
     /**
+     * @var string minimal version for removal
+     */
+    public $version = '6.5.0';
+
+    /**
      * getFilesToRemove
      * Return an array of files/directories to remove for 65x upgrades
      * @param unknown_type $version
@@ -41,7 +46,7 @@ class UpgradeRemoval65x extends UpgradeRemoval
         // In 6.5.0 we did the following
         // 1) Removed template files for RTL theme
 
-        if($version < '650')
+        if (version_compare($version, $this->version, '<'))
         {
             $files[] = 'themes/RTL/tpls/_companyLogo.tpl';
             $files[] = 'themes/RTL/tpls/_globalLinks.tpl';
@@ -58,5 +63,3 @@ class UpgradeRemoval65x extends UpgradeRemoval
 
 
 }
-
-?>

build/scripts_for_patch/files_to_remove/UpgradeRemoval66x.php
@@ -30,6 +30,11 @@ require_once('modules/UpgradeWizard/UpgradeRemoval.php');
 class UpgradeRemoval66x extends UpgradeRemoval
 {
     /**
+     * @var string minimal version for removal
+     */
+    public $version = '6.6.0';
+
+    /**
      * Return an array of files/directories to remove for 66x upgrades
      * 
      * @param string $version
@@ -42,7 +47,7 @@ class UpgradeRemoval66x extends UpgradeRemoval
         // In 6.6.0 we did the following
         // 1) Introduced unified sidecar metadata and studio changes
 
-        if($version < '660')
+        if (version_compare($version, $this->version, '<'))
         {
             // Handle the metadata upgrade files for sidecar conversion
             require_once 'modules/UpgradeWizard/SidecarUpdate/SidecarMetaDataUpgrader.php';

build/scripts_for_patch/files_to_remove/UpgradeRemoval67x.php
@@ -24,6 +24,11 @@ require_once('modules/UpgradeWizard/UpgradeRemoval.php');
 class UpgradeRemoval67x extends UpgradeRemoval
 {
     /**
+     * @var string minimal version for removal
+     */
+    public $version = '6.7.0';
+
+    /**
      * Return an array of files/directories to remove for 66x upgrades
      * 
      * @param string $version
@@ -33,7 +38,7 @@ class UpgradeRemoval67x extends UpgradeRemoval
     {
         $files = array();
 
-        if(version_compare($version, '670', '<'))
+        if (version_compare($version, $this->version, '<'))
         {
             //Remove the themes/Sugar/js directory
             $files[] = 'themes/Sugar/js';

commit f4bd52945e7fd4bce226c764303b2da77d675910

Bug: 59400
Fixed error with Pacific theme (and other themes) not showing correctly on upgrade.  This was caused by 7205cf29eb37c2a05482d9815fbb26955561f301 which incorrectly set the logic for using version_compare.  Also added support to correctly log messages to the upgrade wizard log file.

sugarcrm/modules/UpgradeWizard/end.php
@@ -302,7 +302,7 @@ if (version_compare($_SESSION['current_db_version'], '6.5.0', '<'))
 //Unlink files that have been removed
 if(function_exists('unlinkUpgradeFiles'))
 {
-	unlinkUpgradeFiles($_SESSION['current_db_version']);
+	unlinkUpgradeFiles($_SESSION['current_db_version'], $path);
 }
 
 if(function_exists('rebuildSprites') && function_exists('imagecreatetruecolor'))

sugarcrm/modules/UpgradeWizard/silentUpgrade_step3.php
@@ -321,7 +321,7 @@ logThis('End upgrade_connectors', $path);
 
 //Unlink files that have been removed
 if(function_exists('unlinkUpgradeFiles')) {
-    unlinkUpgradeFiles($sugar_version);
+    unlinkUpgradeFiles($sugar_version, $path);
 }
 
 if(function_exists('rebuildSprites') && function_exists('imagecreatetruecolor'))

sugarcrm/modules/UpgradeWizard/uw_utils.php
@@ -4230,7 +4230,7 @@ function add_unified_search_to_custom_modules_vardefs()
  *
  * @param $version String value of current system version (pre upgrade)
  */
-function unlinkUpgradeFiles($version)
+function unlinkUpgradeFiles($version, $path)
 {
 	if(!isset($version))
 	{
@@ -4239,16 +4239,6 @@ function unlinkUpgradeFiles($version)
 
     //First check if we even have the scripts_for_patch/files_to_remove directory
     require_once('modules/UpgradeWizard/UpgradeRemoval.php');
-
-    /*
-    if(empty($_SESSION['unzip_dir']))
-    {
-        global $sugar_config;
-        $base_upgrade_dir		= $sugar_config['upload_dir'] . "/upgrades";
-        $base_tmp_upgrade_dir	= "$base_upgrade_dir/temp";
-        $_SESSION['unzip_dir'] = mk_temp_dir( $base_tmp_upgrade_dir );
-    }
-    */
 
     if(isset($_SESSION['unzip_dir']) && file_exists($_SESSION['unzip_dir'].'/scripts/files_to_remove'))
     {
@@ -4267,14 +4257,14 @@ function unlinkUpgradeFiles($version)
 
                 //Check to make sure we should load and run this UpgradeRemoval instance
                 $upgradeInstance = new $upgradeClass();
-                if ($upgradeInstance instanceof UpgradeRemoval && version_compare($upgradeInstance->version, $version, '<='))
+                if ($upgradeInstance instanceof UpgradeRemoval && version_compare($upgradeInstance->version, $version, '>='))
                 {
-                    logThis('Running UpgradeRemoval instance ' . $upgradeClass);
-                    logThis('Files will be backed up to custom/backup');
+                    logThis('Running UpgradeRemoval instance ' . $upgradeClass, $path);
+                    logThis('Files will be backed up to custom/backup', $path);
                     $files = $upgradeInstance->getFilesToRemove($version);
                     foreach($files as $file)
                     {
-                       logThis($file);
+                       logThis($file, $path);
                     }
                     $upgradeInstance->processFilesToRemove($files);
                 }
@@ -4307,7 +4297,7 @@ function unlinkUpgradeFiles($version)
 	       	   	   	  $files = $upgradeInstance->getFilesToRemove($version);
 	       	   	   	  foreach($files as $file)
 	       	   	   	  {
-	       	   	   	  	 logThis($file);
+	       	   	   	  	 logThis($file, $path);
 	       	   	   	  }
 	       	   	   	  $upgradeInstance->processFilesToRemove($files);
        	   	   }