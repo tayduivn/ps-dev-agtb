2e6ec7a15fb8a714f866eacd642764390b7bea9b
Jon Whitcraft <jwhitcraft@sugarcrm.com>
2012-12-18 08:34:19 -0500

    Bug N/A - Refactored SalesRep Worksheet isDirty Handling
    - Removed unused function that was left over when the class was created.

--- sugarcrm/modules/Forecasts/clients/base/views/forecastsWorksheet/forecastsWorksheet.js
+++ sugarcrm/modules/Forecasts/clients/base/views/forecastsWorksheet/forecastsWorksheet.js
@@ -819,33 +819,6 @@
     },
 
     /**
-     * Formats the additional details div when a user clicks a row in the grid
-     *
-     * @param {Object} dRow the row from the datagrid that user has clicked on
-     * @return {String} html output to be shown to the user
-     */
-    formatAdditionalDetails:function (dRow) {
-        // grab reference to the datatable
-        var dTable = this.gTable;
-        // get row data from datatable
-        var data = dTable.fnGetData(dRow);
-        // grab column headings array
-        var colHeadings = this.getColumnHeadings(dTable);
-
-        // TEMPORARY PLACEHOLDER OUTPUT - inline CSS, no class
-        // this will all be changed once we have a more firm requirement for what should display here
-        var output = '<table cellpadding="5" cellspacing="0" border="0" style="margin: 10px 0px 10px 50px">';
-        output += '<tr><td>' + colHeadings[0] + '</td><td>' + data[0] + '</td></tr>';
-        output += '<tr><td>' + colHeadings[1] + '</td><td>' + data[1] + '</td></tr>';
-        output += '<tr><td>' + colHeadings[2] + '</td><td>' + data[2] + '</td></tr>';
-        output += '<tr><td>' + colHeadings[3] + '</td><td>' + data[3] + '</td></tr>';
-        output += '<tr><td>' + colHeadings[4] + '</td><td>' + data[4] + '</td></tr>';
-        output += '</table>';
-
-        return output;
-    },
-
-    /**
      * Returns an array of column headings
      *
      * @param {Object} dTable datatable param so we can grab all the column headings from it
