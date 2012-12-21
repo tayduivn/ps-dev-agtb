cef766b503758aa126b60c3cd80051f06bf7d120
Joel Spriggs <jspriggs@sugarcrm.com>
2012-12-21 09:47:26 -0500

    Bug-59637: adding line to unbind data for forecasts worksheets to stop listening for settings changes when it's disposed, otherwise changes to the admin wizard cause an error in the console and require an extra click to save the data and return to the primary forecasts screen.

--- sugarcrm/modules/Forecasts/clients/base/views/forecastsWorksheet/forecastsWorksheet.js
+++ sugarcrm/modules/Forecasts/clients/base/views/forecastsWorksheet/forecastsWorksheet.js
@@ -217,6 +217,7 @@
     unbindData : function() {
         if(this._collection) this._collection.off(null, null, this);
         if(this.context.forecasts) this.context.forecasts.off(null, null, this);
+        if(this.context.forecasts.config) this.context.forecasts.config.off(null, null, this);
         if(this.context.forecasts.worksheet) this.context.forecasts.worksheet.off(null, null, this);
         app.view.View.prototype.unbindData.call(this);
     },
