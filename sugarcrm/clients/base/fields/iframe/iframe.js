f6303460a6411cdcb395be353fb1202401645d08
Matthew Marum <mmarum@sugarcrm.com>
2013-01-03 17:14:24 -0500

    SIDECAR-249: iFrame field widget

--- sugarcrm/clients/base/fields/iframe/iframe.js
+++ sugarcrm/clients/base/fields/iframe/iframe.js
@@ -28,5 +28,11 @@
     unformat:function(value){
         value = (value!='' && value!='http://') ? value : "";
         return value;
+    },
+    format:function(value){
+        if (value && !value.match(/^(http|https):\/\//)) {
+            value = "http://" + value;
+        }
+        return value;
     }
 })
--- sugarcrm/tests/clients/base/fields/iframe/iframe.js
+++ sugarcrm/tests/clients/base/fields/iframe/iframe.js
@@ -1,8 +1,24 @@
 describe("iframe", function() {
 
+    beforeEach(function() {
+        app = SugarTest.app;
+        field = SugarTest.createField("base","iframe", "iframe", "detail");
+    });
+
+    afterEach(function() {
+        app.cache.cutAll();
+        app.view.reset();
+        delete Handlebars.templates;
+        field = null;
+    });
+
     describe("iframe", function() {
-        it("should format the value", function() {
-            var field = SugarTest.createField("base","iframe", "iframe", "detail");
+        it("should add 'http' scheme to URL if http or https scheme is missing", function() {
+            expect(field.format("http://www.google.com")).toEqual("http://www.google.com");
+            expect(field.format("https://www.google.com")).toEqual("https://www.google.com");
+            expect(field.format("www.google.com")).toEqual("http://www.google.com");
+        });
+        it("should unformat 'http://' to an empty string", function() {
             expect(field.unformat("http://")).toEqual("");
             expect(field.unformat("http://www.google.com")).toEqual("http://www.google.com");
         });
