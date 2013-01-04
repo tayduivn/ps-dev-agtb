4ec406f0f6e2ae9df776431d006bfedf62a8cd80
John Barlow <jbarlow@sugarcrm.com>
2013-01-04 12:29:44 -0500

    Bug59918: Spelling and comment updates.

--- sugarcrm/modules/Forecasts/clients/base/views/forecastsWorksheet/forecastsWorksheet.js
+++ sugarcrm/modules/Forecasts/clients/base/views/forecastsWorksheet/forecastsWorksheet.js
@@ -796,13 +796,13 @@
     /**
      * Event Handler for updating the worksheet by a selected category
      *
-     * @param params is always a context
+     * @param params array of selected filters
      */
     updateWorksheetBySelectedRanges:function (params) {
         // Set the filters for the datatable then re-render
         var self = this,
             forecast_ranges_setting = this.context.forecasts.config.get('forecast_ranges') || 'show_binary';
-
+        
         // start with no filters, i. e. show everything.
         if(!_.isUndefined($.fn.dataTableExt)) {
             $.fn.dataTableExt.afnFiltering.splice(0, $.fn.dataTableExt.afnFiltering.length);
@@ -824,7 +824,7 @@
                             checkState = rowCategory.find('input').attr('checked');
                             selectVal = ((checkState == "checked") || (checkState == "on") || (checkState == "1")) ? 'include' : 'exclude';
                         } else {
-                            //we need to check to see if the select exists, becuase this gets fired before the commitStage field rerenders itself back
+                            //we need to check to see if the select exists, because this gets fired before the commitStage field re-renders itself back
                             //to a text field.
                             if(rowCategory.find("select").length == 0){
                                 selectVal = rowCategory.text().trim().toLowerCase();
