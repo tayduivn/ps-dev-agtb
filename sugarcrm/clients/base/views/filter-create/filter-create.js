f36e26a5182e60b3105f3147006d87e40c417b45
Akshay Joshi <me@akshayjoshi.com>
2013-01-09 08:38:01 -0800

    Add the row partial to the controller.

--- sugarcrm/clients/base/views/filter-create/filter-create.js
+++ sugarcrm/clients/base/views/filter-create/filter-create.js
@@ -4,6 +4,41 @@
         'click a.addme': 'addRow'
     },
 
+    rowTemplate: Handlebars.compile('<article class="filter-body" id="filter_row_new">' +
+'     <div class="row-fluid">' +
+'       <div class="filter-field controls span3">' +
+'         <select name="field" class="field_name chzn-select chzn-inherit-width" data-placeholder="Select field name..."> ' +
+'             <option value=""></option> ' +
+'             {{#each filterFields}}' +
+'                 <option data-type="{{type}}" value="{{name}}">{{string}}</option>' +
+'             {{/each}}' +
+'         </select>' +
+'       </div>' +
+'       <div class="filter-operator hide controls span3"> ' +
+'         <select name="operator" class="operator chzn-select chzn-inherit-width" data-placeholder="Select operator...">' +
+'         </select>' +
+'       </div>' +
+'       <div class="filter-value hide controls span4">' +
+'       </div>' +
+'       <div class="filter-actions span2">' +
+'         <a class="removeme btn btn-invisible hide btn-dark"><i class="icon-minus"></i></a>' +
+'         <a class="updateme btn btn-invisible hide btn-dark"><i class="icon-refresh"></i></a>' +
+'         <a class="addme btn btn-invisible disabled btn-dark"><i class="icon-plus"></i></a>' +
+'       </div>' +
+'     </div>' +
+'   </article>'),
+
+    filterOperatorMap: {
+        'enum': ['is', 'is not'],
+        'varchar': ['matches', 'does not match', 'contains', 'does not contains', 'starts with', 'does not start with', 'ends with', 'does not end with'],
+        'name': ['matches', 'does not match', 'contains', 'does not contains', 'starts with', 'does not start with', 'ends with', 'does not end with'],
+        'currency': ['is equal to', 'is greater than', 'is greater than or equal to', 'is less than', 'is less than or equal to'],
+        'int': ['is equal to', 'is greater than', 'is greater than or equal to', 'is less than', 'is less than or equal to'],
+        'double': ['is equal to', 'is greater than', 'is greater than or equal to', 'is less than', 'is less than or equal to'],
+        'datetime': ['datetime operator'],
+        'base': ['fall through to this case']
+    },
+
     initialize: function(opts) {
         var self = this;
         this.title = app.controller.context.get('module');
