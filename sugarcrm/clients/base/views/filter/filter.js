47a1410d91e1243e1900c729c5b57010909e9ab7
John Barlow <jbarlow@sugarcrm.com>
2013-01-08 18:21:22 -0800

    EJS-16: in process check in

--- sugarcrm/clients/base/views/filter/filter.js
+++ sugarcrm/clients/base/views/filter/filter.js
@@ -1,17 +1,20 @@
 ({
     events: {
-        'click .filter-new': 'toggleOpen'
+        'click .filter-new': 'toggleOpen',
+        'click .chzn-results li': 'toggleSelected'
     },
 
     initialize: function(opts) {
         console.log("creating a filter view");
         app.view.View.prototype.initialize.call(this, opts);
+        //this.searchFilterId = _.uniqueId("search_filter");
+        this.searchFilterId = "search_filter";
     },
 
     render: function() {
+        var self = this;
         app.view.View.prototype.render.call(this);
-
-
+        
         /* HTML setup
          ---------------------- */
 
@@ -38,18 +41,51 @@
 
 
         // Enable chosen on the menu
-        this.$(".search_filter").chosen({ searchChoiceConstructor: this.pill});
+        this.$(".search_filter").chosen().change(function(){
+            self.changeToPill();
+        });
+        this.$el.find(".chzn-choices").prepend("<li class='search-choice search-choice-option filter-disabled'><span>Module</span><a>" + this.module + "</a></li>");
 
         this.$(".chzn-choices").prepend('<legend class="chzn-select-legend">Filter <i class="icon-caret-down"></i></legend>');
 
 
         // append checkmarks to filter dropdown results
         this.$('.chzn-results').find('li').after("<span class='icon-ok' />");
+
     },
 
-    pill: function(id, html, index) {
-        console.log(arguments);
-        return '<li class="search-choice search-choice-option" id="search_filter_' + index + '_choice"><span>Filter</span><a>' + html + '</a></li>';
+    changeToPill: function() {
+        var selectedItems = this.$el.find(".chzn-choices li.search-choice"),
+            latestItem = $(selectedItems[selectedItems.length - 1]),
+            latestSpan = latestItem.find("span"),
+            latestItemValue = latestSpan.html(),
+            latestItemLink = latestItem.find("a"),
+            filterName = latestItemValue.replace(/ /g, ""),
+            menuItem = this.$("#" + this.searchFilterId + "_chzn_o_" + latestItem.attr("id").slice(latestItem.attr("id").indexOf("c_") + 2));
+        
+        latestItem.addClass("search-choice search-choice-option " + this.getFilterID(latestItemValue));
+        latestItemLink.removeClass();
+        latestItemLink.html(latestItemValue);
+        latestSpan.html("Placeholder");
+        //$.data(menuItem[0], "selected", true);
+        
+    },
+    
+    getFilterID: function(name){
+        return "filter_" + name.replace(/ /g, "");
+    },
+    
+    toggleSelected: function(e){
+        if($.data(e.currentTarget, "selected")){
+            $.data(e.currentTarget, "selected", false);
+            var target = $(e.currentTarget);
+            target.removeClass("result-selected");
+            target.addClass("active-result");
+            this.$("." + this.getFilterID(target.html())).remove();
+        }
+        else{
+            $.data(e.currentTarget, "selected", true);
+        }
     },
 
     toggleOpen: function() {
