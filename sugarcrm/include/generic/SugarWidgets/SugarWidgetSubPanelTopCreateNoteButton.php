23f78e323f6229551e4a90b76029e382400e0184
Jim Bartek <jbartek@sugarcrm.com>
2013-01-22 17:00:57 -0500

    Modifying files that were using edit as the action to use create when the action is really a create action.

--- sugarcrm/include/generic/SugarWidgets/SugarWidgetSubPanelTopCreateNoteButton.php
+++ sugarcrm/include/generic/SugarWidgets/SugarWidgetSubPanelTopCreateNoteButton.php
@@ -145,7 +145,7 @@ class SugarWidgetSubPanelTopCreateNoteButton extends SugarWidgetSubPanelTopButto
 	function display($defines, $additionalFormFields = null)
 	{
 	    $focus = new Note;
-		if ( !$focus->ACLAccess('EditView') ) {
+		if ( !$focus->ACLAccess('create') ) {
 		    return '';
 	    }
 
