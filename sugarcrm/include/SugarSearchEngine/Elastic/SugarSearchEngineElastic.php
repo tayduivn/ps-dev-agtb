b29496de264f4fbddef26aa080fc6e0ff950c193
Jim Bartek <jbartek@sugarcrm.com>
2013-01-21 19:19:01 -0500

    removing whitespace, i like whitespace

--- sugarcrm/include/SugarSearchEngine/Elastic/SugarSearchEngineElastic.php
+++ sugarcrm/include/SugarSearchEngine/Elastic/SugarSearchEngineElastic.php
@@ -158,7 +158,6 @@ class SugarSearchEngineElastic extends SugarSearchEngineAbstractBase
         if($searchFields == null)
             $searchFields = SugarSearchEngineMetadataHelper::retrieveFtsEnabledFieldsPerModule($bean);
 
-
         $keyValues = array();
         foreach($searchFields as $fieldName => $fieldDef)
         {
