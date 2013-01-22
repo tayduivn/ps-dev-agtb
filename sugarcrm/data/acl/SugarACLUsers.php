9dcd99fb6b4d00b29bffe47937bc16357f578ed5
Rob Aagaard <rob@sugarcrm.com>
2013-01-22 16:45:53 -0700

    Bug 60238: Allow the export permission in the hardcoded Users ACL, otherwise the SOAP API won't work.
    Conflicts:
    	sugarcrm/data/acl/SugarACLUsers.php

--- sugarcrm/data/acl/SugarACLUsers.php
+++ sugarcrm/data/acl/SugarACLUsers.php
@@ -85,8 +85,8 @@ class SugarACLUsers extends SugarACLStrategy
             return false;
         }
 
-        if($view == 'view' || $view == 'list' || $view == 'field' || $view == 'team_security' ) {
-            if($view == 'field' && ($context['field'] == 'password' || $context['field'] == 'user_hash') ) {
+        if($view == 'view' || $view == 'ListView' || $view == 'list' || $view == 'export' || $view == 'Export' || $view == 'field' || $view == 'DetailView' || $view == 'detail' || $view == 'team_security' ) {
+            if($view == 'field' && $context['field'] == 'password') {
                 return false;
             }
             if( $view == 'field'
