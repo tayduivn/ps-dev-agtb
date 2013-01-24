965b172a1f0735828712203eb23b93ce06964a8d
Travis Hubbard <thubbard@sugarcrm.com>
2013-01-24 17:03:13 -0500

    SFA-476 cleaning up JSGroupings

--- sugarcrm/jssource/JSGroupings.php
+++ sugarcrm/jssource/JSGroupings.php
@@ -57,6 +57,7 @@
                             'styleguide/assets/js/bootstrap-alert.js'   => $target,
                             'styleguide/assets/js/bootstrap-datepicker.js' => $target,
                             'styleguide/assets/js/bootstrapx-clickover.js' => $target,
+                            'include/javascript/jquery/jquery.nouislider.js' => $target,
                         );
                         break;
                     case 'bootstrap_core':
@@ -403,10 +404,7 @@
     $sidecar_forecasts['include/SugarCharts/Jit/js/sugarCharts.js'] = $cached_file;
     $sidecar_forecasts['modules/Forecasts/clients/base/helper/hbt-helpers.js'] = $cached_file;
     $sidecar_forecasts['modules/Forecasts/clients/base/lib/ForecastsUtils.js'] = $cached_file;
-    $sidecar_forecasts['styleguide/assets/js/nvd3/lib/d3.v2.min.js'] = $cached_file;
-    $sidecar_forecasts['styleguide/assets/js/nvd3/nv.d3.min.js'] = $cached_file;
     $sidecar_forecasts['modules/Forecasts/clients/base/lib/error.js'] = $cached_file;
-    $sidecar_forecasts['include/javascript/jquery/jquery.nouislider.js'] = $cached_file;
 
     $js_groupings[] = $sidecar_forecasts;
     //END SUGARCRM flav=pro ONLY
