140a1601001e967f742018b027ac99e92f0b9fdf
Rob Aagaard <rob@sugarcrm.com>
2013-01-24 18:37:33 -0700

    Bug 59649: Make the OAuthKeys UI not let you do something dumb as easily anymore.

---
+++ sugarcrm/data/acl/SugarACLOAuthKeys.php
@@ -0,0 +1,70 @@
+<?php
+if(!defined('sugarEntry') || !sugarEntry) die('Not A Valid Entry Point');
+/*********************************************************************************
+ *The contents of this file are subject to the SugarCRM Professional End User License Agreement
+ *("License") which can be viewed at http://www.sugarcrm.com/EULA.
+ *By installing or using this file, You have unconditionally agreed to the terms and conditions of the License, and You may
+ *not use this file except in compliance with the License. Under the terms of the license, You
+ *shall not, among other things: 1) sublicense, resell, rent, lease, redistribute, assign or
+ *otherwise transfer Your rights to the Software, and 2) use the Software for timesharing or
+ *service bureau purposes such as hosting the Software for commercial gain and/or for the benefit
+ *of a third party.  Use of the Software may be subject to applicable fees and any use of the
+ *Software without first paying applicable fees is strictly prohibited.  You do not have the
+ *right to remove SugarCRM copyrights from the source code or user interface.
+ * All copies of the Covered Code must include on each user interface screen:
+ * (i) the "Powered by SugarCRM" logo and
+ * (ii) the SugarCRM copyright notice
+ * in the same form as they appear in the distribution.  See full license for requirements.
+ *Your Warranty, Limitations of liability and Indemnity are expressly stated in the License.  Please refer
+ *to the License for the specific language governing these rights and limitations under the License.
+ *Portions created by SugarCRM are Copyright (C) 2004 SugarCRM, Inc.; All Rights Reserved.
+ ********************************************************************************/
+require_once('data/SugarACLStrategy.php');
+
+class SugarACLOAuthKeys extends SugarACLStrategy
+{
+    public $create_only_fields = array(
+            'c_key' => true,
+            'oauth_type' => true,
+        );
+
+    /**
+     * Check access a current user has on Users and Employees
+     * @param string $module
+     * @param string $view
+     * @param array $context
+     * @return bool|void
+     */
+    public function checkAccess($module, $view, $context) {
+        if ( $view == 'team_security' ) {
+            // Let the other modules decide
+            return true;
+        }
+        // Let's make it a little easier on ourselves and fix up the actions nice and quickly
+        $view = SugarACLStrategy::fixUpActionName($view);
+        if ( $view == 'field' ) {
+            $context['action'] = SugarACLStrategy::fixUpActionName($context['action']);
+        }
+
+        // We can create without further restrictions
+        if( $view == 'create' || ( $view == 'field' && $context['action'] == 'create') ) {
+            return true;
+        }
+
+        // Some c_keys are special, they can't edit them, but if they really want to delete them we will allow it
+        if ( isset($context['bean']) && is_a($context['bean'],'SugarBean') ) {
+            if( $view == 'edit' || ( isset($context['action']) && $context['action'] == 'edit' ) ) {
+                if ( $context['bean']->c_key == 'sugar' || $context['bean']->c_key == 'support_portal' ) {
+                    return false;
+                }
+            }
+        }
+
+        // Other fields can only be edited when you create a record.
+        if ( $view == 'field' && $context['action'] == 'edit' && isset($this->create_only_fields[$context['field']]) ) {
+            return false;
+        }
+
+        return true;
+    }
+}
