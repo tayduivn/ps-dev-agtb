fb18a6aea73bccfedea8b5035ad0c0e935b85c35
Nima Dehnashi <ndehnashi@sugarcrm.com>
2013-01-25 15:41:32 -0800

    [ABE-233] Moved tag-parsing functionality to the activitystream layout. Also moved addPost to omnibar view.

--- sugarcrm/clients/base/views/activitystream-omnibar/activitystream-omnibar.js
+++ sugarcrm/clients/base/views/activitystream-omnibar/activitystream-omnibar.js
@@ -1,5 +1,7 @@
 ({
-	events: {},
+	events: {
+        'click .addPost': 'addPost'
+    },
 
 	initialize: function(options) {
 		app.view.View.prototype.initialize.call(this, options);
@@ -12,5 +14,29 @@
             id: this.user_id,
             field: 'picture'
         }) : app.config.siteUrl + "/styleguide/assets/img/profile.png";
-	}
+	},
+
+    /**
+     * Creates a new post.
+     */
+    addPost: function() {
+        var self = this,
+            myPost = this.$(".activitystream-post"),
+            myPostId = this.context.get("modelId"),
+            myPostModule = this.module,
+            myPostUrl = 'ActivityStream',
+            myPostContents,
+            attachments = myPost.find('.activitystream-pending-attachment');
+
+        if (myPostModule !== "Home") {
+            myPostUrl += '/' + myPostModule;
+            if (!_.isUndefined(myPostId)) {
+                myPostUrl += '/' + myPostId;
+            }
+        }
+
+        myPostUrl = app.api.buildURL(myPostUrl);
+        myPostContents = this.layout._processTags(myPost.find('div.sayit'));
+        this.layout._addPostComment(myPostUrl, myPostContents, attachments);
+    }
 })
--- sugarcrm/clients/base/views/activitystream/activitystream.js
+++ sugarcrm/clients/base/views/activitystream/activitystream.js
@@ -5,11 +5,6 @@
         'click .more': 'showAllComments',
         'click .deleteRecord': 'deleteRecord',
         'click [name=show_more_button]': 'showMoreRecords',
-        'keyup .sayit': 'getEntities',
-        'blur .sayit': 'hideTypeahead',
-        'mouseover ul.typeahead.activitystream-tag-dropdown li': 'switchActiveTypeahead',
-        'click ul.typeahead.activitystream-tag-dropdown li': 'addTag',
-        'click .showAnchor': 'showAnchor',
         'click .preview-stream': 'previewRecord'
     },
 
@@ -25,9 +20,6 @@
         this.layout.on("stream:more:fire", function(collection) {
             app.events.trigger("preview:collection:change", collection);
         }, this);
-
-        this.layout.off("stream:addPost:fire", null, this);
-        this.layout.on("stream:addPost:fire", this.addPost, this);
 
         // Check to see if we need to make a related activity stream.
         if (this.module !== "Home") {
@@ -107,81 +99,6 @@
     },
 
     /**
-     * Helper method for adding a post or a comment. Handles attachments too.
-     * @param {string} url         Endpoint for posting message
-     * @param {string} contents    Some type of message (may have HTML due to tags)
-     * @param {array}  attachments Attachments to save to the post.
-     */
-    _addPostComment: function(url, contents, attachments) {
-        var self = this,
-            callback = _.after(1 + attachments.length, function() {
-                //self.streamCollection.fetch(self.opts);
-            });
-
-        app.api.call('create', url, {'value': contents}, {success: function(post_id) {
-            // TODO: Fix this to be less hacky. Perhaps a flag in arguments?
-            var parent_type = (url.indexOf("ActivityStream/ActivityStream") === -1)? 'ActivityStream' : 'ActivityComments';
-
-            attachments.each(function(index, el) {
-                var id = $(el).attr('id'),
-                    seed = app.data.createBean('Notes', {
-                        'parent_id': post_id,
-                        'parent_type': parent_type,
-                        'team_id': 1
-                    });
-
-                seed.save({}, {
-                    success: function(model) {
-                        var data = new FormData(),
-                            url = app.api.buildURL("Notes/" + model.get("id") + "/file/filename");
-
-                        data.append("filename", app.drag_drop[id]);
-                        url += "?oauth_token=" + app.api.getOAuthToken();
-
-                        $.ajax({
-                            url: url,
-                            type: "POST",
-                            data: data,
-                            processData: false,
-                            contentType: false,
-                            success: function() {
-                                delete app.drag_drop[id];
-                                callback();
-                            }
-                        });
-                    }
-                });
-            });
-            callback();
-        }});
-    },
-
-    /**
-     * Helper method to convert HTML from tags to a text-based format.
-     * @param  {string} postHTML
-     * @return {string}
-     */
-    _processTags: function(postHTML) {
-        var contents = '';
-        $(postHTML).contents().each(function() {
-            if (this.nodeName == "#text") {
-                contents += this.data;
-            } else if (this.nodeName == "SPAN") {
-                var el = $(this);
-                var data = el.data();
-
-                // Check if the span is a tag, else append el text to the post's content
-                if( data.module && data.id ) {
-                    contents += '@[' + data.module + ':' + data.id + ']';
-                } else {
-                    contents += el.text();
-                }
-            }
-        }).html();
-        return contents.replace(/&nbsp;/gi, ' ');
-    },
-
-    /**
      * Creates a new comment on a post.
      * @param {Event} event
      */
@@ -193,32 +110,8 @@
             myPostContents,
             attachments = this.$(event.currentTarget).siblings('.activitystream-pending-attachment');
 
-        myPostContents = this._processTags(myPost.find('div.sayit'));
-        this._addPostComment(myPostUrl, myPostContents, attachments);
-    },
-
-    /**
-     * Creates a new post.
-     */
-    addPost: function() {
-        var self = this,
-            myPost = this.$(".activitystream-post"),
-            myPostId = this.context.get("modelId"),
-            myPostModule = this.module,
-            myPostUrl = 'ActivityStream',
-            myPostContents,
-            attachments = myPost.find('.activitystream-pending-attachment');
-
-        if (myPostModule !== "Home") {
-            myPostUrl += '/' + myPostModule;
-            if (!_.isUndefined(myPostId)) {
-                myPostUrl += '/' + myPostId;
-            }
-        }
-
-        myPostUrl = app.api.buildURL(myPostUrl);
-        myPostContents = this._processTags(myPost.find('div.sayit'));
-        this._addPostComment(myPostUrl, myPostContents, attachments);
+        myPostContents = this.layout._processTags(myPost.find('div.sayit'));
+        this.layout._addPostComment(myPostUrl, myPostContents, attachments);
     },
 
     deleteRecord: function(event) {
@@ -231,182 +124,6 @@
         app.api.call('delete', app.api.buildURL(myPostUrl), {}, {success: function() {
             // self.streamCollection.fetch(self.opts);
         }});
-    },
-
-    _getEntities: _.debounce(function(event) {
-        var list,
-            el = this.$(event.currentTarget),
-            word = event.currentTarget.innerText;
-
-        el.parent().find("ul.typeahead.activitystream-tag-dropdown").remove();
-
-        if (word.indexOf("@") === -1) {
-            // If there's no @, don't do anything.
-            return;
-        } else if (word.indexOf("@") === 0) {
-            word = _.last(word.split('@'));
-        } else {
-            // Prevent email addresses from being caught, even though emails
-            // can have spaces in them according to the RFCs (3696/5322/6351).
-            word = _.last(word.split(' @'));
-        }
-
-        // Do initial list filtering.
-        list = _.filter(app.entityList, function(entity) {
-            return entity.name.toLowerCase().indexOf(word.toLowerCase()) !== -1;
-        });
-
-        // Rank the list and trim it to no more than 8 entries.
-        list = (function(list, query) {
-            var begin = [], caseSensitive = [], caseInsensitive = [], item = list.shift(), i;
-            for (i = 0; i < 8 && item; i++) {
-                if (item.name.toLowerCase().indexOf(query.toLowerCase()) === 0) {
-                    begin.push(item);
-                } else if (item.name.indexOf(query) !== -1) {
-                    caseSensitive.push(item);
-                } else {
-                    caseInsensitive.push(item);
-                }
-                item = list.shift();
-            }
-            return begin.concat(caseSensitive, caseInsensitive);
-        })(list, word);
-
-        var ul = $("<ul/>").addClass('typeahead dropdown-menu activitystream-tag-dropdown');
-        var blank_item = '<li><a href="#"></a></li>';
-        if (list.length) {
-            items = _.map(list, function(item) {
-                var i = $(blank_item).data(item);
-                var query = word.replace(/[\-\[\]{}()*+?.,\\\^$|#\s]/g, '\\$&');
-                i.find('a').html(function() {
-                    return item.name.replace(new RegExp('(' + query + ')', 'ig'), function($1, match) {
-                        return '<strong>' + match + '</strong>';
-                    });
-                });
-
-                return i[0];
-            });
-
-            items[0] = ($(items[0]).addClass('active'))[0];
-
-            ul.css({
-                top: el.position().top + el.height(),
-                left: el.position().left
-            });
-
-            ul.html(items).appendTo(el.parent()).show();
-        }
-    }, 250),
-
-    getEntities: function(event) {
-        var dropdown = this.$("ul.typeahead.activitystream-tag-dropdown"),
-            currentTarget = this.$(event.currentTarget);
-        // Coerce integer to a boolean.
-        var dropdownOpen = !!(dropdown.length);
-
-        if (dropdownOpen) {
-            var active = dropdown.find('.active');
-            // Enter or tab. Tab doesn't work in some browsers.
-            if (event.keyCode == 13 || event.keyCode == 9) {
-                event.preventDefault();
-                event.stopPropagation();
-                dropdown.find('.active').click();
-            }
-            // Up arrow.
-            if (event.keyCode == 38) {
-                var prev = active.prev();
-                if (!prev.length) {
-                    prev = dropdown.find('li').last();
-                }
-                active.removeClass('active');
-                prev.addClass('active');
-            }
-            // Down arrow.
-            if (event.keyCode == 40) {
-                var next = active.next();
-                if (!next.length) {
-                    next = dropdown.find('li').first();
-                }
-                active.removeClass('active');
-                next.addClass('active');
-            }
-        }
-
-        currentTarget.find('.label').each(function() {
-            var el = $(this);
-            if (el.data('name') !== el.text()) {
-                el.remove();
-            }
-        });
-
-        // If we're typing text.
-        if (event.keyCode > 47) {
-            this._getEntities(event);
-        }
-    },
-
-    hideTypeahead: function() {
-        var self = this;
-        setTimeout(function() {
-            self.$("ul.typeahead.activitystream-tag-dropdown").remove();
-        }, 150);
-    },
-
-    switchActiveTypeahead: function(event) {
-        this.$("ul.typeahead.activitystream-tag-dropdown .active").removeClass('active');
-        this.$(event.currentTarget).addClass('active');
-    },
-
-    addTag: function(event) {
-        var el = this.$(event.currentTarget);
-        var body = this.$(el.parents()[1]).find(".sayit");
-        var originalChildren = body.clone(true).children();
-        var lastIndex = body.html().lastIndexOf("@");
-        var data = el.data();
-
-        var tag = $("<span />").addClass("label").addClass("label-" + data.module).html(data.name);
-        tag.data("id", data.id).data("module", data.module).data("name", data.name);
-        var substring = body.html().substring(0, lastIndex);
-        $(body).html(substring).append(tag).append("&nbsp;");
-
-        if($(body).children().length == 1) {
-            // Fixes issue where a random font tag appears. ABE-128.
-            $(body).prepend("&nbsp;");
-        }
-
-        // Since the data is stored as an object, it's not preserved when we add the tag.
-        // For this reason, we need to add it again.
-        body.children().each(function(i) {
-            if (originalChildren[i]) {
-                var tagChild = this;
-                _($.data(originalChildren[i])).each(function(value, key) {
-                    $.data(tagChild, key, value);
-                });
-            }
-        });
-        if (document.createRange) {
-            var range = document.createRange();
-            range.selectNodeContents(body[0]);
-            range.collapse(false);
-            var selection = window.getSelection();
-            selection.removeAllRanges();
-            selection.addRange(range);
-        }
-        this.hideTypeahead();
-
-        event.stopPropagation();
-        event.preventDefault();
-    },
-
-    _parseTags: function(text) {
-        var pattern = new RegExp(/@\[([\d\w\s-]*):([\d\w\s-]*)\]/g);
-
-        return (!text || text.length === 0) ? text : text.replace(pattern, function(str, module, id) {
-            var name = _(app.entityList).find(function(el) {
-                return el.id == id;
-            }).name || "A record";
-            return "<span class='label label-" + module + "'><a href='#" + module + '/' + id + "'>" + name + "</a></span>";
-        });
     },
 
     /**
@@ -482,7 +199,7 @@
                 comments = model.get("comments");
 
             if (activity_data && activity_data.value) {
-                activity_data.value = self._parseTags(activity_data.value);
+                activity_data.value = self.layout._parseTags(activity_data.value);
                 model.set("activity_data", activity_data);
             }
 
@@ -495,7 +212,7 @@
             }
 
             _.each(comments, function(comment) {
-                comment.value = self._parseTags(comment.value);
+                comment.value = self.layout._parseTags(comment.value);
                 processPicture(comment);
                 _.each(comment.notes, processAttachment);
             });
