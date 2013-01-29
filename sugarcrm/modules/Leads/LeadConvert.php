40229544ee87784f4a44294650528bde28cde169
Neil Harding <nharding@sugarcrm.com>
2013-01-29 10:57:26 -0500

    more minor cleanup

--- sugarcrm/modules/Leads/LeadConvert.php
+++ sugarcrm/modules/Leads/LeadConvert.php
@@ -1,4 +1,23 @@
 <?php
+/*********************************************************************************
+ *The contents of this file are subject to the SugarCRM Professional End User License Agreement
+ *("License") which can be viewed at http://www.sugarcrm.com/EULA.
+ *By installing or using this file, You have unconditionally agreed to the terms and conditions of the License, and You may
+ *not use this file except in compliance with the License. Under the terms of the license, You
+ *shall not, among other things: 1) sublicense, resell, rent, lease, redistribute, assign or
+ *otherwise transfer Your rights to the Software, and 2) use the Software for timesharing or
+ *service bureau purposes such as hosting the Software for commercial gain and/or for the benefit
+ *of a third party.  Use of the Software may be subject to applicable fees and any use of the
+ *Software without first paying applicable fees is strictly prohibited.  You do not have the
+ *right to remove SugarCRM copyrights from the source code or user interface.
+ * All copies of the Covered Code must include on each user interface screen:
+ * (i) the "Powered by SugarCRM" logo and
+ * (ii) the SugarCRM copyright notice
+ * in the same form as they appear in the distribution.  See full license for requirements.
+ *Your Warranty, Limitations of liability and Indemnity are expressly stated in the License.  Please refer
+ *to the License for the specific language governing these rights and limitations under the License.
+ *Portions created by SugarCRM are Copyright (C) 2004 SugarCRM, Inc.; All Rights Reserved.
+ ********************************************************************************/
 
 class LeadConvert
 {
@@ -17,10 +36,16 @@ class LeadConvert
     public function initialize($leadId)
     {
         $this->defs = $this->getVarDefs();
+
+        if (empty($this->defs)) {
+            throw new Exception('Could not retrieve lead convert metadata.');
+        }
+
         $this->lead = BeanFactory::getBean('Leads', $leadId, array('strict_retrieve' => true));
 
         if (empty($this->lead)) {
-            throw new Exception('Could not find record: ' . $leadId . ' in module: ' . 'Leads');  //TODO: localize string
+            $errorMessage = string_format('Could not find record: {0} in module: Leads', $leadId);
+            throw new Exception($errorMessage);
         }
     }
 
@@ -199,9 +224,9 @@ class LeadConvert
     protected function getVarDefs()
     {
         $viewdefs = array();
-        SugarAutoLoader::requireWithCustom($this->fileName);
+        $metaDataFile = SugarAutoLoader::existingCustomOne($this->fileName);
+        require_once($metaDataFile);
         return $viewdefs['Leads']['base']['layout']['convert-main']['modules'];
-
     }
 
     /**
