2311482f909e24bfd01db3dc0fa5537548a87ab3
Hai Qi <hqi@sugarcrm.com>
2013-01-29 11:21:40 -0800

    Add access control to row action buttons in list view

--- sugarcrm/clients/base/views/list/list.php
+++ sugarcrm/clients/base/views/list/list.php
@@ -79,6 +79,7 @@ $viewdefs['base']['view']['list'] = array(
                 'tooltip' => 'LBL_PREVIEW',
                 'event' => 'list:preview:fire',
                 'icon' => 'icon-eye-open',
+                'value' => 'view',
             ),
             array(
                 'type' => 'rowaction',
@@ -86,6 +87,7 @@ $viewdefs['base']['view']['list'] = array(
                 'icon' => 'icon-pencil',
                 'label' => 'LBL_EDIT_BUTTON',
                 'event' => 'list:editrow:fire',
+                'value' => 'edit',                
             ),
 
             array(
@@ -93,6 +95,7 @@ $viewdefs['base']['view']['list'] = array(
                 'icon' => 'icon-trash',
                 'event' => 'list:deleterow:fire',
                 'label' => 'LBL_DELETE_BUTTON',
+                'value' => 'delete',                
             ),
         ),
     ),
