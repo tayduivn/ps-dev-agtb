dafcd1bf86cc8c3a50df41a566f42443c67ee874
John Barlow <jbarlow@sugarcrm.com>
2013-01-29 11:11:25 -0500

    SFA-471: Code Review Updates

--- sugarcrm/modules/Forecasts/clients/base/fields/commitStage/commitStage.js
+++ sugarcrm/modules/Forecasts/clients/base/fields/commitStage/commitStage.js
@@ -116,7 +116,7 @@
             self.$el.off("mouseenter");
             self.$el.off("mouseleave");
             self.select = self.$el.find("select");
-            self.select.select2(!_.isUndefined(self.def.searchBarThreshold)? {minimumResultsForSearch: self.def.searchBarThreshold}:null);
+            self.select.select2({minimumResultsForSearch: !_.isUndefined(self.def.searchBarThreshold)? self.def.searchBarThreshold:0});
             self.currentVal = self.value;
             self.select.select2("val", self.value);
             self.select.select2("open");
