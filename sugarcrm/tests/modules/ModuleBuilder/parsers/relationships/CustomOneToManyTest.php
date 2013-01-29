63fe1e28da5156bda0df75eeab4526c09793eed0
Rob Aagaard <rob@sugarcrm.com>
2013-01-29 16:58:47 -0700

    Bug 59889: Fix the unit tests to run in the full suite.

--- sugarcrm/tests/modules/ModuleBuilder/parsers/relationships/CustomOneToManyTest.php
+++ sugarcrm/tests/modules/ModuleBuilder/parsers/relationships/CustomOneToManyTest.php
@@ -35,15 +35,26 @@ require_once('modules/ModuleBuilder/parsers/relationships/OneToManyRelationship.
  */
 class CustomOneToManyTest extends Sugar_PHPUnit_Framework_TestCase
 {
+    public function setUp()
+    {
+        SugarTestHelper::setUp('beanList');
+        SugarTestHelper::setUp('beanFiles');
+
+    }
+    public function tearDown()
+    {
+        SugarTestHelper::tearDown();
+    }
+
 
     public function testSelfReferencing()
     {
         $definition = array(
-            'rhs_label' => 'Basic Module',
-            'lhs_label' => 'Basic Module',
+            'rhs_label' => 'Opportunities',
+            'lhs_label' => 'Opportunities',
             'rhs_subpanel' => 'default',
-            'lhs_module' => 't_bas',
-            'rhs_module' => 't_bas',
+            'lhs_module' => 'Opportunities',
+            'rhs_module' => 'Opportunities',
             'relationship_type' => 'one-to-many',
             'readonly' => false,
             'deleted' => false,
@@ -51,7 +62,7 @@ class CustomOneToManyTest extends Sugar_PHPUnit_Framework_TestCase
             'for_activities' => false,
             'is_custom' => false,
             'from_studio' => false,
-            'relationship_name' => 't_bas_t_bas_2',
+            'relationship_name' => 'opportunities_opportunities_2',
         );
 
         $testRel = new OneToManyRelationship($definition);
@@ -60,10 +71,10 @@ class CustomOneToManyTest extends Sugar_PHPUnit_Framework_TestCase
         $relData = $testRel->buildRelationshipMetaData();
 
         // print_r($vardefs);
-        $this->assertEquals(4,count($vardefs['t_bas']),"Doesn't have all four entries (link, link2, id, name)");
+        $this->assertEquals(4,count($vardefs['Opportunities']),"Doesn't have all four entries (link, link2, id, name)");
         
         $sortedDefs = array();
-        foreach($vardefs['t_bas'] as $def ) {
+        foreach($vardefs['Opportunities'] as $def ) {
             if ( $def['type'] == 'link' ) {
                 if ( isset($def['side']) && $def['side'] == 'right' ) {
                     $sortedDefs['link2'] = $def;
@@ -87,18 +98,18 @@ class CustomOneToManyTest extends Sugar_PHPUnit_Framework_TestCase
         $this->assertNotEquals($sortedDefs['id']['name'],$sortedDefs['link']['id_name'],"Link's id_name is the same as the right side id_name");
 
         // print_r($subPanel);
-        $this->assertEquals($sortedDefs['link']['name'],$subPanel['t_bas'][0]['get_subpanel_data'],"Subpanel is using the incorrect link");
+        $this->assertEquals($sortedDefs['link']['name'],$subPanel['Opportunities'][0]['get_subpanel_data'],"Subpanel is using the incorrect link");
         // print_r($relData);
     }
 
     public function testNormal()
     {
         $definition = array(
-            'rhs_label' => 'Basic Module',
+            'rhs_label' => 'Opportunities',
             'lhs_label' => 'Person Module',
             'rhs_subpanel' => 'default',
-            'lhs_module' => 't_bas',
-            'rhs_module' => 'a_per',
+            'lhs_module' => 'Opportunities',
+            'rhs_module' => 'bugs',
             'relationship_type' => 'one-to-many',
             'readonly' => false,
             'deleted' => false,
@@ -106,7 +117,7 @@ class CustomOneToManyTest extends Sugar_PHPUnit_Framework_TestCase
             'for_activities' => false,
             'is_custom' => false,
             'from_studio' => false,
-            'relationship_name' => 't_bas_a_per',
+            'relationship_name' => 'opportunities_bugs',
         );
 
         $testRel = new OneToManyRelationship($definition);
@@ -115,11 +126,11 @@ class CustomOneToManyTest extends Sugar_PHPUnit_Framework_TestCase
         $relData = $testRel->buildRelationshipMetaData();
 
         // print_r($vardefs);
-        $this->assertEquals(3,count($vardefs['a_per']),"Doesn't have the three right entries (link2, id, name)");
-        $this->assertEquals(1,count($vardefs['t_bas']),"Doesn't have just one left entry)");
+        $this->assertEquals(3,count($vardefs['bugs']),"Doesn't have the three right entries (link2, id, name)");
+        $this->assertEquals(1,count($vardefs['Opportunities']),"Doesn't have just one left entry)");
         
         $sortedDefs = array();
-        foreach($vardefs['a_per'] as $def ) {
+        foreach($vardefs['bugs'] as $def ) {
             if ( $def['type'] == 'link' ) {
                 if ( isset($def['side']) && $def['side'] == 'right' ) {
                     $sortedDefs['link2'] = $def;
@@ -134,7 +145,7 @@ class CustomOneToManyTest extends Sugar_PHPUnit_Framework_TestCase
                 }
             }
         }
-        $sortedDefs['link'] = $vardefs['t_bas'][0];
+        $sortedDefs['link'] = $vardefs['Opportunities'][0];
 
         $this->assertEquals(4,count($sortedDefs),"Couldn't sort out the four entries only found: ".print_r(array_keys($sortedDefs),true));
 
@@ -143,7 +154,7 @@ class CustomOneToManyTest extends Sugar_PHPUnit_Framework_TestCase
         $this->assertEquals($sortedDefs['id']['name'],$sortedDefs['name']['id_name'],"Name's id_name is wrong!");
 
         // print_r($subPanel);
-        $this->assertEquals($sortedDefs['link']['name'],$subPanel['t_bas'][0]['get_subpanel_data'],"Subpanel is using the incorrect link");
+        $this->assertEquals($sortedDefs['link']['name'],$subPanel['Opportunities'][0]['get_subpanel_data'],"Subpanel is using the incorrect link");
         // print_r($relData);
 
     }
