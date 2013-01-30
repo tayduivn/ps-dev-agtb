6b249f42bb70b841f4a75bf13b3670d7614caf50
Joo Lee <jlee@sugarcrm.com>
2013-01-30 12:26:07 -0500

    Add action to select and preview on duplicate check list.
    Jira: MAR-457

--- sugarcrm/clients/base/views/dupecheck-list-edit/dupecheck-list-edit.js
+++ sugarcrm/clients/base/views/dupecheck-list-edit/dupecheck-list-edit.js
@@ -1,3 +1,19 @@
 ({
-    extendsFrom: 'DupecheckListView'
+    extendsFrom: 'DupecheckListView',
+
+    addRowActions: function(panel, options) {
+        panel = app.view.views.BaselistView.prototype.addRowActions.call(this, panel, options);
+
+        if (options.meta.showPreview === true) {
+            panel.fields = panel.fields.concat({
+                type: 'rowaction',
+                css_class: 'btn',
+                tooltip: 'LBL_PREVIEW',
+                event: 'list:preview:fire',
+                icon: 'icon-eye-open'
+            });
+        }
+
+        return panel;
+    }
 })
