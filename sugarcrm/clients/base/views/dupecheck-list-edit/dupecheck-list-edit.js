77fd7426c795b630b295f6d75a72887a0251a42b
Joo Lee <jlee@sugarcrm.com>
2013-01-30 14:29:08 -0500

    Move the ability to preview the records from dupechck-list-edit to dupecheck-list view.
    Jira: MAR-457

--- sugarcrm/clients/base/views/dupecheck-list-edit/dupecheck-list-edit.js
+++ sugarcrm/clients/base/views/dupecheck-list-edit/dupecheck-list-edit.js
@@ -1,19 +1,3 @@
 ({
-    extendsFrom: 'DupecheckListView',
-
-    addRowActions: function(panel, options) {
-        panel = app.view.views.BaselistView.prototype.addRowActions.call(this, panel, options);
-
-        if (options.meta.showPreview === true) {
-            panel.fields = panel.fields.concat({
-                type: 'rowaction',
-                css_class: 'btn',
-                tooltip: 'LBL_PREVIEW',
-                event: 'list:preview:fire',
-                icon: 'icon-eye-open'
-            });
-        }
-
-        return panel;
-    }
+    extendsFrom: 'DupecheckListView'
 })
