d2a57e4f7627e45735661405fe9604bc4aa0bbfa
Peter Deng <ixce@projectixce.net>
2013-02-01 11:21:16 -0800

    Refactored filterpanel helper class

--- sugarcrm/modules/Accounts/clients/base/layouts/subpanel/subpanel.php
+++ sugarcrm/modules/Accounts/clients/base/layouts/subpanel/subpanel.php
@@ -1,8 +1,10 @@
 <?php
 
 $layout = MetaDataManager::getLayout("FilterPanelLayout");
-$layout->push(array("name" => "Member Organizations", "context" => array("link" => "members"), "toggles" => array("list")));
-$layout->push(array("name" => "Contacts", "context" => array("link" => "contacts"), "toggles" => array("list")));
-$layout->push(array("name" => "Opportunities", "context" => array("link" => "opportunities"), "toggles" => array("list")));
-$layout->push(array("name" => "Quotes", "context" => array("link" => "quotes"), "toggles" => array("list")));
+
+// Eventually change this to the proper syntax when we have subpanel lists.
+$layout->push(array("name" => "Member Organizations", "context" => array("link" => "members")));
+$layout->push(array("name" => "Contacts", "context" => array("link" => "contacts")));
+$layout->push(array("name" => "Opportunities", "context" => array("link" => "opportunities")));
+$layout->push(array("name" => "Quotes", "context" => array("link" => "quotes")));
 $viewdefs['Accounts']['base']['layout']['subpanel'] = $layout->getLayout();
