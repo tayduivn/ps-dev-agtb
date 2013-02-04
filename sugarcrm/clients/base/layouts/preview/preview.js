71d1e9a0f17cc4d14f8a2c390a845d1807ea406f
Julien Girault <julien.emac@gmail.com>
2013-02-04 15:52:33 +0100

    Refactor preview layout and introduce preview-header view

--- sugarcrm/clients/base/layouts/preview/preview.js
+++ sugarcrm/clients/base/layouts/preview/preview.js
@@ -1,28 +1,6 @@
 ({
-    events: {
-        'click [data-direction]': 'triggerPagination',
-        'click .preview-headerbar .closeSubdetail': 'triggerClose'
-    },
-
-    initialize: function(opts) {
-        _.bindAll(this);
-
-        this.template = app.template.get("l.preview");
-        this.renderHtml();
-        app.view.Layout.prototype.initialize.call(this, opts);
-    },
-
     renderHtml: function() {
-        this.$el.html(this.template(this));
+        app.view.Layout.prototype.renderHtml.call(this);
         this.$el.addClass("preview-pane");
-    },
-
-    triggerPagination: function(e) {
-        this.trigger("preview:pagination:fire", this.$(e.currentTarget).data());
-    },
-
-    triggerClose: function() {
-        this.context.trigger("list:preview:decorate", null, this);
-        this.context.trigger("preview:close:fire");
     }
 })
--- sugarcrm/clients/base/layouts/preview/preview.php
+++ sugarcrm/clients/base/layouts/preview/preview.php
@@ -1,5 +1,6 @@
 <?php
 
 $layout = MetaDataManager::getLayout('GenericLayout', array('type' => 'preview'));
+$layout->push(array("view" => "preview-header"));
 $layout->push(array("view" => "preview"));
 $viewdefs['base']['layout']['preview'] = $layout->getLayout();
