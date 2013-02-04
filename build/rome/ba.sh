69bb655a95d73aba6a4229c2a23d34aa460a2f46
Henry Rogers <henry@hhrogersii.com>
2013-02-04 10:46:09 -0500

    Uncommit build helper script

--- build/rome/ba.sh
+++
@@ -1,2 +0,0 @@
-#! /bin/bash
-php build.php --ver=7.0.0 --retainCommentSpacing=1 --flav=ent --build_dir=/Users/hrogers/Sites/builds/sugar7-master --dir=/Users/hrogers/Sites/Mango/sugarcrm/$1 --clean=1
