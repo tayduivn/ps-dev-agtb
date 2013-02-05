731a79d580191a99fbf491e50c955edf7de0326e
timwolf <twolf@sugarcrm.com>
2013-02-05 13:09:24 -0500

    Built the Duplicate Check Rules used in the Bugs module on Bug Record Creation.

--- sugarcrm/modules/Bugs/vardefs.php
+++ sugarcrm/modules/Bugs/vardefs.php
@@ -349,9 +349,23 @@ $dictionary['Bug'] = array('table' => 'bugs',    'audited'=>true, 'comment' => '
    'rhs_module'=> 'Bugs', 'rhs_table'=> 'bugs', 'rhs_key' => 'fixed_in_release',
    'relationship_type'=>'one-to-many')
 
-),         //This enables optimistic locking for Saves From EditView
+),
+
+    'duplicate_check' => array('FilterDuplicateCheck' => array(
+        'filter_template' => array(
+            array('$and' => array(
+                array('name' => array('$starts' => '$name')),
+                array('status' => array('$not_equals' => 'Closed')),
+            ))
+        ),
+        'ranking_fields' => array(
+            array('in_field_name' => 'name', 'dupe_field_name' => 'name'),
+        )
+    )),
+
+    //This enables optimistic locking for Saves From EditView
 	'optimistic_locking'=>true,
-                            );
+ );
 
 VardefManager::createVardef('Bugs','Bug', array('default', 'assignable',
 //BEGIN SUGARCRM flav=pro ONLY
