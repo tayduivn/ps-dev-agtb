43aa50936a9110b8cc0b77b1843b7c2855eab74b
Greg Levine <glevine@sugarcrm.com>
2013-02-06 15:40:05 -0500

    Can't run jasmine tests for the recipients field because all files that contain "ci" in their name are excluded from test runs. The "/ci/" directory was intended to be excluded, but not files that simply contain "ci".

--- sugarcrm/tests/index.php
+++ sugarcrm/tests/index.php
@@ -45,7 +45,7 @@ if (file_exists('../config.js')) {
  * jasmine test generator. This file will recursively search the test directory for .js test files and include them.
  */
 
-    $exclude = array("jshelpers", "jssource", "PHPUnit", 'ci');
+    $exclude = array("jshelpers", "jssource", "PHPUnit", "/ci/");
 
     $dirItr = new RecursiveDirectoryIterator('.');
     $itrItr = new RecursiveIteratorIterator($dirItr);
