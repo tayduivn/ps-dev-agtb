e83b497ba88f3aa80edf45acba26572c24828c48
Greg Levine <glevine@sugarcrm.com>
2013-02-06 16:26:27 -0500

    Added doc blocks for the controller methods in the compose-addressbook-filter and compose-addressbook-headerpane views.

--- sugarcrm/modules/Emails/clients/base/views/compose-addressbook-filter/compose-addressbook-filter.js
+++ sugarcrm/modules/Emails/clients/base/views/compose-addressbook-filter/compose-addressbook-filter.js
@@ -4,9 +4,17 @@
         "keyup #addressbook_search": "fireSearchRequest"
     },
 
+    /**
+     * Tells the address book that it should perform a search based on the module and term associated with the event.
+     * Clicking on the Search button and hitting the Enter/Return key are synonymous.
+     *
+     * @param evt
+     */
     fireSearchRequest: function(evt) {
         evt.preventDefault();
 
+        // trigger a search if the event was something other than a keyup (most likely a click) or if the keyup event
+        // was a result of pressing the Enter/Return key (13)
         if (evt.type != "keyup" || evt.which == 13) {
             var module = this.$(evt.currentTarget).attr("module"),
                 term   = this.$("#addressbook_search").val();
--- sugarcrm/modules/Emails/clients/base/views/compose-addressbook-headerpane/compose-addressbook-headerpane.js
+++ sugarcrm/modules/Emails/clients/base/views/compose-addressbook-headerpane/compose-addressbook-headerpane.js
@@ -5,7 +5,14 @@
         "click [name=cancel_button]": "_cancel"
     },
 
+    /**
+     * The user clicked the Done button so trigger an event to add selected recipients from the address book to the
+     * target field and then close the drawer.
+     *
+     * @private
+     */
     _done: function() {
+        // don't bother triggering an event if this.context.parent doesn't exist
         if (this.context.parent) {
             var target     = this.context.get("target"),
                 recipients = this.model.get("compose_addressbook_selected_recipients");
@@ -16,7 +23,13 @@
         this._cancel();
     },
 
+    /**
+     * Close the drawer.
+     *
+     * @private
+     */
     _cancel: function() {
+        // don't bother triggering an event if this.context.parent doesn't exist
         if (this.context.parent) {
             this.context.parent.trigger("drawer:hide");
         }
