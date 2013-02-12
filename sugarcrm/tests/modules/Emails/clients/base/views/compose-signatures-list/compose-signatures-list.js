9d5602ea7c495607cc9437abf83deeae18083b4e
Greg Levine <glevine@sugarcrm.com>
2013-02-12 10:43:02 -0500

    Added a jasmine test to verify that the custom endpoint defined in the view was used during sync.
    jira: MAR-477

---
+++ sugarcrm/tests/modules/Emails/clients/base/views/compose-signatures-list/compose-signatures-list.js
@@ -0,0 +1,38 @@
+describe("Emails.Views.ComposeSignaturesList", function() {
+    var app,
+        context,
+        view;
+
+    beforeEach(function() {
+        app = SugarTest.app;
+        SugarTest.testMetadata.init();
+        SugarTest.loadComponent("base", "view", "baselist");
+        SugarTest.loadComponent("base", "view", "compose-signatures-list", "Emails");
+        SugarTest.testMetadata.set();
+
+        context = app.context.getContext();
+        context.set({
+            module:     "Emails",
+            create:     true,
+            collection: new app.BeanCollection()
+        });
+        context.prepare();
+
+        view = SugarTest.createView("base", "Emails", "compose-signatures-list", null, context, true);
+    });
+
+    afterEach(function() {
+        app.cache.cutAll();
+        app.view.reset();
+        delete Handlebars.templates;
+    });
+
+    it("should call _sync to add a custom endpoint during a sync and the custom endpoint should be used", function() {
+        var apiStub = sinon.stub(app.api, "call");
+
+        view.collection.fetch();
+        expect(apiStub.lastCall.args[1]).toMatch(/.*\/Signatures\?.*/);
+
+        apiStub.restore();
+    });
+});
