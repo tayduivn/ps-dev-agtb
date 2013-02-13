c6f287bff3094f0d4e5329fcc8fefc7da07566c7
timwolf <twolf@sugarcrm.com>
2013-02-13 14:16:46 -0500

    MAR 492 - Relax Mailer rules allowing Email with Subject and No Body  as well as Body with No Subject.  The Cront End will exercise the Business rule to ensure one or the other is present and warn accordingly.

--- sugarcrm/modules/Mailer/SmtpMailer.php
+++ sugarcrm/modules/Mailer/SmtpMailer.php
@@ -400,6 +400,8 @@ class SmtpMailer extends BaseMailer
             $mailer->Body = $textBody; // the plain-text part is the primary message body
         } else {
             // you should never actually send an HTML email without a plain-text part, but we'll allow it (for now)
+            $mailer->Body = " "; // PHPMailer will choke when all message parts are empty, so give it a blank space
+                                 // to allow an "empty" body to be sent
         }
     }
 
