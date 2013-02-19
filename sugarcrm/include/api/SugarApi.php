b01b14d489e22141d8a1ec37b99b130638a72945
Jim Bartek <jbartek@sugarcrm.com>
2013-02-19 11:35:12 -0500

    Bug60638 - the decode was changing true to 1.  This fix will correct this and a new test will validate

--- sugarcrm/include/api/SugarApi.php
+++ sugarcrm/include/api/SugarApi.php
@@ -65,7 +65,7 @@ abstract class SugarApi {
         $data = ApiHelper::getHelper($api,$bean)->formatForApi($bean,$fieldList, $options);
 
         // if data is an array or object we need to decode each element, if not just decode data and pass it back
-        if(is_array($data) || is_object($data)) {
+        if(is_array($data) || is_object($data)) {   
             $this->htmlDecodeReturn($data);
         }
         elseif(!empty($data)) {
@@ -96,7 +96,8 @@ abstract class SugarApi {
             if((is_object($value) || is_array($value)) && !empty($value)) {
                 $this->htmlDecodeReturn($value);
             }
-            elseif(!empty($data) && !empty($value)) {
+            // htmldecode screws up bools..returns '1' for true
+            elseif(!is_bool($value) && (!empty($data) && !empty($value))) {
                 // USE ENT_QUOTES TO REMOVE BOTH SINGLE AND DOUBLE QUOTES, WITHOUT THIS IT WILL NOT CONVERT THEM
                 $data[$key] = html_entity_decode($value, ENT_COMPAT|ENT_QUOTES, 'UTF-8');
             }
