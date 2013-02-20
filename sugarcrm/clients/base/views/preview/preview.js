0bf1cd8b10c2d48cf4c95afe547556558b29a0c7
Steven Parsley <sparsley@sugarcrm.com>
2013-02-19 22:58:59 -0500

    Jira: MAR-498 Fixed issue with Previews pagination of colleciton not working when model does not have attribute _module.  This property is not consistent between modules.

--- sugarcrm/clients/base/views/preview/preview.js
+++ sugarcrm/clients/base/views/preview/preview.js
@@ -124,7 +124,7 @@
     renderPreview: function(model, collection) {
         if (model && collection) {
             this.model = app.data.createBean(model.get('_module') || model.module, model.toJSON());
-            this.collection = app.data.createBeanCollection(model.get('_module'), collection.models);
+            this.collection = app.data.createBeanCollection(model.get('_module') || model.module, collection.models);
 
             // Get the corresponding detail view meta for said module
             this.meta = app.metadata.getView(this.model.module, 'record') || {};
@@ -172,7 +172,7 @@
      */
     switchPreview: function(data, index, id, module) {
         var self = this,
-            currModule = module || this.model.get("_module"),
+            currModule = module || this.model.get("_module") || this.model.module,
             currID = id || this.model.get("postId") || this.model.get("id"),
             currIndex = index || _.indexOf(this.collection.models, this.collection.get(currID));
 
