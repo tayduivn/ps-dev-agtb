487ae22b74446d2d5cadab2b7db9ef945267e600
Collin Lee <clee@sugarcrm.com>
2013-02-21 01:12:09 -0500

    Attempt to fix unit test failure.

--- sugarcrm/tests/modules/UpgradeWizard/SilentUpgradeSessionVarsTest.php
+++ sugarcrm/tests/modules/UpgradeWizard/SilentUpgradeSessionVarsTest.php
@@ -25,9 +25,14 @@
 class SilentUpgradeSessionVarsTest extends Sugar_PHPUnit_Framework_TestCase 
 {
     private $externalTestFileName = 'test_silent_upgrade_vars.php';
+    private $varsCacheFileName;
     
     public function setUp() 
     {
+        $this->varsCacheFileName = sugar_cached("/silentUpgrader/silentUpgradeCache.php");
+        if(file_exists($this->varsCacheFileName)) {
+            unlink($this->varsCacheFileName);
+        }
         $this->writeExternalTestFile();
         SugarAutoLoader::addToMap($this->externalTestFileName, false);
     }
@@ -40,15 +45,12 @@ class SilentUpgradeSessionVarsTest extends Sugar_PHPUnit_Framework_TestCase
 
     public function testSilentUpgradeSessionVars()
     {
-    	
     	require_once('modules/UpgradeWizard/uw_utils.php');
-    	
-    	$varsCacheFileName = sugar_cached("/silentUpgrader/silentUpgradeCache.php");
-        
     	$loaded = loadSilentUpgradeVars();
     	$this->assertTrue($loaded, "Could not load the silent upgrade vars");
     	global $silent_upgrade_vars_loaded;
-    	$this->assertTrue(!empty($silent_upgrade_vars_loaded), "\$silent_upgrade_vars_loaded array should not be empty");
+
+    	$this->assertNotEmpty($silent_upgrade_vars_loaded, "\$silent_upgrade_vars_loaded array should not be empty");
     	
     	$set = setSilentUpgradeVar('SDizzle', 'BSnizzle');
     	$this->assertTrue($set, "Could not set a silent upgrade var");
@@ -58,15 +60,14 @@ class SilentUpgradeSessionVarsTest extends Sugar_PHPUnit_Framework_TestCase
     	
     	$write = writeSilentUpgradeVars();
     	$this->assertTrue($write, "Could not write the silent upgrade vars to the cache file. Function returned false");
-    	$this->assertFileExists($varsCacheFileName, "Cache file doesn't exist after call to writeSilentUpgradeVars()");
-    	
-    	$output = shell_exec("php {$this->externalTestFileName}");
+    	$this->assertFileExists($this->varsCacheFileName, "Cache file doesn't exist after call to writeSilentUpgradeVars()");
+        $output = shell_exec("php {$this->externalTestFileName}");
     	
     	$this->assertEquals('BSnizzle', $output, "Running custom script didn't successfully retrieve the value");
     	
     	$remove = removeSilentUpgradeVarsCache();
-    	$this->assertTrue(empty($silent_upgrade_vars_loaded), "Silent upgrade vars variable should have been unset in removeSilentUpgradeVarsCache() call");
-    	$this->assertFileNotExists($varsCacheFileName, "Cache file exists after call to removeSilentUpgradeVarsCache()");
+    	$this->assertEmpty($silent_upgrade_vars_loaded, "Silent upgrade vars variable should have been unset in removeSilentUpgradeVarsCache() call");
+    	$this->assertFileNotExists($this->varsCacheFileName, "Cache file exists after call to removeSilentUpgradeVarsCache()");
     	
     	$get = getSilentUpgradeVar('SDizzle');
     	$this->assertNotEquals('BSnizzle', $get, "Unexpected value when getting silent upgrade var after resetting");
