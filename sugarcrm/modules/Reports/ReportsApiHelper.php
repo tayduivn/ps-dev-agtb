855a4f3674ca45018c64dd610b3d6556f175de7a
Jim Bartek <jbartek@sugarcrm.com>
2013-02-21 12:02:34 -0500

    Revert "Bug61028 - fill_additional_detail_fields changes Matrix to summary, ReportsApiHelper will now change it back.  Test included"
    This reverts commit 518fec7668653de62cc4a93c662a14b7304f1bcd.

--- sugarcrm/modules/Reports/ReportsApiHelper.php
+++ sugarcrm/modules/Reports/ReportsApiHelper.php
@@ -36,21 +36,4 @@ class ReportsApiHelper extends SugarBeanApiHelper
         $bean->fromApi = true;
         return parent::populateFromApi($bean, $submittedData, $options);
     }
-
-    /**
-     * Formats the bean so it is ready to be handed back to the API's client. Certian fields will get extra processing
-     * to make them easier to work with from the client end.
-     *
-     * @param $bean SugarBean The bean you want formatted
-     * @param $fieldList array Which fields do you want formatted and returned (leave blank for all fields)
-     * @param $options array Currently no options are supported
-     * @return array The bean in array format, ready for passing out the API to clients.
-     */
-    public function formatForApi(SugarBean $bean, array $fieldList = array(), array $options = array())
-    {
-        if(isset($bean->fetched_row) && !empty($bean->fetched_row['report_type']) && $bean->report_type == 'summary' && $bean->fetched_row['report_type'] == 'Matrix') {
-            $bean->report_type = $bean->fetched_row['report_type'];
-        }
-        return parent::formatForApi($bean, $fieldList, $options);
-    }    
 }
