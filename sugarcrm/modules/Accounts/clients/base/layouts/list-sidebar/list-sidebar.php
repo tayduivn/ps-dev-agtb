56608e56037575b8fd7ae350af7f54006baae27f
justinpark <jpark@sugarcrm.com>
2013-02-27 10:45:37 -0800

    Integrated sidebar dashboard on Account module

--- sugarcrm/modules/Accounts/clients/base/layouts/list-sidebar/list-sidebar.php
+++ sugarcrm/modules/Accounts/clients/base/layouts/list-sidebar/list-sidebar.php
@@ -1,5 +1,10 @@
 <?php
 
 $layout = MetaDataManager::getLayout('GenericLayout');
-$layout->push(array('view'=>'countrychart', 'context'=>array('source'=>'SalesByCountry')));
+$layout->push(array('layout'=>'dashboard', 'context' => array(
+    'forceNew' => true,
+    'module' => 'Home',
+)));
+
+//$layout->push(array('view'=>'countrychart', 'context'=>array('source'=>'SalesByCountry')));
 $viewdefs['Accounts']['base']['layout']['list-sidebar'] = $layout->getLayout();
--- sugarcrm/modules/Accounts/clients/base/layouts/sidebar/sidebar.php
+++ sugarcrm/modules/Accounts/clients/base/layouts/sidebar/sidebar.php
@@ -1,9 +1,13 @@
 <?php
 
 $layout = MetaDataManager::getLayout('GenericLayout');
-$layout->push(array('view'=>'opportunity-metrics'));
-$layout->push(array('view'=>'crunchbase'));
-$layout->push(array('view'=>'news'));
+$layout->push(array('layout'=>'dashboard', 'context' => array(
+    'forceNew' => true,
+    'module' => 'Home',
+)));
+//$layout->push(array('view'=>'opportunity-metrics'));
+//$layout->push(array('view'=>'crunchbase'));
+//$layout->push(array('view'=>'news'));
 //$layout->push(array('view'=>'maps'));
 
 $viewdefs['Accounts']['base']['layout']['sidebar'] = $layout->getLayout();
