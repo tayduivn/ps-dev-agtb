430c7e26a7d988abc0466b1c2e8f628038272b1c
Jon Whitcraft <jwhitcraft@sugarcrm.com>
2013-02-28 15:45:31 -0500

    Bug 61514: Opp Quick Create JS Error Fix
    - Added Method Visibility

--- sugarcrm/modules/Opportunities/views/view.edit.php
+++ sugarcrm/modules/Opportunities/views/view.edit.php
@@ -43,7 +43,7 @@ class OpportunitiesViewEdit extends ViewEdit
     /**
      * @deprecated
      */
-    function OpportunitiesViewEdit()
+    public function OpportunitiesViewEdit()
     {
         $this->__construct();
     }
