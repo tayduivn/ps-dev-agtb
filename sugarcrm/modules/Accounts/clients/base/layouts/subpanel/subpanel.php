tayduivn/ps-dev-agtb

commit 87d49767319b13667832b245b6080ea54b7392a9

ABE-263 Added OOTB Subpanel Metadata.
BWC modules not added as they do not work.

sugarcrm/modules/Accounts/clients/base/layouts/subpanel/subpanel.php
@@ -5,4 +5,11 @@ $layout->push(array("name" => "Member Organizations", "context" => array("link" 
 $layout->push(array("name" => "Contacts", "context" => array("link" => "contacts")));
 $layout->push(array("name" => "Opportunities", "context" => array("link" => "opportunities")));
 $layout->push(array("name" => "Quotes", "context" => array("link" => "quotes")));
+$layout->push(array("name" => "Leads", "context" => array("link" => "leads")));
+$layout->push(array("name" => "Campaigns", "context" => array("link" => "campaign_accounts")));
+$layout->push(array("name" => "Cases", "context" => array("link" => "cases")));
+$layout->push(array("name" => "Bugs", "context" => array("link" => "bugs")));
+$layout->push(array("name" => "Products", "context" => array("link" => "products")));
+$layout->push(array("name" => "Notes", "context" => array("link" => "notes")));
+$layout->push(array("name" => "Documents", "context" => array("link" => "documentsw")));
 $viewdefs['Accounts']['base']['layout']['subpanel'] = $layout->getLayout();

sugarcrm/modules/Bugs/clients/base/layouts/subpanel/subpanel.php
@@ -1,0 +1,9 @@
+<?php
+$layout = MetaDataManager::getLayout("SubPanelLayout");
+// Eventually change this to the proper syntax when we have subpanel lists.
+$layout->push(array("name" => "Cases", "context" => array("link" => "cases")));
+$layout->push(array("name" => "Contacts", "context" => array("link" => "contacts")));
+$layout->push(array("name" => "Accounts", "context" => array("link" => "accounts")));
+$layout->push(array("name" => "Notes", "context" => array("link" => "notes")));
+$layout->push(array("name" => "Documents", "context" => array("link" => "documents")));
+$viewdefs['Bugs']['base']['layout']['subpanel'] = $layout->getLayout();

sugarcrm/modules/Cases/clients/base/layouts/subpanel/subpanel.php
@@ -1,0 +1,8 @@
+<?php
+$layout = MetaDataManager::getLayout("SubPanelLayout");
+// Eventually change this to the proper syntax when we have subpanel lists.
+$layout->push(array("name" => "Contacts", "context" => array("link" => "contacts")));
+$layout->push(array("name" => "Bugs", "context" => array("link" => "bugs")));
+$layout->push(array("name" => "Notes", "context" => array("link" => "notes")));
+$layout->push(array("name" => "Documents", "context" => array("link" => "documents")));
+$viewdefs['Cases']['base']['layout']['subpanel'] = $layout->getLayout();

sugarcrm/modules/Contacts/clients/base/layouts/subpanel/subpanel.php
@@ -1,0 +1,13 @@
+<?php
+$layout = MetaDataManager::getLayout("SubPanelLayout");
+// Eventually change this to the proper syntax when we have subpanel lists.
+$layout->push(array("name" => "Leads", "context" => array("link" => "leads")));
+$layout->push(array("name" => "Opportunities", "context" => array("link" => "opportunities")));
+$layout->push(array("name" => "Quotes", "context" => array("link" => "quotes")));
+$layout->push(array("name" => "Campaigns", "context" => array("link" => "campaign_contacts")));
+$layout->push(array("name" => "Cases", "context" => array("link" => "cases")));
+$layout->push(array("name" => "Bugs", "context" => array("link" => "bugs")));
+$layout->push(array("name" => "Direct Reports", "context" => array("link" => "reports_to_link")));
+$layout->push(array("name" => "Notes", "context" => array("link" => "notes")));
+$layout->push(array("name" => "Documents", "context" => array("link" => "documents")));
+$viewdefs['Contacts']['base']['layout']['subpanel'] = $layout->getLayout();

sugarcrm/modules/Leads/clients/base/layouts/subpanel/subpanel.php
@@ -1,0 +1,7 @@
+<?php
+$layout = MetaDataManager::getLayout("SubPanelLayout");
+// Eventually change this to the proper syntax when we have subpanel lists.
+$layout->push(array("name" => "Opportunities", "context" => array("link" => "opportunity")));
+$layout->push(array("name" => "Campaigns", "context" => array("link" => "campaign_leads")));
+$layout->push(array("name" => "Notes", "context" => array("link" => "notes")));
+$viewdefs['Leads']['base']['layout']['subpanel'] = $layout->getLayout();

sugarcrm/modules/Opportunities/clients/base/layouts/subpanel/subpanel.php
@@ -1,0 +1,9 @@
+<?php
+$layout = MetaDataManager::getLayout("SubPanelLayout");
+// Eventually change this to the proper syntax when we have subpanel lists.
+$layout->push(array("name" => "Contacts", "context" => array("link" => "contacts")));
+$layout->push(array("name" => "quotes", "context" => array("link" => "quotes")));
+$layout->push(array("name" => "Notes", "context" => array("link" => "notes")));
+$layout->push(array("name" => "leads", "context" => array("link" => "leads")));
+$layout->push(array("name" => "Documents", "context" => array("link" => "documents")));
+$viewdefs['Opportunities']['base']['layout']['subpanel'] = $layout->getLayout();

sugarcrm/modules/Tasks/clients/base/layouts/subpanel/subpanel.php
@@ -1,0 +1,5 @@
+<?php
+$layout = MetaDataManager::getLayout("SubPanelLayout");
+// Eventually change this to the proper syntax when we have subpanel lists.
+$layout->push(array("name" => "Notes", "context" => array("link" => "notes")));
+$viewdefs['Tasks']['base']['layout']['subpanel'] = $layout->getLayout();

sugarcrm/modules/Tasks/metadata/subpaneldefs.php
@@ -51,7 +51,7 @@ $layout_defs['Tasks'] = array(
 					'subpanel_name' => 'ForTasks',
 					'get_subpanel_data' => 'notes',
 				),		
-			),	
+			),
 		), /* end history subpanel def */
 	),
 );

commit d3ab4da5e551a10326d99135be7fa650427a0669

ABE-263: Typo fix for typo caused in 87d49767319b13667832b245b6080ea54b7392a9.

sugarcrm/modules/Accounts/clients/base/layouts/subpanel/subpanel.php
@@ -11,5 +11,5 @@ $layout->push(array("name" => "Cases", "context" => array("link" => "cases")));
 $layout->push(array("name" => "Bugs", "context" => array("link" => "bugs")));
 $layout->push(array("name" => "Products", "context" => array("link" => "products")));
 $layout->push(array("name" => "Notes", "context" => array("link" => "notes")));
-$layout->push(array("name" => "Documents", "context" => array("link" => "documentsw")));
+$layout->push(array("name" => "Documents", "context" => array("link" => "documents")));
 $viewdefs['Accounts']['base']['layout']['subpanel'] = $layout->getLayout();