a73a1eef2569e5051db14ee50df4c4549c8f3b04
justinpark <jpark@sugarcrm.com>
2013-03-08 09:29:46 -0800

    Clean the dashlet views out of sidebar layout

--- sugarcrm/modules/Accounts/clients/base/layouts/sidebar/sidebar.php
+++ sugarcrm/modules/Accounts/clients/base/layouts/sidebar/sidebar.php
@@ -1,9 +1,4 @@
 <?php
 
 $layout = MetaDataManager::getLayout('GenericLayout');
-//$layout->push(array('view'=>'opportunity-metrics'));
-//$layout->push(array('view'=>'crunchbase'));
-//$layout->push(array('view'=>'news'));
-//$layout->push(array('view'=>'maps'));
-
 $viewdefs['Accounts']['base']['layout']['sidebar'] = $layout->getLayout();
