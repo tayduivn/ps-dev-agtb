46215eb0568f8065e05de02dc5d5b45c7a293578
justinpark <jpark@sugarcrm.com>
2013-03-12 09:32:42 -0700

    [SP-509] Remove dashlet widgets from sidebars

--- sugarcrm/modules/Bugs/clients/base/layouts/list-sidebar/list-sidebar.php
+++ sugarcrm/modules/Bugs/clients/base/layouts/list-sidebar/list-sidebar.php
@@ -1,5 +1,4 @@
 <?php
 
 $layout = MetaDataManager::getLayout('GenericLayout');
-$layout->push(array('view'=>'countrychart', 'context'=>array('source'=>'SalesByCountry')));
 $viewdefs['Bugs']['base']['layout']['list-sidebar'] = $layout->getLayout();
