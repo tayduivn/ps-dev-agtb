c3979ae254d2f6dbaf147d33cb1830bac1675b72
Akshay Joshi <me@akshayjoshi.com>
2013-03-14 19:35:22 -0700

    ABE-407: Convert remaining hard-coded strings to lang strings in subpanels.

--- sugarcrm/modules/Leads/clients/base/layouts/subpanel/subpanel.php
+++ sugarcrm/modules/Leads/clients/base/layouts/subpanel/subpanel.php
@@ -1,9 +1,9 @@
 <?php
 
 $subpanels = array(
-    'Opportunities' => 'opportunity',
+    'LBL_OPPORTUNITIES_SUBPANEL_TITLE' => 'opportunity',
     'LBL_CAMPAIGN_LIST_SUBPANEL_TITLE' => 'campaign_leads',
-    'Notes' => 'notes',
+    'LBL_NOTES_SUBPANEL_TITLE' => 'notes',
 );
 $layout = MetaDataManager::getLayout("SubPanelLayout", $subpanels);
 $viewdefs['Leads']['base']['layout']['subpanel'] = $layout->getLayout();
