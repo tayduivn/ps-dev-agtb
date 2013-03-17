3a36ddf34b261fdd190396733fd261882e880e54
Stanislav Malyshev <smalyshev@gmail.com>
2013-03-17 15:46:16 -0700

    skip activities when running unit tests

--- sugarcrm/modules/ActivityStream/Activities/ActivityQueueManager.php
+++ sugarcrm/modules/ActivityStream/Activities/ActivityQueueManager.php
@@ -39,6 +39,10 @@ class ActivityQueueManager
      */
     public function eventDispatcher(SugarBean $bean, $event, $args)
     {
+        if(defined('SUGAR_PHPUNIT_RUNNER')) {
+            return;
+        }
+
         if ($bean instanceof Activity && $event == 'after_save' && ($bean->activity_type == 'post' || $bean->activity_type == 'attach')) {
             // Posts.
             $this->processPostSubscription($bean);
