3cb9559fc436f75847501502464bba3f76f7b145
Travis Hubbard <thubbard@sugarcrm.com>
2013-03-18 10:12:15 -0400

    SFA-699 updated editableDate for minor optimization

--- sugarcrm/modules/Forecasts/clients/base/fields/editableDate/editableDate.js
+++ sugarcrm/modules/Forecasts/clients/base/fields/editableDate/editableDate.js
@@ -25,8 +25,9 @@
         app.view.Field.prototype.render.call(this);
         // need to add this class to the parent td cell and we dont
         // have access to that from the field level
-        if(!this.$el.parent('td').hasClass('td-inline-edit')) {
-            this.$el.parent('td').addClass('td-inline-edit');
+        var tdEl = this.$el.parent('td');
+        if(!tdEl.hasClass('td-inline-edit')) {
+            tdEl.addClass('td-inline-edit');
         }
     },
 
