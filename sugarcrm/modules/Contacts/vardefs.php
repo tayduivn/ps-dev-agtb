1db29a8f8b6dcac9f485a1edc5babfee5b6a33a1
justinpark <jpark@sugarcrm.com>
2013-03-18 15:05:55 -0700

    [SC-826]Update Contact's metadata to populate matching acct's address and phone number

--- sugarcrm/modules/Contacts/vardefs.php
+++ sugarcrm/modules/Contacts/vardefs.php
@@ -70,6 +70,14 @@ array (
 			'len' => '255',
 			'source' => 'non-db',
 			'unified_search' => true,
+            'populate_list' => array(
+                'billing_address_street' => 'primary_address_street',
+                'billing_address_city' => 'primary_address_city',
+                'billing_address_state' => 'primary_address_state',
+                'billing_address_postalcode' => 'primary_address_postalcode',
+                'billing_address_country' => 'primary_address_country',
+                'phone_office' => 'phone_work',
+            ),
 		),
 	'account_id' =>
 		array (
