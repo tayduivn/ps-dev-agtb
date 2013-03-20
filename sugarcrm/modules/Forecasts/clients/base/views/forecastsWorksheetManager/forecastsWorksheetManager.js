e7f0ed2a1b141e413a45a081006db775ea36da34
Joel Spriggs <jspriggs@sugarcrm.com>
2013-03-20 08:40:12 -0400

    Bug-62065: fixing line to be proper, it could've been performing double addition otherwise

--- sugarcrm/modules/Forecasts/clients/base/views/forecastsWorksheetManager/forecastsWorksheetManager.js
+++ sugarcrm/modules/Forecasts/clients/base/views/forecastsWorksheetManager/forecastsWorksheetManager.js
@@ -708,7 +708,7 @@
             worst_case_adjusted += app.currency.convertWithRate(model.get('worst_case_adjusted'), base_rate);
             included_opp_count += (_.isUndefined(mOpp_count))? 0 : parseInt(mOpp_count);
             pipeline_opp_count += (_.isUndefined(mPipeline_opp_count))? 0 : parseInt(mPipeline_opp_count);
-            pipeline_amount = (_.isUndefined(mPipeline_amount))? 0 : app.math.add(pipeline_amount, model.get("pipeline_amount"));
+            pipeline_amount = app.math.add(pipeline_amount, (_.isUndefined(mPipeline_amount))? 0 : mPipeline_amount);
 
         });
 
