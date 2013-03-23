af74fa50c80519bcfa09b47865e5e6312c8fb867
Filipe Guerra <alias.mac@gmail.com>
2013-03-23 11:05:11 +0100

    Calls menu should use bwc links (fixes SC-943)

--- sugarcrm/modules/Calls/clients/base/menus/header/header.php
+++ sugarcrm/modules/Calls/clients/base/menus/header/header.php
@@ -1,55 +1,63 @@
 <?php
-if(!defined('sugarEntry') || !sugarEntry) die('Not A Valid Entry Point');
-/*********************************************************************************
- *The contents of this file are subject to the SugarCRM Professional End User License Agreement
- *("License") which can be viewed at http://www.sugarcrm.com/EULA.
- *By installing or using this file, You have unconditionally agreed to the terms and conditions of the License, and You may
- *not use this file except in compliance with the License. Under the terms of the license, You
- *shall not, among other things: 1) sublicense, resell, rent, lease, redistribute, assign or
- *otherwise transfer Your rights to the Software, and 2) use the Software for timesharing or
- *otherwise transfer Your rights to the Software, and 2) use the Software for timesharing or
- *service bureau purposes such as hosting the Software for commercial gain and/or for the benefit
- *of a third party.  Use of the Software may be subject to applicable fees and any use of the
- *Software without first paying applicable fees is strictly prohibited.  You do not have the
- *right to remove SugarCRM copyrights from the source code or user interface.
- * All copies of the Covered Code must include on each user interface screen:
- * (i) the "Powered by SugarCRM" logo and
- * (ii) the SugarCRM copyright notice
- * in the same form as they appear in the distribution.  See full license for requirements.
- *Your Warranty, Limitations of liability and Indemnity are expressly stated in the License.  Please refer
- *to the License for the specific language governing these rights and limitations under the License.
- *Portions created by SugarCRM are Copyright (C) 2004 SugarCRM, Inc.; All Rights Reserved.
- ********************************************************************************/
-$module_name = 'Calls';
-$viewdefs[$module_name]['base']['menu']['header'] = array(
+
+/*
+ * By installing or using this file, you are confirming on behalf of the entity
+ * subscribed to the SugarCRM Inc. product ("Company") that Company is bound by
+ * the SugarCRM Inc. Master Subscription Agreement (“MSA”), which is viewable at:
+ * http://www.sugarcrm.com/master-subscription-agreement
+ *
+ * If Company is not bound by the MSA, then by installing or using this file
+ * you are agreeing unconditionally that Company will be bound by the MSA and
+ * certifying that you have authority to bind Company accordingly.
+ *
+ * Copyright  2004-2013 SugarCRM Inc.  All rights reserved.
+ */
+
+$moduleName = 'Calls';
+$viewdefs[$moduleName]['base']['menu']['header'] = array(
     array(
-        'route'=>'#bwc/index.php?module=Calls&action=EditView&return_module=Calls&return_action=DetailView',
-        'label' =>'LNK_NEW_CALL',
-        'acl_action'=>'create',
-        'acl_module'=>$module_name,
+        'route' => '#bwc/index.php?' . http_build_query(
+            array(
+                'module' => $moduleName,
+                'action' => 'EditView',
+            )
+        ),
+        'label' => 'LNK_NEW_CALL',
+        'acl_action' => 'create',
+        'acl_module' => $moduleName,
         'icon' => 'icon-plus',
     ),
     array(
-        'route'=>'#bwc/index.php?module=Calls&action=index&return_module=Calls&return_action=DetailView',
-        'label' =>'LNK_CALL_LIST',
-        'acl_action'=>'list',
-        'acl_module'=>$module_name,
+        'route' => '#' . $moduleName,
+        'label' => 'LNK_CALL_LIST',
+        'acl_action' => 'list',
+        'acl_module' => $moduleName,
         'icon' => 'icon-reorder',
     ),
     array(
-        'route'=>'#bwc/index.php?module=Import&action=Step1&import_module=Accounts&return_module=Accounts&return_action=index',
-        'label' =>'LNK_IMPORT_CALLS',
-        'acl_action'=>'import',
-        'acl_module'=>$module_name,
+        'route' => '#bwc/index.php?' . http_build_query(
+            array(
+                'module' => 'Import',
+                'action' => 'Step1',
+                'import_module' => $moduleName,
+            )
+        ),
+        'label' => 'LNK_IMPORT_CALLS',
+        'acl_action' => 'import',
+        'acl_module' => $moduleName,
         'icon' => 'icon-upload-alternative',
     ),
-    //BEGIN SUGARCRM flav=int ONLY
     array(
-        'route'=>'#bwc/index.php?module=Reports&action=index&view=accounts',
-        'label' =>'LBL_ACTIVITIES_REPORTS',
-        'acl_action'=>'',
-        'acl_module'=>'',
+        'route' => '#bwc/index.php?' . http_build_query(
+            array(
+                'module' => 'Reports',
+                'action' => 'index',
+                'view' => $moduleName,
+            )
+        ),
+        'label' => 'LBL_ACTIVITIES_REPORTS',
+        'acl_action' => 'list',
+        'acl_module' => 'Reports',
         'icon' => 'icon-bar-chart',
     ),
-    //END SUGARCRM flav=int ONLY
 );
