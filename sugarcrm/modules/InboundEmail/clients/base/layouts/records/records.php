15f17b4a282f7d0fc8bddb9997501f2810e19f83
Neil Harding <nharding@sugarcrm.com>
2013-03-25 10:35:14 -0400

    MAR-632 - Inbound Email menu is mislabeled and missing menu items

---
+++ sugarcrm/modules/InboundEmail/clients/base/layouts/records/records.php
@@ -0,0 +1,11 @@
+<?php
+
+$viewdefs['InboundEmail']['base']['layout']['records'] = array(
+    'name' => 'bwc',
+    'type' => 'bwc',
+    'components' => array(
+        array(
+            'view' => 'bwc',
+        ),
+    ),
+);
