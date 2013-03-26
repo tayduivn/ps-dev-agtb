b031149e40eb8ff9e7d5646acf06e9991220e685
Akshay Joshi <me@akshayjoshi.com>
2013-03-26 13:36:28 -0700

    PSR-2 fixes.

--- sugarcrm/modules/Connectors/InstallDefaultConnectors.php
+++ sugarcrm/modules/Connectors/InstallDefaultConnectors.php
@@ -1,24 +1,17 @@
 <?php
 if(!defined('sugarEntry') || !sugarEntry) die('Not A Valid Entry Point');
-/*********************************************************************************
- *The contents of this file are subject to the SugarCRM Professional End User License Agreement
- *("License") which can be viewed at http://www.sugarcrm.com/EULA.
- *By installing or using this file, You have unconditionally agreed to the terms and conditions of the License, and You may
- *not use this file except in compliance with the License. Under the terms of the license, You
- *shall not, among other things: 1) sublicense, resell, rent, lease, redistribute, assign or
- *otherwise transfer Your rights to the Software, and 2) use the Software for timesharing or
- *service bureau purposes such as hosting the Software for commercial gain and/or for the benefit
- *of a third party.  Use of the Software may be subject to applicable fees and any use of the
- *Software without first paying applicable fees is strictly prohibited.  You do not have the
- *right to remove SugarCRM copyrights from the source code or user interface.
- * All copies of the Covered Code must include on each user interface screen:
- * (i) the "Powered by SugarCRM" logo and
- * (ii) the SugarCRM copyright notice
- * in the same form as they appear in the distribution.  See full license for requirements.
- *Your Warranty, Limitations of liability and Indemnity are expressly stated in the License.  Please refer
- *to the License for the specific language governing these rights and limitations under the License.
- *Portions created by SugarCRM are Copyright (C) 2004 SugarCRM, Inc.; All Rights Reserved.
- ********************************************************************************/
+/*
+ * By installing or using this file, you are confirming on behalf of the entity
+ * subscribed to the SugarCRM Inc. product ("Company") that Company is bound by
+ * the SugarCRM Inc. Master Subscription Agreement ("MSA") which is viewable at:
+ * http://www.sugarcrm.com/master-subscription-agreement
+ *
+ * If Company is not bound by the MSA, then by installing or using this file
+ * you are agreeing unconditionally that Company will be bound by the MSA and
+ * certifying that you have authority to bind Company accordingly.
+ *
+ * Copyright  2004-2013 SugarCRM Inc.  All rights reserved.
+ */
 
 $default_modules_sources = array (
   'Accounts' =>
@@ -61,10 +54,10 @@ $default_modules_sources = array (
 );
 
 $previous_connectors = array();
-if(file_exists('custom/modules/Connectors/metadata/connectors.php')){
-    require('custom/modules/Connectors/metadata/connectors.php');
+if (file_exists('custom/modules/Connectors/metadata/connectors.php')) {
+    require 'custom/modules/Connectors/metadata/connectors.php';
 
-    foreach($connectors as $connector_array){
+    foreach ($connectors as $connector_array) {
         $connector_id = $connector_array['id'];
         $previous_connectors[$connector_id] = $connector_id;
     }
@@ -72,14 +65,14 @@ if(file_exists('custom/modules/Connectors/metadata/connectors.php')){
 
 // Merge in old modules the customer added instead of overriding it completely with defaults
 // If they have customized their connectors modules
-if(file_exists('custom/modules/Connectors/metadata/display_config.php')){
-    require('custom/modules/Connectors/metadata/display_config.php');
+if (file_exists('custom/modules/Connectors/metadata/display_config.php')) {
+    require 'custom/modules/Connectors/metadata/display_config.php';
 
     // Remove the default settings from being copied over since they already existed
-    foreach($default_modules_sources as $module => $sources){
-        foreach($sources as $source_key => $source){
-            foreach($previous_connectors as $previous_connector){
-                if(in_array($previous_connector, $default_modules_sources[$module])){
+    foreach ($default_modules_sources as $module => $sources) {
+        foreach ($sources as $source_key => $source) {
+            foreach ($previous_connectors as $previous_connector) {
+                if (in_array($previous_connector, $default_modules_sources[$module])) {
                     unset($default_modules_sources[$module][$previous_connector]);
                 }
             }
@@ -88,24 +81,21 @@ if(file_exists('custom/modules/Connectors/metadata/display_config.php')){
 
     // Merge in the new connector default settings with the current settings
     if ( isset($modules_sources) && is_array($modules_sources) ) {
-        foreach($modules_sources as $module => $sources){
-            if(!empty($default_modules_sources[$module])){
+        foreach ($modules_sources as $module => $sources) {
+            if (!empty($default_modules_sources[$module])) {
                 $merged = array_merge($modules_sources[$module], $default_modules_sources[$module]);
                 $default_modules_sources[$module] = $merged;
-            }
-            else{
+            } else {
                 $default_modules_sources[$module] = $modules_sources[$module];
             }
         }
     }
 }
 
-if(!file_exists('custom/modules/Connectors/metadata')) {
-   mkdir_recursive('custom/modules/Connectors/metadata');
+if (!file_exists('custom/modules/Connectors/metadata')) {
+    mkdir_recursive('custom/modules/Connectors/metadata');
 }
 
-if(!write_array_to_file('modules_sources', $default_modules_sources, 'custom/modules/Connectors/metadata/display_config.php')) {
-   $GLOBALS['log']->fatal('Cannot write file custom/modules/Connectors/metadata/display_config.php');
+if (!write_array_to_file('modules_sources', $default_modules_sources, 'custom/modules/Connectors/metadata/display_config.php')) {
+    $GLOBALS['log']->fatal('Cannot write file custom/modules/Connectors/metadata/display_config.php');
 }
-
-?>
--- sugarcrm/tests/include/connectors/ZoominfoConnectorsTest.php
+++ sugarcrm/tests/include/connectors/ZoominfoConnectorsTest.php
@@ -1,39 +1,28 @@
 <?php
 //FILE SUGARCRM flav=pro ONLY
+/*
+ * By installing or using this file, you are confirming on behalf of the entity
+ * subscribed to the SugarCRM Inc. product ("Company") that Company is bound by
+ * the SugarCRM Inc. Master Subscription Agreement ("MSA") which is viewable at:
+ * http://www.sugarcrm.com/master-subscription-agreement
+ *
+ * If Company is not bound by the MSA, then by installing or using this file
+ * you are agreeing unconditionally that Company will be bound by the MSA and
+ * certifying that you have authority to bind Company accordingly.
+ *
+ * Copyright  2004-2013 SugarCRM Inc.  All rights reserved.
+ */
 
-/*********************************************************************************
- * The contents of this file are subject to the SugarCRM Professional End User
- * License Agreement ("License") which can be viewed at
- * http://www.sugarcrm.com/EULA.  By installing or using this file, You have
- * unconditionally agreed to the terms and conditions of the License, and You may
- * not use this file except in compliance with the License. Under the terms of the
- * license, You shall not, among other things: 1) sublicense, resell, rent, lease,
- * redistribute, assign or otherwise transfer Your rights to the Software, and 2)
- * use the Software for timesharing or service bureau purposes such as hosting the
- * Software for commercial gain and/or for the benefit of a third party.  Use of
- * the Software may be subject to applicable fees and any use of the Software
- * without first paying applicable fees is strictly prohibited.  You do not have
- * the right to remove SugarCRM copyrights from the source code or user interface.
- * All copies of the Covered Code must include on each user interface screen:
- * (i) the "Powered by SugarCRM" logo and (ii) the SugarCRM copyright notice
- * in the same form as they appear in the distribution.  See full license for
- * requirements.  Your Warranty, Limitations of liability and Indemnity are
- * expressly stated in the License.  Please refer to the License for the specific
- * language governing these rights and limitations under the License.
- * Portions created by SugarCRM are Copyright (C) 2004 SugarCRM, Inc.;
- * All Rights Reserved.
- ********************************************************************************/
-
-require_once('include/connectors/ConnectorFactory.php');
-require_once('include/connectors/sources/SourceFactory.php');
-require_once('include/connectors/utils/ConnectorUtils.php');
-require_once('modules/Connectors/controller.php');
-require_once('include/connectors/ConnectorsTestCase.php');
-require_once('tests/include/connectors/ZoominfoHelper.php');
+require_once 'include/connectors/ConnectorFactory.php';
+require_once 'include/connectors/sources/SourceFactory.php';
+require_once 'include/connectors/utils/ConnectorUtils.php';
+require_once 'modules/Connectors/controller.php';
+require_once 'include/connectors/ConnectorsTestCase.php';
+require_once 'tests/include/connectors/ZoominfoHelper.php';
 
 class ZoominfoConnectorsTest extends Sugar_Connectors_TestCase
 {
-	protected $qual_module;
+    protected $qual_module;
     protected $mock;
 
     public function setUp()
@@ -41,35 +30,35 @@ class ZoominfoConnectorsTest extends Sugar_Connectors_TestCase
         parent::setUp();
         $this->mock = new ZoominfoTestHelper();
 
-    	ConnectorFactory::$source_map = array();
+        ConnectorFactory::$source_map = array();
 
-    	if(file_exists('custom/modules/Connectors/connectors/sources/ext/rest/zoominfocompany/mapping.php')) {
-           unlink('custom/modules/Connectors/connectors/sources/ext/rest/zoominfocompany/mapping.php');
+        if (file_exists('custom/modules/Connectors/connectors/sources/ext/rest/zoominfocompany/mapping.php')) {
+            unlink('custom/modules/Connectors/connectors/sources/ext/rest/zoominfocompany/mapping.php');
         }
         SugarAutoLoader::delFromMap('custom/modules/Connectors/connectors/sources/ext/rest/zoominfocompany/mapping.php', false);
 
-    	ConnectorFactory::$source_map = array();
+        ConnectorFactory::$source_map = array();
 
-    	$_REQUEST['module'] = 'Connectors';
-    	$_REQUEST['from_unit_test'] = true;
-    	$_REQUEST['modify'] = true;
-    	$_REQUEST['action'] = 'SaveModifyDisplay';
-    	$_REQUEST['display_values'] = 'ext_rest_zoominfoperson:Leads,ext_rest_zoominfocompany:Leads';
-    	$_REQUEST['display_sources'] = 'ext_rest_linkedin,ext_rest_zoominfocompany,ext_rest_zoominfoperson';
+        $_REQUEST['module'] = 'Connectors';
+        $_REQUEST['from_unit_test'] = true;
+        $_REQUEST['modify'] = true;
+        $_REQUEST['action'] = 'SaveModifyDisplay';
+        $_REQUEST['display_values'] = 'ext_rest_zoominfoperson:Leads,ext_rest_zoominfocompany:Leads';
+        $_REQUEST['display_sources'] = 'ext_rest_linkedin,ext_rest_zoominfocompany,ext_rest_zoominfoperson';
 
-    	$controller = new ConnectorsController();
-    	$controller->action_SaveModifyDisplay();
+        $controller = new ConnectorsController();
+        $controller->action_SaveModifyDisplay();
 
-    	$_REQUEST['action'] = 'SaveModifyMapping';
-    	$_REQUEST['mapping_values'] = 'ext_rest_zoominfoperson:Leads:firstname=first_name,ext_rest_zoominfoperson:Leads:lastname=last_name,ext_rest_zoominfoperson:Leads:jobtitle=title,ext_rest_zoominfoperson:Leads:companyname=account_name,ext_rest_zoominfocompany:Leads:companyname=account_name,ext_rest_zoominfocompany:Leads:companydescription=description';
-    	$_REQUEST['mapping_sources'] = 'ext_rest_zoominfoperson,ext_rest_zoominfocompany';
-    	$controller->action_SaveModifyMapping();
+        $_REQUEST['action'] = 'SaveModifyMapping';
+        $_REQUEST['mapping_values'] = 'ext_rest_zoominfoperson:Leads:firstname=first_name,ext_rest_zoominfoperson:Leads:lastname=last_name,ext_rest_zoominfoperson:Leads:jobtitle=title,ext_rest_zoominfoperson:Leads:companyname=account_name,ext_rest_zoominfocompany:Leads:companyname=account_name,ext_rest_zoominfocompany:Leads:companydescription=description';
+        $_REQUEST['mapping_sources'] = 'ext_rest_zoominfoperson,ext_rest_zoominfocompany';
+        $controller->action_SaveModifyMapping();
 
-    	$this->qual_module = 'Leads';
-    	$this->company_source = ConnectorFactory::getInstance('ext_rest_zoominfocompany')->getSource();
-    	$this->company_props = $this->company_source->getProperties();
-    	$this->person_source = ConnectorFactory::getInstance('ext_rest_zoominfoperson')->getSource();
-    	$this->person_props = $this->person_source->getProperties();
+        $this->qual_module = 'Leads';
+        $this->company_source = ConnectorFactory::getInstance('ext_rest_zoominfocompany')->getSource();
+        $this->company_props = $this->company_source->getProperties();
+        $this->person_source = ConnectorFactory::getInstance('ext_rest_zoominfoperson')->getSource();
+        $this->person_props = $this->person_source->getProperties();
     }
 
     public function tearDown()
@@ -84,55 +73,55 @@ class ZoominfoConnectorsTest extends Sugar_Connectors_TestCase
 
     public function testZoominfoCompanyFillBeans()
     {
-    	require_once('modules/Leads/Lead.php');
-    	$source_instance = ConnectorFactory::getInstance('ext_rest_zoominfocompany');
-    	$source_instance->getSource()->loadMapping();
-    	$props = $source_instance->getSource()->getProperties();
-    	$props['company_search_url'] = $this->mock->url('company_search_query');
-    	$source_instance->getSource()->setProperties($props);
-    	$leads = array();
-    	$leads = $source_instance->fillBeans(array('companyname'=>'Cisco Systems, Inc'), $this->qual_module, $leads);
-        foreach($leads as $count=>$lead) {
-    		$this->assertContains('Cisco', $lead->account_name, "Assert fillBeans set account name to Cisco");
-    		break;
-    	}
+        require_once 'modules/Leads/Lead.php';
+        $source_instance = ConnectorFactory::getInstance('ext_rest_zoominfocompany');
+        $source_instance->getSource()->loadMapping();
+        $props = $source_instance->getSource()->getProperties();
+        $props['company_search_url'] = $this->mock->url('company_search_query');
+        $source_instance->getSource()->setProperties($props);
+        $leads = array();
+        $leads = $source_instance->fillBeans(array('companyname'=>'Cisco Systems, Inc'), $this->qual_module, $leads);
+        foreach ($leads as $count => $lead) {
+            $this->assertContains('Cisco', $lead->account_name, "Assert fillBeans set account name to Cisco");
+            break;
+        }
     }
 
     public function testZoominfoCompanyFillBean()
     {
-    	require_once('modules/Leads/Lead.php');
-    	$source_instance = ConnectorFactory::getInstance('ext_rest_zoominfocompany');
-    	$source_instance->getSource()->loadMapping();
-    	$props = $source_instance->getSource()->getProperties();
-    	$props['company_detail_url'] = $this->mock->url('company_detail');
-    	$source_instance->getSource()->setProperties($props);
-    	$lead = new Lead();
-    	$lead = $source_instance->fillBean(array('id'=>'18579882'), $this->qual_module, $lead);
-    	$this->assertContains('International Business Machines Corporation', $lead->account_name);
+        require_once 'modules/Leads/Lead.php';
+        $source_instance = ConnectorFactory::getInstance('ext_rest_zoominfocompany');
+        $source_instance->getSource()->loadMapping();
+        $props = $source_instance->getSource()->getProperties();
+        $props['company_detail_url'] = $this->mock->url('company_detail');
+        $source_instance->getSource()->setProperties($props);
+        $lead = new Lead();
+        $lead = $source_instance->fillBean(array('id' => '18579882'), $this->qual_module, $lead);
+        $this->assertContains('International Business Machines Corporation', $lead->account_name);
     }
 
     public function testZoominfoPersonFillBeans()
     {
-    	require_once('modules/Leads/Lead.php');
-    	$source_instance = SourceFactory::getSource('ext_rest_zoominfoperson');
-    	$props = $source_instance->getProperties();
-    	$props['person_search_url'] = $this->mock->url('people_search_query');
-    	$source_instance->setProperties($props);
+        require_once 'modules/Leads/Lead.php';
+        $source_instance = SourceFactory::getSource('ext_rest_zoominfoperson');
+        $props = $source_instance->getProperties();
+        $props['person_search_url'] = $this->mock->url('people_search_query');
+        $source_instance->setProperties($props);
 
-    	$args = array('firstname'=>'John', 'lastname'=>'Roberts');
-    	$data = $source_instance->getList($args, $this->qual_module);
-    	$this->assertNotEmpty($data);
+        $args = array('firstname'=>'John', 'lastname'=>'Roberts');
+        $data = $source_instance->getList($args, $this->qual_module);
+        $this->assertNotEmpty($data);
 
-    	$leads = array();
-    	$source_instance = ConnectorFactory::getInstance('ext_rest_zoominfoperson');
-    	$props = $source_instance->getSource()->getProperties();
-    	$props['person_search_url'] = $this->mock->url('people_search_query2');
-    	$source_instance->getSource()->setProperties($props);
-    	$leads = $source_instance->fillBeans($args, $this->qual_module, $leads);
-        foreach($leads as $count=>$lead) {
-    		$this->assertEquals($data[$count]['firstname'], $lead->first_name);
-    		$this->assertEquals($data[$count]['lastname'], $lead->last_name);
-    		break;
-    	}
+        $leads = array();
+        $source_instance = ConnectorFactory::getInstance('ext_rest_zoominfoperson');
+        $props = $source_instance->getSource()->getProperties();
+        $props['person_search_url'] = $this->mock->url('people_search_query2');
+        $source_instance->getSource()->setProperties($props);
+        $leads = $source_instance->fillBeans($args, $this->qual_module, $leads);
+        foreach ($leads as $count => $lead) {
+            $this->assertEquals($data[$count]['firstname'], $lead->first_name);
+            $this->assertEquals($data[$count]['lastname'], $lead->last_name);
+            break;
+        }
     }
 }
