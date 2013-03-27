dbad5ad50a72d896b0f563d32e85dfbf30cdbc50
Filipe Guerra <alias.mac@gmail.com>
2013-03-27 07:29:37 +0100

    Apply PSR-2 standards and made code clean up (fixes SC-851)

--- sugarcrm/clients/base/views/news/news.js
+++ sugarcrm/clients/base/views/news/news.js
@@ -1,52 +1,61 @@
+/*
+ * By installing or using this file, you are confirming on behalf of the entity
+ * subscribed to the SugarCRM Inc. product ('Company') that Company is bound by
+ * the SugarCRM Inc. Master Subscription Agreement (“MSA”), which is viewable at:
+ * http://www.sugarcrm.com/master-subscription-agreement
+ *
+ * If Company is not bound by the MSA, then by installing or using this file
+ * you are agreeing unconditionally that Company will be bound by the MSA and
+ * certifying that you have authority to bind Company accordingly.
+ *
+ * Copyright  2004-2013 SugarCRM Inc.  All rights reserved.
+ */
+
 ({
     plugins: ['Dashlet'],
-    initialize: function(o) {
+    initialize: function (o) {
         app.view.View.prototype.initialize.call(this, o);
-        if(this.context.parent.parent && this.context.parent.parent.get("model")) {
-            this.targetModel = this.context.parent.parent.get("model");
-            this.targetModel.on("change", this.loadData, this);
+        if (this.context.parent.parent && this.context.parent.parent.get('model')) {
+            this.targetModel = this.context.parent.parent.get('model');
+            this.targetModel.on('change', this.loadData, this);
         }
     },
 
-    _render: function() {
-        if (_.isEmpty(this.responseData)) {
-            this.hide();
+    loadData: function (options) {
+        var name, limit;
+
+        if (_.isUndefined(this.targetModel)) {
             return;
         }
 
-        this.show();
-
-        app.view.View.prototype._render.call(this);
-    },
-
-    loadData: function(options) {
-        if(_.isUndefined(this.targetModel)){
+        name = this.targetModel.get('account_name') ||
+            this.targetModel.get('name') ||
+            this.targetModel.get('full_name');
+        if (!name) {
             return;
         }
 
-        var name = this.targetModel.get("account_name") || this.targetModel.get('name') || this.targetModel.get('full_name'),
-            limit = parseInt(this.model.get("limit") || 20, 10);
-
-        if (name) {
-            if (limit < 1 || limit > 8) { // up to 8
-                limit = 8;
-            }
-            $.ajax({
-                url: "https://ajax.googleapis.com/ajax/services/search/news?v=1.0&q=" + name.toLowerCase() + "&rsz=" + limit,
-                dataType: "jsonp",
-                success: function(data) {
-                    _.extend(this, data);
-                    this.render();
-                },
-                context: this,
-                complete: (options) ? options.complete : null
-            });
-        }
+        limit = parseInt(this.model.get('limit') || 8, 10);
+        $.ajax({
+            url: 'https://ajax.googleapis.com/ajax/services/search/news?v=1.0&q=' +
+                name.toLowerCase() + '&rsz=' + limit,
+            dataType: 'jsonp',
+            success: function (data) {
+                if (this.disposed) {
+                    return;
+                }
+                _.extend(this, data);
+                this.render();
+            },
+            context: this,
+            complete: options ? options.complete : null
+        });
     },
 
-    _dispose: function() {
-        if (this.targetModel)
-            this.targetModel.off("change", this.loadData, this);
+    _dispose: function () {
+        if (this.targetModel) {
+            this.targetModel.off('change', this.loadData, this);
+        }
         app.view.View.prototype._dispose.call(this);
     }
 })
