77a22e722acb725365811fa3ab70105e05474e4d
Jon Whitcraft <jwhitcraft@sugarcrm.com>
2013-03-27 17:08:02 -0400

    SFA-523: Fix the PLI Record and Create Layout
    - Fixed the order on the record view to match the create view
    - Added a spacer that will be removed if forecasts is not setup as to maintin the layout.

--- sugarcrm/modules/Products/clients/base/views/record/record.php
+++ sugarcrm/modules/Products/clients/base/views/record/record.php
@@ -99,11 +99,19 @@ $viewdefs['Products']['base']['view']['record'] = array(
                 ),
                 'sales_stage',
                 'probability',
-                'commit_stage',
                 'sales_status',
                 array(
                     'name' => 'date_closed',
                     'required' => true,
+                ),
+                array(
+                    'name' => 'commit_stage',
+                    'span' => 6
+                ),
+                array(
+                    'name' => 'spacer',  // we need this for when forecasts is not setup and we also need to remove the spacer
+                    'span' => 6,
+                    'readonly' => true
                 ),
                 'product_template_name',
                 array(
