b3727ab54935edf2d085cfe29b07fb330fd5979f
Akshay Joshi <me@akshayjoshi.com>
2013-03-27 23:19:14 -0700

    ABE-496: Add right pane to home page activities.

--- sugarcrm/clients/base/layouts/activities/activities.php
+++ sugarcrm/clients/base/layouts/activities/activities.php
@@ -1,4 +1,16 @@
 <?php
 
-$layout = MetaDataManager::getLayout("FilterPanelLayout", array("default" => "activitystream"));
+$layout = MetaDataManager::getLayout('SideBarLayout');
+
+$layout->push('main', array('view' => 'list-headerpane'));
+
+$listLayout = MetaDataManager::getLayout("FilterPanelLayout", array("default" => "activitystream"));
+$layout->push('main', array('layout' => $listLayout->getLayout(true)));
+
+$layout->push('side', array('layout' => 'list-sidebar'));
+$layout->push('dashboard', array('layout' => 'dashboard', 'context' => array(
+    'forceNew' => true,
+    'module' => 'Home',
+)));
+$layout->push('preview', array('layout' => 'preview'));
 $viewdefs['base']['layout']['activities'] = $layout->getLayout();
