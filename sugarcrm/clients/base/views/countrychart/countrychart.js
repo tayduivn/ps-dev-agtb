f6e8153986d4633b1b542780e6d99acaaaf52e6f
Filipe Guerra <alias.mac@gmail.com>
2013-03-30 23:14:18 +0100

    Sales by Country widget renders when no records are returned (fixes SC-1010)
    Show the full map when no records are sent from the server.

--- sugarcrm/clients/base/views/countrychart/countrychart.js
+++ sugarcrm/clients/base/views/countrychart/countrychart.js
@@ -1,4 +1,5 @@
 ({
+    className: 'block',
     results: {},
     plugins: ['Dashlet'],
 
@@ -12,51 +13,49 @@
 
         app.view.View.prototype._renderHtml.call(this);
 
-        if (!_.isEmpty(this.results)) {
-            node = this.$('svg');
-            width = parseInt(node.width(), 10);
-            height = parseInt(node.css('max-height'), 10);
-            max = _.max(_(this.results).values());
-            color = d3.scale.linear().domain([0, max]).range(["gray", "blue"]);
-            xy = d3.geo.equirectangular()
-                   .scale(height)
-                   .translate([width / 2 - 0.5, height / 2]);
-            svg = d3.select("svg#" + this.cid).append('g');
-            path = d3.geo.path().projection(xy);
+        node = this.$('svg');
+        width = parseInt(node.width(), 10);
+        height = parseInt(this.$el.css('height'), 10);
+        max = _.max(_(this.results).values());
+        color = d3.scale.linear().domain([0, max]).range(["gray", "blue"]);
+        xy = d3.geo.equirectangular()
+               .scale(height)
+               .translate([width, height]);
+        svg = d3.select("svg#" + this.cid).append('g');
+        path = d3.geo.path().projection(xy);
 
-            d3.json(app.config.siteUrl + "/clients/base/views/countrychart/world-countries.json", function(collection) {
-                var g = svg.selectAll("path")
-                    .data(collection.features)
-                    .enter().append("g");
-                var bbox = {tl_x: 9999, tl_y: 9999, br_x: 0, br_y: 0};
-                g.append("path")
-                    .attr("d", function(d) {
-                        return path(d);
-                    }).style("fill",function(d) {
-                        return color(self.results[d.properties.name] || 0);
-                    }).attr("title", function(d) {
-                        return d.properties.name;
-                    });
-                g.each(function(d, i) {
-                    if(self.results[d.properties.name] || 0) {
-                        var b = this.getBBox();
-                        bbox.tl_x = Math.floor(Math.min(bbox.tl_x, b.x));
-                        bbox.tl_y = Math.floor(Math.min(bbox.tl_y, b.y));
-                        bbox.br_x = Math.ceil(Math.max(bbox.br_x, b.x + b.width));
-                        bbox.br_y = Math.ceil(Math.max(bbox.br_y, b.y + b.height));
-                    }
+        d3.json(app.config.siteUrl + "/clients/base/views/countrychart/world-countries.json", function(collection) {
+            var g = svg.selectAll("path")
+                .data(collection.features)
+                .enter().append("g");
+            var bbox = {tl_x: 9999, tl_y: 9999, br_x: 0, br_y: 0};
+            g.append("path")
+                .attr("d", function(d) {
+                    return path(d);
+                }).style("fill",function(d) {
+                    return color(self.results[d.properties.name] || 0);
+                }).attr("title", function(d) {
+                    return d.properties.name;
                 });
-                var bb_width = bbox.br_x - bbox.tl_x;
-                var bb_height = bbox.br_y - bbox.tl_y;
-                var scale_factor = Math.max(1, Math.min(width/bb_width, height/bb_height));
-                var transform = '';
-                transform += 'translate(' + -bbox.tl_x*scale_factor + ',' + -bbox.tl_y*scale_factor + ') ';
-                transform += 'scale(' + scale_factor + ') ';
-                svg.attr('transform', transform);
-                self.$(".loading").hide();
+            g.each(function(d, i) {
+                if(self.results[d.properties.name] || _.isEmpty(self.results)) {
+                    var b = this.getBBox();
+                    bbox.tl_x = Math.floor(Math.min(bbox.tl_x, b.x));
+                    bbox.tl_y = Math.floor(Math.min(bbox.tl_y, b.y));
+                    bbox.br_x = Math.ceil(Math.max(bbox.br_x, b.x + b.width));
+                    bbox.br_y = Math.ceil(Math.max(bbox.br_y, b.y + b.height));
+                }
             });
-        }
+            var bb_width = bbox.br_x - bbox.tl_x;
+            var bb_height = bbox.br_y - bbox.tl_y;
+            var scale_factor = Math.max(1, Math.min(width/bb_width, height/bb_height));
+            var transform = '';
+            transform += 'translate(' + -bbox.tl_x*scale_factor + ',' + -bbox.tl_y*scale_factor + ') ';
+            transform += 'scale(' + scale_factor + ') ';
+            svg.attr('transform', transform);
+        });
     },
+
     loadData: function(options) {
         options = options || {};
         var self = this,
@@ -75,9 +74,9 @@
         });
     },
     _checkCountry: function(country) {
-        if (country == "USA") {
-            country = "United States of America";
-        }
-        return country;
+        var map = {
+            'USA': 'United States of America'
+        };
+        return map[country] || country;
     }
 })
