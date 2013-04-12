cd65a297a7eb06f659de1ef38f78bf0e53711f99
Henry Rogers <henry@hhrogersii.com>
2013-04-12 14:43:26 -0400

    reorganize and cleanup chart data

---
+++ sugarcrm/styleguide/styleguide/charts/data/horizbar_data.js
@@ -0,0 +1,453 @@
+
+var horizbar_data_default = {
+  "properties": {
+    "title": "Horizontal Bar Chart with Baseline"
+  },
+  "data": [
+    {
+      "key": "Short",
+      "values": [
+        {
+          "label" : "Info Tech" ,
+          "value" : -1.8746444827653
+        } ,
+        {
+          "label" : "Discretionary" ,
+          "value" : -8.0961543492239
+        } ,
+        {
+          "label" : "Health Care" ,
+          "value" : -0.57072943117674
+        } ,
+        {
+          "label" : "Consumer" ,
+          "value" : -2.4174010336624
+        } ,
+        {
+          "label" : "Financials" ,
+          "value" : -0.72009071426284
+        } ,
+        {
+          "label" : "Industrials" ,
+          "value" : -0.77154485523777
+        } ,
+        {
+          "label" : "Energy" ,
+          "value" : -0.90152097798131
+        } ,
+        {
+          "label" : "Materials" ,
+          "value" : -0.91445417330854
+        } ,
+        {
+          "label" : "Telecomm" ,
+          "value" : -0.055746319141851
+        }
+      ]
+    },
+    {
+      "key": "Long",
+      "values": [
+        {
+          "label" : "Info Tech" ,
+          "value" : 25.307646510375
+        } ,
+        {
+          "label" : "Discretionary" ,
+          "value" : 16.756779544553
+        } ,
+        {
+          "label" : "Health Care" ,
+          "value" : 18.451534877007
+        } ,
+        {
+          "label" : "Consumer" ,
+          "value" : 8.6142352811805
+        } ,
+        {
+          "label" : "Financials" ,
+          "value" : 7.8082472075876
+        } ,
+        {
+          "label" : "Industrials" ,
+          "value" : 5.259101026956
+        } ,
+        {
+          "label" : "Energy" ,
+          "value" : 0.30947953487127
+        } ,
+        {
+          "label" : "Materials" ,
+          "value" : 0
+        } ,
+        {
+          "label" : "Telecomm" ,
+          "value" : 0
+        }
+      ]
+    }
+  ]
+};
+
+
+
+var horizbar_data_color = {
+  "properties": {
+    "title": "Horizontal Bar Chart with Baseline"
+  },
+  "data": [
+    {
+      "key": "Short",
+      "color": "#d62728",
+      "classes": "nv-fill00",
+      "values": [
+        {
+          "label" : "Info Tech" ,
+          "value" : -1.8746444827653
+        } ,
+        {
+          "label" : "Discretionary" ,
+          "value" : -8.0961543492239
+        } ,
+        {
+          "label" : "Health Care" ,
+          "value" : -0.57072943117674
+        } ,
+        {
+          "label" : "Consumer" ,
+          "value" : -2.4174010336624
+        } ,
+        {
+          "label" : "Financials" ,
+          "value" : -0.72009071426284
+        } ,
+        {
+          "label" : "Industrials" ,
+          "value" : -0.77154485523777
+        } ,
+        {
+          "label" : "Energy" ,
+          "value" : -0.90152097798131
+        } ,
+        {
+          "label" : "Materials" ,
+          "value" : -0.91445417330854
+        } ,
+        {
+          "label" : "Telecomm" ,
+          "value" : -0.055746319141851
+        }
+      ]
+    },
+    {
+      "key": "Long",
+      "color": "#1f77b4",
+      "classes": "nv-fill02",
+      "values": [
+        {
+          "label" : "Info Tech" ,
+          "value" : 25.307646510375
+        } ,
+        {
+          "label" : "Discretionary" ,
+          "value" : 16.756779544553
+        } ,
+        {
+          "label" : "Health Care" ,
+          "value" : 18.451534877007
+        } ,
+        {
+          "label" : "Consumer" ,
+          "value" : 8.6142352811805
+        } ,
+        {
+          "label" : "Financials" ,
+          "value" : 7.8082472075876
+        } ,
+        {
+          "label" : "Industrials" ,
+          "value" : 5.259101026956
+        } ,
+        {
+          "label" : "Energy" ,
+          "value" : 0.30947953487127
+        } ,
+        {
+          "label" : "Materials" ,
+          "value" : 0
+        } ,
+        {
+          "label" : "Telecomm" ,
+          "value" : 0
+        }
+      ]
+    }
+  ]
+};
+
+var opportunities_data = {
+  "properties": {
+    "title": "All Opportunities By Lead Source By Outcome"
+  },
+  "data": [
+    {
+      "key": "Prospecting",
+      "color": "#1f77b4",
+      "class": "nv-fill00",
+      "values": [
+        {
+          "label" : "Unassigned" ,
+          "value" : 0
+        } ,
+        {
+          "label" : "Cold Call" ,
+          "value" : 0
+        } ,
+        {
+          "label" : "Existing Customer" ,
+          "value" : 0
+        } ,
+        {
+          "label" : "Self Generated" ,
+          "value" : 0
+        } ,
+        {
+          "label" : "Employee" ,
+          "value" : 50
+        } ,
+        {
+          "label" : "Partner" ,
+          "value" : 0
+        } ,
+        {
+          "label" : "Public Relations" ,
+          "value" : 0
+        } ,
+        {
+          "label" : "Direct Mail" ,
+          "value" : 25
+        } ,
+        {
+          "label" : "Conference" ,
+          "value" : 0
+        }
+      ]
+    },
+    {
+      "key": "Qualification",
+      "color": "#ff7f0e",
+      "class": "nv-fill01",
+      "values": [
+        {
+          "label" : "Unassigned" ,
+          "value" : 0
+        } ,
+        {
+          "label" : "Cold Call" ,
+          "value" : 10
+        } ,
+        {
+          "label" : "Existing Customer" ,
+          "value" : 25
+        } ,
+        {
+          "label" : "Self Generated" ,
+          "value" : 25
+        } ,
+        {
+          "label" : "Employee" ,
+          "value" : 0
+        } ,
+        {
+          "label" : "Partner" ,
+          "value" : 0
+        } ,
+        {
+          "label" : "Public Relations" ,
+          "value" : 50
+        } ,
+        {
+          "label" : "Direct Mail" ,
+          "value" : 0
+        } ,
+        {
+          "label" : "Conference" ,
+          "value" : 0
+        }
+      ]
+    },
+    {
+      "key": "Needs Analysis",
+      "color": "#2ca02c",
+      "class": "nv-fill02",
+      "values": [
+        {
+          "label" : "Unassigned" ,
+          "value" : 0
+        } ,
+        {
+          "label" : "Cold Call" ,
+          "value" : 0
+        } ,
+        {
+          "label" : "Existing Customer" ,
+          "value" : 0
+        } ,
+        {
+          "label" : "Self Generated" ,
+          "value" : 0
+        } ,
+        {
+          "label" : "Employee" ,
+          "value" : 0
+        } ,
+        {
+          "label" : "Partner" ,
+          "value" : 50
+        } ,
+        {
+          "label" : "Public Relations" ,
+          "value" : 0
+        } ,
+        {
+          "label" : "Direct Mail" ,
+          "value" : 0
+        } ,
+        {
+          "label" : "Conference" ,
+          "value" : 0
+        }
+      ]
+    },
+    {
+      "key": "Value Proposition",
+      "color": "#d62728",
+      "class": "nv-fill03",
+      "values": [
+        {
+          "label" : "Unassigned" ,
+          "value" : 0
+        } ,
+        {
+          "label" : "Cold Call" ,
+          "value" : 75
+        } ,
+        {
+          "label" : "Existing Customer" ,
+          "value" : 0
+        } ,
+        {
+          "label" : "Self Generated" ,
+          "value" : 0
+        } ,
+        {
+          "label" : "Employee" ,
+          "value" : 50
+        } ,
+        {
+          "label" : "Partner" ,
+          "value" : 0
+        } ,
+        {
+          "label" : "Public Relations" ,
+          "value" : 0
+        } ,
+        {
+          "label" : "Direct Mail" ,
+          "value" : 50
+        } ,
+        {
+          "label" : "Conference" ,
+          "value" : 0
+        }
+      ]
+    },
+    {
+      "key": "Id. Decision Makers",
+      "color": "#9467bd",
+      "class": "nv-fill04",
+      "values": [
+        {
+          "label" : "Unassigned" ,
+          "value" : 0
+        } ,
+        {
+          "label" : "Cold Call" ,
+          "value" : 60
+        } ,
+        {
+          "label" : "Existing Customer" ,
+          "value" : 0
+        } ,
+        {
+          "label" : "Self Generated" ,
+          "value" : 0
+        } ,
+        {
+          "label" : "Employee" ,
+          "value" : 10
+        } ,
+        {
+          "label" : "Partner" ,
+          "value" : 0
+        } ,
+        {
+          "label" : "Public Relations" ,
+          "value" : 0
+        } ,
+        {
+          "label" : "Direct Mail" ,
+          "value" : 0
+        } ,
+        {
+          "label" : "Conference" ,
+          "value" : 0
+        }
+      ]
+    },
+    {
+      "key": "Perception Analysis",
+      "color": "#e377c2",
+      "class": "nv-fill05",
+      "values": [
+        {
+          "label" : "Unassigned" ,
+          "value" : 150
+        } ,
+        {
+          "label" : "Cold Call" ,
+          "value" : 0
+        } ,
+        {
+          "label" : "Existing Customer" ,
+          "value" : 0
+        } ,
+        {
+          "label" : "Self Generated" ,
+          "value" : 0
+        } ,
+        {
+          "label" : "Employee" ,
+          "value" : 50
+        } ,
+        {
+          "label" : "Partner" ,
+          "value" : 0
+        } ,
+        {
+          "label" : "Public Relations" ,
+          "value" : 0
+        } ,
+        {
+          "label" : "Direct Mail" ,
+          "value" : 0
+        } ,
+        {
+          "label" : "Conference" ,
+          "value" : 50
+        }
+      ]
+    }
+  ]
+};
+
