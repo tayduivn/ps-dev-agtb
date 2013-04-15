4b332c17cc1ea061fb06989488bd74e2de1b5570
timwolf <twolf@sugarcrm.com>
2013-04-15 11:52:47 -0400

    Jira MAR-703:  Bug48800Test.php  -  testAssignedUserName marked incomplete.  Unmarked the test and executed it - test appears to be legitimate and runs fine. (cherry picked from commit f20954b)

--- sugarcrm/tests/modules/EmailTemplates/Bug48800Test.php
+++ sugarcrm/tests/modules/EmailTemplates/Bug48800Test.php
@@ -26,50 +26,50 @@ require_once('modules/EmailTemplates/EmailTemplate.php');
 
 class Bug48800Test extends Sugar_PHPUnit_Framework_TestCase
 {
+    var $emailTemplate;
+    var $user;
 
-var $emailTemplate;
-var $user;
+    public function setUp()
+    {
+        global $current_user, $app_list_strings;
+        $this->user = SugarTestUserUtilities::createAnonymousUser();
+        $current_user = $this->user;
+        $app_list_strings = return_app_list_strings_language('en_us');
+        $this->user->setPreference('default_locale_name_format', 's f l');
+        $this->user->savePreferencesToDB();
+        $this->user->save();
 
-public function setUp()
-{
-    $this->markTestIncomplete('Test fails in suite.');
-    global $current_user, $app_list_strings;
-    $this->user = SugarTestUserUtilities::createAnonymousUser();
-    $current_user = $this->user;
-    $app_list_strings = return_app_list_strings_language('en_us');
-    $this->user->setPreference('default_locale_name_format', 's f l');
-    $this->user->savePreferencesToDB();
-    $this->user->save();
+        $this->emailTemplate = new EmailTemplate();
+        $this->emailTemplate->name = 'Bug48800Test';
+        $this->emailTemplate->assigned_user_id = $this->user->id;
+        //BEGIN SUGARCRM flav=pro ONLY
+        $this->emailTemplate->team_id = $this->user->team_id;
+        $this->emailTemplate->team_set_id = $this->user->team_id;
+        //END SUGARCRM flav=pro ONLY
+        $this->emailTemplate->save();
+        //$this->useOutputBuffering = false;
+    }
 
-    $this->emailTemplate = new EmailTemplate();
-    $this->emailTemplate->name = 'Bug48800Test';
-    $this->emailTemplate->assigned_user_id = $this->user->id;
-    //BEGIN SUGARCRM flav=pro ONLY
-    $this->emailTemplate->team_id = $this->user->team_id;
-    $this->emailTemplate->team_set_id = $this->user->team_id;
-    //END SUGARCRM flav=pro ONLY
-    $this->emailTemplate->save();
-    //$this->useOutputBuffering = false;
+    public function tearDown()
+    {
+        global $sugar_config;
+        SugarTestUserUtilities::removeAllCreatedAnonymousUsers();
+        $GLOBALS['db']->query("DELETE FROM email_templates WHERE id = '{$this->emailTemplate->id}'");
+    }
+
+    public function testAssignedUserName()
+    {
+        global $locale;
+        require_once('include/Localization/Localization.php');
+        $locale = new Localization();
+        $testName = $locale->getLocaleFormattedName($this->user->first_name, $this->user->last_name);
+        $testTemplate = new EmailTemplate();
+        $testTemplate->retrieve($this->emailTemplate->id);
+        $this->assertEquals(
+            $testName,
+            $testTemplate->assigned_user_name,
+            'Assert that the assigned_user_name is the locale formatted name value'
+        );
+    }
 }
-
-public function tearDown()
-{
-    global $sugar_config;
-    SugarTestUserUtilities::removeAllCreatedAnonymousUsers();
-    $GLOBALS['db']->query("DELETE FROM email_templates WHERE id = '{$this->emailTemplate->id}'");
-}
-
-public function testAssignedUserName()
-{
-    global $locale;
-    require_once('include/Localization/Localization.php');
-    $locale = new Localization();
-    $testName = $locale->getLocaleFormattedName($this->user->first_name, $this->user->last_name);
-    $testTemplate = new EmailTemplate();
-    $testTemplate->retrieve($this->emailTemplate->id);
-    $this->assertEquals($testName, $testTemplate->assigned_user_name, 'Assert that the assigned_user_name is the locale formatted name value');
-}
-
-}
-
 ?>
