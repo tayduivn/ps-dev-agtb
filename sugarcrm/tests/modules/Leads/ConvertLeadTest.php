3b47f6f1ce8b0a7ed471751830ae2e67aee642bc
Neil Harding <nharding@sugarcrm.com>
2013-04-17 16:09:06 -0400

    MAR-692 - ConvertLeadTest::testOpportunityNameValueFilled Removed test because: a) we are covering copying of field data in LeadConvert (in a different way) and have tests around that already b) we're not using ConvertLead code anymore anyway (cherry picked from commit 1c18c5f)

--- sugarcrm/tests/modules/Leads/ConvertLeadTest.php
+++ sugarcrm/tests/modules/Leads/ConvertLeadTest.php
@@ -93,47 +93,6 @@ class ConvertLeadTest extends Sugar_PHPUnit_Framework_TestCase
     }
 
     /**
-    * @group bug39787
-    */
-    public function testOpportunityNameValueFilled()
-    {
-        $this->markTestIncomplete('This needs to be fixed by MAR Team');
-        $lead = SugarTestLeadUtilities::createLead();
-        $lead->opportunity_name = 'SBizzle Dollar Store';
-        $lead->save();
-
-        $_REQUEST['module'] = 'Leads';
-        $_REQUEST['action'] = 'ConvertLead';
-        $_REQUEST['record'] = $lead->id;
-
-        // Check that the opportunity name doesn't get populated when it's not in the Leads editview layout
-        require_once('include/MVC/Controller/ControllerFactory.php');
-        require_once('include/MVC/View/ViewFactory.php');
-        $GLOBALS['app']->controller = ControllerFactory::getController($_REQUEST['module']);
-        ob_start();
-        $GLOBALS['app']->controller->execute();
-        $output = ob_get_clean();
-
-        $matches_one = array();
-        $pattern = '/SBizzle Dollar Store/';
-        preg_match($pattern, $output, $matches_one);
-        $this->assertTrue(count($matches_one) == 0, "Opportunity name got carried over to the Convert Leads page when it shouldn't have.");
-
-        // Add the opportunity_name to the Leads EditView
-        SugarTestStudioUtilities::addFieldToLayout('Leads', 'editview', 'opportunity_name');
-
-        // Check that the opportunity name now DOES get populated now that it's in the Leads editview layout
-        ob_start();
-        $GLOBALS['app']->controller = ControllerFactory::getController($_REQUEST['module']);
-        $GLOBALS['app']->controller->execute();
-        $output = ob_get_clean();
-        $matches_two = array();
-        $pattern = '/SBizzle Dollar Store/';
-        preg_match($pattern, $output, $matches_two);
-        $this->assertTrue(count($matches_two) > 0, "Opportunity name did not carry over to the Convert Leads page when it should have.");
-    }
-
-    /**
      * @group bug44033
      */
     public function testActivityMove() {
