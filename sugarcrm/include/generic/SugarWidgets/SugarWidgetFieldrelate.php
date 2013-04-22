27712a7a9b5555f9dcf455cf5aca372761d3657a
Boro Sitnikovski <bsitnikovski@sugarcrm.com>
2013-04-23 00:28:41 +0200

    Bug 62040 UT breakage fixes

--- sugarcrm/include/generic/SugarWidgets/SugarWidgetFieldrelate.php
+++ sugarcrm/include/generic/SugarWidgets/SugarWidgetFieldrelate.php
@@ -57,11 +57,11 @@ class SugarWidgetFieldRelate extends SugarWidgetReportField
     private function displayInputQuery($layout_def)
     {
         $title = $layout_def['rname'];
-        $bean = BeanFactory::getBean($layout_def['module']);
-        $table = $bean->table_name;
-        $concat_fields = $layout_def['db_concat_fields'];
+        $bean = isset($layout_def['module']) ? BeanFactory::getBean($layout_def['module']) : NULL;
+        $table = empty($bean) ? $layout_def['table'] : $bean->table_name;
+        $concat_fields = isset($layout_def['db_concat_fields']) ? $layout_def['db_concat_fields'] : '';
 
-        if (empty($concat_fields) && isset($bean->field_defs[$title]['db_concat_fields']))
+        if (empty($concat_fields) && !empty($bean) && isset($bean->field_defs[$title]['db_concat_fields']))
         {
             $concat_fields = $bean->field_defs[$title]['db_concat_fields'];
         }
