eb4eec8d582e812bf42551e5776f21166e0f71eb
Rob Levin <rlevin@sugarcrm.com>
2013-04-30 17:20:30 -0700

    Got rid of the notifications view in portal header.

---
+++ sugarcrm/clients/portal/layouts/header/header.php
@@ -0,0 +1,17 @@
+<?php
+$viewdefs['portal']['layout']['header'] = array(
+    'components' => array(
+        array(
+            'view' => 'modulelist',
+        ),
+        array(
+            'view' => 'createactions',
+        ),
+        array(
+            'view' => 'profileactions',
+        ),
+        array(
+            'view' => 'globalsearch',
+        )
+    ),
+);
