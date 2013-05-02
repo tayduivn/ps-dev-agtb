be55679a6a8173d17599cb66108001adbb2e36d5
David Tam <david.cheung.tam@gmail.com>
2013-05-02 13:47:29 -0700

    SP-663 contacts shows up in mega menu for portal profile page

--- sugarcrm/modules/Contacts/clients/portal/layouts/record/record.php
+++ sugarcrm/modules/Contacts/clients/portal/layouts/record/record.php
@@ -4,3 +4,4 @@ $layout = MetaDataManager::getLayout('SideBarLayout');
 $layout->push('main', array('view'=>'record'));
 $layout->push('preview', array('layout' => 'preview'));
 $viewdefs['Contacts']['portal']['layout']['record'] = $layout->getLayout();
+$viewdefs['Contacts']['portal']['layout']['record']['updateNav'] = false;
