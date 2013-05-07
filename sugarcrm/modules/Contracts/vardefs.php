b7a5743b593f51594addf33234a6055f63272552
Joe Wang <cwang@sugarcrm.com>
2013-05-07 15:36:22 -0700

    Bug_62918: 'Member Of' field in Contracts returns PHP errors when added to layouts

--- sugarcrm/modules/Contracts/vardefs.php
+++ sugarcrm/modules/Contracts/vardefs.php
@@ -301,6 +301,7 @@ $dictionary['Contract'] = array(
 		  array (
 		    'name' => 'parent_name',
 		    'vname' => 'LBL_MEMBER_OF',
+		    'id_name' => 'parent_id',
 		    'type' => 'relate',
 		    'required'=>false,
 		    'reportable'=>false,
