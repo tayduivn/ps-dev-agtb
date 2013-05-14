840fb564fa1022f19ec69c772063d5c6f7337d7a
Deepali Szczesny <deepali@sugarcrm.com>
2013-05-14 09:39:37 -0700

    Revert "Merge pull request #11809 from dszczesny/SP-773"
    This reverts commit ccc88a1d97c8c272dbf566a065aa2f6780a50db9, reversing
    changes made to 05a28940cdce325ce9ea333dd29e7cd2f5db65a4.

---
+++ sugarcrm/modules/KBDocuments/clients/portal/views/subnav/subnav.js
@@ -0,0 +1,53 @@
+/*********************************************************************************
+ * The contents of this file are subject to the SugarCRM Master Subscription
+ * Agreement (""License"") which can be viewed at
+ * http://www.sugarcrm.com/crm/master-subscription-agreement
+ * By installing or using this file, You have unconditionally agreed to the
+ * terms and conditions of the License, and You may not use this file except in
+ * compliance with the License.  Under the terms of the license, You shall not,
+ * among other things: 1) sublicense, resell, rent, lease, redistribute, assign
+ * or otherwise transfer Your rights to the Software, and 2) use the Software
+ * for timesharing or service bureau purposes such as hosting the Software for
+ * commercial gain and/or for the benefit of a third party.  Use of the Software
+ * may be subject to applicable fees and any use of the Software without first
+ * paying applicable fees is strictly prohibited.  You do not have the right to
+ * remove SugarCRM copyrights from the source code or user interface.
+ *
+ * All copies of the Covered Code must include on each user interface screen:
+ *  (i) the ""Powered by SugarCRM"" logo and
+ *  (ii) the SugarCRM copyright notice
+ * in the same form as they appear in the distribution.  See full license for
+ * requirements.
+ *
+ * Your Warranty, Limitations of liability and Indemnity are expressly stated
+ * in the License.  Please refer to the License for the specific language
+ * governing these rights and limitations under the License.  Portions created
+ * by SugarCRM are Copyright (C) 2004-2012 SugarCRM, Inc.; All Rights Reserved.
+ ********************************************************************************/
+({
+
+    /**
+     * Listens to the app:view:change event and show or hide the subnav
+     */
+    initialize: function(options) {
+        app.view.View.prototype.initialize.call(this, options);
+        this.context.set('subnavModel', new Backbone.Model());
+        this.subnavModel = this.context.get('subnavModel');
+    },
+    /**
+     * For KBDocuments, we need to pull name from kbdocument_name when name field does not exist.
+     * @private
+     */
+    _render: function(){
+        var model = this.context.get("model");
+        this.kbdocument_title = model.get("name") ? model.get("name") : model.get("kbdocument_name");
+        app.view.View.prototype._render.call(this);
+
+    },
+    bindDataChange: function() {
+        if (this.subnavModel) {
+            this.subnavModel.on("change", this.render, this);
+        }
+    }
+
+})
