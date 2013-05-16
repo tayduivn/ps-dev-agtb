9e9f86b6fd84b9fab7007eb411bd4cb59f95c893
João Morais <jcsmorais@gmail.com>
2013-05-16 03:37:27 +0200

    Create role opening a blank page (fixes SC-1148)

---
+++ sugarcrm/modules/ACLRoles/clients/base/layouts/records/records.php
@@ -0,0 +1,11 @@
+<?php
+
+$viewdefs['ACLRoles']['base']['layout']['records'] = array(
+    'name' => 'bwc',
+    'type' => 'bwc',
+    'components' => array(
+        array(
+            'view' => 'bwc',
+        ),
+    ),
+);
