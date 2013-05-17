22d397f8fa90c59874e2e8639f0a550477b30073
João Morais <jcsmorais@gmail.com>
2013-05-17 04:37:44 +0200

    PSR-2 appliance

--- sugarcrm/include/SugarObjects/templates/person/language/en_us.lang.php
+++ sugarcrm/include/SugarObjects/templates/person/language/en_us.lang.php
@@ -1,89 +1,75 @@
 <?php
-/*********************************************************************************
- * The contents of this file are subject to the SugarCRM Enterprise End User
- * License Agreement ("License") which can be viewed at
- * http://www.sugarcrm.com/crm/products/sugar-enterprise-eula.html
- * By installing or using this file, You have unconditionally agreed to the
- * terms and conditions of the License, and You may not use this file except in
- * compliance with the License.  Under the terms of the license, You shall not,
- * among other things: 1) sublicense, resell, rent, lease, redistribute, assign
- * or otherwise transfer Your rights to the Software, and 2) use the Software
- * for timesharing or service bureau purposes such as hosting the Software for
- * commercial gain and/or for the benefit of a third party.  Use of the Software
- * may be subject to applicable fees and any use of the Software without first
- * paying applicable fees is strictly prohibited.  You do not have the right to
- * remove SugarCRM copyrights from the source code or user interface.
+
+/*
+ * By installing or using this file, you are confirming on behalf of the entity
+ * subscribed to the SugarCRM Inc. product ("Company") that Company is bound by
+ * the SugarCRM Inc. Master Subscription Agreement (“MSA”), which is viewable at:
+ * http://www.sugarcrm.com/master-subscription-agreement
  *
- * All copies of the Covered Code must include on each user interface screen:
- *  (i) the "Powered by SugarCRM" logo and
- *  (ii) the SugarCRM copyright notice
- * in the same form as they appear in the distribution.  See full license for
- * requirements.
+ * If Company is not bound by the MSA, then by installing or using this file
+ * you are agreeing unconditionally that Company will be bound by the MSA and
+ * certifying that you have authority to bind Company accordingly.
  *
- * Your Warranty, Limitations of liability and Indemnity are expressly stated
- * in the License.  Please refer to the License for the specific language
- * governing these rights and limitations under the License.  Portions created
- * by SugarCRM are Copyright (C) 2004-2006 SugarCRM, Inc.; All Rights Reserved.
- ********************************************************************************/
+ * Copyright  2004-2013 SugarCRM Inc.  All rights reserved.
+ */
+
 $mod_strings = array(
-'LBL_SALUTATION'=>'Salutation',
-'LBL_NAME'=>'Name',
-'LBL_FIRST_NAME'=>'First Name',
-'LBL_LAST_NAME'=>'Last Name', 
-'LBL_TITLE'=>'Title',
-'LBL_DEPARTMENT'=>'Department',
-'LBL_DO_NOT_CALL'=>'Do Not Call',
-'LBL_HOME_PHONE'=>'Home Phone',
-'LBL_MOBILE_PHONE'=>'Mobile Phone',
-'LBL_OFFICE_PHONE'=>'Office Phone',
-'LBL_OTHER_PHONE'=>'Other Phone',
-'LBL_FAX_PHONE'=>'Fax',
-'LBL_EMAIL_ADDRESS'=>'Email Address(es)',
-'LBL_PRIMARY_ADDRESS'=>'Primary Address', 
-'LBL_PRIMARY_ADDRESS_STREET'=>'Primary Address',
-'LBL_PRIMARY_ADDRESS_STREET_2' => 'Primary Address Street 2:',
-'LBL_PRIMARY_ADDRESS_STREET_3' => 'Primary Address Street 3:',
-'LBL_PRIMARY_ADDRESS_CITY'=>'Primary City',
-'LBL_PRIMARY_ADDRESS_STATE'=>'Primary State',
-'LBL_PRIMARY_ADDRESS_POSTALCODE'=>'Primary Postal Code',
-'LBL_PRIMARY_ADDRESS_COUNTRY' => 'Primary Address Country:',
-'LBL_ALT_ADDRESS'=>'Alternate Address', 
-'LBL_ALT_ADDRESS_STREET'=>'Alternate Address',
-'LBL_ALT_ADDRESS_STREET_2' => 'Alternate Address Street 2:',
-'LBL_ALT_ADDRESS_STREET_3' => 'Alternate Address Street 3:',
-'LBL_ALT_ADDRESS_CITY'=>'Alternate City',
-'LBL_ALT_ADDRESS_STATE'=>'Alternate State',
-'LBL_ALT_ADDRESS_POSTALCODE'=>'Alternate Postal Code',
-'LBL_ALT_ADDRESS_COUNTRY'=>'Alternate Country',
-'LBL_COUNTRY'=>'Country',
-'LBL_STREET'=>'Other Address',
-'LBL_PRIMARY_STREET'=>'Address',
-'LBL_ALT_STREET'=>'Other Address',
-'LBL_CITY'=>'City',
-'LBL_STATE'=>'State',
-'LBL_POSTALCODE'=>'Postal Code',
-'LBL_POSTAL_CODE'=>'Postal Code',
-'LBL_COUNTRY'=>'Country',
-'LBL_CONTACT_INFORMATION'=>'Contact Information',
-'LBL_ADDRESS_INFORMATION'=>'Address(es)',
-'LBL_CONTACT_INFORMATION'=>'Contact Information',
-'LBL_ASSIGNED_TO_NAME'=>'User',
-'LBL_OTHER_EMAIL_ADDRESS' => 'Other Email:',
-'LBL_ASSISTANT'=>'Assistant',
-'LBL_ASSISTANT_PHONE'=>'Assistant Phone',
-'LBL_WORK_PHONE'=>'Work Phone',
-'LNK_IMPORT_VCARD' => 'Create From vCard',
-'LBL_IMPORT_VCARDTEXT' => 'Automatically create a new record by importing a vCard from your file system.',
-'LBL_ANY_EMAIL' => 'Any Email',
+    'LBL_SALUTATION' => 'Salutation',
+    'LBL_NAME' => 'Name',
+    'LBL_FIRST_NAME' => 'First Name',
+    'LBL_LAST_NAME' => 'Last Name',
+    'LBL_TITLE' => 'Title',
+    'LBL_DEPARTMENT' => 'Department',
+    'LBL_DO_NOT_CALL' => 'Do Not Call',
+    'LBL_HOME_PHONE' => 'Home Phone',
+    'LBL_MOBILE_PHONE' => 'Mobile Phone',
+    'LBL_OFFICE_PHONE' => 'Office Phone',
+    'LBL_OTHER_PHONE' => 'Other Phone',
+    'LBL_FAX_PHONE' => 'Fax',
+    'LBL_EMAIL_ADDRESS' => 'Email Address(es)',
+    'LBL_PRIMARY_ADDRESS' => 'Primary Address',
+    'LBL_PRIMARY_ADDRESS_STREET' => 'Primary Address',
+    'LBL_PRIMARY_ADDRESS_STREET_2' => 'Primary Address Street 2:',
+    'LBL_PRIMARY_ADDRESS_STREET_3' => 'Primary Address Street 3:',
+    'LBL_PRIMARY_ADDRESS_CITY' => 'Primary City',
+    'LBL_PRIMARY_ADDRESS_STATE' => 'Primary State',
+    'LBL_PRIMARY_ADDRESS_POSTALCODE' => 'Primary Postal Code',
+    'LBL_PRIMARY_ADDRESS_COUNTRY' => 'Primary Address Country:',
+    'LBL_ALT_ADDRESS' => 'Alternate Address',
+    'LBL_ALT_ADDRESS_STREET' => 'Alternate Address',
+    'LBL_ALT_ADDRESS_STREET_2' => 'Alternate Address Street 2:',
+    'LBL_ALT_ADDRESS_STREET_3' => 'Alternate Address Street 3:',
+    'LBL_ALT_ADDRESS_CITY' => 'Alternate City',
+    'LBL_ALT_ADDRESS_STATE' => 'Alternate State',
+    'LBL_ALT_ADDRESS_POSTALCODE' => 'Alternate Postal Code',
+    'LBL_ALT_ADDRESS_COUNTRY' => 'Alternate Country',
+    'LBL_COUNTRY' => 'Country',
+    'LBL_STREET' => 'Other Address',
+    'LBL_PRIMARY_STREET' => 'Address',
+    'LBL_ALT_STREET' => 'Other Address',
+    'LBL_CITY' => 'City',
+    'LBL_STATE' => 'State',
+    'LBL_POSTALCODE' => 'Postal Code',
+    'LBL_POSTAL_CODE' => 'Postal Code',
+    'LBL_COUNTRY' => 'Country',
+    'LBL_CONTACT_INFORMATION' => 'Contact Information',
+    'LBL_ADDRESS_INFORMATION' => 'Address(es)',
+    'LBL_CONTACT_INFORMATION' => 'Contact Information',
+    'LBL_ASSIGNED_TO_NAME' => 'User',
+    'LBL_OTHER_EMAIL_ADDRESS' => 'Other Email:',
+    'LBL_ASSISTANT' => 'Assistant',
+    'LBL_ASSISTANT_PHONE' => 'Assistant Phone',
+    'LBL_WORK_PHONE' => 'Work Phone',
+    'LNK_IMPORT_VCARD' => 'Create From vCard',
+    'LBL_IMPORT_VCARDTEXT' => 'Automatically create a new record by importing a vCard from your file system.',
+    'LBL_ANY_EMAIL' => 'Any Email',
 //BEGIN SUGARCRM flav!=com ONLY
-'LBL_PICTURE_FILE' => 'Avatar',
+    'LBL_PICTURE_FILE' => 'Avatar',
 //END SUGARCRM flav!=com ONLY
-
-'LBL_EDIT_BUTTON' => 'Edit',
-'LBL_REMOVE' => 'Remove',
-
-'LBL_LINKEDIN'=>'Linkedin Member Account',
-'LBL_FACEBOOK' => 'Facebook Account',
-'LBL_TWITTER' => 'Twitter Account',
-'LBL_GOOGLEPLUS' => 'Google Plus ID',
+    'LBL_EDIT_BUTTON' => 'Edit',
+    'LBL_REMOVE' => 'Remove',
+    'LBL_LINKEDIN' => 'Linkedin Member Account',
+    'LBL_FACEBOOK' => 'Facebook Account',
+    'LBL_TWITTER' => 'Twitter Account',
+    'LBL_GOOGLEPLUS' => 'Google Plus ID',
 );
