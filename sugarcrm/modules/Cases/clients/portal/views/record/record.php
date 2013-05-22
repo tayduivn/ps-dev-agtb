a579915e718bf24d9a98ce0b1b9edbdd6cafb55b
David Tam <david.cheung.tam@gmail.com>
2013-05-21 17:50:54 -0700

    SP-775 update assigned user, date entered and modified fields on portal

--- sugarcrm/modules/Cases/clients/portal/views/record/record.php
+++ sugarcrm/modules/Cases/clients/portal/views/record/record.php
@@ -42,28 +42,50 @@ $viewdefs['Cases']['portal']['view']['record'] = array(
                     'span' => 12,
                 ),
                 array(
-                    'name' => 'date_entered',
-                    'readonly' => true,
+                    'name' => 'description',
+                    'span' => 12,
                 ),
                 'status',
                 'priority',
-                'type',
                 array(
-                    'name' => 'date_modified',
-                    'readonly' => true,
-                ),
-                array(
-                    'name' => 'modified_by_name',
-                    'readonly' => true,
-                ),
-                array(
-                    'name' => 'created_by_name',
-                    'readonly' => true,
-                ),
-                'assigned_user_name',
-                array(
-                    'name' => 'description',
+                    'name' => 'type',
                     'span' => 12,
+                ),
+                array(
+                    'name' => 'date_entered_by',
+                    'readonly' => true,
+                    'type' => 'fieldset',
+                    'label' => 'LBL_DATE_ENTERED',
+                    'fields' => array(
+                        array(
+                            'name' => 'date_entered',
+                        ),
+                        array(
+                            'type' => 'label',
+                            'default_value' => 'LBL_BY',
+                        ),
+                        array(
+                            'name' => 'created_by_name',
+                        ),
+                    ),
+                ),
+                array(
+                    'name' => 'date_modified_by',
+                    'readonly' => true,
+                    'type' => 'fieldset',
+                    'label' => 'LBL_DATE_MODIFIED',
+                    'fields' => array(
+                        array(
+                            'name' => 'date_modified',
+                        ),
+                        array(
+                            'type' => 'label',
+                            'default_value' => 'LBL_BY',
+                        ),
+                        array(
+                            'name' => 'modified_by_name',
+                        ),
+                    ),
                 ),
             ),
         ),
