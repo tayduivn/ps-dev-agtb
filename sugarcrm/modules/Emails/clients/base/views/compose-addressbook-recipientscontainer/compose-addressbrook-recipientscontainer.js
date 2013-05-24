5bcc20f36a55cbfffa2a42e40069a48e84852e73
Joo Lee <jlee@sugarcrm.com>
2013-05-23 21:16:50 -0400

    MAR-367: Refactored how recipients field is rendered on the addressbook.

--- sugarcrm/modules/Emails/clients/base/views/compose-addressbook-recipientscontainer/compose-addressbrook-recipientscontainer.js
+++ sugarcrm/modules/Emails/clients/base/views/compose-addressbook-recipientscontainer/compose-addressbrook-recipientscontainer.js
@@ -6,10 +6,6 @@
 
     initialize: function(options) {
         app.view.invokeParent(this, {type: 'view', name: 'record', method: 'initialize', args: [options]});
-
-        // RecordView only toggles editable fields on render when this.createMode is true. This view is always in an
-        // edit state, since user input can be accepted at any time, so force it.
-        this.createMode       = true;
         this.model.isNotEmpty = true;
     },
 
