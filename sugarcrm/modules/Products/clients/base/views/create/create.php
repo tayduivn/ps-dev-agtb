2ae1d84e07418f4e29f52408be99facc67751bd2
Gabriel Rael <grael@sugarcrm.com>
2013-05-28 20:34:55 -0400

    SFA-1099: Split fields and hidden fields out into separate metadata arrays to make them easier to read when changing metadata

--- sugarcrm/modules/Products/clients/base/views/create/create.php
+++ sugarcrm/modules/Products/clients/base/views/create/create.php
@@ -28,6 +28,261 @@ if (!defined('sugarEntry') || !sugarEntry) {
  * Contributor(s): ______________________________________..
  ********************************************************************************/
 
+//BEGIN SUGARCRM flav=pro && flav!=ent ONLY
+// PRO/CORP only fields
+$fields =array(
+    array(
+        'name' => 'account_name',
+        'readonly' => true
+    ),
+    'sales_status',
+    array(
+        'name' => 'spacer',  // we need this for when forecasts is not setup and we also need to remove the spacer
+        'span' => 6,
+        'readonly' => true
+    ),
+    'product_template_name',
+    array(
+        'name' => 'category_name',
+        'type' => 'productCategoriesRelate',
+        'label' => 'LBL_CATEGORY',
+        'readonly' => true
+    ),
+    'quantity',
+    array(
+        'name' => 'discount_price',
+        'type' => 'currency',
+        'readonly' => true,
+        'related_fields' => array(
+            'discount_price',
+            'currency_id',
+            'base_rate',
+        ),
+        'currency_field' => 'currency_id',
+        'base_rate_field' => 'base_rate',
+    ),
+    array(
+        'name' => 'discount_amount',
+        'type' => 'currency',
+        'related_fields' => array(
+            'discount_amount',
+            'currency_id',
+            'base_rate',
+        ),
+        'convertToBase' => true,
+        'showTransactionalAmount' => true,
+        'currency_field' => 'currency_id',
+        'base_rate_field' => 'base_rate',
+    )
+);
+
+$hiddenFields = array(
+    'next_step',
+    'product_type',
+    'lead_source',
+    'campaign_name',
+    'assigned_user_name',
+    array(
+        'type' => 'teamset',
+        'name' => 'team_name',
+    ),
+    array(
+        'name' => 'description',
+        'span' => 12
+    ),
+    array(
+        'name' => 'list_price',
+        'readonly' => true,
+        'type' => 'currency',
+        'related_fields' => array(
+            'list_price',
+            'currency_id',
+            'base_rate',
+        ),
+        'currency_field' => 'currency_id',
+        'base_rate_field' => 'base_rate',
+    ),
+    'tax_class',
+    array(
+        'name' => 'cost_price',
+        'readonly' => true,
+        'type' => 'currency',
+        'related_fields' => array(
+            'cost_price',
+            'currency_id',
+            'base_rate',
+        ),
+        'currency_field' => 'currency_id',
+        'base_rate_field' => 'base_rate',
+    ),
+    array(
+        'name' => 'quote_name',
+        'label' => 'LBL_ASSOCIATED_QUOTE',
+        'related_fields' => array('quote_id'),  // this is a hack to get the quote_id field loaded
+        'readonly' => true,
+        'bwcLink' => true
+    ),
+);
+//END SUGARCRM flav=pro && flav!=ent ONLY
+
+//BEGIN SUGARCRM flav=ent ONLY
+// ENT/ULT only fields
+$fields = array(
+    array(
+        'name' => 'opportunity_name',
+        'required' => true
+    ),
+    array(
+        'name' => 'account_name',
+        'readonly' => true
+    ),
+    'sales_stage',
+    'probability',
+    'sales_status',
+    array(
+        'name' => 'date_closed',
+        'required' => true
+    ),
+    array(
+        'name' => 'commit_stage',
+        'span' => 6
+    ),
+    array(
+        'name' => 'spacer',  // we need this for when forecasts is not setup and we also need to remove the spacer
+        'span' => 6,
+        'readonly' => true
+    ),
+    'product_template_name',
+    array(
+        'name' => 'category_name',
+        'type' => 'productCategoriesRelate',
+        'label' => 'LBL_CATEGORY',
+        'readonly' => true
+    ),
+    'quantity',
+    array(
+        'name' => 'discount_price',
+        'type' => 'currency',
+        'readonly' => true,
+        'related_fields' => array(
+            'discount_price',
+            'currency_id',
+            'base_rate',
+        ),
+        'currency_field' => 'currency_id',
+        'base_rate_field' => 'base_rate',
+    ),
+    array(
+        'name' => 'discount_amount',
+        'type' => 'currency',
+        'related_fields' => array(
+            'discount_amount',
+            'currency_id',
+            'base_rate',
+        ),
+        'convertToBase' => true,
+        'showTransactionalAmount' => true,
+        'currency_field' => 'currency_id',
+        'base_rate_field' => 'base_rate',
+    ),
+    array(
+        'name' => 'total_amount',
+        'type' => 'currency',
+        'label' => 'LBL_CALCULATED_LINE_ITEM_AMOUNT',
+        'readonly' => true,
+        'related_fields' => array(
+            'total_amount',
+            'currency_id',
+            'base_rate',
+        ),
+        'currency_field' => 'currency_id',
+        'base_rate_field' => 'base_rate',
+    ),
+    array(
+        'name' => 'likely_case',
+        'required' => true,
+        'type' => 'currency',
+        'related_fields' => array(
+            'likely_case',
+            'currency_id',
+            'base_rate',
+        ),
+        'currency_field' => 'currency_id',
+        'base_rate_field' => 'base_rate',
+    )
+);
+
+$hiddenFields = array(
+    array(
+        'name' => 'worst_case',
+        'type' => 'currency',
+        'related_fields' => array(
+            'worst_case',
+            'currency_id',
+            'base_rate',
+        ),
+        'currency_field' => 'currency_id',
+        'base_rate_field' => 'base_rate',
+    ),
+    array(
+        'name' => 'best_case',
+        'type' => 'currency',
+        'related_fields' => array(
+            'best_case',
+            'currency_id',
+            'base_rate',
+        ),
+        'currency_field' => 'currency_id',
+        'base_rate_field' => 'base_rate',
+    ),
+    'next_step',
+    'product_type',
+    'lead_source',
+    'campaign_name',
+    'assigned_user_name',
+    array(
+        'type' => 'teamset',
+        'name' => 'team_name',
+    ),
+    array(
+        'name' => 'description',
+        'span' => 12
+    ),
+    array(
+        'name' => 'list_price',
+        'readonly' => true,
+        'type' => 'currency',
+        'related_fields' => array(
+            'list_price',
+            'currency_id',
+            'base_rate',
+        ),
+        'currency_field' => 'currency_id',
+        'base_rate_field' => 'base_rate',
+    ),
+    'tax_class',
+    array(
+        'name' => 'cost_price',
+        'readonly' => true,
+        'type' => 'currency',
+        'related_fields' => array(
+            'cost_price',
+            'currency_id',
+            'base_rate',
+        ),
+        'currency_field' => 'currency_id',
+        'base_rate_field' => 'base_rate',
+    ),
+    array(
+        'name' => 'quote_name',
+        'label' => 'LBL_ASSOCIATED_QUOTE',
+        'related_fields' => array('quote_id'),  // this is a hack to get the quote_id field loaded
+        'readonly' => true,
+        'bwcLink' => true
+    ),
+);
+//END SUGARCRM flav=ent ONLY
+
 $viewdefs['Products']['base']['view']['create'] = array(
     'type' => 'record',
     'buttons' => array(
@@ -91,98 +346,7 @@ $viewdefs['Products']['base']['view']['create'] = array(
             'labels' => true,
             'labelsOnTop' => true,
             'placeholders' => true,
-            'fields' => array(
-                //BEGIN SUGARCRM flav=ent ONLY
-                array(
-                    'name' => 'opportunity_name',
-                    'required' => true
-                ),
-                //END SUGARCRM flav=ent ONLY
-                array(
-                    'name' => 'account_name',
-                    'readonly' => true
-                ),
-                //BEGIN SUGARCRM flav=ent ONLY
-                'sales_stage',
-                'probability',
-                //END SUGARCRM flav=ent ONLY
-                'sales_status',
-                //BEGIN SUGARCRM flav=ent ONLY
-                array(
-                    'name' => 'date_closed',
-                    'required' => true
-                ),
-                array(
-                    'name' => 'commit_stage',
-                    'span' => 6
-                ),
-                //END SUGARCRM flav=ent ONLY
-                array(
-                    'name' => 'spacer',  // we need this for when forecasts is not setup and we also need to remove the spacer
-                    'span' => 6,
-                    'readonly' => true
-                ),
-                'product_template_name',
-                array(
-                    'name' => 'category_name',
-                    'type' => 'productCategoriesRelate',
-                    'label' => 'LBL_CATEGORY',
-                    'readonly' => true
-                ),
-                'quantity',
-                array(
-                    'name' => 'discount_price',
-                    'type' => 'currency',
-                    'readonly' => true,
-                    'related_fields' => array(
-                        'discount_price',
-                        'currency_id',
-                        'base_rate',
-                    ),
-                    'currency_field' => 'currency_id',
-                    'base_rate_field' => 'base_rate',
-                ),
-                array(
-                    'name' => 'discount_amount',
-                    'type' => 'currency',
-                    'related_fields' => array(
-                        'discount_amount',
-                        'currency_id',
-                        'base_rate',
-                    ),
-                    'convertToBase' => true,
-                    'showTransactionalAmount' => true,
-                    'currency_field' => 'currency_id',
-                    'base_rate_field' => 'base_rate',
-                ),
-                //BEGIN SUGARCRM flav=ent ONLY
-                array(
-                    'name' => 'total_amount',
-                    'type' => 'currency',
-                    'label' => 'LBL_CALCULATED_LINE_ITEM_AMOUNT',
-                    'readonly' => true,
-                    'related_fields' => array(
-                        'total_amount',
-                        'currency_id',
-                        'base_rate',
-                    ),
-                    'currency_field' => 'currency_id',
-                    'base_rate_field' => 'base_rate',
-                ),
-                array(
-                    'name' => 'likely_case',
-                    'required' => true,
-                    'type' => 'currency',
-                    'related_fields' => array(
-                        'likely_case',
-                        'currency_id',
-                        'base_rate',
-                    ),
-                    'currency_field' => 'currency_id',
-                    'base_rate_field' => 'base_rate',
-                )
-                //END SUGARCRM flav=ent ONLY
-            ),
+            'fields' => $fields
         ),
         array(
             'name' => 'panel_hidden',
@@ -190,79 +354,7 @@ $viewdefs['Products']['base']['view']['create'] = array(
             'columns' => 2,
             'labelsOnTop' => true,
             'placeholders' => true,
-            'fields' => array(
-                //BEGIN SUGARCRM flav=ent ONLY
-                array(
-                    'name' => 'worst_case',
-                    'type' => 'currency',
-                    'related_fields' => array(
-                        'worst_case',
-                        'currency_id',
-                        'base_rate',
-                    ),
-                    'currency_field' => 'currency_id',
-                    'base_rate_field' => 'base_rate',
-                ),
-                array(
-                    'name' => 'best_case',
-                    'type' => 'currency',
-                    'related_fields' => array(
-                        'best_case',
-                        'currency_id',
-                        'base_rate',
-                    ),
-                    'currency_field' => 'currency_id',
-                    'base_rate_field' => 'base_rate',
-                ),
-                //END SUGARCRM flav=ent ONLY
-                'next_step',
-                'product_type',
-                'lead_source',
-                'campaign_name',
-                'assigned_user_name',
-                //BEGIN SUGARCRM flav=pro ONLY
-                array(
-                    'type' => 'teamset',
-                    'name' => 'team_name',
-                ),
-                //END SUGARCRM flav=pro ONLY
-                array(
-                    'name' => 'description',
-                    'span' => 12
-                ),
-                array(
-                    'name' => 'list_price',
-                    'readonly' => true,
-                    'type' => 'currency',
-                    'related_fields' => array(
-                        'list_price',
-                        'currency_id',
-                        'base_rate',
-                    ),
-                    'currency_field' => 'currency_id',
-                    'base_rate_field' => 'base_rate',
-                ),
-                'tax_class',
-                array(
-                    'name' => 'cost_price',
-                    'readonly' => true,
-                    'type' => 'currency',
-                    'related_fields' => array(
-                        'cost_price',
-                        'currency_id',
-                        'base_rate',
-                    ),
-                    'currency_field' => 'currency_id',
-                    'base_rate_field' => 'base_rate',
-                ),
-                array(
-                    'name' => 'quote_name',
-                    'label' => 'LBL_ASSOCIATED_QUOTE',
-                    'related_fields' => array('quote_id'),  // this is a hack to get the quote_id field loaded
-                    'readonly' => true,
-                    'bwcLink' => true
-                ),
-            )
+            'fields' => $hiddenFields
         )
     ),
 );
