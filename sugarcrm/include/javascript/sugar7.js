b9712000484b0397e9b6cf0bd95e42e926385a8a
justinpark <jpark@sugarcrm.com>
2013-05-31 13:25:57 -0700

    [SC-918]Hotfix when iframe is empty on bwc module

--- sugarcrm/include/javascript/sugar7.js
+++ sugarcrm/include/javascript/sugar7.js
@@ -177,7 +177,7 @@
 
         if(_.isEmpty(metadata) || metadata.isBwcEnabled) {
             //For BWC module, current document title will be replaced with BWC title
-            title = $('#bwc-frame').get(0).contentWindow.document.title;
+            title = $('#bwc-frame').get(0) ? $('#bwc-frame').get(0).contentWindow.document.title : getTitle();
         } else {
             title = getTitle();
             if(!_.isEmpty(context.get("model"))) {
