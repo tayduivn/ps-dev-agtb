61f167b5fb35e44939fe1362a49e908692b93daf
Jim Bartek <jbartek@sugarcrm.com>
2013-06-04 11:35:12 -0400

    BR-243, BR-149, BR-257 - Password formatting and saving.
    BR-passwords
    
    Fix based on the RobertG comments
    
    Changes based on Filipes comments
    
    Changed comments around

--- sugarcrm/include/SugarFields/Fields/Password/SugarFieldPassword.php
+++ sugarcrm/include/SugarFields/Fields/Password/SugarFieldPassword.php
@@ -22,8 +22,8 @@ class SugarFieldPassword extends SugarFieldBase
     }
 
    /**
-     * This function will blank out any password field
-     * 
+     * This function will set any password field to true if there is
+     * a password, else null
      * @param array $data
      * @param SugarBean $bean
      * @param array $args
@@ -32,7 +32,26 @@ class SugarFieldPassword extends SugarFieldBase
      */
     public function apiFormatField(array &$data, SugarBean $bean, array $args, $fieldName, $properties)
     {
-        $data[$fieldName] = '';
-    }    
+        $data[$fieldName] = true;
+        if(empty($bean->$fieldName)) {
+            $data[$fieldName] = null;
+        }
+    }
+
+    /**
+     * Encrypt and save a password
+     * {@inheritdoc}
+     */
+    public function apiSave(SugarBean $bean, array $params, $fieldName, $properties)
+    {
+        if(!isset($params[$fieldName])) {
+            return;
+        }
+        if(empty($params[$fieldName])) {
+            $bean->$fieldName = null;
+        } elseif($params[$fieldName] !== true) {
+            $bean->$fieldName = User::getPasswordHash($params[$fieldName]);
+        }
+    }
 }
 ?>
