75b4cce5cfd1794c50e91847c0df49754805409e
jwhitcraft <jwhitcraft@sugarcrm.com>
2013-06-05 10:05:21 -0400

    SFA-869: Sales Stage -> Auto Populate Probability
    - Fixed Opportunities create to have the buttons and the dependency

--- sugarcrm/modules/Opportunities/clients/base/views/create-actions/create-actions.php
+++ sugarcrm/modules/Opportunities/clients/base/views/create-actions/create-actions.php
@@ -230,8 +230,51 @@ $fieldsHidden = array(
 );
 //END SUGARCRM flav=ent ONLY
 
-$viewdefs['Opportunities']['base']['view']['create'] = array(
+$viewdefs['Opportunities']['base']['view']['create-actions'] = array(
     'type' => 'record',
+    'buttons' => array(
+        array(
+            'name'      => 'cancel_button',
+            'type'      => 'button',
+            'label'     => 'LBL_CANCEL_BUTTON_LABEL',
+            'css_class' => 'btn-invisible btn-link',
+        ),
+        array(
+            'name'      => 'restore_button',
+            'type'      => 'button',
+            'label'     => 'LBL_RESTORE',
+            'css_class' => 'btn-invisible btn-link',
+            'showOn'    => 'select',
+        ),
+        array(
+            'type'    => 'actiondropdown',
+            'name'    => 'main_dropdown',
+            'primary' => true,
+            'buttons' => array(
+                array(
+                    'type'  => 'rowaction',
+                    'name'  => 'save_button',
+                    'label' => 'LBL_SAVE_BUTTON_LABEL',
+                ),
+                array(
+                    'type'   => 'rowaction',
+                    'name'   => 'save_view_button',
+                    'label'  => 'LBL_SAVE_AND_VIEW',
+                    'showOn' => 'create',
+                ),
+                array(
+                    'type'   => 'rowaction',
+                    'name'   => 'save_create_button',
+                    'label'  => 'LBL_SAVE_AND_CREATE_ANOTHER',
+                    'showOn' => 'create',
+                ),
+            ),
+        ),
+        array(
+            'name' => 'sidebar_toggle',
+            'type' => 'sidebartoggle',
+        ),
+    ),
     'panels' => array(
         array(
             'name' => 'panel_header',
