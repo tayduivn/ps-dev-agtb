8884f89cfad7abb8e006a691322f369659babe1d
Filipe Guerra <alias.mac@gmail.com>
2013-06-06 20:26:21 +0200

    Update layout metadata to not use code - SC modules (fixes SC-1237)

--- sugarcrm/modules/Accounts/clients/base/layouts/list-dashboard/list-dashboard.php
+++ sugarcrm/modules/Accounts/clients/base/layouts/list-dashboard/list-dashboard.php
@@ -1,20 +1,41 @@
 <?php
-$layout = MetaDataManager::getLayout(
-    'DashboardLayout',
-    array(
-        'columns' => 1,
-        'name' => 'My Dashboard',
-    )
-);
-$layout->push(
-    0,
-    array(
-        array(
-            'view' => array(
+
+/*
+ * By installing or using this file, you are confirming on behalf of the entity
+ * subscribed to the SugarCRM Inc. product ("Company") that Company is bound by
+ * the SugarCRM Inc. Master Subscription Agreement (“MSA”), which is viewable at:
+ * http://www.sugarcrm.com/master-subscription-agreement
+ *
+ * If Company is not bound by the MSA, then by installing or using this file
+ * you are agreeing unconditionally that Company will be bound by the MSA and
+ * certifying that you have authority to bind Company accordingly.
+ *
+ * Copyright  2004-2013 SugarCRM Inc.  All rights reserved.
+ */
+
+$viewdefs['Accounts']['base']['layout']['list-dashboard'] = array (
+  'metadata' => 
+  array (
+    'components' => 
+    array (
+      array (
+        'rows' => 
+        array (
+          array (
+            array (
+              'view' => 
+              array (
                 'name' => 'countrychart',
-                'label' => 'Sales By Country'
+                'label' => 'Sales By Country',
+              ),
+              'width' => 12,
             ),
+          ),
         ),
-    )
+        'width' => 12,
+      ),
+    ),
+  ),
+  'name' => 'My Dashboard',
 );
-$viewdefs['Accounts']['base']['layout']['list-dashboard'] = $layout->getLayout();
+
