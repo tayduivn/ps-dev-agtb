c78e2256bba52419ea801abae4838f3c5361c16e
Julien Girault <julien.emac@gmail.com>
2013-06-06 19:59:37 +0200

    SP-871: Refactor and unit tests for views/filter-module-dropdown

---
+++ sugarcrm/tests/clients/base/views/filter-module-dropdown/filter-module-dropdown.js
@@ -0,0 +1,196 @@
+describe("BaseFilterModuleDropdownView", function () {
+    var view, layout, app;
+
+    beforeEach(function () {
+        SugarTest.testMetadata.init();
+        SugarTest.loadComponent('base', 'view', 'filter-module-dropdown');
+        SugarTest.testMetadata.set();
+        layout = SugarTest.createLayout('base', "Cases", "filter", null, null, null, { layout: new Backbone.View() });
+        view = SugarTest.createView("base", "Cases", "filter-module-dropdown", null, null, null, layout);
+        view.layout = layout;
+        app = SUGAR.App;
+    });
+
+    afterEach(function () {
+        SugarTest.testMetadata.dispose();
+        app.cache.cutAll();
+        app.view.reset();
+        delete Handlebars.templates;
+        view = null;
+    });
+
+    describe('handleChange callback of filter:change:module', function() {
+        var layoutStub;
+
+        beforeEach(function() {
+            layoutStub = sinon.stub(view.layout, 'trigger');
+            view.filterNode = $('');
+        });
+
+        afterEach(function() {
+            layoutStub.restore();
+        });
+
+        it('should only trigger subpanel:change if linkName is all_modules and silent is true', function() {
+            var silent = true;
+            view.handleChange('Cases', 'all_modules', silent);
+
+            expect(layoutStub).toHaveBeenCalled();
+            expect(layoutStub.firstCall.args[0]).toEqual('subpanel:change');
+            expect(layoutStub.secondCall).toBeNull();
+        });
+
+        it('should trigger subppanel:change if linkName is all_modules and silent is false', function() {
+            var silent = false;
+            view.handleChange('Cases', 'all_modules', silent);
+
+            expect(layoutStub).toHaveBeenCalled();
+            expect(layoutStub.firstCall.args[0]).toEqual('subpanel:change');
+            expect(layoutStub.secondCall.args[0]).toEqual('filter:get');
+        });
+
+        it('should trigger filter:create:close if link is not all_modules', function() {
+            var silent = true;
+            view.handleChange('Cases', 'Contacts', silent);
+
+            expect(layoutStub).toHaveBeenCalled();
+            expect(layoutStub.firstCall.args[0]).toEqual('filter:create:close');
+            expect(layoutStub.secondCall.args[0]).toEqual('subpanel:change');
+        });
+
+        it('should do same as above with filter:get because silent is false', function() {
+            var silent = false;
+            view.handleChange('Cases', 'Contacts', silent);
+
+            expect(layoutStub).toHaveBeenCalled();
+            expect(layoutStub.firstCall.args[0]).toEqual('filter:create:close');
+            expect(layoutStub.secondCall.args[0]).toEqual('subpanel:change');
+            expect(layoutStub.thirdCall.args[0]).toEqual('filter:get');
+        });
+    });
+
+    describe('filterList', function() {
+
+        it('gets module list for Activity Stream', function() {
+            var expected, filterList;
+            expected = [{ id: 'Activities', text: app.lang.get('LBL_MODULE_NAME', 'Cases')}];
+            filterList = view.getModuleListForActivities();
+            expect(filterList).toEqual(expected);
+            view.module = 'Activities';
+            expected = [{ id: 'Activities', text: app.lang.get("LBL_TABGROUP_ALL")}];
+            filterList = view.getModuleListForActivities();
+            expect(filterList).toEqual(expected);
+        });
+
+        it('gets module list for Records layout', function() {
+            var expected, filterList;
+            expected = [{ id: 'Cases', text: app.lang.get('LBL_MODULE_NAME', 'Cases')}];
+            filterList = view.getModuleListForRecords();
+            expect(filterList).toEqual(expected);
+        });
+
+        it('gets module list for Subpanels', function() {
+            var metadataStub = sinon.stub(app.metadata, 'getModule', function(module) {
+                return {
+                    layouts: {
+                        subpanel: {
+                            meta: {
+                                subpanelList: {
+                                    'LBL_CONTACTS_SUBPANEL_TITLE' : 'contacts'
+                                }
+                            }
+                        }
+                    }
+                };
+            });
+            var dataStub = sinon.stub(app.data, 'getRelatedModule', function(module, link) {
+                return 'Contacts';
+            })
+            var expected, filterList;
+            expected = [{ id: 'all_modules', text: app.lang.get("LBL_TABGROUP_ALL")},
+                        { id: 'contacts', text: app.lang.get("LBL_CONTACTS_SUBPANEL_TITLE") }];
+            filterList = view.getModuleListForSubpanels();
+            expect(filterList).toEqual(expected);
+            metadataStub.restore();
+            dataStub.restore();
+        });
+    });
+
+    describe('pullSubpanelRelationships', function() {
+
+        it('should return subpanels metadata', function() {
+            var metadataStub = sinon.stub(app.metadata, 'getModule', function(module) {
+                return {
+                    layouts: {
+                        subpanel: {
+                            meta: {
+                                subpanelList: {
+                                    'LBL_CONTACTS_SUBPANEL_TITLE' : 'contacts'
+                                }
+                            }
+                        }
+                    }
+                };
+            });
+            var subpanels = view.pullSubpanelRelationships();
+            expect(subpanels).toEqual({'LBL_CONTACTS_SUBPANEL_TITLE' : 'contacts'});
+            metadataStub.restore();
+        });
+    });
+
+    describe('select2 options', function() {
+
+        it('should initSelection for selected module', function() {
+            var $input = $('<input type="text">').val('all_modules'),
+                callback = sinon.stub(),
+                expected = { id: 'all_modules', text: app.lang.get("LBL_MODULE_NAME", 'Cases')};
+
+            view.initSelection($input, callback);
+
+            expect(callback).toHaveBeenCalled();
+            expect(callback.lastCall.args[0]).toEqual(expected);
+            callback.reset();
+
+            $input.val('Cases');
+            expected = { id: 'Cases', text: app.lang.get("LBL_MODULE_NAME", 'Cases')};
+
+            view.filterList = [expected];
+            view.initSelection($input, callback);
+
+            expect(callback).toHaveBeenCalled();
+            expect(callback.lastCall.args[0]).toEqual(expected);
+        });
+
+
+        it('should formatSelection for selected module', function() {
+            var expected = app.lang.get("LBL_MODULE"),
+                html;
+
+            //Template replacement
+            view._select2formatSelectionTemplate = function(val) { return val; };
+
+            html = view.formatSelection({id: 'test', text: 'TEST'});
+
+            expect(html).toEqual(expected);
+
+            view.layout.layoutType = 'record';
+            view.layout.showingActivities = false;
+
+            html = view.formatSelection({id: 'test', text: 'TEST'});
+            expected = app.lang.get("LBL_RELATED") + '<i class="icon-caret-down"></i>';
+            expect(html).toEqual(expected);
+        });
+
+        it('should formatResult for selected module', function() {
+            var expected = 'TEST',
+                html;
+
+            //Template replacement
+            view._select2formatResultTemplate = function(val) { return val; };
+
+            html = view.formatResult({id: 'test', text: 'TEST'});
+
+            expect(html).toEqual(expected);
+        });
+    });
+});
