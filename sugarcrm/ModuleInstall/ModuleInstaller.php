5f8ae376ae407328b07e5970aa4e79c3f6d29273
Matthew Marum <mmarum@sugarcrm.com>
2013-06-06 12:44:29 -0400

    SP-884: Load maxQueryResult and maxSubpanelResult through public metadata

--- sugarcrm/ModuleInstall/ModuleInstaller.php
+++ sugarcrm/ModuleInstall/ModuleInstaller.php
@@ -2427,8 +2427,6 @@ private function dir_file_count($path){
             'alertAutoCloseDelay' => 2500,
             'serverUrl' => $sugar_config['site_url'].'/rest/v10',
             'siteUrl' => $sugar_config['site_url'],
-            'maxQueryResult' => $sugar_config['list_max_entries_per_page'],
-            'maxSubpanelResult' => $sugar_config['list_max_entries_per_subpanel'],
             'unsecureRoutes' => array('login', 'error'),
             'loadCss' => false,
             'themeName' => 'default',
