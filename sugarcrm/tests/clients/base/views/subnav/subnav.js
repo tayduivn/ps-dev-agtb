83cc042c18a55d3904b2ed6b8fcd21365653ee19
Rob Levin <rlevin@sugarcrm.com>
2013-06-12 15:03:11 -0700

    Fixed broken tests since getView now returns null if view not found. Also, replaced flaky jasmine.Clock with sinon.useFakeTimers which resolved failing test on Subnav. Removed unused file and wrong name of subpanel to subpanels for available toggles.

--- sugarcrm/tests/clients/base/views/subnav/subnav.js
+++ sugarcrm/tests/clients/base/views/subnav/subnav.js
@@ -1,24 +1,30 @@
 describe("Subnav View", function() {
 
-    var app, view;
+    var app, view, clock, sinonSandbox;
 
     beforeEach(function() {
+        sinonSandbox = sinon.sandbox.create();
+        clock = sinon.useFakeTimers();
+        if (!$.fn.tooltip) {
+            $.fn.tooltip = sinon.stub();
+        }
         app = SugarTest.app;
         var context = app.context.getContext();
-        view = SugarTest.createView("base","Cases", "subnav", null, context);
+        view = SugarTest.createView("base","Cases", "subnav", {}, context);
         view.model = new Backbone.Model();
     });
-    
+
     afterEach(function() {
+        sinonSandbox.restore();
         app.cache.cutAll();
         app.view.reset();
         delete Handlebars.templates;
         view.model = null;
         view = null;
+        clock.restore();
     });
 
     it("should add a tooltip if the window is too small to display the title", function() {
-        jasmine.Clock.useMock();
         //add to the body otherwise offsetWidth and scrollWidth will be 0
         $('body').append(view.$el);
         var title = 'This is a very very very very very very very very very very very very very very very very very very very very very very very very very very very very very very very very very very very very very very very very very very big title';
@@ -28,12 +34,12 @@ describe("Subnav View", function() {
             .text(title).appendTo(view.$el);
         //call resize and wait because there is a setInterval
         view.resize();
-        jasmine.Clock.tick(500);
+        clock.tick(500);
         expect(view.$('h1').attr('data-original-title')).toEqual(title);
         //change for a very little title
         view.$('h1').text('t');
         view.resize();
-        jasmine.Clock.tick(500);
+        clock.tick(500);
         expect(view.$('h1').attr('data-original-title')).toBeUndefined();
         view.$el.remove();
     });
