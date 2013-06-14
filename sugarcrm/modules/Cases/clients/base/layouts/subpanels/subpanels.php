03b559a509d59f056e50eac0701de246872c8026
Matthew Marum <mmarum@sugarcrm.com>
2013-06-14 16:45:10 -0400

    SP-817: Add project subpanel to Bugs and Cases.  Changed order of subpanels to match order found in Sugar 6.x (sans Tasks/Emails/Meetings/Calls).

--- sugarcrm/modules/Cases/clients/base/layouts/subpanels/subpanels.php
+++ sugarcrm/modules/Cases/clients/base/layouts/subpanels/subpanels.php
@@ -1,34 +1,34 @@
 <?php
 $viewdefs['Cases']['base']['layout']['subpanels'] = array (
   'components' => array (
-    array (
-      'layout' => 'subpanel',
-      'label' => 'LBL_CONTACTS_SUBPANEL_TITLE',
-      'context' => array (
-        'link' => 'contacts',
+      array (
+          'layout' => 'subpanel',
+          'label' => 'LBL_DOCUMENTS_SUBPANEL_TITLE',
+          'context' => array (
+              'link' => 'documents',
+          ),
       ),
-    ),
-    array (
-      'layout' => 'subpanel',
-      'label' => 'LBL_BUGS_SUBPANEL_TITLE',
-      'context' => array (
-        'link' => 'bugs',
+      array (
+          'layout' => 'subpanel',
+          'label' => 'LBL_CONTACTS_SUBPANEL_TITLE',
+          'context' => array (
+              'link' => 'contacts',
+          ),
       ),
-    ),
-    array (
-      'layout' => 'subpanel',
-      'label' => 'LBL_NOTES_SUBPANEL_TITLE',
-      'context' => array (
-        'link' => 'notes',
+      array (
+          'layout' => 'subpanel',
+          'label' => 'LBL_BUGS_SUBPANEL_TITLE',
+          'context' => array (
+              'link' => 'bugs',
+          ),
       ),
-    ),
-    array (
-      'layout' => 'subpanel',
-      'label' => 'LBL_DOCUMENTS_SUBPANEL_TITLE',
-      'context' => array (
-        'link' => 'documents',
+      array (
+          'layout' => 'subpanel',
+          'label' => 'LBL_PROJECTS_SUBPANEL_TITLE',
+          'context' => array (
+              'link' => 'project',
+          ),
       ),
-    ),
   ),
   'type' => 'subpanels',
   'span' => 12,
