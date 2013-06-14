e6744db94fd34d64151dced3cdc09aabc154bcb7
Joo Lee <jlee@sugarcrm.com>
2013-06-14 16:16:08 -0400

    MAR-875: When creating a new record from the selection-list view, the drawer is closed without transitions.

--- sugarcrm/clients/base/views/selection-headerpane/selection-headerpane.js
+++ sugarcrm/clients/base/views/selection-headerpane/selection-headerpane.js
@@ -1,3 +1,15 @@
+/*
+ * By installing or using this file, you are confirming on behalf of the entity
+ * subscribed to the SugarCRM Inc. product ("Company") that Company is bound by
+ * the SugarCRM Inc. Master Subscription Agreement (“MSA”), which is viewable at:
+ * http://www.sugarcrm.com/master-subscription-agreement
+ *
+ * If Company is not bound by the MSA, then by installing or using this file
+ * you are agreeing unconditionally that Company will be bound by the MSA and
+ * certifying that you have authority to bind Company accordingly.
+ *
+ * Copyright  2004-2013 SugarCRM Inc.  All rights reserved.
+ */
 ({
     extendsFrom: 'HeaderpaneView',
 
@@ -34,10 +46,9 @@
                 create: true
             }
         }, _.bind(function (context, model) {
-            if (!model) {
-                return;
+            if (model) {
+                this.context.trigger('selection-list:select', model);
             }
-            this.context.set('selection_model', model);
         }, this));
     },
 
