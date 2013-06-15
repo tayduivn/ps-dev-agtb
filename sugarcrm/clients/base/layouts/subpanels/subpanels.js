36bc48efe47e58ddaf3a7888be76c583b2c4fbcf
Rob Levin <rlevin@sugarcrm.com>
2013-06-14 20:05:56 -0700

    Fixes 'error message when open subpanel view if the login user doesn't have access to a module' SP-924 Integrated hidden and acl checks
    Fixes issue where vardef field def with no 'module' property (which is redudant as it's defined in relationship anyway), was causing ACL not to get checked.

--- sugarcrm/clients/base/layouts/subpanels/subpanels.js
+++ sugarcrm/clients/base/layouts/subpanels/subpanels.js
@@ -4,9 +4,35 @@
      */
     initialize: function(opts) {
         if (!opts.meta) return;
-
         app.view.Layout.prototype.initialize.call(this, opts);
         this.layout.on("subpanel:change", this.showSubpanel, this);
+    },
+
+    /**
+     * Removes subpanels that user doesn't have access to. SP-924: Error message when opening subpanel
+     * user doesn't have access to.
+     *
+     * @param {Array} components list of child components from layout definition
+     * @return {Object} pruned components
+     * @private
+     * @override
+     */
+    _pruneNoAccessComponents: function(components) {
+        var prunedComponents = [];
+        var layoutFromContext = this.context ? this.context.get('layout') || this.context.get('layoutName') : null;
+        this.layoutType = layoutFromContext ? layoutFromContext : app.controller.context.get('layout');
+        this.aclToCheck = this.aclToCheck || (this.layoutType === 'record') ? 'view' : 'list';
+        _.each(components, function(component) {
+            var relatedModule,
+                link = component.context ? component.context.link : null;
+            if (link) {
+                relatedModule = app.data.getRelatedModule(this.module, link);
+                if (!relatedModule || relatedModule && app.acl.hasAccess(this.aclToCheck, relatedModule)) {
+                    prunedComponents.push(component);
+                }
+            }
+        }, this);
+        return prunedComponents;
     },
 
     /**
@@ -14,10 +40,11 @@
      * Removes hidden subpanels from list of components before adding them to layout
      *
      * @param {Array} components list of child components from layout definition
+     * @return {Object} pruned components
      * @private
      * @override
      */
-    _addComponentsFromDef: function(components) {
+    _pruneHiddenComponents: function(components) {
         var hiddenSubpanels = app.metadata.getHiddenSubpanels();
         var visibleSubpanels = _.filter(components, function(component){
             var relatedModule = app.data.getRelatedModule(this.module, component.context.link);
@@ -26,7 +53,21 @@
                 return hiddenPanel.toLowerCase() === relatedModule.toLowerCase();
             }));
         }, this);
-        app.view.Layout.prototype._addComponentsFromDef.call(this, visibleSubpanels);
+        return visibleSubpanels;
+    },
+
+    /**
+     * We override this method which is called early in the Sidecar framework to prune any hidden or acl prohibited components.
+     * @param {Object} components The components
+     * @param {Object} context    Current context
+     * @param {String} module     Module
+     */
+    _addComponentsFromDef: function(components, context, module) {
+        // First checks for hidden components, then checks for ACLs on those components.
+        var allowedComponents = this._pruneHiddenComponents(components);
+        allowedComponents = this._pruneNoAccessComponents(allowedComponents);
+        // Call original Layout with pruned components
+        app.view.Layout.prototype._addComponentsFromDef.call(this, allowedComponents, context, module);
     },
 
     /**
@@ -40,7 +81,8 @@
             app.cache.set(cacheKey, linkName);
         }
         _.each(this._components, function(component) {
-            if(!linkName || linkName === component.context.get("link")) {
+            var link = component.context.get('link');
+            if(!linkName || linkName === link) {
                 component.context.set("hidden", false);
                 component.show();
             } else {
--- sugarcrm/tests/clients/base/layouts/subpanels/subpanels.js
+++ sugarcrm/tests/clients/base/layouts/subpanels/subpanels.js
@@ -1,7 +1,8 @@
 describe("Subpanels layout", function() {
-    var layout, app;
+    var layout, app, sinonSandbox;
 
     beforeEach(function() {
+        sinonSandbox = sinon.sandbox.create();
         SugarTest.testMetadata.init();
         SugarTest.loadComponent('base', 'layout', 'subpanels');
         SugarTest.testMetadata.set();
@@ -11,6 +12,7 @@ describe("Subpanels layout", function() {
     });
 
     afterEach(function() {
+        sinonSandbox.restore();
         layout.dispose();
         SugarTest.testMetadata.dispose();
     });
@@ -20,19 +22,17 @@ describe("Subpanels layout", function() {
 
         beforeEach(function() {
             //Mock sidecar calls
-            hiddenPanelsStub = sinon.stub(app.metadata, "getHiddenSubpanels", function(){
+            hiddenPanelsStub = sinonSandbox.stub(app.metadata, "getHiddenSubpanels", function(){
                 return {0: "bugs", 1: "contacts"};
             });
-            relationshipStub = sinon.stub(app.data, "getRelatedModule", function(module, linkName){
+            relationshipStub = sinonSandbox.stub(app.data, "getRelatedModule", function(module, linkName){
                 return linkName;  //return linkName as module name for test
             });
-            layoutPrototypeStub = sinon.stub(app.view.Layout.prototype, "_addComponentsFromDef", $.noop());
+            layoutPrototypeStub = sinonSandbox.stub(app.view.Layout.prototype, "_addComponentsFromDef", $.noop());
         });
 
         afterEach(function() {
-            hiddenPanelsStub.restore();
-            relationshipStub.restore();
-            layoutPrototypeStub.restore();
+            sinonSandbox.restore();
         });
 
         it('Should not add subpanel components for modules that are hidden in subpanels', function() {
@@ -48,27 +48,78 @@ describe("Subpanels layout", function() {
                 {context: {link: "cases"}, layout: "subpanel"},
                 {context: {link: "accounts"}, layout: "subpanel"}
             ];
+            function reset() {
+                hiddenPanelsStub.reset();
+                relationshipStub.reset();
+                layoutPrototypeStub.reset();
+            }
+            var returnedComponents = layout._pruneHiddenComponents(components);
+            expect(returnedComponents).toEqual(filteredComponents);
+            reset();
+            returnedComponents = layout._pruneHiddenComponents(filteredComponents);
+            expect(returnedComponents).toEqual(filteredComponents);
+            reset();
+            returnedComponents = layout._pruneHiddenComponents(hiddenComponent);
+            expect(returnedComponents).toEqual([]);
+        });
+
+        it('Should prune subpanels for which user has no access to', function() {
+            layout.model = {
+                fields: {
+                    'good': { module: 'GoodLink'},
+                    'bad': { module: 'BadLink'},
+                }
+            }
+            layout.aclToCheck = 'view';
+            var hasAccessStub = sinonSandbox.stub(app.acl, 'hasAccess', function(acl, link) {
+                return link === 'good' ? true : false;
+            });
+            var components = [
+                {context: {link: 'good'}},
+                {context: {link: 'bad'}}
+            ];
+            var actual = layout._pruneNoAccessComponents(components);
+            expect(actual.length).toEqual(1);
+            expect(actual[0].context.link).toEqual('good');
+            layout.model = null;//so we don't try to dispose bogus
+        });
+
+        it('Should hide hidden subpanels and also hide ACL forbidden subpanels', function() {
+            layout.model = {
+                fields: {
+                    'cases': { module: 'contacts'}, // ACL Forbidden
+                    'bugs': { module: 'bugs'},
+                    'accounts': { module: 'accounts'}
+                }
+            }
+            layout.aclToCheck = 'view';
+            var hasAccessStub = sinonSandbox.stub(app.acl, 'hasAccess', function(acl, link) {
+                return link === 'cases' ? false : true;
+            });
+            var components = [
+                {context: {link: "bugs"}, layout: "subpanel"},  //Should be hidden
+                {context: {link: "cases"}, layout: "subpanel"}, //Should be ACL forbidden
+                {context: {link: "accounts"}, layout: "subpanel"}
+            ];
+            var hiddenComponent = [
+                {context: {link: "bugs"}, layout: "subpanel"}
+            ];
+            var aclForbiddenComponent = [
+                {context: {link: "contacts"}, layout: "subpanel"}
+            ];
+            var filteredComponents = [
+                {context: {link: "accounts"}, layout: "subpanel"}
+            ];
+            function reset() {
+                hiddenPanelsStub.reset();
+                relationshipStub.reset();
+                layoutPrototypeStub.reset();
+            }
             layout._addComponentsFromDef(components);
             expect(layoutPrototypeStub.called).toBe(true);
             expect(layoutPrototypeStub.args[0][0]).toEqual(filteredComponents);
-
-            hiddenPanelsStub.reset();
-            relationshipStub.reset();
-            layoutPrototypeStub.reset();
-
-            layout._addComponentsFromDef(filteredComponents);
-            expect(layoutPrototypeStub.called).toBe(true);
-            expect(layoutPrototypeStub.args[0][0]).toEqual(filteredComponents);
-
-            hiddenPanelsStub.reset();
-            relationshipStub.reset();
-            layoutPrototypeStub.reset();
-
-            layout._addComponentsFromDef(hiddenComponent);
-            expect(layoutPrototypeStub.called).toBe(true);
-            expect(layoutPrototypeStub.args[0][0]).toEqual([]);
+            layout.model = null;//so we don't try to dispose bogus
         });
 
     });
-
 });
