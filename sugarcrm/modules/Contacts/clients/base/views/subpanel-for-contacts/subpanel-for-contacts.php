fe0c75a1071a0bed982f2c4c6984bfeeab47d8ba
Rob Levin <rlevin@sugarcrm.com>
2013-06-18 18:38:01 -0700

    SP-936: Properly using link URL for saving subpanel inline edits. Also, metadata was incorrect for full_name fields.

--- sugarcrm/modules/Contacts/clients/base/views/subpanel-for-contacts/subpanel-for-contacts.php
+++ sugarcrm/modules/Contacts/clients/base/views/subpanel-for-contacts/subpanel-for-contacts.php
@@ -12,15 +12,26 @@
  * Copyright  2004-2013 SugarCRM Inc.  All rights reserved.
  */
 $viewdefs['Contacts']['base']['view']['subpanel-for-contacts'] = array(
-  'panels' => 
-  array(
+  'panels' => array(
     array(
       'name' => 'panel_header',
       'label' => 'LBL_PANEL_1',
-      'fields' => 
-      array(
-        array(
-          'name' => 'name',
+      'fields' => array(
+       array(
+          'name' => 'full_name',
+          'type' => 'fieldset',
+          'fields' => array(
+              array(
+                  'name' => 'first_name',
+                  'link' => true,
+              ),
+              array(
+                  'name' => 'last_name',
+                  'link' => true,
+              )
+          ),
+          'css_class' => 'full-name',
+          'width' =>  49,
           'label' => 'LBL_LIST_NAME',
           'enabled' => true,
           'default' => true,
--- sugarcrm/modules/Products/clients/base/views/subpanel-list/subpanel-list.php
+++ sugarcrm/modules/Products/clients/base/views/subpanel-list/subpanel-list.php
@@ -12,18 +12,29 @@
  * Copyright  2004-2013 SugarCRM Inc.  All rights reserved.
  */
 $viewdefs['Products']['base']['view']['subpanel-list'] = array(
-  'panels' =>
-  array(
+  'panels' => array(
     array(
       'name' => 'panel_header',
       'label' => 'LBL_PANEL_1',
-      'fields' =>
-      array(
+      'fields' => array(
         array(
+          'name' => 'full_name',
+          'type' => 'fieldset',
+          'fields' => array(
+              array(
+                  'name' => 'first_name',
+                  'link' => true,
+              ),
+              array(
+                  'name' => 'last_name',
+                  'link' => true,
+              )
+          ),
+          'css_class' => 'full-name',
+          'width' =>  49,
           'label' => 'LBL_LIST_NAME',
           'enabled' => true,
           'default' => true,
-          'name' => 'name',
           'link' => 'true',
         ),
         array(
