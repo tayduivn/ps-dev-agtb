340e2fcd456e743b5aedc3500246a5ec2a8d39f7
Matthew Marum <mmarum@sugarcrm.com>
2013-06-25 10:55:45 -0400

    SP-734 SP-733 Remove Notes subpanel from Leads, Prospects

--- sugarcrm/modules/Prospects/clients/base/layouts/subpanels/subpanels.php
+++ sugarcrm/modules/Prospects/clients/base/layouts/subpanels/subpanels.php
@@ -3,13 +3,6 @@ $viewdefs['Prospects']['base']['layout']['subpanels'] = array (
   'components' => array (
     array (
       'layout' => 'subpanel',
-      'label' => 'LBL_NOTES_SUBPANEL_TITLE',
-      'context' => array (
-        'link' => 'notes',
-      ),
-    ),
-    array (
-      'layout' => 'subpanel',
       'label' => 'LBL_CAMPAIGN_LIST_SUBPANEL_TITLE',
       'context' => array (
         'link' => 'campaigns',
