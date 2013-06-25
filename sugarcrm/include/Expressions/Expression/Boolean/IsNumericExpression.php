3ef5dfc353f578c2dd2f58ff89ee592382423bb9
jwhitcraft <jwhitcraft@sugarcrm.com>
2013-06-25 14:32:00 -0400

    SFA-1234: Fix Notice being thrown
    - Fix the isNumeric check to return false when a empty string is passed in, the regex didn't catch this use case and was returning true, if the string is empty just return false
    - Made File be PSR-2 Compliant

--- sugarcrm/include/Expressions/Expression/Boolean/IsNumericExpression.php
+++ sugarcrm/include/Expressions/Expression/Boolean/IsNumericExpression.php
@@ -26,53 +26,68 @@ require_once("include/Expressions/Expression/Boolean/BooleanExpression.php");
  * negative sign, or a decimal point.
  *
  */
-class IsNumericExpression extends BooleanExpression {
-	/**
-	 * Returns itself when evaluating.
-	 */
-	function evaluate() {
-		$params = $this->getParameters()->evaluate();
-		if ( preg_match('/^(\-)?([0-9]+)?(\.[0-9]+)?$/', $params) )	return AbstractExpression::$TRUE;
-		return AbstractExpression::$FALSE;
-	}
+class IsNumericExpression extends BooleanExpression
+{
+    /**
+     * Returns itself when evaluating.
+     */
+    public function evaluate()
+    {
+        $params = $this->getParameters()->evaluate();
+        if ($params === '') {
+            return AbstractExpression::$FALSE;
+        }
+        if (preg_match('/^(\-)?([0-9]+)?(\.[0-9]+)?$/', $params)) {
+            return AbstractExpression::$TRUE;
+        }
 
-	/**
-	 * Returns the JS Equivalent of the evaluate function.
-	 */
-	static function getJSEvaluate() {
-		return <<<EOQ
+        return AbstractExpression::$FALSE;
+    }
+
+    /**
+     * Returns the JS Equivalent of the evaluate function.
+     */
+    public static function getJSEvaluate()
+    {
+        return <<<EOQ
 			var params = this.getParameters().evaluate();
+			if (params == '') {
+		        SUGAR.expressions.Expression.FALSE
+			}
 			if ( /^(\-)?([0-9]+)?(\.[0-9]+)?$/.test(params) )	return SUGAR.expressions.Expression.TRUE;
 			return SUGAR.expressions.Expression.FALSE;
 EOQ;
-	}
+    }
 
-	/**
-	 * Any generic type will suffice.
-	 */
-	static function getParameterTypes() {
-		return array("string");
-	}
+    /**
+     * Any generic type will suffice.
+     */
+    public static function getParameterTypes()
+    {
+        return array("string");
+    }
 
-	/**
-	 * Returns the maximum number of parameters needed.
-	 */
-	static function getParamCount() {
-		return 1;
-	}
+    /**
+     * Returns the maximum number of parameters needed.
+     */
+    public static function getParamCount()
+    {
+        return 1;
+    }
 
-	/**
-	 * Returns the opreation name that this Expression should be
-	 * called by.
-	 */
-	static function getOperationName() {
-		return "isNumeric";
-	}
+    /**
+     * Returns the opreation name that this Expression should be
+     * called by.
+     */
+    public static function getOperationName()
+    {
+        return "isNumeric";
+    }
 
-	/**
-	 * Returns the String representation of this Expression.
-	 */
-	function toString() {
-	}
+    /**
+     * Returns the String representation of this Expression.
+     */
+    public function toString()
+    {
+    }
 }
-?>
