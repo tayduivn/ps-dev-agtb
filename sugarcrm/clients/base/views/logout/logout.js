f4492096f6da7504b09d274ded4268dc2a9c4e55
Stanislav Malyshev <smalyshev@gmail.com>
2013-07-03 15:57:28 -0700

    add logout views

---
+++ sugarcrm/clients/base/views/logout/logout.js
@@ -0,0 +1,49 @@
+({
+    /**
+     * Login form view.
+     * @class View.Views.LoginView
+     * @alias SUGAR.App.view.views.LoginView
+     */
+    events: {
+        "click [name=login_button]": "login",
+    },
+    
+    /**
+     * Get the fields metadata from panels and declare a Bean with the metadata attached
+     * @param meta
+     * @private
+     */
+    _declareModel: function(meta) {
+        app.data.declareModel('Login');
+    },
+    
+    /**
+     * @override
+     * @param options
+     */
+    initialize: function(options) {
+        // Declare a Bean so we can process field validation
+        this._declareModel(options.meta);
+
+        // Reprepare the context because it was initially prepared without metadata
+        options.context.prepare(true);
+
+        app.view.View.prototype.initialize.call(this, options);
+    },
+
+    /**
+     * @override
+     * @private
+     */
+    _render: function() {
+        app.view.View.prototype._render.call(this);
+    },
+    
+    /**
+     * Process Login
+     */
+    login: function() {
+    	app.router.login();
+    }
+
+})
