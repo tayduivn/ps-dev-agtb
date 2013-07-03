0f7721e4a973924e502670acc6498ba0486baaf0
Neil Harding <nharding@sugarcrm.com>
2013-07-03 14:22:47 -0400

    MAR-1014 - Fix file field on import from vCard to be in edit mode so it doesn't re-render on data change

--- sugarcrm/clients/base/views/vcard-import/vcard-import.js
+++ sugarcrm/clients/base/views/vcard-import/vcard-import.js
@@ -25,11 +25,25 @@
  * by SugarCRM are Copyright (C) 2004-2012 SugarCRM, Inc.; All Rights Reserved.
  ********************************************************************************/
 ({
-    fallbackFieldTemplate: 'edit',
     initialize: function(options) {
         app.view.View.prototype.initialize.call(this, options);
         this.context.off("vcard:import:finish", null, this);
         this.context.on("vcard:import:finish", this.importVCard, this);
+    },
+
+    /**
+     * {@inheritdocs}
+     *
+     * Sets up the file field to edit mode
+     *
+     * @param {View.Field} field
+     * @private
+     */
+    _renderField: function(field) {
+        app.view.View.prototype._renderField.call(this, field);
+        if (field.name === 'vcard_import') {
+            field.setMode('edit');
+        }
     },
 
     importVCard: function() {
