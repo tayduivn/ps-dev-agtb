3d55cd484d756dceff94ab408bb27dc1ca37aa70
Henry Rogers <henry@hhrogersii.com>
2013-07-12 11:35:26 -0400

    UIUX-876: remove unneded initialization function

--- sugarcrm/modules/Styleguide/clients/base/layouts/docs/docs.js
+++ sugarcrm/modules/Styleguide/clients/base/layouts/docs/docs.js
@@ -1,13 +1,6 @@
 ({
     loadPrettyPrint: true,
 
-    initialize: function(options) {
-        app.view.Layout.prototype.initialize.call(this, options);
-        if (window.prettyPrint) {
-            this.loadPrettyPrint = false;
-        }
-    },
-
     _placeComponent: function(component) {
         this.$('#styleguide').append(component.$el);
     },
