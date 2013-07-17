0e30f1db4d7f00c0102fa2f68e8a2d69e4ce247a
Rob Levin <rlevin@sugarcrm.com>
2013-07-17 16:54:57 -0700

    Conversion for portal layout metadata per SP-886

--- sugarcrm/modules/KBDocuments/clients/portal/layouts/record/record.php
+++ sugarcrm/modules/KBDocuments/clients/portal/layouts/record/record.php
@@ -1,5 +1,59 @@
 <?php
-$layout = MetaDataManager::getLayout('SideBarLayout');
-$layout->push('main', array('view'=>'record'));
-$layout->push('preview', array('layout' => 'preview'));
-$viewdefs['KBDocuments']['portal']['layout']['record'] = $layout->getLayout();
+$viewdefs['KBDocuments']['portal']['layout']['record'] = array (
+  'components' => array (
+    array (
+      'layout' => array (
+        'components' => array (
+          array (
+            'layout' => array (
+              'components' => array (
+                array (
+                  'view' => 'record',
+                ),
+              ),
+              'type' => 'simple',
+              'name' => 'main-pane',
+              'span' => 8,
+            ),
+          ),
+          array (
+            'layout' => array (
+              'components' => array (
+              ),
+              'type' => 'simple',
+              'name' => 'side-pane',
+              'span' => 4,
+            ),
+          ),
+          array (
+            'layout' => array (
+              'components' => array (
+              ),
+              'type' => 'simple',
+              'name' => 'dashboard-pane',
+              'span' => 4,
+            ),
+          ),
+          array (
+            'layout' => array (
+              'components' => array (
+                array (
+                  'layout' => 'preview',
+                ),
+              ),
+              'type' => 'simple',
+              'name' => 'preview-pane',
+              'span' => 8,
+            ),
+          ),
+        ),
+        'type' => 'default',
+        'name' => 'sidebar',
+        'span' => 12,
+      ),
+    ),
+  ),
+  'type' => 'simple',
+  'name' => 'base',
+  'span' => 12,
+);
