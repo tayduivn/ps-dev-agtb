ffa1903cb102bd6e226ec40d8930d6b9e53bd624
Matthew Marum <mmarum@sugarcrm.com>
2013-07-25 17:42:28 -0400

    SP-1148: Add setup complete page to user setup wizard SP-1109: Redirect users with user wizard flag set after login
    Conflicts:
    
    	sugarcrm/clients/base/layouts/first-login-wizard/first-login-wizard.php
    
    SP-1109: Updates from Rob's code review
    
    SP-1109: Updates to pass wizard name for work on SP-1149
    
    Refactored so user.show_wizard is simple boolean. user.type will change from 'user' to 'admin' if isAdmin true.
    
    Conflicts:
    
    	sugarcrm/clients/base/layouts/first-login-wizard/first-login-wizard.php
    	sugarcrm/clients/base/views/wizard-page/wizard-page.hbs
    	sugarcrm/include/language/en_us.lang.php
    
    Using is_instance_configured property to determine if user has configured instance and thus whether to show first login wizard or not.
    
    Fix test breakage
    
    Returning hash on PUT for user preferences

--- sugarcrm/include/language/en_us.lang.php
+++ sugarcrm/include/language/en_us.lang.php
@@ -3933,6 +3933,19 @@ $app_strings = array (
     'LBL_WIZ_TIMEFORMAT' => 'Time Format',
     'LBL_WIZ_DATE_FORMAT' => 'Date Format',
     'LBL_WIZ_NAME_FORMAT' => 'Name Format',
+    'LBL_SETUP_PROGRESS' => 'Setup progress',
+    //User wizard links
+    'LBL_WIZ_SUGAR_TRAINING' => 'Sugar Training',
+    'LBL_WIZ_DOCUMENTATION' => 'Documentation',
+    'LBL_WIZ_KNOWLEDGE_BASE' => 'Knowledge Base',
+    'LBL_WIZ_FORUMS' => 'Forums',
+    //Admin wizard links
+    'LBL_WIZ_SYSTEM_LOCALE_SETTINGS' => 'System Locale Settings',
+    'LBL_WIZ_EMAIL_SETTINGS' => 'Email Settings',
+    'LBL_WIZ_IMPORT_DATA' => 'Import Data',
+    'LBL_WIZ_CREATE_USERS' => 'Create Users',
+    'LBL_WIZ_APPLICATION_SETTINGS' => 'View and Manage Application Settings',
+    'LBL_WIZ_CONFIGURE_APPLICATION' => 'Configure the Application',
 
     // For passwordmodal widget
     'LBL_PASSWORD'						=> 'Password',
