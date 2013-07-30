890e338537619d021adfa77c6e213b5e240c8010
Filipe Guerra <alias.mac@gmail.com>
2013-07-30 07:23:40 +0200

    Enable share feature on some BWC modules (related with SC-1291)
    Modules:
    - Calls
    - Meetings
    - Reports
    - Quotes

--- sugarcrm/modules/Calls/metadata/detailviewdefs.php
+++ sugarcrm/modules/Calls/metadata/detailviewdefs.php
@@ -35,10 +35,10 @@ array (
       'form' => array (
         'buttons' => 
         array (
-          0 => 'EDIT',
-          1 => 'DUPLICATE',
-          2 => 'DELETE',
-          3 => 
+          'EDIT',
+          'SHARE',
+          'DUPLICATE',
+          'DELETE',
           array (
             'customCode' => '{if $fields.status.value != "Held" && $bean->aclAccess("edit")} <input type="hidden" name="isSaveAndNew" value="false">  <input type="hidden" name="status" value="">  <input type="hidden" name="isSaveFromDetailView" value="true">  <input title="{$APP.LBL_CLOSE_AND_CREATE_BUTTON_TITLE}"   class="button"  onclick="this.form.status.value=\'Held\'; this.form.action.value=\'Save\';this.form.return_module.value=\'Calls\';this.form.isDuplicate.value=true;this.form.isSaveAndNew.value=true;this.form.return_action.value=\'EditView\'; this.form.return_id.value=\'{$fields.id.value}\'" id="close_create_button" name="button"  value="{$APP.LBL_CLOSE_AND_CREATE_BUTTON_TITLE}"  type="submit">{/if}',
               //Bug#51778: The custom code will be replaced with sugar_html. customCode will be deplicated.
@@ -56,7 +56,6 @@ array (
               ),
 
           ),
-          4 => 
           array (
             'customCode' => '{if $fields.status.value != "Held" && $bean->aclAccess("edit")} <input type="hidden" name="isSave" value="false">  <input title="{$APP.LBL_CLOSE_BUTTON_TITLE}"  accesskey="{$APP.LBL_CLOSE_BUTTON_KEY}"  class="button"  onclick="this.form.status.value=\'Held\'; this.form.action.value=\'Save\';this.form.return_module.value=\'Calls\';this.form.isSave.value=true;this.form.return_action.value=\'DetailView\'; this.form.return_id.value=\'{$fields.id.value}\'" id="close_button" name="button1"  value="{$APP.LBL_CLOSE_BUTTON_TITLE}"  type="submit">{/if}',
               //Bug#51778: The custom code will be replaced with sugar_html. customCode will be deplicated.
