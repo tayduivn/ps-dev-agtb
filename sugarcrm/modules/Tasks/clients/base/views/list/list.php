c65a95abda2e9a3523bf1fbd836230301efa7395
Filipe Guerra <alias.mac@gmail.com>
2013-08-01 05:15:54 +0200

    Remove Due Time field on lists (fixes SC-1567)

--- sugarcrm/modules/Tasks/clients/base/views/list/list.php
+++ sugarcrm/modules/Tasks/clients/base/views/list/list.php
@@ -1,32 +1,17 @@
 <?php
-if(!defined('sugarEntry') || !sugarEntry) die('Not A Valid Entry Point');
-/**
- * LICENSE: The contents of this file are subject to the SugarCRM Professional
- * End User License Agreement ("License") which can be viewed at
- * http://www.sugarcrm.com/EULA.  By installing or using this file, You have
- * unconditionally agreed to the terms and conditions of the License, and You
- * may not use this file except in compliance with the License.  Under the
- * terms of the license, You shall not, among other things: 1) sublicense,
- * resell, rent, lease, redistribute, assign or otherwise transfer Your
- * rights to the Software, and 2) use the Software for timesharing or service
- * bureau purposes such as hosting the Software for commercial gain and/or for
- * the benefit of a third party.  Use of the Software may be subject to
- * applicable fees and any use of the Software without first paying applicable
- * fees is strictly prohibited.  You do not have the right to remove SugarCRM
- * copyrights from the source code or user interface.
- *
- * All copies of the Covered Code must include on each user interface screen:
- *  (i) the "Powered by SugarCRM" logo and
- *  (ii) the SugarCRM copyright notice
- * in the same form as they appear in the distribution.  See full license for
- * requirements.
- *
- * Your Warranty, Limitations of liability and Indemnity are expressly stated
- * in the License.  Please refer to the License for the specific language
- * governing these rights and limitations under the License.  Portions created
- * by SugarCRM are Copyright (C) 2006 SugarCRM, Inc.; All Rights Reserved.
- */
 
+/*
+ * By installing or using this file, you are confirming on behalf of the entity
+ * subscribed to the SugarCRM Inc. product ("Company") that Company is bound by
+ * the SugarCRM Inc. Master Subscription Agreement (“MSA”), which is viewable at:
+ * http://www.sugarcrm.com/master-subscription-agreement
+ *
+ * If Company is not bound by the MSA, then by installing or using this file
+ * you are agreeing unconditionally that Company will be bound by the MSA and
+ * certifying that you have authority to bind Company accordingly.
+ *
+ * Copyright  2004-2013 SugarCRM Inc.  All rights reserved.
+ */
 
 $viewdefs['Tasks']['base']['view']['list'] = array(
     'panels' =>
@@ -37,7 +22,7 @@ $viewdefs['Tasks']['base']['view']['list'] = array(
             array(
                 array(
                     'name' => 'name',
-                    'width' =>  40,
+                    'width' => 40,
                     'link' => true,
                     'label' => 'LBL_LIST_SUBJECT',
                     'enabled' => true,
@@ -45,8 +30,8 @@ $viewdefs['Tasks']['base']['view']['list'] = array(
                 ),
                 array(
                     'name' => 'contact_name',
-                    'width' => '20', 
-                    'label' => 'LBL_LIST_CONTACT', 
+                    'width' => '20',
+                    'label' => 'LBL_LIST_CONTACT',
                     'link' => true,
                     'id' => 'CONTACT_ID',
                     'module' => 'Contacts',
@@ -58,11 +43,11 @@ $viewdefs['Tasks']['base']['view']['list'] = array(
                 ),
                 array(
                     'name' => 'parent_name',
-                    'width'   => '20',
-                    'label'   => 'LBL_LIST_RELATED_TO',
+                    'width' => '20',
+                    'label' => 'LBL_LIST_RELATED_TO',
                     'dynamic_module' => 'PARENT_TYPE',
                     'id' => 'PARENT_ID',
-                    'link' => true, 
+                    'link' => true,
                     'enabled' => true,
                     'default' => true,
                     'sortable' => false,
@@ -71,17 +56,8 @@ $viewdefs['Tasks']['base']['view']['list'] = array(
                 ),
                 array(
                     'name' => 'date_due',
-                    'width' => '15', 
-                    'label' => 'LBL_LIST_DUE_DATE', 
-                    'link' => false,
-                    'enabled' => true,
-                    'default' => true,
-                ),
-                array(
-                    'name' => 'time_due',
-                    'width' => '15', 
-                    'label' => 'LBL_LIST_DUE_TIME', 
-                    'sortable' => false, 
+                    'width' => '15',
+                    'label' => 'LBL_LIST_DUE_DATE',
                     'link' => false,
                     'enabled' => true,
                     'default' => true,
@@ -89,7 +65,7 @@ $viewdefs['Tasks']['base']['view']['list'] = array(
                 //BEGIN SUGARCRM flav=pro ONLY
                 array(
                     'name' => 'team_name',
-                    'width' => '2', 
+                    'width' => '2',
                     'label' => 'LBL_LIST_TEAM',
                     'enabled' => true,
                     'default' => false,
@@ -98,7 +74,7 @@ $viewdefs['Tasks']['base']['view']['list'] = array(
                 //END SUGARCRM flav=pro ONLY
                 array(
                     'name' => 'assigned_user_name',
-                    'width' => '2', 
+                    'width' => '2',
                     'label' => 'LBL_LIST_ASSIGNED_TO_NAME',
                     'id' => 'ASSIGNED_USER_ID',
                     'enabled' => true,
@@ -107,16 +83,16 @@ $viewdefs['Tasks']['base']['view']['list'] = array(
                 ),
                 array(
                     'name' => 'date_start',
-                    'width' => '5', 
-                    'label' => 'LBL_LIST_START_DATE', 
+                    'width' => '5',
+                    'label' => 'LBL_LIST_START_DATE',
                     'link' => false,
                     'enabled' => true,
                     'default' => false,
                 ),
                 array(
                     'name' => 'status',
-                    'width' => '10', 
-                    'label' => 'LBL_LIST_STATUS', 
+                    'width' => '10',
+                    'label' => 'LBL_LIST_STATUS',
                     'link' => false,
                     'enabled' => true,
                     'default' => false,
