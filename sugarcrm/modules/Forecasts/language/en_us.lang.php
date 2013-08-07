bd6f1c47f049c691dc49d5f821834f150c9d8fd7
Lam Huynh <lam@sugarcrm.com>
2013-08-06 23:40:29 -0500

    additional lang strings added

--- sugarcrm/modules/Forecasts/language/en_us.lang.php
+++ sugarcrm/modules/Forecasts/language/en_us.lang.php
@@ -427,6 +427,8 @@ $mod_strings = array (
     // forecast details dashlet
     'LBL_DASHLET_FORECAST_NOT_SETUP' => 'Forecasts has not been configured and needs to be setup in order to use this widget. Please contact your system administrator.',
     'LBL_DASHLET_FORECAST_NOT_SETUP_ADMIN' => 'Forecasts has not been configured and needs to be setup in order to use this widget. <a href="#Forecasts/config">Please click here to configure Forecasting</a>.',
+    'LBL_DASHLET_MY_PIPELINE' => 'My Pipeline',
+    'LBL_DASHLET_MY_TEAMS_PIPELINE' => 'My Teams Pipeline',
     'LBL_FORECAST_DETAILS_DEFICIT' => 'Deficit',
     'LBL_FORECAST_DETAILS_SURPLUS' => 'Surplus',
     'LBL_FORECAST_DETAILS_SHORT' => 'Short by',
