5e6c17e00358166bda5d2232fa85a9e1969a7ad5
justinpark <jpark@sugarcrm.com>
2013-08-07 12:04:37 -0700

    [SC-1589]Ability to reuse dashletaction 'action' with different parameters

--- sugarcrm/clients/base/fields/dashletaction/dashletaction.js
+++ sugarcrm/clients/base/fields/dashletaction/dashletaction.js
@@ -4,15 +4,34 @@
     },
     extendsFrom: 'ButtonField',
     /**
-     * Trigger the function which is in the dashlet view
+     * Trigger the function which is in the dashlet view.
      *
-     * @param {Event} Mouse event.
+     * @param {Event} evt Mouse event.
      */
     actionClicked: function(evt) {
         if (this.preventClick(evt) === false) {
             return;
         }
         var action = $(evt.currentTarget).data('dashletaction');
-        this.view.trigger('dashletaction', action, evt, this.def.params);
+        this._runAction(evt, action);
+    },
+
+    /**
+     * Handles rowaction's event trigger and propagate the event to the main dashlet.
+     *
+     * @param {Event} evt Mouse event.
+     * @param {String} action Name of executing parent action.
+     * @protected
+     */
+    _runAction: function(evt, action) {
+        if (!action) {
+            return;
+        }
+        var dashlet = this.view.layout ? _.first(this.view.layout._components) : null;
+        if (dashlet && _.isFunction(dashlet[action])) {
+            dashlet[action](evt, this.def.params);
+        } else if (_.isFunction(this.view[action])) {
+            this.view[action](evt, this.def.params);
+        }
     }
 })
