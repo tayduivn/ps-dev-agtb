8b6ca33596e78b4aa4ccafbe85c98551ee69842c
Greg Levine <glevine@sugarcrm.com>
2013-08-09 15:37:20 -0400

    jira: MAR-1053 Attaching a file actually produces more than one activity. A client callback creates an activity is created for the file, which is accessible in the client and can be prepended to the Activity Stream. However, a create activity is created on the server for the Note that is related to the file, but that activity does not come back to the client. Have to re-fetch the activities to get all of the expected activities.

--- sugarcrm/include/javascript/sugar7/plugins/dragdrop_attachments.js
+++ sugarcrm/include/javascript/sugar7/plugins/dragdrop_attachments.js
@@ -160,7 +160,7 @@
                                 var errorMessage = app.lang.getAppString('LBL_EMAIL_ATTACHMENT_UPLOAD_FAILED');
                                 app.alert.show('upload_error', errorMessage);
                             } else {
-                                self.context.trigger('activitystream:post:prepend', activity);
+                                self.context.reloadData({recursive: false});
                                 self.clearAttachments.call(self);
                             }
                         });
