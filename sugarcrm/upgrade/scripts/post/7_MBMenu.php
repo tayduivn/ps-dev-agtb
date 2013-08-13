1f434ca306fc83d3df0647ac669cf41815a1f5a1
Stanislav Malyshev <smalyshev@gmail.com>
2013-08-12 23:22:49 -0700

    test & fixes

--- sugarcrm/upgrade/scripts/post/7_MBMenu.php
+++ sugarcrm/upgrade/scripts/post/7_MBMenu.php
@@ -7,49 +7,68 @@ class SugarUpgradeMBMenu extends UpgradeScript
 {
     public $order = 7200;
 
+    /**
+     * Add default menu for module
+     * @param string $module
+     */
+    protected function addMenu($moduleName)
+    {
+        $menu = array();
+        // Create default menu for the module
+        $menu[] = array(
+                'route' => "#$moduleName/create",
+                'label' => 'LNK_NEW_RECORD',
+                'acl_action' => 'create',
+                'acl_module' => $moduleName,
+                'icon' => 'icon-plus',
+        );
+        $menu[] = array(
+                'route' => "#$moduleName",
+                'label' => 'LNK_LIST',
+                'acl_action' => 'list',
+                'acl_module' => $moduleName,
+                'icon' => 'icon-reorder',
+        );
+        $menu[] = array(
+                'route' => '#bwc/index.php?' . http_build_query(
+                        array(
+                                'module' => 'Import',
+                                'action' => 'Step1',
+                                'import_module' => $moduleName,
+                        )
+                ),
+                'label' => 'LNK_IMPORT_'.strtoupper($moduleName),
+                'acl_action' => 'import',
+                'acl_module' => $moduleName,
+                'icon' => 'icon-upload-alternative',
+        );
+        $content = <<<END
+<?php
+/* Created by SugarUpgrader for module $moduleName */
+\$viewdefs['$moduleName']['base']['menu']['header'] =
+END;
+        $content .= var_export($menu, true) . ";\n";
+        $this->ensureDir("modules/$moduleName/clients/base/menus/header");
+        $this->putFile("modules/$moduleName/clients/base/menus/header/header.php", $content);
+        $this->log("Added default menu file for $moduleName");
+    }
+
     public function run()
     {
         if(empty($this->upgrader->state['MBModules'])) return;
 
         foreach($this->upgrader->state['MBModules'] as $moduleName) {
+            if(!file_exists("modules/$moduleName")) continue;
             if(!file_exists("modules/$moduleName/clients/base/menus/header/header.php") && !file_exists("custom/modules/$moduleName/clients/base/menus/header/header.php")) {
-                $menu = array();
-                // Create default menu for the module
-                $menu[] = array(
-                    'route' => "#$moduleName/create",
-                    'label' => 'LNK_NEW_RECORD',
-                    'acl_action' => 'create',
-                    'acl_module' => $moduleName,
-                    'icon' => 'icon-plus',
-                );
-                $menu[] = array(
-                    'route' => "#$moduleName",
-                    'label' => 'LNK_LIST',
-                    'acl_action' => 'list',
-                    'acl_module' => $moduleName,
-                    'icon' => 'icon-reorder',
-                );
-                $menu[] = array(
-                    'route' => '#bwc/index.php?' . http_build_query(
-                        array(
-                            'module' => 'Import',
-                            'action' => 'Step1',
-                            'import_module' => $moduleName,
-                        )
-                    ),
-                    'label' => 'LBL_IMPORT',
-                    'acl_action' => 'import',
-                    'acl_module' => $moduleName,
-                    'icon' => 'icon-upload-alternative',
-                );
-                $content = <<<END
-<?php
-/* Created by SugarUpgrader for module $moduleName */
-\$viewdefs['$moduleName']['base']['menu']['header'] =
-END;
-                $content = var_export($menu, true);
-                $this->putFile("modules/$moduleName/clients/base/menus/header/header.php", $content);
-                $this->log("Added menu file for $moduleName");
+                $this->addMenu($moduleName);
+            }
+        }
+
+        // Do it also for bwcModules since some of them may not have Menu.php and we need it
+        foreach ($GLOBALS['bwcModules'] as $moduleName) {
+            if(!file_exists("modules/$moduleName")) continue;
+            if(!file_exists("modules/$moduleName/clients/base/menus/header/header.php") && !file_exists("custom/modules/$moduleName/clients/base/menus/header/header.php")) {
+                $this->addMenu($moduleName);
             }
         }
     }
