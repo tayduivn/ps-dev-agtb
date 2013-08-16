0a79b0fdb8deaa2ddcbe9d6553fd68132b5bfbb9
Lam Huynh <lam@sugarcrm.com>
2013-08-16 02:30:51 -0500

    UIUX-988 added tooltips to quick create in nav

--- sugarcrm/clients/base/views/quickcreate/quickcreate.js
+++ sugarcrm/clients/base/views/quickcreate/quickcreate.js
@@ -26,7 +26,7 @@
  * by SugarCRM are Copyright (C) 2004-2012 SugarCRM, Inc.; All Rights Reserved.
  ********************************************************************************/
 ({
-    plugins: ['dropdown'],
+    plugins: ['dropdown','tooltip'],
     events: {
       'click .dropdown-toggle':'toggleDropdown'
     },
