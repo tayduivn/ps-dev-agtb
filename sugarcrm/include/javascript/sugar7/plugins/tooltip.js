1820e53bca5efb42e2b577ff8261d93c04ccac05
justinpark <jpark@sugarcrm.com>
2013-08-27 17:41:41 -0700

    [SC-1704]Hotfix for memory leak on tooltip plugin

--- sugarcrm/include/javascript/sugar7/plugins/tooltip.js
+++ sugarcrm/include/javascript/sugar7/plugins/tooltip.js
@@ -1,5 +1,5 @@
-(function (app) {
-    app.events.on('app:init', function () {
+(function(app) {
+    app.events.on('app:init', function() {
         app.plugins.register('tooltip', ['view'], {
             events: {
                 'mouseenter [rel="tooltip"]': 'onShowRecordViewTooltip',
@@ -15,41 +15,30 @@
             },
 
             /**
-             * List of DOM elements for tip box.
-             * Keep this dom in order to remove safe on dispose.
-             *
-             * @property
-             */
-            _tooltipElements: [],
-
-            /**
              * Creates tooltips for all controls on the record view and shows them
              * @param {Window.Event} mouse event.
              */
             onShowRecordViewTooltip: function(e) {
                 if (_.isFunction(this.$(e.currentTarget).tooltip)) {
-                    this.$(e.currentTarget).tooltip().tooltip('show');
-                    this._tooltipElements.push(this.$(e.currentTarget).data('tooltip').$tip);
+                    this.$(e.currentTarget).tooltip({}).tooltip('show');
                 }
             },
-
             /**
              * Hides tooltips for all controls on the record view and destroy them
              * @param {Window.Event} mouse event.
              */
             onHideRecordViewTooltip: function(e) {
                 if (_.isFunction(this.$(e.currentTarget).tooltip)) {
-                    this.$(e.currentTarget).tooltip('hide');
+                    this.$(e.currentTarget).tooltip('destroy');
                 }
             },
 
+            /**
+             * Destroy the jQuery tooltip plugin.
+             */
             unbindTooltip: function() {
                 if (_.isFunction(this.$el.tooltip)) {
                     this.$('[rel=tooltip]').tooltip('destroy');
-                    _.each(this._tooltipElements, function($el) {
-                        $el.remove();
-                    }, this);
-                    this._tooltipElements = [];
                 }
             },
 
