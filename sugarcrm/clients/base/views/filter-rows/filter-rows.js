5306c81c3a71660e0c2108923c4b00e59d10b940
Filipe Guerra <alias.mac@gmail.com>
2013-09-04 06:37:52 +0200

    Revert "SP-1377 remove empty filter value on filter create"
    This reverts commit 6ac0a8f2b776499d4b3d091af5ca50301d0d0ea5.

--- sugarcrm/clients/base/views/filter-rows/filter-rows.js
+++ sugarcrm/clients/base/views/filter-rows/filter-rows.js
@@ -59,7 +59,8 @@
         }
         this.fieldList = this.getFilterableFields(moduleName);
 
-        this.filterFields = {};
+        // This is the Select2 data for the enum field. 1st value must be blank.
+        this.filterFields = {"": ""};
         this.moduleName = moduleName;
 
         _.each(this.fieldList, function(value, key) {
@@ -375,7 +376,7 @@
 
         // Get operators for this filter type
         var fieldType = this.fieldTypeMap[this.fieldList[fieldName].type] || this.fieldList[fieldName].type,
-            payload = {},
+            payload = {"": ""},
             types = _.keys(this.filterOperatorMap[fieldType]);
 
         $fieldWrapper.removeClass('hide').empty();
