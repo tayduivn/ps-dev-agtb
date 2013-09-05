1358df7179c1da59f70cd5454d1254ee66cae615
timwolf <twolf@sugarcrm.com>
2013-09-05 15:53:19 -0400

    Jira Mar-1223 Removed hard-coded module map assignments as all were redundant except one, and that one 'ProjectTasks' was causing an invalid index exception because the 'Projects' module has been removed.  There is no reason to be hardcoding additions to the module map for workflow,  and any future need to extend the workflow module map should be done in a more thoughtful and more strategic way.

--- sugarcrm/include/utils.php
+++ sugarcrm/include/utils.php
@@ -1561,15 +1561,6 @@ function displayWorkflowForCurrentUser()
     foreach ($moduleList as $module) {
         $workflow_mod_list[$module] = $module;
     }
-    // This list is taken from the previous version of workflow_utils.php
-    $workflow_mod_list['Tasks'] = "Tasks";
-    $workflow_mod_list['Calls'] = "Calls";
-    $workflow_mod_list['Meetings'] = "Meetings";
-    $workflow_mod_list['Notes'] = "Notes";
-    $workflow_mod_list['ProjectTask'] = "Project Tasks";
-    $workflow_mod_list['Leads'] = "Leads";
-    $workflow_mod_list['Opportunities'] = "Opportunities";
-    // End of list
 
     $access = get_workflow_admin_modules_for_user($GLOBALS['current_user']);
     foreach ($access as $key=>$val) {
@@ -1611,16 +1602,6 @@ function get_admin_modules_for_user($user)
     foreach ($moduleList as $module) {
         $workflow_mod_list[$module] = $module;
     }
-
-    // This list is taken from teh previous version of workflow_utils.php
-    $workflow_mod_list['Tasks'] = "Tasks";
-    $workflow_mod_list['Calls'] = "Calls";
-    $workflow_mod_list['Meetings'] = "Meetings";
-    $workflow_mod_list['Notes'] = "Notes";
-    $workflow_mod_list['ProjectTask'] = "Project Tasks";
-    $workflow_mod_list['Leads'] = "Leads";
-    $workflow_mod_list['Opportunities'] = "Opportunities";
-    // End of list
 
     $workflow_admin_modules = array();
     if (empty($user)) {
@@ -2771,11 +2752,6 @@ function parse_list_modules(&$listArray)
             $returnArray['ProductTemplates'] = $listArray['ProductTemplates'];
         }
         //END SUGARCRM flav=pro ONLY
-
-        // special case for projects
-        if (array_key_exists('Project', $modListHeader)) {
-            $returnArray['ProjectTask'] = $listArray['ProjectTask'];
-        }
     }
     $returnArray = SugarACL::filterModuleList($listArray, 'access', true);
     asort($returnArray);
@@ -3630,10 +3606,6 @@ function convert_module_to_singular($module_array)
 
         if ($value=="Cases") {
             $module_array[$key] = "Case";
-        }
-        if ($key=="projecttask") {
-            $module_array['ProjectTask'] = "Project Task";
-            unset($module_array[$key]);
         }
     }
 
