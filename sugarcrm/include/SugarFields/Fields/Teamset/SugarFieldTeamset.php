0208f65bcc401f664a3983b0e20c3b5db46ba405
Robert Gonzalez <rgonzalez@sugarcrm.com>
2013-09-18 14:41:52 -0700

    [BR-633] Warning message generate after add a Team field in advance search layout Remove uninstantiated object property that was causing warnings on a specific stack

--- sugarcrm/include/SugarFields/Fields/Teamset/SugarFieldTeamset.php
+++ sugarcrm/include/SugarFields/Fields/Teamset/SugarFieldTeamset.php
@@ -358,7 +358,6 @@ class SugarFieldTeamset extends SugarFieldBase {
 		if(empty($displayParams['formName'])){
 			$displayParams['formName'] = 'search_form';
 		}
-    	$this->view->displayParams = $displayParams;
     	$this->setup($parentFieldArray, $vardef, $displayParams, $tabindex);
     	$this->ss->assign('renderView', 'renderSearchView');
 		return $this->fetch($this->findTemplate('Teamset'));
