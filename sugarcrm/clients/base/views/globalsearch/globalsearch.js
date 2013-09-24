0b38a45bbda54052599c2dcbf3cc4ddbc5b92556
Joo Lee <jlee@sugarcrm.com>
2013-09-24 19:46:19 -0400

    MAR-1309: Reimplemented the dropdown plugin to use the Twitter Bootstrap implementation, rather than our own. The plugin now just enhances the default behavior by hiding the dropdown menu on mouseleave.

--- sugarcrm/clients/base/views/globalsearch/globalsearch.js
+++ sugarcrm/clients/base/views/globalsearch/globalsearch.js
@@ -1,3 +1,15 @@
+/*
+ * By installing or using this file, you are confirming on behalf of the entity
+ * subscribed to the SugarCRM Inc. product ("Company") that Company is bound by
+ * the SugarCRM Inc. Master Subscription Agreement (“MSA”), which is viewable at:
+ * http://www.sugarcrm.com/master-subscription-agreement
+ *
+ * If Company is not bound by the MSA, then by installing or using this file
+ * you are agreeing unconditionally that Company will be bound by the MSA and
+ * certifying that you have authority to bind Company accordingly.
+ *
+ * Copyright  2004-2013 SugarCRM Inc.  All rights reserved.
+ */
 ({
     // FIXME this needs to be removed so that we can be able to reuse this view
     id: 'searchForm',
@@ -10,13 +22,7 @@
         'click .typeahead a': 'clearSearch',
         'click [data-action=search]': 'showResults',
         'click [data-advanced=options]': 'persistMenu',
-        'click [data-action="select-module"]': 'selectModule',
-        'click .dropdown-toggle': 'toggleDropdown'
-    },
-    // FIXME remove this code
-    toggleDropdown: function(event) {
-        var $currentTarget = this.$(event.currentTarget);
-        this.toggleDropdownHTML($currentTarget);
+        'click [data-action="select-module"]': 'selectModule'
     },
     initialize: function(options) {
         app.view.View.prototype.initialize.call(this, options);
