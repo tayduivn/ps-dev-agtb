692b389bd6c3f2cd2bbcbfb9038cd9402c7897bb
timwolf <twolf@sugarcrm.com>
2013-09-25 18:06:36 -0400

    Jira MAR-1340 Created Message set for Add to Target List mass action progress bar. Extended massupdate-progess view  to enable  the newly created message set for  Add To Target List mass action. Renamed  LBL_MASS_ADD_TO_LIST_   Labels  containing  HandleBar variables  to TPL_MASS_ADD_TO_LIST_.

---
+++ sugarcrm/clients/base/views/massaddtolist-progress/massaddtolist-progress.js
@@ -0,0 +1,21 @@
+({
+    extendsFrom: 'MassupdateProgressView',
+
+    /**
+     * Set of labels.
+     *
+     * @property
+     */
+    _labelSet: {
+        'update': {
+            PROGRESS_STATUS: 'TPL_MASSADDTOLIST_PROGRESS_STATUS',
+            DURATION_FORMAT: 'TPL_MASSADDTOLIST_DURATION_FORMAT',
+            FAIL_TO_ATTEMPT: 'TPL_MASSADDTOLIST_FAIL_TO_ATTEMPT',
+            WARNING_CLOSE: 'TPL_MASSADDTOLIST_WARNING_CLOSE',
+            WARNING_INCOMPLETE: 'TPL_MASSADDTOLIST_WARNING_INCOMPLETE',
+            SUCCESS: 'TPL_MASSADDTOLIST_SUCCESS',
+            TITLE: 'TPL_MASSADDTOLIST_TITLE'
+        }
+    }
+
+})
