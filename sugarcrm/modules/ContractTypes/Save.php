8e70a901919a0b002098817dc4d6d42ebb9de24c
Matthew Marum <mmarum@sugarcrm.com>
2013-09-26 15:57:25 -0700

    SP-1612: Fix redirect on save of a ContractType

--- sugarcrm/modules/ContractTypes/Save.php
+++ sugarcrm/modules/ContractTypes/Save.php
@@ -56,8 +56,7 @@ foreach($focus->additional_column_fields as $field) {
 }
 $return_id=$focus->save($check_notify);
 $GLOBALS['log']->debug("Saved record with id of ".$return_id);
-$location="Location: index.php?action=".$_REQUEST['return_action']."&module=".$_REQUEST['return_module']."&record=".$return_id;
-if (isset($_REQUEST['edit'])) $location.="&edit=".$_REQUEST['edit'];
-if (isset($_REQUEST['isDuplicate'])) $location.="&isDuplicate=".$_REQUEST['isDuplicate'];
-
-handleRedirect($return_id, $_REQUEST['return_module']);
+$url = buildRedirectURL($return_id, 'ContractTypes');
+if (isset($_REQUEST['edit'])) $url.="&edit=".$_REQUEST['edit'];
+if (isset($_REQUEST['isDuplicate'])) $url.="&isDuplicate=".$_REQUEST['isDuplicate'];
+SugarApplication::redirect($url);
