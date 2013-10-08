13ba971ea3037a4f36e3dcd1e0699a8663cdcdc5
David Tam <dtam@sugarcrm.com>
2013-10-08 16:00:52 -0700

    INT-9 twitter dashlet configuration is confusing

--- sugarcrm/tests/clients/base/views/twitter/twitter.js
+++ sugarcrm/tests/clients/base/views/twitter/twitter.js
@@ -108,4 +108,20 @@ describe("Twitter View", function() {
         view.loadData();
         expect(apiStub.getCall(3).args[1].indexOf("bob")).toEqual(-1);
     });
+
+    it("should pull twitter field from config when context parent is null", function() {
+        // workaround since dashlet config not testable atm
+        view.meta.config = false;
+        var apiStub = sinonSandbox.stub(SugarTest.app.api, 'call');
+        var settingsStub = sinonSandbox.stub(view.settings, 'get', function(){return 'bob';});
+
+        view.loadData();
+
+        expect(apiStub.getCall(1).args[1].indexOf("bob")).toBeGreaterThan(-1);
+
+        view.context.parent = new Backbone.Model();
+        view.model.set('name','test');
+        view.loadData();
+        expect(apiStub.getCall(3).args[1].indexOf("bob")).toEqual(-1);
+    });
 });
