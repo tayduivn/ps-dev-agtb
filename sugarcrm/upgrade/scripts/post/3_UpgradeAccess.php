d776e2fb4943c9f0cbd164ca9fa5fb1e2957463e
Julien Girault <julien.emac@gmail.com>
2013-10-10 11:05:25 -0700

    SP-1281: Add new rewrite rule (for portal) in .htaccess during upgrade

--- sugarcrm/upgrade/scripts/post/3_UpgradeAccess.php
+++ sugarcrm/upgrade/scripts/post/3_UpgradeAccess.php
@@ -10,7 +10,7 @@ class SugarUpgradeUpgradeAccess extends UpgradeScript
 
     public function run()
     {
-        if(!version_compare($this->from_version, '6.6.0', '<')) {
+        if(!version_compare($this->from_version, '7.1.5', '<')) {
             return;
         }
         require_once "install/install_utils.php";
@@ -36,7 +36,41 @@ class SugarUpgradeUpgradeAccess extends UpgradeScript
         }
         $htaccess_file = $this->context['source_dir']."/.htaccess";
 
-        $status =  $this->putFile($htaccess_file, getHtaccessData($htaccess_file));
+        /**
+         * .htaccess change between 6.7 and 7.0.
+         * This piece used to be outside # SUGARCRM RESTRICTIONS but it's been moved inside in 7.0
+         * Thus we have to delete this piece prior to rebuild the htaccess, so we avoid duplicate rules
+         */
+        if (file_exists($htaccess_file)) {
+
+        $cache_headers = <<<EOQ
+<FilesMatch "\.(jpg|png|gif|js|css|ico)$">
+        <IfModule mod_headers.c>
+                Header set ETag ""
+                Header set Cache-Control "max-age=2592000"
+                Header set Expires "01 Jan 2112 00:00:00 GMT"
+        </IfModule>
+</FilesMatch>
+<IfModule mod_expires.c>
+        ExpiresByType text/css "access plus 1 month"
+        ExpiresByType text/javascript "access plus 1 month"
+        ExpiresByType application/x-javascript "access plus 1 month"
+        ExpiresByType image/gif "access plus 1 month"
+        ExpiresByType image/jpg "access plus 1 month"
+        ExpiresByType image/png "access plus 1 month"
+</IfModule>
+EOQ;
+        
+            $htaccess_contents = file_get_contents($htaccess_file);
+            $htaccess_contents = str_replace($cache_headers, '', $htaccess_contents);
+            $status =  $this->putFile($htaccess_file, $htaccess_contents); 
+            if( !$status ){
+                $this->fail(sprintf($this->mod_strings['ERROR_HT_NO_WRITE'], $htaccess_file));
+                return;
+            }
+        }
+
+        $status =  $this->putFile($htaccess_file, getHtaccessData($htaccess_file));  
         if( !$status ){
             $this->fail(sprintf($this->mod_strings['ERROR_HT_NO_WRITE'], $htaccess_file));
             return;
