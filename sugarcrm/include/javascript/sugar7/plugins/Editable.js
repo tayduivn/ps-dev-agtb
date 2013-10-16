82deed99bc4dd34d33602c2335871f74c39b49b8
Filipe Guerra <alias.mac@gmail.com>
2013-10-16 23:33:36 +0200

    Apply GJSLint

--- sugarcrm/include/javascript/sugar7/plugins/Editable.js
+++ sugarcrm/include/javascript/sugar7/plugins/Editable.js
@@ -1,5 +1,17 @@
-(function (app) {
-    app.events.on("app:init", function () {
+/*
+ * By installing or using this file, you are confirming on behalf of the entity
+ * subscribed to the SugarCRM Inc. product ("Company") that Company is bound by
+ * the SugarCRM Inc. Master Subscription Agreement ("MSA"), which is viewable at:
+ * http://www.sugarcrm.com/master-subscription-agreement
+ *
+ * If Company is not bound by the MSA, then by installing or using this file
+ * you are agreeing unconditionally that Company will be bound by the MSA and
+ * certifying that you have authority to bind Company accordingly.
+ *
+ * Copyright  2004-2013 SugarCRM Inc.  All rights reserved.
+ */
+(function(app) {
+    app.events.on('app:init', function() {
         /**
          * Editable plug-in will help the view controller's fields switching in edit mode.
          *
