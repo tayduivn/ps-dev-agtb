8e9b44310a007395a8886a8f951058bad62c27fb
Gabriel Rael <grael@sugarcrm.com>
2013-10-18 09:43:07 -0400

    Eradicate the curlies...all of them!

--- sugarcrm/tests/modules/Emails/Bug60688Test.php
+++ sugarcrm/tests/modules/Emails/Bug60688Test.php
@@ -3,7 +3,7 @@
 /* * *******************************************************************************
  * By installing or using this file, you are confirming on behalf of the entity
  * subscribed to the SugarCRM Inc. product ("Company") that Company is bound by
- * the SugarCRM Inc. Master Subscription Agreement (“MSA”), which is viewable at:
+ * the SugarCRM Inc. Master Subscription Agreement ("MSA"), which is viewable at:
  * http://www.sugarcrm.com/master-subscription-agreement
  *
  * If Company is not bound by the MSA, then by installing or using this file
--- sugarcrm/tests/modules/UpgradeWizard/metadata/Cases67portaledit.php
+++ sugarcrm/tests/modules/UpgradeWizard/metadata/Cases67portaledit.php
@@ -3,7 +3,7 @@ if(!defined('sugarEntry') || !sugarEntry) die('Not A Valid Entry Point');
 /*********************************************************************************
  * By installing or using this file, you are confirming on behalf of the entity
  * subscribed to the SugarCRM Inc. product ("Company") that Company is bound by
- * the SugarCRM Inc. Master Subscription Agreement (“MSA”), which is viewable at:
+ * the SugarCRM Inc. Master Subscription Agreement ("MSA"), which is viewable at:
  * http://www.sugarcrm.com/master-subscription-agreement
  *
  * If Company is not bound by the MSA, then by installing or using this file
