ca02a84f42eca83f5cb60e33f7959fa1acb740c3
Henry Rogers <henry@hhrogersii.com>
2013-10-19 01:13:05 -0400

    UIUX-1191: update funnel test chart

--- sugarcrm/styleguide/content/charts/data/funnel_data.js
+++ sugarcrm/styleguide/content/charts/data/funnel_data.js
@@ -1,61 +1,61 @@
 var funnel_data_default = {
-    'properties': {
-      'title': 'Pipeline total is $675k',
-      'total': 675,
-      'scale': 1000,
-      'units': '$'
+  'properties': {
+    'title': 'Pipeline total is $675k',
+    'total': 675,
+    'scale': 1000,
+    'units': '$'
+  },
+  'data': [
+    {
+      key: 'Negotiation/Review',
+      'count': 3,
+      'values': [
+        {series: 0, x: 0, y: 75,  y0: 0},
+      ]
     },
-    'data': [
-  {
-    key: 'Negotiation/Review',
-    'count': 3,
-    'values': [
-{series: 0, x: 0, y: 75,  y0: 0},
-    ]
-  },
-  {
-    'key': 'Proposal/PriceQuote',
-    'count': 5,
-    'values': [
-{series: 1, x: 0, y: 110,  y0:  75},
-    ]
-  },
-  {
-    'key': 'Perception Analysis',
-    'count': 1,
-    'values': [
-{series: 2, x: 0, y: 125, y0: 185},
-    ]
-  },
-  {
-    'key': 'Id. Decision Makers',
-    'count': 6,
-    'values': [
-{series: 3, x: 0, y: 85, y0: 310},
-    ]
-  },
-  {
-    'key': 'Value Proposition',
-    'count': 2,
-    'values': [
-{series: 4, x: 0, y:60, y0: 395},
-    ]
-  },
-  {
-    'key': 'Qualification',
-    'count': 8,
-    'values': [
-{series: 5, x: 0, y: 110, y0:455},
-    ]
-  },
-  {
-    'key': 'Prospecting',
-    'count': 10,
-    'values': [
-{series: 6, x: 0, y: 110, y0:565},
-    ]
-  }
-    ]
+    {
+      'key': 'Proposal/PriceQuote',
+      'count': 5,
+      'values': [
+        {series: 1, x: 0, y: 5,  y0: 0},
+      ]
+    },
+    {
+      'key': 'Perception Analysis',
+      'count': 1,
+      'values': [
+        {series: 2, x: 0, y: 195, y0: 0},
+      ]
+    },
+    {
+      'key': 'Id. Decision Makers',
+      'count': 6,
+      'values': [
+        {series: 3, x: 0, y: 85, y0: 0},
+      ]
+    },
+    {
+      'key': 'Value Proposition',
+      'count': 2,
+      'values': [
+        {series: 4, x: 0, y:60, y0: 0},
+      ]
+    },
+    {
+      'key': 'Qualification',
+      'count': 8,
+      'values': [
+        {series: 5, x: 0, y: 110, y0: 0},
+      ]
+    },
+    {
+      'key': 'Prospecting',
+      'count': 10,
+      'values': [
+        {series: 6, x: 0, y: 110, y0: 0},
+      ]
+    }
+  ]
 };
 
 
