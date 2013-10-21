04b5c0830fa1ec5828af30e3f2aa5062e29028cb
Steven Parsley <sparsley@sugarcrm.com>
2013-10-21 11:21:59 -0400

    Jira: BR-646 revert

--- sugarcrm/tests/data/acl/SugarACLForecastWorksheetsTest.php
+++ sugarcrm/tests/data/acl/SugarACLForecastWorksheetsTest.php
@@ -35,6 +35,8 @@ class SugarACLForecastWorksheetsTest extends Sugar_PHPUnit_Framework_TestCase
             ->method('getForecastByBean')
             ->will($this->returnValue($beanMock));
         
+        SugarTestReflection::setProtectedValue($acl_class, 'isAdminForBean', null);
+
         $context = array('field' => 'test_field', 'action' => 'write', 'user' => $userMock);
 
         $ret = $acl_class->checkAccess('ForecastWorksheets', 'field', $context);
@@ -61,6 +63,8 @@ class SugarACLForecastWorksheetsTest extends Sugar_PHPUnit_Framework_TestCase
         $acl_class->expects($this->once())
             ->method('getForecastByBean')
             ->will($this->returnValue($beanMock));
+
+        SugarTestReflection::setProtectedValue($acl_class, 'isAdminForBean', null);
 
         $context = array('field' => 'test_field', 'action' => 'write', 'user' => $userMock);
 
