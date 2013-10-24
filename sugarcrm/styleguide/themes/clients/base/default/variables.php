c9b8f688a8084bd39ebb4d8ea0781c01fa1e3045
Henry Rogers <henry@hhrogersii.com>
2013-10-24 17:41:32 -0400

    UIUX-1340: add instructions for modifying theme LESS variables to Styleguide
    Conflicts:
    	sugarcrm/modules/Styleguide/clients/base/views/sg-headerpane/sg-headerpane.php
    
    UIUX-1340: Themeable variables are now defined in a php file instead of a less file

---
+++ sugarcrm/styleguide/themes/clients/base/default/variables.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Variables.less
+ * Variables to customize the look and feel of Bootstrap
+ */
+
+$lessdefs = array(
+
+    'colors' => array(
+        /**
+         * Primary Color:
+         * 3 pixel line on the navbar
+         * -------------------------
+         * was @primary
+         */
+        'BorderColor' => '#E61718',
+
+        /**
+         * Secondary Color:
+         * color of the navbar
+         * -------------------------
+         * was @secondary
+         */
+        'NavigationBar' => '#000000',
+
+        /**
+         * Primary Button Color:
+         * color of the primary button
+         * -------------------------
+         * was @primaryBtn
+         */
+        'PrimaryButton' => '#177EE5',
+    ),
+);
