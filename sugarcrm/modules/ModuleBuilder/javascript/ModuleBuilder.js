cd872c7bbf3c21dbf969fbd575709a0e28aff122
Robert Gonzalez <rgonzalez@sugarcrm.com>
2013-11-07 21:18:35 -0800

    [BR-850] Double click doesn't show combination field contents in Studio in FF24 Add jQuery DOM element selection to support cross browser functionality Add handling for removing a combo field when one of it's fields is on a layout Add setting of fieldset member fields on construct of the parser

--- sugarcrm/modules/ModuleBuilder/javascript/ModuleBuilder.js
+++ sugarcrm/modules/ModuleBuilder/javascript/ModuleBuilder.js
@@ -811,10 +811,12 @@ if (typeof(ModuleBuilder) == 'undefined') {
 						// Add a double click event that shows what field a combo
 						// field contains
 						fieldElem.ondblclick = function() {
-							var fieldList = document.getElementById('fieldset_' + this.id).innerHTML;
+							// Use jQuery DOM element selection since different
+							// browsers handle content differently. This fixes an
+							// issue where FF was not rendering the popup.
 							YAHOO.SUGAR.MessageBox.show({
-			                    title: this.innerText.replace(" **", "") + " " + SUGAR.language.get("ModuleBuilder", "LBL_COMBO_FIELD_CONTAINS"),
-			                    msg: fieldList,
+			                    title: $('#le_label_' + this.id).text().replace(" **", "") + " " + SUGAR.language.get("ModuleBuilder", "LBL_COMBO_FIELD_CONTAINS"),
+			                    msg: $('#fieldset_' + this.id).html(),
 			                    width: 500
 			                });
 						};
