1588485dcf067dd6238959332770a28980861e46
Tao Shen <tshen@Administrators-MacBook-Pro-67.local>
2013-11-08 18:32:43 -0800

    INT-71: create upgrade scripts for convert custom global links to sidecar format metadata

--- sugarcrm/tests/metadata/MetaDataConverterTest.php
+++ sugarcrm/tests/metadata/MetaDataConverterTest.php
@@ -76,7 +76,6 @@ class MetaDataConverterTest extends Sugar_PHPUnit_Framework_TestCase
         $this->assertTrue(isset($converted['panels'][5][0]), "Conversion failed to convert fieldset at offset 5");
         $this->assertEquals('duration_minutes', $converted['panels'][5][0], "duration_minutes did not convert from a fieldset");
     }
-
     /**
      * Test converting subpanels
      */
@@ -106,21 +105,90 @@ class MetaDataConverterTest extends Sugar_PHPUnit_Framework_TestCase
             if ($aSubPanel->isCollection()) {
                 continue;
             }
-            $panel_definition = $converter->fromLegacySubpanelsViewDefs($aSubPanel->panel_definition);
+            $panel_definition = $converter->fromLegacySubpanelsViewDefs($aSubPanel->panel_definition, 'Quotes');
         }
 
         $this->assertNotEmpty($panel_definition, "Panel Definition not set");
-        foreach ($panel_definition as $panels) {
-            foreach ($panels as $panel) {
-                $this->assertArrayHasKey('name', $panel, "Panel should have a name field");
-                $this->assertArrayHasKey('label', $panel, "Panel should have a label field");
-                $this->assertArrayHasKey('fields', $panel, "Panels should have fields");
-                foreach ($panel['fields'] as $fieldDef) {
-                    foreach ($fieldDef as $key => $value) {
-                        $this->assertContains($key, $fieldMap);
-                    }
+        foreach ($panel_definition['panels'] as $panel) {
+            $this->assertArrayHasKey('name', $panel, "Panel should have a name field");
+            $this->assertArrayHasKey('label', $panel, "Panel should have a label field");
+            $this->assertArrayHasKey('fields', $panel, "Panels should have fields");
+            foreach ($panel['fields'] as $fieldDef) {
+                foreach ($fieldDef as $key => $value) {
+                    $this->assertContains($key, $fieldMap);
                 }
             }
+        }
+    }
+    /**
+     * Test adding custom link to profileactions
+     */
+    public function testConvertProfileactions()
+    {
+        $converter = new MetaDataConverter();
+
+        // Create Input field map includes different test link to see if these links are
+        // converted properly which has correct label, route and acl_action
+        $testlink = array();
+        $testlink['google'] = array('linkinfo' => array('Google' => 'https://www.google.com/'),
+            'submenu' => ''
+        );
+        $testlink['contact'] = array('linkinfo' => array('LBL_CONTACTS' => '#Contacts'),
+            'submenu' => ''
+        );
+        $testlink['report'] = array('linkinfo' => array('LBL_REPORTS' => 'index.php?module=Reports&action=index'),
+            'submenu' => ''
+        );
+        $testlink['administration'] = array('linkinfo' => array('LBL_ADMIN' => 'index.php?module=Administration&action=index'),
+            'submenu' => ''
+        );
+        $testlink['support'] = array(
+            'linkinfo' => array('LBL_TRAINING' => 'javascript:void(window.open(\'http://support.sugarcrm.com\'))'),
+            'submenu' => ''
+        );
+        $testlink['task'] = array('linkinfo' => array('LBL_TASKS' => '#Tasks'),
+            'submenu' => array(
+                'case' => array('LBL_CASES' => '#Cases'),
+                'note' => array('LBL_NOTES' => '#Notes'),
+                'bug'  => array('LBL_BUGS' => '#Bugs'),
+                'support' => array('LBL_TRAINING' => 'javascript:void(window.open(\'http://support.sugarcrm.com\'))'),
+            )
+        );
+        $testlink['attachment'] = array('linkinfo' => array('ATTACHMENTS' => 'client/base/views/attachments/attachments.php'),
+            'submenu' => ''
+        );
+        // Transform globalcontrollink format into regular associate array format
+        $inputTestLinks = $converter->processFromGlobalControlLinkFormat($testlink);
+
+        // Expected output field map
+        $expectedOutput = array(
+            'Google' => array( 'label' => 'Google', 'route' => 'https://www.google.com/', 'acl_action' => ''),
+            'LBL_CONTACTS' => array( 'label' => 'LBL_CONTACTS', 'route' => '#Contacts', 'acl_action' => ''),
+            'LBL_TRAINING' => array( 'label' => 'LBL_TRAINING', 'route' => 'http://support.sugarcrm.com', 'acl_action' => '', 'openwindow' => true),
+            'LBL_TASKS' => array( 'label' => 'LBL_TASKS', 'route' => '#Tasks', 'acl_action' => '',
+                'submenu' => array(
+                    array( 'label' => 'LBL_CASES', 'route' => '#Cases', 'acl_action' => ''),
+                    array( 'label' => 'LBL_NOTES', 'route' => '#Notes', 'acl_action' => ''),
+                    array( 'label' => 'LBL_BUGS', 'route' => '#Bugs', 'acl_action' => ''),
+                    array( 'label' => 'LBL_TRAINING', 'route' => 'http://support.sugarcrm.com', 'acl_action' => '', 'openwindow' => true),
+                )
+            ),
+            'LBL_REPORTS' => array( 'label' => 'LBL_REPORTS', 'route' => '#bwc/index.php?module=Reports&action=index', 'acl_action' => 'list'),
+            'LBL_ADMIN' => array( 'label' => 'LBL_ADMIN', 'route' => '#bwc/index.php?module=Administration&action=index', 'acl_action' => 'admin'),
+            '' => '',
+        );
+
+        // Test if custom links are converted correctly by comparing with expectedOutput
+        foreach($inputTestLinks as $item){
+            $convertedItem = $converter->convertCustomMenu($item);
+            if(!empty($item['SUBMENU'])){
+                $convertedSubmenu = array();
+                foreach($item['SUBMENU'] as $submenu){
+                    $convertedSubmenu[] = $converter->convertCustomMenu($submenu);
+                }
+                $convertedItem['submenu'] = $convertedSubmenu;
+            }
+            $this->assertEquals($expectedOutput[$convertedItem['label']], $convertedItem, "{$convertedItem['label']} array did not convert correctly");
         }
     }
 }
