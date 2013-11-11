2146343f0ce9b4c01271cb65d88f13b77401df0c
David Wheeler <dwheeler@sugarcrm.com>
2013-11-11 10:39:41 -0800

    Revert "BR-565 Upgrader"
    This reverts commit 77adc5dacb3933587f2a04c3129ab44a080211fc.

--- sugarcrm/modules/UpgradeWizard/SidecarUpdate/SidecarAbstractMetaDataUpgrader.php
+++ sugarcrm/modules/UpgradeWizard/SidecarUpdate/SidecarAbstractMetaDataUpgrader.php
@@ -336,31 +336,10 @@ abstract class SidecarAbstractMetaDataUpgrader
                 $defs = $$var;
                 if (isset($this->vardefIndexes[$this->client.$this->viewtype])) {
                     $index = $this->vardefIndexes[$this->client.$this->viewtype];
-                    $legacyDefs = empty($index) ? $defs[$module] : $defs[$module][$index];
-                    $this->legacyViewdefs = $this->cleanupFields($legacyDefs);
-
+                    $this->legacyViewdefs = empty($index) ? $defs[$module] : $defs[$module][$index];
                 }
             }
         }
-    }
-
-    /**
-     * This method removes fields that are not in the field_defs
-     * @param $legacyDefs
-     * @return mixed
-     */
-    public function cleanupFields($legacyDefs)
-    {
-        // instantiate the bean to get the fielddefs
-        $bean = BeanFactory::getBean($this->module);
-        $fieldDefs = $bean->field_defs;
-        foreach ($legacyDefs as $fieldName => $def) {
-            $lowerFieldName = strtolower($fieldName);
-            if (!isset($fieldDefs[$lowerFieldName])) {
-                unset($legacyDefs[$fieldName]);
-            }
-        }
-        return $legacyDefs;
     }
 
     /**
