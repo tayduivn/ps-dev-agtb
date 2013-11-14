f811240949856647be1ed87a0cd8794861871f79
Rob Levin <rlevin@sugarcrm.com>
2013-11-13 18:12:25 -0800

    Fix test fails

--- sugarcrm/tests/clients/base/views/profileactions/profileactions.js
+++ sugarcrm/tests/clients/base/views/profileactions/profileactions.js
@@ -1,12 +1,11 @@
 describe("Profile Actions", function() {
 
-    var app, view, sandbox, menuMeta;
-
+    var app, view, sinonSandbox, menuMeta;
     beforeEach(function() {
         app = SugarTest.app;
         var context = app.context.getContext();
         view = SugarTest.createView("base","Accounts", "profileactions", null, context);
-        sandbox = sinon.sandbox.create();
+        sinonSandbox = sinon.sandbox.create();
         menuMeta = [{
             acl_action: 'admin',
             label: 'LBL_ADMIN'
@@ -15,14 +14,14 @@ describe("Profile Actions", function() {
     afterEach(function() {
         app.cache.cutAll();
         app.view.reset();
+        sinonSandbox.restore();
         Handlebars.templates = {};
         view = null;
         menuMeta = null;
-        sandbox.restore();
     });
 
     it("should show admin link when acl of admin and developer", function() {
-        var stubAdminAndDev = sandbox.stub(app.acl, 'hasAccessToAny', function(a) {
+        var stubAdminAndDev = sinonSandbox.stub(app.acl, 'hasAccessToAny', function(a) {
             if (a === 'admin' || a === 'developer') {
                 return true;
             } else {
@@ -34,22 +33,22 @@ describe("Profile Actions", function() {
         expect(result.length).toEqual(1);
     });
     it("should show admin link when acl of developer", function() {
-        stubDev = sandbox.stub(app.acl, 'hasAccessToAny', function(a) {
+        var stubDev = sinonSandbox.stub(app.acl, 'hasAccessToAny', function(a) {
             if (a === 'developer') {
                 return true;
             } else {
                 return false;
             }
         });
-        result = view.filterAvailableMenu(menuMeta);
+        var result = view.filterAvailableMenu(menuMeta);
         expect(stubDev).toHaveBeenCalled();
         expect(result.length).toEqual(1);
     });
     it("should NOT show admin link when acl is NOT of admin or developer", function() {
-        var notAdminOrDev = sandbox.stub(app.acl, 'hasAccessToAny', function(a) {
+        var notAdminOrDev = sinonSandbox.stub(app.acl, 'hasAccessToAny', function(a) {
             return false;
         });
-        result = view.filterAvailableMenu(menuMeta);
+        var result = view.filterAvailableMenu(menuMeta);
         expect(notAdminOrDev).toHaveBeenCalled();
         expect(result.length).toEqual(0);
     });
