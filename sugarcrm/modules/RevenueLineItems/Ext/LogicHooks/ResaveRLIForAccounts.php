791658bf3cffcffcc3ee124dae1176c21754ea21
John Barlow <jbarlow@sugarcrm.com>
2013-11-20 18:08:03 -0500

    SFA-2043: added after relationship hook to fix account assignment in RLI

---
+++ sugarcrm/modules/RevenueLineItems/Ext/LogicHooks/ResaveRLIForAccounts.php
@@ -0,0 +1,25 @@
+<?php
+/*
+ * By installing or using this file, you are confirming on behalf of the entity
+ * subscribed to the SugarCRM Inc. product ("Company") that Company is bound by
+ * the SugarCRM Inc. Master Subscription Agreement ("MSA"), which is viewable at:
+ * http://www.sugarcrm.com/master-subscription-agreement
+ *
+ * If Company is not bound by the MSA, then by installing or using this file
+ * you are agreeing unconditionally that Company will be bound by the MSA and
+ * certifying that you have authority to bind Company accordingly.
+ *
+ * Copyright  2004-2013 SugarCRM Inc.  All rights reserved.
+ */
+
+/**
+ * Resave RLI bean after the account_link relationship is removed. This will cause the RLI to pick up
+ * the account from it's associated Opportunity through sugarlogic
+ */
+$hook_array['after_relationship_delete'][] = array(
+    1,
+    'afterRelationshipDelete',
+    'modules/RevenueLineItems/RevenueLineItemHooks.php',
+    'RevenueLineItemHooks',
+    'afterRelationshipDelete',
+);
