f54d0ade436ebe0617838ca2f71606c42729f49a
Neil Harding <nharding@sugarcrm.com>
2013-11-20 14:49:20 -0500

    MAR-1497 - Fix notice that won't close on email compose MAR-1495 - Fix issue where attachment can't be added after attachment if removed

---
+++ sugarcrm/modules/Emails/clients/base/fields/attachment-button/attachment-button.js
@@ -0,0 +1,24 @@
+/*
+ * By installing or using this file, you are confirming on behalf of the entity
+ * subscribed to the SugarCRM Inc. product ("Company") that Company is bound by
+ * the SugarCRM Inc. Master Subscription Agreement ("MSA"), which is viewable at:
+ * http://www.sugarcrm.com/master-subscription-agreement
+ *
+ * If Company is not bound by the MSA, then by installing or using this file
+ * you are agreeing unconditionally that Company will be bound by the MSA and
+ * certifying that you have authority to bind Company accordingly.
+ *
+ * Copyright  2004-2013 SugarCRM Inc.  All rights reserved.
+ */
+({
+    /**
+     * Attachment button is a label that is styled like a button and will trigger a given file input field
+     *
+     * @extends View.Fields.ButtonField
+     */
+    extendsFrom: 'ButtonField',
+    initialize: function(options) {
+        this._super('initialize',[options]);
+        this.fileInputId = this.context.get('attachment_field_email_attachment');
+    }
+})
