944e8a52e2b534c8baf9261e7c9d5fde86b16efa
Joo Lee <jlee@sugarcrm.com>
2013-11-26 17:02:18 -0500

    MAR-1596: Fixes portal having an old implementation of tooltip that interferes with the tooltip plugin.

--- sugarcrm/portal2/portal-ui.js
+++ sugarcrm/portal2/portal-ui.js
@@ -34,12 +34,7 @@
 
         __superViewRender__.call(this);
 
-        // do this if greater than 768px page width
-        if ($(window).width() > 768) {
-            this.$("[rel=tooltip]").tooltip({ placement: "bottom" });
-        }
         //popover
-        this.$("[rel=popover]").popover();
         this.$("[rel=popoverTop]").popover({placement: "top"});
 
         if ($.fn.timeago) {
@@ -59,24 +54,10 @@
     };
 
     /**
-     * Overrides View::initialize() to remove the bootstrap widgets element from all the page
-     * The widget is actually bind to an element that will be removed from the DOM when the view changes. So we need to
-     * manually remove elements automatically created by the widget.
-     */
-    var __superViewInit__ = app.view.View.prototype.initialize;
-    app.view.View.prototype.initialize = function(options) {
-        __superViewInit__.call(this, options);
-        $('.popover, .tooltip').remove();
-    };
-
-    /**
      * Overrides Field::_render() to fix placeholders on IE and old browsers
      */
     var __superFieldRender__ = app.view.Field.prototype._render;
     app.view.Field.prototype._render = function() {
-        _.each(this.$('[rel="tooltip"]'), function(element) {
-            $(element).tooltip('hide');
-        })
         __superFieldRender__.call(this);
         if($.fn.placeholder){
             this.$("input[placeholder]").placeholder();
