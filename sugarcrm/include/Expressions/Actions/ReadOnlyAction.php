934305d660fcf633ecc92f4efbe03cca47c5cf2c
jwhitcraft <jwhitcraft@sugarcrm.com>
2013-12-02 15:05:50 -0500

    SFA-2110: PSR-2 ReadOnlyAction.php
    - Fix the formatting.

--- sugarcrm/include/Expressions/Actions/ReadOnlyAction.php
+++ sugarcrm/include/Expressions/Actions/ReadOnlyAction.php
@@ -1,60 +1,57 @@
 <?php
-/*********************************************************************************
- *The contents of this file are subject to the SugarCRM Professional End User License Agreement
- *("License") which can be viewed at http://www.sugarcrm.com/EULA.
- *By installing or using this file, You have unconditionally agreed to the terms and conditions of the License, and You may
- *not use this file except in compliance with the License. Under the terms of the license, You
- *shall not, among other things: 1) sublicense, resell, rent, lease, redistribute, assign or
- *otherwise transfer Your rights to the Software, and 2) use the Software for timesharing or
- *service bureau purposes such as hosting the Software for commercial gain and/or for the benefit
- *of a third party.  Use of the Software may be subject to applicable fees and any use of the
- *Software without first paying applicable fees is strictly prohibited.  You do not have the
- *right to remove SugarCRM copyrights from the source code or user interface.
- * All copies of the Covered Code must include on each user interface screen:
- * (i) the "Powered by SugarCRM" logo and
- * (ii) the SugarCRM copyright notice
- * in the same form as they appear in the distribution.  See full license for requirements.
- *Your Warranty, Limitations of liability and Indemnity are expressly stated in the License.  Please refer
- *to the License for the specific language governing these rights and limitations under the License.
- *Portions created by SugarCRM are Copyright (C) 2004 SugarCRM, Inc.; All Rights Reserved.
- ********************************************************************************/
+/*
+ * By installing or using this file, you are confirming on behalf of the entity
+ * subscribed to the SugarCRM Inc. product ("Company") that Company is bound by
+ * the SugarCRM Inc. Master Subscription Agreement ("MSA"), which is viewable at:
+ * http://www.sugarcrm.com/master-subscription-agreement
+ *
+ * If Company is not bound by the MSA, then by installing or using this file
+ * you are agreeing unconditionally that Company will be bound by the MSA and
+ * certifying that you have authority to bind Company accordingly.
+ *
+ * Copyright  2004-2013 SugarCRM Inc.  All rights reserved.
+ */
+
 require_once("include/Expressions/Actions/AbstractAction.php");
 
-class ReadOnlyAction extends AbstractAction{
-	protected $expression =  "";
+class ReadOnlyAction extends AbstractAction
+{
+    protected $expression = "";
 
-	function ReadOnlyAction($params) {
+    public function ReadOnlyAction($params)
+    {
         $this->params = $params;
-		$this->targetField = $params['target'];
-		$this->expression = str_replace("\n", "",$params['value']);
-	}
+        $this->targetField = $params['target'];
+        $this->expression = str_replace("\n", "", $params['value']);
+    }
 
-/**
-	 * Returns the javascript class equavalent to this php class
-	 *
-	 * @return string javascript.
-	 */
-	static function getJavascriptClass() {
+    /**
+     * Returns the javascript class equavalent to this php class
+     *
+     * @return string javascript.
+     */
+    public static function getJavascriptClass()
+    {
         return "
-		SUGAR.forms.ReadOnlyAction = function(target, expr) {
-			if (_.isObject(target)){
+        SUGAR.forms.ReadOnlyAction = function(target, expr) {
+            if (_.isObject(target)){
                 expr = target.value;
                 target = target.target
             }
-			this.target = target;
-			this.expr = expr;
-		}
+            this.target = target;
+            this.expr = expr;
+        }
 
-		SUGAR.util.extend(SUGAR.forms.ReadOnlyAction, SUGAR.forms.AbstractAction, {
-			/**
-			 * Triggers the style dependencies.
-			 */
-			exec: function(context) {
-				if (typeof(context) == 'undefined') context = this.context;
-				var val = this.evalExpression(this.expr, context),
-					readOnly = val == SUGAR.expressions.Expression.TRUE;
-				
-				if (context.view) {
+        SUGAR.util.extend(SUGAR.forms.ReadOnlyAction, SUGAR.forms.AbstractAction, {
+            /**
+             * Triggers the style dependencies.
+             */
+            exec: function(context) {
+                if (typeof(context) == 'undefined') context = this.context;
+                var val = this.evalExpression(this.expr, context),
+                    readOnly = val == SUGAR.expressions.Expression.TRUE;
+                
+                if (context.view) {
                     //We may get triggered before the view has rendered with the full field list.
                     //If that occurs wait for the next render to apply.
                     if (_.isEmpty(context.view.fields)) {
@@ -67,77 +64,79 @@ class ReadOnlyAction extends AbstractAction{
                     this.bwcExec(context, readOnly);
                 }
 
-			},
+            },
 
-			bwcExec: function(context, readonly) {
-			    var el = SUGAR.forms.AssignmentHandler.getElement(this.target);
+            bwcExec: function(context, readonly) {
+                var el = SUGAR.forms.AssignmentHandler.getElement(this.target);
                 if (!el) {
                     return;
                 }
-				this.setReadOnly(el, set);
-				this.setDateField(el, set);
-			},
+                this.setReadOnly(el, set);
+                this.setDateField(el, set);
+            },
 
-			setReadOnly: function(el, set)
-			{
-				var D = YAHOO.util.Dom;
-				var property = el.type == 'checkbox' || 'select' ? 'disabled' : 'readonly';
-				el[property] = set;
-				if (set)
-				{
-					D.setStyle(el, 'background-color', '#EEE');
-					if (!SUGAR.isIE)
-					   D.setStyle(el, 'color', '#22');
-				} else
-				{
-					D.setStyle(el, 'background-color', '');
-						if (!SUGAR.isIE)
-							D.setStyle(el, 'color', '');
-				}
-			},
+            setReadOnly: function(el, set)
+            {
+                var D = YAHOO.util.Dom;
+                var property = el.type == 'checkbox' || 'select' ? 'disabled' : 'readonly';
+                el[property] = set;
+                if (set)
+                {
+                    D.setStyle(el, 'background-color', '#EEE');
+                    if (!SUGAR.isIE)
+                       D.setStyle(el, 'color', '#22');
+                } else
+                {
+                    D.setStyle(el, 'background-color', '');
+                        if (!SUGAR.isIE)
+                            D.setStyle(el, 'color', '');
+                }
+            },
 
-			setDateField: function(el, set)
-			{
-				var D = YAHOO.util.Dom, id = el.id, trig = D.get(id + '_trigger');
-				if(!trig) return;
-				var fields = [
-					D.get(id + '_date'),
-					D.get(id + '_minutes'),
-					D.get(id + '_meridiem'),
-					D.get(id + '_hours')];
+            setDateField: function(el, set)
+            {
+                var D = YAHOO.util.Dom, id = el.id, trig = D.get(id + '_trigger');
+                if(!trig) return;
+                var fields = [
+                    D.get(id + '_date'),
+                    D.get(id + '_minutes'),
+                    D.get(id + '_meridiem'),
+                    D.get(id + '_hours')];
 
-				for (var i in fields)
-					if (fields[i] && fields[i].id)
-						this.setReadOnly(fields[i], set);
+                for (var i in fields)
+                    if (fields[i] && fields[i].id)
+                        this.setReadOnly(fields[i], set);
 
-				if (set)
-					D.setStyle(trig, 'display', 'none');
-				else
-					D.setStyle(trig, 'display', '');
-			}
-		});";
-	}
+                if (set)
+                    D.setStyle(trig, 'display', 'none');
+                else
+                    D.setStyle(trig, 'display', '');
+            }
+        });";
+    }
 
-	/**
-	 * Returns the javascript code to generate this actions equivalent.
-	 *
-	 * @return string javascript.
-	 */
-	function getJavascriptFire() {
-		return "new SUGAR.forms.ReadOnlyAction('{$this->targetField}','{$this->expression}')";
-	}
+    /**
+     * Returns the javascript code to generate this actions equivalent.
+     *
+     * @return string javascript.
+     */
+    public function getJavascriptFire()
+    {
+        return "new SUGAR.forms.ReadOnlyAction('{$this->targetField}','{$this->expression}')";
+    }
 
-	/**
-	 * Applies the Action to the target.
-	 *
-	 * @param SugarBeam $target
-	 */
-	function fire(&$target) {
-		//This is a no-op under PHP
-	}
+    /**
+     * Applies the Action to the target.
+     *
+     * @param SugarBeam $target
+     */
+    public function fire(&$target)
+    {
+        //This is a no-op under PHP
+    }
 
-	static function getActionName() {
-		return "ReadOnly";
-	}
-
+    public static function getActionName()
+    {
+        return "ReadOnly";
+    }
 }
