ed0a575a357f621eea410f6b92d8ea60621eaa8a
Andrija Vucinic <andrija.vucinic@gmail.com>
2013-12-26 19:24:56 +0100

    48616 - Fix test

--- sugarcrm/tests/include/generic/SugarWidgets/Bug48616Test.php
+++ sugarcrm/tests/include/generic/SugarWidgets/Bug48616Test.php
@@ -24,95 +24,45 @@
 
 require_once "include/generic/LayoutManager.php";
 require_once "include/generic/SugarWidgets/SugarWidgetFielddatetime.php";
-require_once "modules/Reports/Report.php";
 
 class Bug48616Test extends PHPUnit_Framework_TestCase
 {
-    var $sugarWidgetField;
-
     public function setUp()
     {
+        SugarTestHelper::setUp('timedate');
         SugarTestHelper::setUp('beanFiles');
         SugarTestHelper::setUp('beanList');
         SugarTestHelper::setUp('current_user');
-
-        $layoutManager = new LayoutManager();
-        $layoutManager->setAttribute('reporter', new Report());
-        $this->sugarWidgetField = new SugarWidgetFieldDateTime($layoutManager);
     }
 
     public function tearDown()
     {
         SugarTestHelper::tearDown();
-
-        SugarTestUserUtilities::removeAllCreatedAnonymousUsers();
     }
 
-    public function testQueryFilterBefore()
+    /**
+     * Check if expandDate works properly when 'Today' macro is passed
+     * instead of a date string
+     */
+    public function testExpandDateToday()
     {
-        $layout_def =  array (
-            'name' => 'donotinvoiceuntil_c',
-            'table_key' => 'self',
-            'qualifier_name' => 'before',
-            'input_name0' => 'Today',
-            'input_name1' => '01:00am',
-            'input_name2' => 'on',
-            'table_alias' => 'pordr_purchaseorders_cstm',
-            'column_key' => 'self:donotinvoiceuntil_c',
-            'type' => 'datetimecombo'
+        global $timedate;
+        $widget = new SugarWidgetFieldDateTime48616Mock(new LayoutManager());
+
+        $result = $widget->expandDate('Today');
+
+        $this->assertContains(
+            $timedate->asDbDate($timedate->getNow(true)),
+            $result,
+            "'Today' macro was not processed properly by expandDate()"
         );
-
-        $filter = $this->sugarWidgetField->queryFilterBefore($layout_def);
-
-        if ($GLOBALS['db']->getScriptName() == 'mysql') {
-            $this->assertRegExp(
-                "/pordr_purchaseorders_cstm\.donotinvoiceuntil_c [\+|-] INTERVAL \d+ MINUTE <" .
-                " \'\d{4}\-\d{1,2}-\d{1,2} \d{2}:\d{2}:\d{2}\'/",
-                $filter
-            );
-        }
     }
+}
 
-    public function testQueryFilterAfter()
+class SugarWidgetFieldDateTime48616Mock extends SugarWidgetFieldDateTime
+{
+    public function expandDate($date)
     {
-        $layout_def = array(
-            'name' => 'donotinvoiceuntil_c',
-            'table_key' => 'self',
-            'qualifier_name' => 'after',
-            'input_name0' => 'Today',
-            'input_name1' => '01:00am',
-            'input_name2' => 'on',
-            'table_alias' => 'pordr_purchaseorders_cstm',
-            'column_key' => 'self:donotinvoiceuntil_c',
-            'type' => 'datetimecombo'
-        );
-
-        $filter = $this->sugarWidgetField->queryFilterAfter($layout_def);
-
-        if ($GLOBALS['db']->getScriptName() == 'mysql') {
-            $this->assertRegExp(
-                "/pordr_purchaseorders_cstm\.donotinvoiceuntil_c [\+|-] INTERVAL \d+ MINUTE >" .
-                " \'\d{4}\-\d{1,2}-\d{1,2} \d{2}:\d{2}:\d{2}\'/",
-                $filter
-            );
-        }
-    }
-
-    public function testQueryFilterNotEqualsStr()
-    {
-        $layout_def =  array ('name' => 'donotinvoiceuntil_c', 'table_key' => 'self', 'qualifier_name' => 'not_equals', 'input_name0' => 'Today', 'input_name1' => '01:00am', 'input_name2' => 'on', 'table_alias' => 'pordr_purchaseorders_cstm', 'column_key' => 'self:donotinvoiceuntil_c', 'type' => 'datetimecombo');
-        $filter = $this->sugarWidgetField->queryFilterNot_Equals_str($layout_def);
-        $filter = preg_replace('/\s{2,}/', ' ', $filter);
-        $filter = str_replace("\n", '', $filter);
-        $filter = str_replace("\r", '', $filter);
-        if($GLOBALS['db']->getScriptName() == 'mysql')
-        {
-            $this->assertRegExp("/\(pordr_purchaseorders_cstm\.donotinvoiceuntil_c IS NULL OR pordr_purchaseorders_cstm\.donotinvoiceuntil_c < \'\d{4}\-\d{1,2}-\d{1,2} \d{2}:\d{2}:\d{2}\' OR pordr_purchaseorders_cstm\.donotinvoiceuntil_c > \'\d{4}\-\d{1,2}-\d{1,2} \d{2}:\d{2}:\d{2}\'\)/", $filter);
-        }
-        /*
-        else if($GLOBALS['db']->getScriptName() == 'db2') {
-            $this->assertRegExp("/\(pordr_purchaseorders_cstm\.donotinvoiceuntil_c IS NULL OR pordr_purchaseorders_cstm\.donotinvoiceuntil_c < CONVERT\(datetime\,'\d{4}\-\d{1,2}-\d{1,2} \d{2}:\d{2}:\d{2}\',\d+?\) OR pordr_purchaseorders_cstm\.donotinvoiceuntil_c > CONVERT\(datetime\,'\d{4}\-\d{1,2}-\d{1,2} \d{2}:\d{2}:\d{2}\',120\)\)/", $filter);
-        }
-        */
+        return parent::expandDate($date);
     }
 }
