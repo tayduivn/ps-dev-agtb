520795b0f21a653518016c17afe57206e53e3830
João Morais <jcsmorais@gmail.com>
2014-01-07 11:52:57 -0800

    Add 'mobileUrl' to mobile-action (fixes UIUX-1450)

--- sugarcrm/clients/base/views/mobile-action/mobile-action.js
+++ sugarcrm/clients/base/views/mobile-action/mobile-action.js
@@ -11,6 +11,19 @@
  * Copyright (C) 2004-2013 SugarCRM Inc. All rights reserved.
  */
 ({
-    tagName: 'span'
+    tagName: 'span',
+
+    /**
+     * {@inheritDoc}
+     *
+     * Adds `mobileUrl` variable based on `siteUrl` value from config, if none
+     * supplied falls back to current location.
+     */
+    _renderHtml: function() {
+        var baseUrl = app.config.siteUrl || window.location.pathname;
+        this.mobileUrl = baseUrl + 'mobile';
+
+        this._super('_renderHtml');
+    }
 })
 
