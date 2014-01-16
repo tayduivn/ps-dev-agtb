54edcbd0209b65ce6f70e07fe87d663c8ae2323c
Travis Hubbard <thubbard@sugarcrm.com>
2014-01-16 17:46:06 -0500

    SFA-2233 reverting SC-2272 record.js change that was throwing errors

--- sugarcrm/clients/base/views/record/record.js
+++ sugarcrm/clients/base/views/record/record.js
@@ -139,7 +139,6 @@
         });
 
         // check whether the changed attributes are among the editable fields
-        editableFieldNames = _.intersection(editableFieldNames, _.keys(this.model.changedAttributes()));
         unsavedFields = _.intersection(_.keys(changedAttributes), editableFieldNames);
 
         return !_.isEmpty(unsavedFields);
