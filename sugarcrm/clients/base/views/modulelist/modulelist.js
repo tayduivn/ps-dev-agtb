0533d9e5b2ead286071eecac7ab0a248a5971dfe
Filipe Guerra <alias.mac@gmail.com>
2014-01-22 02:09:00 +0100

    Add hbs-helper to properly use url paths (fixes UIUX-1426)
    The helper will allow extensions in the future if we want to reuse it
    to build image fields or other urls based on models or context.

--- sugarcrm/clients/base/views/modulelist/modulelist.js
+++ sugarcrm/clients/base/views/modulelist/modulelist.js
@@ -8,7 +8,7 @@
  * you are agreeing unconditionally that Company will be bound by the MSA and
  * certifying that you have authority to bind Company accordingly.
  *
- * Copyright  2004-2013 SugarCRM Inc.  All rights reserved.
+ * Copyright (C) 2004-2014 SugarCRM Inc. All rights reserved.
  */
 ({
     className: 'module-list',
@@ -214,7 +214,6 @@
             this.resetMenu();
             this.activeModule.set(app.controller.context.get('module'));
         }
-        this.imgUrl = (app.config.siteUrl || window.location.pathname).replace('index.php','') + 'styleguide/assets/img';
     },
 
     completeMenuMeta: function(module_list) {
