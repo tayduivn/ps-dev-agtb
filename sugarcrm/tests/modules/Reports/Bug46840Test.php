455176dd8dfe8b8a08f2b39bd6b56d97ecfae1b4
Michael Gusev <mgusev@sugarcrm.com>
2014-02-12 18:15:11 +0300

    RS-107: Prepare Reports Module
    Removing incomplete from tests.

--- sugarcrm/tests/modules/Reports/Bug46840Test.php
+++ sugarcrm/tests/modules/Reports/Bug46840Test.php
@@ -31,7 +31,6 @@ class Bug46840Test extends Sugar_PHPUnit_Framework_TestCase
 
 	public function setUp()
     {
-        $this->markTestIncomplete('Marking this skipped until Report module to be fixed.See RS-221/RS-107');
     	$beanList = array();
 		$beanFiles = array();
 		require('include/modules.php');
