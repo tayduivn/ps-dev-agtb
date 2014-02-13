6c80ee72baae590a97fe288e4d264861ee044529
Michael Gusev <mgusev@sugarcrm.com>
2014-02-13 17:58:36 +0300

    RS-286: MetaDataManagerPortal fails on pro.
    Adde flav

--- sugarcrm/tests/include/MetaDataManager/MetaDataManagerModulesInfoTest.php
+++ sugarcrm/tests/include/MetaDataManager/MetaDataManagerModulesInfoTest.php
@@ -13,7 +13,9 @@
  */
 
 require_once 'include/MetaDataManager/MetaDataManager.php';
+//BEGIN SUGARCRM flav=ent ONLY
 require_once 'include/MetaDataManager/MetaDataManagerPortal.php';
+//END SUGARCRM flav=ent ONLY
 require_once 'include/MetaDataManager/MetaDataManagerMobile.php';
 
 class MetaDataManagerModulesInfoTest extends Sugar_PHPUnit_Framework_TestCase
