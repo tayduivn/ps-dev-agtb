8ac2fe433efec38d42250afb4defd7ab31ac2104
Julien Girault <julien.emac@gmail.com>
2014-02-13 19:07:29 -0800

    SC-2168: Also handle custom modules not recognized by Module Builder

--- sugarcrm/upgrade/scripts/post/4_FixClassConstructor.php
+++ sugarcrm/upgrade/scripts/post/4_FixClassConstructor.php
@@ -27,26 +27,49 @@ class SugarUpgradeFixClassConstructor extends UpgradeScript
 
     public function run()
     {
-        // only run this when coming from a version lower than 7.2.0
+        // Only run this when coming from a version lower than 7.2.0
         if (version_compare($this->from_version, '7.2.0', '>=')) {
             return;
         }
 
+        // Find all the classes we want to convert.
+        $customModules = array();
+        foreach (glob('modules/*/*_sugar.php') as $customFile) {
+            $moduleName = str_replace('_sugar', '', pathinfo($customFile, PATHINFO_FILENAME));
+            $customModules[] = $moduleName;
+        }
+        $customModules = array_flip($customModules);
+
+        // Go through all the modules using the ModuleBuilder
         $mB = new ModuleBuilder();
         $mB->getPackages();
-
         foreach ($mB->packages as $package) {
             $this->log("FixClassConstructor: Found a custom package {$package->name}");
-            foreach ($package->modules as $module) {
-                $this->replaceCustomModuleClasses($module);
+            foreach ($package->modules as $mbModule) {
+                if (!isset($customModules[$mbModule->key_name])) {
+                    continue;
+                }
+                $this->replaceCustomModuleClasses($mbModule);
+                unset($customModules[$mbModule->key_name]);
             }
         }
+        $customModules = array_flip($customModules);
+
+        // Treat modules that have not been found by the ModuleBuilder
+        foreach ($customModules as $moduleName) {
+            $this->log("FixClassConstructor: Found a custom module {$moduleName} not recognized by ModuleBuilder");
+            $this->replaceCustomModuleClassesByReflection($moduleName);
+        }
+
+        // TODO: ModuleBuilder outputs some blank lines. Verify why.
         ob_end_clean();
     }
 
     /**
      * Rebuild the custom module classes so they have the the proper
      * __construct() instead of class names.
+     *
+     * This method uses the ModuleBuilder class to populate the class template.
      *
      * @param MBModule $mbModule ModuleBuilder Module to be replaced.
      *
@@ -56,66 +79,131 @@ class SugarUpgradeFixClassConstructor extends UpgradeScript
      */
     private function replaceCustomModuleClasses($mbModule)
     {
-        $class = array();
-        $class['name'] = $mbModule->key_name;
+        $moduleName = $mbModule->key_name;
 
-        if (!file_exists('modules/' . $class['name'] . '/' . $class['name'] . '_sugar.php')) {
-            return false;
-        }
-
-        $class['table_name'] = strtolower($class['name']);
-        $class['extends'] = 'Basic';
-        $class['requires'] = array();
-
-        $class['team_security'] = !empty($mbModule->config['team_security']);
-
-        if (empty($mbModule->config['audit'])) {
-            $class['audited'] = 'false';
-        } else {
-            $class['audited'] = 'true';
-        }
-
-        if (empty($mbModule->config['activity_enabled'])) {
-            $class['activity_enabled'] = 'false';
-        } else {
-            $class['activity_enabled'] = 'true';
-        }
-
-        if (empty($mbModule->config['acl'])) {
-            $class['acl'] = 'false';
-        } else {
-            $class['acl'] = 'true';
-        }
-
-        $class['templates'] = "'basic'";
-        foreach ($mbModule->iTemplate as $template) {
-            if (!empty($mbModule->config[$template])) {
-                $class['templates'] .= ",'$template'";
-            }
-        }
-
+        $parentClass = 'Basic';
         foreach ($mbModule->config['templates'] as $template => $a) {
             if ($template == 'basic') {
                 continue;
             }
-            $class['templates'] .= ",'$template'";
-            $class['extends'] = ucFirst($template);
-            $class['requires'][] = 'include/SugarObjects/templates/' . $template . '/' . ucfirst($template) . '.php';
+            $parentClass = ucFirst($template);
         }
-        $class['importable'] = $mbModule->config['importable'];
+
         $mbModule->mbvardefs->updateVardefs();
-        $class['fields'] = $mbModule->mbvardefs->vardefs['fields'];
-        $class['fields_string'] = var_export_helper($mbModule->mbvardefs->vardef['fields']);
-        $relationship = array();
-        $class['relationships'] = var_export_helper($mbModule->mbvardefs->vardef['relationships']);
-        $smarty = new Sugar_Smarty ();
+        $fields = $mbModule->mbvardefs->vardefs['fields'];
+
+        $params = array(
+            'isImportable' => !empty($mbModule->config['importable']),
+            'teamSecurityEnabled' => !empty($mbModule->config['team_security']),
+            'acl' => !empty($mbModule->config['acl']),
+        );
+
+        $content = $this->populateClassTemplate($moduleName, $parentClass, $fields, $params);
+        $this->writeCustomClass($moduleName, $content);
+    }
+
+    /**
+     * Rebuild the custom module classes so they have the the proper
+     * __construct() instead of class names.
+     *
+     * This method uses ReflectionClass to populate the class template.
+     *
+     * @param string $moduleName The module key name.
+     */
+    private function replaceCustomModuleClassesByReflection($moduleName)
+    {
+        $className = $moduleName . '_sugar';
+
+        $reflectionClass = new ReflectionClass($className);
+        $parentClass = get_parent_class($className);
+
+        $fields = array();
+        $notVardefs = array('new_schema', 'module_dir', 'object_name', 'table_name', 'importable');
+        foreach ($reflectionClass->getProperties() as $property) {
+            if ($property->class !== $className) {
+                continue;
+            }
+            $name = $property->name;
+            if (in_array($name, $notVardefs)) {
+                continue;
+            }
+            $fields[$name] = $name;
+        }
+
+        $isImportable = $reflectionClass->getProperty('importable')->getValue(new $className);
+
+        $teamSecurityEnabled = true;
+        $disable_row_level_security = $reflectionClass->getProperty('disable_row_level_security');
+        if (!empty($disable_row_level_security)) {
+            $teamSecurityEnabled = $disable_row_level_security->class !== $className;
+        }
+
+        $acl = false;
+        $beanImplements = $reflectionClass->getMethod('bean_implements');
+        if (!empty($beanImplements)) {
+            $acl = $beanImplements->class === $className;
+        }
+
+        $params = array(
+            'isImportable' => $isImportable,
+            'teamSecurityEnabled' => $teamSecurityEnabled,
+            'acl' => $acl,
+        );
+
+        $content = $this->populateClassTemplate($moduleName, $parentClass, $fields, $params);
+        $this->writeCustomClass($moduleName, $content);
+    }
+
+    /**
+     * Populate the class template.
+     *
+     * @param string $moduleName The module name.
+     * @param array $parentClass The name of the extension class.
+     * @param array $fields The list of fields of this module.
+     * @param array $params The list of boolean parameters.
+     * Expects `isImportable`, `teamSecurityEnabled` and `acl` to be defined.
+     *
+     * @return string The file content, ready to be written.
+     */
+    private function populateClassTemplate($moduleName, $parentClass, $fields, $params)
+    {
+        $class = array();
+        $class['name'] = $moduleName;
+        $class['table_name'] = strtolower($moduleName);
+
+        $class['requires'] = array();
+        if ($parentClass !== 'Basic') {
+            $template = strtolower($parentClass);
+            $class['requires'][] = 'include/SugarObjects/templates/' . $template . '/' . $parentClass . '.php';
+        }
+
+        $class['extends'] = $parentClass;
+
+        $class['fields'] = $fields;
+
+        $class['team_security'] = $params['teamSecurityEnabled'];
+        $class['acl'] = $params['acl'];
+        $class['importable'] = $params['isImportable'];
+
+        $smarty = new Sugar_Smarty();
         $smarty->left_delimiter = '{{';
         $smarty->right_delimiter = '}}';
         $smarty->assign('class', $class);
-
-        //write sugar generated class
-        $this->log("FixClassConstructor: Replace {$class['name']}_sugar.php for module: {$mbModule->key_name}");
         $content = $smarty->fetch('modules/ModuleBuilder/tpls/MBModule/Class.tpl');
-        sugar_file_put_contents_atomic('modules/' . $class['name'] . '/' . $class['name'] . '_sugar.php', $content);
+
+        return $content;
+    }
+
+    /**
+     * Override the existing file.
+     *
+     * @param string $moduleName The module name.
+     * @param string $content The content of the file.
+     */
+    private function writeCustomClass($moduleName, $content)
+    {
+        //write sugar generated class
+        $this->log("FixClassConstructor: Replace {$moduleName}_sugar.php for module: {$moduleName}");
+        sugar_file_put_contents_atomic('modules/' . $moduleName . '/' . $moduleName . '_sugar.php', $content);
     }
 }
