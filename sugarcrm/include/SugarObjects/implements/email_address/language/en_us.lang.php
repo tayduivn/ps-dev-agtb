68344b1ad6c7a27c955679ca73ff9a00f9290f68
facevedo-sugarcrm <facevedo@sugarcrm.com>
2014-02-16 16:58:15 -0800

    UIUX-1509: Update capitalization style

--- sugarcrm/include/SugarObjects/implements/email_address/language/en_us.lang.php
+++ sugarcrm/include/SugarObjects/implements/email_address/language/en_us.lang.php
@@ -29,11 +29,11 @@ if(!defined('sugarEntry') || !sugarEntry) die('Not A Valid Entry Point');
 
 $mod_strings = array (
     'LBL_ANY_EMAIL' => 'Email:',
-    'LBL_EMAIL' => 'Email Address:',
-    'LBL_LIST_EMAIL_ADDRESS' => 'Email Address',
-    'LBL_OTHER_EMAIL_ADDRESS' => 'Other Email:',
-    'LBL_EMAIL_ADDRESS' => 'Email Address',
-    'LBL_EMAIL_ADDRESSES' => 'Email Address(es)',
-    'LBL_INVALID_EMAIL'=>'Invalid Email:',
-    'LBL_EMAIL_OPT_OUT' => 'Email Opt Out:',
+    'LBL_EMAIL' => 'Email address:',
+    'LBL_LIST_EMAIL_ADDRESS' => 'Email address',
+    'LBL_OTHER_EMAIL_ADDRESS' => 'Other email:',
+    'LBL_EMAIL_ADDRESS' => 'Email address',
+    'LBL_EMAIL_ADDRESSES' => 'Email address(es)',
+    'LBL_INVALID_EMAIL'=>'Invalid email:',
+    'LBL_EMAIL_OPT_OUT' => 'Email opt out:',
 );
