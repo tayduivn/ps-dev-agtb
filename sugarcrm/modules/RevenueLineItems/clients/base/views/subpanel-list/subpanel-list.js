86f605b5c46652fc68ec2f675da18518af322d20
jwhitcraft <jwhitcraft@sugarcrm.com>
2014-03-04 15:30:09 -0500

    SFA-2396: Hide Currency Case Columns when Forecast is configured
    - Added the logic to make sure that if a specific _case column is hidden to not show it in the subpanel
    - Added Jasmine Tests to cover new functionality.

--- sugarcrm/modules/RevenueLineItems/clients/base/views/subpanel-list/subpanel-list.js
+++ sugarcrm/modules/RevenueLineItems/clients/base/views/subpanel-list/subpanel-list.js
@@ -8,9 +8,14 @@
  * you are agreeing unconditionally that Company will be bound by the MSA and
  * certifying that you have authority to bind Company accordingly.
  *
- * Copyright  2004-2013 SugarCRM Inc.  All rights reserved.
+ * Copyright (C) 2004-2013 SugarCRM Inc.  All rights reserved.
  */
 
+/**
+ * @class BaseRevenueLineItemsSubpanelListView
+ * @alias SUGAR.App.view.views.BaseRevenueLineItemsSubpanelListView
+ * @extends BaseSubpanelListView
+ */
 ({
     /**
      * Custom Subpanel Layout for Revenue Line Items
@@ -28,16 +33,27 @@
      * @returns {{default: Array, available: Array, visible: Array, options: Array}}
      */
     parseFields : function() {
-        var catalog = this._super('parseFields');
+        var catalog = this._super('parseFields'),
+            config = app.metadata.getModule('Forecasts', 'config'),
+            isForecastSetup = config.is_setup;
 
         // if forecast is not setup, we need to make sure that we hide the commit_stage field
-        if (!app.metadata.getModule("Forecasts", "config").is_setup) {
-            _.each(catalog, function (group, i) {
+        _.each(catalog, function (group, i) {
+            if (isForecastSetup) {
+                catalog[i] = _.filter(group, function(fieldMeta) {
+                    if (fieldMeta.name.indexOf('_case') != -1) {
+                        var field = 'show_worksheet_' + fieldMeta.name.replace('_case', '');
+                        return (config[field] == 1);
+                    }
+
+                    return true;
+                });
+            } else {
                 catalog[i] = _.filter(group, function (fieldMeta) {
                     return (fieldMeta.name != 'commit_stage');
                 });
-            });
-        }
+            }
+        });
 
         return catalog;
     }
