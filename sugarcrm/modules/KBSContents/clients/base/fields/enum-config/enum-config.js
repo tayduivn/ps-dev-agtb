5ddea96e77a410e1ac27650ddf27786f4a1b9a76
Alexander Rymarchik <a.rymarchik@intetics.com>
2014-03-04 18:09:27 +0300

    RS-322: Added extended from `enum` field to show saved languages.

---
+++ sugarcrm/modules/KBSContents/clients/base/fields/enum-config/enum-config.js
@@ -0,0 +1,67 @@
+/*
+ * By installing or using this file, you are confirming on behalf of the entity
+ * subscribed to the SugarCRM Inc. product ("Company") that Company is bound by
+ * the SugarCRM Inc. Master Subscription Agreement ("MSA"), which is viewable at:
+ * http://www.sugarcrm.com/master-subscription-agreement
+ *
+ * If Company is not bound by the MSA, then by installing or using this file
+ * you are agreeing unconditionally that Company will be bound by the MSA and
+ * certifying that you have authority to bind Company accordingly.
+ *
+ * Copyright  2004-2014 SugarCRM Inc.  All rights reserved.
+ */
+
+({
+    extendsFrom: 'EnumField',
+
+   /**
+    * {@inheritDoc}
+    */
+    loadEnumOptions: function (fetch, callback) {
+        var module = this.def.module || this.module,
+            optKey = this.def.key || this.name,
+            config = app.metadata.getModule(module, 'config') || {};
+        this._setItems(config[optKey]);
+        fetch = fetch || false;
+
+        if (fetch || !this.items) {
+            var url = app.api.buildURL(module, 'config', null, {});
+            app.api.call('read', url, null, {
+                success:  _.bind(function (data) {
+                    this._setItems(data[optKey]);
+                    callback.call(this);
+                }, this)
+            });
+        }
+    },
+
+    /**
+     * {@inheritDoc}
+     */
+    _loadTemplate: function () {
+        this.type = 'enum';
+        this._super('_loadTemplate');
+        this.type = this.def.type;
+    },
+
+    /**
+     * Sets current items.
+     * @param {Array} values Values to set into items.
+     */
+    _setItems: function (values) {
+        var result = {},
+            def = null;
+        _.each(values, function (val) {
+            var tmp = _.omit(val, 'primary');
+            _.extend(result, tmp);
+            if (val.primary) {
+                def = _.keys(tmp).first();
+            }
+        });
+        this.items = result;
+        if (def) {
+            this.defaultOnUndefined = false;
+            this.model.setDefaultAttribute(this.name, def);
+        }
+    }
+})
