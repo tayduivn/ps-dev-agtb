50cf956a32780f9f17628d257dc08a02c3f1e504
Filipe Guerra <alias.mac@gmail.com>
2014-03-15 01:58:56 +0100

    Apply code style conventions (relates with SC-2535)

--- sugarcrm/modules/Documents/clients/base/views/subpanel-list/subpanel-list.php
+++ sugarcrm/modules/Documents/clients/base/views/subpanel-list/subpanel-list.php
@@ -12,55 +12,53 @@
  * Copyright  2004-2013 SugarCRM Inc.  All rights reserved.
  */
 $viewdefs['Documents']['base']['view']['subpanel-list'] = array(
-  'panels' =>
-  array(
-    array(
-      'name' => 'panel_header',
-      'label' => 'LBL_PANEL_1',
-      'fields' =>
-      array(
+    'panels' => array(
         array(
-          'name' => 'document_name',
-          'label' => 'LBL_LIST_DOCUMENT_NAME',
-          'enabled' => true,
-          'default' => true,
-          'link' => true,
+            'name' => 'panel_header',
+            'label' => 'LBL_PANEL_1',
+            'fields' => array(
+                array(
+                    'name' => 'document_name',
+                    'label' => 'LBL_LIST_DOCUMENT_NAME',
+                    'enabled' => true,
+                    'default' => true,
+                    'link' => true,
+                ),
+                array(
+                    'name' => 'filename',
+                    'label' => 'LBL_LIST_FILENAME',
+                    'enabled' => true,
+                    'default' => true,
+                    'readonly' => true
+                ),
+                array(
+                    'name' => 'category_id',
+                    'label' => 'LBL_LIST_CATEGORY',
+                    'enabled' => true,
+                    'default' => true,
+                ),
+                array(
+                    'name' => 'doc_type',
+                    'label' => 'LBL_LIST_DOC_TYPE',
+                    'enabled' => true,
+                    'default' => true,
+                    'readonly' => true,
+                ),
+                array(
+                    'name' => 'status_id',
+                    'label' => 'LBL_LIST_STATUS',
+                    'enabled' => true,
+                    'default' => true,
+                ),
+                array(
+                    'name' => 'active_date',
+                    'label' => 'LBL_LIST_ACTIVE_DATE',
+                    'enabled' => true,
+                    'default' => true,
+                ),
+            ),
         ),
-        array(
-          'name' => 'filename',
-          'label' => 'LBL_LIST_FILENAME',
-          'enabled' => true,
-          'default' => true,
-          'readonly' => true
-        ),
-        array(
-          'name' => 'category_id',
-          'label' => 'LBL_LIST_CATEGORY',
-          'enabled' => true,
-          'default' => true,
-        ),
-        array(
-          'name' => 'doc_type',
-          'label' => 'LBL_LIST_DOC_TYPE',
-          'enabled' => true,
-          'default' => true,
-          'readonly' => true,
-        ),
-        array(
-          'name' => 'status_id',
-          'label' => 'LBL_LIST_STATUS',
-          'enabled' => true,
-          'default' => true,
-        ),
-        array(
-          'name' => 'active_date',
-          'label' => 'LBL_LIST_ACTIVE_DATE',
-          'enabled' => true,
-          'default' => true,
-        ),
-      ),
     ),
-  ),
     'rowactions' => array(
         'actions' => array(
             array(
