3c68befffc66425c8fbd379dfb2cf43233b2aafd
Ruby Wang <rubywang1210@gmail.com>
2014-03-21 16:39:52 -0700

    SC-2270 Unit tests for bool.js changes
    changes help bool.js in rendering as a dropdown in special cases
    such as massupdate

---
+++ sugarcrm/tests/clients/base/fields/bool/bool.js
@@ -0,0 +1,172 @@
+describe('Base.Field.Bool', function() {
+    var app;
+    beforeEach(function() {
+        app = SugarTest.app;
+    });
+
+    afterEach(function() {
+        app.cache.cutAll();
+        sinon.collection.restore();
+        Handlebars.Templates = {};
+    });
+
+    describe('format & unformat', function() {
+        var field;
+        beforeEach(function() {
+            field = SugarTest.createField('base', 'bool', 'bool', 'detail');
+        });
+
+        afterEach(function() {
+            field.dispose();
+        });
+
+        it('should format the value', function() {
+            expect(field.format('0')).toEqual(false);
+            expect(field.format('1')).toEqual(true);
+            expect(field.format(false)).toEqual(false);
+            expect(field.format(true)).toEqual(true);
+        });
+
+        it('should unformat the value', function() {
+            expect(field.unformat('0')).toEqual(false);
+            expect(field.unformat('1')).toEqual(true);
+            expect(field.unformat(false)).toEqual(false);
+            expect(field.unformat(true)).toEqual(true);
+        });
+    });
+
+    describe('bindDomChange', function() {
+        var field;
+        beforeEach(function() {
+            SugarTest.testMetadata.init();
+            SugarTest.loadHandlebarsTemplate('bool', 'field', 'base', 'edit');
+            SugarTest.testMetadata.set();
+            field = SugarTest.createField('base', 'bool', 'bool', 'edit');
+        });
+
+        afterEach(function() {
+            field.dispose();
+            SugarTest.testMetadata.dispose();
+        });
+
+        it('should update the model on value change', function() {
+            field.render();
+            var modelSpy = sinon.collection.spy(field.model, 'set');
+            field.$(field.fieldTag).attr('checked', true).trigger('change');
+            expect(modelSpy).toHaveBeenCalledWith('bool', true);
+            field.$(field.fieldTag).attr('checked', false).trigger('change');
+            expect(modelSpy).toHaveBeenCalledWith('bool', false);
+        });
+    });
+
+    describe('render', function() {
+        describe('render detail', function() {
+            var field;
+            beforeEach(function() {
+                SugarTest.testMetadata.init();
+                SugarTest.loadHandlebarsTemplate('bool', 'field', 'base', 'detail');
+                SugarTest.testMetadata.set();
+                field = SugarTest.createField('base', 'bool', 'bool', 'detail');
+            });
+
+            afterEach(function() {
+                field.dispose();
+                SugarTest.testMetadata.dispose();
+            });
+
+            it('should render as a disabled checkbox and toggle according to the value', function() {
+                field.def.default = false;
+                field.render();
+                expect(field.$(field.fieldTag)).toHaveAttr('disabled');
+                expect(field.$(field.fieldTag)).not.toHaveAttr('checked');
+                field.model.set(field.name, true);
+                expect(field.$(field.fieldTag)).toHaveAttr('checked');
+            });
+        });
+
+        describe('render edit', function() {
+            var field;
+            beforeEach(function() {
+                SugarTest.testMetadata.init();
+                SugarTest.loadHandlebarsTemplate('bool', 'field', 'base', 'edit');
+                SugarTest.testMetadata.set();
+                field = SugarTest.createField('base', 'bool', 'bool', 'edit');
+            });
+
+            afterEach(function() {
+                field.dispose();
+                SugarTest.testMetadata.dispose();
+            });
+
+            it('should render with default values in def', function() {
+                field.def.text = 'text';
+                field.def.tabindex = 1;
+                field.render();
+                expect(field.$('label')).toExist();
+                expect(field.$(field.fieldTag)).toHaveAttr('tabindex');
+            });
+
+            it('should render with no default values in def', function() {
+                field.render();
+                expect(field.$('label')).not.toExist();
+            });
+
+        });
+
+        describe('render dropdown', function() {
+            var field;
+            beforeEach(function() {
+                SugarTest.testMetadata.init();
+                SugarTest.loadHandlebarsTemplate('bool', 'field', 'base', 'dropdown');
+                SugarTest.testMetadata.set();
+                field = SugarTest.createField('base', 'bool', 'bool', 'massupdate');
+            });
+
+            afterEach(function() {
+                field.dispose();
+                SugarTest.testMetadata.dispose();
+            });
+
+            it('should call select2 with required params on massupdate', function() {
+                var jqueryStubReturnObj = {
+                        'select2': sinon.collection.spy(),
+                        'on': $.noop,
+                        'off': $.noop
+                    },
+                    jqueryStub = sinon.collection.stub(field, '$', function(val) {
+                        return jqueryStubReturnObj;
+                    });
+                field.render();
+                expect(jqueryStub).toHaveBeenCalledWith(field.select2fieldTag);
+                expect(jqueryStubReturnObj.select2).toHaveBeenCalledWith({'minimumResultsForSearch': -1});
+            });
+
+            it('should fall back to the dropdown template if attempting to render the massupdate template', function() {
+                var getFieldSpy = sinon.collection.spy(app.template, 'getField');
+                field.render();
+                expect(getFieldSpy).toHaveBeenCalledWith('bool', 'massupdate', undefined, 'dropdown');
+            });
+        });
+
+        describe('render disabled', function() {
+            var field;
+            beforeEach(function() {
+                SugarTest.testMetadata.init();
+                SugarTest.loadHandlebarsTemplate('bool', 'field', 'base', 'edit');
+                SugarTest.testMetadata.set();
+                field = SugarTest.createField('base', 'bool', 'bool', 'disabled');
+            });
+
+            afterEach(function() {
+                field.dispose();
+                SugarTest.testMetadata.dispose();
+            });
+
+            it('should render disabled when the action is disabled', function() {
+                field.render();
+                expect(field.action).toEqual('disabled');
+                expect(field.$(field.fieldTag)).toHaveAttr('disabled');
+            });
+        });
+    });
+});
