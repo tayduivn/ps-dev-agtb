5e6b2cd2916a5ddd303e8388bb0f1ecd4b8f0b3d
JimB <jbartek@sugarcrm.com>
2014-03-25 10:17:59 -0400

    BR-1371 - db->quote is not returning a quoted value, hardcode it to return a quoted val if not quoted already

--- sugarcrm/include/SearchForm/SearchForm2.php
+++ sugarcrm/include/SearchForm/SearchForm2.php
@@ -1038,7 +1038,7 @@ require_once('include/EditView/EditView2.php');
                                  }elseif(!empty($parms['query_type']) && $parms['query_type'] == 'format'){
                                      // "stringify" the field value if it isn't wrapped in quotes already
                                      if (substr($field_value, 0, 1) != "'" && substr($field_value, -1) != "'") {
-                                         $field_value = $GLOBALS['db']->quote($field_value);
+                                         $field_value = $GLOBALS['db']->quoted($field_value);
                                      }
                                      $stringFormatParams = array(0 => $field_value, 1 => $GLOBALS['current_user']->id);
                                      $where .= "{$db_field} $in (" . string_format(
