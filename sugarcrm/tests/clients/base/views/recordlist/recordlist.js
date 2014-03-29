18ed121a45c49235fbbdf05dbef719e60b573106
Julien Girault <julien.emac@gmail.com>
2014-03-28 17:11:07 -0700

    SC-2607: Reformat unit tests

--- sugarcrm/tests/clients/base/views/recordlist/recordlist.js
+++ sugarcrm/tests/clients/base/views/recordlist/recordlist.js
@@ -1,7 +1,7 @@
-describe("Base.View.RecordList", function () {
+describe('Base.View.RecordList', function() {
     var view, layout, app, meta, moduleName = 'Cases';
 
-    beforeEach(function () {
+    beforeEach(function() {
         SugarTest.testMetadata.init();
         SugarTest.loadComponent('base', 'view', 'list');
         SugarTest.loadComponent('base', 'view', 'flex-list');
@@ -40,7 +40,8 @@ describe("Base.View.RecordList", function () {
         app = SUGAR.App;
     });
 
-    afterEach(function () {
+    afterEach(function() {
+        sinon.collection.restore();
         layout.dispose();
         view.dispose();
         SugarTest.testMetadata.dispose();
@@ -50,71 +51,76 @@ describe("Base.View.RecordList", function () {
         view = null;
     });
 
-    describe('adding actions to list view', function () {
+    describe('adding actions to list view', function() {
 
-        it('should return my_favorite field when calling getFieldNames', function () {
+        it('should return my_favorite field when calling getFieldNames', function() {
             var fields = view.getFieldNames();
-            expect(_.indexOf(fields, 'my_favorite')).toBeGreaterThan(-1);
+            expect(_.contains(fields, 'my_favorite')).toBeTruthy();
         });
 
-        it('should return my_favorite field and save to context for filtering', function () {
-            expect(_.indexOf(view.context._recordListFields, 'my_favorite')).toBeGreaterThan(-1);
+        it('should return my_favorite field and save to context for filtering', function() {
+            expect(_.contains(view.context._recordListFields, 'my_favorite')).toBeTruthy();
         });
 
-        it('should have added favorite field', function () {
+        it('should have added favorite field', function() {
             view.render();
-            expect(view.leftColumns[0].fields[1]).toEqual({type:'favorite'});
+            expect(view.leftColumns[0].fields[1]).toEqual({type: 'favorite'});
         });
 
-        it('should have added favorite field', function () {
+        it('should have added favorite field', function() {
             view.dispose();
 
-            SugarTest.testMetadata.updateModuleMetadata("Cases", {
+            SugarTest.testMetadata.updateModuleMetadata('Cases', {
                 favoritesEnabled: false
             });
-            var nofavoriteview = SugarTest.createView("base", "Cases", "recordlist", null, null);
+            var nofavoriteview = SugarTest.createView('base', 'Cases', 'recordlist', null, null);
             nofavoriteview.render();
             var actualFavoriteField = _.where(nofavoriteview.leftColumns[0].fields, {type: 'favorite'});
             expect(actualFavoriteField.length).toBe(0);
             nofavoriteview.dispose();
         });
 
-        it('should have added row actions', function () {
+        it('should have added row actions', function() {
             view.render();
             expect(view.leftColumns[0].fields[2]).toEqual({
-                type:'editablelistbutton',
-                label:'LBL_CANCEL_BUTTON_LABEL',
-                name:'inline-cancel',
-                css_class:'btn-link btn-invisible inline-cancel'
+                type: 'editablelistbutton',
+                label: 'LBL_CANCEL_BUTTON_LABEL',
+                name: 'inline-cancel',
+                css_class: 'btn-link btn-invisible inline-cancel'
             });
             expect(view.rightColumns[0].fields[1]).toEqual({
-                type:'editablelistbutton',
-                label:'LBL_SAVE_BUTTON_LABEL',
-                name:'inline-save',
-                css_class:'btn-primary'
+                type: 'editablelistbutton',
+                label: 'LBL_SAVE_BUTTON_LABEL',
+                name: 'inline-save',
+                css_class: 'btn-primary'
             });
             expect(view.rightColumns[0].css_class).toEqual('overflow-visible');
         });
     });
 
     describe('hasUnsavedChanges', function() {
+
         beforeEach(function() {
-            view.collection = new app.data.createBeanCollection('Cases', [{
-                id: 1,
-                name: 'First',
-                case_number: 123,
-                description: 'first description'
-            },{
-                id: 2,
-                name: 'Second',
-                case_number: 123,
-                description: 'second description'
-            },{
-                id: 3,
-                name: 'Third',
-                case_number: 123,
-                description: 'third description'
-            }]);
+            view.collection = new app.data.createBeanCollection('Cases', [
+                {
+                    id: 1,
+                    name: 'First',
+                    case_number: 123,
+                    description: 'first description'
+                },
+                {
+                    id: 2,
+                    name: 'Second',
+                    case_number: 123,
+                    description: 'second description'
+                },
+                {
+                    id: 3,
+                    name: 'Third',
+                    case_number: 123,
+                    description: 'third description'
+                }
+            ]);
             view.render();
         });
 
@@ -212,34 +218,33 @@ describe("Base.View.RecordList", function () {
         });
 
 
-        describe("Warning delete", function() {
-            var sinonSandbox, alertShowStub, routerStub;
+        describe('Warning delete', function() {
+            var alertShowStub, routerStub;
+
             beforeEach(function() {
-                sinonSandbox = sinon.sandbox.create();
-                routerStub = sinonSandbox.stub(app.router, "navigate");
-                sinonSandbox.stub(Backbone.history, "getFragment");
-                alertShowStub = sinonSandbox.stub(app.alert, "show");
+                routerStub = sinon.collection.stub(app.router, 'navigate');
+                sinon.collection.stub(Backbone.history, 'getFragment');
+                alertShowStub = sinon.collection.stub(app.alert, 'show');
             });
 
-            afterEach(function() {
-                sinonSandbox.restore();
-            });
-
-            it("should not alert warning message if _modelToDelete is not defined", function() {
-                app.routing.triggerBefore("route");
+            it('should not alert warning message if _modelToDelete is not defined', function() {
+                app.routing.triggerBefore('route');
                 expect(alertShowStub).not.toHaveBeenCalled();
             });
-            it("should return true if _modelToDelete is not defined", function() {
-                sinonSandbox.stub(view, 'warnDelete');
+
+            it('should return true if _modelToDelete is not defined', function() {
+                sinon.collection.stub(view, 'warnDelete');
                 expect(view.beforeRouteDelete()).toBeTruthy();
             });
-            it("should return false if _modelToDelete is defined (to prevent routing to other views)", function() {
-                sinonSandbox.stub(view, 'warnDelete');
+
+            it('should return false if _modelToDelete is defined (to prevent routing to other views)', function() {
+                sinon.collection.stub(view, 'warnDelete');
                 view._modelToDelete = new Backbone.Model();
                 expect(view.beforeRouteDelete()).toBeFalsy();
             });
-            it("should redirect the user to the targetUrl", function() {
-                var unbindSpy = sinonSandbox.spy(view, 'unbindBeforeRouteDelete');
+
+            it('should redirect the user to the targetUrl', function() {
+                var unbindSpy = sinon.collection.spy(view, 'unbindBeforeRouteDelete');
                 view._modelToDelete = new Backbone.Model();
                 view._currentUrl = 'Accounts';
                 view._targetUrl = 'Contacts';
@@ -249,63 +254,66 @@ describe("Base.View.RecordList", function () {
             });
         });
     });
-    describe("filterMeta", function() {
-        var sinonSandbox, alertShowStub, routerStub;
+
+    describe('_filterMeta', function() {
+
         beforeEach(function() {
-            sinonSandbox = sinon.sandbox.create();
             meta = {
-               selection: {
-                   actions: [{
-                       'name': 'calc_field_button',
-                       'type': 'button',
-                       'label': 'LBL_UPDATE_CALC_FIELDS',
-                       'events': { 'click': 'list:updatecalcfields:fire'
-                       },
-                       'acl_action': 'massupdate'
-                   }]
-               }
-           };
+                selection: {
+                    actions: [
+                        {
+                            'name': 'calc_field_button',
+                            'type': 'button',
+                            'label': 'LBL_UPDATE_CALC_FIELDS',
+                            'events': {
+                                'click': 'list:updatecalcfields:fire'
+                            },
+                            'acl_action': 'massupdate'
+                        }
+                    ]
+                }
+            };
         });
 
-        afterEach(function() {
-            sinonSandbox.restore();
-        });
-
-        it("should leave in the calc_field_button when the user is a developer for the module and the module contains calc fields", function() {
-            sinonSandbox.stub(app.acl, "hasAccess").returns(true);
-            var options = {
-                context: {get: function() {
-                    return {fields: [
-                        {name: "foo", calculated: true, formula: "$name"}
-                    ]}
-                }}
-            };
-            meta = view._filterMeta(meta, options);
-            expect(meta.selection.actions).not.toEqual([]);
-        });
-        it("should remove the calc_field_button when the user is not a developer for the module", function() {
-            sinonSandbox.stub(app.acl, "hasAccess").returns(false);
-            var options = {
-                context: {get: function() {
-                    return {fields: [
-                        {name: "foo", calculated: true, formula: "$name"}
-                    ]}
-                }}
-            };
-            meta = view._filterMeta(meta, options);
-            expect(meta.selection.actions).toEqual([]);
-        });
-        it("should remove the calc_field_button when the module has no calc fields", function() {
-            sinonSandbox.stub(app.acl, "hasAccess").returns(true);
-            var options = {
-                context: {get: function() {
-                    return {fields: [
-                        {name: "foo"}
-                    ]}
-                }}
-            };
-            meta = view._filterMeta(meta, options);
-            expect(meta.selection.actions).toEqual([]);
+        using('different values for user developer access and module contains calc fields or not', [
+            {
+                hasAccess: true,
+                fields: [
+                    {name: 'foo', calculated: true, formula: '$name'}
+                ],
+                leave: true
+            },
+            {
+                hasAccess: false,
+                fields: [
+                    {name: 'foo', calculated: true, formula: '$name'}
+                ],
+                leave: false
+            },
+            {
+                hasAccess: true,
+                fields: [
+                    {name: 'foo'}
+                ],
+                leave: false
+            }
+        ], function(params) {
+            it('should handle the calc_field_button properly', function() {
+                sinon.collection.stub(app.acl, 'hasAccess').returns(params.hasAccess);
+                var options = {
+                    context: {
+                        get: function() {
+                            return { fields: params.fields };
+                        }
+                    }
+                };
+                meta = view._filterMeta(meta, options);
+                if (params.leave) {
+                    expect(meta.selection.actions).not.toEqual([]);
+                } else {
+                    expect(meta.selection.actions).toEqual([]);
+                }
+            });
         });
     });
 
@@ -313,13 +321,11 @@ describe("Base.View.RecordList", function () {
         var models;
 
         beforeEach(function() {
-
             models = [
                 new Backbone.Model({ id: _.uniqueId('_setRowFields-model-id-') }),
                 new Backbone.Model({ id: _.uniqueId('_setRowFields-model-id-') }),
                 new Backbone.Model({ id: _.uniqueId('_setRowFields-model-id-') })
             ];
-
             _.each(models, function(model) {
                 view.fields[_.uniqueId('_setRowFields-field-id-')] = { model: model };
                 view.fields[_.uniqueId('_setRowFields-field-id-')] = { model: model };
