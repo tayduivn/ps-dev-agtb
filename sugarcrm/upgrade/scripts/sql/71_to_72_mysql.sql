5d34436c65233c38449358fca56c68c2253610bb
Julien Girault <julien.emac@gmail.com>
2014-04-04 10:12:48 -0700

    SC-2636: Add license headers

--- sugarcrm/upgrade/scripts/sql/71_to_72_mysql.sql
+++ sugarcrm/upgrade/scripts/sql/71_to_72_mysql.sql
@@ -1,2 +1,14 @@
+/*
+ * By installing or using this file, you are confirming on behalf of the entity
+ * subscribed to the SugarCRM Inc. product ("Company") that Company is bound by
+ * the SugarCRM Inc. Master Subscription Agreement ("MSA"), which is viewable at:
+ * http://www.sugarcrm.com/master-subscription-agreement
+ *
+ * If Company is not bound by the MSA, then by installing or using this file
+ * you are agreeing unconditionally that Company will be bound by the MSA and
+ * certifying that you have authority to bind Company accordingly.
+ *
+ * Copyright (C) 2004-2014 SugarCRM Inc. All rights reserved.
+ */
 alter table `dashboards` change `view` `view_name` varchar(100) NULL;
 alter table `weblogichooks` change `module_name` `webhook_target_module` varchar(100) NULL;
