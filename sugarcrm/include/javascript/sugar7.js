9e3cb702137e1c529adf1bacfc6308c7331e391b
Nima Dehnashi <ndehnashi@sugarcrm.com>
2014-04-11 18:08:09 -0700

    [SC-2662]: sugar7.js formatting fixes.

--- sugarcrm/include/javascript/sugar7.js
+++ sugarcrm/include/javascript/sugar7.js
@@ -1,29 +1,15 @@
-/*********************************************************************************
- * The contents of this file are subject to the SugarCRM Master Subscription
- * Agreement (""License"") which can be viewed at
- * http://www.sugarcrm.com/crm/master-subscription-agreement
- * By installing or using this file, You have unconditionally agreed to the
- * terms and conditions of the License, and You may not use this file except in
- * compliance with the License.  Under the terms of the license, You shall not,
- * among other things: 1) sublicense, resell, rent, lease, redistribute, assign
- * or otherwise transfer Your rights to the Software, and 2) use the Software
- * for timesharing or service bureau purposes such as hosting the Software for
- * commercial gain and/or for the benefit of a third party.  Use of the Software
- * may be subject to applicable fees and any use of the Software without first
- * paying applicable fees is strictly prohibited.  You do not have the right to
- * remove SugarCRM copyrights from the source code or user interface.
+/*
+ * By installing or using this file, you are confirming on behalf of the entity
+ * subscribed to the SugarCRM Inc. product ("Company") that Company is bound by
+ * the SugarCRM Inc. Master Subscription Agreement ("MSA"), which is viewable at:
+ * http://www.sugarcrm.com/master-subscription-agreement
  *
- * All copies of the Covered Code must include on each user interface screen:
- *  (i) the ""Powered by SugarCRM"" logo and
- *  (ii) the SugarCRM copyright notice
- * in the same form as they appear in the distribution.  See full license for
- * requirements.
+ * If Company is not bound by the MSA, then by installing or using this file
+ * you are agreeing unconditionally that Company will be bound by the MSA and
+ * certifying that you have authority to bind Company accordingly.
  *
- * Your Warranty, Limitations of liability and Indemnity are expressly stated
- * in the License.  Please refer to the License for the specific language
- * governing these rights and limitations under the License.  Portions created
- * by SugarCRM are Copyright (C) 2004-2012 SugarCRM, Inc.; All Rights Reserved.
- ********************************************************************************/
+ * Copyright (C) 2004-2014 SugarCRM Inc. All rights reserved.
+ */
 (function(app) {
     app.events.on("app:init", function() {
         var routes,
@@ -107,8 +93,8 @@
 
                     var frame = $('#bwc-frame');
                     if (frame.length === 1 &&
-                    		app.utils.rmIframeMark('index.php' + frame.get(0).contentWindow.location.search) === url
-                        ) {
+                        app.utils.rmIframeMark('index.php' + frame.get(0).contentWindow.location.search) === url
+                    ) {
                         // update hash link only
                         return;
                     }
@@ -338,9 +324,10 @@
             if (showWizard) {
                 // If the license settings need to be input, don't show the wizard
                 var system_config = app.metadata.getConfig();
-                if (system_config.system_status
-                    && system_config.system_status.level
-                    && system_config.system_status.level == 'admin_only') {
+                if (system_config.system_status &&
+                    system_config.system_status.level &&
+                    system_config.system_status.level === 'admin_only'
+                ) {
                     showWizard = false;
                 }
             }
@@ -460,9 +447,9 @@
     }, this);
 
     var refreshExternalLogin = function() {
-    	var config = app.metadata.getConfig();
+        var config = app.metadata.getConfig();
         app.api.setExternalLogin(config && config['externalLogin']);
-    }
+    };
 
     app.events.on("app:sync:complete", refreshExternalLogin, this);
     app.events.on("app:init", refreshExternalLogin, this);
@@ -483,7 +470,7 @@
                 if (bwcAction !== sidecarAction) {
                     action = bwcAction;
                 }
-                
+
                 var redirect = "bwc/index.php?module=" + module + "&action=" + action;
 
                 if (id) {
