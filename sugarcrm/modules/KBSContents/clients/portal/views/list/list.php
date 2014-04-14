ba425a5ee20951fb2aec8e4b018930910a7f25ce
malyshev <malyshev.php@gmail.com>
2014-04-14 11:40:14 +0300

    RS-353-1: KB: Make KB module portal editor enabled

---
+++ sugarcrm/modules/KBSContents/clients/portal/views/list/list.php
@@ -0,0 +1,55 @@
+<?php
+/*
+ * By installing or using this file, you are confirming on behalf of the entity
+ * subscribed to the SugarCRM Inc. product ("Company") that Company is bound by
+ * the SugarCRM Inc. Master Subscription Agreement ("MSA"), which is viewable at:
+ * http://www.sugarcrm.com/master-subscription-agreement
+ *
+ * If Company is not bound by the MSA, then by installing or using this file
+ * you are agreeing unconditionally that Company will be bound by the MSA and
+ * certifying that you have authority to bind Company accordingly.
+ *
+ * Copyright  2004-2013 SugarCRM Inc.  All rights reserved.
+ */
+
+$viewdefs['KBSContents']['portal']['view']['list'] = array(
+    'panels' => array(
+        array(
+            'label' => 'LBL_PANEL_DEFAULT',
+            'fields' => array(
+                array(
+                    'name' => 'name',
+                    'label' => 'LBL_NAME',
+                    'default' => true,
+                    'enabled' => true,
+                    'link' => true,
+                    'related_fields' => array(
+                        'kbsdocument_id',
+                        'kbsarticle_id',
+                    ),
+                ),
+                array(
+                    'name' => 'language',
+                    'label' => 'LBL_LANG',
+                    'default' => true,
+                    'enabled' => true,
+                    'link' => true,
+                    'type' => 'enum-config',
+                    'module' => 'KBSDocuments',
+                    'key' => 'languages',
+                ),
+                array(
+                    'label' => 'LBL_DATE_ENTERED',
+                    'enabled' => true,
+                    'default' => true,
+                    'name' => 'date_entered',
+                    'readonly' => true,
+                ),
+            ),
+        ),
+    ),
+    'orderBy' => array(
+        'field' => 'date_entered',
+        'direction' => 'desc',
+    ),
+);
