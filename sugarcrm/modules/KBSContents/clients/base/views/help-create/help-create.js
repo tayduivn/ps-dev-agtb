8574f38b91b2c09a2158ab8984b1d328a67ab224
Egor Kolotaev <kolotaev@tut.by>
2014-04-15 14:10:28 +0300

    Added code for creation of a url-link to Sugar Documentation

--- sugarcrm/modules/KBSContents/clients/base/views/help-create/help-create.js
+++ sugarcrm/modules/KBSContents/clients/base/views/help-create/help-create.js
@@ -16,8 +16,31 @@
     /**
      * {@inheritDoc}
      */
-    _renderHtml: function() {
-        var helpObject = app.help.get(this.context.get('module'), 'create');
+    _renderHtml: function () {
+        var helpUrl = {
+                more_info_url: this.createMoreHelpLink(),
+                more_info_url_close: '</a>'
+            },
+            helpObject = app.help.get(this.context.get('module'), 'create', helpUrl);
+
         this._super('_renderHtml', [helpObject, this.options]);
+    },
+
+    /**
+     * Collects server version, language, module, and route and returns an HTML link to be used
+     * in the template
+     *
+     * @returns {String} The HTML a-tag for the More Help link
+     */
+    createMoreHelpLink: function () {
+        var serverInfo = app.metadata.getServerInfo(),
+            lang = app.lang.getLanguage(),
+            module = app.controller.context.get('module'),
+            route = 'create';
+
+        var url = 'http://www.sugarcrm.com/crm/product_doc.php?edition=' + serverInfo.flavor
+            + '&version=' + serverInfo.version + '&lang=' + lang + '&module=' + module + '&route=' + route;
+
+        return '<a href="' + url + '" target="_blank">';
     }
 })
