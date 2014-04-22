eb080e0190a00503bfa6fd0a2bd3b8cc98ed83a8
Stanislav Malyshev <smalyshev@gmail.com>
2014-04-21 18:20:08 -0700

    Reuse the environment from parent request

--- sugarcrm/clients/base/api/BulkApi.php
+++ sugarcrm/clients/base/api/BulkApi.php
@@ -33,6 +33,13 @@ class BulkApi extends SugarApi
         );
     }
 
+    /**
+     * Bulk API call
+     * @param ServiceBase $api
+     * @param array $args
+     * @throws SugarApiExceptionMissingParameter
+     * @return array
+     */
     public function bulkCall($api, $args)
     {
         $this->requireArgs($args,array('requests'));
@@ -49,13 +56,63 @@ class BulkApi extends SugarApi
             /**
              * @var $rest RestService
              */
-            $rest = new $restClass();
+            $rest = new BulkRestService($api);
             $rest->setRequest($restReq);
             $rest->setResponse($restResp);
             $rest->execute();
 
         }
         return $restResp->getResponses();
+    }
+}
+
+/**
+ * Bulk API Rest service class
+ * Shortcuts some functions that we don't need to do on bulk requests
+ */
+class BulkRestService extends RestService
+{
+    protected $parent;
+
+    public function __construct($parent)
+    {
+        $this->parent = $parent;
+        parent::__construct();
+    }
+
+    /**
+     * Shortcut authentication since we're already authenticated before
+     * @see RestService::authenticateUser()
+     */
+    protected function authenticateUser()
+    {
+        $this->user = $this->parent->user;
+        return array('isLoggedIn' => true, 'exception' => false);
+    }
+
+    /**
+     * Don't check metadata - top request checks it
+     * @see RestService::isMetadataCurrent()
+     */
+    protected function isMetadataCurrent()
+    {
+        return true;
+    }
+
+    /**
+     * Don't load envt - top request loads it
+     * @see ServiceBase::loadUserEnvironment()
+     */
+    protected function loadUserEnvironment()
+    {
+    }
+
+    /**
+     * Never release session
+     * @see ServiceBase::releaseSession()
+     */
+    protected function releaseSession()
+    {
     }
 }
 
