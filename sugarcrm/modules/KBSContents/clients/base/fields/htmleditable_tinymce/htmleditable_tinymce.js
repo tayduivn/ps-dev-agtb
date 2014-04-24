173ae4e57db598e65170c43d2b2fd4b36db173b3
asuharev <asukharev@sugarcrm.com>
2014-04-24 16:08:39 +0300

    RS-367: KB: Record View: TyniMCE editor

---
+++ sugarcrm/modules/KBSContents/clients/base/fields/htmleditable_tinymce/htmleditable_tinymce.js
@@ -0,0 +1,34 @@
+/*
+ * By installing or using this file, you are confirming on behalf of the entity
+ * subscribed to the SugarCRM Inc. product ("Company") that Company is bound by
+ * the SugarCRM Inc. Master Subscription Agreement ("MSA"), which is viewable at:
+ * http://www.sugarcrm.com/master-subscription-agreement
+ *
+ * If Company is not bound by the MSA, then by installing or using this file
+ * you are agreeing unconditionally that Company will be bound by the MSA and
+ * certifying that you have authority to bind Company accordingly.
+ *
+ * Copyright (C) 2004-2014 SugarCRM Inc. All rights reserved.
+ */
+({
+    extendsFrom: 'Htmleditable_tinymceField',
+
+    /**
+     * {@inheritDoc}
+     * Apply document css style to editor.
+     */
+    getTinyMCEConfig: function() {
+        var config = this._super('getTinyMCEConfig'),
+            content_css = [];
+
+        _.each(document.styleSheets, function(style) {
+            if (style.href) {
+                content_css.push(style.href);
+            }
+        });
+        config.content_css = content_css;
+        config.body_class = 'kbdocument-body';
+
+        return config;
+    }
+})
