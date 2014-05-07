a3767b8bfe0eb1515d63745a7433e2ceeff6e5e3
Neil Harding <nharding@sugarcrm.com>
2014-05-07 14:40:27 -0400

    MAR-1702 - Mass Email on Account and Person list views (for parity with 6.x) - this will eventually be replaced by new Communications funtionality.

--- sugarcrm/include/SugarObjects/templates/person/clients/base/views/recordlist/recordlist.php
+++ sugarcrm/include/SugarObjects/templates/person/clients/base/views/recordlist/recordlist.php
@@ -9,10 +9,10 @@
  * you are agreeing unconditionally that Company will be bound by the MSA and
  * certifying that you have authority to bind Company accordingly.
  *
- * Copyright (C) 2004-2014 SugarCRM Inc. All rights reserved.
+ * Copyright  2004-2014 SugarCRM Inc.  All rights reserved.
  */
-
-$viewdefs['Leads']['base']['view']['recordlist'] = array(
+$module_name = '<module_name>';
+$viewdefs[$module_name]['base']['view']['recordlist'] = array(
     'selection' => array(
         'type' => 'multi',
         'actions' => array(
@@ -39,16 +39,6 @@ $viewdefs['Leads']['base']['view']['recordlist'] = array(
                 'acl_action' => 'massupdate',
             ),
             array(
-                'name' => 'merge_button',
-                'type' => 'button',
-                'label' => 'LBL_MERGE',
-                'primary' => true,
-                'events' => array(
-                    'click' => 'list:mergeduplicates:fire',
-                ),
-                'acl_action' => 'edit',
-            ),
-            array(
                 'name' => 'calc_field_button',
                 'type' => 'button',
                 'label' => 'LBL_UPDATE_CALC_FIELDS',
@@ -58,14 +48,13 @@ $viewdefs['Leads']['base']['view']['recordlist'] = array(
                 'acl_action' => 'massupdate',
             ),
             array(
-                'name' => 'addtolist_button',
+                'name' => 'merge_button',
                 'type' => 'button',
-                'label' => 'LBL_ADD_TO_PROSPECT_LIST_BUTTON_LABEL',
+                'label' => 'LBL_MERGE',
                 'primary' => true,
                 'events' => array(
-                    'click' => 'list:massaddtolist:fire',
+                    'click' => 'list:mergeduplicates:fire',
                 ),
-                'acl_module' => 'ProspectLists',
                 'acl_action' => 'edit',
             ),
             array(
@@ -87,45 +76,6 @@ $viewdefs['Leads']['base']['view']['recordlist'] = array(
                 'events' => array(
                     'click' => 'list:massexport:fire',
                 ),
-            ),
-        ),
-    ),
-    'rowactions' => array(
-        'actions' => array(
-            array(
-                'type' => 'rowaction',
-                'css_class' => 'btn',
-                'tooltip' => 'LBL_PREVIEW',
-                'event' => 'list:preview:fire',
-                'icon' => 'icon-eye-open',
-                'acl_action' => 'view',
-            ),
-            array(
-                'type' => 'rowaction',
-                'name' => 'edit_button',
-                'icon' => 'icon-pencil',
-                'label' => 'LBL_EDIT_BUTTON',
-                'event' => 'list:editrow:fire',
-                'acl_action' => 'edit',
-            ),
-            array(
-                'type' => 'follow',
-                'name' => 'follow_button',
-                'event' => 'list:follow:fire',
-                'acl_action' => 'view',
-            ),
-            array(
-                'type' => 'rowaction',
-                'icon' => 'icon-trash',
-                'event' => 'list:deleterow:fire',
-                'label' => 'LBL_DELETE_BUTTON',
-                'acl_action' => 'delete',
-            ),
-            array(
-                'type' => 'convertbutton',
-                'name' => 'lead_convert_button',
-                'label' => 'LBL_CONVERT_BUTTON_LABEL',
-                'acl_action' => 'edit',
             ),
         ),
     ),
