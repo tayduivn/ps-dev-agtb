4a653a570a0bb545fb5311252011418a9ad2584a
Sushma Sheshadri <ssheshadri@sugarcrm.com>
2014-05-14 11:56:48 -0700

    PAT-538- Added license notice to Facebook lib

--- sugarcrm/include/externalAPI/Facebook/FacebookLib.php
+++ sugarcrm/include/externalAPI/Facebook/FacebookLib.php
@@ -1,19 +1,8 @@
 <?php
-/**
-* Copyright 2011 Facebook, Inc.
-*
-* Licensed under the Apache License, Version 2.0 (the "License"); you may
-* not use this file except in compliance with the License. You may obtain
-* a copy of the License at
-*
-* http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
-* WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
-* License for the specific language governing permissions and limitations
-* under the License.
-*/
+/****************************************************************************************************************
+*© Facebook, Inc., Apache 2.0 License.
+*For information on the full license terms or related notices, go to http://www.sugarcrm.com/third-party-software.
+ *****************************************************************************************************************/
 
 if (!function_exists('curl_init')) {
   throw new Exception('Facebook needs the CURL PHP extension.');
