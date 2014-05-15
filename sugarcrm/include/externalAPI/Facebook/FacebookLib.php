f8784ba66adc1ff5150c2a4ec8f67b2119098124
Sushma Sheshadri <ssheshadri@sugarcrm.com>
2014-05-14 22:32:25 -0700

    PAT-538 - Added sugar license header on top of the Apache header

--- sugarcrm/include/externalAPI/Facebook/FacebookLib.php
+++ sugarcrm/include/externalAPI/Facebook/FacebookLib.php
@@ -1,8 +1,19 @@
 <?php
-/****************************************************************************************************************
-*© Facebook, Inc., Apache 2.0 License.
-*For information on the full license terms or related notices, go to http://www.sugarcrm.com/third-party-software.
- *****************************************************************************************************************/
+/**************************************************************************************************************
+ * By installing or using this file, you are confirming on behalf of the entity
+ * subscribed to the SugarCRM Inc. product ("Company") that Company is bound by
+ * the SugarCRM Inc. Master Subscription Agreement (“MSA”), which is viewable at:
+ * http://www.sugarcrm.com/master-subscription-agreement
+ *
+ * If Company is not bound by the MSA, then by installing or using this file
+ * you are agreeing unconditionally that Company will be bound by the MSA and
+ * certifying that you have authority to bind Company accordingly.
+ *
+ * Copyright (C) 2004-2014 SugarCRM Inc.  All rights reserved.
+ ****************************************************************************************************************
+ *© Facebook, Inc., Apache 2.0 License.
+ *For information on the full license terms or related notices, go to http://www.sugarcrm.com/third-party-software.
+ ****************************************************************************************************************/
 
 if (!function_exists('curl_init')) {
   throw new Exception('Facebook needs the CURL PHP extension.');
