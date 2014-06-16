7e5e32ac2a95bbe8602bc5ac2fe3855cb16c94a3
timwolf <twolf@sugarcrm.com>
2014-06-16 15:48:56 -0400

    MAR-2094  - fixed bad variable name in WebToLeadCapture

--- sugarcrm/modules/Campaigns/WebToLeadCapture.php
+++ sugarcrm/modules/Campaigns/WebToLeadCapture.php
@@ -106,7 +106,7 @@ if (isset($_POST['campaign_id']) && !empty($_POST['campaign_id'])) {
 	            $camplog->related_type = $lead->module_dir;
 	            $camplog->activity_type = "lead";
 	            $camplog->target_type = $lead->module_dir;
-	            $campaign_log->activity_date=$timedate->now();
+	            $camplog->activity_date=$timedate->now();
 	            $camplog->target_id    = $lead->id;
                 if(isset($marketing_data['id']))
                 {
