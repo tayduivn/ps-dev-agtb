a022a6acf273e5baca806354ed712b24c19647d6
David Wheeler <dwheeler@sugarcrm.com>
2014-06-17 18:25:19 -0700

    BR-1564: DeDuping the accounts_contacts table now works on DB2

--- sugarcrm/modules/Contacts/upgrade/scripts/post/7_RemoveDuplicateAccountsContacts.php
+++ sugarcrm/modules/Contacts/upgrade/scripts/post/7_RemoveDuplicateAccountsContacts.php
@@ -30,25 +30,42 @@ class SugarUpgradeRemoveDuplicateAccountsContacts extends UpgradeScript
         if (version_compare($this->from_version, '7.2.1', '<') && version_compare($this->from_version, '7.0.0', '>=')) {
             // Hardcoded for the accounts_contacts relationship for now
 
+            $countUniqueQuery = "SELECT count(*) FROM "
+                . "(SELECT DISTINCT account_id, contact_id, primary_account, deleted "
+                . "FROM accounts_contacts) AS a";
+
             //Create a temp_table to hold the non-dupe entries.
-            $result = $this->db->query(
-                "CREATE table accounts_contacts_tmp AS " .
-                "SELECT * FROM accounts_contacts GROUP BY account_id, contact_id, deleted, primary_account " .
-                "ORDER BY date_modified desc"
-            );
+            if ($this->db->dbName == "IBM_DB2") {
+                $createTempTableQuery = "CREATE TABLE accounts_contacts_tmp LIKE accounts_contacts";
+                $copyDataQuery = "INSERT INTO accounts_contacts_tmp (SELECT * FROM accounts_contacts)";
+                $deDupeQuery = "DELETE FROM ( "
+                             . "SELECT ROWNUMBER() OVER (PARTITION BY account_id, contact_id, primary_account, deleted) "
+                             . "AS rn FROM accounts_contacts) AS row WHERE rn > 1";
+            } else {
+                $createTempTableQuery = "CREATE table accounts_contacts_tmp AS SELECT * FROM accounts_contacts";
+                $deDupeQuery = "DELETE t1 FROM accounts_contacts t1, accounts_contacts t2 "
+                 . "WHERE t1.id > t2.id AND t1.contact_id = t2.contact_id AND t1.account_id = t2.account_id "
+                 . "AND t1.deleted = t2.deleted AND t1.primary_account = t2.primary_account";
+            }
+            $result = $this->db->query($createTempTableQuery);
+            if (!empty($copyDataQuery) && $result) {
+                $result = $this->db->query($copyDataQuery);
+            }
             if ($result) {
-                $uniqueRows = $this->db->getAffectedRowCount($result);
-                //Nuke the existing relationship table...
-                $this->db->query($this->db->truncateTableSQL("accounts_contacts"));
+                $uniqueRows = $this->db->getOne($countUniqueQuery);
+
+                //De-Dupe the existing table
+                $this->db->query($deDupeQuery);
 
                 //Copy the data from the temp back into the original table.
-                $result = $this->db->query("INSERT INTO accounts_contacts SELECT * FROM accounts_contacts_tmp");
+                $finalRows = $this->db->getOne("SELECT count(*) FROM accounts_contacts");
 
                 //Now remove the temp table, only if the clone back into accounts_contacts worked.
-                if ($result && $this->db->getAffectedRowCount($result) == $uniqueRows) {
+                if ($finalRows == $uniqueRows) {
                     $this->db->query($this->db->dropTableNameSQL("accounts_contacts_tmp"));
                 } else {
-                    $this->upgrader->log("Failed to copy from temp table back to accounts_contacts");
+                    $this->upgrader->log("Failed to De-dupe the accounts_contacts table, "
+                                       . "Data is preserved in the accounts_contacts_tmp table.");
                 }
             } else {
                 $this->upgrader->log("Failed to create accounts_contacts temp table");
