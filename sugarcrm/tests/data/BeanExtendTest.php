2ee1429ddcf2efec65959eca6d2efd0703f2ac14
Julien Girault <julien.emac@gmail.com>
2014-06-23 19:21:36 -0700

    SC-2124: Fix license headers in files

--- sugarcrm/tests/data/BeanExtendTest.php
+++ sugarcrm/tests/data/BeanExtendTest.php
@@ -1,9 +1,15 @@
 <?php
 
-/*********************************************************************************
- *The contents of this file are subject to the SugarCRM Professional End User License Agreement
- *Portions created by SugarCRM are Copyright (C) 2004 SugarCRM, Inc.; All Rights Reserved.
- ********************************************************************************/
+/*
+ * Your installation or use of this SugarCRM file is subject to the applicable
+ * terms available at
+ * http://support.sugarcrm.com/06_Customer_Center/10_Master_Subscription_Agreements/.
+ * If you do not agree to all of the applicable terms or do not have the
+ * authority to bind the entity as an authorized representative, then do not
+ * install or use this SugarCRM file.
+ *
+ * Copyright (C) SugarCRM Inc. All rights reserved.
+ */
 
 require_once('data/SugarBean.php');
 
