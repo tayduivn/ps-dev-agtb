8a154ed2d88c6567c2afa96aef40369ac5d94a82
Steven Parsley <sparsley@sugarcrm.com>
2014-06-24 14:19:49 -0400

    Jira: MAR-2068 Fixed the footer to allow proper tabbing for accessibility and refactored buttons to not use ids to reference components.

--- sugarcrm/clients/base/views/footer-actions/footer-actions.js
+++ sugarcrm/clients/base/views/footer-actions/footer-actions.js
@@ -14,13 +14,12 @@
  * @extends View.View
  */
 ({
-    // TODO remove the id links in footer and replace to data-attributes (SC-2580)
     events: {
-        'click #shortcuts': 'shortcuts',
-        'click #tour': 'showTutorialClick',
-        'click #feedback': 'feedback',
-        'click #support': 'support',
-        'click #help': 'help'
+        'click [data-action=shortcuts]': 'shortcuts',
+        'click [data-action=tour]': 'showTutorialClick',
+        'click [data-action=feedback]': 'feedback',
+        'click [data-action=support]': 'support',
+        'click [data-action=help]': 'help'
     },
     tagName: 'span',
     layoutName: '',
@@ -61,14 +60,14 @@
         this.routeParams = {'route': route, 'params': params};
     },
     enableTourButton: function() {
-        this.$('#tour').removeClass('disabled');
-        this.events['click #tour'] = 'showTutorialClick';
+        this.$('[data-action=tour]').removeClass('disabled');
+        this.events['click [data-action=tour]'] = 'showTutorialClick';
         this.undelegateEvents();
         this.delegateEvents();
     },
     disableTourButton: function() {
-        this.$('#tour').addClass('disabled');
-        delete this.events['click #tour'];
+        this.$('[data-action=tour]').addClass('disabled');
+        delete this.events['click [data-action=tour]'];
         this.undelegateEvents();
         this.delegateEvents();
     },
@@ -193,7 +192,7 @@
     disableHelpButton: function(disable) {
         disable = _.isUndefined(disable) ? true : disable;
 
-        var button = this.$('#help');
+        var button = this.$('[data-action=help]');
         if (button) {
             button.toggleClass('disabled', disable);
         }
@@ -213,7 +212,7 @@
      */
     toggleHelpButton: function(active, button) {
         if (_.isUndefined(button)) {
-            button = this.$('#help');
+            button = this.$('[data-action=help]');
         }
 
         if (button) {
