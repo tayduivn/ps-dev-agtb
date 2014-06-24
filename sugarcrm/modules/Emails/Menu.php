2db30a3722eb1b75bdb9384e31977a7a92dc5591
Julien Girault <julien.emac@gmail.com>
2014-06-23 19:12:26 -0700

    SC-2124: Fix license headers in files

--- sugarcrm/modules/Emails/Menu.php
+++ sugarcrm/modules/Emails/Menu.php
@@ -1,14 +1,15 @@
 <?php
 if(!defined('sugarEntry') || !sugarEntry) die('Not A Valid Entry Point');
-/*********************************************************************************
- * The contents of this file are subject to
- * *******************************************************************************/
-/*********************************************************************************
- * $Id: Menu.php 55136 2010-03-08 21:35:33Z roger $
- * Description:
- * Portions created by SugarCRM are Copyright (C) SugarCRM, Inc. All Rights
- * Reserved. Contributor(s): ______________________________________..
- *********************************************************************************/
+/*
+ * Your installation or use of this SugarCRM file is subject to the applicable
+ * terms available at
+ * http://support.sugarcrm.com/06_Customer_Center/10_Master_Subscription_Agreements/.
+ * If you do not agree to all of the applicable terms or do not have the
+ * authority to bind the entity as an authorized representative, then do not
+ * install or use this SugarCRM file.
+ *
+ * Copyright (C) SugarCRM Inc. All rights reserved.
+ */
 
 global $mod_strings;
 global $current_user;
