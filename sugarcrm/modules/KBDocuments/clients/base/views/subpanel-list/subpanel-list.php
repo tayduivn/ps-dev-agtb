7413aa25fd020bb25868b806501bac61ac296029
Joe Wang <cwang@sugarcrm.com>
2014-07-15 15:33:11 -0700

    PAT-669: makes Document Name a link in KBDocuments subpanel

---
+++ sugarcrm/modules/KBDocuments/clients/base/views/subpanel-list/subpanel-list.php
@@ -0,0 +1,37 @@
+<?php
+/*
+ * Your installation or use of this SugarCRM file is subject to the applicable
+ * terms available at
+ * http://support.sugarcrm.com/06_Customer_Center/10_Master_Subscription_Agreements/.
+ * If you do not agree to all of the applicable terms or do not have the
+ * authority to bind the entity as an authorized representative, then do not
+ * install or use this SugarCRM file.
+ *
+ * Copyright (C) SugarCRM Inc. All rights reserved.
+ */
+$viewdefs['KBDocuments']['base']['view']['subpanel-list'] = array(
+    'panels' =>
+    array(
+        array(
+            'name' => 'panel_header',
+            'label' => 'LBL_PANEL_1',
+            'fields' =>
+            array(
+                array(
+                    'label' => 'LBL_LIST_DOCUMENT_NAME',
+                    'enabled' => true,
+                    'default' => true,
+                    'name' => 'kbdocument_name',
+                    'link' => 'true',
+                ),
+                array(
+                    'label' => 'LBL_DATE_MODIFIED',
+                    'enabled' => true,
+                    'default' => true,
+                    'name' => 'date_modified',
+                    'readonly' => true,
+                ),
+            ),
+        ),
+    ),
+);
