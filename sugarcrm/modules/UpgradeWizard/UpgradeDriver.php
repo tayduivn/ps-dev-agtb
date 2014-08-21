4d5c7d94c94668d95fc50bf06a66deb5cc65211b
Alexander Rymarchik <a.rymarchik@intetics.com>
2014-08-21 12:46:08 +0300

    CRYS-373: Should not allow 6.5.17 to 7.5.0.0 upgrade on non-mysql platform.

--- sugarcrm/modules/UpgradeWizard/UpgradeDriver.php
+++ sugarcrm/modules/UpgradeWizard/UpgradeDriver.php
@@ -824,10 +824,10 @@ abstract class UpgradeDriver
         $db = DBManagerFactory::getInstance();
         $manifest = $this->getManifest();
         $to_version = $this->implodeVersion($manifest['version'], 4);
-        if (!(version_compare($this->from_version, 7, '<')
-            && $db instanceof MysqlManager
+        if (version_compare($this->from_version, 7, '<')
             && version_compare($to_version, '7.5.0.0', '=')
-        )) {
+            && !$db instanceof MysqlManager
+        ) {
             return $this->error("Can't upgrade version 6.x to 7.5.0.0 on non-Mysql database", true);
         }
         $res = $this->validateManifest();
