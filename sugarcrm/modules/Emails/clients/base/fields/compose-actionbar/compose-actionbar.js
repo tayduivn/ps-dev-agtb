2e222de3d4f702fba921ccae4c140c6822c3ed00
Steven Parsley <sparsley@sugarcrm.com>
2014-09-19 11:27:37 -0400

    MAR-2323:  refactored compose-actionbar to use the new fieldset.

--- sugarcrm/modules/Emails/clients/base/fields/compose-actionbar/compose-actionbar.js
+++ sugarcrm/modules/Emails/clients/base/fields/compose-actionbar/compose-actionbar.js
@@ -11,88 +11,23 @@
 /**
  * Actionbar for the email compose view
  *
- * Def for this field includes an array called buttonSections
- * Each object in the array can have a type (corresponding to a field that extends from fieldset and includes an array of buttons)
- * Or if object contains no type, a button group is built by default using the buttons array in the section
- * Additional CSS can be added to the default button group using the css_class attribute
- *
  * @class View.Fields.Base.Emails.ComposeActionbarField
  * @alias SUGAR.App.view.fields.BaseEmailsComposeActionbarField
  * @extends View.Fields.Base.FieldsetField
  */
 ({
     extendsFrom: 'FieldsetField',
-    fields: null,
 
     events: {
         'click a:not(.dropdown-toggle)': 'handleButtonClick'
     },
 
     /**
-     * Loop over the button sections and build placeholders for each
-     *
-     * @return {Handlebars.SafeString}
+     * @inheritDoc
      */
-    getPlaceholder: function() {
-        var placeholder = this._super("getPlaceholder");
-        var $container = $(placeholder.toString());
-
-        _.each(this.def.buttonSections, function(buttonSection) {
-            var placeHolderString;
-            if (!_.isUndefined(buttonSection.type)) {
-                placeHolderString = this.buildTypedButtonSection(buttonSection);
-            } else {
-                placeHolderString = this.buildDefaultButtonSection(buttonSection);
-            }
-            $container.append(placeHolderString);
-        }, this);
-
-        return new Handlebars.SafeString($container.get(0).outerHTML);
-    },
-
-    /**
-     * If a type was specified on the button section, use the def to build a field of that type
-     *
-     * @param def
-     * @return {String}
-     */
-    buildTypedButtonSection: function(def) {
-        var field = app.view.createField({
-            def: def,
-            view: this.view,
-            viewName: this.options.viewName,
-            model: this.model
-        });
-        this.fields.push(field);
-
-        return field.getPlaceholder().toString();
-    },
-
-    /**
-     * If button section has no type, build an actions btn-group
-     *
-     * @param def
-     * @return {String}
-     */
-    buildDefaultButtonSection: function(def) {
-        var $defaultSection = $('<div class="actions"></div>');
-
-        if (def.css_class) {
-            $defaultSection.addClass(def.css_class);
-        }
-
-        _.each(def.buttons, function(button) {
-            var field = app.view.createField({
-                def: button,
-                view: this.view,
-                viewName: this.options.viewName,
-                model: this.model
-            });
-            this.fields.push(field);
-            $defaultSection.append(field.getPlaceholder().toString());
-        }, this);
-
-        return $defaultSection.get(0).outerHTML;
+    initialize: function(options) {
+        this._super('initialize', [options]);
+        this.type = 'fieldset';
     },
 
     /**
