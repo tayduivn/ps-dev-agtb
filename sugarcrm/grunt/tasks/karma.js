5ef6ec5c0d4e2dfc09531ec46ae92d545ec08387
João Morais <jcsmorais@gmail.com>
2014-09-20 02:33:27 +0200

    Update karmaOriginal to version 0.9.0 (fixes TDD-34)

--- sugarcrm/grunt/tasks/karma.js
+++ sugarcrm/grunt/tasks/karma.js
@@ -106,7 +106,7 @@ module.exports = function(grunt) {
     };
 
     /**
-     * Copy of the original karma task.
+     * Copy of the karma task available on grunt-karma plugin version 0.9.0.
      */
     karmaOriginal = function() {
         var done = this.async();
@@ -135,6 +135,7 @@ module.exports = function(grunt) {
         var opts = _.cloneDeep(options);
         // Merge options onto data, with data taking precedence.
         var data = _.merge(opts, this.data);
+
         // But override the browsers array.
         if (data.browsers && this.data.browsers) {
             data.browsers = this.data.browsers;
@@ -150,20 +151,28 @@ module.exports = function(grunt) {
             data.configFile = grunt.template.process(data.configFile);
         }
 
-        if (data.files){
+        if (data.files) {
             data.files = _.flatten(data.files);
         }
 
         //support `karma run`, useful for grunt watch
-        if (this.flags.run){
+        if (this.flags.run) {
             runner.run(data, finished.bind(done));
             return;
         }
 
         //allow karma to be run in the background so it doesn't block grunt
-        if (data.background){
-            var backgroundProcess = grunt.util.spawn({cmd: 'node', args: [path.join(__dirname, '..', 'lib', 'background.js'), JSON.stringify(data)]}, function(){});
-            process.on('exit', function () {
+        if (data.background) {
+            var backgroundArgs = {
+                cmd: 'node',
+                args: process.execArgv.concat([path.join(__dirname, '..', 'lib', 'background.js'), JSON.stringify(data)])
+            };
+            var backgroundProcess = grunt.util.spawn(backgroundArgs, function(error) {
+                if (error) {
+                    grunt.log.error(error);
+                }
+            });
+            process.on('exit', function() {
                 backgroundProcess.kill();
             });
             done();
@@ -171,7 +180,7 @@ module.exports = function(grunt) {
         else {
             server.start(data, finished.bind(done));
         }
-    };
+    }
 };
 
 function finished(code){ return this(code === 0); }
