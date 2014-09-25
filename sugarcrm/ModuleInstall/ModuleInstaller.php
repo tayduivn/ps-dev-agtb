8dc207494743d8b3161322d4cf253549f479e1b6
Sergey Morozov <smorozov@sugarcrm.com>
2014-09-25 18:40:37 +0300

    [BR-2076] Custom filter for Custom Module is not deleted after deleting Custom Module

--- sugarcrm/ModuleInstall/ModuleInstaller.php
+++ sugarcrm/ModuleInstall/ModuleInstaller.php
@@ -40,6 +40,9 @@ class ModuleInstaller{
 	var $modulesInPackage = array();
 	public $disabled_path = DISABLED_PATH;
 
+    /** @var array */
+    public $installdefs;
+
 	/**
 	 * List of install sections and modules affected by installation in each
 	 * sections. This is used to handle post install cleanup prior to a complete
@@ -704,6 +707,30 @@ class ModuleInstaller{
             return false;
         }
         return true;
+    }
+
+    /**
+     * Uninstalls module filters
+     */
+    protected function uninstall_filters()
+    {
+        if (empty($this->installdefs['beans'])) {
+            return;
+        }
+
+        $modules = array();
+        foreach ($this->installdefs['beans'] as $definition) {
+            $modules[] = $definition['module'];
+        }
+
+        $filter = BeanFactory::getBean('Filters');
+        $query = new SugarQuery();
+        $query->select('id');
+        $query->from($filter)->where()->in('module_name', $modules);
+        $data = $query->execute();
+        foreach ($data as $row) {
+            $filter->mark_deleted($row['id']);
+        }
     }
 
     /**
@@ -1681,6 +1708,7 @@ class ModuleInstaller{
 			'uninstall_layoutfields',
 		    'uninstall_extensions',
 		    'uninstall_global_search',
+            'uninstall_filters',
 			'disable_manifest_logichooks',
 			'post_uninstall',
 		);
