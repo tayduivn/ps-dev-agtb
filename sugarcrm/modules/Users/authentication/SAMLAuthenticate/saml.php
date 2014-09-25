46e9fca74ce426d60fb462ee5894b351a7679028
Stanislav Malyshev <smalyshev@gmail.com>
2014-09-24 17:30:27 -0700

    Switch to using SAML library code for logouts

--- sugarcrm/modules/Users/authentication/SAMLAuthenticate/saml.php
+++ sugarcrm/modules/Users/authentication/SAMLAuthenticate/saml.php
@@ -18,4 +18,3 @@ require_once ONELOGIN_SAML_DIR . 'AuthRequest.php';
 require_once ONELOGIN_SAML_DIR . 'Response.php';
 require_once ONELOGIN_SAML_DIR . 'Settings.php';
 require_once ONELOGIN_SAML_DIR . 'XmlSec.php';
-require_once 'vendor/OneLogin//Saml/LogoutRequest.php';
