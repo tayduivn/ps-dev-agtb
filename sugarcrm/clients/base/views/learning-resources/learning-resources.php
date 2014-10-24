09c01a8d9a8d38aedf3bae12c41edd0330c5e003
Sangyoun Kim <skim@sugarcrm.com>
2014-10-24 16:35:01 -0700

    Revert "SC-3199 adds metadata to learning resources"

--- sugarcrm/clients/base/views/learning-resources/learning-resources.php
+++ sugarcrm/clients/base/views/learning-resources/learning-resources.php
@@ -21,41 +21,26 @@ $viewdefs['base']['view']['learning-resources'] = array(
         ),
     ),
     'resources' => array(
-        'discover' => array(
-            'color' => 'blue',
-            'icon' => 'icon-youtube-play',
-            'url' => 'https://www.youtube.com/user/DiscoverSugarCRM',
-            'link' => 'LBL_LEARNING_RESOURCES_DISCOVER_LINK',
-        ),
         'sugar_university' => array(
             'color' => 'blue',
             'icon' => 'icon-book',
             'url' => 'http://university.sugarcrm.com/',
             'link' => 'LBL_LEARNING_RESOURCES_SUGAR_UNIVERSITY_LINK',
+            'teaser' => 'LBL_LEARNING_RESOURCES_SUGAR_UNIVERSITY_TEASER',
         ),
-        'start' => array(
-            'color' => 'blue',
-            'icon' => 'icon-compass',
-            'url' => 'http://support.sugarcrm.com/01_Get_Started/',
-            'link' => 'LBL_LEARNING_RESOURCES_START_LINK',
+        'community' => array(
+            'color' => 'green',
+            'icon' => 'icon-comments-alt',
+            'url' => 'https://community.sugarcrm.com/',
+            'link' => 'LBL_LEARNING_RESOURCES_COMMUNITY_LINK',
+            'teaser' => 'LBL_LEARNING_RESOURCES_COMMUNITY_TEASER',
         ),
-        'training' => array(
-            'color' => 'blue',
-            'icon' => 'icon-microphone',
-            'url' => 'http://support.sugarcrm.com/03_Training/Webinars/',
-            'link' => 'LBL_LEARNING_RESOURCES_TRAINING_LINK',
-        ),
-        'webinars' => array(
-            'color' => 'blue',
-            'icon' => 'icon-desktop',
-            'url' => 'http://www.sugarcrm.com/university_classes/list',
-            'link' => 'LBL_LEARNING_RESOURCES_WEBINARS_LINK',
-        ),
-        'videos' => array(
-            'color' => 'blue',
-            'icon' => 'icon-play-sign',
-            'url' => 'http://support.sugarcrm.com/03_Training/02_Videos/02_Videos_Sugar_v7/',
-            'link' => 'LBL_LEARNING_RESOURCES_VIDEOS_LINK',
+        'support' => array(
+            'color' => 'red',
+            'icon' => 'icon-question-sign',
+            'url' => 'http://support.sugarcrm.com/',
+            'link' => 'LBL_LEARNING_RESOURCES_SUPPORT_LINK',
+            'teaser' => 'LBL_LEARNING_RESOURCES_SUPPORT_TEASER',
         ),
     ),
 );
