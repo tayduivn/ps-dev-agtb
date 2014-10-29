ce28f4270a8eb2f71705b274683656b545ab5ff4
Yvan Gadeau <ygadeau@sugarcrm.com>
2014-10-28 19:17:24 -0700

    [SC-3465] Strict Standards in PDF templates for Quotes

--- sugarcrm/vendor/Smarty/Smarty_Compiler.class.php
+++ sugarcrm/vendor/Smarty/Smarty_Compiler.class.php
@@ -799,7 +799,8 @@ class Smarty_Compiler extends Smarty {
 
         $_cacheable_state = $this->_push_cacheable_state('function', $tag_command);
         $attrs = $this->_parse_attrs($tag_args);
-        $arg_list = $this->_compile_arg_list('function', $tag_command, $attrs, $_cache_attrs='');
+        $_cache_attrs = '';
+        $arg_list = $this->_compile_arg_list('function', $tag_command, $attrs, $_cache_attrs);
 
         $output = $this->_compile_plugin_call('function', $tag_command).'(array('.implode(',', $arg_list)."), \$this)";
         if($tag_modifier != '') {
