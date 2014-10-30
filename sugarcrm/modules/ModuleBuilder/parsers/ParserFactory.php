3bf7241724f86519ac1e3e81b58fc2b463b25120
Jon Whitcraft <jwhitcraft@sugarcrm.com>
2014-10-30 13:38:31 -0400

    SFA-3132: Dupecheck-list Update
    - Code Review Changes

--- sugarcrm/modules/ModuleBuilder/parsers/ParserFactory.php
+++ sugarcrm/modules/ModuleBuilder/parsers/ParserFactory.php
@@ -167,11 +167,9 @@ class ParserFactory
 
                 return new DashletMetaDataParser($view, $moduleName, $packageName  );
             case MB_SIDECARPOPUPVIEW:
-                require_once 'modules/ModuleBuilder/parsers/views/SidecarListLayoutMetaDataParser.php';
-                return new SidecarListLayoutMetaDataParser(MB_SIDECARPOPUPVIEW, $moduleName, $packageName, 'base');
             case MB_SIDECARDUPECHECKVIEW:
                 require_once 'modules/ModuleBuilder/parsers/views/SidecarListLayoutMetaDataParser.php';
-                return new SidecarListLayoutMetaDataParser(MB_SIDECARDUPECHECKVIEW, $moduleName, $packageName, 'base');
+                return new SidecarListLayoutMetaDataParser($view, $moduleName, $packageName, 'base');
             case MB_POPUPLIST :
             case MB_POPUPSEARCH :
                 require_once 'modules/ModuleBuilder/parsers/views/PopupMetaDataParser.php';
