42a856e9e347c732c0f4732f49f1a2181959195e
Boro Sitnikovski <bsitnikovski@sugarcrm.com>
2014-11-24 20:58:37 +0100

    PAT-1246 Handle case where we are requesting jslang

--- sugarcrm/modules/Import/controller.php
+++ sugarcrm/modules/Import/controller.php
@@ -32,6 +32,11 @@ class ImportController extends SugarController
     {
         global $mod_strings;
 
+        if (isset($_REQUEST['entryPoint']) && $_REQUEST['entryPoint'] == "jslang") {
+            // js lang requested, in which case this constructor turns into a noop since we don't have import_module
+            return;
+        }
+
         $this->importModule = isset($_REQUEST['import_module']) ? $_REQUEST['import_module'] : '';
 
         $this->bean = BeanFactory::getBean($this->importModule);
