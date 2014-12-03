b56206fd8694df3b6dab075dfead8cf1a7a8d6c9
Joo Lee <jlee@sugarcrm.com>
2014-12-03 14:26:37 -0500

    MAR-2284: Added 'Convert' rowaction to Leads subpanel.

--- sugarcrm/modules/Leads/clients/base/views/subpanel-list/subpanel-list.php
+++ sugarcrm/modules/Leads/clients/base/views/subpanel-list/subpanel-list.php
@@ -71,4 +71,37 @@ $viewdefs['Leads']['base']['view']['subpanel-list'] = array(
       ),
     ),
   ),
+    'rowactions' => array(
+        'actions' => array(
+            array(
+                'type' => 'rowaction',
+                'css_class' => 'btn',
+                'tooltip' => 'LBL_PREVIEW',
+                'event' => 'list:preview:fire',
+                'icon' => 'fa-eye',
+                'acl_action' => 'view',
+                'allow_bwc' => false,
+            ),
+            array(
+                'type' => 'rowaction',
+                'name' => 'edit_button',
+                'icon' => 'fa-pencil',
+                'label' => 'LBL_EDIT_BUTTON',
+                'event' => 'list:editrow:fire',
+                'acl_action' => 'edit',
+                'allow_bwc' => true,
+            ),
+            array(
+                'type' => 'convertbutton',
+                'name' => 'lead_convert_button',
+                'label' => 'LBL_CONVERT_BUTTON_LABEL',
+                'acl_action' => 'edit',
+            ),
+            array(
+                'type' => 'unlink-action',
+                'icon' => 'fa-chain-broken',
+                'label' => 'LBL_UNLINK_BUTTON',
+            ),
+        ),
+    ),
 );
