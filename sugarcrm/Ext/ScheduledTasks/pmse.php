780de738163481e83d4d02eac4824f05371cfe4d
Robert Gonzalez <rgonzalez@sugarcrm.com>
2014-12-12 15:49:43 -0800

    BR-2280: Unable to create an opportunity / account Fix broken build tags in PMSE files

--- sugarcrm/Ext/ScheduledTasks/pmse.php
+++ sugarcrm/Ext/ScheduledTasks/pmse.php
@@ -1,5 +1,5 @@
 <?php
-//SUGARCRM FILE flav=ent ONLY
+//FILE SUGARCRM flav=ent ONLY
 if(!defined('sugarEntry') || !sugarEntry) die('Not A Valid Entry Point');
 /*
  * Your installation or use of this SugarCRM file is subject to the applicable
