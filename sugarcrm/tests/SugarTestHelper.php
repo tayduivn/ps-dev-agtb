6100e25ece49aec47aa1805ad2b5355541b03760
Gustavo Cruz <gustavo.cruz.laura@gmail.com>
2014-12-12 17:20:20 -0400

    BR-2301: Fix ProcessAuthor filenames

--- sugarcrm/tests/SugarTestHelper.php
+++ sugarcrm/tests/SugarTestHelper.php
@@ -137,7 +137,7 @@ require_once 'SugarTestSugarEmailAddressUtilities.php';
 require_once 'SugarTestCaseUtilities.php';
 require_once 'SugarTestTagUtilities.php';
 require_once 'SugarTestDatabaseMock.php';
-require_once 'SugarProcessAutoUtilities.php';
+require_once 'SugarProcessAuthorUtilities.php';
 
 // custom helper support
 $customHelperIncludeFile = 'custom/tests/SugarTestHelperInclude.php';
