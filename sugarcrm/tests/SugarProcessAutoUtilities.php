21c93ec00c490f6f0ee31b0f93f0003ec526d9f7
Gustavo Cruz <gustavo.cruz.laura@gmail.com>
2014-12-12 15:15:39 -0400

    BR-2301: Removing error_display options

--- sugarcrm/tests/SugarProcessAutoUtilities.php
+++ sugarcrm/tests/SugarProcessAutoUtilities.php
@@ -1,8 +1,8 @@
 <?php
-echo "HELLOO FROM SUGAR";
-error_reporting( E_ALL | E_STRICT );
-ini_set('display_startup_errors', 1);
-ini_set('display_errors', 1);
+
+//error_reporting( E_ALL | E_STRICT );
+//ini_set('display_startup_errors', 1);
+//ini_set('display_errors', 1);
 
 // Define path to application directory
 defined('MODULES_PATH')
