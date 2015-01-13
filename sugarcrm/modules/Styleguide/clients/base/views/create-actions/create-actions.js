90f222924b8c1d08930f22ae38d4550836aee842
Henry Rogers <hrogers@sugarcrm.com>
2015-01-13 00:46:46 -0500

    UIUX-1795: add form horizontal option to styleguide

--- sugarcrm/modules/Styleguide/clients/base/views/create-actions/create-actions.js
+++ sugarcrm/modules/Styleguide/clients/base/views/create-actions/create-actions.js
@@ -12,24 +12,31 @@
     extendsFrom: 'CreateActionsView',
     showHelpText: false,
     showErrorDecoration: false,
+    showFormHorizontal: false,
     events: {
         'click a[name=show_help_text]:not(.disabled)': 'toggleHelpText',
-        'click a[name=display_error_state]:not(.disabled)': 'toggleErrorDecoration'
+        'click a[name=display_error_state]:not(.disabled)': 'toggleErrorDecoration',
+        'click a[name=display_form_horizontal]:not(.disabled)': 'toggleFormHorizontal'
     },
 
     _render: function() {
         this._super('_render');
         var error_string = 'You did a bad, bad thing.';
+        _.each(this.meta.panels, function(panel) {
+            if (!panel.header) {
+                panel.labelsOnTop = this.showFormHorizontal;
+            }
+        }, this);
         if (this.showErrorDecoration) {
             _.each(this.fields, function(field) {
-                if (!_.contains(['button','rowaction','actiondropdown'], field.type)) {
+                if (!_.contains(['button', 'rowaction', 'actiondropdown'], field.type)) {
                     field.setMode('edit');
                     field._errors = error_string;
                     if (field.type === 'email') {
                         var errors = {email: ['primary@example.info']};
                         field.handleValidationError([errors]);
                     } else {
-                        if (_.contains(['image','picture','avatar'], field.type)) {
+                        if (_.contains(['image', 'picture', 'avatar'], field.type)) {
                             field.handleValidationError(error_string);
                         } else {
                             field.decorateError(error_string);
@@ -56,6 +63,11 @@
 
     toggleErrorDecoration: function() {
         this.showErrorDecoration = !this.showErrorDecoration;
+        this.render();
+    },
+
+    toggleFormHorizontal: function() {
+        this.showFormHorizontal = !this.showFormHorizontal;
         this.render();
     }
 })
