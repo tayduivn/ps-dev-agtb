a72c14a4fc9d1fa27fa96806560c2e881707941c
Michael Gusev <mgusev@sugarcrm.com>
2015-01-14 16:06:57 +0100

    CRYS-660: [DB2] Health Check show unexpected error
    To insert blob on db2 we have to use prepared statements which aren't implemented in sugar 6.5.
    
    Because of that we switch type from blob to clob what is almost the same for logmeta field.

--- sugarcrm/modules/HealthCheck/vardefs.php
+++ sugarcrm/modules/HealthCheck/vardefs.php
@@ -37,7 +37,7 @@ $dictionary['HealthCheck'] = array(
             'vname' => 'LBL_LOGMETA',
             'type' => 'json',
             //Sugar 6 does not support `type` => `json` so `blob` is needed to skip SugarBean::cleanBean
-            'dbType' => 'longblob',
+            'dbType' => 'longtext',
         ),
         'error' => array(
             'name' => 'error',
