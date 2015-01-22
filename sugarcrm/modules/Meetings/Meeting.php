b062ec5170abac0b5ef2af46c553679f2428bccd
David Wheeler <dwheeler@sugarcrm.com>
2015-01-22 14:46:50 -0800

    Fixing unlink error in CentOS 6 Unit tests.

--- sugarcrm/modules/Meetings/Meeting.php
+++ sugarcrm/modules/Meetings/Meeting.php
@@ -606,7 +606,9 @@ class Meeting extends SugarBean {
 		parent::send_assignment_notifications($notify_user, $admin);
 
 		$path = SugarConfig::getInstance()->get('upload_dir','upload/') . $this->id;
-		unlink($path);
+        if (SugarAutoLoader::fileExists($path)) {
+            SugarAutoLoader::unlink($path);
+        }
 	}
 
 	function get_meeting_users() {
