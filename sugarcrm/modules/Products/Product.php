b51f54d5d4cd162e946943715f64f2b731ea6313
cwang-sugar <cwang@cwang-mac.cup1.sugarcrm.net>
2015-02-02 15:17:06 -0800

    PAT-1497: Unit Price not updating when copying Quote record

--- sugarcrm/modules/Products/Product.php
+++ sugarcrm/modules/Products/Product.php
@@ -586,11 +586,15 @@ class Product extends SugarBean
         $template = BeanFactory::getBean('ProductTemplates', $this->product_template_id);
         
         foreach ($this->template_fields as $template_field) {
-            // Empty isn't good enough here, if they set a total to 0.00 we need to not
-            // copy that from the template
+            // do not copy from template if field is:  Not empty, or has an int value equal to zero, or a string value equal to '0' or '0.0'
             if (!empty($this->$template_field)
                 || (isset($this->$template_field)
-                    && ($this->$template_field === 0 || $this->$template_field === 0.0))) {
+                    && ($this->$template_field === 0
+                        || $this->$template_field === '0'
+                        || $this->$template_field === '0.0'
+                        )
+                    )
+            ) {
                 continue;
             }
             if (isset($template->$template_field)) {
