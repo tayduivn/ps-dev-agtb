a2ada8e2cf0bd371d066a2ff203f162613059e84
skymeyer <jelle.vink@gmail.com>
2015-02-06 06:18:24 -0800

    BR-2570: Some refactoring on the AbstractProvider class: - Expose some more methods as a public api - Ability to retrieve the service container - Some reordering

--- sugarcrm/src/Elasticsearch/Provider/AbstractProvider.php
+++ sugarcrm/src/Elasticsearch/Provider/AbstractProvider.php
@@ -63,12 +63,30 @@ abstract class AbstractProvider implements ProviderInterface
     }
 
     /**
+     * Get service container
+     * @return \Sugarcrm\Sugarcrm\Elasticsearch\Container
+     */
+    public function getContainer()
+    {
+        return $this->container;
+    }
+
+    /**
      * Set user context
      * @param \User $user
      */
     public function setUser(\User $user)
     {
         $this->user = $user;
+    }
+
+    /**
+     * Get user context
+     * @return \User
+     */
+    public function getUser()
+    {
+        return $this->user;
     }
 
     /**
@@ -119,51 +137,6 @@ abstract class AbstractProvider implements ProviderInterface
     }
 
     /**
-     * Get mapping property for given sugar field type
-     * @param string $sugarType
-     * @return array|false
-     * @throws \Sugarcrm\Sugarcrm\Elasticsearch\Exception\InvalidMappingException
-     */
-    protected function getMappingForSugarType($sugarType)
-    {
-        // resolve sugar type with fallback to default definition if set
-        if (!isset($this->sugarTypes[$sugarType])) {
-            if (!isset($this->sugarTypes[self::DEFAULT_SUGAR_TYPE])) {
-                return false;
-            } else {
-                $sugarType = self::DEFAULT_SUGAR_TYPE;
-            }
-        }
-
-        $mappingDefs = $this->sugarTypes[$sugarType];
-
-        // multiple mappings can be defined at once
-        if (!is_array($mappingDefs)) {
-            $mappingDefs = array($mappingDefs);
-        }
-
-        $properties = array();
-        foreach ($mappingDefs as $mappingDef) {
-            if (!isset($this->mappingDefs[$mappingDef])) {
-                throw new InvalidMappingException("Unknown mapping def '{$mappingDef}'");
-            }
-            $properties[$mappingDef] = $this->mappingDefs[$mappingDef];
-        }
-
-        return $properties;
-    }
-
-    /**
-     * Verify if given sugar type is supported
-     * @param string $type Sugar type
-     * @return boolean
-     */
-    protected function isSupportedSugarType($type)
-    {
-        return isset($this->sugarTypes[$type]);
-    }
-
-    /**
      * Add property list to mapping
      * @param string $field
      * @param Mappging $mapping
@@ -189,5 +162,59 @@ abstract class AbstractProvider implements ProviderInterface
                 $this->addProperties($field, $mapping, $properties);
             }
         }
+    }
+
+    /**
+     * Get mapping properties for given sugar field type
+     * @param string $sugarType
+     * @return array
+     * @throws \Sugarcrm\Sugarcrm\Elasticsearch\Exception\InvalidMappingException
+     */
+    public function getMappingForSugarType($sugarType)
+    {
+        $properties = array();
+        foreach ($this->getMappingDefsForSugarType($sugarType) as $mappingDef) {
+            if (!isset($this->mappingDefs[$mappingDef])) {
+                throw new InvalidMappingException("Unknown mapping def '{$mappingDef}'");
+            }
+            $properties[$mappingDef] = $this->mappingDefs[$mappingDef];
+        }
+        return $properties;
+    }
+
+    /**
+     * Get mapping definitions for given sugar field type
+     * @param string $sugarType
+     * @return array
+     */
+    public function getMappingDefsForSugarType($sugarType)
+    {
+        // resolve sugar type with fallback to default definition if set
+        if (!isset($this->sugarTypes[$sugarType])) {
+            if (!isset($this->sugarTypes[self::DEFAULT_SUGAR_TYPE])) {
+                return array();
+            } else {
+                $sugarType = self::DEFAULT_SUGAR_TYPE;
+            }
+        }
+
+        $mappingDefs = $this->sugarTypes[$sugarType];
+
+        // one or multiple mappings can be defined
+        if (!is_array($mappingDefs)) {
+            $mappingDefs = array($mappingDefs);
+        }
+
+        return $mappingDefs;
+    }
+
+    /**
+     * Verify if given sugar type is supported
+     * @param string $type Sugar type
+     * @return boolean
+     */
+    public function isSupportedSugarType($type)
+    {
+        return isset($this->sugarTypes[$type]);
     }
 }
