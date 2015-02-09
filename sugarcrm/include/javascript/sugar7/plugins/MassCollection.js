8c982dd98e4f7e3f6d9bb139548148d25205f154
Yvan Gadeau <ygadeau@sugarcrm.com>
2015-02-09 14:38:54 -0800

    SC-1723: Refactor actionmenu to extend actiondropdown.

--- sugarcrm/include/javascript/sugar7/plugins/MassCollection.js
+++ sugarcrm/include/javascript/sugar7/plugins/MassCollection.js
@@ -40,6 +40,7 @@
             _initMassCollectionPlugin: function() {
                 this.createMassCollection();
                 this._preselectModels();
+                this._initTemplates();
                 this._bindMassCollectionEvents();
             },
 
@@ -74,17 +75,7 @@
                     });
                     this.massCollection = new MassCollection();
                     this.context.set('mass_collection', this.massCollection);
-
-                    // Resets the mass collection on collection reset for non
-                    // independent mass collection.
-                    if (!this.independentMassCollection) {
-                        this.collection.on('reset', function() {
-                            this.massCollection.reset();
-                        }, this);
-                    }
                 }
-
-                return this.massCollection;
             },
 
             /**
@@ -129,6 +120,21 @@
                 this.context.on('mass_collection:remove', this.removeModel, this);
                 this.context.on('mass_collection:remove:all', this.removeAllModels, this);
                 this.context.on('mass_collection:clear', this.clearMassCollection, this);
+                this.context.on('toggleSelectAllAlert', this.toggleSelectAllAlert, this);
+
+                // Resets the mass collection on collection reset for non
+                // independent mass collection.
+                this.collection.on('reset', function() {
+                    if (!this.independentMassCollection || this.massCollection.entire) {
+                        this.massCollection.reset();
+                    }
+                }, this);
+
+                this.collection.on('add', function() {
+                    if (!this.disposed && !this._isAllChecked()) {
+                        this.massCollection.trigger('not:all:checked');
+                    }
+                }, this);
             },
 
             /**
@@ -213,6 +219,204 @@
             },
 
             /**
+             * Shows or hides the appropriate alert based on the state of the mass collection.
+             *
+             * FIXME: This method will be removed by SC-3999 because alerts
+             * displayed within a list view are to be removed.
+             */
+            toggleSelectAllAlert: function() {
+                var alert;
+                if (!this.collection.length) {
+                    return;
+                }
+                var selectedRecordsInPage = _.intersection(this.massCollection.models, this.collection.models);
+                if (selectedRecordsInPage.length === this.collection.length) {
+                    if (this.collection.next_offset > 0) {
+                        alert = this._buildAlertForEntire();
+                    } else {
+                        alert = this._buildAlertForReset();
+                    }
+                } else if (this.massCollection.entire) {
+                    alert = this._buildAlertForReset();
+                }
+                if (alert) {
+                    this.layout.trigger('list:alert:show', alert);
+                } else {
+                    this.layout.trigger('list:alert:hide');
+                }
+            },
+
+
+
+            /**
+             * Builds the DOM alert with an event for resetting the mass collection.
+             *
+             * @param {number} [offset] The collection offset.
+             * @return {jQuery} The alert content.
+             * @protected
+             *
+             * FIXME: This method will be removed by SC-3999 because alerts
+             * displayed within a list view are to be removed.
+             */
+            _buildAlertForReset: function(offset) {
+                var self = this;
+                var alert = $('<span></span>').append(this._selectedOffsetTpl({
+                    offset: offset,
+                    num: this.massCollection.length
+                }));
+                alert.find('[data-action=clear]').each(function() {
+                    var $el = $(this);
+                    $el.on('click', function() {
+                        self.massCollection.reset();
+                        self.massCollection.trigger('not:all:checked');
+                        $el.off('click');
+                    });
+                    app.accessibility.run($el, 'click');
+                });
+                return alert;
+            },
+
+            /**
+             * Builds the DOM alert with event for selecting all records.
+             *
+             * @return {jQuery} The alert content.
+             * @protected
+             *
+             * FIXME: This method will be removed by SC-3999 because alerts
+             * displayed within a list view are to be removed.
+             */
+            _buildAlertForEntire: function() {
+                var self = this;
+                var alert = $('<span></span>').append(this._selectAllTpl({
+                        num: this.massCollection.length,
+                        link: this._selectAllLinkTpl
+                    }));
+                alert.find('[data-action=select-all]').each(function() {
+                    var $el = $(this);
+                    $el.on('click', function() {
+                        self.massCollection.entire = true;
+                        self.getTotalRecords();
+                        $el.off('click');
+                    });
+                    app.accessibility.run($el, 'click');
+                });
+                return alert;
+            },
+
+            /**
+             * Fetch api to retrieve the entire filtered set.
+             */
+            getTotalRecords: function() {
+                var filterDef = this.massCollection.filterDef || [];
+                if (!_.isArray(filterDef)) {
+                    filterDef = [filterDef];
+                }
+                //if list view is for linking and link fetch size configuration exists, set it,
+                //otherwise default to maxRecordFetchSize
+                var max_num = (this.meta.selection.isLinkAction && app.config.maxRecordLinkFetchSize) ?
+                        app.config.maxRecordLinkFetchSize :
+                        app.config.maxRecordFetchSize;
+                var order = this.context.get('collection').orderBy;
+                var fields = ['id'];
+
+                // if any of the buttons require additional fields, add them to the list
+                _.each(this.meta.selection.actions, function(button) {
+                    if (_.isArray(button.related_fields)) {
+                        fields = _.union(fields, button.related_fields);
+                    }
+                }, this);
+
+                var params = {
+                    fields: fields.join(','),
+                    max_num: max_num
+                };
+
+                if (order && order.field) {
+                    params.order_by = order.field + ':' + order.direction;
+                }
+
+                if (!_.isEmpty(filterDef)) {
+                    params.filter = filterDef;
+                }
+
+                var url = app.api.buildURL(this.module, null, null, params);
+
+                app.alert.show('totalrecord', {
+                    level: 'process',
+                    title: app.lang.get('LBL_LOADING'),
+                    autoClose: false
+                });
+
+                this.massCollection.fetched = false;
+                this.massCollection.trigger('massupdate:estimate');
+
+                app.api.call('read', url, null, {
+                    success: _.bind(function(data) {
+                        if (this.disposed) {
+                            return;
+                        }
+                        app.alert.dismiss('totalrecord');
+                        this._processTotalRecords(data.records);
+                        this._alertTotalRecords(data.next_offset);
+                    }, this)
+                });
+            },
+
+            /**
+             * Update total record set from api request.
+             *
+             * @param {Object} collection The list of JSON formatted list of model ids.
+             * @private
+             */
+            _processTotalRecords: function(collection) {
+                this.massCollection.reset(collection);
+                this.massCollection.entire = false;
+                this.massCollection.fetched = true;
+                this.massCollection.trigger('massupdate:estimate');
+            },
+
+            /**
+             * Alert the message for total record set.
+             *
+             * @param {Number} offset Next pagination offset.
+             * @private
+             *
+             * FIXME: This method will be removed by SC-3999 because alerts
+             * displayed within a list view are to be removed.
+             */
+            _alertTotalRecords: function(offset) {
+                var allSelected = this._buildAlertForReset(offset);
+                this.layout.trigger('list:alert:show', allSelected);
+            },
+
+
+            /**
+             * Initialize templates.
+             *
+             * @return {Field.ActionMenuField} Instance of this field.
+             * @template
+             * @protected
+             *
+             * FIXME: This method will be removed by SC-3999 because alerts
+             * displayed within a list view are to be removed.
+             */
+            _initTemplates: function() {
+                this._selectedOffsetTpl = app.template.getView('list.selected-offset') ||
+                    app.template.getView('list.selected-offset', this.module);
+
+                //FIXME: This should be move to a partial template when we are
+                // going to move plugins to the clients folder.
+                this._selectAllLinkTpl = new Handlebars.SafeString(
+                    '<button type="button" class="btn btn-link btn-inline" data-action="select-all">' +
+                        app.lang.get('LBL_LISTVIEW_SELECT_ALL_RECORDS') +
+                        '</button>'
+                );
+                this._selectAllTpl = app.template.compile(null, app.lang.get('TPL_LISTVIEW_SELECT_ALL_RECORDS'));
+
+                return this;
+            },
+
+            /**
              * Unbind events on dispose.
              */
             onDetach: function() {
@@ -221,6 +425,8 @@
                 this.context.off('mass_collection:add:all', null, this);
                 this.context.off('mass_collection:remove', null, this);
                 this.context.off('mass_collection:remove:all', null, this);
+                this.context.off('toggleSelectAllAlert', null, this);
+                this.context.off('mass_collection:clear', null, this);
             }
         });
     });
