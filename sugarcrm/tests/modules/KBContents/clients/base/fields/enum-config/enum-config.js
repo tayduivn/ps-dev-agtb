09923f274037c57c4eaaaa59bf9d10e67ab0bc1d
Alexander Rymarchik <a.rymarchik@intetics.com>
2015-02-17 13:48:04 +0300

    RS-636: KB: JS/Unit Tests.

--- sugarcrm/tests/modules/KBContents/clients/base/fields/enum-config/enum-config.js
+++ sugarcrm/tests/modules/KBContents/clients/base/fields/enum-config/enum-config.js
@@ -33,12 +33,85 @@ describe('modules.kbcontents.clients.base.fields.enum-config', function() {
         expect(field.items).toEqual({});
     });
 
-    it('should be disabled if model is not new record', function () {
-        model.set({
+    it('should be disabled if model has related languages and relate article', function() {
+        field.model.set({
+            related_languages: ['en', 'us'],
+            kbarticle_id: 'kb_id'
+        });
+        field.setMode('edit');
+        expect(field.action).toEqual('disabled');
+    });
+
+    it('should remove related languages from items and be editable', function() {
+        field._setItems([
+            {
+                'us': 'English',
+                'primary': false
+            },
+            {
+                'be': 'Belgium',
+                'primary': true
+            }
+        ]);
+        field.model.set({
+            related_languages: ['en', 'be']
+        });
+        expect(_.keys(field.items)).toEqual(['us', 'be']);
+        expect(field.model.get('language')).toEqual('be');
+        field.setMode('edit');
+        expect(field.action).toEqual('edit');
+        expect(_.keys(field.items)).toEqual(['us']);
+        expect(field.model.get('language')).toEqual('us');
+    });
+
+    it('should be disabled if model is not new record', function() {
+        field.model.set({
             id: 'test_id'
         });
         field.setMode('edit');
         expect(field.action).toEqual('disabled');
     });
 
+    it('should set languages from meta and server correctly', function() {
+        var metaDef = {
+            'category_root': '__',
+            'languages': [
+                {
+                    'en': 'English',
+                    'primary': true
+                }
+            ]
+            },
+            configDef = {
+                'category_root': '__',
+                'languages': [
+                    {
+                        'be': 'Belgium',
+                        'primary': true
+                    }
+                ]
+            },
+            setItems = sandbox.spy(field, '_setItems'),
+            urlRegExp = new RegExp('.*rest/v10/' + module + '/config.*');
+        field.def = {
+            module: module,
+            key: 'languages'
+        };
+        sandbox.stub(app.metadata, 'getModule', function() {
+            return metaDef;
+        });
+
+        SugarTest.seedFakeServer();
+        SugarTest.server.respondWith(
+            'GET',
+            urlRegExp,
+            [200, {'Content-Type': 'application/json'}, JSON.stringify(configDef)]
+        );
+        field.loadEnumOptions(true, $.noop);
+        expect(setItems.callCount).toEqual(1);
+        expect(field.items).toEqual({'en': 'English'});
+        SugarTest.server.respond();
+        expect(setItems.callCount).toEqual(2);
+        expect(field.items).toEqual({'be': 'Belgium'});
+    });
 });
