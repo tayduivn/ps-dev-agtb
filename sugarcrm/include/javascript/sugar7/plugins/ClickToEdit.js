439e9e8f393b633c1451793c1a4107774456b9fb
Travis Hubbard <thubbard@sugarcrm.com>
2015-02-18 11:58:11 -0500

    SFA-3326 updated Forecast enum and CTE to not add isEditable when field is disabled

--- sugarcrm/include/javascript/sugar7/plugins/ClickToEdit.js
+++ sugarcrm/include/javascript/sugar7/plugins/ClickToEdit.js
@@ -221,8 +221,11 @@
                     } else {
                         this.$el.removeClass('active');
                     }
-                    this.$el.addClass('isEditable');
-                    this.$el.wrapInner('<div class="' + cteClass + '" data-cid="' + this.cid + '" />');
+                    // only add isEditable if the field is not disabled
+                    if (!this.$el.hasClass('disabled')) {
+                        this.$el.addClass('isEditable');
+                        this.$el.wrapInner('<div class="' + cteClass + '" data-cid="' + this.cid + '" />');
+                    }
                 }, this);
                 if (this.context.parent) {
                     // Clears errors when navigating from the manager's forecast worksheet to the manager's RLI so that
