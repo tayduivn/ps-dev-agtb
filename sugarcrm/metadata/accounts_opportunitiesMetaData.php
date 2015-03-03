29f7e5b0732406680fc9151ed17e6c5589a1c158
Sergey Morozov <smorozov@sugarcrm.com>
2015-03-03 18:15:19 +0300

    [BR-2641] Opportunities Module Relationships Should Be Accurately Reflected

--- sugarcrm/metadata/accounts_opportunitiesMetaData.php
+++ sugarcrm/metadata/accounts_opportunitiesMetaData.php
@@ -22,12 +22,20 @@ $dictionary['accounts_opportunities'] = array ( 'table' => 'accounts_opportuniti
        array('name' =>'accounts_opportunitiespk', 'type' =>'primary', 'fields'=>array('id'))
       , array('name' => 'idx_account_opportunity', 'type'=>'alternate_key', 'fields'=>array('account_id','opportunity_id'))      
       , array('name' => 'idx_oppid_del_accid', 'type' => 'index', 'fields'=> array('opportunity_id', 'deleted', 'account_id'))
-      )
-      
-      ,'relationships' => array ('accounts_opportunities' => array('lhs_module'=> 'Accounts', 'lhs_table'=> 'accounts', 'lhs_key' => 'id',
-							  'rhs_module'=> 'Opportunities', 'rhs_table'=> 'opportunities', 'rhs_key' => 'id',
-							  'relationship_type'=>'many-to-many',
-							  'join_table'=> 'accounts_opportunities', 'join_key_lhs'=>'account_id', 'join_key_rhs'=>'opportunity_id'))
-	  	
-)
-?>
+      ),
+    'relationships' => array(
+        'accounts_opportunities' => array(
+            'lhs_module' => 'Accounts',
+            'lhs_table' => 'accounts',
+            'lhs_key' => 'id',
+            'rhs_module' => 'Opportunities',
+            'rhs_table' => 'opportunities',
+            'rhs_key' => 'id',
+            'relationship_type' => 'one-to-many',
+            'join_table' => 'accounts_opportunities',
+            'join_key_lhs' => 'account_id',
+            'join_key_rhs' => 'opportunity_id',
+            'true_relationship_type' => 'one-to-many',
+        ),
+    ),
+);
