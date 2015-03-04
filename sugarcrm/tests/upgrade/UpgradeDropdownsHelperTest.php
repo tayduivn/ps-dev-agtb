5ec664f5f854af19c000c9ab74319fd5f5f352ff
Greg Levine <levine.greg@gmail.com>
2015-03-03 23:56:56 -0500

    Refactored tests to not use the filesystem
    Some refactoring of `UpgradeDropdownsHelper` was required.
    
    resolves jira:MAR-2737

--- sugarcrm/tests/upgrade/UpgradeDropdownsHelperTest.php
+++ sugarcrm/tests/upgrade/UpgradeDropdownsHelperTest.php
@@ -17,172 +17,81 @@ require_once 'upgrade/UpgradeDropdownsHelper.php';
  */
 class UpgradeDropdownsHelperTest extends Sugar_PHPUnit_Framework_TestCase
 {
-    protected $corePath = 'include/language/';
-    protected $customPath = 'custom/include/language/';
-    protected $helper;
-
-    public function setUp()
-    {
-        parent::setUp();
-
-        SugarTestHelper::setUp('files');
-        SugarTestHelper::saveFile("{$this->corePath}{$GLOBALS['current_language']}.lang.php");
-        SugarTestHelper::saveFile("{$this->customPath}{$GLOBALS['current_language']}.lang.php");
-        sugar_cache_clear("app_strings.{$GLOBALS['current_language']}");
-        sugar_cache_clear("app_list_strings.{$GLOBALS['current_language']}");
-
-        $this->helper = new UpgradeDropdownsHelper();
-    }
-
-    public function tearDown()
-    {
-        SugarTestLanguageFileUtilities::clearCache();
-        SugarTestHelper::tearDown();
-
-        parent::tearDown();
-    }
-
     /**
      * @covers UpgradeDropdownsHelper::getDropdowns
      */
     public function testGetDropdowns_ReturnsCoreDropDowns()
     {
-        $core = <<<EOF
-\$app_list_strings = array(
-    'sales_stage_default_key' => 'Prospecting',
-    'activity_dom' => array(
-        'Call' => 'Call',
-        'Meeting' => 'Meeting',
-        'Task' => 'Task',
-        'Email' => 'Email',
-        'Note' => 'Note',
-    ),
-    'meeting_status_dom' => array(
-        'Planned' => 'Planned',
-        'Held' => 'Held',
-        'Not Held' => 'Not Held',
-    ),
-);
+        $mockHelper = $this->getMock('UpgradeDropdownsHelper', array('getAppListStringsFromFile'));
+        $mockHelper->expects($this->once())
+            ->method('getAppListStringsFromFile')
+            ->willReturn(array(
+                'sales_stage_default_key' => 'Prospecting',
+                'activity_dom' => array(
+                    'Call' => 'Call',
+                    'Meeting' => 'Meeting',
+                    'Task' => 'Task',
+                    'Email' => 'Email',
+                    'Note' => 'Note',
+                ),
+                'meeting_status_dom' => array(
+                    'Planned' => 'Planned',
+                    'Held' => 'Held',
+                    'Not Held' => 'Not Held',
+                ),
+            ));
 
-EOF;
-
-        $custom = <<<EOF
-\$app_list_strings = array(
-    'activity_dom' => array(
-        'Call' => 'Call',
-        'Meeting' => 'Meeting',
-        'Task' => 'To Do',
-        'Email' => 'Email',
-        'Note' => 'Note',
-        'SMS' => 'Text Message',
-    ),
-);
-
-EOF;
-
-        SugarTestLanguageFileUtilities::write($this->corePath, $GLOBALS['current_language'], $core);
-        SugarTestLanguageFileUtilities::write($this->customPath, $GLOBALS['current_language'], $custom);
-
-        $actual = $this->helper->getDropdowns("{$this->corePath}{$GLOBALS['current_language']}.lang.php");
+        $actual = $mockHelper->getDropdowns('include/language/en_us.lang.php');
 
         $this->assertArrayHasKey('activity_dom', $actual);
         $this->assertArrayHasKey('meeting_status_dom', $actual);
         $this->assertEquals('Task', $actual['activity_dom']['Task']);
     }
 
-    /**
-     * @covers UpgradeDropdownsHelper::getDropdowns
-     */
-    public function testGetDropdowns_ReturnsCustomDropDowns()
-    {
-        $core = <<<EOF
-\$app_list_strings = array(
-    'sales_stage_default_key' => 'Prospecting',
-    'activity_dom' => array(
-        'Call' => 'Call',
-        'Meeting' => 'Meeting',
-        'Task' => 'Task',
-        'Email' => 'Email',
-        'Note' => 'Note',
-    ),
-    'meeting_status_dom' => array(
-        'Planned' => 'Planned',
-        'Held' => 'Held',
-        'Not Held' => 'Not Held',
-    ),
-);
-
-EOF;
-
-        $custom = <<<EOF
-\$app_list_strings = array(
-    'activity_dom' => array(
-        'Call' => 'Call',
-        'Meeting' => 'Meeting',
-        'Task' => 'To Do',
-        'Email' => 'Email',
-        'Note' => 'Note',
-        'SMS' => 'Text Message',
-    ),
-);
-
-EOF;
-
-        SugarTestLanguageFileUtilities::write($this->corePath, $GLOBALS['current_language'], $core);
-        SugarTestLanguageFileUtilities::write($this->customPath, $GLOBALS['current_language'], $custom);
-
-        $actual = $this->helper->getDropdowns("{$this->customPath}{$GLOBALS['current_language']}.lang.php");
-
-        $this->assertArrayHasKey('activity_dom', $actual);
-        $this->assertArrayNotHasKey('meeting_status_dom', $actual);
-        $this->assertEquals('To Do', $actual['activity_dom']['Task']);
-    }
-
     public function getDropDownsRestrictedDropDownsAreIgnoredProvider()
     {
         return array(
-            array(true),
-            array(false),
+            array('eapm_list'),
+            array('eapm_list_documents'),
+            array('eapm_list_import'),
+            array('extapi_meeting_password'),
+            array('Elastic_boost_options'),
+            //BEGIN SUGARCRM flav=pro ONLY
+            array('commit_stage_dom'),
+            array('commit_stage_custom_dom'),
+            array('commit_stage_binary_dom'),
+            array('forecasts_config_ranges_options_dom'),
+            array('forecasts_timeperiod_types_dom'),
+            array('forecasts_chart_options_group'),
+            array('forecasts_config_worksheet_layout_forecast_by_options_dom'),
+            array('forecasts_timeperiod_options_dom'),
+            array('generic_timeperiod_options'),
+            //END SUGARCRM flav=pro ONLY
         );
     }
 
     /**
      * @covers UpgradeDropdownsHelper::getDropdowns
      * @dataProvider getDropDownsRestrictedDropDownsAreIgnoredProvider
-     * @param $isCustom
+     * @param $dropdown
      */
-    public function testGetDropdowns_RestrictedDropDownsAreIgnored($isCustom)
+    public function testGetDropdowns_RestrictedDropDownsAreIgnored($dropdown)
     {
-        $core = <<<EOF
-\$app_list_strings = array(
-    'eapm_list' => array(
-        'Sugar' => 'Sugar',
-        'WebEx' => 'WebEx',
-        'GoToMeeting' => 'GoToMeeting',
-        'IBMSmartCloud' => 'IBM SmartCloud',
-        'Google' => 'Google',
-        'Box' => 'Box.net',
-        'Facebook' => 'Facebook',
-        'Twitter' => 'Twitter',
-    ),
-);
+        $mockHelper = $this->getMock('UpgradeDropdownsHelper', array('getAppListStringsFromFile'));
+        $mockHelper->expects($this->once())
+            ->method('getAppListStringsFromFile')
+            ->willReturn(
+                array(
+                    $dropdown => array(
+                        'Foo' => 'foo',
+                        'Bar' => 'bar',
+                        'Biz' => 'biz',
+                        'Baz' => 'baz',
+                    ),
+                )
+            );
 
-EOF;
-
-        $custom = <<<EOF
-\$app_list_strings = array(
-    'eapm_list' => array(
-        'Foo' => 'Bar',
-    ),
-);
-
-EOF;
-
-        SugarTestLanguageFileUtilities::write($this->corePath, $GLOBALS['current_language'], $core);
-        SugarTestLanguageFileUtilities::write($this->customPath, $GLOBALS['current_language'], $custom);
-
-        $prefix = $isCustom ? $this->customPath : $this->corePath;
-        $actual = $this->helper->getDropdowns("{$prefix}{$GLOBALS['current_language']}.lang.php");
+        $actual = $mockHelper->getDropdowns('include/language/en_us.lang.php');
 
         $this->assertEmpty($actual);
     }
@@ -192,7 +101,8 @@ EOF;
      */
     public function testGetDropdowns_FileDoesNotExist_ReturnsAnEmptyArray()
     {
-        $actual = $this->helper->getDropdowns('./foobar');
+        $helper = new UpgradeDropdownsHelper();
+        $actual = $helper->getDropdowns('./foobar');
 
         $this->assertEmpty($actual);
     }
@@ -202,25 +112,6 @@ EOF;
      */
     public function testGetDropdowns_GLOBALSIsUsedInTheCustomizations_ReturnsCustomDropDowns()
     {
-        $core = <<<EOF
-\$app_list_strings = array(
-    'sales_stage_default_key' => 'Prospecting',
-    'activity_dom' => array(
-        'Call' => 'Call',
-        'Meeting' => 'Meeting',
-        'Task' => 'Task',
-        'Email' => 'Email',
-        'Note' => 'Note',
-    ),
-    'meeting_status_dom' => array(
-        'Planned' => 'Planned',
-        'Held' => 'Held',
-        'Not Held' => 'Not Held',
-    ),
-);
-
-EOF;
-
         $custom = <<<EOF
 \$GLOBALS['app_list_strings']['activity_dom'] = array(
     'Call' => 'Call',
@@ -233,13 +124,15 @@ EOF;
 
 EOF;
 
-        SugarTestLanguageFileUtilities::write($this->corePath, $GLOBALS['current_language'], $core);
-        SugarTestLanguageFileUtilities::write($this->customPath, $GLOBALS['current_language'], $custom);
+        $tmpFileName = time();
+        file_put_contents($tmpFileName, "<?php\n{$custom}\n");
 
-        $actual = $this->helper->getDropdowns("{$this->customPath}{$GLOBALS['current_language']}.lang.php");
+        $helper = new UpgradeDropdownsHelper();
+        $actual = $helper->getDropdowns($tmpFileName);
 
         $this->assertArrayHasKey('activity_dom', $actual);
-        $this->assertArrayNotHasKey('meeting_status_dom', $actual);
         $this->assertEquals('To Do', $actual['activity_dom']['Task']);
+
+        unlink($tmpFileName);
     }
 }
