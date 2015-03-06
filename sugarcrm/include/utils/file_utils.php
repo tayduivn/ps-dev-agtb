a195def0c733e4673f246d25aa2412d52f71ebf8
skymeyer <jelle.vink@gmail.com>
2015-03-06 13:15:42 -0800

    BR-2364: Remove functionality to create deprecated QueueManager hook

--- sugarcrm/include/utils/file_utils.php
+++ sugarcrm/include/utils/file_utils.php
@@ -456,28 +456,6 @@ function get_mime_content_type_from_filename($filename)
     return '';
 }
 
-function createFTSLogicHook($filePath = 'application/Ext/LogicHooks/logichooks.ext.php')
-{
-    $customFileLoc = create_custom_directory($filePath);
-    $fp = sugar_fopen($customFileLoc, 'wb');
-    $contents = <<<CIA
-<?php
-if (!isset(\$hook_array) || !is_array(\$hook_array)) {
-    \$hook_array = array();
-}
-if (!isset(\$hook_array['after_save']) || !is_array(\$hook_array['after_save'])) {
-    \$hook_array['after_save'] = array();
-}
-\$managerClassPath = SugarAutoLoader::requireWithCustom('include/SugarSearchEngine/SugarSearchEngineQueueManager.php');
-\$managerClassName = SugarAutoLoader::customClass('SugarSearchEngineQueueManager');
-\$hook_array['after_save'][] = array(1, 'fts', \$managerClassPath, \$managerClassName, 'populateIndexQueue');
-CIA;
-
-    fwrite($fp,$contents);
-    fclose($fp);
-
-}
-
 function cleanFileName($name)
 {
     return preg_replace('/[^\w-._]+/i', '', $name);
