9c55b51e4f4cdea87e6064129aa1d48bd6714d64
Alexander Rymarchik <a.rymarchik@intetics.com>
2015-03-06 15:26:30 +0300

    RS-353: KB: Make KB module studio enabled.

---
+++ sugarcrm/modules/KBContents/clients/base/views/selection-list/selection-list.php
@@ -0,0 +1,45 @@
+<?php
+/*
+ * Your installation or use of this SugarCRM file is subject to the applicable
+ * terms available at
+ * http://support.sugarcrm.com/06_Customer_Center/10_Master_Subscription_Agreements/.
+ * If you do not agree to all of the applicable terms or do not have the
+ * authority to bind the entity as an authorized representative, then do not
+ * install or use this SugarCRM file.
+ *
+ * Copyright (C) SugarCRM Inc. All rights reserved.
+ */
+
+$viewdefs['KBContents']['base']['view']['selection-list'] = array(
+    'panels' => array(
+        array(
+            'name' => 'panel_header',
+            'label' => 'LBL_PANEL_1',
+            'fields' => array(
+                array(
+                    'name' => 'name',
+                    'label' => 'LBL_NAME',
+                    'default' => true,
+                    'enabled' => true,
+                    'link' => true,
+                ),
+                array(
+                    'name' => 'language',
+                    'label' => 'LBL_LANG',
+                    'default' => true,
+                    'enabled' => true,
+                    'link' => true,
+                    'type' => 'enum-config',
+                    'key' => 'languages',
+                ),
+                array(
+                    'name' => 'status',
+                    'label' => 'LBL_STATUS',
+                    'enabled' => true,
+                    'default' => true,
+                    'type' => 'status',
+                ),
+            ),
+        ),
+    ),
+);
