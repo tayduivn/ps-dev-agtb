55501d60d3b4d592feb697d981db1412ba45259e
Jon Whitcraft <jwhitcraft@sugarcrm.com>
2015-03-16 11:10:15 -0400

    SFA-3448: Fix Currency Conversion Issues
    - Fix the dependencydef on Opps so if we are using Opps w/ RLI's it
    will trigger on `sales_status` change vs `sales_stage`
    - Fix some errors on `cron` when running the currency updated and no
    products are in the system
    - Fix the RLI Currency Update to only run the Opp update when RLI's
    are in use, and make sure to multiple value that is getting set on
    the Opp record by the Opp `base_rate`

--- sugarcrm/modules/Opportunities/include/OpportunityWithRevenueLineItem.php
+++ sugarcrm/modules/Opportunities/include/OpportunityWithRevenueLineItem.php
@@ -159,16 +159,25 @@ EOL;
 
         SugarAutoLoader::ensureDir($this->moduleExtFolder . '/Dependencies');
 
+
+        // below is a ugly hack to update the `set_base_rate` dependency
         $file_contents = <<<EOL
 <?php
 if (isset(\$dependencies['Opportunities'])) {
     foreach(
-        array('set_base_rate','commit_stage_readonly_set_value','best_worst_sales_stage_read_only','likely_case_copy_when_closed')
+        array('commit_stage_readonly_set_value','best_worst_sales_stage_read_only','likely_case_copy_when_closed')
         as \$dep
     ) {
         if (isset(\$dependencies['Opportunities'][\$dep])) {
             unset(\$dependencies['Opportunities'][\$dep]);
         }
+    }
+
+    // the `set_base_rate` dependency needs to use 'sales_status' here
+    if (isset(\$dependencies['Opportunities']['set_base_rate'])) {
+        \$dependencies['Opportunities']['set_base_rate']['triggerFields'] = array('sales_status');
+        \$dependencies['Opportunities']['set_base_rate']['actions'][0]['params']['value'] =
+            'ifElse(isForecastClosed(\$sales_status), \$base_rate, currencyRate(\$currency_id))';
     }
 }
 EOL;
