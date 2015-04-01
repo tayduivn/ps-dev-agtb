9725205a1927a2c420c3ca10222f5c70bbe31bc2
Sanjay <sanjay1991@gmail.com>
2015-04-01 10:22:33 -0700

    SF-36 : Blank tooltip is getting displayed when hover over the "create" icon available under users subpanel of a Target List.

--- sugarcrm/modules/Users/clients/base/views/panel-top-for-prospectlists/panel-top-for-prospectlists.php
+++ sugarcrm/modules/Users/clients/base/views/panel-top-for-prospectlists/panel-top-for-prospectlists.php
@@ -23,6 +23,7 @@ $viewdefs['Users']['base']['view']['panel-top-for-prospectlists'] = array(
                     'icon' => 'fa-plus',
                     'name' => 'create_button',
                     'label' => ' ',
+                    'tooltip' => 'LBL_CREATE_BUTTON_LABEL',
                     'acl_action' => 'create',
                     'css_class' => 'disabled',
                 ),
