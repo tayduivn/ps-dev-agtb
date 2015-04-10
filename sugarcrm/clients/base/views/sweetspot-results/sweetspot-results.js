239559be0dc4ecc648a9386e49de68eadd5522de
jgirault <jgirault@sugarcrm.com>
2015-04-09 19:14:19 -0700

    SC-4317: Change the dom structure, do partial renders, retrieve old highlighted item

--- sugarcrm/clients/base/views/sweetspot-results/sweetspot-results.js
+++ sugarcrm/clients/base/views/sweetspot-results/sweetspot-results.js
@@ -33,12 +33,44 @@
         /**
          * The list of results returned by Sweet Spot, split by category.
          *
-         * @type {Array}
+         * It serves as a helper for the navigation within the results.
+         * Includes:
+         *
+         *  - {@link #actions}
+         *  - {@link #keywords}
+         *  - {@link #records}
+         *
+         * @property {Array}
          */
         this.results = [];
+
+        /**
+         * The list of results returned by Sweet Spot, split by category.
+         *
+         * @property {Array}
+         */
         this.records = [];
+
+        /**
+         * The list of results returned by Sweet Spot, split by category.
+         *
+         * @property {Array}
+         */
         this.actions = [];
+
+        /**
+         * The list of results returned by Sweet Spot, split by category.
+         *
+         * @property {Array}
+         */
         this.keywords = [];
+
+        /**
+         * Partial template for rendering a result row.
+         *
+         * @property {Function}
+         */
+        this._resultPartial = app.template.get(this.name + '.result');
 
         /**
          * Stores the index of the currently highlighted list element.
@@ -48,26 +80,90 @@
          */
         this.activeIndex = null;
 
+        // Listens to new set of results and updates the different sections.
         this.layout.on('sweetspot:results', function(results) {
-            this.actions = this._formatResults(results.actions);
-            this.records = this._formatResults(results.records);
-            this.keywords = this._formatResults(results.keywords);
+            // We want to highlight the same item that was highlighted before,
+            // so first we get the result that was highlighted.
+            var oldHighlighted = this.results[this.activeIndex];
+
+            // Rendering different sections
+            this.renderSection('actions', this._formatResults(results.actions));
+            this.renderSection('records', this._formatResults(results.records));
+            this.renderSection('keywords', this._formatResults(results.keywords));
+
+            // Update with the new list of records.
             this.results = this.keywords.concat(this.actions).concat(this.records);
             this.showMore = results.showMore;
-            this.term = results.term;
-            this.render();
+
+            var newActiveIndex;
+            if (oldHighlighted) {
+                // Try to find the old highlighted result in the new data set.
+                _.find(this.results, function(result, index) {
+                    var test;
+                    if (oldHighlighted.id) {
+                        test = oldHighlighted.id === result.id;
+                    }
+                    if (oldHighlighted.route) {
+                        test = oldHighlighted.route === result.route;
+                    }
+                    if (test) {
+                        // Once we found it, we actually want its new index.
+                        newActiveIndex = index;
+                        return true;
+                    }
+                });
+            }
+            // Sets the item that will be highlighted.
+            this.activeIndex = newActiveIndex || 0;
+            if (this.results.length) {
+                this._highlightActive();
+            }
         }, this);
 
-
+        // Listens to when sweet spot is opened to bind keydown event
         this.layout.on('show', function() {
             this.results = this.actions = this.records = this.keywords = [];
             $(window).on('keydown.' + this.cid, _.bind(this.keydownHandler, this));
             this.render();
         }, this);
 
+        // Listens to when sweet spot is opened to unbind keydown event
         this.layout.on('hide', function() {
             $(window).off('keydown.' + this.cid);
         }, this);
+    },
+
+    /**
+     * Renders a specific section.
+     *
+     * - Only if the list of records for that section has changed.
+     * - Shows the section if there is at least a record
+     * - Hides the section if there are no records.
+     *
+     * @param {string} section The section name (can be `actions`, `keywords`
+     *   or `records`).
+     * @param {Array} results The list of results for that section
+     */
+    renderSection: function(section, results) {
+        var allowed = ['actions', 'keywords', 'records'];
+        if (!_.contains(allowed, section)) {
+            return;
+        }
+        if (_.isEqual(this[section], results)) {
+            return;
+        }
+        var $section = this.$('[data-section="' + section + '"]');
+        $section.find('ul').empty();
+        this[section] = results;
+        if (results.length === 0) {
+            $section.addClass('hide');
+            $section.find('ul').empty();
+        } else {
+            $section.removeClass('hide');
+            _.each(results, function(result) {
+                $section.find('ul').append(this._resultPartial(result));
+            }, this);
+        }
     },
 
     /**
@@ -83,7 +179,8 @@
 
     /**
      * Formats the {@link #results} to:
-     * -include labels if none are present by default.
+     *
+     * - include labels if none are present by default.
      *
      * @param {Array} results The list of actions/commands.
      * @return {Array} The formatted list of actions/commands.
