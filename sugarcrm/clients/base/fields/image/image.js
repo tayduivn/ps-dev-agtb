b18b0daa65e4421c4e00b12fee667f3b867a9eed
Yuk Lai Suen <yuklai@hothouselabs.com>
2015-04-13 23:35:33 -0700

    [SF-41] Remove data change binding for image field when view is merge-duplicates

--- sugarcrm/clients/base/fields/image/image.js
+++ sugarcrm/clients/base/fields/image/image.js
@@ -131,7 +131,9 @@
         //Keep empty for edit because you cannot set a value of an input type `file`
         var viewType = this.view.name || this.options.viewName;
         var ignoreViewType = ["edit", "create"];
-        if (_.indexOf(ignoreViewType, viewType) < 0 && this.view.action !== "edit") {
+        if ((_.indexOf(ignoreViewType, viewType) < 0)
+            && (this.view.action !== "edit")
+            && (this.view.name !== 'merge-duplicates')) {
             app.view.Field.prototype.bindDataChange.call(this);
         }
     },
