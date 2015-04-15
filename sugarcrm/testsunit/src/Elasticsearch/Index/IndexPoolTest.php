13dc9ad61a4fd3def3a374620d9232eebf0feffe
csezheng <ezheng@sugarcrm.com>
2015-04-14 17:44:32 -0700

    br2362: add unit tests for major Elastic components

---
+++ sugarcrm/testsunit/src/Elasticsearch/Index/IndexPoolTest.php
@@ -0,0 +1,149 @@
+<?php
+/*
+ * Your installation or use of this SugarCRM file is subject to the applicable
+ * terms available at
+ * http://support.sugarcrm.com/06_Customer_Center/10_Master_Subscription_Agreements/.
+ * If you do not agree to all of the applicable terms or do not have the
+ * authority to bind the entity as an authorized representative, then do not
+ * install or use this SugarCRM file.
+ *
+ * Copyright (C) SugarCRM Inc. All rights reserved.
+ */
+
+namespace Sugarcrm\SugarcrmTestsUnit\Elasticsearch\Index;
+
+use Sugarcrm\SugarcrmTestsUnit\TestReflection;
+use Sugarcrm\Sugarcrm\Elasticsearch\Index\IndexPool;
+
+/**
+ *
+ * @coversDefaultClass \Sugarcrm\Sugarcrm\Elasticsearch\Index\IndexPool
+ *
+ */
+class IndexPoolTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @covers ::registerStrategies
+     *  @covers ::addStrategy
+     */
+    public function testRegisterStrategies()
+    {
+        $indexPool = $this->getIndexPoolMock();
+        TestReflection::callProtectedMethod($indexPool, 'registerStrategies');
+
+        $strategies = TestReflection::getProtectedValue($indexPool, 'strategies');
+        $this->assertEquals($strategies['static'], '\Sugarcrm\Sugarcrm\Elasticsearch\Index\Strategy\StaticStrategy');
+        $this->assertEquals($strategies['archive'], '\Sugarcrm\Sugarcrm\Elasticsearch\Index\Strategy\ArchiveStrategy');
+    }
+
+    /**
+     * @covers ::normalizeIndexName
+     * @dataProvider providerTestNormalizeIndexName
+     */
+    public function testNormalizeIndexName($prefix, $name, $output)
+    {
+        $indexPool = $this->getIndexPoolMock();
+        TestReflection::setProtectedValue($indexPool, 'prefix', $prefix);
+        $res = $indexPool->normalizeIndexName($name);
+        $this->assertEquals($res, $output);
+    }
+
+    public function providerTestNormalizeIndexName()
+    {
+        return array(
+            array(
+                'foo',
+                'bar',
+                'foo_bar'
+            ),
+            array(
+                '',
+                'bar',
+                'bar'
+            ),
+            array(
+                '',
+                'Bar',
+                'bar'
+            ),
+            array(
+                'Foo',
+                'Bar',
+                'foo_bar'
+            ),
+        );
+    }
+
+    /**
+     * @covers ::getStrategy
+     * @dataProvider providerTestGetStrategy
+     */
+    public function testTestGetStrategy($module, $config, $loadedId, $strategies, $output)
+    {
+        $indexPool = $this->getIndexPoolMock();
+        TestReflection::setProtectedValue($indexPool, 'config', $config);
+
+        $staticStrategy = $this->getStaticStrategyMock();
+        $staticStrategy->setIdentifier($loadedId);
+
+        $loaded = array($loadedId => $staticStrategy);
+        TestReflection::setProtectedValue($indexPool, 'loaded', $loaded);
+
+        TestReflection::setProtectedValue($indexPool, 'strategies', $strategies);
+        $class = $indexPool->getStrategy($module);
+        $this->assertEquals($class->getIdentifier(), $output);
+    }
+
+    public function providerTestGetStrategy()
+    {
+        return array(
+            array(
+                'foo',
+                array(
+                    'foo' => array(
+                        'strategy' => 'archive',
+                    )
+                ),
+                'archive',
+                array(),
+                'archive'
+            ),
+            array(
+                'bar',
+                array(),
+                '',
+                array(
+                    IndexPool::DEFAULT_STRATEGY => '\Sugarcrm\Sugarcrm\Elasticsearch\Index\Strategy\StaticStrategy'
+                ),
+                IndexPool::DEFAULT_STRATEGY,
+            ),
+        );
+    }
+
+    /**
+     * Get IndexPoolTest Mock
+     * @param array $methods
+     * @return \Sugarcrm\Sugarcrm\Elasticsearch\Index\IndexPool
+     */
+    protected function getIndexPoolMock(array $methods = null)
+    {
+        return $this->getMockBuilder('Sugarcrm\Sugarcrm\Elasticsearch\Index\IndexPool')
+            ->disableOriginalConstructor()
+            ->setMethods($methods)
+            ->getMock();
+    }
+
+    /**
+     * Get StaticStrategy Mock
+     * @param array $methods
+     * @return \Sugarcrm\Sugarcrm\Elasticsearch\Index\Strategy\StaticStrategy
+     */
+    protected function getStaticStrategyMock(array $methods = null)
+    {
+        return $this->getMockBuilder('Sugarcrm\Sugarcrm\Elasticsearch\Index\Strategy\StaticStrategy')
+            ->disableOriginalConstructor()
+            ->setMethods($methods)
+            ->getMock();
+    }
+
+}
---
+++ sugarcrm/testsunit/src/Elasticsearch/Mapping/MappingManagerTest.php
@@ -0,0 +1,98 @@
+<?php
+/*
+ * Your installation or use of this SugarCRM file is subject to the applicable
+ * terms available at
+ * http://support.sugarcrm.com/06_Customer_Center/10_Master_Subscription_Agreements/.
+ * If you do not agree to all of the applicable terms or do not have the
+ * authority to bind the entity as an authorized representative, then do not
+ * install or use this SugarCRM file.
+ *
+ * Copyright (C) SugarCRM Inc. All rights reserved.
+ */
+
+namespace Sugarcrm\SugarcrmTestsUnit\Elasticsearch\Mapping;
+
+use Sugarcrm\Sugarcrm\Elasticsearch\Provider\ProviderCollection;
+
+/**
+ *
+ * @coversDefaultClass \Sugarcrm\Sugarcrm\Elasticsearch\Mapping\MappingManager
+ *
+ */
+class MappingManagerTest extends \PHPUnit_Framework_TestCase
+{
+
+    /**
+     * @covers ::buildMapping
+     * @dataProvider providerTestBuildMapping
+     */
+    public function testBuildMapping($modules, $mapping1, $mapping2)
+    {
+        $numCalls = count($modules);
+        $provider1 = $this->getProviderMock(array('buildMapping'));
+        $provider1->expects($this->exactly($numCalls))
+            ->method('buildMapping')
+            ->will($this->returnValue($mapping1));
+
+        $provider2 = $this->getProviderMock(array('buildMapping'));
+        $provider2->expects($this->exactly($numCalls))
+            ->method('buildMapping')
+            ->will($this->returnValue($mapping2));
+
+        $providers = new ProviderCollection($this->getContainerMock(), array($provider1, $provider2));
+
+        $mappingManager = $this->getMappingManagerMock();
+        $mappingManager->buildMapping($providers, $modules);
+
+    }
+
+    public function providerTestBuildMapping()
+    {
+        return array(
+            array(
+                array('Accounts', 'Contacts', 'Leads'),
+                array('mapping1' => array('type' => 'string')),
+                array('mapping2' => array('type' => 'text')),
+            ),
+        );
+    }
+
+    /**
+     * Get Provider Mock
+     * @param array $methods
+     * @return \Sugarcrm\Sugarcrm\Elasticsearch\Provider\GlobalSearch\GlobalSearch
+     */
+    protected function getProviderMock(array $methods = null)
+    {
+        return $this->getMockBuilder('Sugarcrm\Sugarcrm\Elasticsearch\Provider\GlobalSearch\GlobalSearch')
+            ->disableOriginalConstructor()
+            ->setMethods($methods)
+            ->getMock();
+    }
+
+    /**
+     * Get MappingManagerTest Mock
+     * @param array $methods
+     * @return \Sugarcrm\Sugarcrm\Elasticsearch\Mapping\MappingManager
+     */
+    protected function getMappingManagerMock(array $methods = null)
+    {
+        return $this->getMockBuilder('Sugarcrm\Sugarcrm\Elasticsearch\Mapping\MappingManager')
+            ->disableOriginalConstructor()
+            ->setMethods($methods)
+            ->getMock();
+    }
+
+    /**
+     * Get Container Mock
+     * @param array $methods
+     * @return \Sugarcrm\Sugarcrm\Elasticsearch\Container
+     */
+    protected function getContainerMock(array $methods = null)
+    {
+        return $this->getMockBuilder('Sugarcrm\Sugarcrm\Elasticsearch\Container')
+            ->disableOriginalConstructor()
+            ->setMethods($methods)
+            ->getMock();
+    }
+}
