03a02708d8b119c359131df04d6ae20cdd652906
Vishal Jain <vjain@sugarcrm.com>
2015-04-24 13:23:44 -0700

    INT-1062: Jasmine Tests for linkable tags

---
+++ sugarcrm/tests/clients/base/layouts/search/search.js
@@ -0,0 +1,56 @@
+describe('View.Layouts.Base.SearchLayout', function() {
+    var app, layout;
+
+    beforeEach(function() {
+        app = SugarTest.app;
+        SugarTest.testMetadata.init();
+        SugarTest.testMetadata.set();
+        layout = SugarTest.createLayout('base', null, 'search');
+        layout.initialize(layout.options);
+        layout.initComponents();
+    });
+
+    afterEach(function() {
+        SugarTest.testMetadata.dispose();
+        layout.context.clear();
+        layout.dispose();
+        layout = null;
+    });
+
+    describe('tags filters', function() {
+        var sampleTag, stub;
+
+        beforeEach(function(){
+            sampleTag = {name: 'tag1', id: '1234'};
+            layout.context.set('tags', [sampleTag]);
+            stub = sinon.stub(layout, '_super');
+        });
+
+        afterEach(function() {
+            stub.restore();
+        });
+
+        it('sets tag_filters in options on load', function() {
+            var options = {};
+            var setFields = false;
+            layout.loadData(options, setFields);
+            expect(options.apiOptions).toEqual({data:{tag_filters: [sampleTag.id]}, fetchWithPost: true});
+        });
+
+        it('adds tag_filters to search collection', function() {
+            layout.search();
+            expect(layout.collection.options.apiOptions.data.tag_filters).toEqual([sampleTag.id]);
+        });
+
+        it('adds tag_filters to filter collection', function() {
+            layout.filter();
+            expect(layout.collection.options.apiOptions.data.tag_filters).toEqual([sampleTag.id]);
+        });
+
+        it('updates the collection when tags change', function() {
+            var newTag = {name: 'tag2', id: '12345'};
+            layout.context.set('tags', [newTag]);
+            expect(layout.collection.options.apiOptions.data.tag_filters).toEqual([newTag.id]);
+        });
+    });
+});
