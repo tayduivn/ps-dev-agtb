0baa0abdf54295aca954019fbd2907ec93306822
Alexander Rymarchik <a.rymarchik@intetics.com>
2015-04-30 13:28:05 +0300

    RS-795: Values are not showing in Frequency & Author column of Search and Select Knowledge Base drawer.

--- sugarcrm/modules/KBDocuments/KBDocumentsApiHelper.php
+++ sugarcrm/modules/KBDocuments/KBDocumentsApiHelper.php
@@ -21,10 +21,36 @@ class KBDocumentsApiHelper extends SugarBeanApiHelper
 
     public function formatForApi(SugarBean $bean, array $fieldList = array(), array $options = array() )
     {
+        $db = DBManagerFactory::getInstance();
+        $query = "SELECT
+            jt0.id assigned_user_id,
+            jt0.user_name assigned_user_name,
+            jt0.first_name assgn_fn,
+            jt0.last_name assgn_ln,
+            jt1.id kbdoc_approver_id,
+            jt1.user_name kbdoc_approver_name,
+            jt1.first_name appr_fn,
+            jt1.last_name appr_ln,
+            kvr.views_number views_number
+        FROM
+            kbdocuments LEFT JOIN kbdocuments_views_ratings kvr ON kbdocuments.id = kvr.kbdocument_id
+            LEFT JOIN  users jt0 ON jt0.id = kbdocuments.assigned_user_id AND jt0.deleted = 0
+            LEFT JOIN  users jt1 ON jt1.id = kbdocuments.kbdoc_approver_id AND jt1.deleted = 0
+        WHERE
+            kbdocuments.id = {$db->quoted($bean->id)}";
+        $res = $db->query($query);
+        $addon = $db->fetchRow($res);
+
+        if (in_array('views_number', $fieldList) && !empty($addon['views_number'])) {
+            $bean->views_number = $addon['views_number'];
+        }
 
         // bug 56834 - the api doesn't return kbdoc_approver_name
         $isKbApprover = in_array('kbdoc_approver_name', $fieldList);
 
+        if ($isKbApprover && !empty($addon['kbdoc_approver_id'])) {
+            $bean->kbdoc_approver_id = $addon['kbdoc_approver_id'];
+        }
         //add kbdoc_approver_id if not in fieldList
         if ( $isKbApprover && !in_array('kbdoc_approver_id', $fieldList) ) {
             $fieldList[] = 'kbdoc_approver_id';
@@ -33,14 +59,30 @@ class KBDocumentsApiHelper extends SugarBeanApiHelper
 
         // bug 56834 - manually fill kbdoc_approver_name if in fieldList
         if ( (empty($fieldList) || $isKbApprover) && isset($data['kbdoc_approver_id']) ) {
-            $user = new User();
-            $user->retrieve($data['kbdoc_approver_id'],true);
-            $data['kbdoc_approver_name'] = $user->name;
+            $user = BeanFactory::getBean('Users');
+            $user->populateFromRow(
+                array(
+                    'id' => $addon['kbdoc_approver_id'],
+                    'first_name' => $addon['appr_fn'],
+                    'last_name' => $addon['appr_ln'],
+                )
+            );
+            $data['kbdoc_approver_name'] =  Localization::getObject()->formatName($user);
         }
 
-        if ( empty($fieldList) || in_array('attachment_list',$fieldList) ) {
-            $db = DBManagerFactory::getInstance();
+        if (in_array('assigned_user_name', $fieldList) && !empty($addon['assigned_user_id'])) {
+            $user = BeanFactory::getBean('Users');
+            $user->populateFromRow(
+                array(
+                    'id' => $addon['assigned_user_id'],
+                    'first_name' => $addon['assgn_fn'],
+                    'last_name' => $addon['assgn_ln'],
+                )
+            );
+            $data['assigned_user_name'] =  Localization::getObject()->formatName($user);
+        }
 
+        if (empty($fieldList) || in_array('attachment_list', $fieldList)) {
             $query = "SELECT rev.id rev_id, rev.filename filename, kbrev.id docrev_id FROM kbdocument_revisions kbrev LEFT JOIN document_revisions rev ON (kbrev.document_revision_id = rev.id) WHERE kbrev.kbdocument_id = '".$bean->id."' AND kbrev.deleted = 0 AND rev.deleted = 0 AND kbrev.kbcontent_id is NULL";
             $ret = $db->query($query,true);
             $files = array();
