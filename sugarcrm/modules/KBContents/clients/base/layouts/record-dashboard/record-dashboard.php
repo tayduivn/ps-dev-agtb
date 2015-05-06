e18bbe7902706de9619d564e86f870ad6c41d393
Alexander Rymarchik <a.rymarchik@intetics.com>
2015-05-06 11:01:50 +0300

    RS-767: KB Catagory dashlet is not in record view by default.

--- sugarcrm/modules/KBContents/clients/base/layouts/record-dashboard/record-dashboard.php
+++ sugarcrm/modules/KBContents/clients/base/layouts/record-dashboard/record-dashboard.php
@@ -31,6 +31,47 @@ $viewdefs['KBContents']['base']['layout']['record-dashboard'] = array(
                                         'width' => 12,
                                     ),
                                 ),
+                                array(
+                                    array(
+                                        'view' =>
+                                            array(
+                                                'type' => 'related-documents',
+                                                'label' => 'LBL_DASHLET_RELATED_DOCUMENTS',
+                                            ),
+                                        'context' =>
+                                            array(
+                                                'module' => 'KBContents',
+                                                'filter' => array(
+                                                    'module' => array(
+                                                        'KBContents',
+                                                    ),
+                                                    'view' => 'record',
+                                                ),
+                                                'limit' => 5,
+                                            ),
+                                        'width' => 12,
+                                    ),
+                                ),
+                                array(
+                                    array(
+                                        'view' =>
+                                            array(
+                                                'type' => 'kbs-dashlet-localizations',
+                                                'label' => 'LBL_DASHLET_LOCALIZATIONS_NAME',
+                                            ),
+                                        'context' =>
+                                            array(
+                                                'module' => 'KBContents',
+                                                'filter' => array(
+                                                    'module' => array(
+                                                        'KBContents'
+                                                    ),
+                                                    'view' => 'record',
+                                                ),
+                                            ),
+                                        'width' => 12,
+                                    ),
+                                ),
                             ),
                         'width' => 12,
                     ),
