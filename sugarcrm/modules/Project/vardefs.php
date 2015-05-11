69403b5b2c751850647aa8a1ea845a54bf960e09
Ivan Tito <ivan.tito@colosa.com>
2015-05-11 11:40:56 -0400

    MACAROON-585:Set Date_created, date_modified fields to readonly in respective vardefs

--- sugarcrm/modules/Project/vardefs.php
+++ sugarcrm/modules/Project/vardefs.php
@@ -38,6 +38,7 @@ $dictionary['Project'] = array(
 			'comment' => 'Date record created',
 		    'enable_range_search' => true,
 		    'options' => 'date_range_search_dom',
+            'readonly' => true,
 		),
 		'date_modified' => array(
 			'name' => 'date_modified',
@@ -46,6 +47,7 @@ $dictionary['Project'] = array(
 			'comment' => 'Date record last modified',
 		    'enable_range_search' => true,
 		    'options' => 'date_range_search_dom',
+            'readonly' => true,
 		),
 		'assigned_user_id' => array(
 			'name' => 'assigned_user_id',
