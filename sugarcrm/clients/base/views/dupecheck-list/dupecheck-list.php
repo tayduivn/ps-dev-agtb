f6baf11cdf6f6f8a46af916cb1617b63973fcff3
Travis Hubbard <thubbard@sugarcrm.com>
2015-06-09 11:28:24 -0400

    SFA-3630 fixed dupecheck-list after list row partial

--- sugarcrm/clients/base/views/dupecheck-list/dupecheck-list.php
+++ sugarcrm/clients/base/views/dupecheck-list/dupecheck-list.php
@@ -11,7 +11,7 @@ if(!defined('sugarEntry') || !sugarEntry) die('Not A Valid Entry Point');
  * Copyright (C) SugarCRM Inc. All rights reserved.
  */
 $viewdefs['base']['view']['dupecheck-list'] = array(
-    'template' => 'list',
+    'template' => 'flex-list',
     'rowactions' => array(
         'actions' => array(
             array(
