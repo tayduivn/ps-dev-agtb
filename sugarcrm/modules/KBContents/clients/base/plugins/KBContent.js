00713b4966190478c3a73b3ebdcd2d4af00b3112
GulDmitry <guldmitry@gmail.com>
2015-06-10 17:29:20 +0300

    RS-853. Lack of auto-reload after create a Localization or Revision from KB listview.

--- sugarcrm/modules/KBContents/clients/base/plugins/KBContent.js
+++ sugarcrm/modules/KBContents/clients/base/plugins/KBContent.js
@@ -210,7 +210,7 @@
              */
             _openCreateRelatedDrawer: function(prefill, parentModel) {
                 var layoutDef = {
-                    layout: 'create-actions',
+                    layout: 'create',
                     context: {
                         create: true,
                         model: prefill,
@@ -225,9 +225,11 @@
                 } else {
                     app.drawer.open(layoutDef, function(context, newModel) {
                         // Just parent - header's create, parent.parent - subpanel's create.
-                        var recordViewContext = context.parent.parent || context.parent;
-                        parentModel.fetch();
-                        recordViewContext.trigger('subpanel:reload', {links: ['revisions', 'localizations']});
+                        var viewContext = context.parent.parent || context.parent;
+                        viewContext.resetLoadFlag();
+                        viewContext.set('skipFetch', false);
+                        viewContext.loadData();
+                        viewContext.trigger('subpanel:reload', {links: ['revisions', 'localizations']});
                         context.createAction = null;
                         context.loadDrawer = null;
                     });
