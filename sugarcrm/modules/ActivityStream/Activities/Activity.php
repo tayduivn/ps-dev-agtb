a5bcf4946d46b22492aa4b08c236128d1ac1cbd4
Hai Qi <hqi@sugarcrm.com>
2015-07-01 15:32:54 -0700

    PAT-2061:Fatal Relationship Errors overloading log

--- sugarcrm/modules/ActivityStream/Activities/Activity.php
+++ sugarcrm/modules/ActivityStream/Activities/Activity.php
@@ -292,7 +292,7 @@ class Activity extends Basic
      */
     public function processRecord(SugarBean $bean)
     {
-        if ($bean->load_relationship('activities')) {
+        if (ActivityQueueManager::isEnabledForModule($bean->module_name) && $bean->load_relationship('activities')) {
             $bean->activities->add($this);
         }
     }
