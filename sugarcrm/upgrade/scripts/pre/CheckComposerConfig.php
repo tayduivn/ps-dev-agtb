ee4b0ceaa3166701b506c0fab1af2676a5833f16
sparsuram <sparsuram@sugarcrm.com>
2015-07-07 17:02:18 -0700

    Update 7.6.0.0 md5sum and add 7.6.1.0 md5sum to fix 7610_Upgrade_Tests_Stack122 failures

--- sugarcrm/upgrade/scripts/pre/CheckComposerConfig.php
+++ sugarcrm/upgrade/scripts/pre/CheckComposerConfig.php
@@ -91,7 +91,8 @@ class SugarUpgradeCheckComposerConfig extends UpgradeScript
         '7.5.2.0' => '72d3be63d2481cdb3710f86f8a36b851',
         '7.5.2.1' => '72d3be63d2481cdb3710f86f8a36b851',
         '7.5.2.2' => '72d3be63d2481cdb3710f86f8a36b851',
-        '7.6.0.0' => 'e3478997283138a09cbc3d29bf35e520',
+        '7.6.0.0' => 'fafc925d2c41624ab200143c523659a8',
+        '7.6.1.0' => 'c238040b8b749dae658ba1e5ab51c8fc',
     );
 
     /**
