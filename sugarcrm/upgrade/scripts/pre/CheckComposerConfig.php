tayduivn/ps-dev-agtb

commit c714381653715310448f6f79d05ebf8b5bb16cb9

Update 7.6.1.0 md5sum to fix 7610_Upgrade_Tests_Stack122 failures

sugarcrm/upgrade/scripts/pre/CheckComposerConfig.php
@@ -93,7 +93,7 @@ class SugarUpgradeCheckComposerConfig extends UpgradeScript
         '7.5.2.2' => '72d3be63d2481cdb3710f86f8a36b851',
         '7.5.2.3' => '72d3be63d2481cdb3710f86f8a36b851',
         '7.6.0.0' => 'fafc925d2c41624ab200143c523659a8',
-        '7.6.1.0' => 'c238040b8b749dae658ba1e5ab51c8fc',
+        '7.6.1.0' => 'af617767509def89c0e1dea33c176a0c,
     );
 
     /**

commit 3348c79bae2c06ee116ac224c33daabe96f15117

Fix syntax error caused by c7143816

sugarcrm/upgrade/scripts/pre/CheckComposerConfig.php
@@ -93,7 +93,7 @@ class SugarUpgradeCheckComposerConfig extends UpgradeScript
         '7.5.2.2' => '72d3be63d2481cdb3710f86f8a36b851',
         '7.5.2.3' => '72d3be63d2481cdb3710f86f8a36b851',
         '7.6.0.0' => 'fafc925d2c41624ab200143c523659a8',
-        '7.6.1.0' => 'af617767509def89c0e1dea33c176a0c,
+        '7.6.1.0' => 'af617767509def89c0e1dea33c176a0c',
     );
 
     /**