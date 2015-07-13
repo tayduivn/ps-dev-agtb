3e1c7a9209cd224fafaa557853ad103753d0bcea
Gabriel Rael <grael@sugarcrm.com>
2015-07-13 11:01:59 -0400

    Revert "Merge pull request #26006 from alias-mac/SC-4649"
    This reverts commit 86a4ae536b9b2d3d6fdf22587050559e8bf54389, reversing
    changes made to 825793be5647d4e9b419c98ba35207bc679268c7.

--- sugarcrm/clients/base/api/CurrentUserApi.php
+++ sugarcrm/clients/base/api/CurrentUserApi.php
@@ -162,15 +162,8 @@ class CurrentUserApi extends SugarApi
     {
         $current_user = $this->getUserBean();
 
-        // FIXME temporary fix for SC-4596
-        $mHashValid = true;
-        if (isset($api->request_headers[$api::HEADER_META_HASH])) {
-            $mM = MetaDataManager::getManager(array($api->platform));
-            $mHashValid = $mM->isMetadataHashValid($api->request_headers[$api::HEADER_META_HASH]);
-        }
-
         $hash = $current_user->getUserMDHash();
-        if ($mHashValid && $api->generateETagHeader($hash)) {
+        if ($hash && $api->generateETagHeader($hash)) {
             return;
         }
 
