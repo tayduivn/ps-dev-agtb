63002d3de1065b0ade176ac330d86c68e5e2fed1
Vladimir Skrobot <vskrobot@sugarcrm.com>
2015-07-17 11:33:54 -0700

    BR-3121: [strudel] XSRF error on Remove XSS Vulnerabilities

--- sugarcrm/modules/Administration/RepairTeams.php
+++ sugarcrm/modules/Administration/RepairTeams.php
@@ -14,6 +14,7 @@ if(!defined('sugarEntry') || !sugarEntry) die('Not A Valid Entry Point');
 //FILE SUGARCRM flav=pro ONLY
 
 
+require_once 'include/SugarSmarty/plugins/function.sugar_csrf_form_token.php';
 
 
 
@@ -189,8 +190,11 @@ ABC;
         $process_private_team_checked="checked";
     }
 
+    $csrfToken = smarty_function_sugar_csrf_form_token(array(), $smarty);
+
     $xyz=<<<EOF
         <form name="RepairTeams" method="POST" >
+            {$csrfToken}
             <input type="hidden" name="module" value="Administration">
             <input type="hidden" name="action"  value="RepairTeams">
             <input type="hidden" name="process"  value="1">
