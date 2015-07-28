16897f0627dca2075211c7d08d113185e62f3a1c
afedyk-sugarcrm <afedyk@sugarcrm.com>
2015-07-29 00:25:15 +0300

    CRYS-984 Started implementation of CarriersConfigApi

---
+++ sugarcrm/modules/NotificationCenter/clients/base/api/CarriersConfigApi.php
@@ -0,0 +1,93 @@
+<?php
+if(!defined('sugarEntry') || !sugarEntry) die('Not A Valid Entry Point');
+ /*
+ * Your installation or use of this SugarCRM file is subject to the applicable
+ * terms available at
+ * http://support.sugarcrm.com/06_Customer_Center/10_Master_Subscription_Agreements/.
+ * If you do not agree to all of the applicable terms or do not have the
+ * authority to bind the entity as an authorized representative, then do not
+ * install or use this SugarCRM file.
+ *
+ * Copyright (C) SugarCRM Inc. All rights reserved.
+ */
+
+use Sugarcrm\Sugarcrm\Notification\Config\Status;
+use Sugarcrm\Sugarcrm\Notification\CarrierRegistry;
+
+require_once('include/api/SugarApi.php');
+
+class CarriersConfigApi extends SugarApi
+{
+
+    public function registerApiRest()
+    {
+        return array(
+            'getConfig' => array(
+                'reqType' => 'GET',
+                'path' => array('NotificationCenter', 'config', '?'),
+                'pathVars' => array('', '', 'module'),
+                'method' => 'getConfig',
+                'shortHelp' => 'Get configuration for specific carrier',
+                'longHelp' => '',
+                'acl' => 'adminOrDev',
+            ),
+
+            'handleSave' => array(
+                'reqType' => 'PUT',
+                'path' => array('NotificationCenter', 'config', '?'),
+                'pathVars' => array('', '', 'module'),
+                'method' => 'handleSave',
+                'shortHelp' => 'Handle save configuration for specific carrier',
+                'longHelp' => '',
+                'acl' => 'adminOrDev',
+            ),
+        );
+    }
+
+    /**
+     * Function return carrier status
+     *
+     * @param ServiceBase $api
+     * @param array $args
+     * @return bool
+     */
+    public function getConfig(ServiceBase $api, array $args)
+    {
+        $this->requireArgs($args, array('module'));
+        $this->verifyModule($args['module']);
+
+        $status = Status::getInstance();
+        return $status->getCarrierStatus($args['module']);
+    }
+
+    /**
+     * @param ServiceBase $api
+     * @param array $args
+     * @return array
+     * @throws SugarApiExceptionNotFound
+     */
+    public function handleSave(ServiceBase $api, array $args)
+    {
+        $this->requireArgs($args, array('module', 'status'));
+        $this->verifyModule($args['module']);
+
+        $status = Status::getInstance();
+        $status->setCarrierStatus($args['module'], $args['status']);
+        return $status->getCarrierStatus($args['module']);
+    }
+
+    /**
+     * Verifies existing carrier module
+     *
+     * @param string $module
+     * @throws SugarApiExceptionNotFound
+     */
+    protected function verifyModule($module)
+    {
+        $registry = CarrierRegistry::getInstance();
+        if (!in_array($module, $registry->getCarriers())){
+            throw new SugarApiExceptionNotFound('Not found carrier module: '.$module);
+        }
+    }
+
+}
