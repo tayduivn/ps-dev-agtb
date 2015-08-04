82e5b263e280403eabca4771af7176c08ab99a49
Sergey Morozov <smorozov@sugarcrm.com>
2015-08-04 18:32:33 +0300

    [BR-2962] Contacts subpanel does not populate when Opportunity Role column added

---
+++ sugarcrm/tests/modules/ModuleBuilder/parsers/views/SidecarSubpanelLayoutMetaDataParserTest.php
@@ -0,0 +1,60 @@
+<?php
+/*
+ * Your installation or use of this SugarCRM file is subject to the applicable
+ * terms available at
+ * http://support.sugarcrm.com/06_Customer_Center/10_Master_Subscription_Agreements/.
+ * If you do not agree to all of the applicable terms or do not have the
+ * authority to bind the entity as an authorized representative, then do not
+ * install or use this SugarCRM file.
+ *
+ * Copyright (C) SugarCRM Inc. All rights reserved.
+ */
+
+require_once 'modules/ModuleBuilder/parsers/views/SidecarSubpanelLayoutMetaDataParser.php';
+
+/**
+ * @covers SidecarSubpanelLayoutMetaDataParser
+ */
+class SidecarSubpanelLayoutMetaDataParserTest extends Sugar_PHPUnit_Framework_TestCase
+{
+    /**
+     * @dataProvider relationshipFieldsProvider
+     */
+    public function testRelationshipFields($moduleName, $subPanelName, $field, $expected)
+    {
+        $parser = new SidecarSubpanelLayoutMetaDataParser($subPanelName, $moduleName);
+        $this->assertArrayHasKey($field, $parser->_fielddefs);
+        $isValid = $parser->isValidField($field, $parser->_fielddefs[$field]);
+        $this->assertEquals($expected, $isValid);
+    }
+
+    public static function relationshipFieldsProvider()
+    {
+        return array(
+            array(
+                'Opportunities',
+                'contacts',
+                'opportunity_role',
+                true,
+            ),
+            array(
+                'Accounts',
+                'contacts',
+                'opportunity_role',
+                false,
+            ),
+            array(
+                'Contacts',
+                'opportunities',
+                'contact_role',
+                true,
+            ),
+            array(
+                'Accounts',
+                'opportunities',
+                'contact_role',
+                false,
+            ),
+        );
+    }
+}
