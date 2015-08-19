a1bc5063813157d707d3f7e8562917996974ccad
muthukumar <muthukumarts@cognizant.com>
2015-08-19 15:32:00 +0530

    #DNB-110 - BAL – Industry drawer - SIC is the default selection

--- sugarcrm/clients/base/views/dnb-bal-params/dnb-bal-params.php
+++ sugarcrm/clients/base/views/dnb-bal-params/dnb-bal-params.php
@@ -329,7 +329,8 @@ $viewdefs['base']['view']['dnb-bal-params'] = array(
                             'cell_css_class' => 'span2',
                             'placeholder' => "LBL_DNB_BAL_IND_CODE_TYPE",
                             'searchBarThreshold' => -1,
-                            'options' => 'dnb_sic_naics_code'
+                            'options' => 'dnb_sic_naics_code',
+                            'default' => '3599',
                         ),
                         array(
                             'name' => 'dnb_bal_ind_filter',
