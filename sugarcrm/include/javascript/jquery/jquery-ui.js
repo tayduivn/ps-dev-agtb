b09cb8f7937948c1428f8378885f9e70ecb59467
Sergey Morozov <smorozov@sugarcrm.com>
2015-08-21 15:08:37 +0300

    [BR-3239] Save Button is not functional while saving the formula for the calculated field.

--- sugarcrm/include/javascript/jquery/jquery-ui.js
+++ sugarcrm/include/javascript/jquery/jquery-ui.js
@@ -994,6 +994,12 @@ if ( !$.offset.setOffset ) {
 	};
 }
 
+// @link https://github.com/jquery/jquery-ui/commit/04df2b9
+// jQuery <1.4.3 uses curCSS, in 1.4.3 - 1.7.2 curCSS = css, 1.8+ only has css
+if ( !$.curCSS ) {
+	$.curCSS = $.css;
+}
+
 }( jQuery ));
 /*
  * jQuery UI Draggable 1.8.16
