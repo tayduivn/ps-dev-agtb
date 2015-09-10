cdbc403a0eaee610773608cda2c887ed3d858174
sparsuram <sparsuram@sugarcrm.com>
2015-09-10 15:31:00 -0700

    BR-3340: Upgrade failed from 6.5.22Ent/Ult to 7.7.0.0beta4 "Error:Cant upgrade version 6.x on non-Mysql database"

--- sugarcrm/modules/UpgradeWizard/UpgradeDriver.php
+++ sugarcrm/modules/UpgradeWizard/UpgradeDriver.php
@@ -940,9 +940,11 @@ abstract class UpgradeDriver
         // validate manifest
         list($this->from_version, $this->from_flavor) = $this->getFromVersion();
         $db = DBManagerFactory::getInstance();
-        if (version_compare($this->from_version, 7, '<') && !$db instanceof MysqlManager) {
-            return $this->error("Can't upgrade version 6.x on non-Mysql database", true);
-        }
+        
+        // if (version_compare($this->from_version, 7, '<') && !$db instanceof MysqlManager) {
+        //    return $this->error("Can't upgrade version 6.x on non-Mysql database", true);
+        // }
+        
         $res = $this->validateManifest();
         if ($res !== true) {
             if ($this->clean_on_fail) {
