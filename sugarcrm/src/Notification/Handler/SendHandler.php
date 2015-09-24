901250889f628f68d4eea8606f0b7beefc537581
afedyk-sugarcrm <afedyk@sugarcrm.com>
2015-09-24 18:11:48 +0300

    CRYS-1097 moved SendHandler.php -> Handler/SendHandler.php

--- sugarcrm/src/Notification/Handler/SendHandler.php
+++ sugarcrm/src/Notification/Handler/SendHandler.php
@@ -12,16 +12,15 @@
 
 namespace Sugarcrm\Sugarcrm\Notification\Handler;
 
+use Sugarcrm\Sugarcrm\Notification\Carrier\CarrierInterface;
 use Sugarcrm\Sugarcrm\JobQueue\Handler\RunnableInterface;
 use Sugarcrm\Sugarcrm\JobQueue\Handler\SubtaskCapableInterface;
 use Sugarcrm\Sugarcrm\JobQueue\Manager\Manager;
-use Sugarcrm\Sugarcrm\Notification\Carrier\CarrierInterface;
 
 /**
- * Forward Message to Sugar’s transport.
- *
- * Class SendHandler
- * @package Notification
+ * Class SendHandler.
+ * Is used to send messages to a user via various carriers.
+ * @package Sugarcrm\Sugarcrm\Notification
  */
 class SendHandler implements RunnableInterface, SubtaskCapableInterface
 {
@@ -41,25 +40,48 @@ class SendHandler implements RunnableInterface, SubtaskCapableInterface
     protected $client;
 
     /**
-     * Receives carrier, transport value (email, phone number etc.) and message
-     *
-     * @param CarrierInterface $carrier which transport will be used
-     * @param $transportVal transport value (email, phone number etc.)
-     * @param array $message which will be forwarded
+     * Carrier used by the job to send messages.
+     * @var CarrierInterface
      */
-    public function __construct(CarrierInterface $carrier, $transportVal, array $message)
+    protected $carrier;
+
+    /**
+     * Indicates a recipient in various forms. @see AddressTypeInterface.
+     * @var mixed
+     */
+    protected $transportValue;
+
+    /**
+     * Message pack (all required data and metadata) to be sent.
+     * @var array
+     */
+    protected $message = array();
+
+    /**
+     * Create handler with specific carrier, recipients and message.
+     * @param CarrierInterface $carrier Carrier that job uses to send messages.
+     * @param mixed $transportValue recipient data (normally here it is user ID).
+     * @param array $message message pack.
+     */
+    public function __construct(CarrierInterface $carrier, $transportValue, array $message)
     {
-        // TODO: Implement __construct() method.
+        $this->carrier = $carrier;
+        $this->transportValue = $transportValue;
+        $this->message = $message;
     }
 
     /**
-     * Uses transport of carrier to forward message to carrier server for delivery.
-     *
-     * @return string SchedulersJob resolution.
+     * Send message to a specified destination via a specified carrier.
+     * {@inheritdoc}
      */
     public function run()
     {
-        // TODO: Implement run() method.
-        return \SchedulersJob::JOB_SUCCESS;
+        $result = $this->carrier->getTransport()->send($this->transportValue, $this->message);
+
+        if ($result === true) {
+            return \SchedulersJob::JOB_SUCCESS;
+        } else {
+            return \SchedulersJob::JOB_FAILURE;
+        }
     }
 }
