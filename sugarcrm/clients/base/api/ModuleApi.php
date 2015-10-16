06960a90f7d324deb33035e355a6aa08d4f8f483
Boro Sitnikovski <bsitnikovski@sugarcrm.com>
2015-10-16 15:28:02 +0200

    PAT-2303

--- sugarcrm/clients/base/api/ModuleApi.php
+++ sugarcrm/clients/base/api/ModuleApi.php
@@ -379,6 +379,9 @@ class ModuleApi extends SugarApi {
             $filepath = $basepath . $args[$fieldName . '_guid'];
 
             if (!is_file($filepath)) {
+                if (isset($bean->$fieldName)) {
+                    $bean->$fieldName = null;
+                }
                 continue;
             }
 
