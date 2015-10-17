bfa4e5d9ca83389bcaef25d91cb785bc3f2e201c
csezheng <ezheng@sugarcrm.com>
2015-10-16 17:39:48 -0700

    br3432: modify highlighter to provide the option of adding tags in value
    br3432: correct the mis-spelling of highlighter parameter number_of_fragments

---
+++ sugarcrm/testsunit/src/Elasticsearch/Provider/GlobalSearch/HighlighterTest.php
@@ -0,0 +1,72 @@
+<?php
+/*
+ * Your installation or use of this SugarCRM file is subject to the applicable
+ * terms available at
+ * http://support.sugarcrm.com/06_Customer_Center/10_Master_Subscription_Agreements/.
+ * If you do not agree to all of the applicable terms or do not have the
+ * authority to bind the entity as an authorized representative, then do not
+ * install or use this SugarCRM file.
+ *
+ * Copyright (C) SugarCRM Inc. All rights reserved.
+ */
+
+namespace Sugarcrm\SugarcrmTestsUnit\Elasticsearch\Provider\GlobalSearch;
+
+use Sugarcrm\SugarcrmTestsUnit\TestReflection;
+
+/**
+ *
+ * @coversDefaultClass \Sugarcrm\Sugarcrm\Elasticsearch\Provider\GlobalSearch\Highlighter
+ */
+class HighlighterTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @covers ::addTagsInValue
+     * @dataProvider providerTestAddTagsInValue
+     */
+    public function testAddTagsInValue($value, $expected)
+    {
+        $highlighter = $this->getHighlighterMock();
+
+        $res = TestReflection::callProtectedMethod($highlighter, 'wrapValueWithTags', array($value));
+        $this->assertEquals($expected, $res);
+    }
+
+    public function providerTestAddTagsInValue()
+    {
+        return array(
+            array(
+                array('foo'),
+                array('<strong>foo</strong>'),
+            ),
+            array(
+                array("dev.support.beans@example.it"),
+                array("<strong>dev.support.beans@example.it</strong>"),
+            ),
+            array(
+                array("<strong>dev.support.beans@example.it</strong>"),
+                array("<strong>dev.support.beans@example.it</strong>"),
+            ),
+            array(
+                array("dev.support.beans@example.it", "<strong>im.support.qa</strong>@example.edu"),
+                array("<strong>dev.support.beans@example.it</strong>", "<strong>im.support.qa@example.edu</strong>"),
+            ),
+            array(
+                array(),
+                array(),
+            ),
+        );
+    }
+
+
+    /**
+     * @return \Sugarcrm\Sugarcrm\Elasticsearch\Provider\GlobalSearch\Highlighter
+     */
+    protected function getHighlighterMock(array $methods = null)
+    {
+        return $this->getMockBuilder('Sugarcrm\Sugarcrm\Elasticsearch\Provider\GlobalSearch\Highlighter')
+            ->disableOriginalConstructor()
+            ->setMethods($methods)
+            ->getMock();
+    }
+}
