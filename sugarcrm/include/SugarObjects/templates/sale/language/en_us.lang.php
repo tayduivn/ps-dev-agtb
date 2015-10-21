ac18190bbf83786b5414d2270084830a163f47a9
Sergey Ger <sger@sugarcrm.com>
2015-10-21 11:08:00 +0300

    PAT-2296

--- sugarcrm/include/SugarObjects/templates/sale/language/en_us.lang.php
+++ sugarcrm/include/SugarObjects/templates/sale/language/en_us.lang.php
@@ -109,8 +109,8 @@ if(!defined('sugarEntry') || !sugarEntry) die('Not A Valid Entry Point');
   'LBL_CURRENCY_NAME'=>'Currency Name',
   'LBL_CURRENCY_SYMBOL'=>'Currency Symbol',
   'LBL_EDIT_BUTTON' => 'Edit',
-   'LBL_REMOVE' => 'Remove',
-
+  'LBL_REMOVE' => 'Remove',
+  'LBL_CURRENCY_RATE' => 'Base rate',
 
 );
 
