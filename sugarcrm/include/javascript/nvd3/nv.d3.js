8b85258f4b6c91b1c5356f0ab84ea5bd72210c31
Henry Rogers <hrogers@sugarcrm.com>
2015-11-17 09:48:00 -0500

    MAR-3448: replace all getBBox() calls with getBoundingClientRect() in D3 charts

--- sugarcrm/include/javascript/nvd3/nv.d3.js
+++ sugarcrm/include/javascript/nvd3/nv.d3.js
@@ -860,7 +860,7 @@ nv.utils.stringSetLengths = function (_data, _container, _format) {
   txt.style('display', 'inline');
   _data.forEach(function(d, i) {
       txt.text(d, _format);
-      lengths.push(txt.node().getBBox().width);
+      lengths.push(txt.node().getBoundingClientRect().width);
     });
   txt.text('').style('display', 'none');
   return lengths;
@@ -881,21 +881,21 @@ nv.utils.stringEllipsify = function(_string, _container, _length) {
   }
   txt.style('display', 'inline');
   txt.text('...');
-  ell = txt.node().getBBox().width;
+  ell = txt.node().getBoundingClientRect().width;
   txt.text(str);
-  len = txt.node().getBBox().width;
+  len = txt.node().getBoundingClientRect().width;
   strLen = len;
   while (len > _length && len > 30) {
     str = str.slice(0, -1);
     txt.text(str);
-    len = txt.node().getBBox().width + ell;
+    len = txt.node().getBoundingClientRect().width + ell;
   }
   txt.text('').style('display', 'none');
   return str + (strLen > _length ? '...' : '');
 };
 
 nv.utils.getTextBBox = function(text, floats) {
-  var bbox = text.node().getBBox(),
+  var bbox = text.node().getBoundingClientRect(),
       size = {
         width: floats ? bbox.width : parseInt(bbox.width, 10),
         height: floats ? bbox.height : parseInt(bbox.height, 10)
@@ -1940,7 +1940,7 @@ nv.models.legend = function() {
         g.style('display', 'inline');
 
         series.select('text').each(function(d, i) {
-          var textWidth = d3.select(this).node().getBBox().width;
+          var textWidth = d3.select(this).node().getBoundingClientRect().width;
           keyWidths.push(Math.max(Math.floor(textWidth), (type === 'line' ? 50 : 20)));
         });
 
@@ -1951,7 +1951,7 @@ nv.models.legend = function() {
 
       legend.getLineHeight = function() {
         g.style('display', 'inline');
-        var lineHeightBB = Math.floor(series.select('text').node().getBBox().height);
+        var lineHeightBB = Math.floor(series.select('text').node().getBoundingClientRect().height);
         return lineHeightBB;
       };
 
@@ -5040,7 +5040,7 @@ nv.models.funnel = function() {
       }
 
       function calcLabelBBox(lbl) {
-        return d3.select(lbl).node().getBBox();
+        return d3.select(lbl).node().getBoundingClientRect();
       }
 
       function calcFunnelLabelDimensions(lbls) {
@@ -6130,7 +6130,7 @@ nv.models.gauge = function() {
               .style('font-size', prop(0.7)+'em')
             ;
 
-          var bbox = g.select('.nv-odomText').node().getBBox();
+          var bbox = g.select('.nv-odomText').node().getBoundingClientRect();
 
           g.select('.nv-odometer')
             .insert('path','.nv-odomText')
@@ -8828,7 +8828,7 @@ nv.models.multiBar = function() {
 
       if (showValues) {
 
-        var valueBox = barText[0][0].getBBox(),
+        var valueBox = barText[0][0].getBoundingClientRect(),
             valueDim = vertical ? valueBox.height : valueBox.width;
 
         // KEEP: to be used in case you want rect behind text
@@ -8966,7 +8966,7 @@ nv.models.multiBar = function() {
 
         // KEEP: to be used in case you want rect behind text
         // bars.selectAll('text').each(function(d, i) {
-        //       var width = this.getBBox().width + 20;
+        //       var width = this.getBoundingClientRect().width + 20;
         //       if (width > labelBoxWidth) {
         //         labelBoxWidth = width;
         //       }
@@ -11418,7 +11418,7 @@ nv.models.pie = function() {
       holeWrap.call(holeFormat, hole ? [hole] : []);
 
       if (hole) {
-        var heightHoleHalf = holeWrap.node().getBBox().height * 0.30,
+        var heightHoleHalf = holeWrap.node().getBoundingClientRect().height * 0.30,
             heightPieHalf = Math.abs(maxHeightRadius * d3.min(extHeights)),
             holeOffset = Math.round(heightHoleHalf - heightPieHalf);
 
@@ -11556,7 +11556,7 @@ nv.models.pie = function() {
                 return;
               }
               var slice = d3.select(this),
-                  textBox = slice.select('text').node().getBBox();
+                  textBox = slice.select('text').node().getBoundingClientRect();
               slice.select('rect')
                 .attr('rx', 3)
                 .attr('ry', 3)
@@ -15079,7 +15079,7 @@ nv.models.tree = function() {
 
             prevScale = zoom.scale(),
             prevTrans = zoom.translate(),
-            treeBBox = backg.node().getBBox(),
+            treeBBox = backg.node().getBoundingClientRect(),
 
             size = [
               treeBBox.width,
