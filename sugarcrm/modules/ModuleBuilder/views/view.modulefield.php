395fa9796dc59ff6f32eb166f622c8c0d5570872
skymeyer <jelle.vink@gmail.com>
2015-11-30 12:08:44 -0800

    BR-3704: Add strict validation for allowed field types

--- sugarcrm/modules/ModuleBuilder/views/view.modulefield.php
+++ sugarcrm/modules/ModuleBuilder/views/view.modulefield.php
@@ -114,9 +114,7 @@ class ViewModulefield extends SugarView
             // continue like normal
             if (empty($vardef['name']) || $isNew) {
                 if (!empty($_REQUEST['type'])) {
-                    // Until BR-3708 has been solved we cannot use Assert\Choice based on $field_types.
-                    //$vardef['type'] = $this->request->getValidInputRequest('type', array('Assert\Choice' => array('choices' => array_keys($field_types))), '');
-                    $vardef['type'] = $this->request->getValidInputRequest('type', 'Assert\ComponentName');
+                    $vardef['type'] = $this->request->getValidInputRequest('type', array('Assert\Choice' => array('choices' => array_keys($field_types))), '');
                 }
                 $fv->ss->assign('hideLevel', 0);
             } elseif (isset($vardef['custom_module'])) {
