27f67bf578b72353b32a1147e95ce0567d2309eb
skymeyer <jelle.vink@gmail.com>
2015-12-14 22:08:53 -0800

    BR-3743: Ability to return variable content from include file - unit tests

--- sugarcrm/testsunit/include/utils/SugarAutoLoaderTest.php
+++ sugarcrm/testsunit/include/utils/SugarAutoLoaderTest.php
@@ -21,6 +21,22 @@ namespace Sugarcrm\SugarcrmTestUnit\inc\utils;
 class SugarAutoLoaderTest extends \PHPUnit_Framework_TestCase
 {
     /**
+     * List of test files created
+     * @var array
+     */
+    protected $testFiles = array();
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function tearDown()
+    {
+        foreach (array_unique($this->testFiles) as $file) {
+            unlink($file);
+        }
+    }
+
+    /**
      * @covers ::validateFilePath
      * @dataProvider providerTestValidFilePath
      */
@@ -67,5 +83,145 @@ class SugarAutoLoaderTest extends \PHPUnit_Framework_TestCase
                 'File name violation: directory traversal detected'
             ),
         );
+    }
+
+    /**
+     * @covers ::varsFromInclude
+     * @dataProvider providerTestVarsFromInclude
+     */
+    public function testVarsFromInclude($content, array $vars, array $expected)
+    {
+        $file = 'SugarAutoLoaderTestVarsFromInclude.php';
+        $this->createPhpTestFile($file, $content);
+        $actual = \SugarAutoLoader::varsFromInclude($file, $vars);
+        $this->assertSame($expected, $actual);
+    }
+
+    public function providerTestVarsFromInclude()
+    {
+        return array(
+            array(
+                array(
+                    'vardef1' => array('foo' => 'bar'),
+                    'vardef2' => array('beer' => 'buzz'),
+                ),
+                array('vardef'),
+                array('vardef' => null),
+            ),
+            array(
+                array(
+                    'vardef1' => array('happy' => 'joy'),
+                    'vardef2' => array('sad' => 'bugs'),
+                ),
+                array('vardef1'),
+                array('vardef1' => array('happy' => 'joy')),
+            ),
+            array(
+                array(
+                    'vardef1' => array('happy' => 'joy'),
+                    'vardef2' => array('sad' => 'bugs'),
+                ),
+                array('vardef2'),
+                array('vardef2' => array('sad' => 'bugs')),
+            ),
+            array(
+                array(
+                    'vardef1' => array('happy' => 'joy'),
+                    'vardef2' => array('sad' => 'bugs'),
+                ),
+                array('vardef1', 'vardef2'),
+                array(
+                    'vardef1' => array('happy' => 'joy'),
+                    'vardef2' => array('sad' => 'bugs'),
+                ),
+            ),
+            array(
+                array(
+                    'vardef1' => array('happy' => 'joy'),
+                    'vardef2' => array('sad' => 'bugs'),
+                ),
+                array('vardef1', 'bogus', 'vardef2'),
+                array(
+                    'vardef1' => array('happy' => 'joy'),
+                    'bogus' => null,
+                    'vardef2' => array('sad' => 'bugs'),
+                ),
+            ),
+        );
+    }
+
+    /**
+     * @covers ::varFromInclude
+     * @dataProvider providerTestVarFromInclude
+     */
+    public function testVarFromInclude($content, $var, $expected)
+    {
+        $file = 'SugarAutoLoaderTestVarFromInclude.php';
+        $this->createPhpTestFile($file, $content);
+        $actual = \SugarAutoLoader::varFromInclude($file, $var);
+        $this->assertSame($expected, $actual);
+    }
+
+
+    public function providerTestVarFromInclude()
+    {
+        return array(
+            array(
+                array(
+                    'vardef' => array('foo' => 'bar'),
+                ),
+                'vardef',
+                array('foo' => 'bar'),
+            ),
+            array(
+                array(
+                    'vardef1' => array('foo' => 'bar'),
+                    'vardef2' => array('beer' => 'buzz'),
+                ),
+                'vardef',
+                null,
+            ),
+            array(
+                array(
+                    'vardef1' => array('happy' => 'joy'),
+                    'vardef2' => array('sad' => 'bugs'),
+                ),
+                'vardef1',
+                array('happy' => 'joy'),
+            ),
+            array(
+                array(
+                    'vardef1' => array('happy' => 'joy'),
+                    'vardef2' => array('sad' => 'bugs'),
+                ),
+                'vardef2',
+                array('sad' => 'bugs'),
+            ),
+        );
+    }
+
+    /**
+     * Create PHP file with variables
+     * @param string $file
+     * @param array $vars
+     */
+    protected function createPhpTestFile($file, array $vars)
+    {
+        $content = '<?php' . PHP_EOL;
+        foreach ($vars as $varName => $varContent) {
+            $content .= '$' . $varName . ' = ' . var_export($varContent, true) . ';' . PHP_EOL;
+        }
+        $this->createFile($file, $content);
+    }
+
+    /**
+     * Create test file which is cleaned up after every test
+     * @param string $file
+     * @param string $content
+     */
+    protected function createFile($file, $content)
+    {
+        $this->testFiles[] = $file;
+        file_put_contents($file, $content);
     }
 }
