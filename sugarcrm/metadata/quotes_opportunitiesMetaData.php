a4c6fc22053dfb8ff41f1fdb1b7492aa41b84c34
Sergey Ger <sger@sugarcrm.com>
2015-12-15 18:04:08 +0300

    PAT-2494

--- sugarcrm/metadata/quotes_opportunitiesMetaData.php
+++ sugarcrm/metadata/quotes_opportunitiesMetaData.php
@@ -11,24 +11,34 @@ if(!defined('sugarEntry') || !sugarEntry) die('Not A Valid Entry Point');
  * Copyright (C) SugarCRM Inc. All rights reserved.
  */
 
-$dictionary['quotes_opportunities'] = array ( 'table' => 'quotes_opportunities'
-                                  , 'fields' => array (
-       array('name' =>'id', 'type' =>'varchar', 'len'=>'36')
-      , array('name' =>'opportunity_id', 'type' =>'varchar', 'len'=>'36',)
-      , array('name' =>'quote_id', 'type' =>'varchar', 'len'=>'36', )
-      , array ('name' => 'date_modified','type' => 'datetime')
-      , array('name' =>'deleted', 'type' =>'bool', 'len'=>'1', 'default'=>'0', 'required'=>false)
-                                                      )                                  , 'indices' => array (
-       array('name' =>'quotes_opportunitiespk', 'type' =>'primary', 'fields'=>array('id'))
-      , array('name' =>'idx_opp_qte_opp', 'type' =>'index', 'fields'=>array('opportunity_id'))
-      , array('name' => 'idx_quote_oportunities', 'type'=>'alternate_key', 'fields'=>array('quote_id'))
-      
-                                                      )
-  	  , 'relationships' => array ('quotes_opportunities' => array('lhs_module'=> 'Quotes', 'lhs_table'=> 'quotes', 'lhs_key' => 'id',
-							  'rhs_module'=> 'Opportunities', 'rhs_table'=> 'opportunities', 'rhs_key' => 'id',
-							  'relationship_type'=>'many-to-many',
-							  'join_table'=> 'quotes_opportunities', 'join_key_lhs'=>'quote_id', 'join_key_rhs'=>'opportunity_id',
-							  ))
-                                                      
+$dictionary['quotes_opportunities'] = array(
+    'table' => 'quotes_opportunities',
+    'fields' => array(
+        array('name' =>'id', 'type' =>'varchar', 'len'=>'36'),
+        array('name' =>'opportunity_id', 'type' =>'varchar', 'len'=>'36',),
+        array('name' =>'quote_id', 'type' =>'varchar', 'len'=>'36', ),
+        array ('name' => 'date_modified','type' => 'datetime'),
+        array('name' =>'deleted', 'type' =>'bool', 'len'=>'1', 'default'=>'0', 'required'=>false)
+    ),
+    'indices' => array(
+        array('name' =>'quotes_opportunitiespk', 'type' =>'primary', 'fields'=>array('id')),
+        array('name' =>'idx_opp_qte_opp', 'type' =>'index', 'fields'=>array('opportunity_id')),
+        array('name' => 'idx_quote_oportunities', 'type'=>'alternate_key', 'fields'=>array('quote_id'))
+    ),
+    'relationships' => array(
+        'quotes_opportunities' => array(
+            'lhs_module'=> 'Opportunities',
+            'lhs_table'=> 'opportunities',
+            'lhs_key' => 'id',
+            'rhs_module'=> 'Quotes',
+            'rhs_table'=> 'quotes',
+            'rhs_key' => 'id',
+            'relationship_type'=>'many-to-many',
+            'true_relationship_type' => 'one-to-many',
+            'join_table'=> 'quotes_opportunities',
+            'join_key_lhs'=>'opportunity_id',
+            'join_key_rhs'=>'quote_id',
+        )
+    )
 )
 ?>
