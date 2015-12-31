c94c5db896542a61264fe4023ed629ddb1bd9240
Jianzhong Zhu <jzhu@sugarcrm.com>
2015-12-31 14:27:18 -0800

    BR-3801 Application is showing fatal error if access is set as disabled for Accounts module mySQL or other DBs don't like inner join in update stmt. mySQL throws MySQL error 1093: You can't specify target table 'users' for update in FROM clause

--- sugarcrm/modules/ACLRoles/ACLRole.php
+++ sugarcrm/modules/ACLRoles/ACLRole.php
@@ -307,7 +307,7 @@ function mark_relationships_deleted($id){
             SET date_modified = %s
             WHERE deleted = 0 AND
               id IN (
-                    SELECT id
+                    SELECT user_id
                     FROM acl_roles_users
                     WHERE deleted = 0
                         AND role_id = %s
