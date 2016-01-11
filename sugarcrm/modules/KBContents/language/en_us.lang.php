92d58f20abd889a669076d5fda171ffb0320aeff
Andrew Nester <anestor@sugarcrm.com>
2016-01-11 17:40:57 +0300

    RS-1286 - Removed unnecessary word 'article' from success message

--- sugarcrm/modules/KBContents/language/en_us.lang.php
+++ sugarcrm/modules/KBContents/language/en_us.lang.php
@@ -99,6 +99,6 @@ $mod_strings = array (
     'ERROR_ACTIVE_DATE_APPROVE_REQUIRED' => 'The Approved status requires publishing date.',
     'ERROR_ACTIVE_DATE_LOW' => 'The Publish Date must occur on a later date than today\'s date.',
     'ERROR_ACTIVE_DATE_EMPTY' => 'The Publish Date is empty.',
-    'LBL_RECORD_SAVED_SUCCESS' => 'You successfully created the {{moduleSingularLower}} article <a href="#{{buildRoute model=this}}">{{name}}</a>.', // use when a model is available
+    'LBL_RECORD_SAVED_SUCCESS' => 'You successfully created the {{moduleSingularLower}} <a href="#{{buildRoute model=this}}">{{name}}</a>.', // use when a model is available
     'TPL_SHOW_MORE_MODULE' => 'More {{module}} articles...',
 );
