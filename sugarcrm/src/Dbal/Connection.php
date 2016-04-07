b04dcf06b9ca367fc371a61db49b3a59c8285e10
Sergey Morozov <smorozov@sugarcrm.com>
2016-04-07 12:54:01 -0700

    [BR-3362] Added logging for failed queries

--- sugarcrm/src/Dbal/Connection.php
+++ sugarcrm/src/Dbal/Connection.php
@@ -12,7 +12,9 @@
 
 namespace Sugarcrm\Sugarcrm\Dbal;
 
+use Doctrine\DBAL\Cache\QueryCacheProfile;
 use Doctrine\DBAL\Connection as BaseConnection;
+use Doctrine\DBAL\DBALException;
 use Sugarcrm\Sugarcrm\Dbal\Query\QueryBuilder;
 
 /**
@@ -28,5 +30,41 @@ class Connection extends BaseConnection
     public function createQueryBuilder()
     {
         return new QueryBuilder($this);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function executeQuery($query, array $params = array(), $types = array(), QueryCacheProfile $qcp = null)
+    {
+        try {
+            return parent::executeQuery($query, $params, $types, $qcp);
+        } catch (DBALException $e) {
+            $this->logException($e);
+            throw $e;
+        }
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function executeUpdate($query, array $params = array(), $types = array())
+    {
+        try {
+            return parent::executeUpdate($query, $params, $types);
+        } catch (DBALException $e) {
+            $this->logException($e);
+            throw $e;
+        }
+    }
+
+    /**
+     * Logs DBAL exception
+     *
+     * @param DBALException $e Exception
+     */
+    protected function logException(DBALException $e)
+    {
+        $GLOBALS['log']->fatal($e->getMessage());
     }
 }
