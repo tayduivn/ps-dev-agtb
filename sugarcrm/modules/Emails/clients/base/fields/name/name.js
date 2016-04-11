3d11b94bbfad1e181d4517a8e7954437da1020f5
Ed Roesch <eroesch@sugarcrm.com>
2016-04-11 14:00:32 -0400

    MAR-3697: Added (no subject) if the email has no subject to allow user to click link
    MAR-3697: Addressed indenting inconsistencies
    
    MAR-3697: Converted check for no subject line to the format() method instead of a template edit
    
    MAR-3697: Added extending from the base name field

---
+++ sugarcrm/modules/Emails/clients/base/fields/name/name.js
@@ -0,0 +1,30 @@
+/*
+ * Your installation or use of this SugarCRM file is subject to the applicable
+ * terms available at
+ * http://support.sugarcrm.com/Resources/Master_Subscription_Agreements/.
+ * If you do not agree to all of the applicable terms or do not have the
+ * authority to bind the entity as an authorized representative, then do not
+ * install or use this SugarCRM file.
+ *
+ * Copyright (C) SugarCRM Inc. All rights reserved.
+ */
+ /**
+ * @class View.Fields.Base.Emails.NameField
+ * @alias SUGAR.App.view.fields.BaseEmailsNameField
+ * @extends View.Fields.Base.NameField
+ */
+({
+    extendsFrom: 'BaseNameField',
+
+    /**
+     * @inheritdoc
+     */
+    format: function(value) {
+        if (_.isEmpty(value)) {
+            // the subject line is empty, show (no subject) instead of blank
+            return app.lang.get('LBL_NO_SUBJECT', this.module);
+        }
+
+        return value;
+    }
+})
