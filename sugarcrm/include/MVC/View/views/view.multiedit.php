9baf5206859292481e6026bd4cb9658ab28f1337
Vishal Jain <vjain@sugarcrm.com>
2016-04-12 16:40:20 -0700

    RM-2882: Update CRLF on merged files

--- sugarcrm/include/MVC/View/views/view.multiedit.php
+++ sugarcrm/include/MVC/View/views/view.multiedit.php
@@ -16,13 +16,13 @@ use Sugarcrm\Sugarcrm\Security\InputValidation\InputValidation;
 require_once('include/EditView/EditView2.php');
 class ViewMultiedit extends SugarView
 {
- 	var $type ='edit';
- 	
- 	function display(){
-		global $beanList, $beanFiles;
-		if($this->action == 'AjaxFormSave'){
-			echo "<a href='index.php?action=DetailView&module=".$this->module."&record=".$this->bean->id."'>".$this->bean->id."</a>";
-		}else{
+    var $type ='edit';
+
+    function display(){
+        global $beanList, $beanFiles;
+        if($this->action == 'AjaxFormSave'){
+            echo "<a href='index.php?action=DetailView&module=".$this->module."&record=".$this->bean->id."'>".$this->bean->id."</a>";
+        }else{
             $modules = InputValidation::getService()->getValidInputRequest(
                 'modules',
                 array(
@@ -33,32 +33,32 @@ class ViewMultiedit extends SugarView
             );
 
             if (!empty($modules)) {
-				$js_array = 'Array(';
-				
+                $js_array = 'Array(';
+                
                 $count = count($modules);
-				$index = 1;
+                $index = 1;
                 foreach ($modules as $module) {
-					$js_array .= "'form_".$module."'";
-					if($index < $count)
-						$js_array .= ',';
-					$index++;
-				}
-				//$js_array = "Array(".implode(",", $js_array). ")";
-				$js_array .= ');';
-				echo "<script language='javascript'>var ajaxFormArray = new ".$js_array."</script>";
-				if($count > 1)
-					echo '<input type="button" class="button" value="Save All" id=\'ajaxsaveall\' onclick="return saveForms(\'Saving...\', \'Save Complete\');"/>';
+                    $js_array .= "'form_".$module."'";
+                    if($index < $count)
+                        $js_array .= ',';
+                    $index++;
+                }
+                //$js_array = "Array(".implode(",", $js_array). ")";
+                $js_array .= ');';
+                echo "<script language='javascript'>var ajaxFormArray = new ".$js_array."</script>";
+                if($count > 1)
+                    echo '<input type="button" class="button" value="Save All" id=\'ajaxsaveall\' onclick="return saveForms(\'Saving...\', \'Save Complete\');"/>';
                 foreach ($modules as $module) {
-					$bean = $beanList[$module];
-					require_once($beanFiles[$bean]);
-					$GLOBALS['mod_strings'] = return_module_language($GLOBALS['current_language'], $module);
-					$ev = new EditView($module);
-					$ev->process();
-					echo "<div id='multiedit_form_".$module."'>";
-					echo $ev->display(true, true);
-					echo "</div>";
-				}
-			}
-		}
- 	}
+                    $bean = $beanList[$module];
+                    require_once($beanFiles[$bean]);
+                    $GLOBALS['mod_strings'] = return_module_language($GLOBALS['current_language'], $module);
+                    $ev = new EditView($module);
+                    $ev->process();
+                    echo "<div id='multiedit_form_".$module."'>";
+                    echo $ev->display(true, true);
+                    echo "</div>";
+                }
+            }
+        }
+    }
  }
