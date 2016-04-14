245ee4d63930fad0da4a802a233bfd254bdb5893
Andrew Nester <anestor@sugarcrm.com>
2016-04-14 17:39:03 +0300

    RS-1445 - KB: Voted vote does not highlighted after login with portal user

--- sugarcrm/modules/KBContents/clients/portal/fields/usefulness/usefulness.js
+++ sugarcrm/modules/KBContents/clients/portal/fields/usefulness/usefulness.js
@@ -10,5 +10,16 @@
  */
 
 ({
-    extendsFrom: 'BaseKBContentsUsefulnessField'
+    extendsFrom: 'BaseKBContentsUsefulnessField',
+
+    initialize: function(options) {
+        this._super('initialize', [options]);
+
+        //RS-1445 - Need to wait for model to be loaded to get value of usefulness_user_vote
+        this.model.on('data:sync:complete', function() {
+            if (!this.disposed) {
+                this.render();
+            }
+        }, this);
+    }
 })
