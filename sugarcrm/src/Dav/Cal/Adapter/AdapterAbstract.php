876ef654f385edc8f74a5ac49ee04ec4bcf47c9f
atarantej-sugar <atarantej@sugarcrm.com>
2016-05-12 17:44:32 +0300

    [CRYS-1639] Delete the parent event in iCal does not get remove from Sugar

--- sugarcrm/src/Dav/Cal/Adapter/AdapterAbstract.php
+++ sugarcrm/src/Dav/Cal/Adapter/AdapterAbstract.php
@@ -527,6 +527,10 @@ abstract class AdapterAbstract implements AdapterInterface
             $customizeParent = $diffStructure['children'][$parentRecurrenceId->asDb()];
         }
 
+        if ($customizeParent && $customizeParent[0] == 'delete') {
+            $parentAction = 'delete';
+        }
+
         $rrule = isset($diffStructure['rrule']) ? $diffStructure['rrule'] : array();
 
         // we need independent rrule update only when we change event
@@ -619,7 +623,7 @@ abstract class AdapterAbstract implements AdapterInterface
                     array_merge($parentChangedFields, $childChangeFields),
                     array_merge($parentInvites, $childChangeInvitees),
                 );
-            } elseif ($childEvent && !$childEvent->isCustomized() && $parentAction) {
+            } elseif ($childEvent && !$childEvent->isCustomized() && $parentAction && $parentAction !== 'delete') {
                 $result[] = array(
                     array(
                         'update',
