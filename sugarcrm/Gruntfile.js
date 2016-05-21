193e01d1f26351db07c6a9ce1128c7d581acabb1
Filipe Guerra <alias.mac@gmail.com>
2016-05-21 00:43:01 -0700

    Fix karma tests on grunt after upgraded libs (SC-5715)

--- sugarcrm/Gruntfile.js
+++ sugarcrm/Gruntfile.js
@@ -51,7 +51,9 @@ module.exports = function(grunt) {
             },
             ci: {
                 junitReporter: {
-                    outputFile: path + 'karma/test-results.xml'
+                    outputDir: path,
+                    outputFile: '/karma/test-results.xml',
+                    useBrowserName: false
                 },
                 reporters: [
                     'dots',
@@ -66,7 +68,9 @@ module.exports = function(grunt) {
                     ]
                 },
                 junitReporter: {
-                    outputFile: path + 'karma/test-results.xml'
+                    outputDir: path,
+                    outputFile: '/karma/test-results.xml',
+                    useBrowserName: false
                 },
                 reporters: [
                     'coverage',
