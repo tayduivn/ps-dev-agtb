14433677fb962de590763d3b1fecb6ed9e4afa57
Filipe Guerra <alias.mac@gmail.com>
2016-05-26 08:57:53 -0700

    Fix check-license only for 7.7.1 and 7.7.2 (fixes SC-5769) (#31022)
    * Fix check-license only for 7.7.1 and 7.7.2 (fixes SC-5769)
    
    This is only a fix for those 2 versions that ignore spacing and other
    information from the license header.
    
    * Add libs and special files to license-white-list (fixes SC-5769)
    
    * Add 'sidecar/docs' to the white list (fixes SC-5769)
    
    * Fix license header for modules/vCals (fixes SC-5769)
    
    * Fix license header to use the correct url from LICENSE (fixes SC-5769)
    
    * Fix license header for upgrade/scripts (fixes SC-5769)
    
    * Add/Update some license headers (fixes SC-5769)
    
    * Add Minifier lib to the license-white-list (fixes SC-5769)
    
    * Add license header to some Sugar files that were missing (fixes SC-5769)
    
    * Add empty files to license-white-list (fixes SC-5769)
    
    These files need to be reviewed later, they might not be needed anymore.
    
    * Add files to the license white list (Fixes SC-5769)
    
    * Add missing license headers to 2 files (fixes SC-5769):
    
    - styleguide/content/charts/js/translate-data.js
    - sugarcrm/modules/Users/authentication/SAMLAuthenticate/settings.php
    
    * Update sidecar to match the one that has the license headers (SC-5769)
    
    * Add custom/ to white list for license check (fixes SC-5769)

--- sugarcrm/modules/vCals/Server.php
+++ sugarcrm/modules/vCals/Server.php
@@ -1,6 +1,6 @@
 <?php
 if(!defined('sugarEntry') || !sugarEntry) die('Not A Valid Entry Point');
- /*
+/*
  * Your installation or use of this SugarCRM file is subject to the applicable
  * terms available at
  * http://support.sugarcrm.com/06_Customer_Center/10_Master_Subscription_Agreements/.
