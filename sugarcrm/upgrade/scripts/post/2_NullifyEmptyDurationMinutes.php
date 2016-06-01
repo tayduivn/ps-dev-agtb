187d1639154e5ccc6c11696367a65737ed13953d
Robert Gonzalez <rgonzalez@sugarcrm.com>
2016-05-31 20:53:37 -0700

    MACAROON-1451: DB2 Query failed while upgrading from 6.5.23 to 7.7.1.0 Change logic to check for DB column type and vardef type before upgrading Add multiplatform logic for identifying int columns on tables Add handling for Oracle, IBMDB2 and MySQL/MSSQL integer type columns Add unit test for this change

--- sugarcrm/upgrade/scripts/post/2_NullifyEmptyDurationMinutes.php
+++ sugarcrm/upgrade/scripts/post/2_NullifyEmptyDurationMinutes.php
@@ -29,12 +29,142 @@ class SugarUpgradeNullifyEmptyDurationMinutes extends UpgradeScript
      */
     public $type = self::UPGRADE_ALL;
 
+    /**
+     * The name of the column we will be working on
+     * @var string
+     */
+    protected $columnName = 'duration_minutes';
+
+    /**
+     * The name of the table we will be working on
+     * @var string
+     */
+    protected $tableName = 'meetings';
+
+    /**
+     * Mapping of integer column types that are the same as int
+     * @var array
+     */
+    protected $numericColTypes = array(
+        // MySQL and MSSQL integer type keyword
+        'int' => 1,
+        // IBMDB2 integer type keyword
+        'integer' => 1,
+        // Oracle integer type keyword
+        'number' => 1,
+    );
+
     public function run()
     {
-        // Only run this if we are coming from a version lower than 7.6.1
-        if (version_compare($this->from_version, '7.6.1.0', '<')) {
+        // Only run this if the criteria was met
+        if ($this->needsUpdating()) {
             $this->setEmptyValuesToNull();
         }
+    }
+
+    /**
+     * Determines whether the upgrader needs to be run
+     * @return boolean
+     */
+    protected function needsUpdating()
+    {
+        // Grab the database column type for duration_minutes
+        $colType = $this->getColumnType();
+
+        // If it is INT or if it is empty, we have nothing to do
+        if ($this->columnTypeIsInt($colType)) {
+            return false;
+        }
+
+        // At this point, our column type will be known to not be int, so if the
+        // vardef is int, we need to update
+        $fieldType = $this->getFielddefType();
+
+        if ($fieldType === 'int') {
+            return true;
+        }
+
+        // If the field type was other than INT, be it NULL or ENUM or whatever,
+        // we should not proceed
+        return false;
+    }
+
+    /**
+     * Determines if the column type specified in $colType is an integer type
+     * @param string $colType
+     * @return boolean
+     */
+    protected function columnTypeIsInt($colType)
+    {
+        // An empty colType, same as an int colType, means nothing to do
+        if (empty($colType)) {
+            return true;
+        }
+
+        // Normalize the colType
+        $colType = strtolower($colType);
+
+        // If the colType exists, then it is an int type field
+        return !empty($this->numericColTypes[$colType]);
+    }
+
+    /**
+     * Gets the database column type for the field we are trying to upgrade, if
+     * it exists
+     * @return string
+     */
+    protected function getColumnType()
+    {
+        $columns = $this->getColumns();
+        return isset($columns[$this->columnName]['type']) ? $columns[$this->columnName]['type'] : '';
+    }
+
+    /**
+     * Gets the columns for the table
+     * @return array
+     */
+    protected function getColumns()
+    {
+        return $this->db->get_columns($this->tableName);
+    }
+
+    /**
+     * Gets the field type from the vardef for the bean, if it exists
+     * @return string
+     */
+    protected function getFielddefType()
+    {
+        $defs = $this->getFielddefsFromBean();
+
+        // Now get the def for the this field if it exists
+        if (!isset($defs[$this->columnName])) {
+            return null;
+        }
+
+        $def = $defs[$this->columnName];
+
+        // If the dbType is set, as it is as of 7.6.1...
+        if (isset($def['dbType'])) {
+            return $def['dbType'];
+        }
+
+        // Otherwise send back the type if it is found
+        if (isset($def['type'])) {
+            return $def['type'];
+        }
+
+        return null;
+    }
+
+    /**
+     * Gets the field def array from the bean
+     * @return array
+     */
+    protected function getFielddefsFromBean()
+    {
+        // Start with the Meetings bean so we can get the vardef
+        $bean = BeanFactory::getBean('Meetings');
+        return $bean->field_defs;
     }
 
     /**
@@ -49,18 +179,18 @@ class SugarUpgradeNullifyEmptyDurationMinutes extends UpgradeScript
         $empty = $this->db->quoted('');
 
         // Build the sql that will handle the reset
-        $sql = "UPDATE meetings
-                SET duration_minutes = NULL
-                WHERE duration_minutes = $empty";
+        $sql = "UPDATE {$this->tableName}
+                SET {$this->columnName} = NULL
+                WHERE {$this->columnName} = $empty";
 
         // Now capture the result of what just happened. Success means there might
         // be affected rows, failure means there was an error.
         if ($result = $this->db->query($sql)) {
             $rows = $this->db->getAffectedRowCount($result);
-            $msg = "Meetings duration_minutes column reset from empty string to null. Affected records: $rows";
+            $msg = "Meetings {$this->columnName} column reset from empty string to null. Affected records: $rows";
         } else {
             $err = $this->db->lastError();
-            $msg = "Meetings duration_minutes column reset failed: $err";
+            $msg = "Meetings {$this->columnName} column reset failed: $err";
         }
 
         // Log it an carry on
