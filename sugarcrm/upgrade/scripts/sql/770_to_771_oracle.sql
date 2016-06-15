fb58780b6082affe46d595b8a71dba9c5234be13
Alexander Rymarchik <a.rymarchik@intetics.com>
2016-06-15 11:44:20 +0300

    RS-1599: KB: Deleted kb record/id appears in the Related to field after upgrade.

--- sugarcrm/upgrade/scripts/sql/770_to_771_oracle.sql
+++ sugarcrm/upgrade/scripts/sql/770_to_771_oracle.sql
@@ -8,6 +8,6 @@
  *
  * Copyright (C) SugarCRM Inc. All rights reserved.
  */
-UPDATE calls m, kbcontents s SET m.parent_id = NULL WHERE s.deleted = 1 AND s.id = m.parent_id AND m.parent_type = 'KBContents';
-UPDATE tasks m, kbcontents s SET m.parent_id = NULL WHERE s.deleted = 1 AND s.id = m.parent_id AND m.parent_type = 'KBContents';
-UPDATE meetings m, kbcontents s SET m.parent_id = NULL WHERE s.deleted = 1 AND s.id = m.parent_id AND m.parent_type = 'KBContents';
+UPDATE calls m SET m.parent_id = NULL WHERE m.parent_type = 'KBContents' AND m.parent_id = (SELECT id FROM kbcontents s WHERE s.deleted = 1);
+UPDATE tasks m SET m.parent_id = NULL WHERE m.parent_type = 'KBContents' AND m.parent_id = (SELECT id FROM kbcontents s WHERE s.deleted = 1);
+UPDATE meetings m SET m.parent_id = NULL WHERE m.parent_type = 'KBContents' AND m.parent_id = (SELECT id FROM kbcontents s WHERE s.deleted = 1);
