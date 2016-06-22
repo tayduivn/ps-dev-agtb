f30d8c4e36b4650b57ad854322b064dcf0a2b0e2
Greg Levine <levine.greg@gmail.com>
2016-06-22 17:00:48 -0400

    Migrates `emails.state` when upgrading (MAR-3897)

---
+++ sugarcrm/modules/Emails/upgrade/scripts/post/2_MigrateEmailState.php
@@ -0,0 +1,61 @@
+<?php
+/*
+ * Your installation or use of this SugarCRM file is subject to the applicable
+ * terms available at
+ * http://support.sugarcrm.com/Resources/Master_Subscription_Agreements/.
+ * If you do not agree to all of the applicable terms or do not have the
+ * authority to bind the entity as an authorized representative, then do not
+ * install or use this SugarCRM file.
+ *
+ * Copyright (C) SugarCRM Inc. All rights reserved.
+ */
+
+class SugarUpgradeMigrateEmailState extends UpgradeScript
+{
+    public $order = 2100;
+    public $type = self::UPGRADE_DB;
+
+    /**
+     * {@inheritdoc}
+     *
+     * Sets the `emails.state` column to "Archived" or "Draft" for all rows in emails.
+     *
+     * This upgrade script only runs when upgrading from a version prior to 7.9.
+     */
+    public function run()
+    {
+        if (!version_compare($this->from_version, '7.9', '<')) {
+            return;
+        }
+
+        $this->log('Set the default value (Archived) for emails.state for all emails');
+        $sql = "UPDATE emails SET state='Archived'";
+        $this->runQuery($sql);
+
+        $this->log('Set emails.state to Draft where emails.status is draft or send_error');
+        $sql = "UPDATE emails SET state='Draft' WHERE status IN ('draft', 'send_error')";
+        $this->runQuery($sql);
+
+        $sql = "SELECT COUNT(id) FROM emails WHERE state IS NULL OR state=''";
+        $num = $this->db->getOne($sql);
+        $this->log("{$num} emails remain with an empty state");
+    }
+
+    /**
+     * Executes an update query and logs the number of affected rows or the error.
+     *
+     * @param string $sql The query to execute.
+     */
+    protected function runQuery($sql)
+    {
+        $result = $this->db->query($sql);
+
+        if ($result) {
+            $rows = $this->db->getAffectedRowCount($result);
+            $this->log("Number of affected rows: {$rows}");
+        } else {
+            $error = $this->db->lastError();
+            $this->log("Error: {$error}");
+        }
+    }
+}
---
+++ sugarcrm/tests/{old}/modules/Emails/upgrade/scripts/post/MigrateEmailStateTest.php
@@ -0,0 +1,58 @@
+<?php
+/*
+ * Your installation or use of this SugarCRM file is subject to the applicable
+ * terms available at
+ * http://support.sugarcrm.com/Resources/Master_Subscription_Agreements/.
+ * If you do not agree to all of the applicable terms or do not have the
+ * authority to bind the entity as an authorized representative, then do not
+ * install or use this SugarCRM file.
+ *
+ * Copyright (C) SugarCRM Inc. All rights reserved.
+ */
+
+require_once 'tests/{old}/upgrade/UpgradeTestCase.php';
+require_once 'modules/Emails/upgrade/scripts/post/2_MigrateEmailState.php';
+
+/**
+ * @coversDefaultClass SugarUpgradeMigrateEmailState
+ */
+class MigrateEmailStateTest extends UpgradeTestCase
+{
+    public function tearDown()
+    {
+        SugarTestEmailUtilities::removeAllCreatedEmails();
+        parent::tearDown();
+    }
+
+    /**
+     * @covers ::run
+     */
+    public function testRun()
+    {
+        SugarTestEmailUtilities::createEmail();
+        SugarTestEmailUtilities::createEmail();
+        SugarTestEmailUtilities::createEmail();
+        SugarTestEmailUtilities::createEmail('', array('status' => 'sent', 'type' => 'out'));
+        SugarTestEmailUtilities::createEmail('', array('status' => 'unread', 'type' => 'inbound'));
+        SugarTestEmailUtilities::createEmail('', array('status' => 'draft', 'type' => 'draft'));
+        SugarTestEmailUtilities::createEmail('', array('status' => 'draft', 'type' => 'draft'));
+        SugarTestEmailUtilities::createEmail('', array('status' => 'draft', 'type' => 'out'));
+        SugarTestEmailUtilities::createEmail('', array('status' => 'draft', 'type' => 'archived'));
+        SugarTestEmailUtilities::createEmail('', array('status' => 'draft', 'type' => 'draft'));
+        SugarTestEmailUtilities::createEmail('', array('status' => 'send_error', 'type' => 'out'));
+        SugarTestEmailUtilities::createEmail('', array('status' => 'send_error', 'type' => 'out'));
+
+        $GLOBALS['db']->query("UPDATE emails SET state=NULL");
+
+        $script = $this->upgrader->getScript('post', '2_MigrateEmailState');
+        $script->db = $GLOBALS['db'];
+        $script->from_version = '7.8.0.0';
+        $script->run();
+
+        $num = $GLOBALS['db']->getOne("SELECT COUNT(id) FROM emails WHERE state='Archived'");
+        $this->assertEquals(5, $num, 'There should be 5 archived emails');
+
+        $num = $GLOBALS['db']->getOne("SELECT COUNT(id) FROM emails WHERE state='Draft'");
+        $this->assertEquals(7, $num, 'There should be 7 draft emails');
+    }
+}
