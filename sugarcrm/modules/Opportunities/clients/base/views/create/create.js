d2f214b21223d7f9d1c5e019c6aa65adfa5c89e7
hqi <hqi@sugarcrm.com>
2016-06-22 14:53:11 -0700

    CET-70: Opportunity Save - RLI Required Fields Hilighted before Opportunity Required Fields

--- sugarcrm/modules/Opportunities/clients/base/views/create/create.js
+++ sugarcrm/modules/Opportunities/clients/base/views/create/create.js
@@ -50,6 +50,11 @@
     //END SUGARCRM flav=ent ONLY
 
     /**
+     * If subpanel models are valid
+     */
+    validSubpanelModels: true,
+
+    /**
      * @inheritdoc
      */
     initialize: function(options) {
@@ -60,6 +65,85 @@
         //END SUGARCRM flav=ent ONLY
         this._super('initialize', [options]);
         app.utils.hideForecastCommitStageField(this.meta.panels);
+    },
+
+    /**
+     * @override
+     */
+    initiateSave: function(callback) {
+        this.disableButtons();
+        async.waterfall([
+            _.bind(function(cb) {
+                async.parallel([
+                    _.bind(this.validateSubpanelModelsWaterfall, this),
+                    _.bind(this.validateModelWaterfall, this)
+                ], function(err) {
+                    // err is undefined if no errors
+                    cb(!_.isUndefined(err));
+                });
+            }, this),
+            _.bind(this.dupeCheckWaterfall, this),
+            _.bind(this.createRecordWaterfall, this)
+        ], _.bind(function(error) {
+            this.enableButtons();
+            if (error && error.status == 412 && !error.request.metadataRetry) {
+                this.handleMetadataSyncError(error);
+            } else if (!error && !this.disposed) {
+                this.context.lastSaveAction = null;
+                callback();
+            }
+        }, this));
+    },
+
+    /**
+     * Check to see if all fields are valid
+     * @override
+     * @param callback
+     */
+    validateModelWaterfall: function(callback) {
+        // override this.model.doValidate() to display error if subpanel model validation failed
+        this.model.trigger('validation:start');
+        this.model.isValidAsync(this.getFields(this.module), _.bind(function(isValid, errors) {
+            if (this.validSubpanelModels && isValid) {
+                this.model.trigger('validation:success');
+            } else if (!this.validSubpanelModels) {
+                this.model.trigger('error:validation');
+            }
+            this.model.trigger('validation:complete', this.model._processValidationErrors(errors));
+            callback(!isValid);
+        }, this));
+    },
+
+    /**
+     * Check to see if there are subpanel create models on this view
+     * And trigger an event to tell the subpanel to validate itself
+     *
+     * @override
+     * @param callback
+     * @return {Mixed}
+     */
+    validateSubpanelModelsWaterfall: function(callback) {
+        this.hasSubpanelModels = false;
+        this.validSubpanelModels = true;
+        _.each(this.context.children, function(child) {
+            if (child.get('isCreateSubpanel')) {
+                this.hasSubpanelModels = true;
+                this.context.trigger('subpanel:validateCollection:' + child.get('link'),
+                    _.bind(function(notValid) {
+                        if (this.validSubpanelModels && notValid) {
+                            this.validSubpanelModels = false;
+                        }
+                        callback(notValid);
+                    }, this),
+                    true
+                );
+            }
+        }, this);
+
+        // If there are no subpanel models, callback false so the waterfall can continue
+        if (!this.hasSubpanelModels) {
+            return callback(false);
+        }
     },
 
     //BEGIN SUGARCRM flav=ent ONLY
