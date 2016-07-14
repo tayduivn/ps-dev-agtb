b6bf56c00000d70850820040e521593d61871658
Boro Sitnikovski <buritomath@yahoo.com>
2016-07-14 20:04:14 +0200

    [SC-5910]: Gulp support for doc (Mango) (#31620)

--- sugarcrm/gulpfile.js
+++ sugarcrm/gulpfile.js
@@ -300,6 +300,24 @@ gulp.task('check-license', function(done) {
     });
 });
 
+gulp.task('jsduck', function() {
+    var firstPartyFiles = getFirstPartyFiles();
+    var GJSDuck = require('gulp-jsduck');
+    var options = [
+        '--out',
+        'docs',
+        '--title=SugarCRM Javascript Documentation',
+        '--color',
+        '--head-html=<link rel=\"stylesheet\" href=\"../styleguide/assets/css/jsduck.css\" type=\"text/css\">',
+        '--builtin-classes',
+        '--external=jQuery', // FIXME SC-5047: we may need to add more types here
+    ];
+    var gjsduck = new GJSDuck(options);
+
+    return gulp.src(firstPartyFiles)
+        .pipe(gjsduck.doc());
+});
+
 gulp.task('find-todos', function(done) {
     var teams = require('./gulp/plugins/team/team.js');
     commander
