947a17eb76634340b1c6931e6771f60a910b63ff
skymeyer <jelle.vink@gmail.com>
2016-08-02 20:09:37 -0700

    BR-4475: SQL_Injection Link2.php

--- sugarcrm/data/Link2.php
+++ sugarcrm/data/Link2.php
@@ -795,8 +795,8 @@ class Link2 {
             //look for key in  $join_key_values, if found add to filter criteria else abort duplicate checking.
             if (isset($join_key_values[$field])) {
 
-                $this->_duplicate_where .= $delimiter.' '.$field."='".$join_key_values[$field]."'";
-                $delimiter='AND';
+                $this->_duplicate_where .= $delimiter.' '.$field." = ". $this->_db->quoted($join_key_values[$field]);
+                $delimiter = ' AND ';
             } else {
                 $GLOBALS['log']->error('Duplicate checking aborted, Please supply a value for this column '.$field);
                 return false;
