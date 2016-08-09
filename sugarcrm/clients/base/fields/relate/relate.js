6164ee88cec4b2c36288c4203010f4e2fe8bc9ea
Yvan Gadeau <ygadeau@sugarcrm.com>
2016-08-09 03:33:12 -0700

    SC-5503: Update filter-rows - populate relate field using the new format

--- sugarcrm/clients/base/fields/relate/relate.js
+++ sugarcrm/clients/base/fields/relate/relate.js
@@ -634,6 +634,7 @@
             relatedRecords = relatedRecords[0];
         }
         // Silently set related records keeping backward compatibility.
+        // FIXME: We will remove this once BR-4577 is implemented.
         this.model.set(values, {silent: true});
         // Properly set related records in the model.
         this.model.set(this.fieldDefs.link, relatedRecords);
