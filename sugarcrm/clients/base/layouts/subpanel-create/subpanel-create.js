a89778f439dc2a49efd5ca59ab9854dcf79b0f7a
Filipe Guerra <alias.mac@gmail.com>
2016-08-12 02:12:45 +0100

    Revert "Revert "Deprecate subpanel-create layout controller by reusing subpanel (fixes SC-5984)"" (#32020)

--- sugarcrm/clients/base/layouts/subpanel-create/subpanel-create.js
+++ sugarcrm/clients/base/layouts/subpanel-create/subpanel-create.js
@@ -14,5 +14,13 @@
  * @extends View.Layouts.Base.SubpanelLayout
  */
 ({
-    extendsFrom: 'SubpanelLayout'
+    extendsFrom: 'SubpanelLayout',
+
+    initialize: function(options) {
+        app.logger.warn('`BaseSubpanelCreateLayout` controller ' +
+            'has been deprecated since 7.8.0 and will be removed in 7.9.0. To use `BaseSubpanelLayout` controller, ' +
+            'specify the `type` property in your `subpanel-create` metadata file instead.');
+
+        this._super('initialize', [options]);
+    }
 })
