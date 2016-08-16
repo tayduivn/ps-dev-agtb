8bdb490add117eca502f243c6ab22140b084d521
Prashant Shah <pshah@sugarcrm.com>
2016-08-16 11:53:26 -0700

    1. MACAROON-1531: [Oracle]: Cannot Save process definition on Oracle 2. Undo the work done on ABS-815 so that Process modules can continue to function properly

--- sugarcrm/modules/pmse_Business_Rules/vardefs.php
+++ sugarcrm/modules/pmse_Business_Rules/vardefs.php
@@ -45,7 +45,7 @@ $dictionary['pmse_Business_Rules'] = array(
             'required' => true,
             'name' => 'rst_uid',
             'vname' => 'LBL_RST_UID',
-            'type' => 'id',
+            'type' => 'varchar',
             'massupdate' => false,
             'no_default' => false,
             'comments' => '',
@@ -58,6 +58,7 @@ $dictionary['pmse_Business_Rules'] = array(
             'unified_search' => false,
             'merge_filter' => 'disabled',
             'calculated' => false,
+            'len' => '36',
             'size' => '20',
         ),
         'rst_type' => array(
