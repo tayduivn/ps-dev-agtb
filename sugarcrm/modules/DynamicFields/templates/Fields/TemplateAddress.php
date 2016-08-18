28ca7efc176bef9d07d27c231a7f0960dcbdca94
Robert Gonzalez <rgonzalez@sugarcrm.com>
2016-08-18 00:14:25 -0700

    MACAROON-1553: Custom address fields need group_label property Add group_label property to address field template

--- sugarcrm/modules/DynamicFields/templates/Fields/TemplateAddress.php
+++ sugarcrm/modules/DynamicFields/templates/Fields/TemplateAddress.php
@@ -20,17 +20,20 @@ class TemplateAddress extends TemplateField
     {
         // Bug 58560 - Set the group name since addresses are part of a group
         $this->group = $df->getDBName($this->name);
-        
+
         require_once 'modules/ModuleBuilder/parsers/parser.label.php' ;
         $parser = new ParserLabel ( $df->getModuleName() , $df->getPackageName() ) ;
-        
+
         // Clean up the labels so they more accurately reflect the actual field
         if (!empty($this->label_value)) {
             $labelValue = $this->label_value;
         } else {
             $labelValue = empty($_REQUEST['labelValue']) ? '' : $_REQUEST['labelValue'];
         }
-        
+
+        // Create the main label to be used as the group label
+        $this->group_label = 'LBL_' . strtoupper($this->name);
+
         // If there is a label to use, space it here for use below
         if (!empty($labelValue)) {
             $labelValue .= ' ';
@@ -38,10 +41,13 @@ class TemplateAddress extends TemplateField
         // To prevent mutilple calls to the metadata api cache rebuilder, queue
         MetaDataManager::enableCacheRefreshQueue();
 
+        // Save the main group label to the language extension now
+        $parser->handleSave(array('label_' . $this->group_label => $this->group_label), $GLOBALS['current_language']);
+
         // To keep consistency with OOTB address groups, add Street to the fields
         foreach ( array ( 'Street', 'City' , 'State' , 'PostalCode' , 'Country' ) as $addressFieldName )
         {
-            $systemLabel = strtoupper( "LBL_" . $this->name . '_' . $addressFieldName );
+            $systemLabel = $this->group_label . '_' . strtoupper($addressFieldName);
             // Use the entered label value as a prefix instead of the field name
             $parser->handleSave ( array( "label_" . $systemLabel => $labelValue . $addressFieldName ) , $GLOBALS [ 'current_language' ] ) ;
 
@@ -56,10 +62,11 @@ class TemplateAddress extends TemplateField
             $addressField->label = $addressField->vname = $systemLabel ;
             // Bug 58560 - Add the group to this field so it gets written to the custom vardefs
             $addressField->group = $this->group;
-            
+            $addressField->group_label = $this->group_label;
+
             // Maintain unified search setting for 'Street'
             $addressField->supports_unified_search = $addressField == 'Street';
-            
+
             $addressField->save ( $df ) ;
         }
 
