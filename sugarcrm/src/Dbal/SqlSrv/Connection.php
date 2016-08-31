f829772604a47b4165abb88668a9feae537b97c6
Sergey Morozov <smorozov@sugarcrm.com>
2016-08-31 16:40:31 -0700

    [BR-4393] Reuse prepared statements on SQL Server

--- sugarcrm/src/Dbal/SqlSrv/Connection.php
+++ sugarcrm/src/Dbal/SqlSrv/Connection.php
@@ -20,10 +20,32 @@ use Doctrine\DBAL\Driver\SQLSrv\SQLSrvConnection as BaseConnection;
 class Connection extends BaseConnection
 {
     /**
+     * @var \Doctrine\DBAL\Driver\SQLSrv\SQLSrvStatement[]
+     */
+    protected $statements = array();
+
+    /**
      * @param resource $connection
      */
     public function __construct($connection)
     {
         $this->conn = $connection;
+    }
+
+    /**
+     * {@inheritdoc}
+     *
+     * Reuse existing statements
+     */
+    public function prepare($sql)
+    {
+        $hash = md5($sql);
+        if (isset($this->statements[$hash])) {
+            $stmt = $this->statements[$hash];
+        } else {
+            $stmt = $this->statements[$hash] = new Statement($this->conn, $sql, $this->lastInsertId);
+        }
+
+        return $stmt;
     }
 }
