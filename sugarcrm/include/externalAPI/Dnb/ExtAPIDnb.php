50e7deeb232e803e895d3cd3fa5f346d4810cda8
RobertGonzalez <robert@robert-gonzalez.com>
2016-10-05 09:44:23 -0700

    ICE-10: [INT] D&B API endpoint needs to be updated - 7.7.2 (#32568)
    Fix the D&B API base URL

--- sugarcrm/include/externalAPI/Dnb/ExtAPIDnb.php
+++ sugarcrm/include/externalAPI/Dnb/ExtAPIDnb.php
@@ -24,7 +24,7 @@ class ExtAPIDnb extends ExternalAPIBase
     public $connector = "ext_rest_dnb";
     private $dnbBaseURL = array(
         'stg' => 'http://services-ext-stg.dnb.com/',
-        'prod' => 'https://maxcvservices.dnb.com/'
+        'prod' => 'https://direct.dnb.com/',
     );
     private $dnbAuthURL = "rest/Authentication";
     //OrderReasonCode is a parameter required to fetch Firmographic info for all companies
