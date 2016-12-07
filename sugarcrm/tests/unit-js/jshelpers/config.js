00faefca9661af20ff40ffc2b599cdddef56ed5f
Filipe Guerra <alias.mac@gmail.com>
2016-12-06 18:02:56 -0800

    Remove sidecar’s config from Mango tests (SC-6160)
    Mango should not need anything else but the minified files of sidecar to
    run the tests.

---
+++ sugarcrm/tests/unit-js/jshelpers/config.js
@@ -0,0 +1,69 @@
+(function(app) {
+
+    app.augment('config', {
+
+        appId: 'portal',
+        env: 'dev',
+        debugSugarApi: true,
+        logger: {
+            level: 'DEBUG'
+        },
+        logWriter: 'ConsoleWriter',
+        logFormatter: 'SimpleFormatter',
+
+        // For tests our context is the runner which is one level lower!
+        serverUrl: '../../../rest/v10',
+        siteUrl: '../../..',
+
+        serverTimeout: 30,
+        maxQueryResult: 20,
+
+        maxSearchQueryResult: 3,
+        unsecureRoutes: ['signup', 'error'],
+        platform: 'base',
+        defaultModule: 'Cases',
+        metadataTypes: [],
+
+        orderByDefaults: {
+            'Cases': {
+                field: 'case_number',
+                direction: 'asc'
+            },
+            'Bugs': {
+                field: 'bug_number',
+                direction: 'asc'
+            },
+            'Notes': {
+                field: 'date_modified',
+                direction: 'desc'
+            }
+        },
+
+        additionalComponents: {
+            header: {
+                target: '#header'
+            },
+            alert: {
+                target: '#alert'
+            },
+            footer: {
+                target: '#footer'
+            }
+        },
+
+        displayModules: [
+            'Bugs',
+            'Cases',
+            'KBDocuments'
+        ],
+
+        clientID: 'sugar',
+        syncConfig: false,
+        loadCss: false,
+        fetchLimit: {
+            recents: 20
+        },
+
+    }, false);
+
+})(SUGAR.App);
