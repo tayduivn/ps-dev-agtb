0101e287bbbf7d6a6cfdb37f25cbc22fdb65408e
asaxena-sugarcrm <asaxena@sugarcrm.com>
2016-12-20 14:16:45 -0800

    ICE-41 updated twitter url (#33067)

--- sugarcrm/modules/Connectors/connectors/sources/ext/rest/twitter/language/en_us.lang.php
+++ sugarcrm/modules/Connectors/connectors/sources/ext/rest/twitter/language/en_us.lang.php
@@ -19,7 +19,24 @@
 
 
 $connector_strings = array (
-    'LBL_LICENSING_INFO' => '<table border="0" cellspacing="1"><tr><td valign="top" width="35%" class="dataLabel">Obtain a API Key and Secret from Twitter by registering your Sugar instance as a new application.<br/><br>Steps to register your instance:<br/><br/><ol><li>Go to the Twitter Developers site: <a href=\'http://dev.twitter.com/apps/new\' target=\'_blank\'>http://dev.twitter.com/apps/new</a>.</li><li>Sign In using the Twitter account under which you would like to register the application.</li><li>Within the registration form, enter a name for the application. This is the name users will see when they authenticate their Twitter accounts from within Sugar.</li><li>Enter a Description.</li><li>Enter an Application Website URL.</li><li>Enter a Callback URL (could be anything since Sugar bypasses this on authentication. Example: Enter your Sugar site URL).</li><li>Accept the Twitter API Terms of Service.</li><li>Click "Create your Twitter application".</li><li>Within the application page, find the API Key and API Secret under the "API Keys" tab. Enter the Key and Secret below.</li></ol></td></tr></table>',
+    'LBL_LICENSING_INFO' => '<table border="0" cellspacing="1">
+								<tr>
+									<td valign="top" width="35%" class="dataLabel">
+										Obtain a API Key and Secret from Twitter by registering your Sugar instance as a new application.<br/><br>Steps to register your instance:<br/><br/>
+										<ol>
+											<li>Go to the Twitter Developers site: <a href=\'https://apps.twitter.com\' target=\'_blank\'>https://apps.twitter.com</a>.</li>
+											<li>Sign In using the Twitter account under which you would like to register the application.</li>
+											<li>Within the registration form, enter a name for the application. This is the name users will see when they authenticate their Twitter accounts from within Sugar.</li>
+											<li>Enter a Description.</li>
+											<li>Enter an Application Website URL.</li>
+											<li>Enter a Callback URL (could be anything since Sugar bypasses this on authentication. Example: Enter your Sugar site URL).</li>
+											<li>Accept the Twitter API Terms of Service.</li>
+											<li>Click "Create your Twitter application".</li>
+											<li>Within the application page, find the API Key and API Secret under the "API Keys" tab. Enter the Key and Secret below.</li>
+										</ol>
+									</td>
+								</tr>
+							</table>',
     'LBL_NAME' => 'Twitter Username',
     'LBL_ID' => 'Twitter Username',
 	'company_url' => 'URL',
