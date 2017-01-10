f036030533587e775bfd68f5d1b5ec89ab51f1a0
Sergey Morozov <smorozov@sugarcrm.com>
2017-01-09 16:58:44 -0800

    [BR-4796] SQL_Injection in line 20 of GetLatestRevision.php

--- sugarcrm/modules/Documents/GetLatestRevision.php
+++ sugarcrm/modules/Documents/GetLatestRevision.php
@@ -17,9 +17,11 @@ if (!empty($_REQUEST['record'])) {
 
 	$document = BeanFactory::getBean('Documents', $_REQUEST['record']);
 	if (!empty($document->document_revision_id) && !empty($_REQUEST['get_latest_for_id']))  {
-		$query="update linked_documents set document_revision_id='{$document->document_revision_id}', date_modified='".TimeDate::getInstance()->nowDb()."' where id ='{$_REQUEST['get_latest_for_id']}' ";
+        $query = 'UPDATE linked_documents'
+            . ' SET document_revision_id = ' . $document->db->quoted($document->document_revision_id)
+            . ', date_modified = ' . $document->db->quoted(TimeDate::getInstance()->nowDb())
+            . ' WHERE id = ' . $document->db->quoted($_REQUEST['get_latest_for_id']);
 		$document->db->query($query);
 	}	
 }
 handleRedirect();
-?>
