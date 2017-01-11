a9a3a6536b570f4ae9fb21db941f42bcb63daf66
rajesh2011 <rajesh2011@users.noreply.github.com>
2017-01-11 15:41:40 -0800

    BR-4944 (SI 77064) Oracle Search Case Insensitivity Does Not Work (#33143)

--- sugarcrm/include/SugarFields/Fields/Name/SugarFieldName.php
+++ sugarcrm/include/SugarFields/Fields/Name/SugarFieldName.php
@@ -16,9 +16,9 @@ class SugarFieldName extends SugarFieldBase
         //check to see if this name type field has a 'fields' array defined
         if (!empty($bean->field_defs[$fieldName]) && !empty($bean->field_defs[$fieldName]['fields'])) {
             //iterate through array to create search with concatenated fields
-            $expr = $bean->db->concat($bean->table_name, $bean->field_defs[$fieldName]['fields']);
-            $expr .= " LIKE '{$value}%'";
-            $where->addRaw($expr);
+            $conField = $bean->db->concat($bean->table_name, $bean->field_defs[$fieldName]['fields']);
+            $likeSql = $bean->db->getLikeSQL($conField, "{$value}%");
+            $where->addRaw($likeSql);
             //return false so no further processing is done on this field
             return false;
         }
