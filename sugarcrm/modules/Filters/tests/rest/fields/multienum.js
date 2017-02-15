099b59c96af8227a33079794ca4e3b2ab1a65908
jcsmorais <jcsmorais@gmail.com>
2017-02-15 13:09:59 -0800

    Add rest tests for filtering on multienum field types (fixes TY-1904)

---
+++ sugarcrm/modules/Filters/tests/rest/fields/multienum.js
@@ -0,0 +1,17 @@
+/*
+ * Your installation or use of this SugarCRM file is subject to the applicable
+ * terms available at
+ * http://support.sugarcrm.com/Resources/Master_Subscription_Agreements/.
+ * If you do not agree to all of the applicable terms or do not have the
+ * authority to bind the entity as an authorized representative, then do not
+ * install or use this SugarCRM file.
+ *
+ * Copyright (C) SugarCRM Inc. All rights reserved.
+ */
+
+// FIXME TY-1910: Currently there's no OOTB multienum fields available
+// FIXME TY-1822: Convert multienum operator to $in / $not_in
+describe('Filters.Multienum', function() {
+    it.skip('should filter records whose field value is any of the given values');
+    it.skip('should filter records whose field value is not any of the given values');
+});
