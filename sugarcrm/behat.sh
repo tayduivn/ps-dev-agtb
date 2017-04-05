b268b937ce016e6455e946463ff926f1836a255f
Andrii Fedyk <afedyk-sugarcrm@users.noreply.github.com>
2017-04-05 11:26:50 +0300

    BR-5173 IdM: Bootstraped for behat tests (#33762)

---
+++ sugarcrm/behat.sh
@@ -0,0 +1,60 @@
+#!/usr/bin/env bash
+
+if [[ -z $TMPDIR ]]
+then
+TMPDIR="/tmp"
+fi
+
+
+SELENIUM_URL="https://goo.gl/uTXEJ1"
+SELENIUM="selenium-server-standalone-3.3.1.jar"
+SELENIUM_LOG="selenium.log"
+
+if [ "$(uname)" == "Darwin" ]; then
+    CHROME_DRIVER_URL="https://chromedriver.storage.googleapis.com/2.28/chromedriver_mac64.zip"
+else
+    CHROME_DRIVER_URL="https://chromedriver.storage.googleapis.com/2.28/chromedriver_linux64.zip"
+fi
+CHROME_DRIVER_PATH_ARCHIVE="chromedriver.zip"
+CHROME_DRIVER="chromedriver"
+CHROME_DRIVER_LOG="chromedriver.log"
+
+INSTANCE_PATH=$PWD
+
+cd $TMPDIR
+
+if [ ! -f "$SELENIUM" ]
+then
+    printf "Downloading selenium...\n\n"
+    wget $SELENIUM_URL -O $SELENIUM
+fi
+
+if [ ! -f "$CHROME_DRIVER" ]
+then
+    printf "Downloading chrome driver...\n\n"
+    wget $CHROME_DRIVER_URL -O $CHROME_DRIVER_PATH_ARCHIVE
+    unzip $CHROME_DRIVER_PATH_ARCHIVE        #-d cache/
+fi
+
+IS_CHROME_DRIVER_RUNNING=`ps -cax | grep $CHROME_DRIVER`
+if [[  -z  $IS_CHROME_DRIVER_RUNNING ]]
+then
+    printf "Running chrome_driver \n\n"
+    `./$CHROME_DRIVER >> $CHROME_DRIVER_LOG & `
+    sleep 2
+    printf "DONE \n\n"
+fi
+
+IS_SELENIUM_RUNNING=`ps -ax | grep "java -jar $SELENIUM" | grep -v grep| grep -v logs`
+if [[  -z  $IS_SELENIUM_RUNNING  ]]
+then
+    printf "Running selenium \n\n"
+    echo "java -jar $SELENIUM 2>> $SELENIUM_LOG &"
+    java -jar $SELENIUM 2>> $SELENIUM_LOG &
+    sleep 2
+    printf "DONE \n\n"
+fi
+
+cd $INSTANCE_PATH
+
+./vendor/bin/behat
---
+++ sugarcrm/tests/behat/bootstrap/FeatureContext.php
@@ -0,0 +1,138 @@
+<?php
+namespace Sugarcrm\SugarcrmTests\Bootstrap;
+
+use Behat\MinkExtension\Context\MinkContext;
+
+/**
+ * Defines application features from the specific context.
+ */
+class FeatureContext extends MinkContext
+{
+
+    /**
+     * Click on element found by css selector.
+     * @When /^I click "([^"]*)"$/
+     * Example: I click "ul.megamenu li button"
+     */
+    public function iClick($selector)
+    {
+        $element = $this->getSession()->getPage()->find('css', $selector);
+        if (is_null($element)) {
+            throw new \RuntimeException("Not found element by selector($selector)");
+        }
+        $element->click();
+    }
+
+    /**
+     * Wait until the loading is completed
+     * @When /^I wait until the loading is completed$/
+     */
+    public function iWaitUntilTheLoadingIsCompleted()
+    {
+        $condition = '!document.querySelector(".alert-wrapper .alert-process .loading") '
+            . '&& !document.querySelector("span[sfuuid]:empty:not([class])");';
+        $this->getSession()->wait(20000, $condition);
+    }
+
+    /**
+     * Before working with BWC Iframe need this command.
+     * @When /^I switch to BWC$/
+     */
+    public function switchBwc()
+    {
+        $this->getSession()->getDriver()->switchToIFrame('bwc-frame');
+    }
+
+    /**
+     * Output from bwc.
+     * @When /^I switch to sidecar$/
+     */
+    public function switchSidecar()
+    {
+        $this->getSession()->getDriver()->switchToIFrame(null);
+    }
+
+    /**
+     * Wait :milliseconds.
+     * @When I wait :milliseconds
+     */
+    public function wait($milliseconds)
+    {
+        $this->getSession()->wait($milliseconds);
+    }
+
+    /**
+     * Wait for the page to redirected to be specific url.
+     * @Then /^I wait for the page to be redirected to "([^"]*)"$/
+     * @And /^I wait for the page to be redirected to "([^"]*)"$/
+     * Example: I wait for the page to be redirected to "http://localhost:8000"
+     */
+    public function iWaitForThePageToRedirectedTo($redirectedTo)
+    {
+        $this->spin(function (FeatureContext $context) use ($redirectedTo) {
+            $diff = $result = array_diff(
+                parse_url($redirectedTo),
+                parse_url($context->getSession()->getCurrentUrl())
+            );
+            return empty($diff);
+        });
+    }
+
+    /**
+     * Wait for the page to be loaded.
+     * @And /^wait for the page to be loaded$/
+     * @When /^wait for the page to be loaded$/
+     */
+    public function waitForThePageToBeLoaded()
+    {
+        $this->getSession()->wait(10000, "document.readyState === 'complete'");
+    }
+
+    /**
+     * Checking radiobutton.
+     * @When /^I check the "([^"]*)" radio button with "([^"]*)" value$/
+     * @And /^I check the "([^"]*)" radio button with "([^"]*)" value$/
+     * Example: I check the "radioName" radio button with "radioValue" value
+     */
+    public function iCheckTheRadioButtonWithValue($element, $value)
+    {
+        $element = $this->fixStepArgument($element);
+        $value = $this->fixStepArgument($value);
+        $selector = 'input[type="radio"][name="' . $element . '"]';
+        foreach ($this->getSession()->getPage()->findAll('css', $selector) as $radio) {
+            if ($radio->getAttribute('value') == $value) {
+                $radio->check();
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Spin wrapper for long test.
+     * @param \Closure $lambda will call until not return true or time out
+     * @param int $wait
+     * @return bool
+     * @throws \Exception
+     */
+    public function spin(\Closure $lambda, $wait = 60)
+    {
+        for ($i = 0; $i < $wait; $i++) {
+            try {
+                if ($lambda($this)) {
+                    return true;
+                }
+            } catch (\Exception $e) {
+                // do nothing
+            }
+            sleep(1);
+        }
+
+        $backtrace = debug_backtrace();
+
+        throw new \Exception(
+            "Timeout thrown by " . $backtrace[1]['class'] . "::" . $backtrace[1]['function'] . "()\n" .
+            $backtrace[1]['file'] . ", line " . $backtrace[1]['line']
+        );
+    }
+}
