9768f7785a34c7abf7bbd7758d916763f86c20cd
Greg Levine <levine.greg@gmail.com>
2017-04-10 15:03:48 -0400

    Use the help text feature for identifying the system account (MAR-4563)

--- sugarcrm/modules/OutboundEmail/clients/base/fields/name/name.js
+++ sugarcrm/modules/OutboundEmail/clients/base/fields/name/name.js
@@ -14,5 +14,19 @@
  * @extends View.Fields.Base.NameField
  */
 ({
-    extendsFrom: 'BaseNameField'
+    extendsFrom: 'BaseNameField',
+
+    /**
+     * Adds help text (LBL_SYSTEM_ACCOUNT) for the system account. Be aware
+     * that this will replace any help text that is defined in metadata.
+     *
+     * @inheritdoc
+     */
+    _render: function() {
+        if (this.model.get('type') === 'system') {
+            this.def.help = 'LBL_SYSTEM_ACCOUNT';
+        }
+
+        return this._super('_render');
+    }
 })
---
+++ sugarcrm/tests/unit-js/modules/OutboundEmail/clients/base/fields/name/name.js
@@ -0,0 +1,80 @@
+describe('OutboundEmail.BaseNameField', function() {
+    var app;
+    var context;
+    var field;
+    var model;
+    var sandbox;
+
+    beforeEach(function() {
+        var metadata = SugarTest.loadFixture('emails-metadata');
+
+        SugarTest.testMetadata.init();
+
+        _.each(metadata.modules, function(def, module) {
+            SugarTest.testMetadata.updateModuleMetadata(module, def);
+        });
+
+        SugarTest.declareData('base', 'OutboundEmail', true, false);
+        SugarTest.loadHandlebarsTemplate('name', 'field', 'base', 'detail', 'OutboundEmail');
+        SugarTest.loadComponent('base', 'field', 'name');
+        SugarTest.loadComponent('base', 'field', 'name', 'OutboundEmail');
+        SugarTest.testMetadata.set();
+
+        app = SugarTest.app;
+        app.data.declareModels();
+        app.routing.start();
+
+        context = app.context.getContext({module: 'OutboundEmail'});
+        context.prepare(true);
+        model = context.get('model');
+
+        sandbox = sinon.sandbox.create();
+    });
+
+    afterEach(function() {
+        sandbox.restore();
+
+        field.dispose();
+        app.cache.cutAll();
+        app.view.reset();
+
+        SugarTest.testMetadata.dispose();
+        Handlebars.templates = {};
+    });
+
+    describe('declaring the help def', function() {
+        it('should set the help def if it is a system account', function() {
+            field = SugarTest.createField({
+                name: 'name',
+                type: 'name',
+                viewName: 'detail',
+                module: 'OutboundEmail',
+                model: model,
+                context: context,
+                loadFromModule: true
+            });
+            field.model.set('type', 'system');
+            field.render();
+
+            expect(field.def.help).not.toBeEmpty();
+        });
+
+        using('non-system accounts', ['system-override', 'user'], function(type) {
+            it('should not set the help def if it is not a system account', function() {
+                field = SugarTest.createField({
+                    name: 'name',
+                    type: 'name',
+                    viewName: 'detail',
+                    module: 'OutboundEmail',
+                    model: model,
+                    context: context,
+                    loadFromModule: true
+                });
+                field.model.set('type', type);
+                field.render();
+
+                expect(field.def.help).toBeUndefined();
+            });
+        });
+    });
+});
