bdb746de345a5742414dd24df8bacd3df98320ee
sparsuram <sparsuram@sugarcrm.com>
2017-05-05 10:59:32 -0700

    ICE-148: shadowUpgrade fails due to missing file Scanner/package-checklist.php (#33984)

--- sugarcrm/modules/UpgradeWizard/pack_shadow.php
+++ sugarcrm/modules/UpgradeWizard/pack_shadow.php
@@ -18,6 +18,7 @@ $files = array(
     'modules/HealthCheck/Scanner/ScannerCli.php' => 'HealthCheck/Scanner/ScannerCli.php',
     'modules/HealthCheck/Scanner/ScannerMeta.php' => 'HealthCheck/Scanner/ScannerMeta.php',
     'modules/HealthCheck/Scanner/removed-php4-constructors.php' => 'HealthCheck/Scanner/removed-php4-constructors.php',
+    'modules/HealthCheck/Scanner/package-checklist.php' => 'HealthCheck/Scanner/package-checklist.php',
     'modules/HealthCheck/HealthCheckClient.php' => 'HealthCheck/HealthCheckClient.php',
     'modules/HealthCheck/HealthCheckHelper.php' => 'HealthCheck/HealthCheckHelper.php',
     'modules/HealthCheck/language/en_us.lang.php' => 'HealthCheck/language/en_us.lang.php'
