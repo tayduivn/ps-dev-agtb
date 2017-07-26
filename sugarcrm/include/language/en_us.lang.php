01635fcf86d96124563f6c5842a060d996cf9254
jbarlowsugar <jbarlow@sugarcrm.com>
2017-07-26 11:06:57 -0400

    SFA-4987: updated subpanel layout for quotes (#34470)

--- sugarcrm/include/language/en_us.lang.php
+++ sugarcrm/include/language/en_us.lang.php
@@ -6095,7 +6095,7 @@ $app_list_strings['moduleListSingular']['KBContentTemplates'] = 'Knowledge Base 
 $app_list_strings['moduleList']['EmbeddedFiles'] = 'Embedded Files';
 $app_list_strings['moduleListSingular']['EmbeddedFiles'] = 'Embedded File';
 
-$app_list_strings['iso_currency_symbol'] = array(
+    $app_list_strings['iso_currency_symbol'] = array(
     'ALL' => 'Lek',
     'USD' => '$',
     'AFN' => '؋',
