5ff430d84fb2a01c52bae449a3cad7392784d1ed
sament-sugarcrm <31704238+sament-sugarcrm@users.noreply.github.com>
2017-10-17 13:53:01 -0700

    ICE-301: Update unit tests in PMSECriteriaEvaluatorTest.php to the new standard (#34972)
    * ICE-301: Update unit tests in PMSECriteriaEvaluatorTest.php to the new standard
    Added new unit test file and updated tested file to support being tested.
    Fixed test run warning and code style errors.
    Fixed code style errors.

---
+++ sugarcrm/tests/unit-php/modules/pmse_Inbox/engine/PMSECriteriaEvaluatorTest.php
@@ -0,0 +1,177 @@
+<?php
+/*
+ * Your installation or use of this SugarCRM file is subject to the applicable
+ * terms available at
+ * http://support.sugarcrm.com/Resources/Master_Subscription_Agreements/.
+ * If you do not agree to all of the applicable terms or do not have the
+ * authority to bind the entity as an authorized representative, then do not
+ * install or use this SugarCRM file.
+ *
+ * Copyright (C) SugarCRM Inc. All rights reserved.
+ */
+
+// This is needed because the class we are testing has a protected field.
+use Sugarcrm\SugarcrmTestsUnit\TestReflection;
+
+/**
+ * @coversDefaultClass \PMSECriteriaEvaluator
+ */
+class PMSECriteriaEvaluatorTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @covers ::isCriteriaToken
+     * @dataProvider isCriteriaTokenProvider
+     */
+    public function testIsCriteriaToken(stdClass $token, $expect)
+    {
+
+        $criteriaEvaluatorMock = $this->getMockBuilder('\PMSECriteriaEvaluator')
+            // The original constructor instantiates the PMSEExpressionEvaluator class by using
+            // ProcessManager\Factory::getPMSEObject. Easiest way to avoid that is not running it.
+            ->disableOriginalConstructor()
+            // We don't need to mock any methods out as other methods of the class aren't called.
+            ->setMethods(null)
+            ->getMock();
+
+        $result = $criteriaEvaluatorMock->isCriteriaToken($token);
+        $this->assertEquals($expect, $result);
+    }
+
+    public function isCriteriaTokenProvider()
+    {
+        $o1 = (object) ['expType' => 'USER_IDENTITY'];
+        $o2 = (object) ['expType' => 'CONSTANT'];
+
+        return [
+            ['token' => $o1, 'expect' => true],
+            ['token' => $o2, 'expect' => false],
+        ];
+    }
+
+    /**
+     * @covers ::evaluateCriteriaToken
+     * @dataProvider evaluateCriteriaTokenSubtypeProvider
+     */
+    public function testEvaluateCriteriaToken(stdClass $expression, $operator, $expect)
+    {
+        // Since the evaluateCriteriaToken method uses the PMSEExpressionEvaluator
+        // class, we mock it out.
+        $expressionEvaluatorMock = $this->createMock('\PMSEExpressionEvaluator');
+
+        $expressionEvaluatorMock->expects($this->once())
+            ->method('routeFunctionOperator')
+            // Note that the getSubtype function is mocked out below this.
+            ->with($operator, $expression->currentValue, $expression->expOperator, $expression->expValue, $expression->expSubtype)
+            // I just return this because the rest of the function doesn't need it and it's easy to deal with.
+            ->willReturn('Goatman');
+
+        $expressionEvaluatorMock->expects($this->once())
+            ->method('processTokenAttributes')
+            ->willReturnArgument(0);
+
+        $mock = $this->getMockBuilder('\PMSECriteriaEvaluator')
+            ->disableOriginalConstructor()
+            // getSubtype is called by evaluateCriteriaToken so we mock it to avoid testing it.
+            ->setMethods(['getSubtype'])
+            ->getMock();
+
+        // Because $expressionEvaluator is protected, we need reflection to set it.
+        TestReflection::setProtectedValue($mock, 'expressionEvaluator', $expressionEvaluatorMock);
+
+        $mock->expects($this->once())
+            ->method('getSubtype')
+            ->with($expression)
+            ->willReturn($expect);
+
+        $mock->evaluateCriteriaToken($expression);
+    }
+
+    public function evaluateCriteriaTokenSubtypeProvider()
+    {
+        $o1 = (object) [
+            'expType' => 'CONTROL',
+            'expSubtype' => 'string',
+            'expLabel' => 'Task # 4 >= Approved',
+            'expOperator' => 'major_equals_than',
+            'expValue' => 'Approved',
+            'currentValue'  => '$expValue',
+            'expField' => '17842861053ee3573bcb7a4046264057',
+        ];
+
+        $o2 = (object) [
+            'expType' => 'CONTROL',
+            'expSubtype' => null,
+            'expLabel' => 'Task # 4 >= Approved',
+            'expOperator' => 'major_equals_than',
+            'expValue' => 'Approved',
+            'currentValue'  => '$expValue',
+            'expField' => '17842861053ee3573bcb7a4046264057',
+        ];
+        return [
+            [
+                'expression' => $o1,
+                'operator' => 'relation',
+                'expect' => 'string',
+            ],
+            [
+                'expression' => $o2,
+                'operator' => 'relation',
+                'expect' => null,
+            ],
+        ];
+    }
+
+    /**
+     * @covers ::evaluateCriteriaTokenList
+     */
+    public function testEvaluateCriteriaTokenList()
+    {
+        // Since we don't actually use evaluateCriteriaToken in a unit test, the
+        // data does not have to be appropriate for it.
+        $expression = [
+            (object)["expType" => "goat1"],
+            (object)["expType" => "goat2"],
+            (object)["expType" => "goat3"],
+            (object)["expType" => "goat4"],
+            (object)["expType" => "goat5"],
+            (object)["expType" => "goat6"],
+        ];
+
+        $criteriaEvaluatorMock = $this->getMockBuilder('PMSECriteriaEvaluator')
+            ->disableOriginalConstructor()
+            // Mock out other functions of the class that are called
+            ->setMethods(['evaluateCriteriaToken', 'isCriteriaToken'])
+            ->getMock();
+
+        $criteriaEvaluatorMock->expects($this->exactly(3))
+            ->method('evaluateCriteriaToken')
+            // I just return this because it's easy to validate.
+            ->willReturn('boggle');
+
+        $criteriaEvaluatorMock->expects($this->exactly(6))
+            ->method('isCriteriaToken')
+            // The order of true/false will effect how the output is ordered because of the mock above.
+            ->will($this->onConsecutiveCalls(true, false, true, true, false, false));
+
+        $expectedList = '[
+                        "boggle",
+                        {
+                            "expType": "goat2"
+                        },
+                        "boggle",
+                        "boggle",
+                        {
+                            "expType": "goat5"
+                        },
+                        {
+                            "expType": "goat6"
+                        }
+                    ]';
+
+        $expectedList = json_decode($expectedList);
+
+        $result = $criteriaEvaluatorMock->evaluateCriteriaTokenList($expression);
+
+        $this->assertEquals($expectedList, $result);
+    }
+}
