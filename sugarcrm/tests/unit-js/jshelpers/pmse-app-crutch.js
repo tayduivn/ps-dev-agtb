c7a7e2a1eb1c65c3c7efab0d255d1ee5e1734125
Robert Gonzalez <rgonzalez@sugarcrm.com>
2017-10-20 07:14:06 -0700

    ICE-322: pmse-app-cruthc.js broke CI Add license header since it was missed during code review

--- sugarcrm/tests/unit-js/jshelpers/pmse-app-crutch.js
+++ sugarcrm/tests/unit-js/jshelpers/pmse-app-crutch.js
@@ -1,2 +1,12 @@
+/*
+ * Your installation or use of this SugarCRM file is subject to the applicable
+ * terms available at
+ * http://support.sugarcrm.com/Resources/Master_Subscription_Agreements/.
+ * If you do not agree to all of the applicable terms or do not have the
+ * authority to bind the entity as an authorized representative, then do not
+ * install or use this SugarCRM file.
+ *
+ * Copyright (C) SugarCRM Inc. All rights reserved.
+ */
 // Because the includes/javascript/pmse stuff just NEEEEEEEEEEDS this to exist
 var App = App || SUGAR.App;
