88804eabcacfee4db3ff0cbc782a7b2f5768222c
Greg Levine <levine.greg@gmail.com>
2018-01-02 12:38:07 -0500

    [MAR-4894] Fixed Emails subpanel on Contacts records
    - The subpanel metadata now matches the other subpanel-for-*-archived-emails views.
    - The columns are From, Subject, Status (state), Date (date_sent), Assigned To, and Related To.
    - The subject is now linked.
    - The paper clip icon now appears for emails with attachments.
    - The labels have been corrected.
    - The code has been reformatted.

--- sugarcrm/modules/Emails/clients/base/views/subpanel-for-contacts-archived-emails/subpanel-for-contacts-archived-emails.php
+++ sugarcrm/modules/Emails/clients/base/views/subpanel-for-contacts-archived-emails/subpanel-for-contacts-archived-emails.php
@@ -10,62 +10,77 @@
  * Copyright (C) SugarCRM Inc. All rights reserved.
  */
 $viewdefs['Emails']['base']['view']['subpanel-for-contacts-archived-emails'] = array(
-  'type' => 'subpanel-list',
-  'panels' =>
-  array(
-    array(
-      'name' => 'panel_header',
-      'label' => 'LBL_PANEL_1',
-      'fields' =>
-      array(
+    'type' => 'subpanel-list',
+    'panels' => array(
         array(
-          'label' => 'LBL_LIST_SUBJECT',
-          'enabled' => true,
-          'default' => true,
-          'name' => 'name',
+            'name' => 'panel_header',
+            'label' => 'LBL_PANEL_1',
+            'fields' => array(
+                array(
+                    'name' => 'from_collection',
+                    'type' => 'from',
+                    'label' => 'LBL_LIST_FROM_ADDR',
+                    'enabled' => true,
+                    'default' => true,
+                    'readonly' => true,
+                    'fields' => array(
+                        'email_address_id',
+                        'email_address',
+                        'parent_type',
+                        'parent_id',
+                        'parent_name',
+                    ),
+                ),
+                array(
+                    'name' => 'name',
+                    'enabled' => true,
+                    'default' => true,
+                    'link' => 'true',
+                    'related_fields' => array(
+                        'total_attachments',
+                        'state',
+                    ),
+                ),
+                array(
+                    'name' => 'state',
+                    'label' => 'LBL_LIST_STATUS',
+                    'enabled' => true,
+                    'default' => true,
+                ),
+                array(
+                    'name' => 'date_sent',
+                    'label' => 'LBL_LIST_DATE_COLUMN',
+                    'enabled' => true,
+                    'default' => true,
+                ),
+                array(
+                    'name' => 'assigned_user_name',
+                    'target_record_key' => 'assigned_user_id',
+                    'target_module' => 'Employees',
+                    'enabled' => true,
+                    'default' => true,
+                ),
+                array(
+                    'name' => 'parent_name',
+                    'link' => true,
+                    'enabled' => true,
+                    'default' => true,
+                    'sortable' => false,
+                ),
+            ),
         ),
-        array(
-          'label' => 'LBL_LIST_STATUS',
-          'enabled' => true,
-          'default' => true,
-          'name' => 'state',
-        ),
-        array(
-          'label' => 'LBL_DATE_CREATED',
-          'enabled' => true,
-          'default' => true,
-          'name' => 'date_entered',
-          'readonly' => true,
-        ),
-        array(
-          'label' => 'LBL_DATE_MODIFIED',
-          'enabled' => true,
-          'default' => true,
-          'name' => 'date_modified',
-          'readonly' => true,
-        ),
-        array(
-          'name' => 'assigned_user_name',
-          'target_record_key' => 'assigned_user_id',
-          'target_module' => 'Employees',
-          'label' => 'LBL_LIST_ASSIGNED_TO_NAME',
-          'enabled' => true,
-          'default' => true,
-        ),
-      ),
     ),
-  ),
-  'rowactions' => array(
-    'actions' => array(
-      array(
-        'type' => 'rowaction',
-        'css_class' => 'btn',
-        'tooltip' => 'LBL_PREVIEW',
-        'event' => 'list:preview:fire',
-        'icon' => 'fa-eye',
-        'acl_action' => 'view',
-        'allow_bwc' => false,
-      ),
+    'rowactions' => array(
+        'actions' => array(
+            array(
+                'type' => 'rowaction',
+                'css_class' => 'btn',
+                'tooltip' => 'LBL_PREVIEW',
+                'event' => 'list:preview:fire',
+                'icon' => 'fa-eye',
+                'acl_action' => 'view',
+                'allow_bwc' => false,
+            ),
+        ),
     ),
-  ),
 );
