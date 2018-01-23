e3c394c100a5b7abfd4ab55514b22ae7e1311678
John Barlow <jbarlow@sugarcrm.com>
2018-01-23 11:38:43 -0500

    SFA-5224: Sanatized TinyMCE output for Smarty.

---
+++ sugarcrm/tests/unit-php/modules/PdfManager/PdfManagerHooksTest.php
@@ -0,0 +1,48 @@
+<?php
+/*
+ * Your installation or use of this SugarCRM file is subject to the applicable
+ * terms available at
+ * http://support.sugarcrm.com/Resources/Master_Subscription_Agreements/.
+ * If you do not agree to all of the applicable terms or do not have the
+ * authority to bind the entity as an authorized representative, then do not
+ * install or use this SugarCRM file.
+ *
+ * Copyright (C) SugarCRM Inc. All rights reserved.
+ */
+namespace Sugarcrm\SugarcrmTestUnit\modules\PdfManager;
+
+/**
+ * @coversDefaultClass \PdfManagerHooks
+ */
+class PdfManagerHooksTest extends \PHPUnit_Framework_TestCase
+{
+    public function setup()
+    {
+        \SugarAutoLoader::load('../../modules/PdfManager/PdfManagerHooks.php');
+        \SugarAutoLoader::load('../../modules/PdfManager/PdfManager.php');
+        parent::setup();
+    }
+
+    /**
+     * @covers fixAmp
+     *
+     * @return void
+     */
+    public function testFixAmp()
+    {
+        $pdfManagerMock = $this->getMockBuilder('\PdfManager')
+        ->setMethods(null)
+        ->disableOriginalConstructor()
+        ->getMock();
+
+        $pdfManagerHooksMock = $this->getMockBuilder('\PdfManagerHooks')
+        ->setMethods(null)
+        ->disableOriginalConstructor()
+        ->getMock();
+
+        $pdfManagerMock->body_html = 'foo&amp;bar';
+
+        $pdfManagerHooksMock->fixAmp($pdfManagerMock, 'before_save');
+        $this->assertEquals('foo&bar', $pdfManagerMock->body_html);
+    }
+}
