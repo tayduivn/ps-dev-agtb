790799ae763a5ea6ae657b83db19da77a7e2d54c
Bob Wombat Hogg <rhogg@sugarcrm.com>
2018-03-09 17:46:05 -0800

    PX-61: Restore LBL_EMAIL_ADDRESS_OPTED_OUT
    We accidentally deleted this while resolving a merge conflict on TY-2167.
    Sorry!
    
    Signed-off-by: Bob Wombat Hogg <rhogg@sugarcrm.com>

--- sugarcrm/include/language/en_us.lang.php
+++ sugarcrm/include/language/en_us.lang.php
@@ -5371,6 +5371,8 @@ To create a new record using the values you have entered, click "Ignore Duplicat
     'LBL_EMAIL_PARTICIPANTS' => 'Email Participants',
     'LBL_EMAIL_PARTICIPANT' => 'Email Participant',
 
+    'LBL_EMAIL_ADDRESS_OPTED_OUT' => 'Opted out of email campaigns',
+
     'LBL_DATAPRIVACY' => 'Data Privacy',
     'LBL_DATAPRIVACY_SUBPANEL_TITLE' => 'Data Privacy',
     'LBL_DATAPRIVACY_VIEW_PII' => 'View Personal Info',
