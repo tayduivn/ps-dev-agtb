3809927ebd7f7afa691fcd05ff0998532428a9ee
Keshav Adunuthula <kadunuthula@sugarcrm.com>
2018-03-15 12:00:48 -0400

    SFA-5288 code standards met

--- sugarcrm/modules/RevenueLineItems/clients/base/fields/relate/relate.js
+++ sugarcrm/modules/RevenueLineItems/clients/base/fields/relate/relate.js
@@ -20,7 +20,7 @@
     /**
      * @inheritdoc
      */
-    initialize: function (options) {
+    initialize: function(options) {
         // deleting for RLI create when there is no account_id.
         if (options && options.def.filter_relate && !options.model.has('account_id')) {
             delete options.def.filter_relate;
