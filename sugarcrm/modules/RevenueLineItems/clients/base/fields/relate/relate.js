ee84db657d3e3542590a6e270dfbbcafc0ca582c
Keshav Adunuthula <kadunuthula@sugarcrm.com>
2018-03-15 11:54:36 -0400

    SFA-5288 code standards met

--- sugarcrm/modules/RevenueLineItems/clients/base/fields/relate/relate.js
+++ sugarcrm/modules/RevenueLineItems/clients/base/fields/relate/relate.js
@@ -1,3 +1,19 @@
+/*
+ * Your installation or use of this SugarCRM file is subject to the applicable
+ * terms available at
+ * http://support.sugarcrm.com/Resources/Master_Subscription_Agreements/.
+ * If you do not agree to all of the applicable terms or do not have the
+ * authority to bind the entity as an authorized representative, then do not
+ * install or use this SugarCRM file.
+ *
+ * Copyright (C) SugarCRM Inc. All rights reserved.
+ */
+
+/**
+ * @class View.Fields.Base.RevenueLineItems.RelateField
+ * @alias SUGAR.App.view.fields.BaseRevenueLineItemsRelateField
+ * @extends View.Fields.Base.RelateField
+ */
 ({
     extendsFrom: 'BaseRelateField',
 
@@ -5,7 +21,7 @@
      * @inheritdoc
      */
     initialize: function (options) {
-        // deleting for RLI create when there is no account_id.      
+        // deleting for RLI create when there is no account_id.
         if (options && options.def.filter_relate && !options.model.has('account_id')) {
             delete options.def.filter_relate;
         }
