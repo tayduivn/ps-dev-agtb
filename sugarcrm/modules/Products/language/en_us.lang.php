743f2056e3bf41a8a6887b325edc61e43f1b1636
Travis Hubbard <thubbard@sugarcrm.com>
2018-03-22 10:28:54 -0400

    SFA-4790 changed discount amount to type on Product record

--- sugarcrm/modules/Products/language/en_us.lang.php
+++ sugarcrm/modules/Products/language/en_us.lang.php
@@ -175,4 +175,5 @@ To create a {{module_name}}:
     'LBL_PRODUCTS_QUOTE_DATA_LIST' => 'Quoted Line Items Quote Data List',
     'LBL_CREATE_NEW_QLI_IN_DROPDOWN' => ' (New Quoted Line Item)',
     'LBL_DISCOUNT_PERCENT' => '% Percent',
+    'LBL_DISCOUNT_TYPE' => 'Discount Type',
 );
