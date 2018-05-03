0301b6681fc8693ade966d153b58bc0836489fc0
Jimmy Chen <jchen@sugarcrm.com>
2018-05-03 14:35:33 -0700

    RPT-643 Adding users to a report schedule

--- sugarcrm/modules/ReportSchedules/clients/base/layouts/subpanels/subpanels.php
+++ sugarcrm/modules/ReportSchedules/clients/base/layouts/subpanels/subpanels.php
@@ -14,6 +14,8 @@ $viewdefs['ReportSchedules']['base']['layout']['subpanels'] = array (
         array(
             'layout' => 'subpanel',
             'label' => 'LBL_USERS_SUBPANEL_TITLE',
+            'override_subpanel_list_view' => 'subpanel-for-reportschedules',
+            'override_paneltop_view' => 'panel-top-for-reportschedules',
             'context' => array(
                 'link' => 'users',
             ),
