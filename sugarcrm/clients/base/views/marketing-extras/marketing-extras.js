dda782fcfed2b1e172ee8f68002163de37640363
Yuyi Li <yli@sugarcrm.com>
2018-05-29 08:48:46 -0700

    PX-179: Marketing content, when provided, should appear on the Sugar login page Add controller logic to fetch marketing contents with selected language if available

---
+++ sugarcrm/clients/base/views/marketing-extras/marketing-extras.js
@@ -0,0 +1,45 @@
+/*
+ * Your installation or use of this SugarCRM file is subject to the applicable
+ * terms available at
+ * http://support.sugarcrm.com/Resources/Master_Subscription_Agreements/.
+ * If you do not agree to all of the applicable terms or do not have the
+ * authority to bind the entity as an authorized representative, then do not
+ * install or use this SugarCRM file.
+ *
+ * Copyright (C) SugarCRM Inc. All rights reserved.
+ */
+/**
+ * @class View.Views.Base.MarketingExtrasView
+ * @alias SUGAR.App.view.views.BaseMarketingExtrasView
+ * @extends View.View
+ */
+({
+    marketingContentUrl: '',
+
+    /**
+     * @inheritdoc
+     */
+    initialize: function(options) {
+        this._super('initialize', [options]);
+        this.fetchMarketingExtras();
+    },
+
+    /**
+     * Retrieve marketing extras URL from login content endpoint
+     */
+    fetchMarketingExtras() {
+        var config = app.metadata.getConfig();
+        if (config.marketingExtrasEnabled) {
+            var language = app.user.getLanguage();
+            var url = app.api.buildURL('/login/content', null, null, {selected_language: language});
+            app.api.call('read', url, null, {
+                success: _.bind(function(contents) {
+                    if (contents && !_.isEmpty(contents.content_url)) {
+                        this.marketingContentUrl = contents.content_url;
+                        this.render();
+                    }
+                }, this)
+            });
+        }
+    },
+})
