3da3ed2538bc2839844fd7671441bfbd014f9020
Robert Gonzalez <rgonzalez@sugarcrm.com>
2018-06-03 23:19:35 -0700

    Revert "BR-4247 Fixed cache dir being prepended twice."
    This reverts commit 7e7079b10783b45f7f40d0cc9860a985b657b360.

--- sugarcrm/include/SugarQueue/SugarCronJobs.php
+++ sugarcrm/include/SugarQueue/SugarCronJobs.php
@@ -94,7 +94,7 @@ class SugarCronJobs
      */
     protected function markLastRun()
     {
-        if (!sugar_file_put_contents($this->lockfile, time())) {
+        if(!file_put_contents($this->lockfile, time())) {
             $GLOBALS['log']->fatal('Scheduler cannot write PID file.  Please check permissions on '.$this->lockfile);
         }
     }
@@ -108,6 +108,7 @@ class SugarCronJobs
         if($this->min_interval == 0) {
             return true;
         }
+        create_cache_directory($this->lockfile);
         if(!file_exists($this->lockfile)) {
             $this->markLastRun();
             return true;
