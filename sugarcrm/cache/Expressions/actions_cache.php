0b5c73718f5c1fca4a19fb5c1bab8d7ab7bcfee7
Prashanth Koushik <pkoushik@sugarcrm.com>
2018-06-29 12:53:43 -0700

    SC-5938: Revertable commit

---
+++ sugarcrm/cache/Expressions/actions_cache.php
@@ -0,0 +1,44 @@
+<?php
+// created: 2018-06-28 22:36:45
+$actions = array (
+  'SetRequired' => 
+  array (
+    'class' => 'SetRequiredAction',
+    'file' => 'include/Expressions/Actions/SetRequiredAction.php',
+  ),
+  'Style' => 
+  array (
+    'class' => 'StyleAction',
+    'file' => 'include/Expressions/Actions/StyleAction.php',
+  ),
+  'SetVisibility' => 
+  array (
+    'class' => 'VisibilityAction',
+    'file' => 'include/Expressions/Actions/VisibilityAction.php',
+  ),
+  'AssignTo' => 
+  array (
+    'class' => 'AssignToAction',
+    'file' => 'include/Expressions/Actions/AssignToAction.php',
+  ),
+  'SetOptions' => 
+  array (
+    'class' => 'SetOptionsAction',
+    'file' => 'include/Expressions/Actions/SetOptionsAction.php',
+  ),
+  'ReadOnly' => 
+  array (
+    'class' => 'ReadOnlyAction',
+    'file' => 'include/Expressions/Actions/ReadOnlyAction.php',
+  ),
+  'SetPanelVisibility' => 
+  array (
+    'class' => 'PanelVisibilityAction',
+    'file' => 'include/Expressions/Actions/PanelVisibilityAction.php',
+  ),
+  'SetValue' => 
+  array (
+    'class' => 'SetValueAction',
+    'file' => 'include/Expressions/Actions/SetValueAction.php',
+  ),
+);
