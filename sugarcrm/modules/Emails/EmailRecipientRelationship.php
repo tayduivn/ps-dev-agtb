bd91c78440f903e39bf061660106098175c8a203
Sergey Morozov <smorozov@sugarcrm.com>
2018-10-05 13:47:42 -0700

    [BR-5260] Fixed method signature compatibility in One2MBeanRelationship hierarchy
    See https://3v4l.org/uZu1U

--- sugarcrm/modules/Emails/EmailRecipientRelationship.php
+++ sugarcrm/modules/Emails/EmailRecipientRelationship.php
@@ -140,7 +140,7 @@ class EmailRecipientRelationship extends One2MBeanRelationship
      * {@inheritdoc}
      * @throws SugarApiExceptionNotAuthorized
      */
-    public function remove($lhs, $rhs)
+    public function remove($lhs, $rhs, $save = true)
     {
         if ($lhs->isArchived() && !$rhs->deleted && !$lhs->deleted) {
             throw new SugarApiExceptionNotAuthorized("Cannot remove from {$this->name} when the email is archived");
