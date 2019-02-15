53813314b877f630684a427268e9c9181dfcbbd7
Sergey Morozov <smorozov@sugarcrm.com>
2019-02-14 19:06:53 -0800

    [BR-6773] sugar_file_put_contents_atomic() sets incorrect file permissions on Windows

---
+++ sugarcrm/tests/{old}/include/utils/FilePutContentsAtomicTest.php
@@ -0,0 +1,47 @@
+<?php
+/*
+ * Your installation or use of this SugarCRM file is subject to the applicable
+ * terms available at
+ * http://support.sugarcrm.com/Resources/Master_Subscription_Agreements/.
+ * If you do not agree to all of the applicable terms or do not have the
+ * authority to bind the entity as an authorized representative, then do not
+ * install or use this SugarCRM file.
+ *
+ * Copyright (C) SugarCRM Inc. All rights reserved.
+ */
+
+use PHPUnit\Framework\TestCase;
+use Sugarcrm\Sugarcrm\Util\Uuid;
+
+require_once 'include/utils/file_utils.php';
+
+class FilePutContentsAtomicTest extends TestCase
+{
+    public function setUp()
+    {
+        SugarTestHelper::setUpFiles();
+    }
+
+    /**
+     * @test
+     */
+    public function fileIsReadableByWebServer()
+    {
+        global $sugar_config;
+
+        $uuid = Uuid::uuid4();
+
+        $path = sprintf('%s/%s.php', SUGAR_BASE_DIR, $uuid);
+        SugarTestHelper::saveFile($path);
+
+        sugar_file_put_contents_atomic($path, <<<'PHP'
+<?php
+
+echo 'Test';
+PHP
+        );
+
+        $url = sprintf('%s/%s.php', $sugar_config['site_url'], $uuid);
+        $this->assertEquals('Test', file_get_contents($url));
+    }
+}
