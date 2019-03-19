bd66c8b5a0a84ba2c70b468d60978efb208d39ee
Jianzhong Zhu <jzhu@sugarcrm.com>
2019-03-19 09:31:46 -0700

    BR-6855 ElasticSearch Indexing does not use index on FTS_QUEUE table. added bean_id index to fts_queue table, since SELECT and DELETE operations are both using bean_id. It's more favorable than fixing SELECT stmt using bean_id and bean_module.

--- sugarcrm/metadata/fts_queueMetaData.php
+++ sugarcrm/metadata/fts_queueMetaData.php
@@ -55,11 +55,26 @@ $dictionary['fts_queue'] = array(
             ),
         ),
         array(
+            'name' => 'idx_bean_id',
+            'type' => 'index',
+            'fields' => array(
+                'bean_id',
+            ),
+        ),
+        array(
             'name' => 'idx_beans_bean_id',
             'type' => 'index',
             'fields' => array(
                 'bean_module',
                 'bean_id',
+            ),
+        ),
+        array(
+            'name' => 'idx_bean_id_processed',
+            'type' => 'index',
+            'fields' => array(
+                'bean_id',
+                'processed',
             ),
         ),
         array(
