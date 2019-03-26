0257aef02339bf4db8a068026384412294f1ed7c
Alex Tatulchenkov <atatulchenkov@sugarcrm.com>
2019-03-26 17:12:26 +0300

    BR-6746: (SI 82121) PHP object injection in ext4 parameter of RefreshField action in ModuleBuilder module

--- sugarcrm/modules/DynamicFields/templates/Fields/TemplateEnum.php
+++ sugarcrm/modules/DynamicFields/templates/Fields/TemplateEnum.php
@@ -140,8 +140,11 @@ class TemplateEnum extends TemplateText{
 		$def['default'] = !empty($this->default) ? $this->default : $this->default_value;
 		$def['len'] = $this->max_size;
 		// this class may be extended, so only do the unserialize for genuine TemplateEnums
-		if (get_class( $this ) == 'TemplateEnum' && empty($def['dependency']) )
-			$def['dependency'] = isset($this->ext4)? @unserialize(html_entity_decode($this->ext4)) : null ;
+        if (get_class($this) == 'TemplateEnum' && empty($def['dependency'])) {
+            $def['dependency'] = isset($this->ext4) ?
+                @unserialize(html_entity_decode($this->ext4), ['allowed_classes' => false])
+                : null;
+        }
         if (!empty($this->visibility_grid))
             $def['visibility_grid'] = $this->visibility_grid;
 
