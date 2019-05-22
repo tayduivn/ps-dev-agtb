9f570aa577f8b7bc47589aa735e55d392fc13c6b
Vishal Jain <vjain@sugarcrm.com>
2019-05-22 16:56:42 -0700

    CS-140: Update cases recorddashlet metadata

--- sugarcrm/modules/Cases/clients/base/views/recorddashlet/recorddashlet.php
+++ sugarcrm/modules/Cases/clients/base/views/recorddashlet/recorddashlet.php
@@ -1,5 +1,5 @@
 <?php
-
+//FILE SUGARCRM flav=ent ONLY
 
 /*
  * Your installation or use of this SugarCRM file is subject to the applicable
@@ -12,19 +12,19 @@
  * Copyright (C) SugarCRM Inc. All rights reserved.
  */
 
-$viewdefs['Cases']['base']['view']['recorddashlet'] = array(
-    'buttons' => array(
-        array(
+$viewdefs['Cases']['base']['view']['recorddashlet'] = [
+    'buttons' => [
+        [
             'type' => 'button',
             'name' => 'cancel_button',
             'label' => 'LBL_CANCEL_BUTTON_LABEL',
             'css_class' => 'btn-invisible btn-link',
             'showOn' => 'edit',
-            'events' => array(
+            'events' => [
                 'click' => 'button:cancel_button:click',
-            ),
-        ),
-        array(
+            ],
+        ],
+        [
             'type' => 'rowaction',
             'event' => 'button:save_button:click',
             'name' => 'save_button',
@@ -32,219 +32,121 @@ $viewdefs['Cases']['base']['view']['recorddashlet'] = array(
             'css_class' => 'btn btn-primary',
             'showOn' => 'edit',
             'acl_action' => 'edit',
-        ),
-
-        array(
+        ],
+        [
             'type' => 'actiondropdown',
             'name' => 'main_dropdown',
             'primary' => true,
             'showOn' => 'view',
-            'buttons' => array(
-                array(
+            'buttons' => [
+                [
                     'type' => 'rowaction',
                     'event' => 'button:edit_button:click',
                     'name' => 'edit_button',
                     'label' => 'LBL_EDIT_BUTTON_LABEL',
                     'primary' => true,
                     'acl_action' => 'edit',
-                ),
-                array(
-                    'type' => 'shareaction',
-                    'name' => 'share',
-                    'label' => 'LBL_RECORD_SHARE_BUTTON',
-                    'acl_action' => 'view',
-                ),
-                array(
-                    'type' => 'pdfaction',
-                    'name' => 'download-pdf',
-                    'label' => 'LBL_PDF_VIEW',
-                    'action' => 'download',
-                    'acl_action' => 'view',
-                ),
-                array(
-                    'type' => 'pdfaction',
-                    'name' => 'email-pdf',
-                    'label' => 'LBL_PDF_EMAIL',
-                    'action' => 'email',
-                    'acl_action' => 'view',
-                ),
-                array(
-                    'type' => 'divider',
-                ),
-                array(
-                    'name' => 'create_button',
-                    'type' => 'rowaction',
-                    'event' => 'button:create_article_button:click',
-                    'label' => 'LBL_CREATE_KB_DOCUMENT',
-                    'acl_module' => 'KBContents',
-                    'acl_action' => 'create',
-                ),
-                array(
-                    'type' => 'divider',
-                ),
-                array(
-                    'type' => 'rowaction',
-                    'event' => 'button:find_duplicates_button:click',
-                    'name' => 'find_duplicates_button',
-                    'label' => 'LBL_DUP_MERGE',
-                    'acl_action' => 'edit',
-                ),
-                array(
-                    'type' => 'rowaction',
-                    'event' => 'button:duplicate_button:click',
-                    'name' => 'duplicate_button',
-                    'label' => 'LBL_DUPLICATE_BUTTON_LABEL',
-                    'acl_module' => 'Cases',
-                    'acl_action' => 'create',
-                ),
-                array(
-                    'type' => 'rowaction',
-                    'event' => 'button:historical_summary_button:click',
-                    'name' => 'historical_summary_button',
-                    'label' => 'LBL_HISTORICAL_SUMMARY',
-                    'acl_action' => 'view',
-                ),
-                array(
-                    'type' => 'rowaction',
-                    'event' => 'button:audit_button:click',
-                    'name' => 'audit_button',
-                    'label' => 'LNK_VIEW_CHANGE_LOG',
-                    'acl_action' => 'view',
-                ),
-                array(
-                    'type' => 'divider',
-                ),
-                array(
-                    'type' => 'rowaction',
-                    'event' => 'button:delete_button:click',
-                    'name' => 'delete_button',
-                    'label' => 'LBL_DELETE_BUTTON_LABEL',
-                    'acl_action' => 'delete',
-                ),
-            ),
-        ),
-        array(
-            'name' => 'sidebar_toggle',
-            'type' => 'sidebartoggle',
-        ),
-    ),
-    'panels' => array(
-        array(
+                ],
+            ],
+        ],
+    ],
+    'panels' => [
+        [
             'name' => 'panel_header',
             'header' => true,
-            'fields' => array(
-                array(
+            'fields' => [
+                [
                     'name'          => 'picture',
                     'type'          => 'avatar',
                     'size'          => 'large',
                     'dismiss_label' => true,
                     'readonly'      => true,
-                ),
+                ],
                 'name',
-                array(
-                    'name' => 'favorite',
-                    'label' => 'LBL_FAVORITE',
-                    'type' => 'favorite',
-                    'dismiss_label' => true,
-                ),
-                array(
-                    'name' => 'follow',
-                    'label'=> 'LBL_FOLLOW',
-                    'type' => 'follow',
-                    'readonly' => true,
-                    'dismiss_label' => true,
-                ),
-            ),
-        ),
-        array(
+            ],
+        ],
+        [
             'name' => 'panel_body',
             'label' => 'LBL_RECORD_BODY',
             'columns' => 2,
             'labelsOnTop' => true,
             'placeholders' => true,
-            'fields' => array(
-                array(
+            'fields' => [
+                [
                     'name' => 'case_number',
                     'readonly' => true,
-                ),
+                ],
                 'priority',
                 'account_name',
                 'portal_viewable',
                 'type',
                 'source',
                 'status',
-                'follow_up_datetime',
                 'assigned_user_name',
-                array(
+                [
                     'name' => 'description',
                     'nl2br' => true,
                     'span' => 12,
-                ),
-                array(
-                    'name' => 'commentlog',
-                    'label' => 'LBL_COMMENTLOG',
-                    'span' => 12,
-                ),
-                array(
-                    'name' => 'tag',
-                    'span' => 12,
-                ),
-            ),
-        ),
-        array(
+                ],
+                'tag',
+                'business_center_name',
+            ],
+        ],
+        [
             'name' => 'panel_hidden',
             'label' => 'LBL_RECORD_SHOWMORE',
             'hide' => true,
             'columns' => 2,
             'labelsOnTop' => true,
             'placeholders' => true,
-            'fields' => array(
-                array(
-                    'name' => 'resolution',
-                    'nl2br' => true,
-                    'span' => 12,
-                ),
-                array(),
-                array(
+            'fields' => [
+                'follow_up_datetime',
+                'resolved_datetime',
+                'resolution',
+                'team_name',
+                [],
+                [],
+                [
                     'name' => 'date_entered_by',
                     'readonly' => true,
                     'inline' => true,
                     'type' => 'fieldset',
                     'label' => 'LBL_DATE_ENTERED',
-                    'fields' => array(
-                        array(
+                    'span' => 12,
+                    'fields' => [
+                        [
                             'name' => 'date_entered',
-                        ),
-                        array(
+                        ],
+                        [
                             'type' => 'label',
                             'default_value' => 'LBL_BY',
-                        ),
-                        array(
+                        ],
+                        [
                             'name' => 'created_by_name',
-                        ),
-                    ),
-                ),
-                'team_name',
-                array(
+                        ],
+                    ],
+                ],
+                [
                     'name' => 'date_modified_by',
                     'readonly' => true,
                     'inline' => true,
                     'type' => 'fieldset',
                     'label' => 'LBL_DATE_MODIFIED',
-                    'fields' => array(
-                        array(
+                    'span' => 12,
+                    'fields' => [
+                        [
                             'name' => 'date_modified',
-                        ),
-                        array(
+                        ],
+                        [
                             'type' => 'label',
                             'default_value' => 'LBL_BY',
-                        ),
-                        array(
+                        ],
+                        [
                             'name' => 'modified_by_name',
-                        ),
-                    ),
-                ),
-            ),
-        ),
-    ),
-);
+                        ],
+                    ],
+                ],
+            ],
+        ],
+    ],
+];
