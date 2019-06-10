cf827baa5aa8d1d8cb172d79b84958ab278f18bb
Alex Tatulchenkov <atatulchenkov@sugarcrm.com>
2019-06-10 11:36:11 +0300

    BR-6911: Possible Second Order SQL Injection on /modules/UpgradeWizard/uw_utils.php

--- build/scripts_for_patch/pre_install.php
+++
@@ -1,4 +0,0 @@
-<?php
-echo "This version of SugarCRM requires use of the new upgrader script. Sugar 6.x upgrader can not be used to upgrade to this version.
-Please contact SugarCRM support if you need more information.\n";
-exit(1);
