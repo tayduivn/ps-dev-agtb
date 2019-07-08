aa86369b821bd7f4d565d4868d6359f866a41bbf
Portik Norbert <nportik@sugarcrm.com>
2019-07-08 13:21:25 +0300

    CS-182: Replace portal search api for list view to use filterapi

--- sugarcrm/clients/portal/views/portal-list-top/portal-list-top.js
+++ sugarcrm/clients/portal/views/portal-list-top/portal-list-top.js
@@ -8,23 +8,12 @@
  *
  * Copyright (C) SugarCRM Inc. All rights reserved.
  */
+
 ({
-
-    events: {
-        'click .search': 'showSearch'
-    },
-
     _renderHtml: function() {
         if (app.acl.hasAccess('create', this.module)) {
             this.context.set('isCreateEnabled', true);
         }
         app.view.View.prototype._renderHtml.call(this);
     },
-
-    showSearch: function() {
-        // Toggle on search filter and off the pagination buttons
-        this.$('.search').toggleClass('active');
-        this.layout.trigger("list:search:toggle");
-    }
-
-})
+});
