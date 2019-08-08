044bc9eae3ff9f48a065f067f69f398fc335fcda
Norbert <nronai@sugarcrm.com>
2019-08-08 20:33:00 +0300

    CS-285 Added KB dashlet to Portal home

--- sugarcrm/modules/Home/dashboards/portal-home/portal-home.php
+++ sugarcrm/modules/Home/dashboards/portal-home/portal-home.php
@@ -57,6 +57,25 @@ return [
                             'width' => 12,
                         ],
                     ],
+                    [
+                        [
+                            'view' => [
+                                'type' => 'dashlet-nestedset-list',
+                                'label' => 'LBL_DASHLET_CATEGORIES_NAME',
+                                'data_provider' => 'Categories',
+                                'config_provider' => 'KBContents',
+                                'root_name' => 'category_root',
+                                'extra_provider' => [
+                                    'module' => 'KBContents',
+                                    'field' => 'category_id',
+                                ],
+                            ],
+                            'context' => [
+                                'module' => 'KBContents',
+                            ],
+                            'width' => 12,
+                        ],
+                    ],
                 ],
                 'width' => 12,
             ],
