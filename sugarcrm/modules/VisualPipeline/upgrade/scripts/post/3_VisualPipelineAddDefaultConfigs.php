9cca2a15db016a84d36588b4d1f1d18c4dcb9ffc
Piyush Mishra <pmishra@sugarcrm.com>
2020-02-11 15:44:48 -0500

    SS-236: Adding upgrade scripts to add availableColumns to VP defaults

---
+++ sugarcrm/modules/VisualPipeline/upgrade/scripts/post/3_VisualPipelineAddDefaultConfigs.php
@@ -0,0 +1,33 @@
+<?php
+
+/*
+ * Your installation or use of this SugarCRM file is subject to the applicable
+ * terms available at
+ * http://support.sugarcrm.com/Resources/Master_Subscription_Agreements/.
+ * If you do not agree to all of the applicable terms or do not have the
+ * authority to bind the entity as an authorized representative, then do not
+ * install or use this SugarCRM file.
+ *
+ * Copyright (C) SugarCRM Inc. All rights reserved.
+ */
+
+class SugarUpgradeVisualPipelineAddDefaultConfigs extends UpgradeScript
+{
+    public $order = 3100;
+    public $version = '9.3.0';
+    public $type = self::UPGRADE_CUSTOM;
+
+    public function run()
+    {
+        if ($this->shouldInstallPipelineDefaults()) {
+            VisualPipelineDefaults::setupPipelineSettings();
+        }
+    }
+
+    public function shouldInstallPipelineDefaults()
+    {
+        $isConversion = !$this->fromFlavor('ent') && $this->toFlavor('ent');
+        $isAt93Ent = $this->toFlavor('ent') && version_compare($this->from_version, '9.3.0', '=');
+        return $isConversion || $isAt93Ent;
+    }
+}
