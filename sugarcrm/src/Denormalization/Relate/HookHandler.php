69dc78355e8338564a2871d5fad732744095a0c5
Dmitri Pluschaev <dpluschaev@sugarcrm.com>
2020-03-18 15:59:58 +0300

    BR-7447 Denormalization framework is using default Administration settings interface and cache that can cause issues in some cases. Please implement its own settings access interface.

--- sugarcrm/src/Denormalization/Relate/HookHandler.php
+++ sugarcrm/src/Denormalization/Relate/HookHandler.php
@@ -12,7 +12,6 @@
 
 namespace Sugarcrm\Sugarcrm\Denormalization\Relate;
 
-use Administration;
 use SugarBean;
 use Sugarcrm\Sugarcrm\Denormalization\Relate\Db\Db;
 use Sugarcrm\Sugarcrm\Denormalization\Relate\Db\OnlineOperations;
@@ -30,9 +29,13 @@ final class HookHandler
     /** @var OnlineOperations */
     private $db;
 
+    /** @var HookConfig */
+    private $config;
+
     public function __construct()
     {
         $this->db = Db::getInstance();
+        $this->config = new HookConfig();
     }
 
     /**
@@ -178,69 +181,13 @@ final class HookHandler
         }
     }
 
-    /**
-     * Used in tests
-     */
-    public static function clearCache(): void
-    {
-        self::$settingsCache = null;
-    }
-
     private function getSettings($bean): array
     {
         if (!$bean instanceof SugarBean) {
             return [];
         }
 
-        if (is_null(self::$settingsCache)) {
-            self::$settingsCache = Administration::getSettings('denormalization')->settings['denormalization_fields']
-                ?? [];
-
-            // special case:
-            // APCu cache for the web server and the CLI environments are isolated and
-            // there is no way for Denormalization Job to invalidate Administration::$settings cache.
-            // So the solution for the web server process is to confirm that a module is in sync.
-            $this->checkSynchronizationStatus($bean->getModuleName());
-        }
-
-        $settings = self::$settingsCache[$bean->getModuleName()] ?? [];
-
-        return $settings;
-    }
-
-    private function checkSynchronizationStatus(string $moduleName): void
-    {
-        // check and get a list of out of sync fields
-        $potentiallyOutOfSync = array_filter(
-            self::$settingsCache[$moduleName],
-            function ($fieldOptions) {
-                return empty($fieldOptions['synchronization_confirmed']);
-            }
-        );
-
-        if (count($potentiallyOutOfSync) > 0) {
-            // here we reset cache and populating it again
-            $administration = Administration::getSettings('denormalization', true);
-            $updatedSettings = $administration->settings['denormalization_fields']
-                ?? [];
-            $updatedModuleSettings = $updatedSettings[$moduleName] ?? [];
-
-            // now set up confirmation flags
-            $updateRequired = false;
-            foreach ($potentiallyOutOfSync as $field => $options) {
-                if (!empty($updatedModuleSettings)
-                    && $updatedModuleSettings[$field]['synchronization_in_progress'] === false) {
-                    $updatedSettings[$moduleName][$field]['synchronization_confirmed'] = true;
-                    $updateRequired = true;
-                }
-            }
-
-            // update if confirmed
-            if ($updateRequired) {
-                $administration->saveSetting('denormalization', 'fields', $updatedSettings, 'base');
-                self::$settingsCache = $updatedSettings;
-            }
-        }
+        return $this->config->getModuleConfiguration($bean->getModuleName());
     }
 
     private function handleRelationshipModification(string $fieldName, SugarBean $bean, array $options): void
