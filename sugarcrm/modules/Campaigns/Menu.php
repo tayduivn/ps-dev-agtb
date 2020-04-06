82a29c2b6750aae25ca41a066150ae4d4bc637be
jwelborn-sugar <jwelborn@sugarcrm.com>
2020-04-06 12:33:52 -0400

    SS-374 Remove Campaign Email Setup Wizard files completely
    coding standards

--- sugarcrm/modules/Campaigns/Menu.php
+++ sugarcrm/modules/Campaigns/Menu.php
@@ -18,43 +18,51 @@
  ********************************************************************************/
 
 global $mod_strings, $app_strings;
-if(ACLController::checkAccess('Campaigns', 'edit', true))
-	$module_menu[] = array(
-		"index.php?module=Campaigns&action=WizardHome&return_module=Campaigns&return_action=index", 
-		$mod_strings['LNL_NEW_CAMPAIGN_WIZARD'],"CampaignsWizard"
-	);
-if(ACLController::checkAccess('Campaigns', 'edit', true))
-	$module_menu[]=	array(
-		"index.php?module=Campaigns&action=EditView&return_module=Campaigns&return_action=index", 
-		$mod_strings['LNK_NEW_CAMPAIGN'],"CreateCampaigns"
-	);
-if(ACLController::checkAccess('Campaigns', 'list', true))
-	$module_menu[]=	array(
-		"index.php?module=Campaigns&action=index&return_module=Campaigns&return_action=index", 
-		$mod_strings['LNK_CAMPAIGN_LIST'],"Campaigns"
-	);
-if(ACLController::checkAccess('Campaigns', 'list', true))
-	$module_menu[]= array(
-		"index.php?module=Campaigns&action=newsletterlist&return_module=Campaigns&return_action=index", 
-		$mod_strings['LBL_NEWSLETTERS'], "Newsletters"
-	);
-if(ACLController::checkAccess('EmailTemplates', 'edit', true))
-	$module_menu[] = array(
-		"index.php?module=EmailTemplates&action=EditView&return_module=EmailTemplates&return_action=DetailView",
-		$mod_strings['LNK_NEW_EMAIL_TEMPLATE'],"CreateEmails","Emails"
-	);
-if(ACLController::checkAccess('EmailTemplates', 'list', true))
-	$module_menu[] = array(
-		"index.php?module=EmailTemplates&action=index",
-		$mod_strings['LNK_EMAIL_TEMPLATE_LIST'],"EmailFolder", 'Emails'
-	);
-if(ACLController::checkAccess('Campaigns', 'edit', true))
-	$module_menu[] = array(
-		"index.php?module=Campaigns&action=CampaignDiagnostic&return_module=Campaigns&return_action=index",
-		$mod_strings['LBL_DIAGNOSTIC_WIZARD'],"EmailDiagnostic"
-	);
-if(ACLController::checkAccess('Campaigns', 'edit', true))
-	$module_menu[] = array(
-		"index.php?module=Campaigns&action=WebToLeadCreation&return_module=Campaigns&return_action=index",
-		$mod_strings['LBL_WEB_TO_LEAD'],"CreateWebToLeadForm"
-	);
+if (ACLController::checkAccess('Campaigns', 'edit', true)) {
+    $module_menu[] = array(
+        "index.php?module=Campaigns&action=WizardHome&return_module=Campaigns&return_action=index",
+        $mod_strings['LNL_NEW_CAMPAIGN_WIZARD'],"CampaignsWizard",
+    );
+}
+if (ACLController::checkAccess('Campaigns', 'edit', true)) {
+    $module_menu[] = array(
+        "index.php?module=Campaigns&action=EditView&return_module=Campaigns&return_action=index",
+        $mod_strings['LNK_NEW_CAMPAIGN'],"CreateCampaigns",
+    );
+}
+if (ACLController::checkAccess('Campaigns', 'list', true)) {
+    $module_menu[] = array(
+        "index.php?module=Campaigns&action=index&return_module=Campaigns&return_action=index",
+        $mod_strings['LNK_CAMPAIGN_LIST'],"Campaigns",
+    );
+}
+if (ACLController::checkAccess('Campaigns', 'list', true)) {
+    $module_menu[] = array(
+        "index.php?module=Campaigns&action=newsletterlist&return_module=Campaigns&return_action=index",
+        $mod_strings['LBL_NEWSLETTERS'], "Newsletters",
+    );
+}
+if (ACLController::checkAccess('EmailTemplates', 'edit', true)) {
+    $module_menu[] = array(
+        "index.php?module=EmailTemplates&action=EditView&return_module=EmailTemplates&return_action=DetailView",
+        $mod_strings['LNK_NEW_EMAIL_TEMPLATE'],"CreateEmails","Emails",
+    );
+}
+if (ACLController::checkAccess('EmailTemplates', 'list', true)) {
+    $module_menu[] = array(
+        "index.php?module=EmailTemplates&action=index",
+        $mod_strings['LNK_EMAIL_TEMPLATE_LIST'],"EmailFolder", "Emails",
+    );
+}
+if (ACLController::checkAccess('Campaigns', 'edit', true)) {
+    $module_menu[] = array(
+        "index.php?module=Campaigns&action=CampaignDiagnostic&return_module=Campaigns&return_action=index",
+        $mod_strings['LBL_DIAGNOSTIC_WIZARD'],"EmailDiagnostic",
+    );
+}
+if (ACLController::checkAccess('Campaigns', 'edit', true)) {
+    $module_menu[] = array(
+        "index.php?module=Campaigns&action=WebToLeadCreation&return_module=Campaigns&return_action=index",
+        $mod_strings['LBL_WEB_TO_LEAD'],"CreateWebToLeadForm",
+    );
+}
