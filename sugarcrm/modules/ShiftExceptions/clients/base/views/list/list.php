a3a733681109e5bcb430fcbc9e06a19e25a56b08
Roger Yang <roger.yang@sugarcrm.com>
2020-04-17 14:08:29 -0400

    CS-612 List View for Shift Exceptions Module
    - Adjusted list view for Shift Exceptions Module per acceptance criteria

---
+++ sugarcrm/modules/ShiftExceptions/clients/base/views/list/list.php
@@ -0,0 +1,121 @@
+<?php
+
+/*
+ * Your installation or use of this SugarCRM file is subject to the applicable
+ * terms available at
+ * http://support.sugarcrm.com/Resources/Master_Subscription_Agreements/.
+ * If you do not agree to all of the applicable terms or do not have the
+ * authority to bind the entity as an authorized representative, then do not
+ * install or use this SugarCRM file.
+ *
+ * Copyright (C) SugarCRM Inc. All rights reserved.
+ */
+
+$viewdefs['ShiftExceptions']['base']['view']['list'] = [
+    'name' => 'LBL_MODULE_NAME',
+    'panels' => [
+        [
+            'label' => 'LBL_PANEL_1',
+            'fields' => [
+                [
+                    'name' => 'date_start',
+                    'label' => 'LBL_CALENDAR_START_DATE',
+                    'default' => true,
+                    'enabled' => true,
+                ],
+                [
+                    'name' => 'date_end',
+                    'label' => 'LBL_CALENDAR_END_DATE',
+                    'default' => true,
+                    'enabled' => true,
+                ],
+                [
+                    'name' => 'name',
+                    'label' => 'LBL_NAME',
+                    'default' => true,
+                    'enabled' => true,
+                    'link' => true,
+                ],
+                [
+                    'name' => 'type',
+                    'label' => 'LBL_TYPE',
+                    'default' => true,
+                    'enabled' => true,
+                ],
+                [
+                    'name' => 'start_time',
+                    'label' => 'LBL_START_TIME',
+                    'default' => false,
+                    'enabled' => true,
+                    'readonly' => true,
+                ],
+                [
+                    'name' => 'end_time',
+                    'label' => 'LBL_END_TIME',
+                    'default' => false,
+                    'enabled' => true,
+                    'readonly' => true,
+                ],
+                [
+                    'name' => 'timezone',
+                    'label' => 'LBL_TIMEZONE',
+                    'default' => false,
+                    'enabled' => true,
+                ],
+                [
+                    'name' => 'all_day',
+                    'label' => 'LBL_ALL_DAY',
+                    'default' => false,
+                    'enabled' => true,
+                ],
+                [
+                    'name' => 'enabled',
+                    'label' => 'LBL_ENABLED',
+                    'default' => false,
+                    'enabled' => true,
+                ],
+                [
+                    'name' => 'assigned_user_name',
+                    'label' => 'LBL_ASSIGNED_TO_NAME',
+                    'default' => false,
+                    'enabled' => true,
+                    'link' => true,
+                ],
+                [
+                    'name' => 'team_name',
+                    'label' => 'LBL_TEAM',
+                    'default' => false,
+                    'enabled' => true,
+                ],
+                [
+                    'name' => 'created_by_name',
+                    'label' => 'LBL_CREATED',
+                    'default' => false,
+                    'enabled' => true,
+                    'readonly' => true,
+                    'sortable' => true,
+                ],
+                [
+                    'name' => 'date_entered',
+                    'label' => 'LBL_DATE_ENTERED',
+                    'default' => false,
+                    'enabled' => true,
+                ],
+                [
+                    'name' => 'modified_by_name',
+                    'label' => 'LBL_MODIFIED',
+                    'default' => false,
+                    'enabled' => true,
+                    'readonly' => true,
+                    'sortable' => true,
+                ],
+                [
+                    'name' => 'date_modified',
+                    'label' => 'LBL_DATE_MODIFIED',
+                    'default' => false,
+                    'enabled' => true,
+                ],
+            ],
+        ],
+    ],
+];
