9af8fc55bf0070abe3f501a33ca49822b146709b
Egor Kravchuk <ekravchuk@sugarcrm.com>
2020-05-29 11:42:27 +0300

    BR-7649: Possible Reflected/Stored issues (Subgroup 4)

--- sugarcrm/modules/Roles/DeleteUserRelationship.php
+++ sugarcrm/modules/Roles/DeleteUserRelationship.php
@@ -26,8 +26,13 @@ $focus = BeanFactory::getBean('Roles', $_REQUEST['record']);
 
 $focus->clear_user_relationship($focus->id, $_REQUEST['user_id']);
 
-$header_URL = "Location: index.php?action={$_REQUEST['return_action']}&module={$_REQUEST['return_module']}&record={$_REQUEST['return_id']}";
+$query = http_build_query(array(
+    'module' => $_REQUEST['return_module'],
+    'action' => $_REQUEST['return_action'],
+    'record' => $_REQUEST['return_id'],
+));
+
+$header_URL = 'Location: index.php?' . $query;
 $GLOBALS['log']->debug("about to post header URL of: $header_URL");
 
 header($header_URL);
-?>
