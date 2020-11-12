16753464e495eaeb68201c7080c0eede63f35988
nbleyh-sugarcrm <nbleyh@sugarcrm.com>
2020-11-12 11:59:18 +0100

    AGTB-7: Fields and some Views of new module Positions

---
+++ sugarcrm/custom/Extension/application/Ext/Include/gtb_positions.php
@@ -0,0 +1,7 @@
+<?php 
+ //WARNING: The contents of this file are auto-generated
+$beanList['gtb_positions'] = 'gtb_positions';
+$beanFiles['gtb_positions'] = 'modules/gtb_positions/gtb_positions.php';
+$moduleList[] = 'gtb_positions';
+
+?>
---
+++ sugarcrm/modules/gtb_positions/clients/base/filters/default/default.php
@@ -0,0 +1,54 @@
+<?php
+// created: 2020-11-12 10:49:17
+$viewdefs['gtb_positions']['base']['filter']['default'] = array (
+  'default_filter' => 'all_records',
+  'fields' => 
+  array (
+    'name' => 
+    array (
+    ),
+    'tag' => 
+    array (
+    ),
+    'assigned_user_name' => 
+    array (
+    ),
+    '$owner' => 
+    array (
+      'predefined_filter' => true,
+      'vname' => 'LBL_CURRENT_USER_FILTER',
+    ),
+    '$favorite' => 
+    array (
+      'predefined_filter' => true,
+      'vname' => 'LBL_FAVORITES_FILTER',
+    ),
+    'date_entered' => 
+    array (
+    ),
+    'date_modified' => 
+    array (
+    ),
+    'pos_function' => 
+    array (
+    ),
+    'real_position' => 
+    array (
+    ),
+    'process_step' => 
+    array (
+    ),
+    'org_unit' => 
+    array (
+    ),
+    'status' => 
+    array (
+    ),
+    'location' => 
+    array (
+    ),
+    'gtb_source' => 
+    array (
+    ),
+  ),
+);
