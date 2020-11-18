b22bd5dbaa1fc6b81420ee36efcd21510bc92644
Constantine Shpikat <cshpikat@sugarcrm.com>
2020-11-18 09:50:49 +0100

    AGTB-1: Fix-bug: All languages are still there

--- customer/upgrade/agtb_102_phase1/scripts/php/post/updateLanguages.php
+++ customer/upgrade/agtb_102_phase1/scripts/php/post/updateLanguages.php
@@ -12,8 +12,7 @@ $logger->debug(sprintf('%s: Starting upgrade script', $_SERVER['argv'][0]));
 try {
     $cf = new Configurator();
     $cf->loadConfig();
-    $cf->config['languages'] = array('en_us' => 'English (US)');
-
+    $cf->config['disabled_languages'] = 'bg_BG,cs_CZ,el_EL,fi_FI,da_DK,hu_HU,ro_RO,ja_JP,he_IL,ko_KR,sr_RS,sk_SK,es_LA,en_UK,sq_AL,et_EE,uk_UA,ar_SA,lv_LV,lt_LT,pt_BR,hr_HR,zh_TW,ca_ES,tr_TR,th_TH,ru_RU,sv_SE,nl_NL,nb_NO';
 
     $cf->handleOverride();
 } catch (Exception $e) {
