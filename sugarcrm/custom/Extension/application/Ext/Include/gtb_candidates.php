ef8a66c72bb268119425d5bb49ad48285e51986d
nbleyh-sugarcrm <nbleyh@sugarcrm.com>
2020-11-19 11:46:10 +0100

    AGTB-5: Outdated include file

--- sugarcrm/custom/Extension/application/Ext/Include/gtb_candidates.php
+++
@@ -1,7 +0,0 @@
-<?php 
- //WARNING: The contents of this file are auto-generated
-$beanList['gtb_candidates'] = 'gtb_candidates';
-$beanFiles['gtb_candidates'] = 'modules/gtb_candidates/gtb_candidates.php';
-$moduleList[] = 'gtb_candidates';
-
-?>
