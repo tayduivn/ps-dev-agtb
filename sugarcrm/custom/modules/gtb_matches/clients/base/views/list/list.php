310d0c65f48fc953637c72749e9bd6f317754162
Constantine Shpikat <cshpikat@sugarcrm.com>
2020-11-20 16:35:46 +0100

    AGTB-22: Update Views

---
+++ sugarcrm/custom/modules/gtb_matches/clients/base/views/list/list.php
@@ -0,0 +1,116 @@
+<?php
+$module_name = 'gtb_matches';
+$viewdefs[$module_name]['base']['view']['list'] = array (
+  'panels' => 
+  array (
+    0 => 
+    array (
+      'label' => 'LBL_PANEL_1',
+      'fields' => 
+      array (
+        0 => 
+        array (
+          'name' => 'name',
+          'label' => 'LBL_NAME',
+          'default' => true,
+          'enabled' => true,
+          'link' => true,
+        ),
+        1 => 
+        array (
+          'name' => 'contacts_gtb_matches_1_name',
+          'label' => 'LBL_CONTACTS_GTB_MATCHES_1_FROM_CONTACTS_TITLE',
+          'enabled' => true,
+          'id' => 'CONTACTS_GTB_MATCHES_1CONTACTS_IDA',
+          'link' => true,
+          'sortable' => false,
+          'default' => true,
+        ),
+        2 => 
+        array (
+          'name' => 'gtb_positions_gtb_matches_1_name',
+          'label' => 'LBL_GTB_POSITIONS_GTB_MATCHES_1_FROM_GTB_POSITIONS_TITLE',
+          'enabled' => true,
+          'id' => 'GTB_POSITIONS_GTB_MATCHES_1GTB_POSITIONS_IDA',
+          'link' => true,
+          'sortable' => false,
+          'default' => true,
+        ),
+        3 => 
+        array (
+          'name' => 'status',
+          'label' => 'LBL_STATUS',
+          'enabled' => true,
+          'default' => true,
+        ),
+        4 => 
+        array (
+          'name' => 'stage',
+          'label' => 'LBL_STAGE',
+          'enabled' => true,
+          'default' => true,
+        ),
+        5 => 
+        array (
+          'name' => 'fulfillment',
+          'label' => 'LBL_FULFILLMENT',
+          'enabled' => true,
+          'default' => true,
+        ),
+        6 => 
+        array (
+          'name' => 'func_mobility_fulfilled',
+          'label' => 'LBL_FUNC_MOBILITY_FULFILLED',
+          'enabled' => true,
+          'default' => false,
+        ),
+        7 => 
+        array (
+          'name' => 'geo_mobility_fulfilled',
+          'label' => 'LBL_GEO_MOBILITY_FULFILLED',
+          'enabled' => true,
+          'default' => false,
+        ),
+        8 => 
+        array (
+          'name' => 'oe_mobility_fulfilled',
+          'label' => 'LBL_OE_MOBILITY_FULFILLED',
+          'enabled' => true,
+          'default' => false,
+        ),
+        9 => 
+        array (
+          'name' => 'assigned_user_name',
+          'label' => 'LBL_ASSIGNED_TO_NAME',
+          'default' => false,
+          'enabled' => true,
+          'link' => true,
+        ),
+        10 => 
+        array (
+          'name' => 'date_modified',
+          'enabled' => true,
+          'default' => false,
+        ),
+        11 => 
+        array (
+          'name' => 'date_entered',
+          'enabled' => true,
+          'default' => false,
+        ),
+        12 => 
+        array (
+          'name' => 'team_name',
+          'label' => 'LBL_TEAM',
+          'default' => false,
+          'enabled' => true,
+        ),
+      ),
+    ),
+  ),
+  'orderBy' => 
+  array (
+    'field' => 'date_modified',
+    'direction' => 'desc',
+  ),
+);
--- sugarcrm/custom/modules/gtb_matches/clients/base/views/record/record.php
+++ sugarcrm/custom/modules/gtb_matches/clients/base/views/record/record.php
@@ -1,297 +1,281 @@
 <?php
 $module_name = 'gtb_matches';
-$viewdefs[$module_name] = 
-array (
-  'base' => 
+$viewdefs[$module_name]['base']['view']['record'] = array (
+  'buttons' => 
   array (
-    'view' => 
+    0 => 
     array (
-      'record' => 
+      'type' => 'button',
+      'name' => 'cancel_button',
+      'label' => 'LBL_CANCEL_BUTTON_LABEL',
+      'css_class' => 'btn-invisible btn-link',
+      'showOn' => 'edit',
+      'events' => 
       array (
-        'buttons' => 
+        'click' => 'button:cancel_button:click',
+      ),
+    ),
+    1 => 
+    array (
+      'type' => 'rowaction',
+      'event' => 'button:save_button:click',
+      'name' => 'save_button',
+      'label' => 'LBL_SAVE_BUTTON_LABEL',
+      'css_class' => 'btn btn-primary',
+      'showOn' => 'edit',
+      'acl_action' => 'edit',
+    ),
+    2 => 
+    array (
+      'type' => 'actiondropdown',
+      'name' => 'main_dropdown',
+      'primary' => true,
+      'showOn' => 'view',
+      'buttons' => 
+      array (
+        0 => 
         array (
-          0 => 
-          array (
-            'type' => 'button',
-            'name' => 'cancel_button',
-            'label' => 'LBL_CANCEL_BUTTON_LABEL',
-            'css_class' => 'btn-invisible btn-link',
-            'showOn' => 'edit',
-            'events' => 
-            array (
-              'click' => 'button:cancel_button:click',
-            ),
-          ),
-          1 => 
-          array (
-            'type' => 'rowaction',
-            'event' => 'button:save_button:click',
-            'name' => 'save_button',
-            'label' => 'LBL_SAVE_BUTTON_LABEL',
-            'css_class' => 'btn btn-primary',
-            'showOn' => 'edit',
-            'acl_action' => 'edit',
-          ),
-          2 => 
-          array (
-            'type' => 'actiondropdown',
-            'name' => 'main_dropdown',
-            'primary' => true,
-            'showOn' => 'view',
-            'buttons' => 
-            array (
-              0 => 
-              array (
-                'type' => 'rowaction',
-                'event' => 'button:edit_button:click',
-                'name' => 'edit_button',
-                'label' => 'LBL_EDIT_BUTTON_LABEL',
-                'acl_action' => 'edit',
-              ),
-              1 => 
-              array (
-                'type' => 'shareaction',
-                'name' => 'share',
-                'label' => 'LBL_RECORD_SHARE_BUTTON',
-                'acl_action' => 'view',
-              ),
-              2 => 
-              array (
-                'type' => 'pdfaction',
-                'name' => 'download-pdf',
-                'label' => 'LBL_PDF_VIEW',
-                'action' => 'download',
-                'acl_action' => 'view',
-              ),
-              3 => 
-              array (
-                'type' => 'pdfaction',
-                'name' => 'email-pdf',
-                'label' => 'LBL_PDF_EMAIL',
-                'action' => 'email',
-                'acl_action' => 'view',
-              ),
-              4 => 
-              array (
-                'type' => 'divider',
-              ),
-              5 => 
-              array (
-                'type' => 'rowaction',
-                'event' => 'button:find_duplicates_button:click',
-                'name' => 'find_duplicates_button',
-                'label' => 'LBL_DUP_MERGE',
-                'acl_action' => 'edit',
-              ),
-              6 => 
-              array (
-                'type' => 'rowaction',
-                'event' => 'button:duplicate_button:click',
-                'name' => 'duplicate_button',
-                'label' => 'LBL_DUPLICATE_BUTTON_LABEL',
-                'acl_module' => 'gtb_matches',
-                'acl_action' => 'create',
-              ),
-              7 => 
-              array (
-                'type' => 'rowaction',
-                'event' => 'button:audit_button:click',
-                'name' => 'audit_button',
-                'label' => 'LNK_VIEW_CHANGE_LOG',
-                'acl_action' => 'view',
-              ),
-              8 => 
-              array (
-                'type' => 'divider',
-              ),
-              9 => 
-              array (
-                'type' => 'rowaction',
-                'event' => 'button:delete_button:click',
-                'name' => 'delete_button',
-                'label' => 'LBL_DELETE_BUTTON_LABEL',
-                'acl_action' => 'delete',
-              ),
-            ),
-          ),
-          3 => 
-          array (
-            'name' => 'sidebar_toggle',
-            'type' => 'sidebartoggle',
-          ),
+          'type' => 'rowaction',
+          'event' => 'button:edit_button:click',
+          'name' => 'edit_button',
+          'label' => 'LBL_EDIT_BUTTON_LABEL',
+          'acl_action' => 'edit',
         ),
-        'panels' => 
+        1 => 
         array (
-          0 => 
-          array (
-            'name' => 'panel_header',
-            'label' => 'LBL_RECORD_HEADER',
-            'header' => true,
-            'fields' => 
-            array (
-              0 => 
-              array (
-                'name' => 'picture',
-                'type' => 'avatar',
-                'width' => 42,
-                'height' => 42,
-                'dismiss_label' => true,
-                'readonly' => true,
-                'size' => 'large',
-              ),
-              1 => 'name',
-              2 => 
-              array (
-                'name' => 'favorite',
-                'label' => 'LBL_FAVORITE',
-                'type' => 'favorite',
-                'readonly' => true,
-                'dismiss_label' => true,
-              ),
-              3 => 
-              array (
-                'name' => 'follow',
-                'label' => 'LBL_FOLLOW',
-                'type' => 'follow',
-                'readonly' => true,
-                'dismiss_label' => true,
-              ),
-            ),
-          ),
-          1 => 
-          array (
-            'name' => 'panel_body',
-            'label' => 'LBL_RECORD_BODY',
-            'columns' => 2,
-            'placeholders' => true,
-            'newTab' => true,
-            'panelDefault' => 'expanded',
-            'fields' => 
-            array (
-              0 => 
-              array (
-                'name' => 'contacts_gtb_matches_1_name',
-              ),
-              1 => 
-              array (
-                'name' => 'gtb_positions_gtb_matches_1_name',
-              ),
-              2 => 
-              array (
-                'name' => 'status',
-                'label' => 'LBL_STATUS',
-              ),
-              3 => 
-              array (
-                'name' => 'stage',
-                'label' => 'LBL_STAGE',
-              ),
-              4 => 
-              array (
-                'name' => 'fulfillment',
-                'label' => 'LBL_FULFILLMENT',
-                'span' => 6,
-              ),
-              5 => 
-              array (
-                'span' => 6,
-              ),
-              6 => 
-              array (
-                'name' => 'func_mobility_fulfilled',
-                'label' => 'LBL_FUNC_MOBILITY_FULFILLED',
-              ),
-              7 => 
-              array (
-                'name' => 'geo_mobility_fulfilled',
-                'label' => 'LBL_GEO_MOBILITY_FULFILLED',
-              ),
-              8 => 
-              array (
-                'name' => 'oe_mobility_fulfilled',
-                'label' => 'LBL_OE_MOBILITY_FULFILLED',
-              ),
-              9 => 
-              array (
-                'name' => 'tag',
-              ),
-              10 => 
-              array (
-              ),
-              11 => 
-              array (
-              ),
-            ),
-          ),
-          2 => 
-          array (
-            'name' => 'panel_hidden',
-            'label' => 'LBL_SHOW_MORE',
-            'hide' => true,
-            'columns' => 2,
-            'placeholders' => true,
-            'newTab' => true,
-            'panelDefault' => 'expanded',
-            'fields' => 
-            array (
-              0 => 'assigned_user_name',
-              1 => 
-              array (
-                'name' => 'date_entered_by',
-                'readonly' => true,
-                'inline' => true,
-                'type' => 'fieldset',
-                'label' => 'LBL_DATE_ENTERED',
-                'fields' => 
-                array (
-                  0 => 
-                  array (
-                    'name' => 'date_entered',
-                  ),
-                  1 => 
-                  array (
-                    'type' => 'label',
-                    'default_value' => 'LBL_BY',
-                  ),
-                  2 => 
-                  array (
-                    'name' => 'created_by_name',
-                  ),
-                ),
-              ),
-              2 => 'team_name',
-              3 => 
-              array (
-                'name' => 'date_modified_by',
-                'readonly' => true,
-                'inline' => true,
-                'type' => 'fieldset',
-                'label' => 'LBL_DATE_MODIFIED',
-                'fields' => 
-                array (
-                  0 => 
-                  array (
-                    'name' => 'date_modified',
-                  ),
-                  1 => 
-                  array (
-                    'type' => 'label',
-                    'default_value' => 'LBL_BY',
-                  ),
-                  2 => 
-                  array (
-                    'name' => 'modified_by_name',
-                  ),
-                ),
-              ),
-            ),
-          ),
+          'type' => 'shareaction',
+          'name' => 'share',
+          'label' => 'LBL_RECORD_SHARE_BUTTON',
+          'acl_action' => 'view',
         ),
-        'templateMeta' => 
+        2 => 
         array (
-          'useTabs' => true,
+          'type' => 'pdfaction',
+          'name' => 'download-pdf',
+          'label' => 'LBL_PDF_VIEW',
+          'action' => 'download',
+          'acl_action' => 'view',
+        ),
+        3 => 
+        array (
+          'type' => 'pdfaction',
+          'name' => 'email-pdf',
+          'label' => 'LBL_PDF_EMAIL',
+          'action' => 'email',
+          'acl_action' => 'view',
+        ),
+        4 => 
+        array (
+          'type' => 'divider',
+        ),
+        5 => 
+        array (
+          'type' => 'rowaction',
+          'event' => 'button:find_duplicates_button:click',
+          'name' => 'find_duplicates_button',
+          'label' => 'LBL_DUP_MERGE',
+          'acl_action' => 'edit',
+        ),
+        6 => 
+        array (
+          'type' => 'rowaction',
+          'event' => 'button:duplicate_button:click',
+          'name' => 'duplicate_button',
+          'label' => 'LBL_DUPLICATE_BUTTON_LABEL',
+          'acl_module' => 'gtb_matches',
+          'acl_action' => 'create',
+        ),
+        7 => 
+        array (
+          'type' => 'rowaction',
+          'event' => 'button:audit_button:click',
+          'name' => 'audit_button',
+          'label' => 'LNK_VIEW_CHANGE_LOG',
+          'acl_action' => 'view',
+        ),
+        8 => 
+        array (
+          'type' => 'divider',
+        ),
+        9 => 
+        array (
+          'type' => 'rowaction',
+          'event' => 'button:delete_button:click',
+          'name' => 'delete_button',
+          'label' => 'LBL_DELETE_BUTTON_LABEL',
+          'acl_action' => 'delete',
         ),
       ),
     ),
+    3 => 
+    array (
+      'name' => 'sidebar_toggle',
+      'type' => 'sidebartoggle',
+    ),
+  ),
+  'panels' => 
+  array (
+    0 => 
+    array (
+      'name' => 'panel_header',
+      'label' => 'LBL_RECORD_HEADER',
+      'header' => true,
+      'fields' => 
+      array (
+        0 => 
+        array (
+          'name' => 'picture',
+          'type' => 'avatar',
+          'width' => 42,
+          'height' => 42,
+          'dismiss_label' => true,
+          'readonly' => true,
+          'size' => 'large',
+        ),
+        1 => 'name',
+        2 => 
+        array (
+          'name' => 'favorite',
+          'label' => 'LBL_FAVORITE',
+          'type' => 'favorite',
+          'readonly' => true,
+          'dismiss_label' => true,
+        ),
+        3 => 
+        array (
+          'name' => 'follow',
+          'label' => 'LBL_FOLLOW',
+          'type' => 'follow',
+          'readonly' => true,
+          'dismiss_label' => true,
+        ),
+      ),
+    ),
+    1 => 
+    array (
+      'name' => 'panel_body',
+      'label' => 'LBL_RECORD_BODY',
+      'columns' => 2,
+      'placeholders' => true,
+      'newTab' => true,
+      'panelDefault' => 'expanded',
+      'fields' => 
+      array (
+        0 => 
+        array (
+          'name' => 'contacts_gtb_matches_1_name',
+        ),
+        1 => 
+        array (
+          'name' => 'gtb_positions_gtb_matches_1_name',
+        ),
+        2 => 
+        array (
+          'name' => 'status',
+          'label' => 'LBL_STATUS',
+        ),
+        3 => 
+        array (
+          'name' => 'stage',
+          'label' => 'LBL_STAGE',
+        ),
+        4 => 
+        array (
+          'name' => 'fulfillment',
+          'label' => 'LBL_FULFILLMENT',
+          'span' => 6,
+        ),
+        5 => 
+        array (
+          'span' => 6,
+        ),
+        6 => 
+        array (
+          'name' => 'func_mobility_fulfilled',
+          'label' => 'LBL_FUNC_MOBILITY_FULFILLED',
+        ),
+        7 => 
+        array (
+          'name' => 'geo_mobility_fulfilled',
+          'label' => 'LBL_GEO_MOBILITY_FULFILLED',
+        ),
+        8 => 
+        array (
+          'name' => 'oe_mobility_fulfilled',
+          'label' => 'LBL_OE_MOBILITY_FULFILLED',
+        ),
+        9 => 
+        array (
+          'name' => 'tag',
+        ),
+      ),
+    ),
+    2 => 
+    array (
+      'name' => 'panel_hidden',
+      'label' => 'LBL_SHOW_MORE',
+      'hide' => true,
+      'columns' => 2,
+      'placeholders' => true,
+      'newTab' => true,
+      'panelDefault' => 'expanded',
+      'fields' => 
+      array (
+        0 => 'assigned_user_name',
+        1 => 
+        array (
+          'name' => 'date_entered_by',
+          'readonly' => true,
+          'inline' => true,
+          'type' => 'fieldset',
+          'label' => 'LBL_DATE_ENTERED',
+          'fields' => 
+          array (
+            0 => 
+            array (
+              'name' => 'date_entered',
+            ),
+            1 => 
+            array (
+              'type' => 'label',
+              'default_value' => 'LBL_BY',
+            ),
+            2 => 
+            array (
+              'name' => 'created_by_name',
+            ),
+          ),
+        ),
+        2 => 'team_name',
+        3 => 
+        array (
+          'name' => 'date_modified_by',
+          'readonly' => true,
+          'inline' => true,
+          'type' => 'fieldset',
+          'label' => 'LBL_DATE_MODIFIED',
+          'fields' => 
+          array (
+            0 => 
+            array (
+              'name' => 'date_modified',
+            ),
+            1 => 
+            array (
+              'type' => 'label',
+              'default_value' => 'LBL_BY',
+            ),
+            2 => 
+            array (
+              'name' => 'modified_by_name',
+            ),
+          ),
+        ),
+      ),
+    ),
+  ),
+  'templateMeta' => 
+  array (
+    'useTabs' => true,
   ),
 );
