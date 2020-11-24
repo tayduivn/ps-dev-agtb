cfa5c221809bcc59ad83177b9323a28fc63072c7
nbleyh-sugarcrm <nbleyh@sugarcrm.com>
2020-11-24 12:33:29 +0100

    AGTB-39: Country dependency

---
+++ sugarcrm/custom/Extension/modules/gtb_positions/Ext/Vardefs/sugarfield_country.php
@@ -0,0 +1,82 @@
+<?php
+ // created: 2020-11-12 12:01:23
+$dictionary['gtb_positions']['fields']['country']['visibility_grid']=array (
+  'trigger' => 'region',
+  'values' => 
+  array (
+    '' => 
+    array (
+      0 => '',
+    ),
+    'Americas' => 
+    array (
+      0 => 'USA',
+      1 => 'BRAZIL',
+      2 => 'CANADA',
+      3 => 'COLOMBIA',
+    ),
+    'Asia_Pacific' => 
+    array (
+      0 => 'HONG KONG',
+      1 => 'SINGAPORE',
+      2 => 'PHILIPPINES',
+      3 => 'TAIWAN',
+      4 => 'THAILAND',
+      5 => 'INDIA',
+      6 => 'JAPAN',
+      7 => 'CHINA',
+      8 => 'KOREA, SOUTH',
+      9 => 'AUSTRALIA',
+      10 => 'INDONESIA',
+      11 => 'MALAYSIA',
+    ),
+    'CEE' => 
+    array (
+      0 => 'BULGARIA',
+      1 => 'ROMANIA',
+      2 => 'TURKEY',
+      3 => 'CZECH REPUBLIC',
+      4 => 'POLAND',
+      5 => 'RUSSIA',
+      6 => 'CROATIA',
+      7 => 'AUSTRIA',
+      8 => 'SLOVAKIA',
+    ),
+    'Africa' => 
+    array (
+      0 => 'SAUDI ARABIA',
+      1 => 'SOUTH AFRICA',
+      2 => 'TUNISIA',
+      3 => 'KENYA',
+      4 => 'UNITED ARAB EMIRATES',
+      5 => 'MOROCCO',
+      6 => 'REUNION',
+      7 => 'LEBANON',
+    ),
+    'Europe' => 
+    array (
+      0 => 'GERMANY',
+      1 => 'FRANCE',
+      2 => 'ITALY',
+      3 => 'LUXEMBOURG',
+      4 => 'SWITZERLAND',
+      5 => 'UNITED KINGDOM',
+      6 => 'IRELAND',
+      7 => 'SPAIN',
+      8 => 'GREECE',
+      9 => 'NORWAY',
+      10 => 'SWEDEN',
+      11 => 'BELGIUM',
+      12 => 'DENMARK',
+      13 => 'NETHERLANDS',
+      14 => 'FINLAND',
+      15 => 'PORTUGAL',
+    ),
+    'Other' => 
+    array (
+      0 => 'OTHER',
+    ),
+  ),
+);
+
+ ?>
