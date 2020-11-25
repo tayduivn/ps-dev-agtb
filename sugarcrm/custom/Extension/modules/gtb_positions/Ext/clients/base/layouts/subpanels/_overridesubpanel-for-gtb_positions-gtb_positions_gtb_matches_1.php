33daac40006a3e149a81f109a1b7325fbcdf321d
nbleyh-sugarcrm <nbleyh@sugarcrm.com>
2020-11-25 11:47:57 +0100

    AGTB-37: Fix subpanels

---
+++ sugarcrm/custom/Extension/modules/gtb_positions/Ext/clients/base/layouts/subpanels/_overridesubpanel-for-gtb_positions-gtb_positions_gtb_matches_1.php
@@ -0,0 +1,6 @@
+<?php
+//auto-generated file DO NOT EDIT
+$viewdefs['gtb_positions']['base']['layout']['subpanels']['components'][]['override_subpanel_list_view'] = array (
+  'link' => 'gtb_positions_gtb_matches_1',
+  'view' => 'subpanel-for-gtb_positions-gtb_positions_gtb_matches_1',
+);
