3841a19b0e585fea24b397e74862618b01a69693
nbleyh-sugarcrm <nbleyh@sugarcrm.com>
2020-11-27 11:50:55 +0100

    AGTB-42: Fix dependency

--- sugarcrm/custom/Extension/modules/gtb_positions/Ext/Vardefs/sugarfield_country.php
+++ sugarcrm/custom/Extension/modules/gtb_positions/Ext/Vardefs/sugarfield_country.php
@@ -1,86 +1,87 @@
 <?php
- // created: 2020-11-12 12:01:23
+// created: 2020-11-13 13:15:05
 $dictionary['gtb_positions']['fields']['country']['visibility_grid']=array (
-  'trigger' => 'region',
-  'values' => 
-  array (
-    '' => 
-    array (
-      0 => '',
-    ),
-    'Americas' => 
-    array (
-      0 => 'USA',
-      1 => 'BRAZIL',
-      2 => 'CANADA',
-      3 => 'COLOMBIA',
-    ),
-    'Asia_Pacific' => 
-    array (
-      0 => 'HONG KONG',
-      1 => 'SINGAPORE',
-      2 => 'PHILIPPINES',
-      3 => 'TAIWAN',
-      4 => 'THAILAND',
-      5 => 'INDIA',
-      6 => 'JAPAN',
-      7 => 'CHINA',
-      8 => 'KOREA, SOUTH',
-      9 => 'AUSTRALIA',
-      10 => 'INDONESIA',
-      11 => 'MALAYSIA',
-        12 => '',
-    ),
-    'CEE' => 
-    array (
-      0 => 'BULGARIA',
-      1 => 'ROMANIA',
-      2 => 'TURKEY',
-      3 => 'CZECH REPUBLIC',
-      4 => 'POLAND',
-      5 => 'RUSSIA',
-      6 => 'CROATIA',
-      7 => 'AUSTRIA',
-      8 => 'SLOVAKIA',
-        9 => '',
-    ),
-    'Africa' => 
-    array (
-      0 => 'SAUDI ARABIA',
-      1 => 'SOUTH AFRICA',
-      2 => 'TUNISIA',
-      3 => 'KENYA',
-      4 => 'UNITED ARAB EMIRATES',
-      5 => 'MOROCCO',
-      6 => 'REUNION',
-      7 => 'LEBANON',
-        8 => '',
-    ),
-    'Europe' => 
-    array (
-      0 => 'GERMANY',
-      1 => 'FRANCE',
-      2 => 'ITALY',
-      3 => 'LUXEMBOURG',
-      4 => 'SWITZERLAND',
-      5 => 'UNITED KINGDOM',
-      6 => 'IRELAND',
-      7 => 'SPAIN',
-      8 => 'GREECE',
-      9 => 'NORWAY',
-      10 => 'SWEDEN',
-      11 => 'BELGIUM',
-      12 => 'DENMARK',
-      13 => 'NETHERLANDS',
-      14 => 'FINLAND',
-      15 => 'PORTUGAL',
-        16 => '',
-    ),
-    'Other' => 
-    array (
-      0 => 'OTHER',
-    ),
-  ),
+    'trigger' => 'region',
+    'values' =>
+        array (
+            '' =>
+                array (
+                    0 => '',
+                ),
+            'Americas' =>
+                array (
+                    0 => '',
+                    1 => 'BRAZIL',
+                    2 => 'CANADA',
+                    3 => 'COLOMBIA',
+                    4 => 'USA',
+                ),
+            'Asia_Pacific' =>
+                array (
+                    0 => '',
+                    1 => 'AUSTRALIA',
+                    2 => 'CHINA',
+                    3 => 'HONG KONG',
+                    4 => 'INDIA',
+                    5 => 'INDONESIA',
+                    6 => 'JAPAN',
+                    7 => 'MALAYSIA',
+                    8 => 'PHILIPPINES',
+                    9 => 'SINGAPORE',
+                    10 => 'KOREA, SOUTH',
+                    11 => 'TAIWAN',
+                    12 => 'THAILAND',
+                ),
+            'CEE' =>
+                array (
+                    0 => '',
+                    1 => 'AUSTRIA',
+                    2 => 'BULGARIA',
+                    3 => 'CROATIA',
+                    4 => 'CZECH REPUBLIC',
+                    5 => 'POLAND',
+                    6 => 'ROMANIA',
+                    7 => 'RUSSIA',
+                    8 => 'SLOVAKIA',
+                    9 => 'TURKEY',
+                ),
+            'Africa' =>
+                array (
+                    0 => '',
+                    1 => 'KENYA',
+                    2 => 'LEBANON',
+                    3 => 'MOROCCO',
+                    4 => 'REUNION',
+                    5 => 'SAUDI ARABIA',
+                    6 => 'SOUTH AFRICA',
+                    7 => 'TUNISIA',
+                    8 => 'UNITED ARAB EMIRATES',
+                ),
+            'Europe' =>
+                array (
+                    0 => '',
+                    1 => 'BELGIUM',
+                    2 => 'FRANCE',
+                    3 => 'GERMANY',
+                    4 => 'GREECE',
+                    5 => 'IRELAND',
+                    6 => 'ITALY',
+                    7 => 'LUXEMBOURG',
+                    8 => 'NORWAY',
+                    9 => 'SPAIN',
+                    10 => 'SWEDEN',
+                    11 => 'SWITZERLAND',
+                    12 => 'UNITED KINGDOM',
+                    13 => 'DENMARK',
+                    14 => 'NETHERLANDS',
+                    15 => 'FINLAND',
+                    16 => 'PORTUGAL',
+                ),
+            'Other' =>
+                array (
+                    0 => 'OTHER',
+                ),
+        ),
 );
 
- ?>
+?>
