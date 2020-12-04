ebf202557ce86d234a493fa5fa0b31c0eec18e77
nbleyh-sugarcrm <nbleyh@sugarcrm.com>
2020-12-04 15:34:55 +0100

    AGTB-67: Fix value list

--- sugarcrm/custom/Extension/application/Ext/Language/en_us.sugar_gtb_matches_stage_list.php
+++ sugarcrm/custom/Extension/application/Ext/Language/en_us.sugar_gtb_matches_stage_list.php
@@ -1,13 +1,15 @@
 <?php
 
-$app_list_strings['gtb_pos_process_step_list']['Position_Identified'] = '0. Position Identified';
-$app_list_strings['gtb_pos_process_step_list']['Contacted_Recruiter'] = '1.a Contacted Recruiter';
-$app_list_strings['gtb_pos_process_step_list']['Contacted_Manager'] = '1.b Contacted Hiring Manager';
-$app_list_strings['gtb_pos_process_step_list']['Identify_Candidates'] = '2. Identify Candidates';
-$app_list_strings['gtb_pos_process_step_list']['Profiles_Sent'] = '3. Profiles Sent';
-$app_list_strings['gtb_pos_process_step_list']['Refined_Search'] = '4.a Refined Search';
-$app_list_strings['gtb_pos_process_step_list']['On_Hold'] = '4.b On Hold';
-$app_list_strings['gtb_pos_process_step_list']['Filled_by_GTB'] = '5.a Filled by GTB';
-$app_list_strings['gtb_pos_process_step_list']['Not_Filled_by_GTB'] = '5.b Not Filled by GTB';
-$app_list_strings['gtb_pos_process_step_list']['Out_of_Scope'] = '5.c Out of Scope';
-$app_list_strings['gtb_pos_process_step_list']['Not_Truly_Vacant'] = '5.d Not Truly Vacant';
+$app_list_strings['gtb_matches_stage_list']['Cdd_identified'] = 'Cdd identified';
+$app_list_strings['gtb_matches_stage_list']['CV_shared'] = 'CV shared with HM';
+$app_list_strings['gtb_matches_stage_list']['Cdd_picked'] = 'Cdd was picked up';
+$app_list_strings['gtb_matches_stage_list']['Position_tobe_shared'] = 'Position to be shared w cdd';
+$app_list_strings['gtb_matches_stage_list']['Position_shared'] = 'Position shared with cdd';
+$app_list_strings['gtb_matches_stage_list']['Cdd_tobe_interviewed'] = 'Cdd to be interviewed';
+$app_list_strings['gtb_matches_stage_list']['Cdd_in_discussion'] = 'Cdd in discussion';
+$app_list_strings['gtb_matches_stage_list']['Cdd_not_interested'] = 'Cdd not interested';
+$app_list_strings['gtb_matches_stage_list']['Cdd_was_rejected'] = 'Cdd was rejected';
+$app_list_strings['gtb_matches_stage_list']['Cdd_received_offer'] = 'Cdd received offer';
+$app_list_strings['gtb_matches_stage_list']['move'] = 'It\'s a MOVE!';
+$app_list_strings['gtb_matches_stage_list']['Other'] = 'Other';
+$app_list_strings['gtb_matches_stage_list'][''] = '';
